6de876353de896de45bb177929ae8ccf1b37be29
vedatakd <vedat@vebze.com>
2019-06-24 18:08:03 +0300

    Import Demo Ads From Remote Source

--- addons/default/visiosoft/advs-module/src/AdvsModuleSeeder.php
+++ addons/default/visiosoft/advs-module/src/AdvsModuleSeeder.php
@@ -1,6 +1,8 @@
 <?php namespace Visiosoft\AdvsModule;
+
 use Anomaly\Streams\Platform\Database\Seeder\Seeder;
 use Anomaly\Streams\Platform\Model\Options\OptionsAdvertisementEntryModel;
+use Chumper\Zipper\Zipper;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Visiosoft\AdvsModule\Adv\AdvSeeder;
@@ -12,6 +14,14 @@ class AdvsModuleSeeder extends Seeder
      */
     public function run()
     {
+        //Download demo SQL
+        file_put_contents(__DIR__."/demo.sql", fopen("http://ilandemo.vebze.com/demo.sql", 'r'));
+        //Download demo Files and Extract to Files
+        file_put_contents("advs-files.zip", fopen("http://ilandemo.vebze.com/advs-files.zip", 'r'));
+        $zipper = new Zipper();
+        $zipper->make('advs-files.zip')->folder('advs-files')->extractTo(base_path().'/public/app/default/files-module/local/images/');
+        $zipper->close();
+
         $this->call(AdvSeeder::class);
 
         /* Demo Start */
