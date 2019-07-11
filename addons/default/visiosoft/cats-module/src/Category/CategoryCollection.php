7a567681d743c4769ea8614f18491860f5ffa191
vedatakd <vedat@vebze.com>
2019-07-11 12:19:36 +0300

    #35 Can't delete categories fix

--- addons/default/visiosoft/cats-module/src/Category/CategoryCollection.php
+++ addons/default/visiosoft/cats-module/src/Category/CategoryCollection.php
@@ -16,17 +16,11 @@ class CategoryCollection extends EntryCollection
     {
         $counter = 0;
 
-        echo "<div style='background-image:url(/addons/default/visiosoft/default-theme/resources/images/loading-animation.gif);
-        background-repeat:no-repeat;
-        width:248px;
-        height:248px;    
-        padding-left: 20px;'></div>";
-
         for ($i = 0; $i <= $counter; $i++) {
             $data = CategoryModel::query()
                 ->where('parent_category_id', $id)
                 ->where('deleted_at', null)
-                ->select('id', 'parent_category_id')
+                ->select('cats_category.id', 'cats_category.parent_category_id')
                 ->first();
             if ($data != "") {
                 $id = $data['id'];
