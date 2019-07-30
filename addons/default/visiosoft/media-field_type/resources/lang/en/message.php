6b6e8ba06eaf4093311058ac46e627cafb48eeed
vedatakd <vedat@vebze.com>
2019-07-30 12:38:05 +0300

    Add image message for new ad

--- addons/default/visiosoft/media-field_type/resources/lang/en/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/en/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder'     => 'Which folder would you like to upload to?',
-    'upload'            => 'Click or drop files here to upload.',
+    'upload'            => 'Add İmages | Upload İmages | Attach a photo',
     'choose_files'      => 'Which files would you like to use?',
     'no_files_selected' => 'No files selected.',
     'no_uploads'        => 'No files uploaded.',
