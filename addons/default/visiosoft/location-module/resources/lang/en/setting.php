4bf1efdf728e365c3cec4fa0a930a17445010804
vedatakd <vedat@vebze.com>
2019-09-06 14:11:29 +0300

    #402 location settings lang

--- addons/default/visiosoft/location-module/resources/lang/en/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/en/setting.php
@@ -1,4 +1,13 @@
 <?php
 
 return [
+    'home_page_location' => [
+        'name' => 'Home Page Location Field',
+    ],
+    'list_page_location' => [
+        'name' => 'List Page Location Field',
+    ],
+    'detail_page_location' => [
+        'name' => 'Detail Page Location Field',
+    ],
 ];
