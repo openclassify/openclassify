1f8888b56b1670bd36da0fcf83c6ec6fd1828ca6
vedatakd <vedat@vebze.com>
2019-10-10 20:03:05 +0300

    #431 add favicon for General Settings

--- addons/default/visiosoft/advs-module/src/AdvsModuleSeeder.php
+++ addons/default/visiosoft/advs-module/src/AdvsModuleSeeder.php
@@ -1,5 +1,7 @@
 <?php namespace Visiosoft\AdvsModule;
 
+use Anomaly\FilesModule\Disk\Contract\DiskRepositoryInterface;
+use Anomaly\FilesModule\Folder\Contract\FolderRepositoryInterface;
 use Anomaly\Streams\Platform\Database\Seeder\Seeder;
 use Anomaly\Streams\Platform\Model\Options\OptionsAdvertisementEntryModel;
 use Chumper\Zipper\Zipper;
@@ -10,10 +12,52 @@ use Visiosoft\AdvsModule\Seed\BlockSeeder;
 class AdvsModuleSeeder extends Seeder
 {
     /**
+     * The disk repository.
+     *
+     * @var DiskRepositoryInterface
+     */
+    protected $disks;
+
+    /**
+     * The folder repository.
+     *
+     * @var FolderRepositoryInterface
+     */
+    protected $folders;
+
+    /**
+     * Create a new FolderSeeder instance.
+     *
+     * @param DiskRepositoryInterface   $disks
+     * @param FolderRepositoryInterface $folders
+     */
+    public function __construct(DiskRepositoryInterface $disks, FolderRepositoryInterface $folders)
+    {
+        $this->disks   = $disks;
+        $this->folders = $folders;
+    }
+
+    /**
      * Run the seeder.
      */
     public function run()
     {
+        $disk = $this->disks->findBySlug('local');
+
+        $this->folders->create(
+            [
+                'en'            => [
+                    'name'        => 'Favicon',
+                    'description' => 'A folder for Favicon.',
+                ],
+                'slug'          => 'favicon',
+                'disk'          => $disk,
+                'allowed_types' => [
+                    'ico'
+                ],
+            ]
+        );
+
 
         //Download demo SQL
         $repository = "https://raw.githubusercontent.com/openclassify/Openclassify-Demo-Data/master/";
