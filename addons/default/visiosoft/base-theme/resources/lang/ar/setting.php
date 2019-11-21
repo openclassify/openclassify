7c244ba62db7b5fd34a8a5c2659a5c58add72044
Diatrex <diahshalabi@hotmail.com>
2019-11-21 17:39:03 +0300

    #504 Arapça (AR) çeviriler eklenecek

---
+++ addons/default/visiosoft/base-theme/resources/lang/ar/setting.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'meta_tags' => [
+        'name' => 'كلمات ميتا المفتاحية',
+    ],
+    'template' => [
+        'name' => 'القالب',
+    ],
+    's-type-latest-limit' => [
+        'name' => 'الحد الأقصى من نوع-اس',
+    ],
+    's-type-showcase' => [
+        'name' => 'اسم حد عرض S-type',
+    ],
+    's-type-banner-code' => [
+        'name' => 'رمز شعار نوع-اس',
+    ],
+
+    's-type-banner-mobile-code' => [
+        'name' => 'رمز الجوال لراية نوع-اس',
+    ],
+
+    'show_post_your_add_btn' => [
+        'name' => 'أظهر زر "انشر اعلانك"',
+    ],
+
+    'show_home_search_on_map_btn' => [
+        'name' => 'أظهر زر "على الخريطة" في البحث في الصفحة الرئيسية',
+    ],
+
+    'show_last_search_btn' => [
+        'name' => 'أظهر زر "آخر عملية بحث"',
+    ],
+];
