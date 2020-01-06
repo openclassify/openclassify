a4e3f716d94df45deca87125ba0e37bc7493b80a
vedatakd <vedat@vebze.com>
2020-01-06 14:22:19 +0300

    add catalog mode && fixed country filter problem

--- addons/default/visiosoft/base-theme/resources/lang/en/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/en/field.php
@@ -7,4 +7,13 @@ return [
     'show_all' => [
         'name' => 'Show All',
     ],
+    'login' => [
+        'name' => 'Login',
+    ],
+    'logout' => [
+        'name' => 'Logout',
+    ],
+    'profile' => [
+        'name' => 'Profile',
+    ],
 ];
---
+++ addons/default/visiosoft/base-theme/resources/lang/tr/section.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'catalog_mode' => [
+        'name' => 'Katalog Modu',
+    ],
+];
