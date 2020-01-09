1d021c90f275aa77ff758c59d750f2bfdae2978c
Fatih Alp <mail@fatihalp.com>
2020-01-09 13:19:45 +0300

    New translations permission.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/permission.php
@@ -1,60 +1,52 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "name"      =>"Les publicités",
-      "option"      =>      [
-         "read"         =>"Peut lire les annonces?        ",
-         "write"         =>"Peut créer / modifier des annonces?        ",
-         "delete"         =>"Peut supprimer des annonces?"
-      ],
-
-   ],
-   "categories"   =>   [
-      "name"      =>"Les catégories",
-      "option"      =>      [
-         "read"         =>"Peut lire des catégories?        ",
-         "write"         =>"Peut créer / éditer des catégories?        ",
-         "delete"         =>"Peut supprimer des catégories?"
-      ],
-
-   ],
-   "village"   =>   [
-      "name"      =>"Village",
-      "option"      =>      [
-         "read"         =>"Peut lire le village?        ",
-         "write"         =>"Peut créer / éditer un village?        ",
-         "delete"         =>"Peut supprimer le village?"
-      ],
-
-   ],
-   "custom_fields"   =>   [
-      "name"      =>"Les champs personnalisés",
-      "option"      =>      [
-         "read"         =>"Peut lire les champs personnalisés?        ",
-         "write"         =>"Peut créer / éditer des champs personnalisés?        ",
-         "delete"         =>"Peut supprimer des champs personnalisés?"
-      ],
-
-   ],
-   "custom_field_advs"   =>   [
-      "name"      =>"Annonces personnalisées",
-      "option"      =>      [
-         "read"         =>"Peut lire des annonces de terrain personnalisées?        ",
-         "write"         =>"Peut créer / modifier des annonces de terrain personnalisées?        ",
-         "delete"         =>"Peut-on supprimer des annonces de terrain personnalisées?"
-      ],
-
-   ],
-   "cf_values"   =>   [
-      "name"      =>"Cf valeurs",
-      "option"      =>      [
-         "read"         =>"Peut lire les valeurs cf?        ",
-         "write"         =>"Peut créer / éditer des valeurs cf?        ",
-         "delete"         =>"Peut-on supprimer les valeurs cf?",
-
-      ],
-
-   ],
-
+return [
+    'advs' => [
+        'name'   => 'Les publicités',
+        'option' => [
+            'read'   => 'Peut lire les annonces?        ',
+            'write'  => 'Peut créer / modifier des annonces?        ',
+            'delete' => 'Peut supprimer des annonces?',
+        ],
+    ],
+    'categories' => [
+        'name'   => 'Les catégories',
+        'option' => [
+            'read'   => 'Peut lire des catégories?        ',
+            'write'  => 'Peut créer / éditer des catégories?        ',
+            'delete' => 'Peut supprimer des catégories?',
+        ],
+    ],
+    'village' => [
+        'name'   => 'Village',
+        'option' => [
+            'read'   => 'Peut lire le village?        ',
+            'write'  => 'Peut créer / éditer un village?        ',
+            'delete' => 'Peut supprimer le village?',
+        ],
+    ],
+    'custom_fields' => [
+        'name'   => 'Les champs personnalisés',
+        'option' => [
+            'read'   => 'Peut lire les champs personnalisés?        ',
+            'write'  => 'Peut créer / éditer des champs personnalisés?        ',
+            'delete' => 'Peut supprimer des champs personnalisés?',
+        ],
+    ],
+    'custom_field_advs' => [
+        'name'   => 'Annonces personnalisées',
+        'option' => [
+            'read'   => 'Peut lire des annonces de terrain personnalisées?        ',
+            'write'  => 'Peut créer / modifier des annonces de terrain personnalisées?        ',
+            'delete' => 'Peut-on supprimer des annonces de terrain personnalisées?',
+        ],
+    ],
+    'cf_values' => [
+        'name'   => 'Cf valeurs',
+        'option' => [
+            'read'   => 'Peut lire les valeurs cf?        ',
+            'write'  => 'Peut créer / éditer des valeurs cf?        ',
+            'delete' => 'Peut-on supprimer les valeurs cf?',
+        ],
+    ],
 ];
