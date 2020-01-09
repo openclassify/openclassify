cc7df1de279a3d339ccb159b36cc4c93b5cd9bf6
Fatih Alp <mail@fatihalp.com>
2020-01-09 13:19:37 +0300

    New translations message.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/message.php
@@ -1,6 +1,27 @@
 <?php
 
- return[
-   "error_added_cart"   =>"المنتج المطلوب مفقود أو غير نشط حاليًا",
-   "please_buy_package"   =>"يرجى شراء الحزمة"
+return [
+    'error_added_cart' => 'المنتج المطلوب مفقود أو غير نشط حاليًا',
+    'please_buy_package' => 'يرجى شراء الحزمة',
+    'please_payment' => 'Please Pay',
+    'bank_account_info' => 'Bank account information',
+    'agreement' => 'By clicking Post, you agree to our Terms of Use and Privacy Policy acknowledge that you are the rightful owner of this item.',
+    'update_category_msg' => 'The classified category will be updated.',
+    'updated_category_msg' => 'Category successfully edited.',
+    'max_ad_limit' => [
+        'title' => "Your Free Ad Limit is Out.",
+        'msg' => "You have reached your free ad limit.",
+        'nextBtn' => "Show Ads",
+        'cancelBtn' => "or Cancel",
+        'link' => "/profile",
+    ],
+    'create_ad_with_post_cat' => [
+        'title' => "Post an Ad in just 30 seconds",
+        'msg' => "Please DO NOT post multiple ads for the same items or service. All duplicate, spam and wrongly categorized ads will be deleted.",
+        'nextBtn' => "Next",
+        'cancelBtn' => "or Cancel",
+        'link' => "",
+    ],
+    'update_category_info' => 'Please update category.',
+    'filter_by_search_msg' => 'Filter By Search.',
 ];
