5037a1bda5c54cbe3958106f43b0555b377a45e6
Fatih Alp <mail@fatihalp.com>
2020-01-09 13:19:23 +0300

    New translations section.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/section.php
@@ -1,49 +1,40 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "title"      =>"Annunci",
-
-   ],
-   "categories"   =>   [
-      "title"      =>"categorie",
-
-   ],
-   "countries"   =>   [
-      "title"      =>"paesi",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Città",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Quartieri",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"quartieri",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Villaggio",
-
-   ],
-   "custom_fields"   =>   [
-      "title"      =>"Campi personalizzati",
-
-   ],
-   "custom_field_advs"   =>   [
-      "title"      =>"Annunci di campo personalizzati",
-
-   ],
-   "cf_values"   =>   [
-      "title"      =>"Valori Cf",
-
-   ],
-   "fields"   =>   [
-      "title"      =>"Campi personalizzati",
-
-   ],
-
+return [
+    'advs' => [
+        'title' => 'Annunci',
+    ],
+    'categories' => [
+        'title' => 'categorie',
+    ],
+    'countries' => [
+        'title' => 'paesi',
+    ],
+    'cities' => [
+        'title' => 'Città',
+    ],
+    'districts' => [
+        'title' => 'Quartieri',
+    ],
+    'neighborhoods' => [
+        'title' => 'quartieri',
+    ],
+    'village' => [
+        'title' => 'Villaggio',
+    ],
+    'custom_fields' => [
+        'title' => 'Campi personalizzati',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Annunci di campo personalizzati',
+    ],
+    'cf_values' => [
+        'title' => 'Valori Cf',
+    ],
+    'fields' => [
+        'title' => 'Campi personalizzati'
+    ],
+    'assets_clear' => [
+        'name' => 'Assets Clear'
+    ]
 ];
