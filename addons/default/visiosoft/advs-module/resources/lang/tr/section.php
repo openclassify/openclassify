1ec1184959d4f3f272c51454176171ef450bc166
Fatih Alp <mail@fatihalp.com>
2020-01-09 13:19:13 +0300

    New translations section.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/section.php
@@ -1,49 +1,40 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "title"      =>"İlanlar",
-
-   ],
-   "categories"   =>   [
-      "title"      =>"Kategoriler",
-
-   ],
-   "countries"   =>   [
-      "title"      =>"Ülkeler",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Şehirler",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"İlçeler",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"Çevredekiler",
-
-   ],
-   "village"   =>   [
-      "title"      =>"köy",
-
-   ],
-   "custom_fields"   =>   [
-      "title"      =>"Özel Alanlar",
-
-   ],
-   "custom_field_advs"   =>   [
-      "title"      =>"Özel alan İlanları",
-
-   ],
-   "cf_values"   =>   [
-      "title"      =>"Cf değerleri",
-
-   ],
-   "fields"   =>   [
-      "title"      =>"Özel Alanlar",
-
-   ],
-
+return [
+    'advs' => [
+        'title' => 'İlanlar',
+    ],
+    'categories' => [
+        'title' => 'Kategoriler',
+    ],
+    'countries' => [
+        'title' => 'Ülkeler',
+    ],
+    'cities' => [
+        'title' => 'Şehirler',
+    ],
+    'districts' => [
+        'title' => 'İlçeler',
+    ],
+    'neighborhoods' => [
+        'title' => 'Çevredekiler',
+    ],
+    'village' => [
+        'title' => 'köy',
+    ],
+    'custom_fields' => [
+        'title' => 'Özel Alanlar',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Özel alan İlanları',
+    ],
+    'cf_values' => [
+        'title' => 'Cf değerleri',
+    ],
+    'fields' => [
+        'title' => 'Özel Alanlar'
+    ],
+    'assets_clear' => [
+        'name' => 'Assets Clear'
+    ]
 ];
