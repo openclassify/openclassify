a0f5f0d166924ce8d333c911d81cdbfcd050dfd9
Fatih Alp <mail@fatihalp.com>
2020-01-09 13:19:33 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
@@ -1,136 +1,114 @@
 <?php
 
- return[
-   "auto_approve"   =>   [
-      "name"      =>"الموافقة التلقائية",
+return [
+    'auto_approve' => [
+        'name' => 'الموافقة التلقائية',
+    ],
+    'default_published_time' => [
+        'name' => 'وقت النشر الافتراضي',
+    ],
+    'default_adv_limit' => [
+        'name' => 'الحد الافتراضي للإعلانات',
+    ],
+    'address' => [
+        'name' => 'الاتصال العنوان',
+    ],
+    'phone' => [
+        'name' => 'الاتصال الهاتف',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ],
+    'contact_mail' => [
+        'name' => 'بريد الاتصال',
+        'warning'      => 'تمت إضافة <strong>معلومات</strong> sitename تلقائيًاsitename',
+    ],'map_coordinates_long' => [
+        'name' => 'خريطة تنسيق طويل',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'خريطة اللات تنسيق',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'شعار',
+    ],
+    'ogImage' => [
+        'name' => 'شعار المشاركة الاجتماعية',
+        'warning'      => 'يجب أن يكون بدقة 1200 × 630.',
 
-   ],
-   "default_published_time"   =>   [
-      "name"      =>"وقت النشر الافتراضي",
+    ],
+    'currencies' => [
+        'name' => 'العملات النشطة',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'عنوان الموقع',
+        'warning'      => 'تمت إضافة www. شبكة الاتصالات العالمية. <strong>sitename.com</strong>',
+    ],
+    'google_map_key' => [
+        'name' => 'خريطة جوجل ابي كي',
+    ],
 
-   ],
-   "default_adv_limit"   =>   [
-      "name"      =>"الحد الافتراضي للإعلانات",
-
-   ],
-   "address"   =>   [
-      "name"      =>"الاتصال العنوان",
-
-   ],
-   "phone"   =>   [
-      "name"      =>"الاتصال الهاتف",
-
-   ],
-   "contact_mail"   =>   [
-      "name"      =>"بريد الاتصال",
-      "warning"      =>"تمت إضافة <strong>معلومات</strong> sitename تلقائيًاsitename",
-
-   ],
-   "map_coordinates_long"   =>   [
-      "name"      =>"خريطة تنسيق طويل",
-
-   ],
-   "map_coordinates_lat"   =>   [
-      "name"      =>"خريطة اللات تنسيق",
-
-   ],
-   "logo"   =>   [
-      "name"      =>"شعار",
-
-   ],
-   "ogImage"   =>   [
-      "name"      =>"شعار المشاركة الاجتماعية",
-      "warning"      =>"يجب أن يكون بدقة 1200 × 630.",
-
-   ],
-   "currencies"   =>   [
-      "name"      =>"العملات النشطة",
-
-   ],
-   "site_address"   =>   [
-      "name"      =>"عنوان الموقع",
-      "warning"      =>"تمت إضافة www. شبكة الاتصالات العالمية. <strong>sitename.com</strong>",
-
-   ],
-   "google_map_key"   =>   [
-      "name"      =>"خريطة جوجل ابي كي",
-
-   ],
-   "default_country"   =>   [
-      "name"      =>"بلد الإعلان الافتراضي",
-
-   ],
-   "free_currencyconverterapi_key"   =>   [
-      "name"      =>"تحويل العملات الرئيسية API",
-
-   ],
-   "default_city"   =>   [
-      "name"      =>"المدينة الإعلانية الافتراضية",
-
-   ],
-   "default_district"   =>   [
-      "name"      =>"منطقة الإعلان الافتراضية",
-
-   ],
-   "default_neighborhood"   =>   [
-      "name"      =>"حي الإعلانات الافتراضي",
-
-   ],
-   "default_currency"   =>   [
-      "name"      =>"عملة الإعلان الافتراضية",
-
-   ],
-   "default_GET"   =>   [
-      "name"      =>"الإعلان الافتراضي",
-
-   ],
-   "twitter"   =>   [
-      "name"      =>"تغريد",
-
-   ],
-   "facebook"   =>   [
-      "name"      =>"فيس بوك",
-
-   ],
-   "youtube"   =>   [
-      "name"      =>"موقع YouTube",
-
-   ],
-   "google"   =>   [
-      "name"      =>"جوجل",
-
-   ],
-   "watermark_type"   =>   [
-      "name"      =>"نوع العلامة المائية",
-
-   ],
-   "watermark_text"   =>   [
-      "name"      =>"نص العلامة المائية",
-
-   ],
-   "watermark_image"   =>   [
-      "name"      =>"صورة العلامة المائية",
-
-   ],
-   "watermark_position"   =>   [
-      "name"      =>"موقف العلامة المائية",
-
-   ],
-   "watermark_opacity"   =>   [
-      "name"      =>"عتامة العلامة المائية",
-
-   ],
-   "listing_page_image"   =>   [
-      "name"      =>"سرد صورة الصفحة",
-
-   ],
-   "enabled_currencies"   =>   [
-      "name"      =>"العملات الممكّنة",
-
-   ],
-   "google_statistic_code"   =>   [
-      "name"      =>"Google Statistic Code",
-
-   ],
+    'default_country' => [
+        'name' => 'بلد الإعلان الافتراضي',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'تحويل العملات الرئيسية API'
+    ],
+    'default_city' => [
+        'name' => 'المدينة الإعلانية الافتراضية',
+    ],
+    'default_district' => [
+        'name' => 'منطقة الإعلان الافتراضية',
+    ],
+    'default_neighborhood' => [
+        'name' => 'حي الإعلانات الافتراضي',
+    ],
+    'default_currency' => [
+        'name' => 'عملة الإعلان الافتراضية',
+    ],
+    'default_GET' => [
+        'name' => 'الإعلان الافتراضي',
+    ],
+    'twitter' => [
+        'name' => 'تغريد',
+    ],
+    'facebook' => [
+        'name' => 'فيس بوك',
+    ],
+    'youtube' => [
+        'name' => 'موقع YouTube',
+    ],
+    'google' => [
+        'name' => 'جوجل',
+    ],
+    'watermark_type' => [
+        'name' => 'نوع العلامة المائية',
+    ],
+    'watermark_text' => [
+        'name' => 'نص العلامة المائية',
+    ],
+    'watermark_image' => [
+        'name' => 'صورة العلامة المائية',
+    ],
+    'watermark_position' => [
+        'name' => 'موقف العلامة المائية',
+    ],
+    'watermark_opacity' => [
+        'name' => 'عتامة العلامة المائية',
+    ],
+    'listing_page_image' => [
+        'name' => 'سرد صورة الصفحة',
+    ],
+    'enabled_currencies' => [
+        'name' => 'العملات الممكّنة',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google Statistic Code',
+    ],
 
 ];
