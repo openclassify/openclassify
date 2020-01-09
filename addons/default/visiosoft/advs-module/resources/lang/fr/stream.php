ec18dbfb50c11a492387fbe28ba4323104958774
Fatih Alp <mail@fatihalp.com>
2020-01-09 13:19:42 +0300

    New translations stream.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/stream.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/stream.php
@@ -1,29 +1,22 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "name"      =>"Advs",
-
-   ],
-   "categories"   =>   [
-      "name"      =>"Les catégories",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Village",
-
-   ],
-   "custom_fields"   =>   [
-      "name"      =>"Les champs personnalisés",
-
-   ],
-   "custom_field_advs"   =>   [
-      "name"      =>"Advs personnalisé",
-
-   ],
-   "cf_values"   =>   [
-      "name"      =>"Cf valeurs",
-
-   ],
-
+return [
+    'advs' => [
+        'name' => 'Advs',
+    ],
+    'categories' => [
+        'name' => 'Les catégories',
+    ],
+    'village' => [
+        'name' => 'Village',
+    ],
+    'custom_fields' => [
+        'name' => 'Les champs personnalisés',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Advs personnalisé',
+    ],
+    'cf_values' => [
+        'name' => 'Cf valeurs',
+    ],
 ];
