006f7d2b7bedebeafa77442baa89cf952636a762
Fatih Alp <mail@fatihalp.com>
2020-01-09 13:19:14 +0300

    New translations permission.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/permission.php
@@ -1,60 +1,52 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "name"      =>"İlanlar",
-      "option"      =>      [
-         "read"         =>"İlan okuyabilir mi?        ",
-         "write"         =>"İlan oluşturabilir / düzenleyebilir mi?        ",
-         "delete"         =>"İlanları silebilir mi?"
-      ],
-
-   ],
-   "categories"   =>   [
-      "name"      =>"Kategoriler",
-      "option"      =>      [
-         "read"         =>"Kategoriler okuyabilir mi?        ",
-         "write"         =>"Kategori oluşturabilir / düzenleyebilir mi?        ",
-         "delete"         =>"Kategorileri silebilir miyim?"
-      ],
-
-   ],
-   "village"   =>   [
-      "name"      =>"köy",
-      "option"      =>      [
-         "read"         =>"Köy okuyabilir mi        ",
-         "write"         =>"Köy oluşturabilir / düzenleyebilir mi?        ",
-         "delete"         =>"Köyü silebilir mi?"
-      ],
-
-   ],
-   "custom_fields"   =>   [
-      "name"      =>"Özel Alanlar",
-      "option"      =>      [
-         "read"         =>"Özel alanları okuyabilir mi?        ",
-         "write"         =>"Özel alanlar oluşturabilir / düzenleyebilir mi?        ",
-         "delete"         =>"Özel alanları silebilir mi?"
-      ],
-
-   ],
-   "custom_field_advs"   =>   [
-      "name"      =>"Özel alan İlanları",
-      "option"      =>      [
-         "read"         =>"Özel alan İlanlarını okuyabilir mi?        ",
-         "write"         =>"Özel alan İlanları oluşturabilir / düzenleyebilir mi?        ",
-         "delete"         =>"Özel alan İlanlarını silebilir mi?"
-      ],
-
-   ],
-   "cf_values"   =>   [
-      "name"      =>"Cf değerleri",
-      "option"      =>      [
-         "read"         =>"CF değerleri okuyabilir mi?        ",
-         "write"         =>"CF değerleri oluşturabilir / düzenleyebilir mi?        ",
-         "delete"         =>"CF değerlerini silebilir mi?",
-
-      ],
-
-   ],
-
+return [
+    'advs' => [
+        'name'   => 'İlanlar',
+        'option' => [
+            'read'   => 'İlan okuyabilir mi?        ',
+            'write'  => 'İlan oluşturabilir / düzenleyebilir mi?        ',
+            'delete' => 'İlanları silebilir mi?',
+        ],
+    ],
+    'categories' => [
+        'name'   => 'Kategoriler',
+        'option' => [
+            'read'   => 'Kategoriler okuyabilir mi?        ',
+            'write'  => 'Kategori oluşturabilir / düzenleyebilir mi?        ',
+            'delete' => 'Kategorileri silebilir miyim?',
+        ],
+    ],
+    'village' => [
+        'name'   => 'köy',
+        'option' => [
+            'read'   => 'Köy okuyabilir mi        ',
+            'write'  => 'Köy oluşturabilir / düzenleyebilir mi?        ',
+            'delete' => 'Köyü silebilir mi?',
+        ],
+    ],
+    'custom_fields' => [
+        'name'   => 'Özel Alanlar',
+        'option' => [
+            'read'   => 'Özel alanları okuyabilir mi?        ',
+            'write'  => 'Özel alanlar oluşturabilir / düzenleyebilir mi?        ',
+            'delete' => 'Özel alanları silebilir mi?',
+        ],
+    ],
+    'custom_field_advs' => [
+        'name'   => 'Özel alan İlanları',
+        'option' => [
+            'read'   => 'Özel alan İlanlarını okuyabilir mi?        ',
+            'write'  => 'Özel alan İlanları oluşturabilir / düzenleyebilir mi?        ',
+            'delete' => 'Özel alan İlanlarını silebilir mi?',
+        ],
+    ],
+    'cf_values' => [
+        'name'   => 'Cf değerleri',
+        'option' => [
+            'read'   => 'CF değerleri okuyabilir mi?        ',
+            'write'  => 'CF değerleri oluşturabilir / düzenleyebilir mi?        ',
+            'delete' => 'CF değerlerini silebilir mi?',
+        ],
+    ],
 ];
