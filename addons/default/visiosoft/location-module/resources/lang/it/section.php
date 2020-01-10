860598d046738b9eb296b21091298c2da7a51652
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:03:52 +0300

    New translations section.php (Italian)

--- addons/default/visiosoft/location-module/resources/lang/it/section.php
+++ addons/default/visiosoft/location-module/resources/lang/it/section.php
@@ -1,25 +1,19 @@
 <?php
 
- return[
-   "countries"   =>   [
-      "title"      =>"paesi",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Città",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Quartieri",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"quartieri",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Villaggio",
-
-   ],
-
+return [
+    'countries' => [
+        'title' => 'paesi',
+    ],
+    'cities' => [
+        'title' => 'Città',
+    ],
+    'districts' => [
+        'title' => 'Quartieri',
+    ],
+    'neighborhoods' => [
+        'title' => 'quartieri',
+    ],
+    'village' => [
+        'title' => 'Villaggio',
+    ],
 ];
