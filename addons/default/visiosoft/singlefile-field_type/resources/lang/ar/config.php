42af5b385a4c10693e4954392d60ac039d66ceaa
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:06:52 +0300

    New translations config.php (Arabic)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/ar/config.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/ar/config.php
@@ -6,4 +6,18 @@ return [
         'instructions' => 'حدد المجلد المتاح لهذا الحقل. اتركه فارغاً إذا أردت عرض كل المجلدات.',
         'warning'      => 'صلاحيات المجلد الحالية لديهاأولوية أكثر من المجلدات المختارة.',
     ],
+    'max'     => [
+        'name'         => 'Max Upload Size',
+        'instructions' => 'Specify the max upload size in <strong>megabytes</strong>.',
+        'warning'      => 'If not specified the folder max and then server max will be used instead.',
+    ],
+    'mode'    => [
+        'name'         => 'Input Mode',
+        'instructions' => 'How should users provide file input?',
+        'option'       => [
+            'default' => 'Upload and/or select files.',
+            'select'  => 'Select files only.',
+            'upload'  => 'Upload files only.',
+        ],
+    ],
 ];
