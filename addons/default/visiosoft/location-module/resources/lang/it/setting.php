7a7716281a952b9d90e848ce284a3fc3a8d5a65c
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:03:53 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/location-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/it/setting.php
@@ -1,5 +1,19 @@
 <?php
 
- return[
-
+return [
+    'home_page_location' => [
+        'name' => 'Home Page Location Field',
+    ],
+    'list_page_location' => [
+        'name' => 'List Page Location Field',
+    ],
+    'detail_page_location' => [
+        'name' => 'Detail Page Location Field',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'Show Search Location Button',
+    ],
+    'default_country' => [
+        'name' => 'Default Country',
+    ],
 ];
