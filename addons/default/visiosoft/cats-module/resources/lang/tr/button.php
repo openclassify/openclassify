16b2f36c3dbb974a5557478500bfe8b5bfc7a3b4
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:21:11 +0300

    New translations button.php (Turkish)

--- addons/default/visiosoft/cats-module/resources/lang/tr/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/tr/button.php
@@ -1,8 +1,8 @@
 <?php
 
- return[
-   "new_category"   =>"Yeni kategori",
-   "add_sub_category"   =>"Alt Kategori Ekle",
-   "sub_category"   =>"Alt Kategoriyi Göster",
-   "new_placeholderforsearch"   =>"Arama için Yeni Yer Tutucu"
+return [
+    'new_category' => 'Yeni kategori',
+    'add_sub_category' => 'Alt Kategori Ekle',
+    'sub_category' => 'Alt Kategoriyi Göster',
+    'new_placeholderforsearch' => 'Arama için Yeni Yer Tutucu',
 ];
