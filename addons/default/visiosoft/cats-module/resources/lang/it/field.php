d28a4bdbe9c9e0bbae1fe501af9b95e7367f7409
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:04:32 +0300

    New translations field.php (Italian)

--- addons/default/visiosoft/cats-module/resources/lang/it/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/it/field.php
@@ -1,21 +1,27 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Titolo",
+return [
+    'name' => [
+        'name' => 'Titolo',
+    ],
+    'order' => [
+        'name' => 'Ordine',
+    ],
+    'slug' => [
+        'name' => 'lumaca'
+    ],
+    'files' => [
+        'name' => 'Immagine'
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword'
+    ],
+    'seo_description' => [
+        'name' => 'Seo Description'
+    ],
+    'icon' => [
+        'name' => 'Icon'
+],
 
-   ],
-   "order"   =>   [
-      "name"      =>"Ordine",
-
-   ],
-   "slug"   =>   [
-      "name"      =>"lumaca",
-
-   ],
-   "files"   =>   [
-      "name"      =>"Immagine",
-
-   ],
-   "please_wait"   =>"Attendere. Eliminazione delle sottocategorie"
+    'please_wait' => 'Attendere. Eliminazione delle sottocategorie',
 ];
