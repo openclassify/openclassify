3cd4a73f370355c924af377a331798391118c963
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:04:26 +0300

    New translations field.php (Italian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/it/field.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'latest_ads' => [
+        'name' => 'Latest Ads',
+    ],
+    'show_all' => [
+        'name' => 'Show All',
+    ],
+    'login' => [
+        'name' => 'Login',
+    ],
+    'logout' => [
+        'name' => 'Logout',
+    ],
+    'profile' => 'Profile',
+    'email' => 'Email Address',
+    'password' => 'Password',
+    'display_name' => 'Display Name',
+    'username' => 'Username',
+    'remember_me' => 'Remember Me',
+];
