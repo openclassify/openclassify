64cff5057591f3edede05daceeefd85cb2144311
Fatih Alp <mail@fatihalp.com>
2020-01-10 16:27:07 +0300

    New translations setting.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/setting.php
@@ -1,136 +1,114 @@
 <?php
 
- return[
-   "auto_approve"   =>   [
-      "name"      =>"Approuver automatiquement",
+return [
+    'auto_approve' => [
+        'name' => 'Approuver automatiquement',
+    ],
+    'default_published_time' => [
+        'name' => 'Heure de publication par défaut',
+    ],
+    'default_adv_limit' => [
+        'name' => 'Limite des annonces par défaut',
+    ],
+    'address' => [
+        'name' => 'Adresse de contact',
+    ],
+    'phone' => [
+        'name' => 'Numéro du contact',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ],
+    'contact_mail' => [
+        'name' => 'Contact mail',
+        'warning'      => 'Ajout automatique du nom de site <strong>info</strong> @sitename',
+    ],'map_coordinates_long' => [
+        'name' => 'Carte Longue Coordonnée',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Carte Lat Coordonnée',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Logo de partage social',
+        'warning'      => 'Il devrait être à une résolution de 1200 x 630.',
 
-   ],
-   "default_published_time"   =>   [
-      "name"      =>"Heure de publication par défaut",
+    ],
+    'currencies' => [
+        'name' => 'Devises Actives',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'Adresse du site',
+        'warning'      => 'Auto ajouté www. www. <strong>sitename.com</strong>',
+    ],
+    'google_map_key' => [
+        'name' => 'Clé Api Google Map',
+    ],
 
-   ],
-   "default_adv_limit"   =>   [
-      "name"      =>"Limite des annonces par défaut",
-
-   ],
-   "address"   =>   [
-      "name"      =>"Adresse de contact",
-
-   ],
-   "phone"   =>   [
-      "name"      =>"Numéro du contact",
-
-   ],
-   "contact_mail"   =>   [
-      "name"      =>"Contact mail",
-      "warning"      =>"Ajout automatique du nom de site <strong>info</strong> @sitename",
-
-   ],
-   "map_coordinates_long"   =>   [
-      "name"      =>"Carte Longue Coordonnée",
-
-   ],
-   "map_coordinates_lat"   =>   [
-      "name"      =>"Carte Lat Coordonnée",
-
-   ],
-   "logo"   =>   [
-      "name"      =>"Logo",
-
-   ],
-   "ogImage"   =>   [
-      "name"      =>"Logo de partage social",
-      "warning"      =>"Il devrait être à une résolution de 1200 x 630.",
-
-   ],
-   "currencies"   =>   [
-      "name"      =>"Devises Actives",
-
-   ],
-   "site_address"   =>   [
-      "name"      =>"Adresse du site",
-      "warning"      =>"Auto ajouté www. www. <strong>sitename.com</strong>",
-
-   ],
-   "google_map_key"   =>   [
-      "name"      =>"Clé Api Google Map",
-
-   ],
-   "default_country"   =>   [
-      "name"      =>"Pays d\'annonce par défaut",
-
-   ],
-   "free_currencyconverterapi_key"   =>   [
-      "name"      =>"Clé API de convertisseur de devises",
-
-   ],
-   "default_city"   =>   [
-      "name"      =>"Ville d\'annonce par défaut",
-
-   ],
-   "default_district"   =>   [
-      "name"      =>"District d\'annonce par défaut",
-
-   ],
-   "default_neighborhood"   =>   [
-      "name"      =>"Quartier de l\'annonce par défaut",
-
-   ],
-   "default_currency"   =>   [
-      "name"      =>"Monnaie d\'annonce par défaut",
-
-   ],
-   "default_GET"   =>   [
-      "name"      =>"Annonce par défaut GET",
-
-   ],
-   "twitter"   =>   [
-      "name"      =>"Gazouillement",
-
-   ],
-   "facebook"   =>   [
-      "name"      =>"Facebook",
-
-   ],
-   "youtube"   =>   [
-      "name"      =>"Youtube",
-
-   ],
-   "google"   =>   [
-      "name"      =>"Google",
-
-   ],
-   "watermark_type"   =>   [
-      "name"      =>"Type de filigrane",
-
-   ],
-   "watermark_text"   =>   [
-      "name"      =>"Texte en filigrane",
-
-   ],
-   "watermark_image"   =>   [
-      "name"      =>"Image en filigrane",
-
-   ],
-   "watermark_position"   =>   [
-      "name"      =>"Position du filigrane",
-
-   ],
-   "watermark_opacity"   =>   [
-      "name"      =>"Opacité du filigrane",
-
-   ],
-   "listing_page_image"   =>   [
-      "name"      =>"Image de page de liste",
-
-   ],
-   "enabled_currencies"   =>   [
-      "name"      =>"Devises activées",
-
-   ],
-   "google_statistic_code"   =>   [
-      "name"      =>"Code statistique Google",
-
-   ],
+    'default_country' => [
+        'name' => 'Pays d\\'annonce par défaut',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Clé API de convertisseur de devises'
+    ],
+    'default_city' => [
+        'name' => 'Ville d\\'annonce par défaut',
+    ],
+    'default_district' => [
+        'name' => 'District d\\'annonce par défaut',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Quartier de l\\'annonce par défaut',
+    ],
+    'default_currency' => [
+        'name' => 'Monnaie d\\'annonce par défaut',
+    ],
+    'default_GET' => [
+        'name' => 'Annonce par défaut GET',
+    ],
+    'twitter' => [
+        'name' => 'Gazouillement',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Type de filigrane',
+    ],
+    'watermark_text' => [
+        'name' => 'Texte en filigrane',
+    ],
+    'watermark_image' => [
+        'name' => 'Image en filigrane',
+    ],
+    'watermark_position' => [
+        'name' => 'Position du filigrane',
+    ],
+    'watermark_opacity' => [
+        'name' => 'Opacité du filigrane',
+    ],
+    'listing_page_image' => [
+        'name' => 'Image de page de liste',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Devises activées',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Code statistique Google',
+    ],
 
 ];
