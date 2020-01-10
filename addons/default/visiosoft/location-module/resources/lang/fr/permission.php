e8051dafd5616a8c23d84a6b68d69bcd49c3de22
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:05:35 +0300

    New translations permission.php (French)

--- addons/default/visiosoft/location-module/resources/lang/fr/permission.php
+++ addons/default/visiosoft/location-module/resources/lang/fr/permission.php
@@ -1,15 +1,13 @@
 <?php
 
- return[
-   "village"   =>   [
-      "name"      =>"Village",
-      "option"      =>      [
-         "read"         =>"Peut lire le village?        ",
-         "write"         =>"Peut créer / éditer un village?        ",
-         "delete"         =>"Peut supprimer le village?",
+return [
 
-      ],
-
-   ],
-
+    'village' => [
+        'name'   => 'Village',
+        'option' => [
+            'read'   => 'Peut lire le village?        ',
+            'write'  => 'Peut créer / éditer un village?        ',
+            'delete' => 'Peut supprimer le village?',
+        ],
+    ],
 ];
