2019e2a7f814280a0a322a0ec813a408aebff8a8
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:06:24 +0300

    New translations control_panel.php (French)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/control_panel.php
@@ -2,7 +2,8 @@
 
 return [
     'help'      => 'Aide',
+    'search'    => 'Search',
     'logout'    => 'Déconnexion',
     'view_site' => 'Voir le site',
-    'title'     => 'Panneau de contrôle'
+    'title'     => 'Panneau de contrôle',
 ];
