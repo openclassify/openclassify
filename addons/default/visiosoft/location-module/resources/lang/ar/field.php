a397bb67ad74e14ed4f6dbebbe998367d17b3429
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:06:54 +0300

    New translations field.php (Arabic)

--- addons/default/visiosoft/location-module/resources/lang/ar/field.php
+++ addons/default/visiosoft/location-module/resources/lang/ar/field.php
@@ -1,41 +1,37 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"عنوان",
-
-   ],
-   "slug"   =>   [
-      "name"      =>"سبيكة",
-
-   ],
-   "city"   =>   [
-      "name"      =>"مدينة",
-
-   ],
-   "country"   =>   [
-      "name"      =>"بلد",
-
-   ],
-   "district"   =>   [
-      "name"      =>"منطقة",
-
-   ],
-   "order"   =>   [
-      "name"      =>"امر ترتيب",
-
-   ],
-   "neighborhood"   =>   [
-      "name"      =>"حي",
-
-   ],
-   "village"   =>   [
-      "name"      =>"قرية",
-
-   ],
-   "description"   =>   [
-      "name"      =>"وصف",
-
-   ],
-
+return [
+    'name' => [
+        'name' => 'عنوان'
+    ],
+    'slug' => [
+        'name' => 'سبيكة'
+    ],
+    'city' => [
+        'name' => 'مدينة'
+    ],
+    'country' => [
+        'name' => 'بلد'
+    ],
+    'district' => [
+        'name' => 'منطقة'
+    ],
+    'order' => [
+        'name' => 'امر ترتيب'
+    ],
+    'neighborhood' => [
+        'name' => 'حي'
+    ],
+    'village' => [
+        'name' => 'قرية'
+    ],
+    'description' => [
+        'name' => 'وصف'
+    ],
+    'pick_option' => [
+        'name' => 'Pick an option',
+    ],
+    'address' => [
+        'name' => 'Address',
+    ],
 ];
