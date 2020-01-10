20c6f2c6247342c39aeb64d91de0e752f2f53180
Fatih Alp <mail@fatihalp.com>
2020-01-10 16:26:59 +0300

    New translations section.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/section.php
@@ -1,49 +1,40 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "title"      =>"إعلانات",
-
-   ],
-   "categories"   =>   [
-      "title"      =>"الاقسام",
-
-   ],
-   "countries"   =>   [
-      "title"      =>"بلدان",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"مدن",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"المناطق",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"أحياء",
-
-   ],
-   "village"   =>   [
-      "title"      =>"قرية",
-
-   ],
-   "custom_fields"   =>   [
-      "title"      =>"الحقول المخصصة",
-
-   ],
-   "custom_field_advs"   =>   [
-      "title"      =>"الإعلانات الميدانية المخصصة",
-
-   ],
-   "cf_values"   =>   [
-      "title"      =>"قيم cf",
-
-   ],
-   "fields"   =>   [
-      "title"      =>"الحقول المخصصة",
-
-   ],
-
+return [
+    'advs' => [
+        'title' => 'إعلانات',
+    ],
+    'categories' => [
+        'title' => 'الاقسام',
+    ],
+    'countries' => [
+        'title' => 'بلدان',
+    ],
+    'cities' => [
+        'title' => 'مدن',
+    ],
+    'districts' => [
+        'title' => 'المناطق',
+    ],
+    'neighborhoods' => [
+        'title' => 'أحياء',
+    ],
+    'village' => [
+        'title' => 'قرية',
+    ],
+    'custom_fields' => [
+        'title' => 'الحقول المخصصة',
+    ],
+    'custom_field_advs' => [
+        'title' => 'الإعلانات الميدانية المخصصة',
+    ],
+    'cf_values' => [
+        'title' => 'قيم cf',
+    ],
+    'fields' => [
+        'title' => 'الحقول المخصصة'
+    ],
+    'assets_clear' => [
+        'name' => 'الأصول واضحة'
+    ]
 ];
