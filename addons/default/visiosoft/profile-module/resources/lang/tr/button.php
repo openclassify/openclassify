4dbf96f250593c667949f44e4eb43b27b4ae6238
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:20:02 +0300

    New translations button.php (Turkish)

--- addons/default/visiosoft/profile-module/resources/lang/tr/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/tr/button.php
@@ -4,4 +4,6 @@ return [
     'new_profile' => 'Yeni Profil',
     'new_adress' => 'Yeni Adres',
     'show' => 'Göster',
+    'update_password' => 'Update Password',
+    'update_profile' => 'Update Profile',
 ];
