fadb608040da5b08729aea0f448e541c01adc3ab
Fatih Alp <mail@fatihalp.com>
2020-01-10 16:26:42 +0300

    New translations message.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/message.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    "error_added_cart" => "İstenen ürün şu anda eksik veya etkin değil",
-    "please_buy_package" => "Lütfen paket satın al",
+    'error_added_cart' => 'İstenen ürün şu anda eksik veya etkin değil',
+    'please_buy_package' => 'Lütfen paket satın al',
     'please_payment' => 'Lütfen Ödeme Yapınız',
     'bank_account_info' => 'Banka Hesap Bilgilerimiz',
     'agreement' => 'Yayınla\'yı tıkladığınızda, Kullanım Koşulları ve Gizlilik Politikasını kabul etmiş olursunuz, bu öğenin hak sahibi olduğunuzu kabul edersiniz.',
