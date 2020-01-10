fdf22b1ee57c319b98f49080720fed89f3b6b302
Fatih Alp <mail@fatihalp.com>
2020-01-10 16:26:48 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/setting.php
@@ -1,136 +1,114 @@
 <?php
 
- return[
-   "auto_approve"   =>   [
-      "name"      =>"Approvazione automatica",
+return [
+    'auto_approve' => [
+        'name' => 'Approvazione automatica',
+    ],
+    'default_published_time' => [
+        'name' => 'Tempo di pubblicazione predefinito',
+    ],
+    'default_adv_limit' => [
+        'name' => 'Limite annunci predefiniti',
+    ],
+    'address' => [
+        'name' => 'Indirizzo di contatto',
+    ],
+    'phone' => [
+        'name' => 'Contatto telefonico',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ],
+    'contact_mail' => [
+        'name' => 'Mail di contatto',
+        'warning'      => 'Aggiunta automatica delle <strong>informazioni sul nome sito</strong> @ nome sito',
+    ],'map_coordinates_long' => [
+        'name' => 'Mappa lunga coordinata',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Map Lat Coordinate',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Logo di condivisione sociale',
+        'warning'      => 'Dovrebbe avere una risoluzione di 1200 x 630.',
 
-   ],
-   "default_published_time"   =>   [
-      "name"      =>"Tempo di pubblicazione predefinito",
+    ],
+    'currencies' => [
+        'name' => 'Valute attive',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'Indirizzo del sito',
+        'warning'      => 'Aggiunta automatica www. www. <strong>sitename.com</strong>',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Key',
+    ],
 
-   ],
-   "default_adv_limit"   =>   [
-      "name"      =>"Limite annunci predefiniti",
-
-   ],
-   "address"   =>   [
-      "name"      =>"Indirizzo di contatto",
-
-   ],
-   "phone"   =>   [
-      "name"      =>"Contatto telefonico",
-
-   ],
-   "contact_mail"   =>   [
-      "name"      =>"Mail di contatto",
-      "warning"      =>"Aggiunta automatica delle <strong>informazioni sul nome sito</strong> @ nome sito",
-
-   ],
-   "map_coordinates_long"   =>   [
-      "name"      =>"Mappa lunga coordinata",
-
-   ],
-   "map_coordinates_lat"   =>   [
-      "name"      =>"Map Lat Coordinate",
-
-   ],
-   "logo"   =>   [
-      "name"      =>"Logo",
-
-   ],
-   "ogImage"   =>   [
-      "name"      =>"Logo di condivisione sociale",
-      "warning"      =>"Dovrebbe avere una risoluzione di 1200 x 630.",
-
-   ],
-   "currencies"   =>   [
-      "name"      =>"Valute attive",
-
-   ],
-   "site_address"   =>   [
-      "name"      =>"Indirizzo del sito",
-      "warning"      =>"Aggiunta automatica www. www. <strong>sitename.com</strong>",
-
-   ],
-   "google_map_key"   =>   [
-      "name"      =>"Google Map Api Key",
-
-   ],
-   "default_country"   =>   [
-      "name"      =>"Paese annuncio predefinito",
-
-   ],
-   "free_currencyconverterapi_key"   =>   [
-      "name"      =>"Chiave API del convertitore di valuta",
-
-   ],
-   "default_city"   =>   [
-      "name"      =>"Città annuncio predefinita",
-
-   ],
-   "default_district"   =>   [
-      "name"      =>"Distretto annunci predefinito",
-
-   ],
-   "default_neighborhood"   =>   [
-      "name"      =>"Quartiere annunci predefinito",
-
-   ],
-   "default_currency"   =>   [
-      "name"      =>"Valuta annuncio predefinita",
-
-   ],
-   "default_GET"   =>   [
-      "name"      =>"Annuncio predefinito GET",
-
-   ],
-   "twitter"   =>   [
-      "name"      =>"cinguettio",
-
-   ],
-   "facebook"   =>   [
-      "name"      =>"Facebook",
-
-   ],
-   "youtube"   =>   [
-      "name"      =>"Youtube",
-
-   ],
-   "google"   =>   [
-      "name"      =>"Google",
-
-   ],
-   "watermark_type"   =>   [
-      "name"      =>"Tipo di filigrana",
-
-   ],
-   "watermark_text"   =>   [
-      "name"      =>"Testo filigrana",
-
-   ],
-   "watermark_image"   =>   [
-      "name"      =>"Immagine filigrana",
-
-   ],
-   "watermark_position"   =>   [
-      "name"      =>"Posizione della filigrana",
-
-   ],
-   "watermark_opacity"   =>   [
-      "name"      =>"Opacità filigrana",
-
-   ],
-   "listing_page_image"   =>   [
-      "name"      =>"Immagine della pagina di elenco",
-
-   ],
-   "enabled_currencies"   =>   [
-      "name"      =>"Valute abilitate",
-
-   ],
-   "google_statistic_code"   =>   [
-      "name"      =>"Codice statistico di Google",
-
-   ],
+    'default_country' => [
+        'name' => 'Paese annuncio predefinito',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Chiave API del convertitore di valuta'
+    ],
+    'default_city' => [
+        'name' => 'Città annuncio predefinita',
+    ],
+    'default_district' => [
+        'name' => 'Distretto annunci predefinito',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Quartiere annunci predefinito',
+    ],
+    'default_currency' => [
+        'name' => 'Valuta annuncio predefinita',
+    ],
+    'default_GET' => [
+        'name' => 'Annuncio predefinito GET',
+    ],
+    'twitter' => [
+        'name' => 'cinguettio',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Tipo di filigrana',
+    ],
+    'watermark_text' => [
+        'name' => 'Testo filigrana',
+    ],
+    'watermark_image' => [
+        'name' => 'Immagine filigrana',
+    ],
+    'watermark_position' => [
+        'name' => 'Posizione della filigrana',
+    ],
+    'watermark_opacity' => [
+        'name' => 'Opacità filigrana',
+    ],
+    'listing_page_image' => [
+        'name' => 'Immagine della pagina di elenco',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Valute abilitate',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Codice statistico di Google',
+    ],
 
 ];
