30f74a00aaa60635f0974e05c5195a1b3138eeb0
Fatih Alp <mail@fatihalp.com>
2020-01-10 17:20:42 +0300

    New translations setting.php (Turkish)

---
+++ addons/default/visiosoft/base-theme/resources/lang/tr/setting.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Navigation Title',
+    ],
+    'navigation_action' => [
+        'name' => 'Navigation Action',
+    ],
+    'country_fields' => [
+        'name' => 'Country Fields',
+    ],
+    'date_fields' => [
+        'name' => 'Date Fields',
+    ],
+    'price_fields' => [
+        'name' => 'Price Fields',
+    ],
+    'breadcrumbs' => [
+        'name' => 'Breadcrumb',
+    ],
+    'ad_details' => [
+        'name' => 'Ad Details',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Ad Details Tab',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Latest and View All Btn',
+    ],
+];
