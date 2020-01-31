0722d251de524147cb06e6fac7baab5beb5a81de
Fatih Alp <mail@fatihalp.com>
2020-01-31 12:02:39 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/profile-module/resources/lang/tr/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/tr/field.php
@@ -8,7 +8,7 @@ return [
         'name' => 'İsim'
     ],
     'last_name' => [
-        'name' => 'Soyisim'
+        'name' => 'Soyad'
     ],
     'username' => [
         'name' => 'Kullanıcı Adı'
@@ -20,7 +20,7 @@ return [
         'name' => 'Email Adresi'
     ],
     'sitename' => [
-        'name' => 'siteadı'
+        'name' => 'Siteadı'
     ],
     'activated' => [
         'name' => 'Aktif Kullanıcı'
@@ -198,28 +198,28 @@ return [
 
     /* Right Dock*/
     'right_secure_trading_subject' => [
-        'name' => 'Secure Trading'
+        'name' => 'Güvenli Ticaret'
     ],
     'right_secure_trading_msg' => [
-        'name' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        'name' => 'Lorem Ipsum Dolor Sit Amet, Consipter Adipisicing Elit'
     ],
     'right_support_subject' => [
-        'name' => '24/7 Support'
+        'name' => '7/24 Destek'
     ],
     'right_support_msg' => [
-        'name' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        'name' => 'Lorem Ipsum Dolor Sit Amet, Consipter Adipisicing Elit'
     ],
     'right_easy_trading_subject' => [
-        'name' => 'Easy Trading'
+        'name' => 'Kolay Ticaret'
     ],
     'right_easy_trading_msg' => [
-        'name' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        'name' => 'Lorem Ipsum Dolor Sit Amet, Consipter Adipisicing Elit'
     ],
     'right_need_help_subject' => [
-        'name' => 'Need Help?'
+        'name' => 'Yardıma mı ihtiyacınız var?'
     ],
     'right_need_help_msg' => [
-        'name' => 'Give a call on'
+        'name' => 'Telefon etmek'
     ],
     'disable_account' => [
         'name' => 'Hesabı Dondur'
