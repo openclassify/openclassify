1eb8d34dff7234597271e512d386e7b7f985750f
vedatakd <vedat@vebze.com>
2020-02-04 11:35:32 +0300

    phone field updated

--- addons/default/visiosoft/base-theme/resources/js/theme/initialize.js
+++ addons/default/visiosoft/base-theme/resources/js/theme/initialize.js
@@ -3,18 +3,3 @@
     // Go!
     
 })(window, document);
-
-
-var inputQuery = document.querySelector("input[name=\"phone\"]");
-var iti = intlTelInput(inputQuery, {
-    hiddenInput: "full_phone",
-    class:"form-control",
-    initialCountry: "auto",
-    geoIpLookup: function (success, failure) {
-        $.get("https://ipinfo.io", function () {
-        }, "jsonp").always(function (resp) {
-            var countryCode = (resp && resp.country) ? resp.country : "";
-            success(countryCode);
-        });
-    }
-});
