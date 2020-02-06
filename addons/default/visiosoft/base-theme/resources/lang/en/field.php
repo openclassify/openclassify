24d288c2d02ca0791cffc267499edd1cfa61f9b0
Diatrex <diahshalabi@hotmail.com>
2020-02-06 16:15:48 +0300

    #707 [Store] If store is installed show store register

--- addons/default/visiosoft/base-theme/resources/lang/en/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/en/field.php
@@ -22,4 +22,12 @@ return [
     'forgot_password' => 'I Forgot My Password',
     'email_or_phone_number' => 'Email Address or Phone Number',
     'phone_number' => 'Phone Number',
+
+    // Registration instructions
+    'personal_registration_header' => 'What Are Individual Membership Advantages?',
+    'personal_registration_body' => 'Sell and rent your home, car, sell your unused items, get new ones.',
+    'personal_registration_list_1' => 'Post a free ad,',
+    'personal_registration_list_2' => 'Add the ads you are interested in, follow the price changes after adding them to your favorites, create favorite searches that meet your criteria,',
+    'personal_registration_list_3' => 'Send messages to the ad owners on the site.',
+    'register_information_note' => 'The information on this page is taken for ' . env('APPLICATION_DOMAIN') . ' membership. You can find detailed information about the protection of personal data here.',
 ];
