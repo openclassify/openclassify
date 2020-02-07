e8fd34003312e070a95b7672c9773bda8ef0e944
Fatih Alp <mail@fatihalp.com>
2020-02-07 13:11:44 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/base-theme/resources/lang/tr/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/tr/field.php
@@ -22,4 +22,12 @@ return [
     'forgot_password' => 'Şifremi Unuttum',
     'email_or_phone_number' => 'E-Posta Adresiniz veya Telefon Numaranız',
     'phone_number' => 'Telefon Numarası',
+
+    // Registration instructions
+    'personal_registration_header' => 'What Are Individual Membership Advantages?',
+    'personal_registration_body' => 'Sell and rent your home, car, sell your unused items, get new ones.',
+    'personal_registration_list_1' => 'Post a free ad,',
+    'personal_registration_list_2' => 'Add the ads you are interested in, follow the price changes after adding them to your favorites, create favorite searches that meet your criteria,',
+    'personal_registration_list_3' => 'Send messages to the ad owners on the site.',
+    'register_information_note' => 'The information on this page is taken for ' . env('APPLICATION_DOMAIN') . ' membership. You can find detailed information about the protection of personal data here.',
 ];
