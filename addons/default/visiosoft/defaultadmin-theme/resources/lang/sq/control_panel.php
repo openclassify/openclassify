ec20b54a4e283afb4a74517016caf5c4e0ac2d03
Fatih Alp <mail@fatihalp.com>
2020-02-12 13:18:08 +0300

    New translations control_panel.php (Albanian)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/sq/control_panel.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'help'      => 'Help',
+    'search'    => 'Search',
+    'logout'    => 'Logout',
+    'view_site' => 'View Site',
+    'title'     => 'Control Panel',
+    'search_placeholder'     => 'Type to search',
+];
