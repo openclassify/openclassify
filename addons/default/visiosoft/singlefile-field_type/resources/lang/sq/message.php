74690f621deacd18e5e04cfb956f7f675988a028
Fatih Alp <mail@fatihalp.com>
2020-02-12 14:18:20 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/sq/message.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/sq/message.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'choose_file'      => 'Which file would you like to use?',
-    'choose_folder'    => 'Which folder would you like to upload to?',
-    'upload'           => 'Click or drop files here to upload.',
-    'no_file_selected' => 'No file selected.',
-    'no_uploads'       => 'No files uploaded.',
-    'overwrite'        => 'has already been uploaded. Would you like to overwrite it?',
-    'uploading'        => 'Uploading',
-    'loading'          => 'Loading',
+    'choose_file'      => 'Cilin skedar dëshironi të përdorni?',
+    'choose_folder'    => 'Në cilën dosje dëshironi të ngarkoni?',
+    'upload'           => 'Klikoni ose lëshoni skedarët këtu për të ngarkuar.',
+    'no_file_selected' => 'Asnjë skedar nuk është zgjedhur.',
+    'no_uploads'       => 'Asnjë fotografi të ngarkuar.',
+    'overwrite'        => 'tashmë është ngarkuar. Dëshiron ta rishkruash?',
+    'uploading'        => 'ngarkimi',
+    'loading'          => 'ngarkim',
 ];
