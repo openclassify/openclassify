ee6280c357ddae3ddb0486f9de77aad2057f9bf7
Fatih Alp <mail@fatihalp.com>
2020-02-12 13:18:01 +0300

    New translations field.php (Albanian)

---
+++ addons/default/visiosoft/cats-module/resources/lang/sq/field.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Title',
+    ],
+    'order' => [
+        'name' => 'Order',
+    ],
+    'slug' => [
+        'name' => 'Slug'
+    ],
+    'files' => [
+        'name' => 'Image'
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword'
+    ],
+    'seo_description' => [
+        'name' => 'Seo Description'
+    ],
+    'icon' => [
+        'name' => 'Icon'
+    ],
+
+    'please_wait' => 'Please wait.Deleting Sub Categories',
+    'category_selection' => 'Category Selection',
+];
