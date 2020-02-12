07e2730812c885e14ed7a13245e31b4c1ea5d308
Fatih Alp <mail@fatihalp.com>
2020-02-12 14:18:19 +0300

    New translations button.php (Albanian)

--- addons/default/visiosoft/profile-module/resources/lang/sq/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/sq/button.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'new_profile' => 'New Profile',
-    'new_adress' => 'New Adress',
-    'show' => 'Show Adress',
-    'update_password' => 'Update Password',
-    'update_profile' => 'Update Profile',
-    'delete' => 'Delete',
-    'edit' => 'Edit',
+    'new_profile' => 'Profili i ri',
+    'new_adress' => 'Adresa e Re',
+    'show' => 'Trego Adresën',
+    'update_password' => 'Përditëso fjalëkalimin',
+    'update_profile' => 'Përditësoje profilin',
+    'delete' => 'fshini',
+    'edit' => 'redaktoj',
 ];
