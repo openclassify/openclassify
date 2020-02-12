7b442e3a5b95edb76c95afa26074710a8709c2b1
Fatih Alp <mail@fatihalp.com>
2020-02-12 13:17:50 +0300

    New translations setting.php (Albanian)

---
+++ addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
@@ -0,0 +1,113 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'Auto Approve',
+    ],
+    'default_published_time' => [
+        'name' => 'Default Publish Time',
+    ],
+    'default_adv_limit' => [
+        'name' => 'Default Ads Limit',
+    ],
+    'address' => [
+        'name' => 'Contact Adress',
+    ],
+    'phone' => [
+        'name' => 'Contact Phone',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Map Long Coordinate',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Map Lat Coordinate',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Social Share Logo',
+        'warning'      => 'It should be at a resolution of 1200 x 630. ',
+
+    ],
+    'currencies' => [
+        'name' => 'Active Currencies',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'Site address',
+        'warning'      => 'Auto added www.  www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Key',
+    ],
+
+    'default_country' => [
+        'name' => 'Default Ad Country',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Currency Converter API Key'
+    ],
+    'default_city' => [
+        'name' => 'Default Ad City',
+    ],
+    'default_district' => [
+        'name' => 'Default Ad District',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Default Ad Neighborhood',
+    ],
+    'default_currency' => [
+        'name' => 'Default Ad Currency',
+    ],
+    'default_GET' => [
+        'name' => 'Default Ad GET',
+    ],
+    'twitter' => [
+        'name' => 'Twitter',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Watermark Type',
+    ],
+    'watermark_text' => [
+        'name' => 'Watermark Text',
+    ],
+    'watermark_image' => [
+        'name' => 'Watermark Image',
+    ],
+    'watermark_position' => [
+        'name' => 'Watermark Position',
+    ],
+    'watermark_opacity' => [
+        'name' => 'Watermark Opacity',
+    ],
+    'listing_page_image' => [
+        'name' => 'Listing Page Image',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Enabled Currencies',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google Statistic Code',
+    ],
+    'register_email_field' => [
+        'name' => 'Member Registration Email Requirement',
+    ],
+];
