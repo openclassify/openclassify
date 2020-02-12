64e791164520b40714cf7635ee28f34258f216ac
Fatih Alp <mail@fatihalp.com>
2020-02-12 14:18:28 +0300

    New translations permission.php (Albanian)

--- addons/default/visiosoft/profile-module/resources/lang/sq/permission.php
+++ addons/default/visiosoft/profile-module/resources/lang/sq/permission.php
@@ -4,17 +4,17 @@ return [
     'profile' => [
         'name'   => 'Profile',
         'option' => [
-            'read'   => 'Can read profile?',
-            'write'  => 'Can create/edit profile?',
-            'delete' => 'Can delete profile?',
+            'read'   => 'Mund të lexoni profilin?',
+            'write'  => 'Mund të krijoni / modifikoni profilin?',
+            'delete' => 'Mund të fshini profilin?',
         ],
     ],
     'adress' => [
-        'name'   => 'Adress',
+        'name'   => 'Adresa',
         'option' => [
-            'read'   => 'Can read adress?',
-            'write'  => 'Can create/edit adress?',
-            'delete' => 'Can delete adress?',
+            'read'   => 'Mund të lexoni adresën?',
+            'write'  => 'Mund të krijoni / modifikoni adresën?',
+            'delete' => 'Mund të fshini adresën?',
         ],
     ],
 ];
