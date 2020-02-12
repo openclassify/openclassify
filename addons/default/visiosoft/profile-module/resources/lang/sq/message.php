f4e265857efd6840c7b24100cfdf0e8fb6872265
Fatih Alp <mail@fatihalp.com>
2020-02-12 13:17:29 +0300

    New translations message.php (Albanian)

---
+++ addons/default/visiosoft/profile-module/resources/lang/sq/message.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    "email"      => "E-Mail Field is Required!",
+    "username"      => "Username Field is Required!",
+    "success_update"      => "Profile Update Successfully!",
+    "adress_success_update"      => "Adress Update Successfully!",
+    "adress_success_create"      => "Adress Create Successfully!",
+    "login_error" => "Incorrect login or password.",
+    "login_noMail_old_user" => "The email address you entered couldn’t be found.",
+    "login_noMail_old_user2" => "Please check the email address and try again.",
+    "login_noMail_old_user3" => "If the problem continues, please contact at 'info@openclassify.com'.",
+    "login_info_old_user" => "A new password has been sent to your e-mail address.",
+    "success" => "Success",
+    "notified_new_updates" => "I want to be notified of new updates",
+    "notified_about_ads" => "Allow notifications about my ads",
+    "receive_messages_email" => "I want to receive messages as email",
+    "no_packages_module" => "No Packages Module!",
+    "required_add"=> "Please fill in all required lines.",
+    "no_extend_package" => "No Extend Ad Packages",
+    "saved" => "Saved!",
+    'please_confirm_transaction' => 'Please Confirm Transaction',
+    'password_do_not_match' => 'Passwords do not match',
+    'your_password_changed' => 'Your password was successfully updated',
+    'error_valid_email_or_phone' => 'Phone number or E-mail address format is not correct.',
+    'error_valid_phone' => 'Phone number format is not correct.',
+    'registered_phone' => 'This phone number has already been registered.',
+];
