c8862cb50c90fb23a80ad368f79a7542ca033a83
Fatih Alp <mail@fatihalp.com>
2020-02-12 14:18:05 +0300

    New translations addon.php (Albanian)

--- addons/default/visiosoft/location-module/resources/lang/sq/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/sq/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Location',
-    'name'        => 'Location Module',
-    'description' => 'Description',
+    'title'       => 'vend',
+    'name'        => 'Moduli i vendndodhjes',
+    'description' => 'Përshkrim',
 ];
