b071264af8f60edb655696c60139db962e4c3c71
Fatih Alp <mail@fatihalp.com>
2020-02-12 14:18:01 +0300

    New translations permission.php (Albanian)

--- addons/default/visiosoft/cats-module/resources/lang/sq/permission.php
+++ addons/default/visiosoft/cats-module/resources/lang/sq/permission.php
@@ -2,19 +2,19 @@
 
 return [
     'category' => [
-        'name'   => 'Category',
+        'name'   => 'kategori',
         'option' => [
-            'read'   => 'Can read category?',
-            'write'  => 'Can create/edit category?',
-            'delete' => 'Can delete category?',
+            'read'   => 'Mund të lexoni kategori?',
+            'write'  => 'Mund të krijoni / modifikoni kategori?',
+            'delete' => 'Mund të fshini kategorinë?',
         ],
     ],
     'placeholderforsearch' => [
         'name'   => 'Placeholderforsearch',
         'option' => [
-            'read'   => 'Can read placeholderforsearch?',
-            'write'  => 'Can create/edit placeholderforsearch?',
-            'delete' => 'Can delete placeholderforsearch?',
+            'read'   => 'A mund të lexoni pronarin e faqes?',
+            'write'  => 'Mund të krijojë / modifikojë vendin e fshehtë?',
+            'delete' => 'Mund ta fshini vendin e mbajtjes së faqes?',
         ],
     ],
 ];
