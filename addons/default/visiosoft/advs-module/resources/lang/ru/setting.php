ee007c70c1b6d58a44f73352503be7eb61a5e49a
Fatih Alp <mail@fatihalp.com>
2020-02-14 15:06:35 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
@@ -1,136 +1,110 @@
 <?php
 
- return[
-   "auto_approve"   =>   [
-      "name"      =>"Авто Одобрить",
+return [
+    'auto_approve' => [
+        'name' => 'Auto Approve',
+    ],
+    'default_published_time' => [
+        'name' => 'Default Publish Time',
+    ],
+    'default_adv_limit' => [
+        'name' => 'Default Ads Limit',
+    ],
+    'address' => [
+        'name' => 'Contact Adress',
+    ],
+    'phone' => [
+        'name' => 'Contact Phone',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Map Long Coordinate',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Map Lat Coordinate',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Social Share Logo',
+        'warning'      => 'It should be at a resolution of 1200 x 630. ',
 
-   ],
-   "default_published_time"   =>   [
-      "name"      =>"Время публикации по умолчанию",
+    ],
+    'currencies' => [
+        'name' => 'Active Currencies',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'Site address',
+        'warning'      => 'Auto added www.  www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Key',
+    ],
 
-   ],
-   "default_adv_limit"   =>   [
-      "name"      =>"Предел рекламы по умолчанию",
-
-   ],
-   "address"   =>   [
-      "name"      =>"Контактный адрес",
-
-   ],
-   "phone"   =>   [
-      "name"      =>"Контактный телефон",
-
-   ],
-   "contact_mail"   =>   [
-      "name"      =>"Контактная почта",
-      "warning"      =>"Авто добавлено информация <strong>о</strong> названии сайта @sitename",
-
-   ],
-   "map_coordinates_long"   =>   [
-      "name"      =>"Карта Длинная координата",
-
-   ],
-   "map_coordinates_lat"   =>   [
-      "name"      =>"Карта Лат Координата",
-
-   ],
-   "logo"   =>   [
-      "name"      =>"логотип",
-
-   ],
-   "ogImage"   =>   [
-      "name"      =>"Social Share Logo",
-      "warning"      =>"Это должно быть с разрешением 1200 х 630.",
-
-   ],
-   "currencies"   =>   [
-      "name"      =>"Активные валюты",
-
-   ],
-   "site_address"   =>   [
-      "name"      =>"Адрес сайта",
-      "warning"      =>"Авто добавлено www. WWW. <strong>sitename.com</strong>",
-
-   ],
-   "google_map_key"   =>   [
-      "name"      =>"Google Map Api Key",
-
-   ],
-   "default_country"   =>   [
-      "name"      =>"Страна объявления по умолчанию",
-
-   ],
-   "free_currencyconverterapi_key"   =>   [
-      "name"      =>"Конвертер валют API Key",
-
-   ],
-   "default_city"   =>   [
-      "name"      =>"Город объявлений по умолчанию",
-
-   ],
-   "default_district"   =>   [
-      "name"      =>"Район рекламы по умолчанию",
-
-   ],
-   "default_neighborhood"   =>   [
-      "name"      =>"Окружение объявлений по умолчанию",
-
-   ],
-   "default_currency"   =>   [
-      "name"      =>"Валюта объявления по умолчанию",
-
-   ],
-   "default_GET"   =>   [
-      "name"      =>"Реклама по умолчанию GET",
-
-   ],
-   "twitter"   =>   [
-      "name"      =>"щебет",
-
-   ],
-   "facebook"   =>   [
-      "name"      =>"facebook",
-
-   ],
-   "youtube"   =>   [
-      "name"      =>"YouTube",
-
-   ],
-   "google"   =>   [
-      "name"      =>"Google",
-
-   ],
-   "watermark_type"   =>   [
-      "name"      =>"Тип водяного знака",
-
-   ],
-   "watermark_text"   =>   [
-      "name"      =>"Текст водяного знака",
-
-   ],
-   "watermark_image"   =>   [
-      "name"      =>"Изображение водяного знака",
-
-   ],
-   "watermark_position"   =>   [
-      "name"      =>"Положение водяного знака",
-
-   ],
-   "watermark_opacity"   =>   [
-      "name"      =>"Непрозрачность водяного знака",
-
-   ],
-   "listing_page_image"   =>   [
-      "name"      =>"Изображение страницы со списком",
-
-   ],
-   "enabled_currencies"   =>   [
-      "name"      =>"Включенные валюты",
-
-   ],
-   "google_statistic_code"   =>   [
-      "name"      =>"Статистический код Google",
-
-   ],
-
+    'default_country' => [
+        'name' => 'Default Ad Country',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Currency Converter API Key'
+    ],
+    'default_city' => [
+        'name' => 'Default Ad City',
+    ],
+    'default_district' => [
+        'name' => 'Default Ad District',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Default Ad Neighborhood',
+    ],
+    'default_currency' => [
+        'name' => 'Default Ad Currency',
+    ],
+    'default_GET' => [
+        'name' => 'Default Ad GET',
+    ],
+    'twitter' => [
+        'name' => 'Twitter',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Watermark Type',
+    ],
+    'watermark_text' => [
+        'name' => 'Watermark Text',
+    ],
+    'watermark_image' => [
+        'name' => 'Watermark Image',
+    ],
+    'watermark_position' => [
+        'name' => 'Watermark Position',
+    ],
+    'listing_page_image' => [
+        'name' => 'Listing Page Image',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Enabled Currencies',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google Statistic Code',
+    ],
+    'register_email_field' => [
+        'name' => 'Member Registration Email Requirement',
+    ],
 ];
