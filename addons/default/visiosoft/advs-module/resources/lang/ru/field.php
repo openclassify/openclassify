4b7a7875078236124d07d21218c53ff15d8218ef
Fatih Alp <mail@fatihalp.com>
2020-02-14 15:06:40 +0300

    New translations field.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/field.php
@@ -1,266 +1,262 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"заглавие",
-
-   ],
-   "order"   =>   [
-      "name"      =>"порядок",
-
-   ],
-   "slug"   =>   [
-      "name"      =>"слизень",
-
-   ],
-   "parent_category"   =>   [
-      "name"      =>"родитель",
-
-   ],
-   "category"   =>   [
-      "name"      =>"Название категории",
-
-   ],
-   "price"   =>   [
-      "name"      =>"Цена",
-
-   ],
-   "stock"   =>   [
-      "name"      =>"Склад",
-
-   ],
-   "advs_desc"   =>   [
-      "name"      =>"Описание",
-
-   ],
-   "online_payment"   =>   [
-      "name"      =>"Онлайн платеж",
-
-   ],
-   "city"   =>   [
-      "name"      =>"город",
-
-   ],
-   "country"   =>   [
-      "name"      =>"Страна",
-
-   ],
-   "district"   =>   [
-      "name"      =>"район",
-
-   ],
-   "neighborhood"   =>   [
-      "name"      =>"окрестности",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Деревня",
-
-   ],
-   "files"   =>   [
-      "name"      =>"Изображений",
-
-   ],
-   "cat1"   =>   [
-      "name"      =>"Главная категория",
-
-   ],
-   "cat2"   =>   [
-      "name"      =>"Подкатегория 1",
-
-   ],
-   "cat3"   =>   [
-      "name"      =>"Подкатегория 2",
-
-   ],
-   "cat4"   =>   [
-      "name"      =>"Подкатегория 3",
-
-   ],
-   "cat5"   =>   [
-      "name"      =>"Подкатегория 4",
-
-   ],
-   "cat6"   =>   [
-      "name"      =>"Подкатегория 5",
-
-   ],
-   "cat7"   =>   [
-      "name"      =>"Подкатегория 6",
-
-   ],
-   "currency"   =>   [
-      "name"      =>"валюта",
-
-   ],
-   "status"   =>   [
-      "name"      =>"Статус",
-
-   ],
-   "next_add_advs_title"   =>   [
-      "name"      =>"Разместите объявление всего за 30 секунд",
-
-   ],
-   "next_add_advs_msg"   =>   [
-      "name"      =>"Пожалуйста,
-      НЕ публикуйте несколько объявлений для одних и тех же товаров или услуг. Все дубликаты,
-      спам и неправильно классифицированные объявления будут удалены.",
-
-   ],
-   "next_add_advs_btn"   =>   [
-      "name"      =>"следующий",
-
-   ],
-   "cancel_add_advs_btn"   =>   [
-      "name"      =>"или Отмена",
-
-   ],
-   "pending_adv"   =>   [
-      "name"      =>"Ожидающие объявления",
-
-   ],
-   "archived_adv"   =>   [
-      "name"      =>"Архивные объявления",
-
-   ],
-   "favs_adv"   =>   [
-      "name"      =>"Избранные объявления",
-
-   ],
-   "my_adv"   =>   [
-      "name"      =>"ADVS",
-
-   ],
-   "description"   =>   [
-      "name"      =>"Описание",
-
-   ],
-   "type"   =>   [
-      "name"      =>"Тип",
-
-   ],
-   "parent_adv"   =>   [
-      "name"      =>"Похожие объявления",
-
-   ],
-   "value"   =>   [
-      "name"      =>"Значение",
-
-   ],
-   "home"   =>"Главная",
-   "list-page"   =>"Страница списка",
-   "search"   =>"Поиск",
-   "recommended"   =>"Рекомендуемые объявления для вас",
-   "sort_by"   =>"Сортировать по",
-   "pick_option"   =>"Выберите опцию",
-   "price_high"   =>"Цена по убыванию",
-   "price-low"   =>"Цена от низкой к высокой",
-   "newest"   =>"новейший",
-   "categories"   =>"категории",
-   "all_categories"   =>"все категории",
-   "location"   =>"Место нахождения",
-   "send_message"   =>"Отправить сообщение",
-   "phone_gsm"   =>"Gsm Phone",
-   "phone_office"   =>"Офисный телефон",
-   "add_to_favorites"   =>"Добавить в избранное",
-   "send"   =>"послать",
-   "search_name"   =>"Имя поиска",
-   "my_favorite_searches"   =>"Избранные поиски",
-   "search_desc"   =>"Вы можете добавлять и управлять вашими любимыми поисковыми запросами здесь.",
-   "complaint_placeholder"   =>"Введите данные вашей жалобы здесь.",
-   "show_details"   =>"Показать детали",
-   "edit"   =>"редактировать",
-   "approve"   =>"Утвердить",
-   "passive"   =>"пассивный",
-   "offered_by"   =>"Предложено",
-   "more_ads_by"   =>"Больше объявлений от",
-   "click_phone"   =>"Показать номер телефона",
-   "share"   =>"Поделиться этим объявлением",
-   "report"   =>"Сообщить об этом объявлении",
-   "features"   =>"Характеристики",
-   "streetview"   =>"Просмотр улиц",
-   "recommended_ads"   =>"Рекомендуемые объявления для вас",
-   "new"   =>"новый",
-   "short_info"   =>"Краткая информация",
-   "secure_trading"   =>"Безопасная торговля",
-   "24/7"   =>"Поддержка 24/7",
-   "easy_trading"   =>"Easy Trading",
-   "need_help"   =>"Нужна помощь?",
-   "give_a_call"   =>"Позвонить на",
-   "message_sent"   =>"Сообщение отправлено!",
-   "asap"   =>"Сообщение отправлено,
-   продавец свяжется с вами как можно скорее.",
-   "ad_id"   =>"Идентификатор объявления",
-   "contact_with"   =>"Связаться с",
-   "create_adv"   =>"Создать объявление",
-   "edit_adv"   =>"Изменить объявление",
-   "mandatory_fields"   =>"Обязательные поля",
-   "ad_post"   =>"Объявление",
-   "sell_item"   =>"Продать предмет или услугу",
-   "search_save"   =>"Избранное успешно сохранено",
-   "address"   =>"Адрес",
-   "email"   =>"Эл. почта",
-   "phone"   =>"Телефон",
-   "send_us_your_feedback"   =>"Отправьте нам свой отзыв",
-   "online_shopping"   =>"ПОЛУЧИТЬ",
-   "approved"   =>"Успешно утвержден",
-   "declined"   =>"Успешно отклонено",
-   "search_on_map"   =>"Поиск по карте",
-   "post_free_ad"   =>"Разместить бесплатное объявление",
-   "select_a_category"   =>"Выбрать категорию",
-   "select_a_sub_category"   =>"Выберите подкатегорию",
-   "all_dopings_added"   =>"Все виды допинга добавляются для этого объявления.",
-   "is_get_adv"   =>   [
-      "name"      =>"ПОЛУЧИТЬ",
-
-   ],
-   "corporate_info"   =>"Корпоративная информация",
-   "featured_ads"   =>"Избранные объявления",
-   "trending_ads"   =>"Trendind Ads",
-   "search_ads_result_message"   =>   [
-      "name"      =>":      количество объявлений найдено для:поиск по названию",
-
-   ],
-   "total"   =>"Всего",
-   "quantity"   =>"Количество",
-   "view_cart"   =>"Просмотр корзины",
-   "no_ads"   =>"Без рекламы!",
-   "show_display_mode"   =>   [
-      "map"      =>"карта",
-      "list"      =>"Список",
-
-   ],
-   "advs_list_table_thead"   =>   [
-      "name"      =>"Название объявления",
-      "price"      =>"Цена",
-      "publish_at"      =>"Дата объявления",
-      "location_city"      =>"город",
-      "location_country"      =>"Страна",
-
-   ],
-   "show_list_view"   =>   [
-      "name"      =>"Список",
-
-   ],
-   "show_table_view"   =>   [
-      "name"      =>"Таблица",
-
-   ],
-   "showcase"   =>   [
-      "name"      =>"витрина",
-
-   ],
-   "new_ad"   =>   [
-      "name"      =>"Новое объявление",
-
-   ],
-   "profile"   =>   [
-      "name"      =>"Профиль",
-
-   ],
-   "my_ads"   =>   [
-      "name"      =>"Мои объявления",
-
-   ],
-
+return [
+    'name' => [
+        'name' => 'Title'
+    ],
+    'order' => [
+        'name' => 'Order'
+    ],
+    'slug' => [
+        'name' => 'Slug'
+    ],
+    'parent_category' => [
+        'name' => 'Parent'
+    ],
+    'category' => [
+        'name' => 'Category Title'
+    ],
+    'price' => [
+        'name' => 'Price'
+    ],
+    'date' => [
+        'name' => 'Date'
+    ],
+    'photo' => [
+        'name' => 'Photo'
+    ],
+    'video' => [
+        'name' => 'Video'
+    ],
+    'map' => [
+        'name' => 'Map'
+    ],
+    'stock' => [
+        'name' => 'Stock'
+    ],
+    'advs_desc' => [
+        'name' => 'Description'
+    ],
+    'online_payment' => [
+        'name' => 'Online Payment'
+    ],
+    'city' => [
+        'name' => 'City'
+    ],
+    'country' => [
+        'name' => 'Country'
+    ],
+    'district' => [
+        'name' => 'District'
+    ],
+    'neighborhood' => [
+        'name' => 'Neighborhood'
+    ],
+    'village' => [
+        'name' => 'Village'
+    ],
+    'files' => [
+        'name' => 'Images'
+    ],
+    'cat1' => [
+        'name' => 'Main Category'
+    ],
+    'cat2' => [
+        'name' => 'Sub Category 1'
+    ],
+    'cat3' => [
+        'name' => 'Sub Category 2'
+    ],
+    'cat4' => [
+        'name' => 'Sub Category 3'
+    ],
+    'cat5' => [
+        'name' => 'Sub Category 4'
+    ],
+    'cat6' => [
+        'name' => 'Sub Category 5'
+    ],
+    'cat7' => [
+        'name' => 'Sub Category 6'
+    ],
+    'currency' => [
+        'name' => 'Currency'
+    ],
+    'status' => [
+        'name' => 'Status',
+        'option' => [
+            'approved' => 'Approved',
+            'expired' => 'Expired',
+            'unpublished' => 'Unpublished',
+            'pending_admin' => 'Pending Admin',
+            'pending_user' => 'Pending User'
+        ]
+    ],
+    'pending_adv' => [
+        'name' => 'Pending Ads'
+    ],
+    'archived_adv' => [
+        'name' => 'Archived Ads'
+    ],
+    'finish_at' => [
+        'name' => 'Finish At'
+    ],
+    'cover_photo' => [
+        'name' => 'Cover Photo'
+    ],
+    'favs_adv' => [
+        'name' => 'Fav Ads'
+    ],
+    'my_adv' => [
+        'name' => 'ADVS'
+    ],
+    'description' =>  'Description',
+    'explanation' =>  'Explanation',
+    'type' => [
+        'name' => 'Type'
+    ],
+    'parent_adv' => [
+        'name' => 'Related Ad'
+    ],
+    'value' => [
+        'name' => 'Value'
+    ],
+    'list' => 'List',
+    'home' => 'Home',
+    'list-page' => 'List Page',
+    'search' => 'Search',
+    'recommended' => 'Recommended Ads for You',
+    'sort_by' => 'Sort By',
+    'pick_option' => 'Pick an option',
+    'pick_ordering' => 'Ordering',
+    'price_high' => 'Price High to Low',
+    'price-low' => 'Price Low to High',
+    'newest' => 'Newest',
+    'categories' => 'Categories',
+    'all_categories' => 'All Categories',
+    'location' => 'Location',
+    'send_message' => 'Send Message',
+    'phone_gsm' => 'Gsm Phone',
+    'phone_office' => 'Office Phone',
+    'add_to_favorites' => "Add to Favorites",
+    'send' => 'Send',
+    'search_name' => 'Search Name',
+    'my_favorite_searches' => 'Favorite Searches',
+    'search_desc' => 'You can add and manage your favorite search selections here.',
+    'complaint_placeholder' => 'Type your complaint details here.',
+    'show_details' => 'Show Details',
+    'edit' => 'Edit',
+    'approve' => 'Approve',
+    'passive' => "Passive",
+    'offered_by' => 'Offered By',
+    'more_ads_by' => 'More Ads By',
+    'click_phone' => 'Show phone number',
+    'share' => 'Share',
+    'report' => 'Report this ad',
+    'features' => 'Features',
+    "streetview" => "Street View",
+    'recommended_ads' => 'Recommended Ads for You',
+    'new' => 'New',
+    'short_info' => 'Short Info',
+    'secure_trading' => 'Secure Trading',
+    '24/7' => '24/7 Support',
+    'easy_trading' => 'Easy Trading',
+    'need_help' => 'Need help?',
+    'give_a_call' => 'Give a call on',
+    'message_sent' => 'Message Sent!',
+    'asap' => 'Message sent, seller will contact you asap.',
+    'ad_id' => 'Ad Id',
+    'contact_with' => 'Contact With',
+    'create_adv' => 'Create Ad',
+    'edit_adv' => 'Edit Ad',
+    'mandatory_fields' => 'Mandatory Fields',
+    'ad_post' => 'Ad Post',
+    'sell_item' => 'Sell an item or service',
+    'search_save' => 'Favorite search saved successfully',
+    'address' => 'Address',
+    'email' => 'E-Mail',
+    'phone' => 'Phone',
+    'send_us_your_feedback' => 'Send Us Your Feedback',
+    'online_shopping' => 'GET',
+    'approved' => 'Approved Successfully',
+    'extended' => 'Extended Successfully',
+    'declined' => 'Declined Successfully',
+    'search_on_map' => 'Search on Map',
+    'post_free_ad' => 'Post Free Ad',
+    'select_a_category' => 'Select a Category',
+    'select_a_sub_category' => 'Select a Sub Category',
+    'all_dopings_added' => 'All doping types are added for this ad.',
+    'is_get_adv' => [
+        'name' => 'GET'
+    ],
+    'corporate_info' => 'Corporate Info',
+    'featured_ads' => "Featured Ads",
+    'trending_ads' => 'Trendind Ads',
+    'search_ads_result_message' => [
+        'name' => ':count ads found for :name search',
+    ],
+    'total' => 'Total',
+    'quantity' => 'Quantity',
+    'view_cart' => 'View Cart',
+    'no_ads' => 'No Ads!',
+    'show_display_mode' => [
+        'map' => 'Map',
+        'list' => 'List',
+    ],
+    'advs_list_table_thead' => [
+        'name' => 'Ad Name',
+        'price' => 'Price',
+        'publish_at' => 'Ad Date',
+        'location_city' => 'City',
+        'location_country' => 'Country',
+    ],
+    'show_list_view' => [
+        'name' => 'List',
+    ],
+    'show_table_view' => [
+        'name' => 'Table',
+    ],
+    'showcase' => [
+        'name' => 'Showcase',
+    ],
+    'new_ad' => [
+        'name' => 'New Ad',
+    ],
+    'profile' => [
+        'name' => 'Profile',
+    ],
+    'my_ads' => [
+        'name' => 'My Ads',
+    ],
+    'sold_out' => [
+        'name' => 'Sold out',
+    ],
+    'edit_category' => 'Edit Category',
+    'max' => [
+        'name' => 'Max',
+    ],
+    'min' => [
+        'name' => 'Min',
+    ],
+    'in_the_last_24_hours' => [
+        'name' => 'In The Last 24 Hours'
+    ],
+    'in_the_last_week' => [
+        'name' => 'In The Last Week'
+    ],
+    'in_the_last_month' => [
+        'name' => 'In The Last Month'
+    ],
+    'ads_with_photo' => [
+        'name' => 'Ads With Photo'
+    ],
+    'ads_with_video' => [
+        'name' => 'Ads With Video'
+    ],
+    'yes' => [
+        'name' => 'Yes'
+    ],
+    "no_location" => "No location is selected.",
+    "continue" => 'Continue',
+    "gallery" => 'Gallery',
 ];
