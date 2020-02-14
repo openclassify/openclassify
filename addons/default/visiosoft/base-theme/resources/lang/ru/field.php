f17b23964752741ef6d6158afda07b018c8a9e8e
Fatih Alp <mail@fatihalp.com>
2020-02-14 15:06:33 +0300

    New translations field.php (Russian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ru/field.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'latest_ads' => 'Latest Ads',
+    'show_all' => 'Show All',
+    'login' => [
+        'name' => 'Login',
+    ],
+    'logout' => [
+        'name' => 'Logout',
+    ],
+    'profile' => 'Profile',
+    'email' => 'Email Address',
+    'reset_code' => 'Reset Code',
+    'password_confirmation' => 'Password Confirmation',
+    'password' => 'Password',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'remember_me' => 'Remember Me',
+    'not_a_member_yet' => 'Not a Member Yet',
+    'not_a_member_yet_message' => 'Sign up for our members to benefit from our special service',
+    'forgot_password' => 'I Forgot My Password',
+    'email_or_phone_number' => 'Email Address or Phone Number',
+    'phone_number' => 'Phone Number',
+
+    // Registration instructions
+    'personal_registration_header' => 'What Are Individual Membership Advantages?',
+    'personal_registration_body' => 'Sell and rent your home, car, sell your unused items, get new ones.',
+    'personal_registration_list_1' => 'Post a free ad,',
+    'personal_registration_list_2' => 'Add the ads you are interested in, follow the price changes after adding them to your favorites, create favorite searches that meet your criteria,',
+    'personal_registration_list_3' => 'Send messages to the ad owners on the site.',
+    'register_information_note' => 'The information on this page is taken for ' . env('APPLICATION_DOMAIN') . ' membership. You can find detailed information about the protection of personal data here.',
+];
