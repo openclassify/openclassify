3fc67841fcd57321dc4884b64cf28246c4b62cf1
Fatih Alp <mail@fatihalp.com>
2020-02-14 15:07:18 +0300

    New translations section.php (Russian)

--- addons/default/visiosoft/location-module/resources/lang/ru/section.php
+++ addons/default/visiosoft/location-module/resources/lang/ru/section.php
@@ -1,25 +1,22 @@
 <?php
 
- return[
-   "countries"   =>   [
-      "title"      =>"страны",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Города",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Районы",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"Окрестности",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Деревня",
-
-   ],
-
+return [
+    'countries' => [
+        'title' => 'Countries',
+    ],
+    'cities' => [
+        'title' => 'Cities',
+    ],
+    'districts' => [
+        'title' => 'Districts',
+    ],
+    'neighborhoods' => [
+        'title' => 'Neighborhoods',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'general' => 'General',
+    'map' => 'Map',
+    'setting' => 'Setting',
 ];
