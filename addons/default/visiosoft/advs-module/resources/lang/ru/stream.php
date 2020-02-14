823017992202a25d7b214e048a0a16dfbe155cb5
Fatih Alp <mail@fatihalp.com>
2020-02-14 15:06:28 +0300

    New translations stream.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/stream.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/stream.php
@@ -1,29 +1,22 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "name"      =>"Advs",
-
-   ],
-   "categories"   =>   [
-      "name"      =>"категории",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Деревня",
-
-   ],
-   "custom_fields"   =>   [
-      "name"      =>"Настраиваемые поля",
-
-   ],
-   "custom_field_advs"   =>   [
-      "name"      =>"Настраиваемые поля",
-
-   ],
-   "cf_values"   =>   [
-      "name"      =>"Значения ср",
-
-   ],
-
+return [
+    'advs' => [
+        'name' => 'Advs',
+    ],
+    'categories' => [
+        'name' => 'Categories',
+    ],
+    'village' => [
+        'name' => 'Village',
+    ],
+    'custom_fields' => [
+        'name' => 'Custom fields',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Custom field advs',
+    ],
+    'cf_values' => [
+        'name' => 'Cf values',
+    ],
 ];
