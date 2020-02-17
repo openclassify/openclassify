e0679d19fe978197632a0e004ccd5d7201811c47
vedatakd <vedat@vebze.com>
2020-02-17 17:52:52 +0300

    #895 Login page

--- addons/default/visiosoft/base-theme/resources/lang/tr/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/tr/field.php
@@ -20,7 +20,7 @@ return [
     'not_a_member_yet' => 'Henüz Üye Değil Misiniz',
     'not_a_member_yet_message' => 'Üyelerimize özel hizmetlerimizden faydalanabilmek için üye olun',
     'forgot_password' => 'Şifremi Unuttum',
-    'email_or_phone_number' => 'E-Posta Adresiniz veya Telefon Numaranız',
+    'email_or_phone_number' => 'E-Posta veya Telefon Numarası',
     'phone_number' => 'Telefon Numarası',
 
     // Registration instructions
