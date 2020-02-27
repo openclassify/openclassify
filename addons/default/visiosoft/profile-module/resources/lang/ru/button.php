26f13c2d84aa8ccf7727c8622b6504f95cda83af
Fatih Alp <mail@fatihalp.com>
2020-02-27 17:59:26 +0300

    New translations button.php (Russian)

--- addons/default/visiosoft/profile-module/resources/lang/ru/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/ru/button.php
@@ -8,4 +8,6 @@ return [
     'update_profile' => 'Update Profile',
     'delete' => 'Delete',
     'edit' => 'Edit',
+    'go_profile' => 'Go to Profile Detail',
+    'go_user' => 'Go to User Detail',
 ];
