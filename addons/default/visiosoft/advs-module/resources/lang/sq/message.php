2d137db02185b37ab7a0167000b8365030248bf6
Fatih Alp <mail@fatihalp.com>
2020-02-28 11:17:49 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/message.php
@@ -26,4 +26,5 @@ return [
     'delete_author_error' => 'Ju nuk mund të fshini një postim që nuk ju përket juve',
     'success_delete' => 'Shpallja është fshirë',
     'pending_ad_status' => 'Your ad is pending approval by the Editor.',
+    'extended' => ':number ad(s) has been extended successfully.',
 ];
