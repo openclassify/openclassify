d2e8c0b32d54363b2e0de2aa3cbe55c7b2698b09
Diatrex <diahshalabi@hotmail.com>
2020-03-02 17:13:48 +0300

    #718 Netgsm error when posting ad

--- addons/default/visiosoft/profile-module/src/Profile/ProfileModel.php
+++ addons/default/visiosoft/profile-module/src/Profile/ProfileModel.php
@@ -5,12 +5,10 @@ use Anomaly\Streams\Platform\Model\Profile\ProfileProfileEntryModel;
 
 class ProfileModel extends ProfileProfileEntryModel implements ProfileInterface
 {
-
     public function getProfile($id = null)
     {
-        if($id != null)
-        {
-            return $this->query()->where('user_id',$id);
+        if($id != null) {
+            return $this->query()->where('user_id', $id);
         }
         return $this->query();
     }
