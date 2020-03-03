45ddd98af81dd3604cfa8fc167a1a55db0652bc8
Fatih Alp <mail@fatihalp.com>
2020-03-03 12:42:04 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/base-theme/resources/lang/tr/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/tr/setting.php
@@ -28,6 +28,9 @@ return [
     'latest_and_view_all_btn' => [
         'name' => 'Tümünü Görüntüle',
     ],
+    'register_page_instruction_logo' => [
+        'name' => 'Kayıt Sayfası Talimat Logosu',
+    ],
     'style' => [
         'name' => 'Style(CSS)',
     ],
