de00ceca1f5305729458fabcd3814455f09585ee
Fatih Alp <mail@fatihalp.com>
2020-03-03 12:43:05 +0300

    New translations message.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/message.php
@@ -1,7 +1,31 @@
 <?php
 
- return[
-   "error_added_cart"   =>"Запрашиваемый продукт в настоящее время отсутствует или неактивен",
-   "please_buy_package"   =>"Пожалуйста,
-   купите пакет"
+return [
+    'error_added_cart' => 'Запрашиваемый продукт в настоящее время отсутствует или неактивен',
+    'please_buy_package' => 'Пожалуйста,
+   купите пакет',
+    'please_payment' => 'Please Pay',
+    'bank_account_info' => 'Bank account information',
+    'agreement' => 'By clicking Post, you agree to our Terms of Use and Privacy Policy acknowledge that you are the rightful owner of this item.',
+    'update_category_msg' => 'The classified category will be updated.',
+    'updated_category_msg' => 'Category successfully edited.',
+    'max_ad_limit' => [
+        'title' => "Your Free Ad Limit is Out.",
+        'msg' => "You have reached your free ad limit.",
+        'nextBtn' => "Show Ads",
+        'cancelBtn' => "or Cancel",
+    ],
+    'create_ad_with_post_cat' => [
+        'title' => "Post an Ad in just 30 seconds",
+        'msg' => "Please DO NOT post multiple ads for the same items or service. All duplicate, spam and wrongly categorized ads will be deleted.",
+        'nextBtn' => "Next",
+        'cancelBtn' => "or Cancel",
+        'link' => "",
+    ],
+    'update_category_info' => 'Please update category.',
+    'filter_by_search_msg' => 'Filter By Search.',
+    'delete_author_error' => 'You cannot delete a post that does not belong to you',
+    'success_delete' => 'Ad Deleted',
+    'pending_ad_status' => 'Your ad is pending approval by the Editor.',
+    'extended' => ':number ad(s) has been extended successfully.',
 ];
