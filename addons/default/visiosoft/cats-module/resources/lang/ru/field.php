5797699201462b27c7ef0cf74b5dac51cac9f847
Fatih Alp <mail@fatihalp.com>
2020-03-03 12:42:47 +0300

    New translations field.php (Russian)

--- addons/default/visiosoft/cats-module/resources/lang/ru/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ru/field.php
@@ -1,22 +1,29 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"заглавие",
+return [
+    'name' => [
+        'name' => 'заглавие',
+    ],
+    'order' => [
+        'name' => 'порядок',
+    ],
+    'slug' => [
+        'name' => 'слизень'
+    ],
+    'files' => [
+        'name' => 'Образ'
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword'
+    ],
+    'seo_description' => [
+        'name' => 'Seo Description'
+    ],
+    'icon' => [
+        'name' => 'Icon'
+    ],
 
-   ],
-   "order"   =>   [
-      "name"      =>"порядок",
-
-   ],
-   "slug"   =>   [
-      "name"      =>"слизень",
-
-   ],
-   "files"   =>   [
-      "name"      =>"Образ",
-
-   ],
-   "please_wait"   =>"Пожалуйста,
-   подождите. Удаление подкатегорий"
+    'please_wait' => 'Пожалуйста,
+   подождите. Удаление подкатегорий',
+    'category_selection' => 'Category Selection',
 ];
