5f0f8c209c009d21bd422978326c392e3c619977
Fatih Alp <mail@fatihalp.com>
2020-03-03 12:42:55 +0300

    New translations button.php (Russian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ru/button.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'post_ad' => [
+        'name' => 'Post Ad',
+    ],
+    'login' => 'Login',
+    'register' => 'Register',
+    'continue' => 'Continue',
+    'reset_password' => 'Reset Password',
+];
