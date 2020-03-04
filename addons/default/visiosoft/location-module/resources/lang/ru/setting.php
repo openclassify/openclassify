81f37c9796f1f5c1fa874e906cc2bdc0fa208ae6
Fatih Alp <mail@fatihalp.com>
2020-03-04 18:36:37 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/location-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/ru/setting.php
@@ -1,5 +1,43 @@
 <?php
 
- return[
+return [
+    'home_page_location' => [
+        'name' => 'Домашняя страница Расположение Поле',
+    ],
+    'list_page_location' => [
+        'name' => 'Поле расположения страницы списка',
+    ],
+    'detail_page_location' => [
+        'name' => 'Подробное поле расположения страницы',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'Показать кнопку поиска местоположения',
+    ],
+    'default_country' => [
+        'name' => 'Страна по умолчанию',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'Создать страницу с рекламой',
+    ],
+    'default_city' => [
+        'name' => 'Город по умолчанию',
+    ],
+    'default_district' => [
+        'name' => 'Район по умолчанию',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Окрестности по умолчанию',
+    ],
 
+    'google_map_key' => [
+        'name' => 'Google Maps Api Key',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'Длинный по умолчанию',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'Lat по умолчанию',
+    ],
 ];
