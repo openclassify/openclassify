f1f4ac4a0e182961e91870919d247c4b778ea125
Fatih Alp <mail@fatihalp.com>
2020-03-04 18:46:05 +0300

    New translations field.php (Arabic)

--- addons/default/visiosoft/base-theme/resources/lang/ar/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/ar/field.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'latest_ads' =>  'أحدث الإعلانات',
-    'show_all' =>  'عرض الكل',
+    'latest_ads' => 'أحدث الإعلانات',
+    'show_all' => 'عرض الكل',
     'login' => [
         'name' => 'تسجيل الدخول',
     ],
@@ -11,8 +11,23 @@ return [
     ],
     'profile' => 'الملف الشخصي',
     'email' => 'عنوان البريد الإلكتروني',
+    'reset_code' => 'إعادة تعيين الرمز',
+    'password_confirmation' => 'تأكيد كلمة المرور',
     'password' => 'كلمه السر',
-    'display_name' => 'اسم العرض',
-    'username' => 'اسم المستخدم',
+    'first_name' => 'الاسم الاول',
+    'last_name' => 'الكنية',
     'remember_me' => 'تذكرنى',
+    'not_a_member_yet' => 'لست عضوا حتى الآن',
+    'not_a_member_yet_message' => 'الاشتراك لأعضائنا للاستفادة من خدماتنا الخاصة',
+    'forgot_password' => 'لقد نسيت كلمة المرور',
+    'email_or_phone_number' => 'عنوان البريد الإلكتروني أو رقم الهاتف',
+    'phone_number' => 'رقم الهاتف',
+
+    // Registration instructions
+    'personal_registration_header' => 'ما هي مزايا العضوية الفردية؟',
+    'personal_registration_body' => 'بيع واستأجر منزلك أو سيارتك أو بيع أغراضك غير المستخدمة واحصل على أشياء جديدة.',
+    'personal_registration_list_1' => 'نشر إعلان مجاني ،',
+    'personal_registration_list_2' => 'أضف الإعلانات التي تهتم بها ، واتبع تغيرات الأسعار بعد إضافتها إلى مفضلاتك ، وإنشاء عمليات بحث مفضلة تلبي معاييرك ،',
+    'personal_registration_list_3' => 'أرسل رسائل إلى مالكي الإعلانات على الموقع.',
+    'register_information_note' => 'تؤخذ المعلومات على هذه الصفحة ل ' . env('APPLICATION_DOMAIN') . ' عضوية. يمكنك العثور على معلومات مفصلة حول حماية البيانات الشخصية هنا.',
 ];
