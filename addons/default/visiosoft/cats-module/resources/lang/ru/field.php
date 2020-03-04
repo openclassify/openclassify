e212aa22c03d0a17b8c6de957cb91f4263133a7a
Fatih Alp <mail@fatihalp.com>
2020-03-04 14:02:35 +0300

    New translations field.php (Russian)

--- addons/default/visiosoft/cats-module/resources/lang/ru/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ru/field.php
@@ -3,24 +3,31 @@
 return [
     'name' => [
         'name' => 'заглавие',
+        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
     ],
     'order' => [
         'name' => 'порядок',
     ],
+
     'slug' => [
-        'name' => 'слизень'
+        'name' => 'слизень',
+        'instructions' => 'Specify any organizational tags to help group your post with others.',
+
     ],
     'files' => [
-        'name' => 'Образ'
+        'name' => 'Образ',
     ],
     'seo_keyword' => [
-        'name' => 'Seo Keyword'
+        'name' => 'Seo Keyword',
+        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
     ],
     'seo_description' => [
-        'name' => 'Seo Description'
+        'name' => 'Seo Description',
+        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
     ],
     'icon' => [
-        'name' => 'Icon'
+        'name' => 'Icon',
+        'instructions' => 'It is used to add icons indicating the category type.',
     ],
 
     'please_wait' => 'Пожалуйста,
