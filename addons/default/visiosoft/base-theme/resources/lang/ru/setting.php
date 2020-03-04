8380bb3ed598358e9e21020af9f3fd2d367e78a7
Fatih Alp <mail@fatihalp.com>
2020-03-04 18:36:29 +0300

    New translations setting.php (Russian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ru/setting.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Название навигации',
+    ],
+    'navigation_action' => [
+        'name' => 'Навигация Действие',
+    ],
+    'country_fields' => [
+        'name' => 'Поля страны',
+    ],
+    'date_fields' => [
+        'name' => 'Поля даты',
+    ],
+    'price_fields' => [
+        'name' => 'Поля цен',
+    ],
+    'breadcrumbs' => [
+        'name' => 'хлебный мякиш',
+    ],
+    'ad_details' => [
+        'name' => 'Детали объявления',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Вкладка "Детали объявления"',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Последние и Просмотреть все Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'Логотип Инструкции на странице регистрации',
+    ],
+    'style' => [
+        'name' => 'Style (CSS)',
+    ],
+];
