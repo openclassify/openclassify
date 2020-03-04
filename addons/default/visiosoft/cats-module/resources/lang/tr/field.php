cd113015714f13585b3511197e966f65290cb573
Fatih Alp <mail@fatihalp.com>
2020-03-04 14:02:29 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/cats-module/resources/lang/tr/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/tr/field.php
@@ -3,24 +3,31 @@
 return [
     'name' => [
         'name' => 'Başlık',
+        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
     ],
     'order' => [
         'name' => 'Sipariş',
     ],
+
     'slug' => [
-        'name' => 'Özel isim'
+        'name' => 'Özel isim',
+        'instructions' => 'Specify any organizational tags to help group your post with others.',
+
     ],
     'files' => [
-        'name' => 'Resim'
+        'name' => 'Resim',
     ],
     'seo_keyword' => [
-        'name' => 'Seo Anahtar Kelimeler'
+        'name' => 'Seo Anahtar Kelimeler',
+        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
     ],
     'seo_description' => [
-        'name' => 'Seo Açıklaması'
+        'name' => 'Seo Açıklaması',
+        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
     ],
     'icon' => [
-        'name' => 'İkon'
+        'name' => 'İkon',
+        'instructions' => 'It is used to add icons indicating the category type.',
     ],
 
     'please_wait' => 'Lütfen bekleyin. Alt Kategoriler Siliniyor',
