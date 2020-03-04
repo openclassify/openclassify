eb5cbe82e48890dca8dd1477993bdbfe71c780ad
Fatih Alp <mail@fatihalp.com>
2020-03-04 14:02:52 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/cats-module/resources/lang/sq/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/sq/field.php
@@ -3,24 +3,31 @@
 return [
     'name' => [
         'name' => 'titull',
+        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
     ],
     'order' => [
         'name' => 'urdhër',
     ],
+
     'slug' => [
-        'name' => 'goditje e fortë'
+        'name' => 'goditje e fortë',
+        'instructions' => 'Specify any organizational tags to help group your post with others.',
+
     ],
     'files' => [
-        'name' => 'imazh'
+        'name' => 'imazh',
     ],
     'seo_keyword' => [
-        'name' => 'Fjalë kyçe SEO'
+        'name' => 'Fjalë kyçe SEO',
+        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
     ],
     'seo_description' => [
-        'name' => 'Përshkrimi i Seos'
+        'name' => 'Përshkrimi i Seos',
+        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
     ],
     'icon' => [
-        'name' => 'ikonë'
+        'name' => 'ikonë',
+        'instructions' => 'It is used to add icons indicating the category type.',
     ],
 
     'please_wait' => 'Ju lutemi prisni. Zgjedhja e kategorive nën',
