f6ab539981733767c55aa0d2fcab58dad34e406e
Fatih Alp <mail@fatihalp.com>
2020-03-05 15:55:26 +0300

    New translations control_panel.php (Albanian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/sq/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/sq/control_panel.php
@@ -7,4 +7,7 @@ return [
     'view_site' => 'Shiko sitin',
     'title'     => 'Paneli i kontrollit',
     'search_placeholder'     => 'Tipi për të kërkuar',
+
+    // Users Module
+    'search_by_gsm_number'     => 'Search By GSM Number',
 ];
