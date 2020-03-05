21fb4854677252de3eeba46f35baa053af83f8a3
Diatrex <diahshalabi@hotmail.com>
2020-03-05 10:13:01 +0300

    #1057 Login with mobile phone is not working

--- addons/default/visiosoft/profile-module/src/Profile/Validation/ValidateCredentials.php
+++ addons/default/visiosoft/profile-module/src/Profile/Validation/ValidateCredentials.php
@@ -35,10 +35,8 @@ class ValidateCredentials
 
                 //Is email or phone number
                 if (!filter_var($credentials['email'], FILTER_VALIDATE_EMAIL)) {
-                    if ($profile = $this->profile->findPhoneNumber($credentials['email'])) {
-                        if ($user = $this->repository->find($profile->user_id)) {
-                            $credentials['email'] = $user->email;
-                        }
+                    if ($user = $this->repository->findBy('gsm_phone', $credentials['email'])) {
+                        $credentials['email'] = $user->email;
                     }
                 }
                 $response = $this->repository->findByCredentials($credentials);
