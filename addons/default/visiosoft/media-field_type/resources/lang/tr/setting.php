1c8c92630aa0753ccd730506f60bc8999bf427ef
Fatih Alp <mail@fatihalp.com>
2020-03-09 18:50:47 +0300

    New translations setting.php (Turkish)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/tr/setting.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'imageCanvasW' => [
+        'name' => 'Resim Kanvas Genişliği'
+    ],
+    'imageCanvasH' => [
+        'name' => 'Resim Tuval Yüksekliği'
+    ],
+    'imageResizeW' => [
+        'name' => 'Görüntü Yeniden Boyutlandırma Genişliği'
+    ],
+    'imageResizeH' => [
+        'name' => 'Görüntü Boyut Yüksekliği'
+    ],
+];
