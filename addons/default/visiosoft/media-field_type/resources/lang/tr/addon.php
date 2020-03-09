6b7f31e420a0192e586992805ea3d77649f884fc
Fatih Alp <mail@fatihalp.com>
2020-03-09 17:43:52 +0300

    New translations addon.php (Turkish)

--- addons/default/visiosoft/media-field_type/resources/lang/tr/addon.php
+++ addons/default/visiosoft/media-field_type/resources/lang/tr/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Dosyalar',
+    'title'       => 'Media Field Type',
     'name'        => 'Dosya Alanı Tipi',
     'description' => 'Birden fazla dosya yükleme alanı türü.',
 ];
