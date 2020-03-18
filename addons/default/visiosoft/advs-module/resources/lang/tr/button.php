72bfa2d189391f9caae8132e601d4f845093b213
Fatih Alp <mail@fatihalp.com>
2020-03-18 10:27:13 +0300

    New translations button.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/button.php
@@ -8,10 +8,12 @@ return [
     'new_district' => 'Yeni bölge',
     'new_neighborhood' => 'Yeni mahalle',
     'new_village' => 'Yeni köy',
+    'edit' => "Düzenle",
     'approve' => "Onayla",
     'decline' => 'Reddet',
     'extend' => 'Uzat',
     'extend_all' => 'Tümünün Tarihini Güncelle',
+    'categories' => 'Kategoriler',
     'sub_category' => 'Alt Kategoriler',
     'add_sub_category' => 'Alt Kategori Ekle',
     'sub_cities' => 'Alt Şehirler',
@@ -23,6 +25,7 @@ return [
     'sub_neighborhoods' => 'Alt Mahalleler',
     'sub_village' => 'Alt Köy',
     'general_settings' => 'Genel Ayarlar',
+    'save_category' => 'Kategoriyi Kaydet',
     'update_category' => [
         'name' => 'Kategori Güncelle'
     ],
