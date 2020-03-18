efe3bd8d88704245b645161f7f6496c81cecd988
Fatih Alp <mail@fatihalp.com>
2020-03-18 19:26:29 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
@@ -4,6 +4,10 @@ return [
     'auto_approve' => [
         'name' => 'Miratimi automatik',
     ],
+    'estimated_pending_time' => [
+        'name' => 'Estimated Pending Time',
+        'instructions' => 'In Hours',
+    ],
     'default_published_time' => [
         'name' => 'Koha e publikimit të paracaktuar',
     ],
