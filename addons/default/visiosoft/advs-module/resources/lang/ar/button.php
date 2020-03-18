d5793f6afccaabf4e86596250d76a53097f22653
Fatih Alp <mail@fatihalp.com>
2020-03-18 10:28:23 +0300

    New translations button.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/button.php
@@ -8,10 +8,12 @@ return [
     'new_district' => 'حي جديد',
     'new_neighborhood' => 'حي جديد',
     'new_village' => 'قرية جديدة',
+    'edit' => "تعديل",
     'approve' => "يوافق",
     'decline' => 'انخفاض',
     'extend' => 'تمديد',
     'extend_all' => 'تمديد الكل',
+    'categories' => 'التصنيفات',
     'sub_category' => 'الفئات الفرعية',
     'add_sub_category' => 'إضافة فئة فرعية',
     'sub_cities' => 'مدن فرعية',
@@ -23,6 +25,7 @@ return [
     'sub_neighborhoods' => 'الأحياء الفرعية',
     'sub_village' => 'قرية فرعية',
     'general_settings' => 'الاعدادات العامة',
+    'save_category' => 'حفظ الفئة',
     'update_category' => [
         'name' => 'تحديث الفئة'
     ],
