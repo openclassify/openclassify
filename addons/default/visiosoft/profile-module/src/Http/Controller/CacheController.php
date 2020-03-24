9efeee005df3a924ed420e50bfcecb932357d6da
Diatrex <diahshalabi@hotmail.com>
2020-03-24 11:40:09 +0300

    #1011 Ad post preview page #442

--- addons/default/visiosoft/profile-module/src/Http/Controller/CacheController.php
+++ addons/default/visiosoft/profile-module/src/Http/Controller/CacheController.php
@@ -1,14 +1,18 @@
 <?php namespace Visiosoft\ProfileModule\Http\Controller;
 
 use Anomaly\Streams\Platform\Http\Controller\PublicController;
+use Visiosoft\AddblockExtension\Command\addBlock;
 
 class CacheController extends PublicController
 {
     public function getUserInfo()
     {
         $user = auth()->user();
-        $response = $user ? $user->first_name . ' ' . $user->last_name : $user;
+        $user = $user ? $user->first_name . ' ' . $user->last_name : $user;
 
-        return ['userName' => $response];
+        $getAddBlockHtml = new addBlock('navigation/dropdown', []);
+        $addBlockHtml = $getAddBlockHtml->handle();
+
+        return ['userName' => $user, 'addBlockHtml' => $addBlockHtml];
     }
 }
