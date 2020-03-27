7e993bb45414fb8181f2551c3aab0478a7de15da
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:18:06 +0300

    New translations field.php (German)

---
+++ addons/default/visiosoft/base-theme/resources/lang/de/field.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'latest_ads' => 'Neueste Anzeigen',
+    'show_all' => 'Zeige alles',
+    'login' => [
+        'name' => 'Einloggen',
+    ],
+    'logout' => [
+        'name' => 'Ausloggen',
+    ],
+    'profile' => 'Profil',
+    'email' => 'E-Mail-Addresse',
+    'reset_code' => 'Code zurücksetzen',
+    'password_confirmation' => 'Passwort Bestätigung',
+    'password' => 'Passwort',
+    'first_name' => 'Vorname',
+    'last_name' => 'Nachname',
+    'remember_me' => 'Behalte mich in Erinnerung',
+    'not_a_member_yet' => 'Noch kein Mitglied',
+    'not_a_member_yet_message' => 'Melden Sie sich für unsere Mitglieder an, um von unserem besonderen Service zu profitieren',
+    'forgot_password' => 'Ich habe mein Passwort vergessen',
+    'email_or_phone_number' => 'E-Mail-Adresse oder Telefonnummer',
+    'phone_number' => 'Telefonnummer',
+
+    // Registration instructions
+    'personal_registration_header' => 'Was sind individuelle Mitgliedschaftsvorteile?',
+    'personal_registration_body' => 'Verkaufen und vermieten Sie Ihr Haus, Auto, verkaufen Sie Ihre unbenutzten Gegenstände, holen Sie sich neue.',
+    'personal_registration_list_1' => 'Veröffentlichen Sie eine kostenlose Anzeige,',
+    'personal_registration_list_2' => 'Fügen Sie die Anzeigen hinzu, an denen Sie interessiert sind, folgen Sie den Preisänderungen, nachdem Sie sie zu Ihren Favoriten hinzugefügt haben, und erstellen Sie Lieblingssuchen, die Ihren Kriterien entsprechen.',
+    'personal_registration_list_3' => 'Senden Sie Nachrichten an die Anzeigeninhaber auf der Website.',
+    'register_information_note' => 'Die Informationen auf dieser Seite sind übernommen ' . env('APPLICATION_DOMAIN') . ' Mitgliedschaft. Detaillierte Informationen zum Schutz personenbezogener Daten finden Sie hier.',
+
+    // Register page
+    'phone_validation_error' => 'Diese Telefonnummer wird von einem anderen Mitglied verwendet.',
+
+    // Forgot Password
+    'create_new_password' => 'Neues Passwort erstellen',
+
+    // Login page
+    'or' => 'oder'
+];
