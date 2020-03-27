b91f163a8d67dadcc7ce11d43e6e0c684b437645
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:17:35 +0300

    New translations message.php (German)

--- addons/default/visiosoft/profile-module/resources/lang/de/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/de/message.php
@@ -1,23 +1,33 @@
 <?php
 
- return[
-   "email"   =>"E-Mail-Feld ist erforderlich!",
-   "username"   =>"Benutzername Feld ist erforderlich!",
-   "success_update"   =>"Profil-Update erfolgreich!",
-   "adress_success_update"   =>"Adress-Update erfolgreich!",
-   "adress_success_create"   =>"Adresse erfolgreich erstellen!",
-   "login_error"   =>"Falsches Login oder Passwort.",
-   "login_noMail_old_user"   =>"Die eingegebene E-Mail-Adresse wurde nicht gefunden.",
-   "login_noMail_old_user2"   =>"Bitte überprüfen Sie die E-Mail-Adresse und versuchen Sie es erneut.",
-   "login_noMail_old_user3"   =>"Wenn das Problem weiterhin besteht,
-   wenden Sie sich bitte an \'info@openclassify.com\'.",
-   "login_info_old_user"   =>"Ein neues Passwort wurde an Ihre E-Mail-Adresse gesendet.",
-   "success"   =>"Erfolg",
-   "notified_new_updates"   =>"Ich möchte über neue Updates informiert werden",
-   "notified_about_ads"   =>"Benachrichtigungen zu meinen Anzeigen zulassen",
-   "receive_messages_email"   =>"Ich möchte Nachrichten als E-Mail erhalten",
-   "no_packages_module"   =>"Kein Paketmodul!",
-   "required_add"   =>"Bitte füllen Sie alle erforderlichen Zeilen aus.",
-   "no_extend_package"   =>"Nein Anzeigenpakete erweitern",
-   "saved"   =>"Gerettet!"
+return [
+    "email"      => "E-Mail-Feld ist erforderlich!",
+    "username"      => "Benutzername Feld ist erforderlich!",
+    "success_update"      => "Profilaktualisierung erfolgreich!",
+    "adress_success_update"      => "Adressaktualisierung erfolgreich!",
+    "adress_success_create"      => "Adresse erfolgreich erstellen!",
+    "login_error" => "Falsches Login oder Passwort.",
+    "login_noMail_old_user" => "Die von Ihnen eingegebene E-Mail-Adresse wurde nicht gefunden.",
+    "login_noMail_old_user2" => "Bitte überprüfen Sie die E-Mail-Adresse und versuchen Sie es erneut.",
+    "login_noMail_old_user3" => "Wenn das Problem weiterhin besteht, wenden Sie sich bitte an 'info@openclassify.com'.",
+    "login_info_old_user" => "Ein neues Passwort wurde an Ihre E-Mail-Adresse gesendet.",
+    "success" => "Erfolg",
+    "notified_new_updates" => "Ich möchte über neue Updates informiert werden",
+    "notified_about_ads" => "Benachrichtigungen über meine Anzeigen zulassen",
+    "receive_messages_email" => "Ich möchte Nachrichten als E-Mail erhalten",
+    "no_packages_module" => "Kein Paketmodul!",
+    "required_add"=> "Bitte füllen Sie alle erforderlichen Zeilen aus.",
+    "no_extend_package" => "Keine Anzeigenpakete verlängern",
+    "saved" => "Gerettet!",
+    'please_confirm_transaction' => 'Bitte bestätigen Sie die Transaktion',
+    'password_do_not_match' => 'Passwörter stimmen nicht überein',
+    'your_password_changed' => 'Ihr Passwort wurde erfolgreich aktualisiert',
+    'error_valid_email_or_phone' => 'Das Format der Telefonnummer oder der E-Mail-Adresse ist nicht korrekt.',
+    'error_valid_phone' => 'Das Telefonnummernformat ist nicht korrekt.',
+    'registered_phone' => 'Diese Telefonnummer wurde bereits registriert.',
+    'ajax_address_error' => 'Keine Adresse oder nicht zum Anzeigen berechtigt.',
+    'empty_password_sms_message' => 'Aus Sicherheitsgründen haben wir Ihr Passwort geändert! Dein neues Passwort ist:',
+
+    // Forgot Password
+    'email_phone_not_found' => 'Die E-Mail, Telefonnummer ist nicht korrekt!'
 ];
