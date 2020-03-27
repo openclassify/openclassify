265eaf9d0d79ad3e94122298800d6f3d22a798a7
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:17:20 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/location-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/de/setting.php
@@ -1,5 +1,43 @@
 <?php
 
- return[
+return [
+    'home_page_location' => [
+        'name' => 'Feld für den Standort der Startseite',
+    ],
+    'list_page_location' => [
+        'name' => 'Feld "Seitenstandort auflisten"',
+    ],
+    'detail_page_location' => [
+        'name' => 'Feld für die Position der Detailseite',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'Schaltfläche "Suchort anzeigen"',
+    ],
+    'default_country' => [
+        'name' => 'Standardland',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'Position der Anzeigenseite erstellen',
+    ],
+    'default_city' => [
+        'name' => 'Standardstadt',
+    ],
+    'default_district' => [
+        'name' => 'Standardbezirk',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Standardnachbarschaft',
+    ],
 
+    'google_map_key' => [
+        'name' => 'Google Maps API-Schlüssel',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'Standard lang',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'Standard Lat',
+    ],
 ];
