c62324b9bfb2fc0f12c0b07d46fdab2b2144631a
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:17:11 +0300

    New translations addon.php (German)

--- addons/default/visiosoft/location-module/resources/lang/de/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/de/addon.php
@@ -1,7 +1,7 @@
 <?php
 
- return[
-   "title"   =>"Ort",
-   "name"   =>"Standortmodul",
-   "description"   =>"Beschreibung"
+return [
+    'title'       => 'Ort',
+    'name'        => 'Standortmodul',
+    'description' => 'Beschreibung',
 ];
