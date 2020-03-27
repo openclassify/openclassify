9a6ac46493b3c05968af7ded7d85033e97c5ba9d
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:17:39 +0300

    New translations setting.php (German)

---
+++ addons/default/visiosoft/profile-module/resources/lang/de/setting.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'show_my_ads' => [
+        'name' => 'Registerkarte "Meine Anzeigen anzeigen"',
+    ],
+    'upload_avatar' => [
+        'name' => 'Avatar hochladen',
+    ],
+];
