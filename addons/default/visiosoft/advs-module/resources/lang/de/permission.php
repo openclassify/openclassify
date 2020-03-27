c1d0039952d266aa77e50b9b8f55fadc6206f706
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:17:58 +0300

    New translations permission.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/permission.php
@@ -1,60 +1,52 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "name"      =>"Anzeigen",
-      "option"      =>      [
-         "read"         =>"Kann Anzeigen lesen?        ",
-         "write"         =>"Kann Anzeigen erstellen / bearbeiten?        ",
-         "delete"         =>"Kann Anzeigen löschen?"
-      ],
-
-   ],
-   "categories"   =>   [
-      "name"      =>"Kategorien",
-      "option"      =>      [
-         "read"         =>"Kann Kategorien lesen?        ",
-         "write"         =>"Kann Kategorien erstellen / bearbeiten?        ",
-         "delete"         =>"Kann Kategorien löschen?"
-      ],
-
-   ],
-   "village"   =>   [
-      "name"      =>"Dorf",
-      "option"      =>      [
-         "read"         =>"Kann Dorf lesen?        ",
-         "write"         =>"Kann ein Dorf erstellen / bearbeiten?        ",
-         "delete"         =>"Kann Dorf löschen?"
-      ],
-
-   ],
-   "custom_fields"   =>   [
-      "name"      =>"Benutzerdefinierte Felder",
-      "option"      =>      [
-         "read"         =>"Kann benutzerdefinierte Felder lesen?        ",
-         "write"         =>"Kann benutzerdefinierte Felder erstellen / bearbeiten?        ",
-         "delete"         =>"Kann benutzerdefinierte Felder löschen?"
-      ],
-
-   ],
-   "custom_field_advs"   =>   [
-      "name"      =>"Benutzerdefinierte Feldanzeigen",
-      "option"      =>      [
-         "read"         =>"Kann benutzerdefinierte Feldanzeigen lesen?        ",
-         "write"         =>"Kann benutzerdefinierte Feldanzeigen erstellen / bearbeiten?        ",
-         "delete"         =>"Kann benutzerdefinierte Feldanzeigen löschen?"
-      ],
-
-   ],
-   "cf_values"   =>   [
-      "name"      =>"Vgl. Werte",
-      "option"      =>      [
-         "read"         =>"Kann cf Werte lesen?        ",
-         "write"         =>"Kann cf-Werte erstellen / bearbeiten?        ",
-         "delete"         =>"Kann cf Werte löschen?",
-
-      ],
-
-   ],
-
+return [
+    'advs' => [
+        'name'   => 'Anzeigen',
+        'option' => [
+            'read'   => 'Kann Werbung lesen?',
+            'write'  => 'Können Anzeigen erstellt / bearbeitet werden?',
+            'delete' => 'Kann Werbung löschen?',
+        ],
+    ],
+    'categories' => [
+        'name'   => 'Kategorien',
+        'option' => [
+            'read'   => 'Kann Kategorien lesen?',
+            'write'  => 'Kann Kategorien erstellen / bearbeiten?',
+            'delete' => 'Kann Kategorien löschen?',
+        ],
+    ],
+    'village' => [
+        'name'   => 'Dorf',
+        'option' => [
+            'read'   => 'Kann Dorf lesen?',
+            'write'  => 'Kann Dorf erstellen / bearbeiten?',
+            'delete' => 'Kann Dorf löschen?',
+        ],
+    ],
+    'custom_fields' => [
+        'name'   => 'Benutzerdefinierte Felder',
+        'option' => [
+            'read'   => 'Kann benutzerdefinierte Felder lesen?',
+            'write'  => 'Können benutzerdefinierte Felder erstellt / bearbeitet werden?',
+            'delete' => 'Kann benutzerdefinierte Felder löschen?',
+        ],
+    ],
+    'custom_field_advs' => [
+        'name'   => 'Benutzerdefinierte Feldanzeigen',
+        'option' => [
+            'read'   => 'Können benutzerdefinierte Feldanzeigen gelesen werden?',
+            'write'  => 'Können benutzerdefinierte Feldanzeigen erstellt / bearbeitet werden?',
+            'delete' => 'Können benutzerdefinierte Feldanzeigen gelöscht werden?',
+        ],
+    ],
+    'cf_values' => [
+        'name'   => 'Siehe Werte',
+        'option' => [
+            'read'   => 'Kann cf Werte lesen?',
+            'write'  => 'Kann cf Werte erstellen / bearbeiten?',
+            'delete' => 'Kann cf Werte löschen?',
+        ],
+    ],
 ];
