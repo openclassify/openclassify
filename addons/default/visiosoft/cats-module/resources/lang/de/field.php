ac0a8978279aed6ddf846341132714cc38f0ec27
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:18:14 +0300

    New translations field.php (German)

--- addons/default/visiosoft/cats-module/resources/lang/de/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/de/field.php
@@ -1,21 +1,35 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Titel",
+return [
+    'name' => [
+        'name' => 'Titel',
+        'instructions' => 'Dies ist der Name der Hauptkategorie, die Sie definieren werden. Dadurch kann der Benutzer das gesuchte Produkt leicht finden.',
+    ],
+    'order' => [
+        'name' => 'Bestellung',
+    ],
 
-   ],
-   "order"   =>   [
-      "name"      =>"Auftrag",
+    'slug' => [
+        'name' => 'Schnecke',
+        'instructions' => 'Geben Sie organisatorische Tags an, um Ihren Beitrag mit anderen zu gruppieren.',
 
-   ],
-   "slug"   =>   [
-      "name"      =>"Schnecke",
+    ],
+    'files' => [
+        'name' => 'Bild',
+    ],
+    'seo_keyword' => [
+        'name' => 'SEO-Schlüsselwort',
+        'instructions' => 'Der Website im Google-Browser hinzugefügte Wörter werden über den Suchergebnissen angezeigt.',
+    ],
+    'seo_description' => [
+        'name' => 'SEO Beschreibung',
+        'instructions' => 'Dies kann die Entscheidung Ihrer Besucher beeinflussen, ob sie auf den Inhalt in den Suchergebnissen klicken möchten.',
+    ],
+    'icon' => [
+        'name' => 'Symbol',
+        'instructions' => 'Es wird verwendet, um Symbole hinzuzufügen, die den Kategorietyp angeben.',
+    ],
 
-   ],
-   "files"   =>   [
-      "name"      =>"Bild",
-
-   ],
-   "please_wait"   =>"Bitte warten. Löschen von Unterkategorien"
+    'please_wait' => 'Bitte warten. Unterkategorien löschen',
+    'category_selection' => 'Kategorieauswahl',
 ];
