5b09826fe318c549973758e8104948668c6a0a9a
Fatih Alp <mail@fatihalp.com>
2020-03-27 17:17:08 +0300

    New translations message.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/message.php
@@ -1,6 +1,34 @@
 <?php
 
- return[
-   "error_added_cart"   =>"Angefordertes Produkt fehlt derzeit oder ist inaktiv",
-   "please_buy_package"   =>"Bitte Paket kaufen"
+return [
+    'error_added_cart' => 'Das angeforderte Produkt fehlt derzeit oder ist inaktiv',
+    'please_buy_package' => 'Bitte Paket kaufen',
+    'please_payment' => 'Bitte zahlen Sie',
+    'bank_account_info' => 'Bankkonto Information',
+    'agreement' => 'Durch Klicken auf "Posten" erklären Sie sich mit unseren Nutzungsbedingungen einverstanden und bestätigen, dass Sie der rechtmäßige Eigentümer dieses Artikels sind.',
+    'update_category_msg' => 'Die klassifizierte Kategorie wird aktualisiert.',
+    'updated_category_msg' => 'Kategorie erfolgreich bearbeitet.',
+    'max_ad_limit' => [
+        'title' => "Ihr kostenloses Anzeigenlimit ist abgelaufen.",
+        'msg' => "Sie haben Ihr kostenloses Anzeigenlimit erreicht.",
+        'nextBtn' => "Anzeigen anzeigen",
+        'cancelBtn' => "Oder Abbrechen",
+    ],
+    'create_ad_with_post_cat' => [
+        'title' => "Schalten Sie eine Anzeige in nur 30 Sekunden",
+        'msg' => "Bitte schalten Sie NICHT mehrere Anzeigen für dieselben Artikel oder Dienstleistungen. Alle doppelten, Spam- und falsch kategorisierten Anzeigen werden gelöscht.",
+        'nextBtn' => "Nächster",
+        'cancelBtn' => "Oder Abbrechen",
+        'link' => "",
+    ],
+    'update_category_info' => 'Bitte aktualisieren Sie die Kategorie.',
+    'filter_by_search_msg' => 'Nach Suche filtern.',
+    'delete_author_error' => 'Sie können keinen Beitrag löschen, der Ihnen nicht gehört',
+    'success_delete' => 'Anzeige gelöscht',
+    'pending_ad_status' => 'Ihre Anzeige muss noch vom Editor genehmigt werden.',
+    'extended' => ': Nummer Anzeige (n) wurde erfolgreich erweitert.',
+    'category_not_exist' => 'Diese Kategorie existiert nicht mehr!',
+    'currency_converted' => 'Währung erfolgreich umgerechnet.',
+    'choose_user' => 'Wählen Sie einen Benutzer',
+    'owner_updated_successfully' => 'Der Besitzer wurde erfolgreich aktualisiert!',
 ];
