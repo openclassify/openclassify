3cbc47f20d041fa26977827a9e98030b6b6567b0
Fatih Alp <mail@fatihalp.com>
2020-03-30 17:29:02 +0300

    New translations section.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/section.php
@@ -1,49 +1,43 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "title"      =>"Anzeigen",
-
-   ],
-   "categories"   =>   [
-      "title"      =>"Kategorien",
-
-   ],
-   "countries"   =>   [
-      "title"      =>"Länder",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Städte",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Bezirke",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"Nachbarschaften",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Dorf",
-
-   ],
-   "custom_fields"   =>   [
-      "title"      =>"Benutzerdefinierte Felder",
-
-   ],
-   "custom_field_advs"   =>   [
-      "title"      =>"Benutzerdefinierte Feldanzeigen",
-
-   ],
-   "cf_values"   =>   [
-      "title"      =>"Vgl. Werte",
-
-   ],
-   "fields"   =>   [
-      "title"      =>"Benutzerdefinierte Felder",
-
-   ],
-
+return [
+    'advs' => [
+        'title' => 'Anzeigen',
+    ],
+    'categories' => [
+        'title' => 'Kategorien',
+    ],
+    'countries' => [
+        'title' => 'Länder',
+    ],
+    'cities' => [
+        'title' => 'Städte',
+    ],
+    'districts' => [
+        'title' => 'Bezirke',
+    ],
+    'neighborhoods' => [
+        'title' => 'Nachbarschaften',
+    ],
+    'village' => [
+        'title' => 'Dorf',
+    ],
+    'custom_fields' => [
+        'title' => 'Benutzerdefinierte Felder',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Benutzerdefinierte Feldanzeigen',
+    ],
+    'cf_values' => [
+        'title' => 'Vgl. Werte',
+    ],
+    'fields' => [
+        'title' => 'Benutzerdefinierte Felder'
+    ],
+    'assets_clear' => [
+        'name' => 'Vermögenswerte löschen'
+    ],
+    'general' => 'Allgemeines',
+    'ads' => 'Anzeigen',
+    'user' => 'Benutzer',
 ];
