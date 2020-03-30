e1bed197eb68d1fec022724a104185a80356b3a3
Fatih Alp <mail@fatihalp.com>
2020-03-30 17:28:43 +0300

    New translations addon.php (German)

--- addons/default/visiosoft/profile-module/resources/lang/de/addon.php
+++ addons/default/visiosoft/profile-module/resources/lang/de/addon.php
@@ -1,7 +1,7 @@
 <?php
 
- return[
-   "title"   =>"Profil",
-   "name"   =>"Profilmodul",
-   "description"   =>""
+return [
+    'title'       => 'Profil',
+    'name'        => 'Profilmodul',
+    'description' => ''
 ];
