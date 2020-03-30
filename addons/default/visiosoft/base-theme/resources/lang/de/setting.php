b7f18404bb561cab97975bdc668c74fdf9e4e9b0
Fatih Alp <mail@fatihalp.com>
2020-03-30 17:29:11 +0300

    New translations setting.php (German)

---
+++ addons/default/visiosoft/base-theme/resources/lang/de/setting.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Navigationstitel',
+    ],
+    'navigation_action' => [
+        'name' => 'Navigationsaktion',
+    ],
+    'country_fields' => [
+        'name' => 'Länderfelder',
+    ],
+    'date_fields' => [
+        'name' => 'Datumsfelder',
+    ],
+    'price_fields' => [
+        'name' => 'Preisfelder',
+    ],
+    'breadcrumbs' => [
+        'name' => 'Brotkrume',
+    ],
+    'ad_details' => [
+        'name' => 'Anzeigendetails',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Registerkarte "Anzeigendetails"',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Neueste und Alle anzeigen Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'Registrieren Sie das Anweisungslogo für die Seite',
+    ],
+    'style' => [
+        'name' => 'Stil (CSS)',
+    ],
+];
