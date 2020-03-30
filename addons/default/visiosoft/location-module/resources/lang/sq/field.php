45ef326b0efb39d54e63583429f36f031fd5a08e
Fatih Alp <mail@fatihalp.com>
2020-03-30 14:37:40 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/location-module/resources/lang/sq/field.php
+++ addons/default/visiosoft/location-module/resources/lang/sq/field.php
@@ -11,10 +11,10 @@ return [
         'name' => 'Qytet'
     ],
     'country' => [
-        'name' => 'vend'
+        'name' => 'Vendodhja'
     ],
     'district' => [
-        'name' => 'rreth'
+        'name' => 'Rreth'
     ],
     'order' => [
         'name' => 'Renditja e renditjes'
