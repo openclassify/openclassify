afe58a92cd1c157fb7b3eb424dff953c60e90687
vedatakd <vedat@vebze.com>
2020-04-13 09:12:08 +0300

    update event

--- addons/default/visiosoft/advs-module/src/Adv/Table/Handler/Approve.php
+++ addons/default/visiosoft/advs-module/src/Adv/Table/Handler/Approve.php
@@ -2,6 +2,7 @@
 
 use Anomaly\SettingsModule\Setting\Contract\SettingRepositoryInterface;
 use Anomaly\Streams\Platform\Ui\Table\Component\Action\ActionHandler;
+use Visiosoft\AdvsModule\Adv\Event\ChangedStatusAd;
 use Visiosoft\AdvsModule\Adv\Table\AdvTableBuilder;
 
 
@@ -14,11 +15,14 @@ class Approve extends ActionHandler
         foreach ($selected as $id) {
             $defaultAdPublishTime = $settingRepository->value('visiosoft.module.advs::default_published_time');
 
-            $model->newQuery()->find($id)->update([
+            $ad = $model->newQuery()->find($id);
+            $ad->update([
                 'status' => 'approved',
                 'finish_at' => date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' + ' . $defaultAdPublishTime . ' day')),
                 'publish_at' => date('Y-m-d H:i:s')
             ]);
+            event(new ChangedStatusAd($ad));//Create Notify
+
         }
 
         if ($selected) {
