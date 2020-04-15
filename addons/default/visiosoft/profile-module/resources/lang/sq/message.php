2bc365ba4cdc565adb52489bcdfdbe98d0450a58
Fatih Alp <mail@fatihalp.com>
2020-04-15 09:41:45 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/profile-module/resources/lang/sq/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/sq/message.php
@@ -29,5 +29,7 @@ return [
     'empty_password_sms_message' => 'Për shkak të çështjeve të sigurisë, ne ndryshuam fjalëkalimin tuaj! Fjalëkalimi juaj i ri është:',
 
     // Forgot Password
-    'email_phone_not_found' => 'E-maili, numri i telefonit nuk është i saktë!'
+    'email_phone_not_found' => 'E-maili, numri i telefonit nuk është i saktë!',
+
+    'disable_account' => 'Do you want to close your account? This operation cannot be undone.',
 ];
