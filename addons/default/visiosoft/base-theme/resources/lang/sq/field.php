23488944265e0d8573cdee9edddf4224d2b198a1
Fatih Alp <mail@fatihalp.com>
2020-04-17 16:15:29 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/base-theme/resources/lang/sq/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/sq/field.php
@@ -1,42 +1,48 @@
 <?php
 
 return [
-    'latest_ads' => 'Reklamat më të fundit',
+    'latest_ads' => 'Njoftimet e Fundit',
     'show_all' => 'Trego të Gjithë',
     'login' => [
-        'name' => 'hyrje',
+        'name' => 'Hyrje',
     ],
     'logout' => [
-        'name' => 'Shkyç',
+        'name' => 'Dalje',
     ],
-    'profile' => 'Profile',
-    'email' => 'Adresa e postës elektronike',
-    'reset_code' => 'Rivendosni kodin',
-    'password_confirmation' => 'Konfirmimi i fjalëkalimit',
-    'password' => 'fjalëkalim',
+    'profile' => 'Profil',
+    'email' => 'Adresa e-mail',
+    'reset_code' => 'Rivendosni Kodin',
+    'password_confirmation' => 'Konfirmimi i Fjalëkalimit',
+    'password' => 'Fjalëkalimi',
     'first_name' => 'Emri',
     'last_name' => 'Mbiemri',
     'remember_me' => 'Më kujto',
-    'not_a_member_yet' => 'Jo Anëtar ende',
-    'not_a_member_yet_message' => 'Regjistrohuni që anëtarët tanë të përfitojnë nga shërbimi ynë special',
-    'forgot_password' => 'Kam harruar fjalëkalimin tim',
+    'not_a_member_yet' => 'I pa Regjistruar',
+    'not_a_member_yet_message' => 'Regjistrohu si anëtar dhe përfito nga shërbimi ynë special',
+    'forgot_password' => 'Keni harruar Fjalekalimin',
     'email_or_phone_number' => 'Adresa e emailit ose numri i telefonit',
-    'phone_number' => 'Numri i telefonit',
+    'phone_number' => 'Numri i Telefonit',
 
     // Registration instructions
     'personal_registration_header' => 'Cilat janë avantazhet e anëtarësisë individuale?',
-    'personal_registration_body' => 'Shes dhe marr me qira shtëpinë, makinën tënde, shes sendet e tua të papërdorura, merr të reja.',
+    'personal_registration_body' => 'Shit dhe merr me qera Shtëpinë, Makinën tënde, shit sendet e tua të përdorura, merr të reja.',
     'personal_registration_list_1' => 'Posto një reklamë falas,',
-    'personal_registration_list_2' => 'Shtoni reklamat që ju interesojnë, ndiqni ndryshimet e çmimeve pasi t\'i shtoni ato në të preferuarat tuaja, krijoni kërkime të preferuara që plotësojnë kriteret tuaja,',
-    'personal_registration_list_3' => 'Dërgoni mesazhe pronarëve të reklamave në faqe.',
-    'register_information_note' => 'Informacioni në këtë faqe merret për ' . env('APPLICATION_DOMAIN') . ' anëtarësimi. Mund të gjeni informacione të hollësishme në lidhje me mbrojtjen e të dhënave personale këtu.',
+    'personal_registration_list_2' => 'Shtoni njoftimet që ju interesojnë, ndiqni ndryshimet e çmimeve pasi t\'i shtoni ato në të preferuarat tuaja, krijoni kërkime të preferuara që plotësojnë kriteret tuaja,',
+    'personal_registration_list_3' => 'Dërgoni mesazhe pronarëve të njoftimet në faqe.',
+    'register_information_note' => 'Informacioni në këtë faqe merret për ' . env('APPLICATION_DOMAIN') . ' Anëtarësimi. Mund të gjeni informacione të hollësishme në lidhje me mbrojtjen e të dhënave personale këtu.',
 
     // Register page
     'phone_validation_error' => 'Ky numër telefoni është në përdorim nga një anëtar tjetër.',
 
     // Forgot Password
-    'create_new_password' => 'Krijoni fjalëkalimin e ri',
+    'create_new_password' => 'Krijoni Fjalëkalimin e ri',
 
     // Login page
-    'or' => 'ose'
+    'or' => 'ose',
+
+    // Side menu links
+    'company_directory' => 'Company directory',
+    'popular_ads' => 'Popular Ads',
+    'last_48_hours' => 'Last 48 Hours',
+    'secure_e-commerce_ads' => 'Secure e-Commerce Ads',
 ];
