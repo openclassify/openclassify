ec2c0591d20b82efb36737d2187714d331512d52
Fatih Alp <mail@fatihalp.com>
2020-04-17 16:16:22 +0300

    New translations field.php (German)

--- addons/default/visiosoft/base-theme/resources/lang/de/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/de/field.php
@@ -38,5 +38,11 @@ return [
     'create_new_password' => 'Neues Passwort erstellen',
 
     // Login page
-    'or' => 'oder'
+    'or' => 'oder',
+
+    // Side menu links
+    'company_directory' => 'Company directory',
+    'popular_ads' => 'Popular Ads',
+    'last_48_hours' => 'Last 48 Hours',
+    'secure_e-commerce_ads' => 'Secure e-Commerce Ads',
 ];
