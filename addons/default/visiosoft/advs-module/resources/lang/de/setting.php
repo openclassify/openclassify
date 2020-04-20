c6a36516c9bcad591f889509f72590d2f9ade292
Fatih Alp <mail@fatihalp.com>
2020-04-20 19:09:29 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/setting.php
@@ -4,6 +4,13 @@ return [
     'auto_approve' => [
         'name' => 'Automatisch genehmigen',
     ],
+    'default_view_type' => [
+        'name' => 'Default View Type',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Hide Price if Zero',
+        'instructions' => 'Hide the price if the ad price is 0',
+    ],
     'estimated_pending_time' => [
         'name' => 'Geschätzte ausstehende Zeit',
         'instructions' => 'In Stunden',
