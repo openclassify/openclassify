86a934d24d6969e8db4bbb0908e541094b5e4ee0
Fatih Alp <mail@fatihalp.com>
2020-04-24 02:16:46 +0300

    New translations section.php (Azerbaijani)

---
+++ addons/default/visiosoft/advs-module/resources/lang/az/section.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => 'Elanlar',
+    ],
+    'categories' => [
+        'title' => 'Kateqoriyalar',
+    ],
+    'countries' => [
+        'title' => 'Ölkələr',
+    ],
+    'cities' => [
+        'title' => 'Şəhərlər',
+    ],
+    'districts' => [
+        'title' => 'Rayonlar',
+    ],
+    'neighborhoods' => [
+        'title' => 'Qonşuluq',
+    ],
+    'village' => [
+        'title' => 'Kənd',
+    ],
+    'custom_fields' => [
+        'title' => 'Xüsusi sahələr',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Xüsusi sahə reklamları',
+    ],
+    'cf_values' => [
+        'title' => 'Cf dəyərləri',
+    ],
+    'fields' => [
+        'title' => 'Xüsusi sahələr'
+    ],
+    'assets_clear' => [
+        'name' => 'Aktivlər Təmiz'
+    ],
+    'general' => 'Ümumi',
+    'ads' => 'Elanlar',
+    'user' => 'İstifadəçi',
+];
