dd4d42160e7d54e44fffa4d270f3d7b1117ee328
Fatih Alp <mail@fatihalp.com>
2020-04-24 02:15:47 +0300

    New translations setting.php (Azerbaijani)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/az/setting.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Side Panel Hover',
+        'instructions' => 'Yan tərəfdəki paneli genişləndirin?',
+    ],
+    'navigation'    => [
+        'name'         => 'Naviqasiya',
+        'instructions' => '<em>standart</em> naviqasiya qaydasını göstərin.',
+        'warning'      => 'İlk əlçatan naviqasiya elementi <strong>ev</strong> sahə kimi istifadə olunur.',
+        'reorder'      => 'Sıralamaq üçün <strong>yan çubuğun</strong> də ilkin naviqasiya elementlərini sürükləyin və buraxın.',
+    ],
+];
