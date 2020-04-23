658410d2fcf93f46192eb4d2ce1b03bbc8e2d28b
Fatih Alp <mail@fatihalp.com>
2020-04-24 02:08:12 +0300

    New translations field.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/field.php
@@ -280,5 +280,5 @@ return [
     'views' => 'الآراء',
 
     // Detail page
-    "no_street_view" => "No street view image in this location"
+    "no_street_view" => "لا توجد صورة التجوّل الافتراضي في هذا الموقع"
 ];
