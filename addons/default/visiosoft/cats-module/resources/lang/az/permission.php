0078ca70be4a09ce74e074fea11448ac038be8c5
Fatih Alp <mail@fatihalp.com>
2020-04-24 02:16:31 +0300

    New translations permission.php (Azerbaijani)

---
+++ addons/default/visiosoft/cats-module/resources/lang/az/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'category' => [
+        'name'   => 'Kateqoriya',
+        'option' => [
+            'read'   => 'Kateqoriyanı oxuya bilərsiniz?',
+            'write'  => 'Kateqoriya yarada / redaktə edə bilərsiniz?',
+            'delete' => 'Kateqoriyanı silə bilərsiniz?',
+        ],
+    ],
+    'placeholderforsearch' => [
+        'name'   => 'Yerdəyişən axtarışı',
+        'option' => [
+            'read'   => 'Yerdəyişmə axtarışını oxuya bilərsiniz?',
+            'write'  => 'Yerdəyişmə axtarışını yarada / düzəldə bilərsiniz?',
+            'delete' => 'Yerdəyişmə axtarışını silə bilərsiniz?',
+        ],
+    ],
+];
