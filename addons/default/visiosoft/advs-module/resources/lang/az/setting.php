daaba7d81d133b85be602419a7ad14bf9c69168f
Fatih Alp <mail@fatihalp.com>
2020-04-24 02:16:57 +0300

    New translations setting.php (Azerbaijani)

---
+++ addons/default/visiosoft/advs-module/resources/lang/az/setting.php
@@ -0,0 +1,121 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'Avtomatik təsdiq',
+    ],
+    'default_view_type' => [
+        'name' => 'Defolt Görünüş Növü',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Sıfır varsa Qiyməti Gizlət',
+        'instructions' => 'Reklam qiyməti 0 olarsa qiyməti gizlət',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Təxmini gözlənilən vaxt',
+        'instructions' => 'Saatda',
+    ],
+    'default_published_time' => [
+        'name' => 'Defolt Vaxtı',
+    ],
+    'default_adv_limit' => [
+        'name' => 'Defolt Məhdudiyyətlər',
+    ],
+    'address' => [
+        'name' => 'Əlaqə ünvanı',
+    ],
+    'phone' => [
+        'name' => 'Əlaqə telefonu',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Xəritə Uzun Koordinat',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Xəritə Lat koordinatı',
+    ],
+    'iban_numbers' => [
+        'name' => 'İBAN',
+    ],
+    'logo' => [
+        'name' => 'Loqo',
+    ],
+    'ogImage' => [
+        'name' => 'Sosial paylaşma loqosu',
+        'warning'      => '1200 x 630 qətnamə ilə olmalıdır. ',
+
+    ],
+    'currencies' => [
+        'name' => 'Aktiv valyutalar',
+    ],
+    'latest-limit' => [
+        'name' => 'Ən son hədd',
+    ],
+    'site_address' => [
+        'name' => 'Sayt ünvanı',
+        'warning'      => 'Avtomatik olaraq www. www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Açarı',
+    ],
+
+    'default_country' => [
+        'name' => 'Defolt Elan Ölkəsi',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Valyuta çeviricisinin API açarı'
+    ],
+    'default_city' => [
+        'name' => 'Defolt Şəhər',
+    ],
+    'default_district' => [
+        'name' => 'Defolt Elan Rayonu',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Defolt Elan Qonşuluq',
+    ],
+    'default_currency' => [
+        'name' => 'Defolt Valyuta',
+    ],
+    'default_GET' => [
+        'name' => 'Defolt Elan GET',
+    ],
+    'twitter' => [
+        'name' => 'Twitter',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Su nişanının növü',
+    ],
+    'watermark_text' => [
+        'name' => 'Su nişanı Mətn',
+    ],
+    'watermark_image' => [
+        'name' => 'Su nişanı şəkli',
+    ],
+    'watermark_position' => [
+        'name' => 'Su nişanının mövqeyi',
+    ],
+    'listing_page_image' => [
+        'name' => 'Səhifənin görüntüləməsi',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Aktiv valyutalar',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google Statistik Kod',
+    ],
+    'register_email_field' => [
+        'name' => 'İstifadəçi Qeyd E-poçtu tələbi',
+    ],
+];
