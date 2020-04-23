3a03100b913cc0591982ff04dc741886b1578015
Fatih Alp <mail@fatihalp.com>
2020-04-24 02:15:43 +0300

    New translations field.php (Azerbaijani)

---
+++ addons/default/visiosoft/location-module/resources/lang/az/field.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Başlıq'
+    ],
+    'slug' => [
+        'name' => 'Slug'
+    ],
+    'city' => [
+        'name' => 'Şəhər'
+    ],
+    'country' => [
+        'name' => 'Ölkə'
+    ],
+    'district' => [
+        'name' => 'Rayon'
+    ],
+    'order' => [
+        'name' => 'Sırala Sırala'
+    ],
+    'neighborhood' => [
+        'name' => 'Qonşuluq'
+    ],
+    'village' => [
+        'name' => 'Kənd'
+    ],
+    'description' => [
+        'name' => 'Təsvir'
+    ],
+    'pick_option' => [
+        'name' => 'Bir seçim seçin',
+    ],
+    'address' => [
+        'name' => 'Ünvan',
+    ],
+];
