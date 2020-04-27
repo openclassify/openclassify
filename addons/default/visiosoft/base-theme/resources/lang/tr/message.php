11930bf47cf99c1703043410d29146abcab01448
Fatih Alp <mail@fatihalp.com>
2020-04-27 04:56:58 +0300

    New translations message.php (Turkish)

--- addons/default/visiosoft/base-theme/resources/lang/tr/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/tr/message.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'send_forgot_sms' => 'SMS sent to your registered phone. Please check.',
-    'found_phone' => 'The phone number registered in the system was not found.',
+    'send_forgot_sms' => 'Kayıtlı telefonunuza SMS gönderildi. Lütfen kontrol edin.',
+    'found_phone' => 'Sistemde kayıtlı olan telefon numarası bulunamadı.',
 ];
