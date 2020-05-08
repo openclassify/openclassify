3530a8eff008398bdfe9c4ecd14f84778cbc71ba
Fatih Alp <mail@fatihalp.com>
2020-05-09 01:25:36 +0300

    New translations button.php (Portuguese)

--- addons/default/visiosoft/location-module/resources/lang/pt/button.php
+++ addons/default/visiosoft/location-module/resources/lang/pt/button.php
@@ -1,17 +1,17 @@
 <?php
 
- return[
-   "new_country"   =>"Novo país",
-   "new_city"   =>"Nova cidade",
-   "new_district"   =>"Novo Distrito",
-   "new_neighborhood"   =>"Bairro Novo",
-   "new_village"   =>"Nova aldeia",
-   "sub_cities"   =>"Sub Cities",
-   "add_sub_cities"   =>"Adicionar Sub Cidade",
-   "add_sub_districts"   =>"Adicionar Sub Distrito",
-   "add_sub_neighborhoods"   =>"Adicionar Sub Bairro",
-   "add_sub_village"   =>"Adicionar Sub Village",
-   "sub_districts"   =>"Sub distritos",
-   "sub_neighborhoods"   =>"Sub-bairros",
-   "sub_village"   =>"Aldeia Sub"
+return [
+    'new_country' => 'New Country',
+    'new_city' => 'New City',
+    'new_district' => 'New District',
+    'new_neighborhood' => 'New Neighborhood',
+    'new_village' => 'New Village',
+    'sub_cities' => 'Sub Cities',
+    'add_sub_cities' => 'Add Sub City',
+    'add_sub_districts' => 'Add Sub District',
+    'add_sub_neighborhoods' => 'Add Sub Neighborhood',
+    'add_sub_village' => 'Add Sub Village',
+    'sub_districts' => 'Sub Districts',
+    'sub_neighborhoods' => 'Sub Neighborhoods',
+    'sub_village' => 'Sub Village',
 ];
