824b47c29dc8d232d9b7c4e8371b51ece2818c10
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:26:26 +0300

    New translations permission.php (French)

--- addons/default/visiosoft/profile-module/resources/lang/fr/permission.php
+++ addons/default/visiosoft/profile-module/resources/lang/fr/permission.php
@@ -1,24 +1,20 @@
 <?php
 
- return[
-   "profile"   =>   [
-      "name"      =>"Profil",
-      "option"      =>      [
-         "read"         =>"Peut lire le profil?        ",
-         "write"         =>"Peut créer / éditer un profil?        ",
-         "delete"         =>"Peut supprimer le profil?"
-      ],
-
-   ],
-   "adress"   =>   [
-      "name"      =>"Une robe",
-      "option"      =>      [
-         "read"         =>"Peut lire l\'adresse?        ",
-         "write"         =>"Peut créer / modifier l\'adresse?        ",
-         "delete"         =>"Peut effacer l\'adresse?",
-
-      ],
-
-   ],
-
+return [
+    'profile' => [
+        'name'   => 'Profil',
+        'option' => [
+            'read'   => 'Peut lire le profil?        ',
+            'write'  => 'Peut créer / éditer un profil?        ',
+            'delete' => 'Peut supprimer le profil?',
+        ],
+    ],
+    'adress' => [
+        'name'   => 'Une robe',
+        'option' => [
+            'read'   => 'Peut lire l\\'adresse?        ',
+            'write'  => 'Peut créer / modifier l\\'adresse?        ',
+            'delete' => 'Peut effacer l\\'adresse?',
+        ],
+    ],
 ];
