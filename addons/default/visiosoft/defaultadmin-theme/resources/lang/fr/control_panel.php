efd5c4e22ce428c35e6924e875d4721a7a0d3ccb
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:23:32 +0300

    New translations control_panel.php (French)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/control_panel.php
@@ -2,7 +2,12 @@
 
 return [
     'help'      => 'Aide',
+    'search'    => 'Search',
     'logout'    => 'Déconnexion',
     'view_site' => 'Voir le site',
-    'title'     => 'Panneau de contrôle'
+    'title'     => 'Panneau de contrôle',
+    'search_placeholder'     => 'Type to search',
+
+    // Users Module
+    'search_by_gsm_number'     => 'Search By GSM Number',
 ];
