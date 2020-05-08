c6447aea608a30bce02645e174a63d906e08446b
Fatih Alp <mail@fatihalp.com>
2020-05-09 01:25:54 +0300

    New translations button.php (Portuguese)

--- addons/default/visiosoft/cats-module/resources/lang/pt/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/pt/button.php
@@ -1,8 +1,8 @@
 <?php
 
- return[
-   "new_category"   =>"Nova categoria",
-   "add_sub_category"   =>"Adicionar subcategoria",
-   "sub_category"   =>"Mostrar subcategoria",
-   "new_placeholderforsearch"   =>"Novo espaço reservado para pesquisa"
+return [
+    'new_category' => 'New Category',
+    'add_sub_category' => 'Add Sub Category',
+    'sub_category' => 'Show Sub Category',
+    'new_placeholderforsearch' => 'New Placeholderforsearch',
 ];
