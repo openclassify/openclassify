07722fccc449003e8c84e706bfc2a33f38b9470d
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:29:28 +0300

    New translations section.php (Chinese Simplified)

---
+++ addons/default/visiosoft/profile-module/resources/lang/zh/section.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'Profile',
+    ],
+    'adress' => [
+        'title' => 'Adress',
+    ],
+    'general_setting' => 'General Settings',
+];
