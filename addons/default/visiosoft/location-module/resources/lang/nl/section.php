a04ae83310135d1921da52dc2518457b8302261b
Fatih Alp <mail@fatihalp.com>
2020-05-09 01:25:10 +0300

    New translations section.php (Dutch)

--- addons/default/visiosoft/location-module/resources/lang/nl/section.php
+++ addons/default/visiosoft/location-module/resources/lang/nl/section.php
@@ -1,25 +1,22 @@
 <?php
 
- return[
-   "countries"   =>   [
-      "title"      =>"landen",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Cities",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"districten",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"buurten",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Dorp",
-
-   ],
-
+return [
+    'countries' => [
+        'title' => 'Countries',
+    ],
+    'cities' => [
+        'title' => 'Cities',
+    ],
+    'districts' => [
+        'title' => 'Districts',
+    ],
+    'neighborhoods' => [
+        'title' => 'Neighborhoods',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'general' => 'General',
+    'map' => 'Map',
+    'setting' => 'Setting',
 ];
