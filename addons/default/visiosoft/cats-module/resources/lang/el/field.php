47de8edba40ec08d71d8074611137bcf841de292
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:25:40 +0300

    New translations field.php (Greek)

--- addons/default/visiosoft/cats-module/resources/lang/el/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/el/field.php
@@ -1,21 +1,35 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Τίτλος",
+return [
+    'name' => [
+        'name' => 'Title',
+        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
+    ],
+    'order' => [
+        'name' => 'Order',
+    ],
 
-   ],
-   "order"   =>   [
-      "name"      =>"Σειρά",
+    'slug' => [
+        'name' => 'Slug',
+        'instructions' => 'Specify any organizational tags to help group your post with others.',
 
-   ],
-   "slug"   =>   [
-      "name"      =>"Γυμνοσάλιαγκας",
+    ],
+    'files' => [
+        'name' => 'Image',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword',
+        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Description',
+        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
+    ],
+    'icon' => [
+        'name' => 'Icon',
+        'instructions' => 'It is used to add icons indicating the category type.',
+    ],
 
-   ],
-   "files"   =>   [
-      "name"      =>"Εικόνα",
-
-   ],
-   "please_wait"   =>"Παρακαλώ περιμένετε. Διαγραφή υπο-κατηγοριών"
+    'please_wait' => 'Please wait.Deleting Sub Categories',
+    'category_selection' => 'Category Selection',
 ];
