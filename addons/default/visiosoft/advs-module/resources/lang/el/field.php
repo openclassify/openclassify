82c8705ee2afdd3cdc941af903462c0397abc87e
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:25:44 +0300

    New translations field.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/field.php
@@ -1,265 +1,291 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Τίτλος",
+return [
+    'name' => [
+        'name' => 'Title'
+    ],
+    'order' => [
+        'name' => 'Order'
+    ],
+    'slug' => [
+        'name' => 'Slug'
+    ],
+    'parent_category' => [
+        'name' => 'Parent'
+    ],
+    'category' => [
+        'name' => 'Category Title'
+    ],
+    'price' => [
+        'name' => 'Price'
+    ],
+    'date' => [
+        'name' => 'Date'
+    ],
+    'photo' => [
+        'name' => 'Photo'
+    ],
+    'video' => [
+        'name' => 'Video'
+    ],
+    'map' => [
+        'name' => 'Map'
+    ],
+    'stock' => [
+        'name' => 'Stock'
+    ],
+    'advs_desc' => [
+        'name' => 'Description'
+    ],
+    'online_payment' => [
+        'name' => 'Online Payment'
+    ],
+    'city' => [
+        'name' => 'City'
+    ],
+    'country' => [
+        'name' => 'Country'
+    ],
+    'district' => [
+        'name' => 'District'
+    ],
+    'neighborhood' => [
+        'name' => 'Neighborhood'
+    ],
+    'village' => [
+        'name' => 'Village'
+    ],
+    'files' => [
+        'name' => 'Images'
+    ],
+    'cat1' => [
+        'name' => 'Main Category'
+    ],
+    'cat2' => [
+        'name' => 'Sub Category 1'
+    ],
+    'cat3' => [
+        'name' => 'Sub Category 2'
+    ],
+    'cat4' => [
+        'name' => 'Sub Category 3'
+    ],
+    'cat5' => [
+        'name' => 'Sub Category 4'
+    ],
+    'cat6' => [
+        'name' => 'Sub Category 5'
+    ],
+    'cat7' => [
+        'name' => 'Sub Category 6'
+    ],
+    'currency' => [
+        'name' => 'Currency'
+    ],
+    'status' => [
+        'name' => 'Status',
+        'option' => [
+            'approved' => 'Approved',
+            'expired' => 'Expired',
+            'unpublished' => 'Unpublished',
+            'pending_admin' => 'Pending Admin',
+            'pending_user' => 'Pending User'
+        ]
+    ],
+    'pending_adv' => [
+        'name' => 'Unpublished'
+    ],
+    'finish_at' => [
+        'name' => 'Finish At'
+    ],
+    'cover_photo' => [
+        'name' => 'Cover Photo'
+    ],
+    'favs_adv' => [
+        'name' => 'Fav Ads'
+    ],
+    'my_adv' => [
+        'name' => 'ADVS'
+    ],
+    'description' =>  'Description',
+    'explanation' =>  'Explanation',
+    'type' => [
+        'name' => 'Type'
+    ],
+    'parent_adv' => [
+        'name' => 'Related Ad'
+    ],
+    'value' => [
+        'name' => 'Value'
+    ],
+    'list' => 'List',
+    'home' => 'Home',
+    'list-page' => 'List Page',
+    'search' => 'Search',
+    'recommended' => 'Recommended Ads for You',
+    'filter' => 'Filter',
+    'sort' => 'Sort',
+    'sort_by' => 'Sort By',
+    'pick_option' => 'Pick an option',
+    'pick_ordering' => 'Ordering',
+    'price_high' => 'Price High to Low',
+    'price_low' => 'Price Low to High',
+    'newest' => 'Newest',
+    'oldest' => 'Oldest',
+    'address_a_z' => 'Address (A to Z)',
+    'address_z_a' => 'Address (Z to A)',
+    'categories' => 'Categories',
+    'all_categories' => 'All Categories',
+    'location' => 'Location',
+    'send_message' => 'Send Message',
+    'phone_gsm' => 'Gsm Phone',
+    'phone_office' => 'Office Phone',
+    'add_to_favorites' => "Add to Favorites",
+    'send' => 'Send',
+    'search_name' => 'Search Name',
+    'my_favorite_searches' => 'Favorite Searches',
+    'search_desc' => 'You can add and manage your favorite search selections here.',
+    'complaint_placeholder' => 'Type your complaint details here.',
+    'show_details' => 'Show Details',
+    'ad_actions' => 'Ad Actions',
+    'edit' => 'Edit',
+    'approve' => 'Approve',
+    'passive' => "Passive",
+    'offered_by' => 'Offered By',
+    'more_ads_by' => 'More Ads By',
+    'click_phone' => 'Show phone number',
+    'share' => 'Share',
+    'report' => 'Report this ad',
+    'features' => 'Features',
+    "streetview" => "Street View",
+    'recommended_ads' => 'Recommended Ads for You',
+    'new' => 'New',
+    'short_info' => 'Short Info',
+    'secure_trading' => 'Secure Trading',
+    '24/7' => '24/7 Support',
+    'easy_trading' => 'Easy Trading',
+    'need_help' => 'Need help?',
+    'give_a_call' => 'Give a call on',
+    'message_sent' => 'Message Sent!',
+    'asap' => 'Message sent, seller will contact you asap.',
+    'ad_id' => 'Ad Id',
+    'contact_with' => 'Contact With',
+    'create_adv' => 'Create Ad',
+    'edit_adv' => 'Edit Ad',
+    'mandatory_fields' => 'Mandatory Fields',
+    'ad_post' => 'Ad Post',
+    'sell_item' => 'Sell an item or service',
+    'search_save' => 'Favorite search saved successfully',
+    'address' => 'Address',
+    'email' => 'E-Mail',
+    'phone' => 'Phone',
+    'send_us_your_feedback' => 'Send Us Your Feedback',
+    'online_shopping' => 'GET',
+    'approved' => 'Approved Successfully',
+    'extended' => 'Extended Successfully',
+    'declined' => 'Declined Successfully',
+    'search_on_map' => 'Search on Map',
+    'post_free_ad' => 'Post Free Ad',
+    'select_a_category' => 'Select a Category',
+    'select_a_sub_category' => 'Select a Sub Category',
+    'all_dopings_added' => 'All doping types are added for this ad.',
+    'is_get_adv' => [
+        'name' => 'GET'
+    ],
+    'corporate_info' => 'Corporate Info',
+    'featured_ads' => "Featured Ads",
+    'trending_ads' => 'Trendind Ads',
+    'search_ads_result_message' => [
+        'name' => ':count ads found for :name search',
+    ],
+    'total' => 'Total',
+    'quantity' => 'Quantity',
+    'view_cart' => 'View Cart',
+    'no_ads' => 'No Ads found.',
+    'show_display_mode' => [
+        'map' => 'Map',
+        'list' => 'List',
+    ],
+    'advs_list_table_thead' => [
+        'name' => 'Ad Name',
+        'price' => 'Price',
+        'publish_at' => 'Ad Date',
+        'location_city' => 'City',
+        'location_country' => 'Country',
+    ],
+    'show_list_view' => [
+        'name' => 'List',
+    ],
+    'show_table_view' => [
+        'name' => 'Table',
+    ],
+    'showcase' => [
+        'name' => 'Showcase',
+    ],
+    'new_ad' => [
+        'name' => 'New Ad',
+    ],
+    'profile' => [
+        'name' => 'Profile',
+    ],
+    'my_ads' => [
+        'name' => 'My Ads',
+    ],
+    'sold_out' => [
+        'name' => 'Sold out',
+    ],
+    'edit_category' => 'Edit Category',
+    'set_category' => 'Set Category',
+    'max' => [
+        'name' => 'Max',
+    ],
+    'min' => [
+        'name' => 'Min',
+    ],
+    'in_the_last_24_hours' => [
+        'name' => 'In The Last 24 Hours'
+    ],
+    'in_the_last_week' => [
+        'name' => 'In The Last Week'
+    ],
+    'in_the_last_month' => [
+        'name' => 'In The Last Month'
+    ],
+    'ads_with_photo' => [
+        'name' => 'Ads With Photo'
+    ],
+    'ads_with_video' => [
+        'name' => 'Ads With Video'
+    ],
+    'yes' => [
+        'name' => 'Yes'
+    ],
+    "no_location" => "No location is selected.",
+    "continue" => 'Continue',
+    "gallery" => 'Gallery',
 
-   ],
-   "order"   =>   [
-      "name"      =>"Σειρά",
+    // Pending screen
+    'pending_message_1' => 'The Ad is in the Approval Process',
+    'pending_message_2' => "Ad's Update Date",
+    'pending_message_3' => 'Estimated Waiting Time',
+    'pending_message_4' => 'The time spent in the approval process will be added to the doping time.',
+    'pending_message_5' => 'No transactions can be made from the call center regarding the ad in the approval process.',
+    'hours' => 'hours',
 
-   ],
-   "slug"   =>   [
-      "name"      =>"Γυμνοσάλιαγκας",
+    // Preview page
+    'preview_text' => [
+        'part_1' => 'If the following information about your ad is correct, click the "Continue" button and go to the next step.',
+        'part_2' => 'If not, click the "Edit" button.',
+    ],
+    'views' => 'Views',
 
-   ],
-   "parent_category"   =>   [
-      "name"      =>"Μητρική εταιρεία",
+    // Detail page
+    "no_street_view" => "No street view image in this location",
+    'edit_my_contact_info' => 'Edit My Contact Information',
+    'update_my_contact_info' => 'update my contact info',
+    'contact_info' => 'Contact information',
+    'ad_info' => 'Ad Information',
 
-   ],
-   "category"   =>   [
-      "name"      =>"Τίτλος κατηγορίας",
-
-   ],
-   "price"   =>   [
-      "name"      =>"Τιμή",
-
-   ],
-   "stock"   =>   [
-      "name"      =>"Στοκ",
-
-   ],
-   "advs_desc"   =>   [
-      "name"      =>"Περιγραφή",
-
-   ],
-   "online_payment"   =>   [
-      "name"      =>"Διαδικτυακή πληρωμή",
-
-   ],
-   "city"   =>   [
-      "name"      =>"Πόλη",
-
-   ],
-   "country"   =>   [
-      "name"      =>"Χώρα",
-
-   ],
-   "district"   =>   [
-      "name"      =>"Περιοχή",
-
-   ],
-   "neighborhood"   =>   [
-      "name"      =>"Γειτονιά",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Χωριό",
-
-   ],
-   "files"   =>   [
-      "name"      =>"Εικόνες",
-
-   ],
-   "cat1"   =>   [
-      "name"      =>"Κύρια κατηγορία",
-
-   ],
-   "cat2"   =>   [
-      "name"      =>"Υποκατηγορία 1",
-
-   ],
-   "cat3"   =>   [
-      "name"      =>"Υποκατηγορία 2",
-
-   ],
-   "cat4"   =>   [
-      "name"      =>"Υποκατηγορία 3",
-
-   ],
-   "cat5"   =>   [
-      "name"      =>"Υποκατηγορία 4",
-
-   ],
-   "cat6"   =>   [
-      "name"      =>"Υποκατηγορία 5",
-
-   ],
-   "cat7"   =>   [
-      "name"      =>"Υποκατηγορία 6",
-
-   ],
-   "currency"   =>   [
-      "name"      =>"Νόμισμα",
-
-   ],
-   "status"   =>   [
-      "name"      =>"Κατάσταση",
-
-   ],
-   "next_add_advs_title"   =>   [
-      "name"      =>"Δημοσίευση μιας διαφήμισης σε μόλις 30 δευτερόλεπτα",
-
-   ],
-   "next_add_advs_msg"   =>   [
-      "name"      =>"Μην δημοσιεύετε πολλές διαφημίσεις για τα ίδια στοιχεία ή υπηρεσία. Όλες οι διπλές,
-      ανεπιθύμητες και εσφαλμένα κατηγοριοποιημένες διαφημίσεις θα διαγραφούν.",
-
-   ],
-   "next_add_advs_btn"   =>   [
-      "name"      =>"Επόμενο",
-
-   ],
-   "cancel_add_advs_btn"   =>   [
-      "name"      =>"ή Ακύρωση",
-
-   ],
-   "pending_adv"   =>   [
-      "name"      =>"Εν αναμονή διαφημίσεων",
-
-   ],
-   "archived_adv"   =>   [
-      "name"      =>"Αρχειοθετημένες διαφημίσεις",
-
-   ],
-   "favs_adv"   =>   [
-      "name"      =>"Fav Ads",
-
-   ],
-   "my_adv"   =>   [
-      "name"      =>"ADVS",
-
-   ],
-   "description"   =>   [
-      "name"      =>"Περιγραφή",
-
-   ],
-   "type"   =>   [
-      "name"      =>"Τύπος",
-
-   ],
-   "parent_adv"   =>   [
-      "name"      =>"Σχετική διαφήμιση",
-
-   ],
-   "value"   =>   [
-      "name"      =>"αξία",
-
-   ],
-   "home"   =>"Σπίτι",
-   "list-page"   =>"Λίστα σελίδας",
-   "search"   =>"Ψάξιμο",
-   "recommended"   =>"Προτεινόμενες διαφημίσεις για εσάς",
-   "sort_by"   =>"Ταξινόμηση κατά",
-   "pick_option"   =>"Επιλέξτε μια επιλογή",
-   "price_high"   =>"Τιμή υψηλή προς χαμηλή",
-   "price-low"   =>"Τιμή χαμηλή έως υψηλή",
-   "newest"   =>"Νεότερο",
-   "categories"   =>"Κατηγορίες",
-   "all_categories"   =>"Όλες οι κατηγορίες",
-   "location"   =>"Τοποθεσία",
-   "send_message"   =>"Να στείλετε μήνυμα",
-   "phone_gsm"   =>"Τηλέφωνο Gsm",
-   "phone_office"   =>"Τηλέφωνο γραφείου",
-   "add_to_favorites"   =>"Προσθήκη στα αγαπημένα",
-   "send"   =>"Στείλετε",
-   "search_name"   =>"Όνομα αναζήτησης",
-   "my_favorite_searches"   =>"Αγαπημένες αναζητήσεις",
-   "search_desc"   =>"Μπορείτε να προσθέσετε και να διαχειριστείτε τις αγαπημένες σας επιλογές αναζήτησης εδώ.",
-   "complaint_placeholder"   =>"Πληκτρολογήστε τα στοιχεία της καταγγελίας σας εδώ.",
-   "show_details"   =>"Δείξε λεπτομέρειες",
-   "edit"   =>"Επεξεργασία",
-   "approve"   =>"Εγκρίνω",
-   "passive"   =>"Παθητικός",
-   "offered_by"   =>"Προσφέρεται από",
-   "more_ads_by"   =>"Περισσότερες διαφημίσεις από",
-   "click_phone"   =>"Εμφάνιση αριθμού τηλεφώνου",
-   "share"   =>"Κοινή χρήση αυτής της διαφήμισης",
-   "report"   =>"Αναφορά αυτής της διαφήμισης",
-   "features"   =>"Χαρακτηριστικά",
-   "streetview"   =>"θέα στο δρόμο",
-   "recommended_ads"   =>"Προτεινόμενες διαφημίσεις για εσάς",
-   "new"   =>"Νέος",
-   "short_info"   =>"Σύντομες πληροφορίες",
-   "secure_trading"   =>"Ασφαλής εμπορία",
-   "24/7"   =>"Υποστήριξη 24/7",
-   "easy_trading"   =>"Εύκολη συναλλαγή",
-   "need_help"   =>"Χρειάζεστε βοήθεια?",
-   "give_a_call"   =>"Δώστε μια κλήση",
-   "message_sent"   =>"Το μήνυμα στάλθηκε!",
-   "asap"   =>"Μήνυμα αποστέλλεται,
-   ο πωλητής θα επικοινωνήσει μαζί σας σύντομα.",
-   "ad_id"   =>"Αναγνωριστικό διαφήμισης",
-   "contact_with"   =>"Επικοινωνήστε με",
-   "create_adv"   =>"Δημιουργία διαφήμισης",
-   "edit_adv"   =>"Επεξεργασία διαφήμισης",
-   "mandatory_fields"   =>"Υποχρεωτικά πεδία",
-   "ad_post"   =>"Δημοσίευση διαφημίσεων",
-   "sell_item"   =>"Πουλήστε ένα στοιχείο ή μια υπηρεσία",
-   "search_save"   =>"Η αγαπημένη αναζήτηση αποθηκεύτηκε με επιτυχία",
-   "address"   =>"Διεύθυνση",
-   "email"   =>"ΗΛΕΚΤΡΟΝΙΚΗ ΔΙΕΥΘΥΝΣΗ",
-   "phone"   =>"Τηλέφωνο",
-   "send_us_your_feedback"   =>"Στείλτε μας τα σχόλιά σας",
-   "online_shopping"   =>"ΠΑΙΡΝΩ",
-   "approved"   =>"Εγκρίθηκε με επιτυχία",
-   "declined"   =>"Απορρίφθηκε με επιτυχία",
-   "search_on_map"   =>"Αναζήτηση στο χάρτη",
-   "post_free_ad"   =>"Δημοσίευση δωρεάν διαφήμισης",
-   "select_a_category"   =>"Επιλέξτε μία κατηγορία",
-   "select_a_sub_category"   =>"Επιλέξτε μία υπο-κατηγορία",
-   "all_dopings_added"   =>"Όλοι οι τύποι ντόπινγκ προστίθενται για αυτήν τη διαφήμιση.",
-   "is_get_adv"   =>   [
-      "name"      =>"ΠΑΙΡΝΩ",
-
-   ],
-   "corporate_info"   =>"Εταιρικές πληροφορίες",
-   "featured_ads"   =>"Προτεινόμενες διαφημίσεις",
-   "trending_ads"   =>"Αλλαγή διαφημίσεων",
-   "search_ads_result_message"   =>   [
-      "name"      =>":      βρέθηκαν οι αγγελίες που βρέθηκαν για:αναζήτηση ονόματος",
-
-   ],
-   "total"   =>"Σύνολο",
-   "quantity"   =>"Ποσότητα",
-   "view_cart"   =>"Δείτε το καλάθι",
-   "no_ads"   =>"Χωρίς διαφημίσεις!",
-   "show_display_mode"   =>   [
-      "map"      =>"Χάρτης",
-      "list"      =>"Λίστα",
-
-   ],
-   "advs_list_table_thead"   =>   [
-      "name"      =>"Όνομα διαφήμισης",
-      "price"      =>"Τιμή",
-      "publish_at"      =>"Ημερομηνία διαφήμισης",
-      "location_city"      =>"Πόλη",
-      "location_country"      =>"Χώρα",
-
-   ],
-   "show_list_view"   =>   [
-      "name"      =>"Λίστα",
-
-   ],
-   "show_table_view"   =>   [
-      "name"      =>"Τραπέζι",
-
-   ],
-   "showcase"   =>   [
-      "name"      =>"Βιτρίνα",
-
-   ],
-   "new_ad"   =>   [
-      "name"      =>"Νέα διαφήμιση",
-
-   ],
-   "profile"   =>   [
-      "name"      =>"Προφίλ",
-
-   ],
-   "my_ads"   =>   [
-      "name"      =>"Οι διαφημίσεις μου",
-
-   ],
-
+    // Select cat
+    'select_category_step_by_step' => 'Select Category Step by Step',
 ];
