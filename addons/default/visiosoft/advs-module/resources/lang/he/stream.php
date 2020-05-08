8affafa53f143beba17363bb6cb31012d36efeed
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:19:41 +0300

    New translations stream.php (Hebrew)

---
+++ addons/default/visiosoft/advs-module/resources/lang/he/stream.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => 'Advs',
+    ],
+    'categories' => [
+        'name' => 'Categories',
+    ],
+    'village' => [
+        'name' => 'Village',
+    ],
+    'custom_fields' => [
+        'name' => 'Custom fields',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Custom field advs',
+    ],
+    'cf_values' => [
+        'name' => 'Cf values',
+    ],
+];
