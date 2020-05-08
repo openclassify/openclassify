03656c954ece16facd3f4cb3e0032f4314a9fe48
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:20:39 +0300

    New translations section.php (Japanese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ja/section.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => 'Ads',
+    ],
+    'categories' => [
+        'title' => 'Categories',
+    ],
+    'countries' => [
+        'title' => 'Countries',
+    ],
+    'cities' => [
+        'title' => 'Cities',
+    ],
+    'districts' => [
+        'title' => 'Districts',
+    ],
+    'neighborhoods' => [
+        'title' => 'Neighborhoods',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'custom_fields' => [
+        'title' => 'Custom fields',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Custom field ads',
+    ],
+    'cf_values' => [
+        'title' => 'Cf values',
+    ],
+    'fields' => [
+        'title' => 'Custom Fields'
+    ],
+    'assets_clear' => [
+        'name' => 'Assets Clear'
+    ],
+    'general' => 'General',
+    'ads' => 'Ads',
+    'user' => 'User',
+];
