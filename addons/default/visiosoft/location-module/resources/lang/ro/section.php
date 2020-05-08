286fe9583b26bea11956549ac6a241ba2c117c96
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:24:41 +0300

    New translations section.php (Romanian)

---
+++ addons/default/visiosoft/location-module/resources/lang/ro/section.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'countries' => [
+        'title' => 'Countries',
+    ],
+    'cities' => [
+        'title' => 'Cities',
+    ],
+    'districts' => [
+        'title' => 'Districts',
+    ],
+    'neighborhoods' => [
+        'title' => 'Neighborhoods',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'general' => 'General',
+    'map' => 'Map',
+    'setting' => 'Setting',
+];
