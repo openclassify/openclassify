2db8e7dd2833b940f0d61c4e8ef8f0d14f3fa706
Fatih Alp <mail@fatihalp.com>
2020-05-09 01:25:31 +0300

    New translations section.php (Portuguese)

--- addons/default/visiosoft/location-module/resources/lang/pt/section.php
+++ addons/default/visiosoft/location-module/resources/lang/pt/section.php
@@ -1,25 +1,22 @@
 <?php
 
- return[
-   "countries"   =>   [
-      "title"      =>"Países",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Cidades",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Distritos",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"Bairros",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Aldeia",
-
-   ],
-
+return [
+    'countries' => [
+        'title' => 'Countries',
+    ],
+    'cities' => [
+        'title' => 'Cities',
+    ],
+    'districts' => [
+        'title' => 'Districts',
+    ],
+    'neighborhoods' => [
+        'title' => 'Neighborhoods',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'general' => 'General',
+    'map' => 'Map',
+    'setting' => 'Setting',
 ];
