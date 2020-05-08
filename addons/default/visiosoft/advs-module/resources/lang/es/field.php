0692bee1e850871477a73c7bc8f215d0f2acbea0
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:26:08 +0300

    New translations field.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/field.php
@@ -1,265 +1,291 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Título",
+return [
+    'name' => [
+        'name' => 'Title'
+    ],
+    'order' => [
+        'name' => 'Order'
+    ],
+    'slug' => [
+        'name' => 'Slug'
+    ],
+    'parent_category' => [
+        'name' => 'Parent'
+    ],
+    'category' => [
+        'name' => 'Category Title'
+    ],
+    'price' => [
+        'name' => 'Price'
+    ],
+    'date' => [
+        'name' => 'Date'
+    ],
+    'photo' => [
+        'name' => 'Photo'
+    ],
+    'video' => [
+        'name' => 'Video'
+    ],
+    'map' => [
+        'name' => 'Map'
+    ],
+    'stock' => [
+        'name' => 'Stock'
+    ],
+    'advs_desc' => [
+        'name' => 'Description'
+    ],
+    'online_payment' => [
+        'name' => 'Online Payment'
+    ],
+    'city' => [
+        'name' => 'City'
+    ],
+    'country' => [
+        'name' => 'Country'
+    ],
+    'district' => [
+        'name' => 'District'
+    ],
+    'neighborhood' => [
+        'name' => 'Neighborhood'
+    ],
+    'village' => [
+        'name' => 'Village'
+    ],
+    'files' => [
+        'name' => 'Images'
+    ],
+    'cat1' => [
+        'name' => 'Main Category'
+    ],
+    'cat2' => [
+        'name' => 'Sub Category 1'
+    ],
+    'cat3' => [
+        'name' => 'Sub Category 2'
+    ],
+    'cat4' => [
+        'name' => 'Sub Category 3'
+    ],
+    'cat5' => [
+        'name' => 'Sub Category 4'
+    ],
+    'cat6' => [
+        'name' => 'Sub Category 5'
+    ],
+    'cat7' => [
+        'name' => 'Sub Category 6'
+    ],
+    'currency' => [
+        'name' => 'Currency'
+    ],
+    'status' => [
+        'name' => 'Status',
+        'option' => [
+            'approved' => 'Approved',
+            'expired' => 'Expired',
+            'unpublished' => 'Unpublished',
+            'pending_admin' => 'Pending Admin',
+            'pending_user' => 'Pending User'
+        ]
+    ],
+    'pending_adv' => [
+        'name' => 'Unpublished'
+    ],
+    'finish_at' => [
+        'name' => 'Finish At'
+    ],
+    'cover_photo' => [
+        'name' => 'Cover Photo'
+    ],
+    'favs_adv' => [
+        'name' => 'Fav Ads'
+    ],
+    'my_adv' => [
+        'name' => 'ADVS'
+    ],
+    'description' =>  'Description',
+    'explanation' =>  'Explanation',
+    'type' => [
+        'name' => 'Type'
+    ],
+    'parent_adv' => [
+        'name' => 'Related Ad'
+    ],
+    'value' => [
+        'name' => 'Value'
+    ],
+    'list' => 'List',
+    'home' => 'Home',
+    'list-page' => 'List Page',
+    'search' => 'Search',
+    'recommended' => 'Recommended Ads for You',
+    'filter' => 'Filter',
+    'sort' => 'Sort',
+    'sort_by' => 'Sort By',
+    'pick_option' => 'Pick an option',
+    'pick_ordering' => 'Ordering',
+    'price_high' => 'Price High to Low',
+    'price_low' => 'Price Low to High',
+    'newest' => 'Newest',
+    'oldest' => 'Oldest',
+    'address_a_z' => 'Address (A to Z)',
+    'address_z_a' => 'Address (Z to A)',
+    'categories' => 'Categories',
+    'all_categories' => 'All Categories',
+    'location' => 'Location',
+    'send_message' => 'Send Message',
+    'phone_gsm' => 'Gsm Phone',
+    'phone_office' => 'Office Phone',
+    'add_to_favorites' => "Add to Favorites",
+    'send' => 'Send',
+    'search_name' => 'Search Name',
+    'my_favorite_searches' => 'Favorite Searches',
+    'search_desc' => 'You can add and manage your favorite search selections here.',
+    'complaint_placeholder' => 'Type your complaint details here.',
+    'show_details' => 'Show Details',
+    'ad_actions' => 'Ad Actions',
+    'edit' => 'Edit',
+    'approve' => 'Approve',
+    'passive' => "Passive",
+    'offered_by' => 'Offered By',
+    'more_ads_by' => 'More Ads By',
+    'click_phone' => 'Show phone number',
+    'share' => 'Share',
+    'report' => 'Report this ad',
+    'features' => 'Features',
+    "streetview" => "Street View",
+    'recommended_ads' => 'Recommended Ads for You',
+    'new' => 'New',
+    'short_info' => 'Short Info',
+    'secure_trading' => 'Secure Trading',
+    '24/7' => '24/7 Support',
+    'easy_trading' => 'Easy Trading',
+    'need_help' => 'Need help?',
+    'give_a_call' => 'Give a call on',
+    'message_sent' => 'Message Sent!',
+    'asap' => 'Message sent, seller will contact you asap.',
+    'ad_id' => 'Ad Id',
+    'contact_with' => 'Contact With',
+    'create_adv' => 'Create Ad',
+    'edit_adv' => 'Edit Ad',
+    'mandatory_fields' => 'Mandatory Fields',
+    'ad_post' => 'Ad Post',
+    'sell_item' => 'Sell an item or service',
+    'search_save' => 'Favorite search saved successfully',
+    'address' => 'Address',
+    'email' => 'E-Mail',
+    'phone' => 'Phone',
+    'send_us_your_feedback' => 'Send Us Your Feedback',
+    'online_shopping' => 'GET',
+    'approved' => 'Approved Successfully',
+    'extended' => 'Extended Successfully',
+    'declined' => 'Declined Successfully',
+    'search_on_map' => 'Search on Map',
+    'post_free_ad' => 'Post Free Ad',
+    'select_a_category' => 'Select a Category',
+    'select_a_sub_category' => 'Select a Sub Category',
+    'all_dopings_added' => 'All doping types are added for this ad.',
+    'is_get_adv' => [
+        'name' => 'GET'
+    ],
+    'corporate_info' => 'Corporate Info',
+    'featured_ads' => "Featured Ads",
+    'trending_ads' => 'Trendind Ads',
+    'search_ads_result_message' => [
+        'name' => ':count ads found for :name search',
+    ],
+    'total' => 'Total',
+    'quantity' => 'Quantity',
+    'view_cart' => 'View Cart',
+    'no_ads' => 'No Ads found.',
+    'show_display_mode' => [
+        'map' => 'Map',
+        'list' => 'List',
+    ],
+    'advs_list_table_thead' => [
+        'name' => 'Ad Name',
+        'price' => 'Price',
+        'publish_at' => 'Ad Date',
+        'location_city' => 'City',
+        'location_country' => 'Country',
+    ],
+    'show_list_view' => [
+        'name' => 'List',
+    ],
+    'show_table_view' => [
+        'name' => 'Table',
+    ],
+    'showcase' => [
+        'name' => 'Showcase',
+    ],
+    'new_ad' => [
+        'name' => 'New Ad',
+    ],
+    'profile' => [
+        'name' => 'Profile',
+    ],
+    'my_ads' => [
+        'name' => 'My Ads',
+    ],
+    'sold_out' => [
+        'name' => 'Sold out',
+    ],
+    'edit_category' => 'Edit Category',
+    'set_category' => 'Set Category',
+    'max' => [
+        'name' => 'Max',
+    ],
+    'min' => [
+        'name' => 'Min',
+    ],
+    'in_the_last_24_hours' => [
+        'name' => 'In The Last 24 Hours'
+    ],
+    'in_the_last_week' => [
+        'name' => 'In The Last Week'
+    ],
+    'in_the_last_month' => [
+        'name' => 'In The Last Month'
+    ],
+    'ads_with_photo' => [
+        'name' => 'Ads With Photo'
+    ],
+    'ads_with_video' => [
+        'name' => 'Ads With Video'
+    ],
+    'yes' => [
+        'name' => 'Yes'
+    ],
+    "no_location" => "No location is selected.",
+    "continue" => 'Continue',
+    "gallery" => 'Gallery',
 
-   ],
-   "order"   =>   [
-      "name"      =>"Orden",
+    // Pending screen
+    'pending_message_1' => 'The Ad is in the Approval Process',
+    'pending_message_2' => "Ad's Update Date",
+    'pending_message_3' => 'Estimated Waiting Time',
+    'pending_message_4' => 'The time spent in the approval process will be added to the doping time.',
+    'pending_message_5' => 'No transactions can be made from the call center regarding the ad in the approval process.',
+    'hours' => 'hours',
 
-   ],
-   "slug"   =>   [
-      "name"      =>"Babosa",
+    // Preview page
+    'preview_text' => [
+        'part_1' => 'If the following information about your ad is correct, click the "Continue" button and go to the next step.',
+        'part_2' => 'If not, click the "Edit" button.',
+    ],
+    'views' => 'Views',
 
-   ],
-   "parent_category"   =>   [
-      "name"      =>"Padre",
+    // Detail page
+    "no_street_view" => "No street view image in this location",
+    'edit_my_contact_info' => 'Edit My Contact Information',
+    'update_my_contact_info' => 'update my contact info',
+    'contact_info' => 'Contact information',
+    'ad_info' => 'Ad Information',
 
-   ],
-   "category"   =>   [
-      "name"      =>"Título de categoría",
-
-   ],
-   "price"   =>   [
-      "name"      =>"Precio",
-
-   ],
-   "stock"   =>   [
-      "name"      =>"Valores",
-
-   ],
-   "advs_desc"   =>   [
-      "name"      =>"Descripción",
-
-   ],
-   "online_payment"   =>   [
-      "name"      =>"Pago en línea",
-
-   ],
-   "city"   =>   [
-      "name"      =>"Ciudad",
-
-   ],
-   "country"   =>   [
-      "name"      =>"País",
-
-   ],
-   "district"   =>   [
-      "name"      =>"Distrito",
-
-   ],
-   "neighborhood"   =>   [
-      "name"      =>"Barrio",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Pueblo",
-
-   ],
-   "files"   =>   [
-      "name"      =>"Imágenes",
-
-   ],
-   "cat1"   =>   [
-      "name"      =>"categoria principal",
-
-   ],
-   "cat2"   =>   [
-      "name"      =>"Subcategoría 1",
-
-   ],
-   "cat3"   =>   [
-      "name"      =>"Subcategoría 2",
-
-   ],
-   "cat4"   =>   [
-      "name"      =>"Subcategoría 3",
-
-   ],
-   "cat5"   =>   [
-      "name"      =>"Subcategoría 4",
-
-   ],
-   "cat6"   =>   [
-      "name"      =>"Subcategoría 5",
-
-   ],
-   "cat7"   =>   [
-      "name"      =>"Subcategoría 6",
-
-   ],
-   "currency"   =>   [
-      "name"      =>"Moneda",
-
-   ],
-   "status"   =>   [
-      "name"      =>"Estado",
-
-   ],
-   "next_add_advs_title"   =>   [
-      "name"      =>"Publique un anuncio en solo 30 segundos",
-
-   ],
-   "next_add_advs_msg"   =>   [
-      "name"      =>"NO publique varios anuncios para los mismos artículos o servicios. Se eliminarán todos los anuncios duplicados,
-      spam y categorizados incorrectamente.",
-
-   ],
-   "next_add_advs_btn"   =>   [
-      "name"      =>"próximo",
-
-   ],
-   "cancel_add_advs_btn"   =>   [
-      "name"      =>"O cancelar",
-
-   ],
-   "pending_adv"   =>   [
-      "name"      =>"Anuncios pendientes",
-
-   ],
-   "archived_adv"   =>   [
-      "name"      =>"Anuncios archivados",
-
-   ],
-   "favs_adv"   =>   [
-      "name"      =>"Anuncios Fav",
-
-   ],
-   "my_adv"   =>   [
-      "name"      =>"ADVS",
-
-   ],
-   "description"   =>   [
-      "name"      =>"Descripción",
-
-   ],
-   "type"   =>   [
-      "name"      =>"Tipo",
-
-   ],
-   "parent_adv"   =>   [
-      "name"      =>"Anuncio relacionado",
-
-   ],
-   "value"   =>   [
-      "name"      =>"Valor",
-
-   ],
-   "home"   =>"Casa",
-   "list-page"   =>"Página de lista",
-   "search"   =>"Buscar",
-   "recommended"   =>"Anuncios recomendados para usted",
-   "sort_by"   =>"Ordenar por",
-   "pick_option"   =>"Elige una opción",
-   "price_high"   =>"Precio mayor a menor",
-   "price-low"   =>"Precios de barato a caro",
-   "newest"   =>"El más nuevo",
-   "categories"   =>"Categorias",
-   "all_categories"   =>"todas las categorias",
-   "location"   =>"Ubicación",
-   "send_message"   =>"Enviar mensaje",
-   "phone_gsm"   =>"Teléfono gsm",
-   "phone_office"   =>"Telefono de oficina",
-   "add_to_favorites"   =>"Agregar a los favoritos",
-   "send"   =>"Enviar",
-   "search_name"   =>"Buscar nombre",
-   "my_favorite_searches"   =>"Búsquedas favoritas",
-   "search_desc"   =>"Puede agregar y administrar sus selecciones de búsqueda favoritas aquí.",
-   "complaint_placeholder"   =>"Escriba los detalles de su queja aquí.",
-   "show_details"   =>"Mostrar detalles",
-   "edit"   =>"Editar",
-   "approve"   =>"Aprobar",
-   "passive"   =>"Pasivo",
-   "offered_by"   =>"Ofrecido por",
-   "more_ads_by"   =>"Más anuncios de",
-   "click_phone"   =>"Mostrar número de teléfono",
-   "share"   =>"Comparte este anuncio",
-   "report"   =>"Denunciar este anuncio",
-   "features"   =>"Caracteristicas",
-   "streetview"   =>"vista de calle",
-   "recommended_ads"   =>"Anuncios recomendados para usted",
-   "new"   =>"Nuevo",
-   "short_info"   =>"Información corta",
-   "secure_trading"   =>"Comercio seguro",
-   "24/7"   =>"Soporte 24/7",
-   "easy_trading"   =>"Comercio fácil",
-   "need_help"   =>"¿Necesitas ayuda?",
-   "give_a_call"   =>"Dar una llamada en",
-   "message_sent"   =>"¡Mensaje enviado!",
-   "asap"   =>"Mensaje enviado,
-   el vendedor se comunicará con usted lo antes posible.",
-   "ad_id"   =>"ID del anuncio",
-   "contact_with"   =>"Contactar con",
-   "create_adv"   =>"Crear anuncio",
-   "edit_adv"   =>"Editar anuncio",
-   "mandatory_fields"   =>"Campos obligatorios",
-   "ad_post"   =>"Publicación de anuncios",
-   "sell_item"   =>"Vender un artículo o servicio",
-   "search_save"   =>"Búsqueda favorita guardada con éxito",
-   "address"   =>"Dirección",
-   "email"   =>"Correo electrónico",
-   "phone"   =>"Teléfono",
-   "send_us_your_feedback"   =>"Envíenos sus comentarios",
-   "online_shopping"   =>"OBTENER",
-   "approved"   =>"Aprobado exitosamente",
-   "declined"   =>"Rechazado con éxito",
-   "search_on_map"   =>"Buscar en el mapa",
-   "post_free_ad"   =>"Publicar anuncio gratis",
-   "select_a_category"   =>"Seleccione una categoría",
-   "select_a_sub_category"   =>"Selecciona una sub-categoría",
-   "all_dopings_added"   =>"Todos los tipos de dopaje se agregan para este anuncio.",
-   "is_get_adv"   =>   [
-      "name"      =>"OBTENER",
-
-   ],
-   "corporate_info"   =>"Información corporativa",
-   "featured_ads"   =>"Anuncios destacados",
-   "trending_ads"   =>"Anuncios de tendencias",
-   "search_ads_result_message"   =>   [
-      "name"      =>":      cuenta de anuncios encontrados para:búsqueda de nombre",
-
-   ],
-   "total"   =>"Total",
-   "quantity"   =>"Cantidad",
-   "view_cart"   =>"Ver carrito",
-   "no_ads"   =>"¡Sin anuncios!",
-   "show_display_mode"   =>   [
-      "map"      =>"Mapa",
-      "list"      =>"Lista",
-
-   ],
-   "advs_list_table_thead"   =>   [
-      "name"      =>"Nombre del anuncio",
-      "price"      =>"Precio",
-      "publish_at"      =>"Fecha del anuncio",
-      "location_city"      =>"Ciudad",
-      "location_country"      =>"País",
-
-   ],
-   "show_list_view"   =>   [
-      "name"      =>"Lista",
-
-   ],
-   "show_table_view"   =>   [
-      "name"      =>"Mesa",
-
-   ],
-   "showcase"   =>   [
-      "name"      =>"Escaparate",
-
-   ],
-   "new_ad"   =>   [
-      "name"      =>"Anuncio nuevo",
-
-   ],
-   "profile"   =>   [
-      "name"      =>"Perfil",
-
-   ],
-   "my_ads"   =>   [
-      "name"      =>"Mis anuncios",
-
-   ],
-
+    // Select cat
+    'select_category_step_by_step' => 'Select Category Step by Step',
 ];
