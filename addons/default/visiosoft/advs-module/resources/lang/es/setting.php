19f1c62d8bdf2a0c2991d0fffc7f0e7130c1adff
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:26:03 +0300

    New translations setting.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/setting.php
@@ -1,136 +1,133 @@
 <?php
 
- return[
-   "auto_approve"   =>   [
-      "name"      =>"Aprobar automáticamente",
+return [
+    'auto_approve' => [
+        'name' => 'Auto Approve',
+    ],
+    'default_view_type' => [
+        'name' => 'Default View Type',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Hide Price if Zero',
+        'instructions' => 'Hide the price if the ad price is 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Estimated Pending Time',
+        'instructions' => 'In Hours',
+    ],
+    'default_published_time' => [
+        'name' => 'Default Publish Time',
+    ],
+    'default_adv_limit' => [
+        'name' => 'Default Ads Limit',
+    ],
+    'address' => [
+        'name' => 'Contact Adress',
+    ],
+    'phone' => [
+        'name' => 'Contact Phone',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Map Long Coordinate',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Map Lat Coordinate',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Social Share Logo',
+        'warning'      => 'It should be at a resolution of 1200 x 630. ',
 
-   ],
-   "default_published_time"   =>   [
-      "name"      =>"Tiempo de publicación predeterminado",
+    ],
+    'currencies' => [
+        'name' => 'Active Currencies',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'Site address',
+        'warning'      => 'Auto added www.  www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Key',
+    ],
 
-   ],
-   "default_adv_limit"   =>   [
-      "name"      =>"Límite de anuncios predeterminado",
-
-   ],
-   "address"   =>   [
-      "name"      =>"Dirección de contacto",
-
-   ],
-   "phone"   =>   [
-      "name"      =>"teléfono de contacto",
-
-   ],
-   "contact_mail"   =>   [
-      "name"      =>"Correo de contacto",
-      "warning"      =>"Auto añadido sitename <strong>info</strong> @sitename",
-
-   ],
-   "map_coordinates_long"   =>   [
-      "name"      =>"Mapa de coordenadas largas",
-
-   ],
-   "map_coordinates_lat"   =>   [
-      "name"      =>"Map Lat Coordinate",
-
-   ],
-   "logo"   =>   [
-      "name"      =>"Logo",
-
-   ],
-   "ogImage"   =>   [
-      "name"      =>"Logotipo de Social Share",
-      "warning"      =>"Debe tener una resolución de 1200 x 630.",
-
-   ],
-   "currencies"   =>   [
-      "name"      =>"Monedas activas",
-
-   ],
-   "site_address"   =>   [
-      "name"      =>"Dirección del sitio",
-      "warning"      =>"Auto añadido www. www. <strong>sitename.com</strong>",
-
-   ],
-   "google_map_key"   =>   [
-      "name"      =>"Google Map Api Key",
-
-   ],
-   "default_country"   =>   [
-      "name"      =>"País de anuncio predeterminado",
-
-   ],
-   "free_currencyconverterapi_key"   =>   [
-      "name"      =>"Clave API de convertidor de divisas",
-
-   ],
-   "default_city"   =>   [
-      "name"      =>"Ciudad de anuncio predeterminada",
-
-   ],
-   "default_district"   =>   [
-      "name"      =>"Distrito de anuncios predeterminado",
-
-   ],
-   "default_neighborhood"   =>   [
-      "name"      =>"Barrio de anuncio predeterminado",
-
-   ],
-   "default_currency"   =>   [
-      "name"      =>"Moneda de anuncio predeterminada",
-
-   ],
-   "default_GET"   =>   [
-      "name"      =>"Anuncio predeterminado GET",
-
-   ],
-   "twitter"   =>   [
-      "name"      =>"Gorjeo",
-
-   ],
-   "facebook"   =>   [
-      "name"      =>"Facebook",
-
-   ],
-   "youtube"   =>   [
-      "name"      =>"Youtube",
-
-   ],
-   "google"   =>   [
-      "name"      =>"Google",
-
-   ],
-   "watermark_type"   =>   [
-      "name"      =>"Tipo de marca de agua",
-
-   ],
-   "watermark_text"   =>   [
-      "name"      =>"Texto de marca de agua",
-
-   ],
-   "watermark_image"   =>   [
-      "name"      =>"Imagen de marca de agua",
-
-   ],
-   "watermark_position"   =>   [
-      "name"      =>"Posición de marca de agua",
-
-   ],
-   "watermark_opacity"   =>   [
-      "name"      =>"Opacidad de marca de agua",
-
-   ],
-   "listing_page_image"   =>   [
-      "name"      =>"Imagen de la página de listado",
-
-   ],
-   "enabled_currencies"   =>   [
-      "name"      =>"Monedas habilitadas",
-
-   ],
-   "google_statistic_code"   =>   [
-      "name"      =>"Código estadístico de Google",
-
-   ],
-
+    'default_country' => [
+        'name' => 'Default Ad Country',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Currency Converter API Key'
+    ],
+    'default_city' => [
+        'name' => 'Default Ad City',
+    ],
+    'default_district' => [
+        'name' => 'Default Ad District',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Default Ad Neighborhood',
+    ],
+    'default_currency' => [
+        'name' => 'Default Ad Currency',
+    ],
+    'default_GET' => [
+        'name' => 'Default Ad GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'Thumbnail Width',
+    ],
+    'thumbnail_height' => [
+        'name' => 'Thumbnail Height',
+    ],
+    'picture_width' => [
+        'name' => 'Picture Width',
+    ],
+    'picture_height' => [
+        'name' => 'Picture Height',
+    ],
+    'twitter' => [
+        'name' => 'Twitter',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Watermark Type',
+    ],
+    'watermark_text' => [
+        'name' => 'Watermark Text',
+    ],
+    'watermark_image' => [
+        'name' => 'Watermark Image',
+    ],
+    'watermark_position' => [
+        'name' => 'Watermark Position',
+    ],
+    'listing_page_image' => [
+        'name' => 'Listing Page Image',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Enabled Currencies',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google Statistic Code',
+    ],
+    'register_email_field' => [
+        'name' => 'Member Registration Email Requirement',
+    ],
 ];
