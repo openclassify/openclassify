24415ce6556bdaf59f4714979641c5ecc98266ab
Fatih Alp <mail@fatihalp.com>
2020-05-09 00:26:04 +0300

    New translations section.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/section.php
@@ -1,49 +1,43 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "title"      =>"Anuncios",
-
-   ],
-   "categories"   =>   [
-      "title"      =>"Categorias",
-
-   ],
-   "countries"   =>   [
-      "title"      =>"Países",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Ciudades",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Distritos",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"Barrios",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Pueblo",
-
-   ],
-   "custom_fields"   =>   [
-      "title"      =>"Campos Personalizados",
-
-   ],
-   "custom_field_advs"   =>   [
-      "title"      =>"Anuncios de campo personalizados",
-
-   ],
-   "cf_values"   =>   [
-      "title"      =>"Valores de Cf",
-
-   ],
-   "fields"   =>   [
-      "title"      =>"Campos Personalizados",
-
-   ],
-
+return [
+    'advs' => [
+        'title' => 'Ads',
+    ],
+    'categories' => [
+        'title' => 'Categories',
+    ],
+    'countries' => [
+        'title' => 'Countries',
+    ],
+    'cities' => [
+        'title' => 'Cities',
+    ],
+    'districts' => [
+        'title' => 'Districts',
+    ],
+    'neighborhoods' => [
+        'title' => 'Neighborhoods',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'custom_fields' => [
+        'title' => 'Custom fields',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Custom field ads',
+    ],
+    'cf_values' => [
+        'title' => 'Cf values',
+    ],
+    'fields' => [
+        'title' => 'Custom Fields'
+    ],
+    'assets_clear' => [
+        'name' => 'Assets Clear'
+    ],
+    'general' => 'General',
+    'ads' => 'Ads',
+    'user' => 'User',
 ];
