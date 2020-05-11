b2276a015f8b3c2168a1e1ecd14317dadc7390e8
Fatih Alp <mail@fatihalp.com>
2020-05-11 12:50:18 +0300

    New translations button.php (Persian)

--- addons/default/visiosoft/cats-module/resources/lang/fa/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/fa/button.php
@@ -1,8 +1,8 @@
 <?php
 
- return[
-   "new_category"   =>"دسته بندی جدید",
-   "add_sub_category"   =>"زیر مجموعه را اضافه کنید",
-   "sub_category"   =>"نمایش زیر گروه",
-   "new_placeholderforsearch"   =>"New Placeholderforsearch"
+return [
+    'new_category' => 'New Category',
+    'add_sub_category' => 'Add Sub Category',
+    'sub_category' => 'Show Sub Category',
+    'new_placeholderforsearch' => 'New Placeholderforsearch',
 ];
