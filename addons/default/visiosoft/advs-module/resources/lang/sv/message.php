db9da985dc10ced3dfafa26ecf5d29d85572d4a4
Fatih Alp <mail@fatihalp.com>
2020-05-11 16:18:51 +0300

    New translations message.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/message.php
@@ -2,7 +2,6 @@
 
 return [
     'error_added_cart' => 'Requested product is currently missing or inactive',
-    'please_buy_package' => 'Please buy package',
     'please_payment' => 'Please Pay',
     'bank_account_info' => 'Bank account information',
     'agreement' => 'By clicking Post, you agree to our Terms of Use and Privacy Policy acknowledge that you are the rightful owner of this item.',
