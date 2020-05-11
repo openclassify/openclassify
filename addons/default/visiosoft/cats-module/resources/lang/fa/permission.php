dbc94870cbcc0323eaf6d555388a7a738394d6c4
Fatih Alp <mail@fatihalp.com>
2020-05-11 12:50:25 +0300

    New translations permission.php (Persian)

--- addons/default/visiosoft/cats-module/resources/lang/fa/permission.php
+++ addons/default/visiosoft/cats-module/resources/lang/fa/permission.php
@@ -1,24 +1,20 @@
 <?php
 
- return[
-   "category"   =>   [
-      "name"      =>"دسته بندی",
-      "option"      =>      [
-         "read"         =>"می توانید دسته بخوانید؟        ",
-         "write"         =>"آیا می توانید دسته ایجاد یا ویرایش کنید؟        ",
-         "delete"         =>"آیا می توان دسته را حذف کرد؟"
-      ],
-
-   ],
-   "placeholderforsearch"   =>   [
-      "name"      =>"مکان نگهدارنده",
-      "option"      =>      [
-         "read"         =>"آیا می توانید placeyarderforsearch بخوانید؟        ",
-         "write"         =>"آیا می توانید placeyarderforsearch ایجاد یا ویرایش کنید؟        ",
-         "delete"         =>"آیا می توانید متغیرهایی را حذف کنید؟",
-
-      ],
-
-   ],
-
+return [
+    'category' => [
+        'name'   => 'Category',
+        'option' => [
+            'read'   => 'Can read category?',
+            'write'  => 'Can create/edit category?',
+            'delete' => 'Can delete category?',
+        ],
+    ],
+    'placeholderforsearch' => [
+        'name'   => 'Placeholderforsearch',
+        'option' => [
+            'read'   => 'Can read placeholderforsearch?',
+            'write'  => 'Can create/edit placeholderforsearch?',
+            'delete' => 'Can delete placeholderforsearch?',
+        ],
+    ],
 ];
