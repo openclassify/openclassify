04643374734cdf65ebceb05fb6a34669a044ec8b
Fatih Alp <mail@fatihalp.com>
2020-05-11 12:49:17 +0300

    New translations permission.php (Persian)

--- addons/default/visiosoft/location-module/resources/lang/fa/permission.php
+++ addons/default/visiosoft/location-module/resources/lang/fa/permission.php
@@ -1,15 +1,13 @@
 <?php
 
- return[
-   "village"   =>   [
-      "name"      =>"روستا",
-      "option"      =>      [
-         "read"         =>"می توانید دهکده بخوانید؟        ",
-         "write"         =>"آیا می توانید دهکده ایجاد یا ویرایش کنید؟        ",
-         "delete"         =>"می توانید روستا را حذف کنید؟",
+return [
 
-      ],
-
-   ],
-
+    'village' => [
+        'name'   => 'Village',
+        'option' => [
+            'read'   => 'Can read village?',
+            'write'  => 'Can create/edit village?',
+            'delete' => 'Can delete village?',
+        ],
+    ],
 ];
