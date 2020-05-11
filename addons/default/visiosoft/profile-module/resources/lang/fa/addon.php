f47ce07a24575358670f87d2289f5f60f502cefd
Fatih Alp <mail@fatihalp.com>
2020-05-11 12:49:31 +0300

    New translations addon.php (Persian)

--- addons/default/visiosoft/profile-module/resources/lang/fa/addon.php
+++ addons/default/visiosoft/profile-module/resources/lang/fa/addon.php
@@ -1,7 +1,7 @@
 <?php
 
- return[
-   "title"   =>"مشخصات",
-   "name"   =>"ماژول پروفایل",
-   "description"   =>""
+return [
+    'title'       => 'Profile',
+    'name'        => 'Profile Module',
+    'description' => ''
 ];
