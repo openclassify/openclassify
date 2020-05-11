ceedd5e0ab39e358e5d7dd0af2b6c1c0c391449a
Fatih Alp <mail@fatihalp.com>
2020-05-11 18:11:26 +0300

    New translations button.php (Albanian)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/sq/button.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/sq/button.php
@@ -4,5 +4,5 @@ return [
     'select_file' => 'Zgjidhni skedarin',
     'upload'      => 'Ngarkoni',
     'remove'      => 'Hiq',
-    'change'      => 'ndryshim',
+    'change'      => 'Ndryshim',
 ];
