562ea2e356be8765c07bcc149463d10a8dd2985e
Fatih Alp <mail@fatihalp.com>
2020-05-11 12:51:06 +0300

    New translations preference.php (Persian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/preference.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/preference.php
@@ -1,10 +1,30 @@
 <?php
 
 return [
-    'navigation' => [
-        'name'         => 'منو ها',
-        'instructions' => 'ترتیب منوها را مشخص کنید',
-        'warning'      => 'اولین آیتم در منو به عنوان خانه در نظر گرفته می شود',
-        'reorder'      => 'برای مرتب کردن آیتم ها را در سایدبار با موس جابجا کنید',
+    'sidebar_hover' => [
+        'name'         => 'Sidebar Hover',
+        'instructions' => 'Expand the sidebar on hover?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigation',
+        'instructions' => 'Specify your <em>personal</em> order of navigation.',
+        'warning'      => 'The first accessible navigation item is used as your <strong>home</strong> area.',
+        'reorder'      => 'Drag and drop the primary navigation items in the <strong>sidebar</strong> to reorder them.',
+    ],
+    'display'       => [
+        'name'         => 'Display Density',
+        'instructions' => 'Compact display allows more content to be shown on the screen at once.',
+        'option'       => [
+            'default' => 'Default',
+            'compact' => 'Compact',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => 'Sidebar Mode',
+        'instructions' => 'Static sidebars will always be visible.',
+        'option'       => [
+            'default' => 'Default',
+            'static'  => 'Static',
+        ],
     ],
 ];
