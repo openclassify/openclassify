041edf6ad190c8510ee4a4549771eecc3e1a6c68
Fatih Alp <mail@fatihalp.com>
2020-05-11 12:49:54 +0300

    New translations button.php (Persian)

--- addons/default/visiosoft/location-module/resources/lang/fa/button.php
+++ addons/default/visiosoft/location-module/resources/lang/fa/button.php
@@ -1,17 +1,17 @@
 <?php
 
- return[
-   "new_country"   =>"کشور جدید",
-   "new_city"   =>"شهر جدید",
-   "new_district"   =>"منطقه جدید",
-   "new_neighborhood"   =>"محله جدید",
-   "new_village"   =>"دهکده جدید",
-   "sub_cities"   =>"شهرهای زیر شهر",
-   "add_sub_cities"   =>"اضافه کردن زیر شهر",
-   "add_sub_districts"   =>"اضافه کردن Sub District",
-   "add_sub_neighborhoods"   =>"اضافه کردن زیر محله",
-   "add_sub_village"   =>"اضافه کردن Sub Village",
-   "sub_districts"   =>"مناطق فرعی",
-   "sub_neighborhoods"   =>"محله های فرعی",
-   "sub_village"   =>"دهکده زیر"
+return [
+    'new_country' => 'New Country',
+    'new_city' => 'New City',
+    'new_district' => 'New District',
+    'new_neighborhood' => 'New Neighborhood',
+    'new_village' => 'New Village',
+    'sub_cities' => 'Sub Cities',
+    'add_sub_cities' => 'Add Sub City',
+    'add_sub_districts' => 'Add Sub District',
+    'add_sub_neighborhoods' => 'Add Sub Neighborhood',
+    'add_sub_village' => 'Add Sub Village',
+    'sub_districts' => 'Sub Districts',
+    'sub_neighborhoods' => 'Sub Neighborhoods',
+    'sub_village' => 'Sub Village',
 ];
