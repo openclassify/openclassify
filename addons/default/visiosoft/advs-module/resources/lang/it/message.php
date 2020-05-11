d197ff0048a8b6366fba9ed995e77ad2430a0d66
Fatih Alp <mail@fatihalp.com>
2020-05-11 16:18:40 +0300

    New translations message.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/message.php
@@ -2,7 +2,6 @@
 
 return [
     'error_added_cart' => 'Il prodotto richiesto è attualmente mancante o inattivo',
-    'please_buy_package' => 'Si prega di acquistare il pacchetto',
     'please_payment' => 'Please Pay',
     'bank_account_info' => 'Bank account information',
     'agreement' => 'By clicking Post, you agree to our Terms of Use and Privacy Policy acknowledge that you are the rightful owner of this item.',
