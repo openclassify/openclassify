76952e82be0c51e474054f902acde15e1a16b675
Fatih Alp <mail@fatihalp.com>
2020-05-12 01:38:24 +0300

    New translations button.php (Vietnamese)

---
+++ addons/default/visiosoft/location-module/resources/lang/vi/button.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'new_country' => 'New Country',
+    'new_city' => 'New City',
+    'new_district' => 'New District',
+    'new_neighborhood' => 'New Neighborhood',
+    'new_village' => 'New Village',
+    'sub_cities' => 'Sub Cities',
+    'add_sub_cities' => 'Add Sub City',
+    'add_sub_districts' => 'Add Sub District',
+    'add_sub_neighborhoods' => 'Add Sub Neighborhood',
+    'add_sub_village' => 'Add Sub Village',
+    'sub_districts' => 'Sub Districts',
+    'sub_neighborhoods' => 'Sub Neighborhoods',
+    'sub_village' => 'Sub Village',
+];
