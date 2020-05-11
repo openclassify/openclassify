8d0e7d13a0c1800f6827631e7bca27677c512c51
Fatih Alp <mail@fatihalp.com>
2020-05-11 12:51:15 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/setting.php
@@ -1,10 +1,14 @@
 <?php
 
 return [
-    'navigation' => [
-        'name'         => 'منو ها',
-        'instructions' => 'ترتیب منوها را مشخص کنید',
-        'warning'      => 'اولین آیتم در منو به عنوان خانه در نظر گرفته می شود',
-        'reorder'      => 'برای مرتب کردن آیتم ها را در سایدبار با موس جابجا کنید',
+    'sidebar_hover' => [
+        'name'         => 'Sidebar Hover',
+        'instructions' => 'Expand the sidebar on hover?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigation',
+        'instructions' => 'Specify the <em>default</em> order of navigation.',
+        'warning'      => 'The first accessible navigation item is used as the <strong>home</strong> area.',
+        'reorder'      => 'Drag and drop the primary navigation items in the <strong>sidebar</strong> to reorder them.',
     ],
 ];
