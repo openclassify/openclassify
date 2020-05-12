ea06edda2219941b8d5bba1ffcb948b8a1e60d12
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:05:02 +0300

    New translations message.php (Romanian)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/ro/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_folder'     => 'În ce folder doriți să încărcați?',
+    'upload'            => 'Adăugați İmages | Încărcați imagini | Atașați o fotografie',
+    'choose_files'      => 'Ce fișiere doriți să utilizați?',
+    'no_files_selected' => 'Nu au fost selectate fișiere.',
+    'no_uploads'        => 'Nu s-au încărcat fișiere.',
+    'overwrite'         => 'a fost deja încărcat. Doriți să îl suprascrieți?',
+    'uploading'         => 'Se încarcă',
+    'loading'           => 'Se încarcă',
+];
