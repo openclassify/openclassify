91d64fb1961bbb6bc7b836e5ea1a10f33c779f65
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:18:50 +0300

    New translations section.php (Swedish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/sv/section.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => 'annonser',
+    ],
+    'categories' => [
+        'title' => 'kategorier',
+    ],
+    'countries' => [
+        'title' => 'Länder',
+    ],
+    'cities' => [
+        'title' => 'städer',
+    ],
+    'districts' => [
+        'title' => 'distrikt',
+    ],
+    'neighborhoods' => [
+        'title' => 'stadsdelar',
+    ],
+    'village' => [
+        'title' => 'By',
+    ],
+    'custom_fields' => [
+        'title' => 'Anpassade fält',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Anpassade fältannonser',
+    ],
+    'cf_values' => [
+        'title' => 'Cf-värden',
+    ],
+    'fields' => [
+        'title' => 'Anpassade fält'
+    ],
+    'assets_clear' => [
+        'name' => 'Tillgångar tydliga'
+    ],
+    'general' => 'Allmän',
+    'ads' => 'annonser',
+    'user' => 'Användare',
+];
