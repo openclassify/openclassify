8e8e5c77c2961e8ca1e2d1a20ea7a6b26b37a651
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:31 +0300

    New translations message.php (Chinese Traditional)

---
+++ addons/default/visiosoft/profile-module/resources/lang/zh/message.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    "email"      => "電子郵件字段為必填項！",
+    "username"      => "用戶名字段為必填項！",
+    "success_update"      => "個人資料更新成功！",
+    "adress_success_update"      => "地址更新成功！",
+    "adress_success_create"      => "地址創建成功！",
+    "login_error" => "寫錯了登錄名或密碼。",
+    "login_noMail_old_user" => "找不到您輸入的電子郵件地址。",
+    "login_noMail_old_user2" => "請檢查電子郵件地址，然後重試。",
+    "login_noMail_old_user3" => "如果問題仍然存在，請聯繫“ info@openclassify.com”。",
+    "login_info_old_user" => "新密碼已發送到您的電子郵件地址。",
+    "success" => "成功",
+    "notified_new_updates" => "我想收到新的更新通知",
+    "notified_about_ads" => "允許有關我的廣告的通知",
+    "receive_messages_email" => "我想通過電子郵件接收消息",
+    "no_packages_module" => "沒有套餐模塊！",
+    "required_add"=> "請填寫所有必填行。",
+    "no_extend_package" => "沒有擴展廣告包",
+    "saved" => "保存了！",
+    'please_confirm_transaction' => '請確認交易',
+    'password_do_not_match' => '密碼不匹配',
+    'your_password_changed' => '您的密碼已成功更新',
+    'error_valid_email_or_phone' => '電話號碼或電子郵件地址格式不正確。',
+    'error_valid_phone' => '電話號碼格式不正確。',
+    'registered_phone' => '該電話號碼已被註冊。',
+    'ajax_address_error' => '沒有地址或無權查看。',
+    'empty_password_sms_message' => '由於安全問題，我們更改了您的密碼！您的新密碼是：',
+    'required_all' => "所有字段均為必填項！",
+
+    // Forgot Password
+    'email_phone_not_found' => '電子郵件，電話號碼不正確！',
+
+    'disable_account' => '您要關閉帳戶嗎？此操作無法撤消。',
+];
