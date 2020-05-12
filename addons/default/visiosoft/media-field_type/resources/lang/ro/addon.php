041b6e71e901096a38f19ae8535c87232da014b1
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:05:08 +0300

    New translations addon.php (Romanian)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/ro/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Tipul câmpului media',
+    'name'        => 'Tip câmp fișiere',
+    'description' => 'Un tip de câmp de încărcare a mai multor fișiere.',
+];
