ffea2126dfe68bbadd4d630ddf93ebf019f95df2
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:13:18 +0300

    New translations message.php (Japanese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ja/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'error_added_cart' => 'リクエストされた製品が現在見つからないか、非アクティブです',
+    'please_payment' => '支払ってください',
+    'bank_account_info' => '銀行口座情報',
+    'agreement' => '[投稿]をクリックすると、利用規約に同意したことになります。プライバシーポリシーでは、このアイテムの正当な所有者であることを認めます。',
+    'update_category_msg' => '分類されたカテゴリが更新されます。',
+    'updated_category_msg' => 'カテゴリを編集しました。',
+    'create_ad_with_post_cat' => [
+        'title' => "カテゴリの選択が完了しました。",
+        'success' => true,
+        'continueBtn' => "継続する",
+    ],
+    'update_category_info' => 'カテゴリを更新してください。',
+    'filter_by_search_msg' => '検索でフィルター。',
+    'delete_author_error' => '自分に属していない投稿は削除できません',
+    'success_delete' => '広告が削除されました',
+    'pending_ad_status' => '広告は編集者の承認待ちです。',
+    'extended' => '：number個の広告が正常に拡張されました。',
+    'category_not_exist' => 'このカテゴリはもう存在しません！',
+    'currency_converted' => '通貨は正常に変換されました。',
+    'choose_user' => 'ユーザーを選択してください',
+    'owner_updated_successfully' => 'オーナーが更新されました！',
+];
