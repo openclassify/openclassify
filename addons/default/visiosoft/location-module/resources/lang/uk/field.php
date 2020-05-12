d67d562f7c1b8bd9b937fcc9558c3a8bb1fde258
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:20:14 +0300

    New translations field.php (Ukrainian)

---
+++ addons/default/visiosoft/location-module/resources/lang/uk/field.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Назва'
+    ],
+    'slug' => [
+        'name' => 'Слизень'
+    ],
+    'city' => [
+        'name' => 'Місто'
+    ],
+    'country' => [
+        'name' => 'Країна'
+    ],
+    'district' => [
+        'name' => 'Район'
+    ],
+    'order' => [
+        'name' => 'Порядок сортування'
+    ],
+    'neighborhood' => [
+        'name' => 'Сусідство'
+    ],
+    'village' => [
+        'name' => 'Село'
+    ],
+    'description' => [
+        'name' => 'Опис'
+    ],
+    'pick_option' => [
+        'name' => 'Виберіть варіант',
+    ],
+    'address' => [
+        'name' => 'Адреса',
+    ],
+    'selected' => 'Вибрано'
+];
