345977bbcd522122517252fc9c23e386810cf085
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:04:44 +0300

    New translations button.php (Romanian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ro/button.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'post_ad' => [
+        'name' => 'Publicare',
+    ],
+    'login' => 'Autentificare',
+    'register' => 'Inregistreaza-te',
+    'continue' => 'Continua',
+    'reset_password' => 'Reseteaza parola',
+    'email' => 'E-mail',
+    'phone' => 'Telefon',
+];
