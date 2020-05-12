875ee8e5ba0dc2d0fe1122b7ad202339c556c6d6
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:14:08 +0300

    New translations stream.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/stream.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/stream.php
@@ -1,29 +1,22 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "name"      =>"ADVS",
-
-   ],
-   "categories"   =>   [
-      "name"      =>"Categorieën",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Dorp",
-
-   ],
-   "custom_fields"   =>   [
-      "name"      =>"Aangepaste velden",
-
-   ],
-   "custom_field_advs"   =>   [
-      "name"      =>"Aangepast veld advs",
-
-   ],
-   "cf_values"   =>   [
-      "name"      =>"Zie waarden",
-
-   ],
-
+return [
+    'advs' => [
+        'name' => 'ADVS',
+    ],
+    'categories' => [
+        'name' => 'Categorieën',
+    ],
+    'village' => [
+        'name' => 'Dorp',
+    ],
+    'custom_fields' => [
+        'name' => 'Aangepaste velden',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Aangepast veld advs',
+    ],
+    'cf_values' => [
+        'name' => 'Zie waarden',
+    ],
 ];
