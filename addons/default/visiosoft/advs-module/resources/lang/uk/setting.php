68e1b32aa4ede604eb11dfee8ee24c2ed8009ac1
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:21:23 +0300

    New translations setting.php (Ukrainian)

---
+++ addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'Автоматичне затвердження',
+    ],
+    'default_view_type' => [
+        'name' => 'Тип перегляду за замовчуванням',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Сховати ціну, якщо нульова',
+        'instructions' => 'Прихойте ціну, якщо ціна оголошення 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Приблизний час очікування',
+        'instructions' => 'У години',
+    ],
+    'default_published_time' => [
+        'name' => 'Час публікації за замовчуванням',
+    ],
+    'address' => [
+        'name' => 'Контактна адреса',
+    ],
+    'phone' => [
+        'name' => 'Контактний телефон',
+    ],
+    'favicon' => [
+        'name' => 'Фавікон',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Карта довгий координат',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Координатна карта Лат',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Логотип',
+    ],
+    'ogImage' => [
+        'name' => 'Соціальний логотип Share',
+        'warning'      => 'Він повинен бути при роздільній здатності 1200 x 630. ',
+
+    ],
+    'currencies' => [
+        'name' => 'Активні валюти',
+    ],
+    'latest-limit' => [
+        'name' => 'Останній ліміт',
+    ],
+    'site_address' => [
+        'name' => 'Адреса сайту',
+        'warning'      => 'Авто додано www. www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Ключ Api Google Map',
+    ],
+
+    'default_country' => [
+        'name' => 'Країна оголошення за замовчуванням',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Ключ API конвертора валют'
+    ],
+    'default_city' => [
+        'name' => 'Місто оголошення за умовчанням',
+    ],
+    'default_district' => [
+        'name' => 'Район оголошень за замовчуванням',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Район оголошення за замовчуванням',
+    ],
+    'default_currency' => [
+        'name' => 'Валюта оголошення за замовчуванням',
+    ],
+    'default_GET' => [
+        'name' => 'Оголошення за замовчуванням GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'Ширина мініатюри',
+    ],
+    'thumbnail_height' => [
+        'name' => 'Висота мініатюри',
+    ],
+    'picture_width' => [
+        'name' => 'Ширина зображення',
+    ],
+    'picture_height' => [
+        'name' => 'Висота зображення',
+    ],
+    'twitter' => [
+        'name' => 'Twitter',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Тип водяного знака',
+    ],
+    'watermark_text' => [
+        'name' => 'Текст водяного знака',
+    ],
+    'watermark_image' => [
+        'name' => 'Зображення водяного знака',
+    ],
+    'watermark_position' => [
+        'name' => 'Позиція водяного знака',
+    ],
+    'listing_page_image' => [
+        'name' => 'Зображення сторінки лістингу',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Увімкнено валюти',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Статистичний код Google',
+    ],
+    'register_email_field' => [
+        'name' => 'Вимога електронної пошти щодо реєстрації учасника',
+    ],
+];
