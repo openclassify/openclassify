49efc1d763b19798193a060a6175e97317ad9807
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:09:39 +0300

    New translations button.php (Korean)

---
+++ addons/default/visiosoft/profile-module/resources/lang/ko/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'new_profile' => '새 프로필',
+    'new_adress' => '새로운 주소',
+    'show' => '주소 표시',
+    'update_password' => '비밀번호 업데이트',
+    'update_profile' => '프로필 업데이트',
+    'delete' => '지우다',
+    'extend' => '넓히다',
+    'extend_all' => '모두 확장',
+    'edit' => '편집하다',
+    'go_profile' => '프로필 정보로 이동',
+    'go_user' => '사용자 정보로 이동',
+];
