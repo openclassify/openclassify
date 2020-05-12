8260e5a91c1af5509c522bb0b78c89c9d903350c
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:05:24 +0300

    New translations preference.php (Romanian)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ro/preference.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Bara laterală Hover',
+        'instructions' => 'Extindeți bara laterală în timpul rulării?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigare',
+        'instructions' => 'Restrângeți <em>personale</em> ordinea de navigare.',
+        'warning'      => 'Primul articol de navigație accesibil este utilizat ca zonă <strong>origine</strong>.',
+        'reorder'      => 'Trageți și fixați elementele principale de navigație în bara laterală <strong></strong> pentru a le reordona.',
+    ],
+    'display'       => [
+        'name'         => 'Densitatea afișajului',
+        'instructions' => 'Afișajul compact permite afișarea mai multă conținut simultan pe ecran.',
+        'option'       => [
+            'default' => 'Mod implicit',
+            'compact' => 'Compact',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => 'Modul Bara laterală',
+        'instructions' => 'Barele laterale statice vor fi întotdeauna vizibile.',
+        'option'       => [
+            'default' => 'Mod implicit',
+            'static'  => 'Static',
+        ],
+    ],
+];
