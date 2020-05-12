5130a9e5070c550176dbdad6bfed4d05a52e4daa
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:14:43 +0300

    New translations button.php (Hebrew)

---
+++ addons/default/visiosoft/advs-module/resources/lang/he/button.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'new_adv' => 'עו"ד חדש',
+    'new_category' => 'קטגוריה חדשה',
+    'new_country' => 'מדינה חדשה',
+    'new_city' => 'עיר חדשה',
+    'new_district' => 'מחוז חדש',
+    'new_neighborhood' => 'שכונה חדשה',
+    'new_village' => 'הכפר החדש',
+    'edit' => "ערוך",
+    'approve' => "אשר",
+    'decline' => 'דחה',
+    'extend' => 'הרחיבו',
+    'extend_all' => 'הרחב את הכל',
+    'convert_currency' => 'המרת מטבע',
+    'categories' => 'קטגוריות',
+    'sub_category' => 'תת קטגוריות',
+    'add_sub_category' => 'הוסף קטגוריית משנה',
+    'sub_cities' => 'ערי משנה',
+    'add_sub_cities' => 'הוסף תת עיר',
+    'add_sub_districts' => 'הוסף מחוז מחוז',
+    'add_sub_neighborhoods' => 'הוסף שכונת משנה',
+    'add_sub_village' => 'הוסף כפר משנה',
+    'sub_districts' => 'מחוזות משנה',
+    'sub_neighborhoods' => 'שכונות משנה',
+    'sub_village' => 'סוב כפר',
+    'general_settings' => 'הגדרות כלליות',
+    'save_category' => 'שמור קטגוריה',
+    'change_owner' => 'לשנות בעלים',
+    'update_category' => [
+        'name' => 'עדכן קטגוריה'
+    ],
+];
