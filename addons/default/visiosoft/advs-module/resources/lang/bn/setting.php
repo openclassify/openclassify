3110e5f7c25fc10c5cb480d0d717cd5e657fbe1a
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:29:35 +0300

    New translations setting.php (Bengali)

---
+++ addons/default/visiosoft/advs-module/resources/lang/bn/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'অটো অনুমোদন',
+    ],
+    'default_view_type' => [
+        'name' => 'ডিফল্ট দর্শন প্রকার',
+    ],
+    'hide_zero_price' => [
+        'name' => 'জিরো হলে দাম লুকান',
+        'instructions' => 'বিজ্ঞাপনের দাম 0 হলে দামটি লুকান',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'আনুমানিক মুলতুবি সময়',
+        'instructions' => 'ইন আওয়ারস',
+    ],
+    'default_published_time' => [
+        'name' => 'ডিফল্ট প্রকাশের সময়',
+    ],
+    'address' => [
+        'name' => 'যোগাযোগ ঠিকানা',
+    ],
+    'phone' => [
+        'name' => 'ফোনে যোগাযোগ',
+    ],
+    'favicon' => [
+        'name' => 'ফেভিকন',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'মানচিত্র দীর্ঘ সমন্বয়',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'মানচিত্র ল্যাট সমন্বয়',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN রয়েছে',
+    ],
+    'logo' => [
+        'name' => 'লোগো',
+    ],
+    'ogImage' => [
+        'name' => 'সামাজিক ভাগ লোগো',
+        'warning'      => 'এটি 1200 x 630 এর রেজোলিউশনে হওয়া উচিত। ',
+
+    ],
+    'currencies' => [
+        'name' => 'সক্রিয় মুদ্রা',
+    ],
+    'latest-limit' => [
+        'name' => 'সর্বশেষ সীমা',
+    ],
+    'site_address' => [
+        'name' => 'সাইটের ঠিকানা',
+        'warning'      => 'অটো যুক্ত www। WWW।<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'গুগল ম্যাপ এপি কী',
+    ],
+
+    'default_country' => [
+        'name' => 'ডিফল্ট বিজ্ঞাপন দেশ',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'মুদ্রা রূপান্তরকারী API কী'
+    ],
+    'default_city' => [
+        'name' => 'ডিফল্ট অ্যাড সিটি',
+    ],
+    'default_district' => [
+        'name' => 'ডিফল্ট বিজ্ঞাপন জেলা',
+    ],
+    'default_neighborhood' => [
+        'name' => 'ডিফল্ট বিজ্ঞাপন প্রতিবেশী',
+    ],
+    'default_currency' => [
+        'name' => 'ডিফল্ট বিজ্ঞাপন মুদ্রা',
+    ],
+    'default_GET' => [
+        'name' => 'ডিফল্ট বিজ্ঞাপন জিইটি',
+    ],
+    'thumbnail_width' => [
+        'name' => 'থাম্বনেইল প্রস্থ',
+    ],
+    'thumbnail_height' => [
+        'name' => 'থাম্বনেইল উচ্চতা',
+    ],
+    'picture_width' => [
+        'name' => 'চিত্র প্রস্থ',
+    ],
+    'picture_height' => [
+        'name' => 'চিত্র উচ্চতা',
+    ],
+    'twitter' => [
+        'name' => 'টুইটার',
+    ],
+    'facebook' => [
+        'name' => 'ফেসবুক',
+    ],
+    'youtube' => [
+        'name' => 'ইউটিউব',
+    ],
+    'google' => [
+        'name' => 'গুগল',
+    ],
+    'watermark_type' => [
+        'name' => 'ওয়াটারমার্ক টাইপ',
+    ],
+    'watermark_text' => [
+        'name' => 'জলছবি পাঠ্য',
+    ],
+    'watermark_image' => [
+        'name' => 'জলছবি ইমেজ',
+    ],
+    'watermark_position' => [
+        'name' => 'জলছবি অবস্থান',
+    ],
+    'listing_page_image' => [
+        'name' => 'পৃষ্ঠার চিত্র তালিকাভুক্ত',
+    ],
+    'enabled_currencies' => [
+        'name' => 'সক্ষম মুদ্রা',
+    ],
+    'google_statistic_code' => [
+        'name' => 'গুগল পরিসংখ্যান কোড',
+    ],
+    'register_email_field' => [
+        'name' => 'সদস্য নিবন্ধকরণ ইমেল প্রয়োজনীয়তা',
+    ],
+];
