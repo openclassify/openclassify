d6f62a39c39959c8c4c9384a4967c38b62fb3617
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:20:01 +0300

    New translations addon.php (Ukrainian)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/uk/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Visiosoft Admin',
+    'name'        => 'Тема адміністратора Visiosoft',
+    'description' => 'Офіційна тема адміністратора для OpenClassify.',
+];
