e5b8a58bd896f142c8b5977e73e30d3256f562ea
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:29 +0300

    New translations section.php (Chinese Traditional)

---
+++ addons/default/visiosoft/advs-module/resources/lang/zh/section.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => '廣告',
+    ],
+    'categories' => [
+        'title' => '分類目錄',
+    ],
+    'countries' => [
+        'title' => '國別',
+    ],
+    'cities' => [
+        'title' => '城市',
+    ],
+    'districts' => [
+        'title' => '地區',
+    ],
+    'neighborhoods' => [
+        'title' => '鄰里',
+    ],
+    'village' => [
+        'title' => '村',
+    ],
+    'custom_fields' => [
+        'title' => '自訂欄位',
+    ],
+    'custom_field_advs' => [
+        'title' => '自定義字段廣告',
+    ],
+    'cf_values' => [
+        'title' => 'CF值',
+    ],
+    'fields' => [
+        'title' => '自定義字段'
+    ],
+    'assets_clear' => [
+        'name' => '資產清除'
+    ],
+    'general' => '一般',
+    'ads' => '廣告',
+    'user' => '用戶',
+];
