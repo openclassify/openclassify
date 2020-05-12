46bf4ec4540d7473df72e9c8f51a79c692f86264
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:17:27 +0300

    New translations field.php (Dutch)

--- addons/default/visiosoft/base-theme/resources/lang/nl/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/nl/field.php
@@ -1,53 +1,53 @@
 <?php
 
 return [
-    'latest_ads' => 'Latest Ads',
-    'show_all' => 'Show All',
+    'latest_ads' => 'Nieuwste advertenties',
+    'show_all' => 'Toon alles',
     'login' => [
-        'name' => 'Login',
+        'name' => 'Log in',
     ],
     'logout' => [
-        'name' => 'Logout',
+        'name' => 'Uitloggen',
     ],
-    'profile' => 'Profile',
-    'email' => 'Email Address',
-    'phone' => 'Phone Number',
-    'reset_code' => 'Reset Code',
-    'password_confirmation' => 'Password Confirmation',
-    'password' => 'Password',
-    'first_name' => 'First Name',
-    'last_name' => 'Last Name',
-    'remember_me' => 'Remember Me',
-    'not_a_member_yet' => 'Not a Member Yet',
-    'not_a_member_yet_message' => 'Sign up for our members to benefit from our special service',
-    'forgot_password' => 'I Forgot My Password',
-    'email_or_phone_number' => 'Email Address or Phone Number',
-    'phone_number' => 'Phone Number',
+    'profile' => 'Profiel',
+    'email' => 'E-mailadres',
+    'phone' => 'Telefoonnummer',
+    'reset_code' => 'reset code',
+    'password_confirmation' => 'Wachtwoordbevestiging',
+    'password' => 'Wachtwoord',
+    'first_name' => 'Voornaam',
+    'last_name' => 'Achternaam',
+    'remember_me' => 'Onthoud me',
+    'not_a_member_yet' => 'Ben je nog geen lid',
+    'not_a_member_yet_message' => 'Meld u aan voor onze leden om te profiteren van onze speciale service',
+    'forgot_password' => 'Ik ben mijn wachtwoord vergeten',
+    'email_or_phone_number' => 'E-mailadres of telefoonnummer',
+    'phone_number' => 'Telefoonnummer',
 
     // Registration instructions
-    'personal_registration_header' => 'What Are Individual Membership Advantages?',
-    'personal_registration_body' => 'Sell and rent your home, car, sell your unused items, get new ones.',
-    'personal_registration_list_1' => 'Post a free ad,',
-    'personal_registration_list_2' => 'Add the ads you are interested in, follow the price changes after adding them to your favorites, create favorite searches that meet your criteria,',
-    'personal_registration_list_3' => 'Send messages to the ad owners on the site.',
-    'register_information_note' => 'The information on this page is taken for ' . env('APPLICATION_DOMAIN') . ' membership. You can find detailed information about the protection of personal data here.',
+    'personal_registration_header' => 'Wat zijn individuele lidmaatschapsvoordelen?',
+    'personal_registration_body' => 'Verkoop en huur uw huis, auto, verkoop uw ongebruikte items, koop nieuwe.',
+    'personal_registration_list_1' => 'Plaats een gratis advertentie,',
+    'personal_registration_list_2' => 'Voeg de advertenties toe waarin u geïnteresseerd bent, volg de prijswijzigingen nadat u ze aan uw favorieten heeft toegevoegd, maak favoriete zoekopdrachten die aan uw criteria voldoen,',
+    'personal_registration_list_3' => 'Stuur berichten naar de advertentie-eigenaren op de site.',
+    'register_information_note' => 'De informatie op deze pagina is bedoeld voor ' . env('APPLICATION_DOMAIN') . ' lidmaatschap. Hier vindt u gedetailleerde informatie over de bescherming van persoonsgegevens.',
 
     // Register page
-    'phone_validation_error' => 'This phone number is in use by another member.',
+    'phone_validation_error' => 'Dit telefoonnummer wordt gebruikt door een ander lid.',
 
     // Forgot Password
-    'create_new_password' => 'Create New Password',
+    'create_new_password' => 'Maak een nieuw wachtwoord aan',
 
     // Login page
-    'or' => 'or',
-    'login_with_phone_number' => 'Login with Phone Number',
-    'login_with_email_address' => 'Login with Email Address',
+    'or' => 'of',
+    'login_with_phone_number' => 'Login met telefoonnummer',
+    'login_with_email_address' => 'Login met e-mailadres',
 
     // Side menu links
-    'company_directory' => 'Company directory',
-    'popular_ads' => 'Popular Ads',
-    'last_48_hours' => 'Last 48 Hours',
-    'secure_e-commerce_ads' => 'Secure e-Commerce Ads',
-    'sms' => 'Send Text Message (SMS)',
-    'mail' => 'Send Mail',
+    'company_directory' => 'Bedrijfsadresboek',
+    'popular_ads' => 'Populaire advertenties',
+    'last_48_hours' => 'Afgelopen 48 uur',
+    'secure_e-commerce_ads' => 'Veilige e-commerce-advertenties',
+    'sms' => 'Sms verzenden',
+    'mail' => 'Verzend mail',
 ];
