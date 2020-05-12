10872c84f1b97f34a8746224f0ff89317f4de36a
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:10:03 +0300

    New translations field.php (Polish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/pl/field.php
@@ -0,0 +1,291 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Tytuł'
+    ],
+    'order' => [
+        'name' => 'Zamówienie'
+    ],
+    'slug' => [
+        'name' => 'Ślimak'
+    ],
+    'parent_category' => [
+        'name' => 'Rodzic'
+    ],
+    'category' => [
+        'name' => 'Tytuł kategorii'
+    ],
+    'price' => [
+        'name' => 'Cena £'
+    ],
+    'date' => [
+        'name' => 'Data'
+    ],
+    'photo' => [
+        'name' => 'Zdjęcie'
+    ],
+    'video' => [
+        'name' => 'Wideo'
+    ],
+    'map' => [
+        'name' => 'Mapa'
+    ],
+    'stock' => [
+        'name' => 'Zbiory'
+    ],
+    'advs_desc' => [
+        'name' => 'Opis'
+    ],
+    'online_payment' => [
+        'name' => 'Płatność online'
+    ],
+    'city' => [
+        'name' => 'Miasto'
+    ],
+    'country' => [
+        'name' => 'Kraj'
+    ],
+    'district' => [
+        'name' => 'Dzielnica'
+    ],
+    'neighborhood' => [
+        'name' => 'Sąsiedztwo'
+    ],
+    'village' => [
+        'name' => 'Wioska'
+    ],
+    'files' => [
+        'name' => 'Obrazy'
+    ],
+    'cat1' => [
+        'name' => 'główna kategoria'
+    ],
+    'cat2' => [
+        'name' => 'Podkategoria 1'
+    ],
+    'cat3' => [
+        'name' => 'Podkategoria 2'
+    ],
+    'cat4' => [
+        'name' => 'Podkategoria 3'
+    ],
+    'cat5' => [
+        'name' => 'Podkategoria 4'
+    ],
+    'cat6' => [
+        'name' => 'Podkategoria 5'
+    ],
+    'cat7' => [
+        'name' => 'Podkategoria 6'
+    ],
+    'currency' => [
+        'name' => 'Waluta'
+    ],
+    'status' => [
+        'name' => 'Status',
+        'option' => [
+            'approved' => 'Zatwierdzony',
+            'expired' => 'Przedawniony',
+            'unpublished' => 'Niepublikowane',
+            'pending_admin' => 'W oczekiwaniu na administratora',
+            'pending_user' => 'Oczekujący użytkownik'
+        ]
+    ],
+    'pending_adv' => [
+        'name' => 'Niepublikowane'
+    ],
+    'finish_at' => [
+        'name' => 'Zakończ o'
+    ],
+    'cover_photo' => [
+        'name' => 'Zdjęcie na okładce'
+    ],
+    'favs_adv' => [
+        'name' => 'Ulubione reklamy'
+    ],
+    'my_adv' => [
+        'name' => 'ADVS'
+    ],
+    'description' =>  'Opis',
+    'explanation' =>  'Wyjaśnienie',
+    'type' => [
+        'name' => 'Rodzaj'
+    ],
+    'parent_adv' => [
+        'name' => 'Powiązana reklama'
+    ],
+    'value' => [
+        'name' => 'Wartość'
+    ],
+    'list' => 'Lista',
+    'home' => 'Dom',
+    'list-page' => 'Strona listy',
+    'search' => 'Szukaj',
+    'recommended' => 'Polecane reklamy dla Ciebie',
+    'filter' => 'Filtr',
+    'sort' => 'Sortować',
+    'sort_by' => 'Sortuj według',
+    'pick_option' => 'Wybierz opcję',
+    'pick_ordering' => 'Zamawianie',
+    'price_high' => 'Cena od najwyższej do najniższej',
+    'price_low' => 'Cena od niskiej do wysokiej',
+    'newest' => 'Najnowszy',
+    'oldest' => 'Najstarszy',
+    'address_a_z' => 'Adres (od A do Z)',
+    'address_z_a' => 'Adres (od Z do A)',
+    'categories' => 'Kategorie',
+    'all_categories' => 'Wszystkie kategorie',
+    'location' => 'Lokalizacja',
+    'send_message' => 'Wyślij wiadomość',
+    'phone_gsm' => 'Telefon GSM',
+    'phone_office' => 'Telefon biurowy',
+    'add_to_favorites' => "Dodaj do ulubionych",
+    'send' => 'Wysłać',
+    'search_name' => 'Wyszukaj nazwę',
+    'my_favorite_searches' => 'Ulubione wyszukiwania',
+    'search_desc' => 'Tutaj możesz dodawać ulubione opcje wyszukiwania i zarządzać nimi.',
+    'complaint_placeholder' => 'Wpisz tutaj szczegóły swojej skargi.',
+    'show_details' => 'Pokaż szczegóły',
+    'ad_actions' => 'Działania reklamowe',
+    'edit' => 'Edytować',
+    'approve' => 'Zatwierdzać',
+    'passive' => "Bierny",
+    'offered_by' => 'Oferowane przez',
+    'more_ads_by' => 'Więcej reklam autorstwa',
+    'click_phone' => 'Pokaż numer telefonu',
+    'share' => 'Dzielić',
+    'report' => 'Zgłoś tę reklamę',
+    'features' => 'funkcje',
+    "streetview" => "widok ulicy",
+    'recommended_ads' => 'Polecane reklamy dla Ciebie',
+    'new' => 'Nowy',
+    'short_info' => 'Krótka informacja',
+    'secure_trading' => 'Bezpieczny handel',
+    '24/7' => 'Wsparcie 24/7',
+    'easy_trading' => 'Łatwy handel',
+    'need_help' => 'Potrzebuję pomocy?',
+    'give_a_call' => 'Zadzwoń',
+    'message_sent' => 'Wiadomość wysłana!',
+    'asap' => 'Wiadomość wysłana, sprzedawca skontaktuje się z Tobą jak najszybciej.',
+    'ad_id' => 'Identyfikator reklamy',
+    'contact_with' => 'Kontakt z',
+    'create_adv' => 'Utwórz reklamę',
+    'edit_adv' => 'Edytuj reklamę',
+    'mandatory_fields' => 'Pola obowiązkowe',
+    'ad_post' => 'Ogłoszenie',
+    'sell_item' => 'Sprzedaj przedmiot lub usługę',
+    'search_save' => 'Ulubione wyszukiwanie zostało zapisane',
+    'address' => 'Adres',
+    'email' => 'E-mail',
+    'phone' => 'Telefon',
+    'send_us_your_feedback' => 'Wyślij nam swoją opinię',
+    'online_shopping' => 'DOSTAĆ',
+    'approved' => 'Zatwierdzono pomyślnie',
+    'extended' => 'Pomyślnie przedłużony',
+    'declined' => 'Odrzucono pomyślnie',
+    'search_on_map' => 'Szukaj na mapie',
+    'post_free_ad' => 'Opublikuj bezpłatną reklamę',
+    'select_a_category' => 'Wybierz kategorię',
+    'select_a_sub_category' => 'Wybierz podkategorię',
+    'all_dopings_added' => 'Wszystkie typy dopingu są dodawane do tej reklamy.',
+    'is_get_adv' => [
+        'name' => 'DOSTAĆ'
+    ],
+    'corporate_info' => 'Informacje o firmie',
+    'featured_ads' => "Polecane reklamy",
+    'trending_ads' => 'Trendind Ads',
+    'search_ads_result_message' => [
+        'name' => ': liczba znalezionych reklam dla: wyszukiwania nazw',
+    ],
+    'total' => 'Całkowity',
+    'quantity' => 'Ilość',
+    'view_cart' => 'Zobacz Koszyk',
+    'no_ads' => 'Nie znaleziono reklam.',
+    'show_display_mode' => [
+        'map' => 'Mapa',
+        'list' => 'Lista',
+    ],
+    'advs_list_table_thead' => [
+        'name' => 'Nazwa reklamy',
+        'price' => 'Cena £',
+        'publish_at' => 'Data ogłoszenia',
+        'location_city' => 'Miasto',
+        'location_country' => 'Kraj',
+    ],
+    'show_list_view' => [
+        'name' => 'Lista',
+    ],
+    'show_table_view' => [
+        'name' => 'Stół',
+    ],
+    'showcase' => [
+        'name' => 'Gablota',
+    ],
+    'new_ad' => [
+        'name' => 'Nowa reklama',
+    ],
+    'profile' => [
+        'name' => 'Profil',
+    ],
+    'my_ads' => [
+        'name' => 'Moje reklamy',
+    ],
+    'sold_out' => [
+        'name' => 'Wyprzedane',
+    ],
+    'edit_category' => 'Edytuj kategorię',
+    'set_category' => 'Ustaw kategorię',
+    'max' => [
+        'name' => 'Max',
+    ],
+    'min' => [
+        'name' => 'Min',
+    ],
+    'in_the_last_24_hours' => [
+        'name' => 'W ciągu ostatnich 24 godzin'
+    ],
+    'in_the_last_week' => [
+        'name' => 'W ostatnim tygodniu'
+    ],
+    'in_the_last_month' => [
+        'name' => 'W ostatnim miesiącu'
+    ],
+    'ads_with_photo' => [
+        'name' => 'Reklamy ze zdjęciem'
+    ],
+    'ads_with_video' => [
+        'name' => 'Reklamy z wideo'
+    ],
+    'yes' => [
+        'name' => 'tak'
+    ],
+    "no_location" => "Nie wybrano lokalizacji.",
+    "continue" => 'Kontyntynuj',
+    "gallery" => 'Galeria',
+
+    // Pending screen
+    'pending_message_1' => 'Reklama jest w trakcie procesu zatwierdzania',
+    'pending_message_2' => "Data aktualizacji reklamy",
+    'pending_message_3' => 'Szacowany czas oczekiwania',
+    'pending_message_4' => 'Czas spędzony w procesie zatwierdzania zostanie dodany do czasu dopingu.',
+    'pending_message_5' => 'Z centrum telefonicznego nie można dokonywać transakcji dotyczących reklamy w procesie zatwierdzania.',
+    'hours' => 'godziny',
+
+    // Preview page
+    'preview_text' => [
+        'part_1' => 'Jeśli poniższe informacje o reklamie są prawidłowe, kliknij przycisk „Kontynuuj” i przejdź do następnego kroku.',
+        'part_2' => 'Jeśli nie, kliknij przycisk „Edytuj”.',
+    ],
+    'views' => 'Wyświetlenia',
+
+    // Detail page
+    "no_street_view" => "Brak obrazu widoku ulicy w tej lokalizacji",
+    'edit_my_contact_info' => 'Edytuj moje dane kontaktowe',
+    'update_my_contact_info' => 'zaktualizuj moje dane kontaktowe',
+    'contact_info' => 'Informacje kontaktowe',
+    'ad_info' => 'Informacje o reklamie',
+
+    // Select cat
+    'select_category_step_by_step' => 'Wybierz kategorię krok po kroku',
+];
