d52dd657897c80d25f6a995cd00715e445988e95
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:08:12 +0300

    New translations field.php (Spanish)

--- addons/default/visiosoft/location-module/resources/lang/es/field.php
+++ addons/default/visiosoft/location-module/resources/lang/es/field.php
@@ -1,41 +1,38 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Título",
-
-   ],
-   "slug"   =>   [
-      "name"      =>"Babosa",
-
-   ],
-   "city"   =>   [
-      "name"      =>"Ciudad",
-
-   ],
-   "country"   =>   [
-      "name"      =>"País",
-
-   ],
-   "district"   =>   [
-      "name"      =>"Distrito",
-
-   ],
-   "order"   =>   [
-      "name"      =>"Orden de clasificación",
-
-   ],
-   "neighborhood"   =>   [
-      "name"      =>"Barrio",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Pueblo",
-
-   ],
-   "description"   =>   [
-      "name"      =>"Descripción",
-
-   ],
-
+return [
+    'name' => [
+        'name' => 'Título'
+    ],
+    'slug' => [
+        'name' => 'Babosa'
+    ],
+    'city' => [
+        'name' => 'Ciudad'
+    ],
+    'country' => [
+        'name' => 'País'
+    ],
+    'district' => [
+        'name' => 'Distrito'
+    ],
+    'order' => [
+        'name' => 'Orden de clasificación'
+    ],
+    'neighborhood' => [
+        'name' => 'Barrio'
+    ],
+    'village' => [
+        'name' => 'Pueblo'
+    ],
+    'description' => [
+        'name' => 'Descripción'
+    ],
+    'pick_option' => [
+        'name' => 'Pick an option',
+    ],
+    'address' => [
+        'name' => 'Address',
+    ],
+    'selected' => 'Selected'
 ];
