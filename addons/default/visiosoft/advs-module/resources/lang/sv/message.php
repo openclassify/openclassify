a62adef58c715e3e26422adbc35a27adb2ecfd02
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:18:55 +0300

    New translations message.php (Swedish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/sv/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'error_added_cart' => 'Den begärda produkten saknas eller är inaktiv för närvarande',
+    'please_payment' => 'Var snäll och betala',
+    'bank_account_info' => 'Bankkontoinformation',
+    'agreement' => 'Genom att klicka på Post godkänner du våra användarvillkor och integritetspolicy att du erkänner att du är den rättmätiga ägaren av denna artikel.',
+    'update_category_msg' => 'Den klassificerade kategorin kommer att uppdateras.',
+    'updated_category_msg' => 'Kategori har redigerats.',
+    'create_ad_with_post_cat' => [
+        'title' => "Val av kategori har slutförts.",
+        'success' => true,
+        'continueBtn' => "Fortsätta",
+    ],
+    'update_category_info' => 'Vänligen uppdatera kategorin.',
+    'filter_by_search_msg' => 'Filtrera efter sökning.',
+    'delete_author_error' => 'Du kan inte ta bort ett inlägg som inte tillhör dig',
+    'success_delete' => 'Annons raderad',
+    'pending_ad_status' => 'Din annons väntar på godkännande av redaktören.',
+    'extended' => ': nummerannons (er) har utökats.',
+    'category_not_exist' => 'Denna kategori finns inte längre!',
+    'currency_converted' => 'Valutakonvertering framgångsrikt.',
+    'choose_user' => 'Välj en användare',
+    'owner_updated_successfully' => 'Ägaren har uppdaterats framgångsrikt!',
+];
