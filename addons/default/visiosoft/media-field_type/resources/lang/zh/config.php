aa72a637214707cf86ef84ab3163b4f94336c78b
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:27:17 +0300

    New translations config.php (Chinese Simplified)

--- addons/default/visiosoft/media-field_type/resources/lang/zh/config.php
+++ addons/default/visiosoft/media-field_type/resources/lang/zh/config.php
@@ -2,25 +2,25 @@
 
 return [
     'folders' => [
-        'name'         => '資料夾',
-        'instructions' => '指定可用於此字段的文件夾。保留空白以顯示所有文件夾。',
-        'warning'      => '現有文件夾權限優先於所選文件夾。',
+        'name'         => '资料夹',
+        'instructions' => '指定可用于此字段的文件夹。保留空白以显示所有文件夹。',
+        'warning'      => '现有文件夹权限优先于所选文件夹。',
     ],
     'min'     => [
-        'label'        => '最少選擇',
-        'instructions' => '輸入允許的選擇的最小數量。',
+        'label'        => '最少选择',
+        'instructions' => '输入允许的选择的最小数量。',
     ],
     'max'     => [
-        'label'        => '最大選擇',
-        'instructions' => '輸入允許的最大選擇數。',
+        'label'        => '最大选择',
+        'instructions' => '输入允许的最大选择数。',
     ],
     'mode'    => [
-        'name'         => '輸入模式',
-        'instructions' => '用戶應如何提供文件輸入？',
+        'name'         => '输入模式',
+        'instructions' => '用户应如何提供文件输入？',
         'option'       => [
-            'default' => '上傳和/或選擇文件。',
-            'select'  => '僅選擇文件。',
-            'upload'  => '僅上傳文件。',
+            'default' => '上传和/或选择文件。',
+            'select'  => '仅选择文件。',
+            'upload'  => '仅上传文件。',
         ],
     ],
 ];
