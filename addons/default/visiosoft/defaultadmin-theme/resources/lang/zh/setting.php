7f2d6a3c405605469ec9a8d40de0e690636b9950
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:19 +0300

    New translations setting.php (Chinese Traditional)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/zh/setting.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => '邊欄懸停',
+        'instructions' => '展開懸停邊欄？',
+    ],
+    'navigation'    => [
+        'name'         => '導航',
+        'instructions' => '指定 <em>默認</em> 導航順序。',
+        'warning'      => '第一個可訪問的導航項目用作 <strong>home</strong> 區域。',
+        'reorder'      => '將主要導航項目拖放到 <strong>側欄</strong> 以對其重新排序。',
+    ],
+];
