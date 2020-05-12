4aad7e2b5ff5de497b3a45b2d3bb9319380be966
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:26:16 +0300

    New translations message.php (Chinese Traditional)

---
+++ addons/default/visiosoft/advs-module/resources/lang/zh/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'error_added_cart' => '請求的產品當前缺失或無效',
+    'please_payment' => '請付錢',
+    'bank_account_info' => '銀行賬戶信息',
+    'agreement' => '單擊“發布”，即表示您同意我們的使用條款和隱私政策，即表示您是此項目的合法所有者。',
+    'update_category_msg' => '分類類別將被更新。',
+    'updated_category_msg' => '類別已成功編輯。',
+    'create_ad_with_post_cat' => [
+        'title' => "類別選擇已完成。",
+        'success' => true,
+        'continueBtn' => "繼續",
+    ],
+    'update_category_info' => '請更新類別。',
+    'filter_by_search_msg' => '按搜索過濾。',
+    'delete_author_error' => '您不能刪除不屬於您的信息',
+    'success_delete' => '廣告已刪除',
+    'pending_ad_status' => '您的廣告有待編輯批准。',
+    'extended' => '：number廣告已成功擴展。',
+    'category_not_exist' => '該類別不再存在！',
+    'currency_converted' => '貨幣已成功轉換。',
+    'choose_user' => '選擇一個用戶',
+    'owner_updated_successfully' => '所有者已成功更新！',
+];
