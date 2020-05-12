cda418512a660d95219ee793a8524f8a1b693765
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:59 +0300

    New translations field.php (Chinese Traditional)

---
+++ addons/default/visiosoft/base-theme/resources/lang/zh/field.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+    'latest_ads' => '最新廣告',
+    'show_all' => '顯示所有',
+    'login' => [
+        'name' => '登錄',
+    ],
+    'logout' => [
+        'name' => '登出',
+    ],
+    'profile' => '輪廓',
+    'email' => '電子郵件地址',
+    'phone' => '電話號碼',
+    'reset_code' => '重設密碼',
+    'password_confirmation' => '確認密碼',
+    'password' => '密碼',
+    'first_name' => '名字',
+    'last_name' => '姓',
+    'remember_me' => '記住賬號',
+    'not_a_member_yet' => '還不是會員',
+    'not_a_member_yet_message' => '註冊我們的會員以享受我們的特殊服務',
+    'forgot_password' => '我忘記了我的密碼',
+    'email_or_phone_number' => '電子郵件地址或電話號碼',
+    'phone_number' => '電話號碼',
+
+    // Registration instructions
+    'personal_registration_header' => '個人會員優勢是什麼？',
+    'personal_registration_body' => '買賣房屋，汽車，出售未使用的物品，購買新物品。',
+    'personal_registration_list_1' => '發布免費廣告，',
+    'personal_registration_list_2' => '添加您感興趣的廣告，將價格添加到收藏夾之後，跟隨價格變化，創建符合條件的收藏夾搜索，',
+    'personal_registration_list_3' => '向網站上的廣告所有者發送消息。',
+    'register_information_note' => '此頁面上的信息用於 ' . env('APPLICATION_DOMAIN') . ' 成員資格。您可以在此處找到有關個人數據保護的詳細信息。',
+
+    // Register page
+    'phone_validation_error' => '該電話號碼正在由另一個成員使用。',
+
+    // Forgot Password
+    'create_new_password' => '創建新密碼',
+
+    // Login page
+    'or' => '要么',
+    'login_with_phone_number' => '用電話號碼登錄',
+    'login_with_email_address' => '用電子郵件地址登錄',
+
+    // Side menu links
+    'company_directory' => '公司目錄',
+    'popular_ads' => '熱門廣告',
+    'last_48_hours' => '最近48小時',
+    'secure_e-commerce_ads' => '安全的電子商務廣告',
+    'sms' => '發送短信（SMS）',
+    'mail' => '發送郵件',
+];
