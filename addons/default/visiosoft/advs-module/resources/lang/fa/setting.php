df9d1779ce2aaec81f0b5679cf18874609677b00
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:12:52 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
@@ -1,136 +1,130 @@
 <?php
 
- return[
-   "auto_approve"   =>   [
-      "name"      =>"تأیید خودکار",
+return [
+    'auto_approve' => [
+        'name' => 'تأیید خودکار',
+    ],
+    'default_view_type' => [
+        'name' => 'Default View Type',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Hide Price if Zero',
+        'instructions' => 'Hide the price if the ad price is 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Estimated Pending Time',
+        'instructions' => 'In Hours',
+    ],
+    'default_published_time' => [
+        'name' => 'زمان انتشار پیش فرض',
+    ],
+    'address' => [
+        'name' => 'با آدرس تماس بگیرید',
+    ],
+    'phone' => [
+        'name' => 'تماس با تلفن',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'نقشه مختصات طولانی',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'نقشه مختصات Lat',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'آرم',
+    ],
+    'ogImage' => [
+        'name' => 'آرم اشتراک گذاری اجتماعی',
+        'warning'      => 'باید در رزولوشن 1200 x 630 باشد.',
 
-   ],
-   "default_published_time"   =>   [
-      "name"      =>"زمان انتشار پیش فرض",
+    ],
+    'currencies' => [
+        'name' => 'ارزهای فعال',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'آدرس سایت',
+        'warning'      => 'خودکار اضافه شد www. www <strong>sitename.com</strong>',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Key',
+    ],
 
-   ],
-   "default_adv_limit"   =>   [
-      "name"      =>"محدودیت تبلیغات پیش فرض",
-
-   ],
-   "address"   =>   [
-      "name"      =>"با آدرس تماس بگیرید",
-
-   ],
-   "phone"   =>   [
-      "name"      =>"تماس با تلفن",
-
-   ],
-   "contact_mail"   =>   [
-      "name"      =>"تماس با ایمیل",
-      "warning"      =>"خودکار <strong>اطلاعات نام</strong> مکانsitename را اضافه کرد",
-
-   ],
-   "map_coordinates_long"   =>   [
-      "name"      =>"نقشه مختصات طولانی",
-
-   ],
-   "map_coordinates_lat"   =>   [
-      "name"      =>"نقشه مختصات Lat",
-
-   ],
-   "logo"   =>   [
-      "name"      =>"آرم",
-
-   ],
-   "ogImage"   =>   [
-      "name"      =>"آرم اشتراک گذاری اجتماعی",
-      "warning"      =>"باید در رزولوشن 1200 x 630 باشد.",
-
-   ],
-   "currencies"   =>   [
-      "name"      =>"ارزهای فعال",
-
-   ],
-   "site_address"   =>   [
-      "name"      =>"آدرس سایت",
-      "warning"      =>"خودکار اضافه شد www. www <strong>sitename.com</strong>",
-
-   ],
-   "google_map_key"   =>   [
-      "name"      =>"Google Map Api Key",
-
-   ],
-   "default_country"   =>   [
-      "name"      =>"کشور پیش فرض آگهی",
-
-   ],
-   "free_currencyconverterapi_key"   =>   [
-      "name"      =>"کلید API مبدل ارز",
-
-   ],
-   "default_city"   =>   [
-      "name"      =>"پیش فرض آگهی شهر",
-
-   ],
-   "default_district"   =>   [
-      "name"      =>"منطقه پیش فرض آگهی",
-
-   ],
-   "default_neighborhood"   =>   [
-      "name"      =>"محله پیش فرض آگهی",
-
-   ],
-   "default_currency"   =>   [
-      "name"      =>"پیش فرض آگهی ارز",
-
-   ],
-   "default_GET"   =>   [
-      "name"      =>"تبلیغات پیش فرض GET",
-
-   ],
-   "twitter"   =>   [
-      "name"      =>"توییتر",
-
-   ],
-   "facebook"   =>   [
-      "name"      =>"فیس بوک",
-
-   ],
-   "youtube"   =>   [
-      "name"      =>"یوتیوب",
-
-   ],
-   "google"   =>   [
-      "name"      =>"گوگل",
-
-   ],
-   "watermark_type"   =>   [
-      "name"      =>"نوع علامت",
-
-   ],
-   "watermark_text"   =>   [
-      "name"      =>"متن علامت",
-
-   ],
-   "watermark_image"   =>   [
-      "name"      =>"تصویر علامت",
-
-   ],
-   "watermark_position"   =>   [
-      "name"      =>"موقعیت علامت سفید",
-
-   ],
-   "watermark_opacity"   =>   [
-      "name"      =>"کدورت علامت سفید",
-
-   ],
-   "listing_page_image"   =>   [
-      "name"      =>"لیست صفحه تصویر",
-
-   ],
-   "enabled_currencies"   =>   [
-      "name"      =>"ارزهای فعال شده",
-
-   ],
-   "google_statistic_code"   =>   [
-      "name"      =>"کد آماری Google",
-
-   ],
-
+    'default_country' => [
+        'name' => 'کشور پیش فرض آگهی',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'کلید API مبدل ارز'
+    ],
+    'default_city' => [
+        'name' => 'پیش فرض آگهی شهر',
+    ],
+    'default_district' => [
+        'name' => 'منطقه پیش فرض آگهی',
+    ],
+    'default_neighborhood' => [
+        'name' => 'محله پیش فرض آگهی',
+    ],
+    'default_currency' => [
+        'name' => 'پیش فرض آگهی ارز',
+    ],
+    'default_GET' => [
+        'name' => 'تبلیغات پیش فرض GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'Thumbnail Width',
+    ],
+    'thumbnail_height' => [
+        'name' => 'Thumbnail Height',
+    ],
+    'picture_width' => [
+        'name' => 'Picture Width',
+    ],
+    'picture_height' => [
+        'name' => 'Picture Height',
+    ],
+    'twitter' => [
+        'name' => 'توییتر',
+    ],
+    'facebook' => [
+        'name' => 'فیس بوک',
+    ],
+    'youtube' => [
+        'name' => 'یوتیوب',
+    ],
+    'google' => [
+        'name' => 'گوگل',
+    ],
+    'watermark_type' => [
+        'name' => 'نوع علامت',
+    ],
+    'watermark_text' => [
+        'name' => 'متن علامت',
+    ],
+    'watermark_image' => [
+        'name' => 'تصویر علامت',
+    ],
+    'watermark_position' => [
+        'name' => 'موقعیت علامت سفید',
+    ],
+    'listing_page_image' => [
+        'name' => 'لیست صفحه تصویر',
+    ],
+    'enabled_currencies' => [
+        'name' => 'ارزهای فعال شده',
+    ],
+    'google_statistic_code' => [
+        'name' => 'کد آماری Google',
+    ],
+    'register_email_field' => [
+        'name' => 'Member Registration Email Requirement',
+    ],
 ];
