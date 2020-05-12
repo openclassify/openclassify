3b26a0c4fa347470fb5068925b67b7c0bb9e94ce
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:21:21 +0300

    New translations stream.php (Ukrainian)

---
+++ addons/default/visiosoft/advs-module/resources/lang/uk/stream.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => 'Оголошення',
+    ],
+    'categories' => [
+        'name' => 'Категорії',
+    ],
+    'village' => [
+        'name' => 'Село',
+    ],
+    'custom_fields' => [
+        'name' => 'Спеціальні поля',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Спеціальні рекламні поля',
+    ],
+    'cf_values' => [
+        'name' => 'Значення Cf',
+    ],
+];
