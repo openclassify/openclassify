68d5a100214d906bef5ec128f1daa7f34b5f6115
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:16 +0300

    New translations help.php (Chinese Traditional)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/zh/help.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'                     => '有用的資源',
+    'description'               => '使用這些有用的資源查找幫助並發現功能。',
+    'documentation_link'        => 'OpenClassify文檔',
+    'documentation_description' => '查找有關OpenClassify，開發附加組件以及也使用附加組件的文檔。',
+    'slack_link'                => '鬆弛隊',
+    'slack_description'         => '查找其他OpenClassify用戶和開發人員並與之通信。',
+    'forum_link'                => '討論區',
+    'forum_description'         => '查找答案並發布有關使用OpenClassify進行開發的問題。',
+    'addons_link'               => '可用插件',
+    'addons_description'        => '發現可用於OpenClassify的插件。',
+];
