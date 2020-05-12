f110dd7c6f0ce4921d7dfb2dab4ef07e8d370b63
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:28 +0300

    New translations button.php (Chinese Traditional)

---
+++ addons/default/visiosoft/profile-module/resources/lang/zh/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'new_profile' => '新的配置文件',
+    'new_adress' => '新地址',
+    'show' => '顯示地址',
+    'update_password' => '更新密碼',
+    'update_profile' => '更新個人信息',
+    'delete' => '刪除',
+    'extend' => '延伸',
+    'extend_all' => '全部延伸',
+    'edit' => '編輯',
+    'go_profile' => '轉到個人資料詳細信息',
+    'go_user' => '轉到用戶詳細信息',
+];
