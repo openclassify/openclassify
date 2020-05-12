09c013e9f6162b5b267f9f01e745d415f139bddf
Fatih Alp <mail@fatihalp.com>
2020-05-12 12:47:19 +0300

    New translations message.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/message.php
@@ -7,11 +7,6 @@ return [
     'agreement' => 'Durch Klicken auf "Posten" erklären Sie sich mit unseren Nutzungsbedingungen einverstanden und bestätigen, dass Sie der rechtmäßige Eigentümer dieses Artikels sind.',
     'update_category_msg' => 'Die klassifizierte Kategorie wird aktualisiert.',
     'updated_category_msg' => 'Kategorie erfolgreich bearbeitet.',
-    'max_ad_limit' => [
-        'title' => "Ihr kostenloses Anzeigenlimit ist abgelaufen.",
-        'continueBtn' => "Anzeigen anzeigen",
-        'link' => "/Profil",
-    ],
     'create_ad_with_post_cat' => [
         'title' => "Die Kategorieauswahl wurde abgeschlossen.",
         'success' => true,
