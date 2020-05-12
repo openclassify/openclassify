2e81231148691fc57f2c81a3f5f392747b978a34
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:04:36 +0300

    New translations button.php (Romanian)

---
+++ addons/default/visiosoft/cats-module/resources/lang/ro/button.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'new_category' => 'Categorie noua',
+    'add_sub_category' => 'Adăugați subcategorie',
+    'sub_category' => 'Arată subcategorie',
+    'new_placeholderforsearch' => 'Nouă locație de căutare',
+];
