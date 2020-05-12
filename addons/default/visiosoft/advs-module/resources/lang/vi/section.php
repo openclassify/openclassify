be4349aea7259420ff81f2dbf675d5b4c949320a
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:25:11 +0300

    New translations section.php (Vietnamese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/vi/section.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => 'Quảng cáo',
+    ],
+    'categories' => [
+        'title' => 'Thể loại',
+    ],
+    'countries' => [
+        'title' => 'Quốc gia',
+    ],
+    'cities' => [
+        'title' => 'Các thành phố',
+    ],
+    'districts' => [
+        'title' => 'Quận',
+    ],
+    'neighborhoods' => [
+        'title' => 'Vùng lân cận',
+    ],
+    'village' => [
+        'title' => 'Làng',
+    ],
+    'custom_fields' => [
+        'title' => 'Trường tùy chỉnh',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Quảng cáo trường tùy chỉnh',
+    ],
+    'cf_values' => [
+        'title' => 'Giá trị Cf',
+    ],
+    'fields' => [
+        'title' => 'Trường tùy chỉnh'
+    ],
+    'assets_clear' => [
+        'name' => 'Tài sản rõ ràng'
+    ],
+    'general' => 'Chung',
+    'ads' => 'Quảng cáo',
+    'user' => 'Người dùng',
+];
