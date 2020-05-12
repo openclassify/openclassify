ea61cd94bda34671a9b322989c8fb71a5bc4ceb8
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:10:10 +0300

    New translations setting.php (Polish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/pl/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'Automatyczne zatwierdzanie',
+    ],
+    'default_view_type' => [
+        'name' => 'Domyślny typ widoku',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Ukryj cenę, jeśli zero',
+        'instructions' => 'Ukryj cenę, jeśli cena reklamy wynosi 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Szacowany czas oczekiwania',
+        'instructions' => 'W godzinach',
+    ],
+    'default_published_time' => [
+        'name' => 'Domyślny czas publikacji',
+    ],
+    'address' => [
+        'name' => 'Skontaktuj się z adresem',
+    ],
+    'phone' => [
+        'name' => 'telefon kontaktowy',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Współrzędna długa mapy',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Współrzędna mapy Lat',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Logo akcji społecznościowych',
+        'warning'      => 'Powinien mieć rozdzielczość 1200 x 630. ',
+
+    ],
+    'currencies' => [
+        'name' => 'Aktywne waluty',
+    ],
+    'latest-limit' => [
+        'name' => 'Ostatni limit',
+    ],
+    'site_address' => [
+        'name' => 'Adres miejsca',
+        'warning'      => 'Dodano auto www. www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Key',
+    ],
+
+    'default_country' => [
+        'name' => 'Domyślny kraj reklamy',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Klucz API przelicznika walut'
+    ],
+    'default_city' => [
+        'name' => 'Domyślne miasto reklamy',
+    ],
+    'default_district' => [
+        'name' => 'Domyślny okręg reklamowy',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Domyślne sąsiedztwo reklam',
+    ],
+    'default_currency' => [
+        'name' => 'Domyślna waluta reklamy',
+    ],
+    'default_GET' => [
+        'name' => 'Domyślna reklama GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'Szerokość miniatury',
+    ],
+    'thumbnail_height' => [
+        'name' => 'Wysokość miniatury',
+    ],
+    'picture_width' => [
+        'name' => 'Szerokość obrazu',
+    ],
+    'picture_height' => [
+        'name' => 'Wysokość obrazu',
+    ],
+    'twitter' => [
+        'name' => 'Świergot',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Rodzaj znaku wodnego',
+    ],
+    'watermark_text' => [
+        'name' => 'Tekst znaku wodnego',
+    ],
+    'watermark_image' => [
+        'name' => 'Obraz znaku wodnego',
+    ],
+    'watermark_position' => [
+        'name' => 'Pozycja znaku wodnego',
+    ],
+    'listing_page_image' => [
+        'name' => 'Obraz strony z listą',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Włączone waluty',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Kod statystyczny Google',
+    ],
+    'register_email_field' => [
+        'name' => 'Wymagany adres e-mail rejestracji członka',
+    ],
+];
