afcc0bcd434230d18d462d8348fd97512b8e1c05
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:25:55 +0300

    New translations addon.php (Chinese Simplified)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/zh/addon.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/zh/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Visiosoft管理員',
-    'name'        => 'Visiosoft管理員主題',
-    'description' => 'OpenClassify的官方管理主題。',
+    'title'       => 'Visiosoft管理员',
+    'name'        => 'Visiosoft管理员主题',
+    'description' => 'OpenClassify的官方管理主题。',
 ];
