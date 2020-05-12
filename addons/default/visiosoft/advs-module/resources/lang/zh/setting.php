a913a561714afdddeb2bb34c87a15e60f3cc9e72
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:25:45 +0300

    New translations setting.php (Chinese Traditional)

---
+++ addons/default/visiosoft/advs-module/resources/lang/zh/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => '自動批准',
+    ],
+    'default_view_type' => [
+        'name' => '默認視圖類型',
+    ],
+    'hide_zero_price' => [
+        'name' => '如果價格為零，則隱藏價格',
+        'instructions' => '如果廣告價格為0，則隱藏價格',
+    ],
+    'estimated_pending_time' => [
+        'name' => '預計待處理時間',
+        'instructions' => '在幾小時內',
+    ],
+    'default_published_time' => [
+        'name' => '默認發佈時間',
+    ],
+    'address' => [
+        'name' => '聯繫地址',
+    ],
+    'phone' => [
+        'name' => '聯繫電話',
+    ],
+    'favicon' => [
+        'name' => '網站圖標',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => '地圖長坐標',
+    ],
+    'map_coordinates_lat' => [
+        'name' => '地圖緯度坐標',
+    ],
+    'iban_numbers' => [
+        'name' => '伊班',
+    ],
+    'logo' => [
+        'name' => '商標',
+    ],
+    'ogImage' => [
+        'name' => '社交分享徽標',
+        'warning'      => '分辨率應為1200 x 630。 ',
+
+    ],
+    'currencies' => [
+        'name' => '有效貨幣',
+    ],
+    'latest-limit' => [
+        'name' => '最新限制',
+    ],
+    'site_address' => [
+        'name' => '網站地址',
+        'warning'      => '自動添加www。萬維網。<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api密鑰',
+    ],
+
+    'default_country' => [
+        'name' => '默認廣告國家',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => '貨幣轉換器API密鑰'
+    ],
+    'default_city' => [
+        'name' => '默認廣告城市',
+    ],
+    'default_district' => [
+        'name' => '默認廣告區',
+    ],
+    'default_neighborhood' => [
+        'name' => '默認廣告鄰居',
+    ],
+    'default_currency' => [
+        'name' => '默認廣告幣種',
+    ],
+    'default_GET' => [
+        'name' => '默認廣告GET',
+    ],
+    'thumbnail_width' => [
+        'name' => '縮略圖寬度',
+    ],
+    'thumbnail_height' => [
+        'name' => '縮略圖高度',
+    ],
+    'picture_width' => [
+        'name' => '圖片寬度',
+    ],
+    'picture_height' => [
+        'name' => '圖片高度',
+    ],
+    'twitter' => [
+        'name' => '推特',
+    ],
+    'facebook' => [
+        'name' => '臉書',
+    ],
+    'youtube' => [
+        'name' => '優酷',
+    ],
+    'google' => [
+        'name' => '谷歌',
+    ],
+    'watermark_type' => [
+        'name' => '水印類型',
+    ],
+    'watermark_text' => [
+        'name' => '水印文字',
+    ],
+    'watermark_image' => [
+        'name' => '水印圖像',
+    ],
+    'watermark_position' => [
+        'name' => '水印位置',
+    ],
+    'listing_page_image' => [
+        'name' => '列表頁面圖片',
+    ],
+    'enabled_currencies' => [
+        'name' => '啟用貨幣',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google統計代碼',
+    ],
+    'register_email_field' => [
+        'name' => '會員註冊電子郵件要求',
+    ],
+];
