cf9b9f5df2169e93f00067657579df619584abd2
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:10:44 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/cats-module/resources/lang/sq/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/sq/field.php
@@ -2,20 +2,20 @@
 
 return [
     'name' => [
-        'name' => 'titull',
+        'name' => 'Titull',
         'instructions' => 'Theshtë emri i kategorisë kryesore që do të përcaktoni. Kjo i lejon përdoruesit të gjejë me lehtësi produktin që po kërkon.',
     ],
     'order' => [
-        'name' => 'urdhër',
+        'name' => 'Porosi',
     ],
 
     'slug' => [
-        'name' => 'goditje e fortë',
+        'name' => 'Goditje e fortë',
         'instructions' => 'Specifikoni çdo etiketë organizative për të ndihmuar në grupimin e postimit tuaj me të tjerët.',
 
     ],
     'files' => [
-        'name' => 'imazh',
+        'name' => 'Imazhi',
     ],
     'seo_keyword' => [
         'name' => 'Fjalë kyçe SEO',
@@ -26,7 +26,7 @@ return [
         'instructions' => 'Mund të ndikojë në vendimin e vizitorëve tuaj nëse duan të klikojnë përmbajtjen në rezultatet e kërkimit.',
     ],
     'icon' => [
-        'name' => 'ikonë',
+        'name' => 'Ikona',
         'instructions' => 'Përdoret për të shtuar ikona që tregojnë llojin e kategorisë.',
     ],
 
