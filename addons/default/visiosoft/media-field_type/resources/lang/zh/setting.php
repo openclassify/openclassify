ee1081d1d534decacc969de3f7c4911238b6b48a
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:45 +0300

    New translations setting.php (Chinese Traditional)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/zh/setting.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'imageCanvasW' => [
+        'name' => '圖像畫布寬度'
+    ],
+    'imageCanvasH' => [
+        'name' => '圖像畫布高度'
+    ],
+    'imageResizeW' => [
+        'name' => '影像調整寬度'
+    ],
+    'imageResizeH' => [
+        'name' => '圖像調整高度'
+    ],
+];
