cac7a255f960bb9c4cc8a4cfaf7c484ff0039f41
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:12:33 +0300

    New translations setting.php (Hebrew)

---
+++ addons/default/visiosoft/location-module/resources/lang/he/setting.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => 'שדה מיקום דף הבית',
+    ],
+    'list_page_location' => [
+        'name' => 'שדה מיקום דף רשימה',
+    ],
+    'detail_page_location' => [
+        'name' => 'שדה מיקום עמוד בפירוט',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'הצג לחצן מיקום לחיפוש',
+    ],
+    'default_country' => [
+        'name' => 'מדינה ברירת מחדל',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'צור מיקום של דף מודעות',
+    ],
+    'default_city' => [
+        'name' => 'עיר ברירת מחדל',
+    ],
+    'default_district' => [
+        'name' => 'מחוז ברירת מחדל',
+    ],
+    'default_neighborhood' => [
+        'name' => 'שכונת ברירת מחדל',
+    ],
+
+    'google_map_key' => [
+        'name' => 'מפתח המפתח של גוגל',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'ברירת מחדל ארוכה',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'Lat ברירת מחדל',
+    ],
+];
