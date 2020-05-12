81984f1181fa50b72e9d337946d7f9bfaf994c5d
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:15:42 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/cats-module/resources/lang/pt/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/pt/field.php
@@ -1,22 +1,36 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Título",
+return [
+    'name' => [
+        'name' => 'Título',
+        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
+    ],
+    'order' => [
+        'name' => 'Ordem',
+    ],
 
-   ],
-   "order"   =>   [
-      "name"      =>"Ordem",
+    'slug' => [
+        'name' => 'Lesma',
+        'instructions' => 'Specify any organizational tags to help group your post with others.',
 
-   ],
-   "slug"   =>   [
-      "name"      =>"Lesma",
+    ],
+    'files' => [
+        'name' => 'Imagem',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword',
+        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Description',
+        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
+    ],
+    'icon' => [
+        'name' => 'Icon',
+        'instructions' => 'It is used to add icons indicating the category type.',
+    ],
 
-   ],
-   "files"   =>   [
-      "name"      =>"Imagem",
-
-   ],
-   "please_wait"   =>"Por favor,
-   aguarde. Excluindo Subcategorias"
+    'please_wait' => 'Por favor,
+   aguarde. Excluindo Subcategorias',
+    'category_selection' => 'Category Selection',
 ];
