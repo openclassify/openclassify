096ae58f8aae729db8a22f6fc60d7fbff2e7ec16
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:02 +0300

    New translations setting.php (Chinese Traditional)

---
+++ addons/default/visiosoft/base-theme/resources/lang/zh/setting.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => '導航標題',
+    ],
+    'navigation_action' => [
+        'name' => '導航動作',
+    ],
+    'country_fields' => [
+        'name' => '國家領域',
+    ],
+    'date_fields' => [
+        'name' => '日期字段',
+    ],
+    'price_fields' => [
+        'name' => '價格字段',
+    ],
+    'breadcrumbs' => [
+        'name' => '麵包屑',
+    ],
+    'ad_details' => [
+        'name' => '廣告詳情',
+    ],
+    'ad_details_tab' => [
+        'name' => '廣告詳情標籤',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => '最新和查看全部Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => '註冊頁面說明徽標',
+    ],
+    'style' => [
+        'name' => '樣式（CSS）',
+    ],
+];
