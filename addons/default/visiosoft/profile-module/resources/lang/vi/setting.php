99b5a2aaaff56267a0c05f94f74206237069aa79
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:51 +0300

    New translations setting.php (Vietnamese)

---
+++ addons/default/visiosoft/profile-module/resources/lang/vi/setting.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'show_my_ads' => [
+        'name' => 'Hiển thị tab quảng cáo của tôi',
+    ],
+    'upload_avatar' => [
+        'name' => 'Tải lên hình đại diện',
+    ],
+];
