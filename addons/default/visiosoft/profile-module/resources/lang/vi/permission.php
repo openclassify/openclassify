f390862e97f559ee6a589f484a2c97b0ee7bb205
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:47 +0300

    New translations permission.php (Vietnamese)

---
+++ addons/default/visiosoft/profile-module/resources/lang/vi/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'profile' => [
+        'name'   => 'Hồ sơ',
+        'option' => [
+            'read'   => 'Có thể đọc hồ sơ?',
+            'write'  => 'Có thể tạo / chỉnh sửa hồ sơ?',
+            'delete' => 'Có thể xóa hồ sơ?',
+        ],
+    ],
+    'adress' => [
+        'name'   => 'Địa chỉ',
+        'option' => [
+            'read'   => 'Có thể đọc địa chỉ?',
+            'write'  => 'Có thể tạo / chỉnh sửa địa chỉ?',
+            'delete' => 'Có thể xóa địa chỉ?',
+        ],
+    ],
+];
