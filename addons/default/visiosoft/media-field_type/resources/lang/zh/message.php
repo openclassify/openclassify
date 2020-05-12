049d3bc6f62fdbfd7ee9cf3cf9565e8ed6a80a24
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:27:21 +0300

    New translations message.php (Chinese Simplified)

--- addons/default/visiosoft/media-field_type/resources/lang/zh/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/zh/message.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'choose_folder'     => '您想上傳到哪個文件夾？',
-    'upload'            => '添加İmages|上傳İmages|附上照片',
+    'choose_folder'     => '您想上传到哪个文件夹？',
+    'upload'            => '添加İmages|上传İmages|附上照片',
     'choose_files'      => '您要使用哪些文件？',
-    'no_files_selected' => '未選擇文件。',
-    'no_uploads'        => '沒有文件上傳。',
-    'overwrite'         => '已經上傳。您要覆蓋它嗎？',
-    'uploading'         => '上載中',
-    'loading'           => '載入中',
+    'no_files_selected' => '未选择文件。',
+    'no_uploads'        => '没有文件上传。',
+    'overwrite'         => '已经上传。您要覆盖它吗？',
+    'uploading'         => '上载中',
+    'loading'           => '载入中',
 ];
