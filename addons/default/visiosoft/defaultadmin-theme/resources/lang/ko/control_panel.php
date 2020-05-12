ea8725ff2ba95dd6c47e09a0fe9b852db5130bfe
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:10:32 +0300

    New translations control_panel.php (Korean)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ko/control_panel.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'help'      => '도움',
+    'search'    => '검색',
+    'logout'    => '로그 아웃',
+    'view_site' => '사이트보기',
+    'title'     => '제어판',
+    'search_placeholder'     => '검색 할 유형',
+
+    // Users Module
+    'search_by_gsm_number'     => 'GSM 번호로 검색',
+];
