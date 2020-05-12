b8611ee6debbbdfabb70bebf6eb8a34502c44f56
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:20:49 +0300

    New translations message.php (Swedish)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/sv/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_folder'     => 'Vilken mapp vill du ladda upp till?',
+    'upload'            => 'Lägg till İmages | Ladda upp İmages | Bifoga ett foto',
+    'choose_files'      => 'Vilka filer vill du använda?',
+    'no_files_selected' => 'Inga filer valda.',
+    'no_uploads'        => 'Inga filer laddas upp.',
+    'overwrite'         => 'har redan laddats upp. Vill du skriva över det?',
+    'uploading'         => 'uppladdning',
+    'loading'           => 'Läser in',
+];
