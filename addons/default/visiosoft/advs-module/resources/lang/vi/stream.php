8b934adda26a946aea8d8baac8bb8610d4334472
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:25:14 +0300

    New translations stream.php (Vietnamese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/vi/stream.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => 'Advs',
+    ],
+    'categories' => [
+        'name' => 'Thể loại',
+    ],
+    'village' => [
+        'name' => 'Làng',
+    ],
+    'custom_fields' => [
+        'name' => 'Trường tùy chỉnh',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Tùy chỉnh trường adv',
+    ],
+    'cf_values' => [
+        'name' => 'Giá trị Cf',
+    ],
+];
