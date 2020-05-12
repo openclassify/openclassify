7293f8e71396b23a46cd07dc6cee0098d5829602
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:04:48 +0300

    New translations setting.php (Romanian)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'Aprobare automată',
+    ],
+    'default_view_type' => [
+        'name' => 'Tip de vizualizare implicit',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Ascundeți prețul dacă este zero',
+        'instructions' => 'Ascundeți prețul dacă prețul anunțului este 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Timp de așteptare estimat',
+        'instructions' => 'În ore',
+    ],
+    'default_published_time' => [
+        'name' => 'Ora de publicare implicită',
+    ],
+    'address' => [
+        'name' => 'Contact Adresa',
+    ],
+    'phone' => [
+        'name' => 'Telefon de Contact',
+    ],
+    'favicon' => [
+        'name' => 'favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Coordonarea pe hartă lungă',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Harta Coordonata Lat',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Siglă',
+    ],
+    'ogImage' => [
+        'name' => 'Logo Social Share',
+        'warning'      => 'Ar trebui să fie la o rezoluție de 1200 x 630. ',
+
+    ],
+    'currencies' => [
+        'name' => 'Monede active',
+    ],
+    'latest-limit' => [
+        'name' => 'Ultima limită',
+    ],
+    'site_address' => [
+        'name' => 'Adresa site-ului',
+        'warning'      => 'Auto adăugat www. www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Cheie Api Google Map',
+    ],
+
+    'default_country' => [
+        'name' => 'Țara anunțurilor implicite',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Cheia API a convertorului de monede'
+    ],
+    'default_city' => [
+        'name' => 'Orașul publicitar implicit',
+    ],
+    'default_district' => [
+        'name' => 'Districtul publicitar implicit',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Imprimați cartierul publicitar',
+    ],
+    'default_currency' => [
+        'name' => 'Valoarea publicitară implicită',
+    ],
+    'default_GET' => [
+        'name' => 'Anunț implicit GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'Lățimea miniaturii',
+    ],
+    'thumbnail_height' => [
+        'name' => 'Înălțimea miniaturii',
+    ],
+    'picture_width' => [
+        'name' => 'Lățimea imaginii',
+    ],
+    'picture_height' => [
+        'name' => 'Înălțimea imaginii',
+    ],
+    'twitter' => [
+        'name' => 'Stare de nervozitate',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Tip filigran',
+    ],
+    'watermark_text' => [
+        'name' => 'Text Filigran',
+    ],
+    'watermark_image' => [
+        'name' => 'Filigran imagine',
+    ],
+    'watermark_position' => [
+        'name' => 'Poziția filigranului',
+    ],
+    'listing_page_image' => [
+        'name' => 'Listare imagine pagină',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Valute activate',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Cod statistic Google',
+    ],
+    'register_email_field' => [
+        'name' => 'Cerință de e-mail de înregistrare a membrilor',
+    ],
+];
