48c5cdb10f7046d8c6d1f129bf055a3501610ae5
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:11:31 +0300

    New translations field.php (Persian)

--- addons/default/visiosoft/location-module/resources/lang/fa/field.php
+++ addons/default/visiosoft/location-module/resources/lang/fa/field.php
@@ -1,41 +1,38 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"عنوان",
-
-   ],
-   "slug"   =>   [
-      "name"      =>"مثل حلزون حرکت کردن",
-
-   ],
-   "city"   =>   [
-      "name"      =>"شهر",
-
-   ],
-   "country"   =>   [
-      "name"      =>"کشور",
-
-   ],
-   "district"   =>   [
-      "name"      =>"ناحیه",
-
-   ],
-   "order"   =>   [
-      "name"      =>"ترتیب مرتب سازی",
-
-   ],
-   "neighborhood"   =>   [
-      "name"      =>"محله",
-
-   ],
-   "village"   =>   [
-      "name"      =>"روستا",
-
-   ],
-   "description"   =>   [
-      "name"      =>"شرح",
-
-   ],
-
+return [
+    'name' => [
+        'name' => 'عنوان'
+    ],
+    'slug' => [
+        'name' => 'مثل حلزون حرکت کردن'
+    ],
+    'city' => [
+        'name' => 'شهر'
+    ],
+    'country' => [
+        'name' => 'کشور'
+    ],
+    'district' => [
+        'name' => 'ناحیه'
+    ],
+    'order' => [
+        'name' => 'ترتیب مرتب سازی'
+    ],
+    'neighborhood' => [
+        'name' => 'محله'
+    ],
+    'village' => [
+        'name' => 'روستا'
+    ],
+    'description' => [
+        'name' => 'شرح'
+    ],
+    'pick_option' => [
+        'name' => 'Pick an option',
+    ],
+    'address' => [
+        'name' => 'Address',
+    ],
+    'selected' => 'Selected'
 ];
