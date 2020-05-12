5b80de2b3ee6b06616ad156a9054410f67d213bc
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:15:48 +0300

    New translations setting.php (Portuguese)

--- addons/default/visiosoft/location-module/resources/lang/pt/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/pt/setting.php
@@ -1,5 +1,43 @@
 <?php
 
- return[
+return [
+    'home_page_location' => [
+        'name' => 'Home Page Location Field',
+    ],
+    'list_page_location' => [
+        'name' => 'List Page Location Field',
+    ],
+    'detail_page_location' => [
+        'name' => 'Detail Page Location Field',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'Show Search Location Button',
+    ],
+    'default_country' => [
+        'name' => 'Default Country',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'Create Ad Page Location',
+    ],
+    'default_city' => [
+        'name' => 'Default City',
+    ],
+    'default_district' => [
+        'name' => 'Default District',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Default Neighborhood',
+    ],
 
+    'google_map_key' => [
+        'name' => 'Google Maps Api Key',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'Default Long',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'Default Lat',
+    ],
 ];
