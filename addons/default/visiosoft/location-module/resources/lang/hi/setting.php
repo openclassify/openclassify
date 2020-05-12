562a02e03cd51f8d0845629c78834f0fe843f617
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:27:59 +0300

    New translations setting.php (Hindi)

---
+++ addons/default/visiosoft/location-module/resources/lang/hi/setting.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => 'होम पेज स्थान फ़ील्ड',
+    ],
+    'list_page_location' => [
+        'name' => 'सूची पृष्ठ स्थान फ़ील्ड',
+    ],
+    'detail_page_location' => [
+        'name' => 'विस्तार पृष्ठ स्थान फ़ील्ड',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'खोज स्थान बटन दिखाएं',
+    ],
+    'default_country' => [
+        'name' => 'डिफ़ॉल्ट देश',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'विज्ञापन पृष्ठ स्थान बनाएँ',
+    ],
+    'default_city' => [
+        'name' => 'डिफ़ॉल्ट शहर',
+    ],
+    'default_district' => [
+        'name' => 'डिफ़ॉल्ट जिला',
+    ],
+    'default_neighborhood' => [
+        'name' => 'डिफ़ॉल्ट पड़ोस',
+    ],
+
+    'google_map_key' => [
+        'name' => 'Google मैप्स Api Key',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'डिफ़ॉल्ट लंबा',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'डिफ़ॉल्ट लैट',
+    ],
+];
