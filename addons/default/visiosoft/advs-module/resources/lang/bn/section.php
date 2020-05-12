b126748288362f4432dca394a8d419532b336600
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:29:33 +0300

    New translations section.php (Bengali)

---
+++ addons/default/visiosoft/advs-module/resources/lang/bn/section.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => 'বিজ্ঞাপন',
+    ],
+    'categories' => [
+        'title' => 'ধরন',
+    ],
+    'countries' => [
+        'title' => 'দেশ',
+    ],
+    'cities' => [
+        'title' => 'শহর',
+    ],
+    'districts' => [
+        'title' => 'জেলা',
+    ],
+    'neighborhoods' => [
+        'title' => 'প্রতিবেশী',
+    ],
+    'village' => [
+        'title' => 'গ্রাম',
+    ],
+    'custom_fields' => [
+        'title' => 'অস্ত্রোপচার',
+    ],
+    'custom_field_advs' => [
+        'title' => 'কাস্টম ফিল্ড বিজ্ঞাপন',
+    ],
+    'cf_values' => [
+        'title' => 'সিএফ মান',
+    ],
+    'fields' => [
+        'title' => 'অস্ত্রোপচার'
+    ],
+    'assets_clear' => [
+        'name' => 'সম্পদ পরিষ্কার'
+    ],
+    'general' => 'সাধারণ',
+    'ads' => 'বিজ্ঞাপন',
+    'user' => 'ব্যবহারকারী',
+];
