c7b76d8aa10f6ed79551f080e25912907307de17
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:10:48 +0300

    New translations section.php (Polish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/pl/section.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => 'Reklamy',
+    ],
+    'categories' => [
+        'title' => 'Kategorie',
+    ],
+    'countries' => [
+        'title' => 'Kraje',
+    ],
+    'cities' => [
+        'title' => 'Miasta',
+    ],
+    'districts' => [
+        'title' => 'Dzielnice',
+    ],
+    'neighborhoods' => [
+        'title' => 'Okolice',
+    ],
+    'village' => [
+        'title' => 'Wioska',
+    ],
+    'custom_fields' => [
+        'title' => 'Pola niestandardowe',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Niestandardowe reklamy polowe',
+    ],
+    'cf_values' => [
+        'title' => 'Wartości Cf',
+    ],
+    'fields' => [
+        'title' => 'Pola niestandardowe'
+    ],
+    'assets_clear' => [
+        'name' => 'Aktywa Wyczyść'
+    ],
+    'general' => 'Generał',
+    'ads' => 'Reklamy',
+    'user' => 'Użytkownik',
+];
