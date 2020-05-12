74d6d45dd1a1d8e9a6aacbdcad2514a7998f19b5
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:12:25 +0300

    New translations field.php (Persian)

--- addons/default/visiosoft/cats-module/resources/lang/fa/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/fa/field.php
@@ -1,21 +1,35 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"عنوان",
+return [
+    'name' => [
+        'name' => 'عنوان',
+        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
+    ],
+    'order' => [
+        'name' => 'سفارش',
+    ],
 
-   ],
-   "order"   =>   [
-      "name"      =>"سفارش",
+    'slug' => [
+        'name' => 'مثل حلزون حرکت کردن',
+        'instructions' => 'Specify any organizational tags to help group your post with others.',
 
-   ],
-   "slug"   =>   [
-      "name"      =>"مثل حلزون حرکت کردن",
+    ],
+    'files' => [
+        'name' => 'تصویر',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword',
+        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Description',
+        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
+    ],
+    'icon' => [
+        'name' => 'Icon',
+        'instructions' => 'It is used to add icons indicating the category type.',
+    ],
 
-   ],
-   "files"   =>   [
-      "name"      =>"تصویر",
-
-   ],
-   "please_wait"   =>"لطفاً صبر کنید. حذف دسته بندی های فرعی"
+    'please_wait' => 'لطفاً صبر کنید. حذف دسته بندی های فرعی',
+    'category_selection' => 'Category Selection',
 ];
