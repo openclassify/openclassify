ae89aebb8654a1e2c0cea7fff49fae6dbc12bc9f
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:21:11 +0300

    New translations permission.php (Swedish)

---
+++ addons/default/visiosoft/location-module/resources/lang/sv/permission.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'village' => [
+        'name'   => 'By',
+        'option' => [
+            'read'   => 'Kan läsa byn?',
+            'write'  => 'Kan skapa / redigera byn?',
+            'delete' => 'Kan radera byn?',
+        ],
+    ],
+];
