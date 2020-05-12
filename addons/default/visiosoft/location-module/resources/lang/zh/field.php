f433c0b9e5b529acba43823dbb7bebe4afa169ee
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:25 +0300

    New translations field.php (Chinese Traditional)

---
+++ addons/default/visiosoft/location-module/resources/lang/zh/field.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'name' => [
+        'name' => '標題'
+    ],
+    'slug' => [
+        'name' => 'ug'
+    ],
+    'city' => [
+        'name' => '市'
+    ],
+    'country' => [
+        'name' => '國家'
+    ],
+    'district' => [
+        'name' => '區'
+    ],
+    'order' => [
+        'name' => '排序'
+    ],
+    'neighborhood' => [
+        'name' => '鄰里'
+    ],
+    'village' => [
+        'name' => '村'
+    ],
+    'description' => [
+        'name' => '描述'
+    ],
+    'pick_option' => [
+        'name' => '選擇一個選項',
+    ],
+    'address' => [
+        'name' => '地址',
+    ],
+    'selected' => '已選'
+];
