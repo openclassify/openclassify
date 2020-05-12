6f0c9b0779e2c965c715e32b1869db9622d5419b
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:13:57 +0300

    New translations message.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/message.php
@@ -1,6 +1,25 @@
 <?php
 
- return[
-   "error_added_cart"   =>"Gevraagde product ontbreekt momenteel of is inactief",
-   "please_buy_package"   =>"Koop pakket"
+return [
+    'error_added_cart' => 'Gevraagde product ontbreekt momenteel of is inactief',
+    'please_payment' => 'Please Pay',
+    'bank_account_info' => 'Bank account information',
+    'agreement' => 'By clicking Post, you agree to our Terms of Use and Privacy Policy acknowledge that you are the rightful owner of this item.',
+    'update_category_msg' => 'The classified category will be updated.',
+    'updated_category_msg' => 'Category successfully edited.',
+    'create_ad_with_post_cat' => [
+        'title' => "Category selection has been completed.",
+        'success' => true,
+        'continueBtn' => "Continue",
+    ],
+    'update_category_info' => 'Please update category.',
+    'filter_by_search_msg' => 'Filter By Search.',
+    'delete_author_error' => 'You cannot delete a post that does not belong to you',
+    'success_delete' => 'Ad Deleted',
+    'pending_ad_status' => 'Your ad is pending approval by the Editor.',
+    'extended' => ':number ad(s) has been extended successfully.',
+    'category_not_exist' => 'This category does not exist anymore!',
+    'currency_converted' => 'Currency Converted Successfully.',
+    'choose_user' => 'Choose a user',
+    'owner_updated_successfully' => 'The Owner Has Been Updated Successfully!',
 ];
