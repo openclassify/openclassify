568ac27160ded0b8f8767b05007f6939aa504e8c
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:10:37 +0300

    New translations setting.php (Korean)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ko/setting.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => '사이드 바 호버',
+        'instructions' => '호버에서 사이드 바를 확장 하시겠습니까?',
+    ],
+    'navigation'    => [
+        'name'         => '항해',
+        'instructions' => '탐색의 <em>기본</em> 순서를 지정하십시오.',
+        'warning'      => '액세스 가능한 첫 번째 탐색 항목은 <strong>홈</strong> 영역으로 사용됩니다.',
+        'reorder'      => '<strong>사이드 바</strong> 에서 기본 탐색 항목을 끌어다 놓아 순서를 바꿉니다.',
+    ],
+];
