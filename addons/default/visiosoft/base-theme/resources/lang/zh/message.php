6526612a4310800222fed6aa713f29cb798f4c1c
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:26:48 +0300

    New translations message.php (Chinese Simplified)

--- addons/default/visiosoft/base-theme/resources/lang/zh/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/zh/message.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'send_forgot_sms' => '短信已發送到您的註冊電話。請檢查。',
-    'found_phone' => '找不到系統中註冊的電話號碼。',
+    'send_forgot_sms' => '短信已发送到您的注册电话。请检查。',
+    'found_phone' => '找不到系统中注册的电话号码。',
 ];
