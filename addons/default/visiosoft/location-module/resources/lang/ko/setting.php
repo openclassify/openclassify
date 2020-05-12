95966b7a76cac45ae537c463c7a560950e0302ac
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:10:30 +0300

    New translations setting.php (Korean)

---
+++ addons/default/visiosoft/location-module/resources/lang/ko/setting.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => '홈페이지 위치 필드',
+    ],
+    'list_page_location' => [
+        'name' => '목록 페이지 위치 필드',
+    ],
+    'detail_page_location' => [
+        'name' => '세부 사항 페이지 위치 필드',
+    ],
+    'show_search_location_btn' => [
+        'name' => '검색 위치 표시 버튼',
+    ],
+    'default_country' => [
+        'name' => '기본 국가',
+    ],
+    'create_ad_page_location' => [
+        'name' => '광고 페이지 위치 만들기',
+    ],
+    'default_city' => [
+        'name' => '기본 도시',
+    ],
+    'default_district' => [
+        'name' => '기본 지구',
+    ],
+    'default_neighborhood' => [
+        'name' => '기본 이웃',
+    ],
+
+    'google_map_key' => [
+        'name' => '구글 맵스 API 키',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => '기본 긴',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => '기본 위도',
+    ],
+];
