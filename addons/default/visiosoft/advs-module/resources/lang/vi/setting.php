c25566b7f62184825d46c34c81c8ea89a2a940d7
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:25:13 +0300

    New translations setting.php (Vietnamese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/vi/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'Tự động phê duyệt',
+    ],
+    'default_view_type' => [
+        'name' => 'Kiểu xem mặc định',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Ẩn giá nếu không',
+        'instructions' => 'Ẩn giá nếu giá quảng cáo là 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Thời gian chờ xử lý',
+        'instructions' => 'Trong giờ',
+    ],
+    'default_published_time' => [
+        'name' => 'Thời gian xuất bản mặc định',
+    ],
+    'address' => [
+        'name' => 'Địa chỉ liên lạc',
+    ],
+    'phone' => [
+        'name' => 'Điện thoại liên hệ',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Bản đồ tọa độ dài',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Bản đồ tọa độ Lat',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Logo chia sẻ xã hội',
+        'warning'      => 'Nó phải ở độ phân giải 1200 x 630. ',
+
+    ],
+    'currencies' => [
+        'name' => 'Tiền tệ hoạt động',
+    ],
+    'latest-limit' => [
+        'name' => 'Giới hạn mới nhất',
+    ],
+    'site_address' => [
+        'name' => 'Địa chỉ trang web',
+        'warning'      => 'Tự động thêm www. www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'Khóa bản đồ Google',
+    ],
+
+    'default_country' => [
+        'name' => 'Quốc gia quảng cáo mặc định',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Khóa API chuyển đổi tiền tệ'
+    ],
+    'default_city' => [
+        'name' => 'Thành phố quảng cáo mặc định',
+    ],
+    'default_district' => [
+        'name' => 'Quận quảng cáo mặc định',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Vùng lân cận quảng cáo mặc định',
+    ],
+    'default_currency' => [
+        'name' => 'Tiền tệ quảng cáo mặc định',
+    ],
+    'default_GET' => [
+        'name' => 'Quảng cáo mặc định NHẬN',
+    ],
+    'thumbnail_width' => [
+        'name' => 'Chiều rộng hình thu nhỏ',
+    ],
+    'thumbnail_height' => [
+        'name' => 'Chiều cao hình thu nhỏ',
+    ],
+    'picture_width' => [
+        'name' => 'Chiều rộng hình ảnh',
+    ],
+    'picture_height' => [
+        'name' => 'Chiều cao hình ảnh',
+    ],
+    'twitter' => [
+        'name' => 'Twitter',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Loại hình mờ',
+    ],
+    'watermark_text' => [
+        'name' => 'Văn bản thủy ấn',
+    ],
+    'watermark_image' => [
+        'name' => 'Hình mờ',
+    ],
+    'watermark_position' => [
+        'name' => 'Vị trí hình mờ',
+    ],
+    'listing_page_image' => [
+        'name' => 'Danh sách trang hình ảnh',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Tiền tệ được kích hoạt',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Mã thống kê Google',
+    ],
+    'register_email_field' => [
+        'name' => 'Yêu cầu email đăng ký thành viên',
+    ],
+];
