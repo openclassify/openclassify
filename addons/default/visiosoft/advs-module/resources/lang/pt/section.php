a95d94c1ab207cdb0d059ab6644d94e95910ba7b
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:17:00 +0300

    New translations section.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/section.php
@@ -1,49 +1,43 @@
 <?php
 
- return[
-   "advs"   =>   [
-      "title"      =>"Publicidades",
-
-   ],
-   "categories"   =>   [
-      "title"      =>"Categorias",
-
-   ],
-   "countries"   =>   [
-      "title"      =>"Países",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Cidades",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Distritos",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"Bairros",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Aldeia",
-
-   ],
-   "custom_fields"   =>   [
-      "title"      =>"Os campos personalizados",
-
-   ],
-   "custom_field_advs"   =>   [
-      "title"      =>"Anúncios de campo personalizado",
-
-   ],
-   "cf_values"   =>   [
-      "title"      =>"Valores Cf",
-
-   ],
-   "fields"   =>   [
-      "title"      =>"Os campos personalizados",
-
-   ],
-
+return [
+    'advs' => [
+        'title' => 'Publicidades',
+    ],
+    'categories' => [
+        'title' => 'Categorias',
+    ],
+    'countries' => [
+        'title' => 'Países',
+    ],
+    'cities' => [
+        'title' => 'Cidades',
+    ],
+    'districts' => [
+        'title' => 'Distritos',
+    ],
+    'neighborhoods' => [
+        'title' => 'Bairros',
+    ],
+    'village' => [
+        'title' => 'Aldeia',
+    ],
+    'custom_fields' => [
+        'title' => 'Os campos personalizados',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Anúncios de campo personalizado',
+    ],
+    'cf_values' => [
+        'title' => 'Valores Cf',
+    ],
+    'fields' => [
+        'title' => 'Os campos personalizados'
+    ],
+    'assets_clear' => [
+        'name' => 'Assets Clear'
+    ],
+    'general' => 'General',
+    'ads' => 'Ads',
+    'user' => 'User',
 ];
