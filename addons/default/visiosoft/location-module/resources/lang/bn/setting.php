6917db9e1fd3d8a44c04915b74c9cae8ba9c917a
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:31:20 +0300

    New translations setting.php (Bengali)

---
+++ addons/default/visiosoft/location-module/resources/lang/bn/setting.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => 'হোম পেজ অবস্থান ক্ষেত্র',
+    ],
+    'list_page_location' => [
+        'name' => 'পৃষ্ঠার তালিকা ক্ষেত্রের তালিকা করুন',
+    ],
+    'detail_page_location' => [
+        'name' => 'বিশদ পৃষ্ঠা অবস্থান ক্ষেত্র',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'অনুসন্ধান অবস্থান বোতামটি দেখান',
+    ],
+    'default_country' => [
+        'name' => 'ডিফল্ট দেশ',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'বিজ্ঞাপন পৃষ্ঠা অবস্থান তৈরি করুন',
+    ],
+    'default_city' => [
+        'name' => 'ডিফল্ট শহর',
+    ],
+    'default_district' => [
+        'name' => 'ডিফল্ট জেলা',
+    ],
+    'default_neighborhood' => [
+        'name' => 'ডিফল্ট নেবারহুড',
+    ],
+
+    'google_map_key' => [
+        'name' => 'গুগল ম্যাপস এপি কি',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'ডিফল্ট দীর্ঘ',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'ডিফল্ট ল্যাট',
+    ],
+];
