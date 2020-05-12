56968cff816f5f2cf8009b943edfb519e4a5d450
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:05:18 +0300

    New translations field.php (Romanian)

---
+++ addons/default/visiosoft/location-module/resources/lang/ro/field.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Titlu'
+    ],
+    'slug' => [
+        'name' => 'Melc'
+    ],
+    'city' => [
+        'name' => 'Oraș'
+    ],
+    'country' => [
+        'name' => 'Țară'
+    ],
+    'district' => [
+        'name' => 'District'
+    ],
+    'order' => [
+        'name' => 'Ordinea de sortare'
+    ],
+    'neighborhood' => [
+        'name' => 'Cartier'
+    ],
+    'village' => [
+        'name' => 'Sat'
+    ],
+    'description' => [
+        'name' => 'Descriere'
+    ],
+    'pick_option' => [
+        'name' => 'Alegeți o opțiune',
+    ],
+    'address' => [
+        'name' => 'Abordare',
+    ],
+    'selected' => 'Selectat'
+];
