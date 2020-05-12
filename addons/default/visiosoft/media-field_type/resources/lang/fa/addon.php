377fceeec46c1d54090221e5b7c811f07ec3106f
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:30:02 +0300

    New translations addon.php (Persian)

--- addons/default/visiosoft/media-field_type/resources/lang/fa/addon.php
+++ addons/default/visiosoft/media-field_type/resources/lang/fa/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Media Field Type',
-    'name'        => 'Files Field Type',
-    'description' => 'A multiple files upload field type.',
+    'title'       => 'نوع فیلد رسانه',
+    'name'        => 'نوع فیلد پرونده ها',
+    'description' => 'چندین پرونده نوع فیلد را بارگذاری می کنند.',
 ];
