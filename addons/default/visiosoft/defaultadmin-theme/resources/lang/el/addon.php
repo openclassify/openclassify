6aabd833bce900a2f23217044cf9f8108ec867d9
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:09:25 +0300

    New translations addon.php (Greek)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/el/addon.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/el/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Visiosoft Admin',
-    'name'        => 'Visiosoft Admin Theme',
-    'description' => 'The official admin theme for OpenClassify.',
+    'title'       => 'Διαχειριστής της Visiosoft',
+    'name'        => 'Θέμα διαχειριστή Visiosoft',
+    'description' => 'Το επίσημο θέμα διαχειριστή για το OpenClassify.',
 ];
