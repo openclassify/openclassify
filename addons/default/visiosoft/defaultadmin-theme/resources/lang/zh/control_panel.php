e32db43feb2d7285a4c9d440e0f53124978225f2
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:25:54 +0300

    New translations control_panel.php (Chinese Simplified)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/zh/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/zh/control_panel.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'help'      => '幫幫我',
+    'help'      => '帮帮我',
     'search'    => '搜索',
     'logout'    => '登出',
-    'view_site' => '查看網站',
+    'view_site' => '查看网站',
     'title'     => '控制面板',
-    'search_placeholder'     => '輸入搜索',
+    'search_placeholder'     => '输入搜索',
 
     // Users Module
-    'search_by_gsm_number'     => 'GSM號碼查詢',
+    'search_by_gsm_number'     => 'GSM号码查询',
 ];
