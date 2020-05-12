d4e515975216928f0747ae1af41c679c61abe216
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:18:38 +0300

    New translations validation.php (Portuguese)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/pt/validation.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/pt/validation.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'valid_disk' => 'The configured upload disk for :attribute does not exist.',
+    'valid_disk' => 'O disco de upload configurado para: attribute não existe.',
 ];
