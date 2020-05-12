6b8dda9f1b1dbc36db952daaf21fb8ea6469b3ac
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:24 +0300

    New translations button.php (Chinese Traditional)

---
+++ addons/default/visiosoft/location-module/resources/lang/zh/button.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'new_country' => '新國家',
+    'new_city' => '新城市',
+    'new_district' => '新區',
+    'new_neighborhood' => '新鄰居',
+    'new_village' => '新村',
+    'sub_cities' => '子城市',
+    'add_sub_cities' => '添加子城市',
+    'add_sub_districts' => '添加分區',
+    'add_sub_neighborhoods' => '添加子社區',
+    'add_sub_village' => '添加子村',
+    'sub_districts' => '分區',
+    'sub_neighborhoods' => '子社區',
+    'sub_village' => '子村',
+];
