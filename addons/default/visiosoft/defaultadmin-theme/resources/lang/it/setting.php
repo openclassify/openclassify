adf37bc582439234cf7f70fa797ff67109554148
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:16:39 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/it/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/it/setting.php
@@ -2,13 +2,13 @@
 
 return [
     'sidebar_hover' => [
-        'name'         => 'Sidebar Hover',
-        'instructions' => 'Expand the sidebar on hover?',
+        'name'         => 'Hover barra laterale',
+        'instructions' => 'Espandere la barra laterale al passaggio del mouse?',
     ],
     'navigation'    => [
-        'name'         => 'Navigation',
-        'instructions' => 'Specify the <em>default</em> order of navigation.',
-        'warning'      => 'The first accessible navigation item is used as the <strong>home</strong> area.',
-        'reorder'      => 'Drag and drop the primary navigation items in the <strong>sidebar</strong> to reorder them.',
+        'name'         => 'Navigazione',
+        'instructions' => 'Specifica l\'ordine di navigazione <em>predefinito</em>.',
+        'warning'      => 'Il primo elemento di navigazione accessibile viene utilizzato come area <strong>home</strong>.',
+        'reorder'      => 'Trascina e rilascia gli elementi di navigazione principali nella barra laterale <strong></strong> per riordinarli.',
     ],
 ];
