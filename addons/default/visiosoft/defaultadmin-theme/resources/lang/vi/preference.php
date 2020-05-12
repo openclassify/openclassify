8cf214532fe7eff390057983af72824c134545ed
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:25:37 +0300

    New translations preference.php (Vietnamese)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/vi/preference.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Di chuột bên',
+        'instructions' => 'Mở rộng thanh bên trên di chuột?',
+    ],
+    'navigation'    => [
+        'name'         => 'dẫn đường',
+        'instructions' => 'Chỉ định <em>cá nhân</em> thứ tự điều hướng.',
+        'warning'      => 'Mục điều hướng có thể truy cập đầu tiên được sử dụng làm khu vực <strong>nhà</strong> của bạn.',
+        'reorder'      => 'Kéo và thả các mục điều hướng chính trong thanh bên <strong></strong> để sắp xếp lại chúng.',
+    ],
+    'display'       => [
+        'name'         => 'Mật độ hiển thị',
+        'instructions' => 'Màn hình nhỏ gọn cho phép hiển thị nhiều nội dung hơn trên màn hình cùng một lúc.',
+        'option'       => [
+            'default' => 'Mặc định',
+            'compact' => 'gọn nhẹ',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => 'Chế độ thanh bên',
+        'instructions' => 'Sidebars tĩnh sẽ luôn luôn được nhìn thấy.',
+        'option'       => [
+            'default' => 'Mặc định',
+            'static'  => 'Tĩnh',
+        ],
+    ],
+];
