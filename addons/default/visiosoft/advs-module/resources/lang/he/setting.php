c084f66624254fe5066e4712183600509c1804d0
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:14:36 +0300

    New translations setting.php (Hebrew)

---
+++ addons/default/visiosoft/advs-module/resources/lang/he/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => 'אישור אוטומטי',
+    ],
+    'default_view_type' => [
+        'name' => 'סוג תצוגה ברירת מחדל',
+    ],
+    'hide_zero_price' => [
+        'name' => 'הסתר מחיר אם אפס',
+        'instructions' => 'הסתר את המחיר אם מחיר המודעה הוא 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'זמן ממתין משוער',
+        'instructions' => 'בשעות',
+    ],
+    'default_published_time' => [
+        'name' => 'זמן פרסום ברירת מחדל',
+    ],
+    'address' => [
+        'name' => 'צרו קשר עם כתובת',
+    ],
+    'phone' => [
+        'name' => 'צור קשר עם טלפון',
+    ],
+    'favicon' => [
+        'name' => 'פייביקון',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'מפה לתיאום ארוך',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'קואורדינטת Lat מפת',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'לוגו',
+    ],
+    'ogImage' => [
+        'name' => 'לוגו של שיתוף חברתי',
+        'warning'      => 'זה אמור להיות ברזולוציה של 1200 x 630. ',
+
+    ],
+    'currencies' => [
+        'name' => 'מטבעות פעילים',
+    ],
+    'latest-limit' => [
+        'name' => 'המגבלה האחרונה',
+    ],
+    'site_address' => [
+        'name' => 'כתובת אתר',
+        'warning'      => 'נוסף אוטומטית www. www.<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'מפתח המפתח של גוגל',
+    ],
+
+    'default_country' => [
+        'name' => 'מדינה ברירת מחדל',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'מפתח API של ממיר מטבע'
+    ],
+    'default_city' => [
+        'name' => 'עיר ברירת מחדל',
+    ],
+    'default_district' => [
+        'name' => 'מחוז ברירת מחדל',
+    ],
+    'default_neighborhood' => [
+        'name' => 'שכונת מודעות ברירת מחדל',
+    ],
+    'default_currency' => [
+        'name' => 'מטבע ברירת מחדל של מודעות',
+    ],
+    'default_GET' => [
+        'name' => 'ברירת מחדל של מודעות GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'רוחב תמונה ממוזערת',
+    ],
+    'thumbnail_height' => [
+        'name' => 'גובה תמונה ממוזערת',
+    ],
+    'picture_width' => [
+        'name' => 'רוחב תמונה',
+    ],
+    'picture_height' => [
+        'name' => 'גובה תמונה',
+    ],
+    'twitter' => [
+        'name' => 'טוויטר',
+    ],
+    'facebook' => [
+        'name' => 'פייסבוק',
+    ],
+    'youtube' => [
+        'name' => 'יוטיוב',
+    ],
+    'google' => [
+        'name' => 'גוגל',
+    ],
+    'watermark_type' => [
+        'name' => 'סוג סימן מים',
+    ],
+    'watermark_text' => [
+        'name' => 'טקסט סימן מים',
+    ],
+    'watermark_image' => [
+        'name' => 'תמונה של סימן מים',
+    ],
+    'watermark_position' => [
+        'name' => 'מיקום סימן מים',
+    ],
+    'listing_page_image' => [
+        'name' => 'תמונת עמוד רישום',
+    ],
+    'enabled_currencies' => [
+        'name' => 'מטבעות מופעלים',
+    ],
+    'google_statistic_code' => [
+        'name' => 'קוד סטטיסטי של גוגל',
+    ],
+    'register_email_field' => [
+        'name' => 'דרישת דוא"ל לרישום חבר',
+    ],
+];
