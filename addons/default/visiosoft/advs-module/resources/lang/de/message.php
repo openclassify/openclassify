9dd7e9e84097c0046432a9d11042da6efd119518
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:06:14 +0300

    New translations message.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/message.php
@@ -2,21 +2,15 @@
 
 return [
     'error_added_cart' => 'Angefordertes Produkt fehlt derzeit oder ist inaktiv',
-    'please_buy_package' => 'Bitte Paket kaufen',
     'please_payment' => 'Bitte zahlen Sie',
     'bank_account_info' => 'Bankkonto Information',
     'agreement' => 'Durch Klicken auf "Posten" erklären Sie sich mit unseren Nutzungsbedingungen einverstanden und bestätigen, dass Sie der rechtmäßige Eigentümer dieses Artikels sind.',
     'update_category_msg' => 'Die klassifizierte Kategorie wird aktualisiert.',
     'updated_category_msg' => 'Kategorie erfolgreich bearbeitet.',
-    'max_ad_limit' => [
-        'title' => "Ihr kostenloses Anzeigenlimit ist abgelaufen.",
-        'continueBtn' => "Show Ads",
-        'link' => "/profile",
-    ],
     'create_ad_with_post_cat' => [
-        'title' => "Category selection has been completed.",
+        'title' => "Die Kategorieauswahl wurde abgeschlossen.",
         'success' => true,
-        'continueBtn' => "Continue",
+        'continueBtn' => "Fortsetzen",
     ],
     'update_category_info' => 'Bitte aktualisieren Sie die Kategorie.',
     'filter_by_search_msg' => 'Nach Suche filtern.',
