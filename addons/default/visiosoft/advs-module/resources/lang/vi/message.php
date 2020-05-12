6aee0db06eed478b9c2eaeddd968c82b8c74da3b
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:28 +0300

    New translations message.php (Vietnamese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/vi/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'error_added_cart' => 'Sản phẩm được yêu cầu hiện đang thiếu hoặc không hoạt động',
+    'please_payment' => 'Vui long thanh toan',
+    'bank_account_info' => 'Thông tin tài khoản ngân hàng',
+    'agreement' => 'Bằng cách nhấp vào Bài đăng, bạn đồng ý với Điều khoản sử dụng và Chính sách quyền riêng tư của chúng tôi thừa nhận rằng bạn là chủ sở hữu hợp pháp của mặt hàng này.',
+    'update_category_msg' => 'Các danh mục phân loại sẽ được cập nhật.',
+    'updated_category_msg' => 'Thể loại được chỉnh sửa thành công.',
+    'create_ad_with_post_cat' => [
+        'title' => "Lựa chọn danh mục đã được hoàn thành.",
+        'success' => true,
+        'continueBtn' => "Tiếp tục",
+    ],
+    'update_category_info' => 'Vui lòng cập nhật danh mục.',
+    'filter_by_search_msg' => 'Lọc theo tìm kiếm.',
+    'delete_author_error' => 'Bạn không thể xóa một bài viết không thuộc về bạn',
+    'success_delete' => 'Đã xóa quảng cáo',
+    'pending_ad_status' => 'Quảng cáo của bạn đang chờ biên tập viên phê duyệt.',
+    'extended' => ': số quảng cáo (s) đã được mở rộng thành công.',
+    'category_not_exist' => 'Thể loại này không tồn tại nữa!',
+    'currency_converted' => 'Chuyển đổi tiền tệ thành công.',
+    'choose_user' => 'Chọn người dùng',
+    'owner_updated_successfully' => 'Chủ sở hữu đã được cập nhật thành công!',
+];
