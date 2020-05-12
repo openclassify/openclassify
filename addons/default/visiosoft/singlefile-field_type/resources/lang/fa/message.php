37610757d43266d355107b897170ffc228ea5d92
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:11:21 +0300

    New translations message.php (Persian)

---
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/fa/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_file'      => 'Which file would you like to use?',
+    'choose_folder'    => 'Which folder would you like to upload to?',
+    'upload'           => 'Click or drop files here to upload.',
+    'no_file_selected' => 'No file selected.',
+    'no_uploads'       => 'No files uploaded.',
+    'overwrite'        => 'has already been uploaded. Would you like to overwrite it?',
+    'uploading'        => 'Uploading',
+    'loading'          => 'Loading',
+];
