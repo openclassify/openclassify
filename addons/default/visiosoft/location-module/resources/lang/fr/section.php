ad76ba97761ca3d419ca9e30bfe64d2900f487ca
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:08:30 +0300

    New translations section.php (French)

--- addons/default/visiosoft/location-module/resources/lang/fr/section.php
+++ addons/default/visiosoft/location-module/resources/lang/fr/section.php
@@ -1,25 +1,22 @@
 <?php
 
- return[
-   "countries"   =>   [
-      "title"      =>"Des pays",
-
-   ],
-   "cities"   =>   [
-      "title"      =>"Villes",
-
-   ],
-   "districts"   =>   [
-      "title"      =>"Les quartiers",
-
-   ],
-   "neighborhoods"   =>   [
-      "title"      =>"Quartiers",
-
-   ],
-   "village"   =>   [
-      "title"      =>"Village",
-
-   ],
-
+return [
+    'countries' => [
+        'title' => 'Des pays',
+    ],
+    'cities' => [
+        'title' => 'Villes',
+    ],
+    'districts' => [
+        'title' => 'Les quartiers',
+    ],
+    'neighborhoods' => [
+        'title' => 'Quartiers',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'general' => 'General',
+    'map' => 'Map',
+    'setting' => 'Setting',
 ];
