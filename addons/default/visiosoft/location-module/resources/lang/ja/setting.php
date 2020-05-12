07c9beee14d43e9a4846b0c9036fd3092d36e888
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:13:51 +0300

    New translations setting.php (Japanese)

---
+++ addons/default/visiosoft/location-module/resources/lang/ja/setting.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => 'ホームページの場所フィールド',
+    ],
+    'list_page_location' => [
+        'name' => 'リストページの場所フィールド',
+    ],
+    'detail_page_location' => [
+        'name' => '詳細ページの場所フィールド',
+    ],
+    'show_search_location_btn' => [
+        'name' => '検索場所ボタンを表示',
+    ],
+    'default_country' => [
+        'name' => 'デフォルトの国',
+    ],
+    'create_ad_page_location' => [
+        'name' => '広告ページの場所を作成',
+    ],
+    'default_city' => [
+        'name' => 'デフォルトの都市',
+    ],
+    'default_district' => [
+        'name' => 'デフォルトの地区',
+    ],
+    'default_neighborhood' => [
+        'name' => 'デフォルトのネイバーフッド',
+    ],
+
+    'google_map_key' => [
+        'name' => 'GoogleマップAPIキー',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'デフォルトの長い',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'デフォルトの緯度',
+    ],
+];
