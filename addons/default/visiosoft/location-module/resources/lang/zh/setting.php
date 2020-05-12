3d1721d9ee3490caa8ec45deed149a1622d5b113
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:49 +0300

    New translations setting.php (Chinese Traditional)

---
+++ addons/default/visiosoft/location-module/resources/lang/zh/setting.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => '主頁位置字段',
+    ],
+    'list_page_location' => [
+        'name' => '列表頁面位置字段',
+    ],
+    'detail_page_location' => [
+        'name' => '詳細頁面位置字段',
+    ],
+    'show_search_location_btn' => [
+        'name' => '顯示搜索位置按鈕',
+    ],
+    'default_country' => [
+        'name' => '默認國家',
+    ],
+    'create_ad_page_location' => [
+        'name' => '創建廣告頁面位置',
+    ],
+    'default_city' => [
+        'name' => '默認城市',
+    ],
+    'default_district' => [
+        'name' => '默認區',
+    ],
+    'default_neighborhood' => [
+        'name' => '默認鄰居',
+    ],
+
+    'google_map_key' => [
+        'name' => 'Google Maps Api密鑰',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => '默認長',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => '默認緯度',
+    ],
+];
