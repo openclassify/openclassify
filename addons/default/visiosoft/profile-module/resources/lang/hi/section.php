222e368899c18b24dbd1c5d4cbb3187b51105273
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:29:04 +0300

    New translations section.php (Hindi)

---
+++ addons/default/visiosoft/profile-module/resources/lang/hi/section.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'प्रोफ़ाइल',
+    ],
+    'adress' => [
+        'title' => 'पता',
+    ],
+    'general_setting' => 'सामान्य सेटिंग्स',
+];
