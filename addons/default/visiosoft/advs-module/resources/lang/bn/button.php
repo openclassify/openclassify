f728a9a578874c4e3ca52c05e20ac026a2ebb544
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:29:28 +0300

    New translations button.php (Bengali)

---
+++ addons/default/visiosoft/advs-module/resources/lang/bn/button.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'new_adv' => 'নতুন অ্যাড',
+    'new_category' => 'নতুন বিভাগ',
+    'new_country' => 'নতুন দেশ',
+    'new_city' => 'নতুন শহর',
+    'new_district' => 'নতুন জেলা',
+    'new_neighborhood' => 'নিউ নেবারহুড',
+    'new_village' => 'নতুন গ্রাম',
+    'edit' => "সম্পাদন করা",
+    'approve' => "অনুমোদন করা",
+    'decline' => 'পতন',
+    'extend' => 'প্রসারিত করা',
+    'extend_all' => 'সমস্ত প্রসারিত করুন',
+    'convert_currency' => 'রূপান্তর করুন',
+    'categories' => 'ধরন',
+    'sub_category' => 'উপ বিভাগ',
+    'add_sub_category' => 'সাব ক্যাটাগরি যুক্ত করুন',
+    'sub_cities' => 'সাব শহর',
+    'add_sub_cities' => 'সাব সিটি যুক্ত করুন',
+    'add_sub_districts' => 'সাব জেলা যোগ করুন',
+    'add_sub_neighborhoods' => 'সাব নেবারহুড যুক্ত করুন',
+    'add_sub_village' => 'সাব ভিলেজ যুক্ত করুন',
+    'sub_districts' => 'উপ জেলা',
+    'sub_neighborhoods' => 'সাব নেবারহুডস',
+    'sub_village' => 'সাব ভিলেজ',
+    'general_settings' => 'সাধারণ সেটিংস',
+    'save_category' => 'বিভাগ সংরক্ষণ করুন',
+    'change_owner' => 'মালিক পরিবর্তন করুন',
+    'update_category' => [
+        'name' => 'আপডেট বিভাগ'
+    ],
+];
