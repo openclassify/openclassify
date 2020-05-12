b3bb6ad843000d78d86304ee9b5f2aa11be03774
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:44 +0300

    New translations button.php (Vietnamese)

---
+++ addons/default/visiosoft/profile-module/resources/lang/vi/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'new_profile' => 'Hồ sơ mới',
+    'new_adress' => 'Địa chỉ mới',
+    'show' => 'Hiển thị địa chỉ',
+    'update_password' => 'Cập nhật mật khẩu',
+    'update_profile' => 'Cập nhật hồ sơ',
+    'delete' => 'Xóa bỏ',
+    'extend' => 'Mở rộng',
+    'extend_all' => 'Mở rộng tất cả',
+    'edit' => 'Biên tập',
+    'go_profile' => 'Đi đến chi tiết hồ sơ',
+    'go_user' => 'Đi đến chi tiết người dùng',
+];
