4916186bd9a9dd4b14b0cd6d8d83d44fa910da62
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:16:45 +0300

    New translations field.php (Dutch)

--- addons/default/visiosoft/location-module/resources/lang/nl/field.php
+++ addons/default/visiosoft/location-module/resources/lang/nl/field.php
@@ -1,41 +1,38 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Titel",
-
-   ],
-   "slug"   =>   [
-      "name"      =>"Naaktslak",
-
-   ],
-   "city"   =>   [
-      "name"      =>"stad",
-
-   ],
-   "country"   =>   [
-      "name"      =>"land",
-
-   ],
-   "district"   =>   [
-      "name"      =>"Wijk",
-
-   ],
-   "order"   =>   [
-      "name"      =>"sorteervolgorde",
-
-   ],
-   "neighborhood"   =>   [
-      "name"      =>"Buurt",
-
-   ],
-   "village"   =>   [
-      "name"      =>"Dorp",
-
-   ],
-   "description"   =>   [
-      "name"      =>"Omschrijving",
-
-   ],
-
+return [
+    'name' => [
+        'name' => 'Titel'
+    ],
+    'slug' => [
+        'name' => 'Naaktslak'
+    ],
+    'city' => [
+        'name' => 'stad'
+    ],
+    'country' => [
+        'name' => 'land'
+    ],
+    'district' => [
+        'name' => 'Wijk'
+    ],
+    'order' => [
+        'name' => 'sorteervolgorde'
+    ],
+    'neighborhood' => [
+        'name' => 'Buurt'
+    ],
+    'village' => [
+        'name' => 'Dorp'
+    ],
+    'description' => [
+        'name' => 'Omschrijving'
+    ],
+    'pick_option' => [
+        'name' => 'Pick an option',
+    ],
+    'address' => [
+        'name' => 'Address',
+    ],
+    'selected' => 'Selected'
 ];
