f9fb19021a4ec46cf5aff3baef35128dde997660
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:06:46 +0300

    New translations button.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/button.php
@@ -8,13 +8,13 @@ return [
     'new_district' => 'Nuevo distrito',
     'new_neighborhood' => 'Barrio nuevo',
     'new_village' => 'Pueblo Nuevo',
-    'edit' => "Edit",
+    'edit' => "Editar",
     'approve' => "Aprobar",
     'decline' => 'Disminución',
-    'extend' => 'Extend',
-    'extend_all' => 'Extend All',
-    'convert_currency' => 'Convert Currency',
-    'categories' => 'Categories',
+    'extend' => 'Ampliar',
+    'extend_all' => 'Extender todo',
+    'convert_currency' => 'Convertir moneda',
+    'categories' => 'Categorias',
     'sub_category' => 'Subcategorías',
     'add_sub_category' => 'Añadir subcategoría',
     'sub_cities' => 'Sub ciudades',
@@ -26,9 +26,9 @@ return [
     'sub_neighborhoods' => 'Sub Vecindarios',
     'sub_village' => 'Sub Village',
     'general_settings' => 'Configuración general',
-    'save_category' => 'Save Category',
-    'change_owner' => 'Change Owner',
+    'save_category' => 'Guardar categoría',
+    'change_owner' => 'Cambio de propietario',
     'update_category' => [
-        'name' => 'Update Category'
+        'name' => 'Actualizar categoría'
     ],
 ];
