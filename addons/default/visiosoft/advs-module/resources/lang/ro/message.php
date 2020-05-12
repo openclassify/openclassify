389b88347e85f0a5700df30f7946189959d6c80e
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:04:52 +0300

    New translations message.php (Romanian)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ro/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'error_added_cart' => 'Produsul solicitat lipsește în prezent sau este inactiv',
+    'please_payment' => 'Te rog plateste',
+    'bank_account_info' => 'Informații despre contul bancar',
+    'agreement' => 'Dacă faceți clic pe Postare, sunteți de acord cu Termenii și condițiile noastre de utilizare a confidențialității, recunoașteți că sunteți proprietarul de drept al acestui articol.',
+    'update_category_msg' => 'Categoria clasificată va fi actualizată.',
+    'updated_category_msg' => 'Categoria a fost modificată cu succes.',
+    'create_ad_with_post_cat' => [
+        'title' => "Selectarea categoriilor a fost finalizată.",
+        'success' => true,
+        'continueBtn' => "Continua",
+    ],
+    'update_category_info' => 'Vă rugăm să actualizați categoria.',
+    'filter_by_search_msg' => 'Filtrează după căutare.',
+    'delete_author_error' => 'Nu puteți șterge o postare care nu vă aparține',
+    'success_delete' => 'Anunț șters',
+    'pending_ad_status' => 'Anunțul dvs. este în așteptarea aprobării editorului.',
+    'extended' => ': numărul (anunțurile) a fost extins cu succes.',
+    'category_not_exist' => 'Această categorie nu mai există!',
+    'currency_converted' => 'Moneda convertită cu succes.',
+    'choose_user' => 'Alegeți un utilizator',
+    'owner_updated_successfully' => 'Proprietarul a fost actualizat cu succes!',
+];
