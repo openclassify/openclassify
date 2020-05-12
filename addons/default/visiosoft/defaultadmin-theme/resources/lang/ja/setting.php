7fe8d32fba496020400ba0184124f310abba9892
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:12:23 +0300

    New translations setting.php (Japanese)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ja/setting.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'サイドバーホバー',
+        'instructions' => 'ホバー時にサイドバーを展開しますか？',
+    ],
+    'navigation'    => [
+        'name'         => 'ナビゲーション',
+        'instructions' => '指定 <em>デフォルト</em> ナビゲーションの順序を。',
+        'warning'      => '最初のアクセス可能なナビゲーション項目は、 <strong>ホーム</strong> エリアとして使用されます。',
+        'reorder'      => 'プライマリナビゲーションアイテムを <strong>サイドバー</strong> にドラッグアンドドロップして並べ替えます。',
+    ],
+];
