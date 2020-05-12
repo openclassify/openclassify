86ea1af913b89fc6695d5589e22e2d27268d3fc8
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:34 +0300

    New translations section.php (Chinese Traditional)

---
+++ addons/default/visiosoft/location-module/resources/lang/zh/section.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'countries' => [
+        'title' => '國別',
+    ],
+    'cities' => [
+        'title' => '城市',
+    ],
+    'districts' => [
+        'title' => '地區',
+    ],
+    'neighborhoods' => [
+        'title' => '鄰里',
+    ],
+    'village' => [
+        'title' => '村',
+    ],
+    'general' => '一般',
+    'map' => '地圖',
+    'setting' => '設置',
+];
