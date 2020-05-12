476e6d84d2ef8cb6f35937953d22b7dadba38bb2
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:26:12 +0300

    New translations addon.php (Chinese Simplified)

--- addons/default/visiosoft/base-theme/resources/lang/zh/addon.php
+++ addons/default/visiosoft/base-theme/resources/lang/zh/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => '基本主題',
-    'name'        => '基本主題',
-    'description' => '基本主題'
+    'title'       => '基本主题',
+    'name'        => '基本主题',
+    'description' => '基本主题'
 ];
