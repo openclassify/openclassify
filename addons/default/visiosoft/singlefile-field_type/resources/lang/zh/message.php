bee2003f881aaa12baf383f6c50b7e19fa94917b
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:42 +0300

    New translations message.php (Chinese Traditional)

---
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/zh/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_file'      => '您要使用哪個文件？',
+    'choose_folder'    => '您想上傳到哪個文件夾？',
+    'upload'           => '單擊或將文件拖放到此處進行上傳。',
+    'no_file_selected' => '未選擇文件。',
+    'no_uploads'       => '沒有文件上傳。',
+    'overwrite'        => '已經上傳。您要覆蓋它嗎？',
+    'uploading'        => '上載中',
+    'loading'          => '載入中',
+];
