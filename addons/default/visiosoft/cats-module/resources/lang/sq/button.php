661f70430861b79f91bb640725541d56d942c74f
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:10:46 +0300

    New translations button.php (Albanian)

--- addons/default/visiosoft/cats-module/resources/lang/sq/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/sq/button.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'new_category' => 'Kategoria e Re',
+    'new_category' => 'Kategoria e re',
     'add_sub_category' => 'Shtoni Nën Kategorinë',
     'sub_category' => 'Trego Nën Kategorinë',
-    'new_placeholderforsearch' => 'Vendndodhësi i ri i vendndodhjes',
+    'new_placeholderforsearch' => 'Vendndodhësi i ri ',
 ];
