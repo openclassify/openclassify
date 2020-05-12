5ea47561e628f2e8c43d8ec6fe5c74fe45280dc9
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:08 +0300

    New translations permission.php (Chinese Traditional)

---
+++ addons/default/visiosoft/cats-module/resources/lang/zh/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'category' => [
+        'name'   => '類別',
+        'option' => [
+            'read'   => '可以閱讀類別嗎？',
+            'write'  => '可以創建/編輯類別嗎？',
+            'delete' => '可以刪除類別嗎？',
+        ],
+    ],
+    'placeholderforsearch' => [
+        'name'   => '搜索佔位符',
+        'option' => [
+            'read'   => '可以閱讀佔位符以進行搜索嗎？',
+            'write'  => '可以創建/編輯佔位符以進行搜索嗎？',
+            'delete' => '可以刪除佔位符進行搜索嗎？',
+        ],
+    ],
+];
