e66b05b91acbccb09375475e015e46e20e06d73e
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:08:04 +0300

    New translations setting.php (Spanish)

--- addons/default/visiosoft/profile-module/resources/lang/es/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/es/setting.php
@@ -2,9 +2,9 @@
 
 return [
     'show_my_ads' => [
-        'name' => 'Show My Ads Tab',
+        'name' => 'Mostrar la pestaña Mis anuncios',
     ],
     'upload_avatar' => [
-        'name' => 'Avatar Upload',
+        'name' => 'Carga de avatar',
     ],
 ];
