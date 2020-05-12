f9c8d6aa339c5a5c762b6a5cf354b7a7e83309bb
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:18:49 +0300

    New translations help.php (Swedish)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/sv/help.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'                     => 'Hjälpsamma resurser',
+    'description'               => 'Hitta hjälp och upptäck funktioner med dessa användbara resurser.',
+    'documentation_link'        => 'OpenClassify-dokumentation',
+    'documentation_description' => 'Hitta dokumentation för OpenClassify, utveckla addons och använda addons också.',
+    'slack_link'                => 'Slack Team',
+    'slack_description'         => 'Hitta och kommunicera med andra OpenClassify-användare och utvecklare.',
+    'forum_link'                => 'Diskussionsforum',
+    'forum_description'         => 'Hitta svar och skicka frågor om användning och utveckling med OpenClassify.',
+    'addons_link'               => 'Tillgängliga Addons',
+    'addons_description'        => 'Upptäck tillägg tillgängliga för OpenClassify.',
+];
