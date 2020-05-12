43c787f4d3d22e8baaf9e78c5c09d741a7c1bb76
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:13:40 +0300

    New translations message.php (Japanese)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/ja/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_folder'     => 'どのフォルダにアップロードしますか？',
+    'upload'            => 'İmagesを追加する| İmagesをアップロードする|写真を添付',
+    'choose_files'      => 'どのファイルを使用しますか？',
+    'no_files_selected' => 'ファイルが選択されていません。',
+    'no_uploads'        => 'アップロードされたファイルはありません。',
+    'overwrite'         => 'すでにアップロードされています。上書きしますか？',
+    'uploading'         => 'アップロードしています',
+    'loading'           => '読み込み中',
+];
