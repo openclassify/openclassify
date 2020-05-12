b2a04fbd60872b63316b727eeaf652cfd4756346
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:20:56 +0300

    New translations button.php (Swedish)

---
+++ addons/default/visiosoft/profile-module/resources/lang/sv/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'new_profile' => 'Ny profil',
+    'new_adress' => 'Ny adress',
+    'show' => 'Visa adress',
+    'update_password' => 'Uppdatera lösenord',
+    'update_profile' => 'Uppdatera profil',
+    'delete' => 'Radera',
+    'extend' => 'Förlänga',
+    'extend_all' => 'Förläng alla',
+    'edit' => 'Redigera',
+    'go_profile' => 'Gå till profildetalj',
+    'go_user' => 'Gå till användarinformation',
+];
