9bbb20b0be60518720d1989fd04f72a444662f32
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:13:12 +0300

    New translations stream.php (Japanese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ja/stream.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => '前売',
+    ],
+    'categories' => [
+        'name' => 'カテゴリー',
+    ],
+    'village' => [
+        'name' => '村',
+    ],
+    'custom_fields' => [
+        'name' => 'カスタムフィールド',
+    ],
+    'custom_field_advs' => [
+        'name' => 'カスタムフィールド広告',
+    ],
+    'cf_values' => [
+        'name' => 'Cf値',
+    ],
+];
