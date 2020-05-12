30c5a5f5804f79836640fcf5fea4388d51d88774
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:26 +0300

    New translations button.php (Vietnamese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/vi/button.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'new_adv' => 'Adv mới',
+    'new_category' => 'Thể loại mới',
+    'new_country' => 'Quốc gia mới',
+    'new_city' => 'Thành phố mới',
+    'new_district' => 'Quận mới',
+    'new_neighborhood' => 'Khu phố mới',
+    'new_village' => 'Ngôi làng mới',
+    'edit' => "Biên tập",
+    'approve' => "Phê duyệt",
+    'decline' => 'Từ chối',
+    'extend' => 'Mở rộng',
+    'extend_all' => 'Mở rộng tất cả',
+    'convert_currency' => 'Chuyển tiền tệ',
+    'categories' => 'Thể loại',
+    'sub_category' => 'Danh mục phụ',
+    'add_sub_category' => 'Thêm danh mục phụ',
+    'sub_cities' => 'Thành phố phụ',
+    'add_sub_cities' => 'Thêm thành phố phụ',
+    'add_sub_districts' => 'Thêm quận phụ',
+    'add_sub_neighborhoods' => 'Thêm khu phố phụ',
+    'add_sub_village' => 'Thêm làng phụ',
+    'sub_districts' => 'Các quận phụ',
+    'sub_neighborhoods' => 'Khu dân cư phụ',
+    'sub_village' => 'Làng phụ',
+    'general_settings' => 'Cài đặt chung',
+    'save_category' => 'Lưu danh mục',
+    'change_owner' => 'Chủ sở hữu thay đổi',
+    'update_category' => [
+        'name' => 'Cập nhật danh mục'
+    ],
+];
