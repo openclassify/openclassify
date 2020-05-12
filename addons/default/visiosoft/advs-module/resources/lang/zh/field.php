bff001a254e3f5fc015ddf94981b5209a4f46853
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:26:38 +0300

    New translations field.php (Chinese Simplified)

---
+++ addons/default/visiosoft/advs-module/resources/lang/zh/field.php
@@ -0,0 +1,291 @@
+<?php
+
+return [
+    'name' => [
+        'name' => '标题'
+    ],
+    'order' => [
+        'name' => '订购'
+    ],
+    'slug' => [
+        'name' => 'ug'
+    ],
+    'parent_category' => [
+        'name' => '父母'
+    ],
+    'category' => [
+        'name' => '分类标题'
+    ],
+    'price' => [
+        'name' => '价钱'
+    ],
+    'date' => [
+        'name' => '日期'
+    ],
+    'photo' => [
+        'name' => '照片'
+    ],
+    'video' => [
+        'name' => '视频'
+    ],
+    'map' => [
+        'name' => '地图'
+    ],
+    'stock' => [
+        'name' => '股票'
+    ],
+    'advs_desc' => [
+        'name' => '描述'
+    ],
+    'online_payment' => [
+        'name' => '网上支付'
+    ],
+    'city' => [
+        'name' => '市'
+    ],
+    'country' => [
+        'name' => '国家'
+    ],
+    'district' => [
+        'name' => '区'
+    ],
+    'neighborhood' => [
+        'name' => '邻里'
+    ],
+    'village' => [
+        'name' => '村'
+    ],
+    'files' => [
+        'name' => '图片'
+    ],
+    'cat1' => [
+        'name' => '主分类'
+    ],
+    'cat2' => [
+        'name' => '子类别1'
+    ],
+    'cat3' => [
+        'name' => '子类别2'
+    ],
+    'cat4' => [
+        'name' => '子类别3'
+    ],
+    'cat5' => [
+        'name' => '子类别4'
+    ],
+    'cat6' => [
+        'name' => '子类别5'
+    ],
+    'cat7' => [
+        'name' => '子类别6'
+    ],
+    'currency' => [
+        'name' => '货币'
+    ],
+    'status' => [
+        'name' => '状态',
+        'option' => [
+            'approved' => '已批准',
+            'expired' => '已过期',
+            'unpublished' => '未发表',
+            'pending_admin' => '待处理管理员',
+            'pending_user' => '待处理的用户'
+        ]
+    ],
+    'pending_adv' => [
+        'name' => '未发表'
+    ],
+    'finish_at' => [
+        'name' => '在完成'
+    ],
+    'cover_photo' => [
+        'name' => '封面照片'
+    ],
+    'favs_adv' => [
+        'name' => '收藏广告'
+    ],
+    'my_adv' => [
+        'name' => '虚拟助理'
+    ],
+    'description' =>  '描述',
+    'explanation' =>  '说明',
+    'type' => [
+        'name' => '类型'
+    ],
+    'parent_adv' => [
+        'name' => '相关广告'
+    ],
+    'value' => [
+        'name' => '值'
+    ],
+    'list' => '清单',
+    'home' => '家',
+    'list-page' => '清单页面',
+    'search' => '搜索',
+    'recommended' => '为您推荐的广告',
+    'filter' => '过滤',
+    'sort' => '分类',
+    'sort_by' => '排序方式',
+    'pick_option' => '选择一个选项',
+    'pick_ordering' => '定购',
+    'price_high' => '价格从高到低',
+    'price_low' => '价格从低到高',
+    'newest' => '最新',
+    'oldest' => '最老的',
+    'address_a_z' => '地址（A到Z）',
+    'address_z_a' => '地址（Z到A）',
+    'categories' => '分类目录',
+    'all_categories' => '所有类别',
+    'location' => '位置',
+    'send_message' => '发信息',
+    'phone_gsm' => 'Gsm电话',
+    'phone_office' => '办公室电话',
+    'add_to_favorites' => "添加到收藏夹",
+    'send' => '发送',
+    'search_name' => '搜索名称',
+    'my_favorite_searches' => '最喜欢的搜寻',
+    'search_desc' => '您可以在此处添加和管理自己喜欢的搜索选择。',
+    'complaint_placeholder' => '在此处输入您的投诉详细信息。',
+    'show_details' => '显示详细资料',
+    'ad_actions' => '广告动作',
+    'edit' => '编辑',
+    'approve' => '批准',
+    'passive' => "被动",
+    'offered_by' => '由...提供',
+    'more_ads_by' => '更多广告',
+    'click_phone' => '显示电话号码',
+    'share' => '分享',
+    'report' => '举报此广告',
+    'features' => '特征',
+    "streetview" => "街景",
+    'recommended_ads' => '为您推荐的广告',
+    'new' => '新',
+    'short_info' => '简短信息',
+    'secure_trading' => '安全交易',
+    '24/7' => '24/7支持',
+    'easy_trading' => '轻松交易',
+    'need_help' => '需要帮忙？',
+    'give_a_call' => '打个电话',
+    'message_sent' => '讯息已发送！',
+    'asap' => '邮件已发送，卖家将尽快与您联系。',
+    'ad_id' => '广告编号',
+    'contact_with' => '联系方式',
+    'create_adv' => '制作广告',
+    'edit_adv' => '修改广告',
+    'mandatory_fields' => '必须填写',
+    'ad_post' => '广告发布',
+    'sell_item' => '出售物品或服务',
+    'search_save' => '收藏搜索成功保存',
+    'address' => '地址',
+    'email' => '电子邮件',
+    'phone' => '电话',
+    'send_us_your_feedback' => '向我们发送您的反馈意见',
+    'online_shopping' => '得到',
+    'approved' => '批准成功',
+    'extended' => '成功扩展',
+    'declined' => '成功拒绝',
+    'search_on_map' => '在地图上搜寻',
+    'post_free_ad' => '发布免费广告',
+    'select_a_category' => '选择一个类别',
+    'select_a_sub_category' => '选择一个子分类',
+    'all_dopings_added' => '为此广告添加了所有掺杂类型。',
+    'is_get_adv' => [
+        'name' => '得到'
+    ],
+    'corporate_info' => '公司资料',
+    'featured_ads' => "精选广告",
+    'trending_ads' => 'Trendind广告',
+    'search_ads_result_message' => [
+        'name' => '：count个针对：name搜索找到的广告',
+    ],
+    'total' => '总',
+    'quantity' => '数量',
+    'view_cart' => '查看购物车',
+    'no_ads' => '找不到广告。',
+    'show_display_mode' => [
+        'map' => '地图',
+        'list' => '清单',
+    ],
+    'advs_list_table_thead' => [
+        'name' => '广告名称',
+        'price' => '价钱',
+        'publish_at' => '广告日期',
+        'location_city' => '市',
+        'location_country' => '国家',
+    ],
+    'show_list_view' => [
+        'name' => '清单',
+    ],
+    'show_table_view' => [
+        'name' => '表',
+    ],
+    'showcase' => [
+        'name' => '展示柜',
+    ],
+    'new_ad' => [
+        'name' => '新广告',
+    ],
+    'profile' => [
+        'name' => '轮廓',
+    ],
+    'my_ads' => [
+        'name' => '我的广告',
+    ],
+    'sold_out' => [
+        'name' => '卖光了',
+    ],
+    'edit_category' => '编辑类别',
+    'set_category' => '设置类别',
+    'max' => [
+        'name' => '最高',
+    ],
+    'min' => [
+        'name' => '敏',
+    ],
+    'in_the_last_24_hours' => [
+        'name' => '在过去的24小时内'
+    ],
+    'in_the_last_week' => [
+        'name' => '在最后一周'
+    ],
+    'in_the_last_month' => [
+        'name' => '在上个月'
+    ],
+    'ads_with_photo' => [
+        'name' => '带有照片的广告'
+    ],
+    'ads_with_video' => [
+        'name' => '带有视频的广告'
+    ],
+    'yes' => [
+        'name' => '是'
+    ],
+    "no_location" => "未选择位置。",
+    "continue" => '继续',
+    "gallery" => '画廊',
+
+    // Pending screen
+    'pending_message_1' => '广告正在审批过程中',
+    'pending_message_2' => "广告的更新日期",
+    'pending_message_3' => '预计轮候时间',
+    'pending_message_4' => '审批过程中花费的时间将添加到掺杂时间中。',
+    'pending_message_5' => '在批准过程中，无法从呼叫中心进行有关广告的交易。',
+    'hours' => '小时',
+
+    // Preview page
+    'preview_text' => [
+        'part_1' => '如果以下有关您的广告的信息正确无误，请单击“继续”按钮，然后转到下一步。',
+        'part_2' => '如果没有，请单击“编辑”按钮。',
+    ],
+    'views' => '观看次数',
+
+    // Detail page
+    "no_street_view" => "此位置没有街景图像",
+    'edit_my_contact_info' => '编辑我的联系信息',
+    'update_my_contact_info' => '更新我的联系方式',
+    'contact_info' => '联系信息',
+    'ad_info' => '广告资讯',
+
+    // Select cat
+    'select_category_step_by_step' => '逐步选择类别',
+];
