91bb06faf8192f84f18eb604d42e9a878b17d0a7
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:55 +0300

    New translations stream.php (Chinese Traditional)

---
+++ addons/default/visiosoft/advs-module/resources/lang/zh/stream.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => '進階',
+    ],
+    'categories' => [
+        'name' => '分類目錄',
+    ],
+    'village' => [
+        'name' => '村',
+    ],
+    'custom_fields' => [
+        'name' => '自訂欄位',
+    ],
+    'custom_field_advs' => [
+        'name' => '自定義字段廣告',
+    ],
+    'cf_values' => [
+        'name' => 'CF值',
+    ],
+];
