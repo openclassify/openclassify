c520df2de86d9a3608a0325e32da4cbe7b213e69
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:07 +0300

    New translations field.php (Chinese Traditional)

---
+++ addons/default/visiosoft/cats-module/resources/lang/zh/field.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'name' => [
+        'name' => '標題',
+        'instructions' => '它是您將定義的主要類別的名稱。它使用戶可以輕鬆找到所需的產品。',
+    ],
+    'order' => [
+        'name' => '訂購',
+    ],
+
+    'slug' => [
+        'name' => 'ug',
+        'instructions' => '指定任何組織標記以幫助將您的帖子與其他人分組。',
+
+    ],
+    'files' => [
+        'name' => '圖片',
+    ],
+    'seo_keyword' => [
+        'name' => '徐關鍵詞',
+        'instructions' => '在Google瀏覽器中添加到網站的字詞顯示在搜索結果上方。',
+    ],
+    'seo_description' => [
+        'name' => '搜索引擎優化說明',
+        'instructions' => '這可能會影響訪問者是否要單擊搜索結果中內容的決定。',
+    ],
+    'icon' => [
+        'name' => '圖標',
+        'instructions' => '用於添加指示類別類型的圖標。',
+    ],
+
+    'please_wait' => '請稍候。正在刪除子類別',
+    'category_selection' => '類別選擇',
+];
