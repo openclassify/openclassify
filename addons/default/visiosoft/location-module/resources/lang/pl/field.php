7719e6da40f430974ceb595d2dae205d67e7bf5a
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:11:58 +0300

    New translations field.php (Polish)

---
+++ addons/default/visiosoft/location-module/resources/lang/pl/field.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Tytuł'
+    ],
+    'slug' => [
+        'name' => 'Ślimak'
+    ],
+    'city' => [
+        'name' => 'Miasto'
+    ],
+    'country' => [
+        'name' => 'Kraj'
+    ],
+    'district' => [
+        'name' => 'Dzielnica'
+    ],
+    'order' => [
+        'name' => 'Porządek sortowania'
+    ],
+    'neighborhood' => [
+        'name' => 'Sąsiedztwo'
+    ],
+    'village' => [
+        'name' => 'Wioska'
+    ],
+    'description' => [
+        'name' => 'Opis'
+    ],
+    'pick_option' => [
+        'name' => 'Wybierz opcję',
+    ],
+    'address' => [
+        'name' => 'Adres',
+    ],
+    'selected' => 'Wybrany'
+];
