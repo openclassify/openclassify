84426208eaf9f9e31ce9f29044c634688d1c7510
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:13:44 +0300

    New translations button.php (Japanese)

---
+++ addons/default/visiosoft/profile-module/resources/lang/ja/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'new_profile' => '新しいプロフィール',
+    'new_adress' => '新しいアドレス',
+    'show' => '住所を表示',
+    'update_password' => 'パスワードを更新',
+    'update_profile' => 'プロフィールを更新',
+    'delete' => '削除する',
+    'extend' => '伸ばす',
+    'extend_all' => 'すべて拡張',
+    'edit' => '編集する',
+    'go_profile' => 'プロファイルの詳細に移動',
+    'go_user' => 'ユーザーの詳細に移動',
+];
