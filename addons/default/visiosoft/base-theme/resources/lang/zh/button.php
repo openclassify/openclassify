77b19434e4ff5bc2da7a8d3c7f4cfb8fdb155775
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:23:58 +0300

    New translations button.php (Chinese Traditional)

---
+++ addons/default/visiosoft/base-theme/resources/lang/zh/button.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'post_ad' => [
+        'name' => '發布廣告',
+    ],
+    'login' => '登錄',
+    'register' => '寄存器',
+    'continue' => '繼續',
+    'reset_password' => '重設密碼',
+    'email' => '電子郵件',
+    'phone' => '電話',
+];
