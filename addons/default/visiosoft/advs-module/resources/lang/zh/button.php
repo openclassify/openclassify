adb3f924b6abe13a4f5bfdebbbabbb262e898968
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:26:29 +0300

    New translations button.php (Chinese Simplified)

---
+++ addons/default/visiosoft/advs-module/resources/lang/zh/button.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'new_adv' => '新进阶',
+    'new_category' => '新类别',
+    'new_country' => '新国家',
+    'new_city' => '新城市',
+    'new_district' => '新区',
+    'new_neighborhood' => '新邻居',
+    'new_village' => '新村',
+    'edit' => "编辑",
+    'approve' => "批准",
+    'decline' => '下降',
+    'extend' => '延伸',
+    'extend_all' => '全部延伸',
+    'convert_currency' => '转换货币',
+    'categories' => '分类目录',
+    'sub_category' => '子类别',
+    'add_sub_category' => '添加子类别',
+    'sub_cities' => '子城市',
+    'add_sub_cities' => '添加子城市',
+    'add_sub_districts' => '添加分区',
+    'add_sub_neighborhoods' => '添加子社区',
+    'add_sub_village' => '添加子村',
+    'sub_districts' => '分区',
+    'sub_neighborhoods' => '子社区',
+    'sub_village' => '子村',
+    'general_settings' => '常规设置',
+    'save_category' => '保存类别',
+    'change_owner' => '变更拥有者',
+    'update_category' => [
+        'name' => '更新类别'
+    ],
+];
