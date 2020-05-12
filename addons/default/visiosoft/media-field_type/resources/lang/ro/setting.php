184c84a99e9833e5ca5813a3b10d504d831dbe86
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:03:55 +0300

    New translations setting.php (Romanian)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/ro/setting.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'imageCanvasW' => [
+        'name' => 'Lățimea panzei imaginii'
+    ],
+    'imageCanvasH' => [
+        'name' => 'Imagine Canvas Înălțime'
+    ],
+    'imageResizeW' => [
+        'name' => 'Lățimea redimensionării imaginii'
+    ],
+    'imageResizeH' => [
+        'name' => 'Înălțimea dimensiunii imaginii'
+    ],
+];
