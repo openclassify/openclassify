826a34924cb78107c0d772558d14d7f91c9ec937
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:13:30 +0300

    New translations control_panel.php (Hebrew)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/he/control_panel.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'help'      => 'עזרה',
+    'search'    => 'לחפש',
+    'logout'    => 'להתנתק',
+    'view_site' => 'צפה באתר',
+    'title'     => 'לוח בקרה',
+    'search_placeholder'     => 'הקלד לחיפוש',
+
+    // Users Module
+    'search_by_gsm_number'     => 'חיפוש לפי מספר GSM',
+];
