861d94d681fdcde4992b5b091794a074580e0491
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:13:13 +0300

    New translations setting.php (Japanese)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ja/setting.php
@@ -0,0 +1,130 @@
+<?php
+
+return [
+    'auto_approve' => [
+        'name' => '自動承認',
+    ],
+    'default_view_type' => [
+        'name' => 'デフォルトのビュータイプ',
+    ],
+    'hide_zero_price' => [
+        'name' => 'ゼロの場合は価格を非表示',
+        'instructions' => '広告価格が0の場合、価格を非表示にします',
+    ],
+    'estimated_pending_time' => [
+        'name' => '推定保留時間',
+        'instructions' => '時間で',
+    ],
+    'default_published_time' => [
+        'name' => 'デフォルトの公開時間',
+    ],
+    'address' => [
+        'name' => 'お問い合わせ',
+    ],
+    'phone' => [
+        'name' => '連絡先電話',
+    ],
+    'favicon' => [
+        'name' => 'ファビコン',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => '長い座標をマップ',
+    ],
+    'map_coordinates_lat' => [
+        'name' => '地図の緯度座標',
+    ],
+    'iban_numbers' => [
+        'name' => 'イバン',
+    ],
+    'logo' => [
+        'name' => 'ロゴ',
+    ],
+    'ogImage' => [
+        'name' => 'ソーシャルシェアロゴ',
+        'warning'      => '1200 x 630の解像度である必要があります。 ',
+
+    ],
+    'currencies' => [
+        'name' => '有効通貨',
+    ],
+    'latest-limit' => [
+        'name' => '最新の制限',
+    ],
+    'site_address' => [
+        'name' => 'サイトアドレス',
+        'warning'      => '自動追加www。 www。<strong>sitename.com</strong> ',
+    ],
+    'google_map_key' => [
+        'name' => 'GoogleマップAPIキー',
+    ],
+
+    'default_country' => [
+        'name' => 'デフォルトの広告国',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => '通貨コンバーターAPIキー'
+    ],
+    'default_city' => [
+        'name' => 'デフォルトの広告都市',
+    ],
+    'default_district' => [
+        'name' => 'デフォルトの広告地区',
+    ],
+    'default_neighborhood' => [
+        'name' => 'デフォルトの広告近傍',
+    ],
+    'default_currency' => [
+        'name' => 'デフォルトの広告通貨',
+    ],
+    'default_GET' => [
+        'name' => 'デフォルト広告GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'サムネイルの幅',
+    ],
+    'thumbnail_height' => [
+        'name' => 'サムネイルの高さ',
+    ],
+    'picture_width' => [
+        'name' => '画像の幅',
+    ],
+    'picture_height' => [
+        'name' => '画像の高さ',
+    ],
+    'twitter' => [
+        'name' => 'ツイッター',
+    ],
+    'facebook' => [
+        'name' => 'フェイスブック',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'グーグル',
+    ],
+    'watermark_type' => [
+        'name' => '透かしタイプ',
+    ],
+    'watermark_text' => [
+        'name' => '透かしテキスト',
+    ],
+    'watermark_image' => [
+        'name' => '透かし画像',
+    ],
+    'watermark_position' => [
+        'name' => '透かしの位置',
+    ],
+    'listing_page_image' => [
+        'name' => 'リストページの画像',
+    ],
+    'enabled_currencies' => [
+        'name' => '有効な通貨',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google統計コード',
+    ],
+    'register_email_field' => [
+        'name' => '会員登録メールの要件',
+    ],
+];
