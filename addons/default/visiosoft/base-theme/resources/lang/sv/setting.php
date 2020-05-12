b2987b4141c7af2e8be6687a3604676bb225361b
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:19:04 +0300

    New translations setting.php (Swedish)

---
+++ addons/default/visiosoft/base-theme/resources/lang/sv/setting.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Navigeringstitel',
+    ],
+    'navigation_action' => [
+        'name' => 'Navigeringsåtgärd',
+    ],
+    'country_fields' => [
+        'name' => 'Landsfält',
+    ],
+    'date_fields' => [
+        'name' => 'Datumfält',
+    ],
+    'price_fields' => [
+        'name' => 'Prisfält',
+    ],
+    'breadcrumbs' => [
+        'name' => 'Brödsmula',
+    ],
+    'ad_details' => [
+        'name' => 'Annonsuppgifter',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Fliken Annonsinformation',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Senaste och visa alla Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'Registrera sidinstruktionslogotyp',
+    ],
+    'style' => [
+        'name' => 'Stil (CSS)',
+    ],
+];
