2a33b7383b39d7d910cc9feeb474768c0d8f1495
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:18 +0300

    New translations preference.php (Chinese Traditional)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/zh/preference.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => '邊欄懸停',
+        'instructions' => '展開懸停邊欄？',
+    ],
+    'navigation'    => [
+        'name'         => '導航',
+        'instructions' => '指定您的 <em>個人</em> 導航順序。',
+        'warning'      => '第一個可訪問的導航項目用作您的 <strong>home</strong> 區域。',
+        'reorder'      => '將主要導航項目拖放到 <strong>側欄</strong> 以對其重新排序。',
+    ],
+    'display'       => [
+        'name'         => '顯示密度',
+        'instructions' => '緊湊的顯示屏可讓更多內容立即顯示在屏幕上。',
+        'option'       => [
+            'default' => '默認',
+            'compact' => '緊湊',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => '側邊欄模式',
+        'instructions' => '靜態邊欄將始終可見。',
+        'option'       => [
+            'default' => '默認',
+            'static'  => '靜態的',
+        ],
+    ],
+];
