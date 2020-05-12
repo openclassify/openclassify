9cff688198a665d72057a59a750768daf7914a24
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:24:40 +0300

    New translations message.php (Vietnamese)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/vi/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_folder'     => 'Thư mục nào bạn muốn tải lên?',
+    'upload'            => 'Thêm phần mềm | Tải lên | Đính kèm ảnh',
+    'choose_files'      => 'Những tập tin bạn muốn sử dụng?',
+    'no_files_selected' => 'Không có tập tin được chọn.',
+    'no_uploads'        => 'Không có tập tin tải lên.',
+    'overwrite'         => 'đã được tải lên. Bạn có muốn ghi đè lên nó?',
+    'uploading'         => 'Đang tải lên',
+    'loading'           => 'Đang tải',
+];
