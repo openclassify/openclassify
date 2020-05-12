728857c1100a4c0bf627b425a8df81db983409fb
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:20:00 +0300

    New translations help.php (Ukrainian)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/uk/help.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'                     => 'Корисні ресурси',
+    'description'               => 'Знайдіть довідку та знайдіть функції за допомогою цих корисних ресурсів.',
+    'documentation_link'        => 'OpenClassify Documentation',
+    'documentation_description' => 'Знайдіть документацію для OpenClassify, розробки аддонів та також використання аддонів.',
+    'slack_link'                => 'Ковзна команда',
+    'slack_description'         => 'Пошук і спілкування з іншими користувачами та розробниками OpenClassify.',
+    'forum_link'                => 'Дискусійний форум',
+    'forum_description'         => 'Знайдіть відповіді та опублікуйте питання щодо використання та розробки за допомогою OpenClassify.',
+    'addons_link'               => 'Доступні додатки',
+    'addons_description'        => 'Відкрийте для себе додатки, доступні для OpenClassify.',
+];
