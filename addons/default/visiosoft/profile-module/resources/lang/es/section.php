22dde472cb5a90a92e7128555816ed6b4097ec0f
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:07:01 +0300

    New translations section.php (Spanish)

--- addons/default/visiosoft/profile-module/resources/lang/es/section.php
+++ addons/default/visiosoft/profile-module/resources/lang/es/section.php
@@ -1,13 +1,11 @@
 <?php
 
- return[
-   "profile"   =>   [
-      "title"      =>"Perfil",
-
-   ],
-   "adress"   =>   [
-      "title"      =>"Dirección",
-
-   ],
-
+return [
+    'profile' => [
+        'title' => 'Perfil',
+    ],
+    'adress' => [
+        'title' => 'Dirección',
+    ],
+    'general_setting' => 'General Settings',
 ];
