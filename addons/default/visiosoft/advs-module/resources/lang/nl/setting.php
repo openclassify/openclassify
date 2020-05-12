3e87bd912a142cb60b2c51cfe541033690ec0f67
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:14:09 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -1,136 +1,130 @@
 <?php
 
- return[
-   "auto_approve"   =>   [
-      "name"      =>"Automatisch goedkeuren",
+return [
+    'auto_approve' => [
+        'name' => 'Automatisch goedkeuren',
+    ],
+    'default_view_type' => [
+        'name' => 'Default View Type',
+    ],
+    'hide_zero_price' => [
+        'name' => 'Hide Price if Zero',
+        'instructions' => 'Hide the price if the ad price is 0',
+    ],
+    'estimated_pending_time' => [
+        'name' => 'Estimated Pending Time',
+        'instructions' => 'In Hours',
+    ],
+    'default_published_time' => [
+        'name' => 'Standaard publicatietijd',
+    ],
+    'address' => [
+        'name' => 'Contact adres',
+    ],
+    'phone' => [
+        'name' => 'Contact telefoon',
+    ],
+    'favicon' => [
+        'name' => 'Favicon',
+    ]
+    ,'map_coordinates_long' => [
+        'name' => 'Lange coördinaten in kaart brengen',
+    ],
+    'map_coordinates_lat' => [
+        'name' => 'Kaart Lat Coordinate',
+    ],
+    'iban_numbers' => [
+        'name' => 'IBAN',
+    ],
+    'logo' => [
+        'name' => 'Logo',
+    ],
+    'ogImage' => [
+        'name' => 'Social Share-logo',
+        'warning'      => 'Het moet een resolutie hebben van 1200 x 630.',
 
-   ],
-   "default_published_time"   =>   [
-      "name"      =>"Standaard publicatietijd",
+    ],
+    'currencies' => [
+        'name' => 'Actieve valuta\\'s',
+    ],
+    'latest-limit' => [
+        'name' => 'Latest Limit',
+    ],
+    'site_address' => [
+        'name' => 'Siteadres',
+        'warning'      => 'Auto toegevoegd www. www. <strong>sitename.com</strong>',
+    ],
+    'google_map_key' => [
+        'name' => 'Google Map Api Key',
+    ],
 
-   ],
-   "default_adv_limit"   =>   [
-      "name"      =>"Standaardadvertentielimiet",
-
-   ],
-   "address"   =>   [
-      "name"      =>"Contact adres",
-
-   ],
-   "phone"   =>   [
-      "name"      =>"Contact telefoon",
-
-   ],
-   "contact_mail"   =>   [
-      "name"      =>"Contact Mail",
-      "warning"      =>"Automatisch toegevoegde sitename <strong>info</strong> @sitename",
-
-   ],
-   "map_coordinates_long"   =>   [
-      "name"      =>"Lange coördinaten in kaart brengen",
-
-   ],
-   "map_coordinates_lat"   =>   [
-      "name"      =>"Kaart Lat Coordinate",
-
-   ],
-   "logo"   =>   [
-      "name"      =>"Logo",
-
-   ],
-   "ogImage"   =>   [
-      "name"      =>"Social Share-logo",
-      "warning"      =>"Het moet een resolutie hebben van 1200 x 630.",
-
-   ],
-   "currencies"   =>   [
-      "name"      =>"Actieve valuta\'s",
-
-   ],
-   "site_address"   =>   [
-      "name"      =>"Siteadres",
-      "warning"      =>"Auto toegevoegd www. www. <strong>sitename.com</strong>",
-
-   ],
-   "google_map_key"   =>   [
-      "name"      =>"Google Map Api Key",
-
-   ],
-   "default_country"   =>   [
-      "name"      =>"Standaardadvertentieland",
-
-   ],
-   "free_currencyconverterapi_key"   =>   [
-      "name"      =>"Valutacalculator API-sleutel",
-
-   ],
-   "default_city"   =>   [
-      "name"      =>"Standaard advertentiestad",
-
-   ],
-   "default_district"   =>   [
-      "name"      =>"Standaard advertentiedistrict",
-
-   ],
-   "default_neighborhood"   =>   [
-      "name"      =>"Standaard advertentieomgeving",
-
-   ],
-   "default_currency"   =>   [
-      "name"      =>"Standaard advertentievaluta",
-
-   ],
-   "default_GET"   =>   [
-      "name"      =>"Standaardadvertentie GET",
-
-   ],
-   "twitter"   =>   [
-      "name"      =>"tjilpen",
-
-   ],
-   "facebook"   =>   [
-      "name"      =>"Facebook",
-
-   ],
-   "youtube"   =>   [
-      "name"      =>"Youtube",
-
-   ],
-   "google"   =>   [
-      "name"      =>"Google",
-
-   ],
-   "watermark_type"   =>   [
-      "name"      =>"Watermerk Type",
-
-   ],
-   "watermark_text"   =>   [
-      "name"      =>"Watermerktekst",
-
-   ],
-   "watermark_image"   =>   [
-      "name"      =>"Watermerk afbeelding",
-
-   ],
-   "watermark_position"   =>   [
-      "name"      =>"Watermerkpositie",
-
-   ],
-   "watermark_opacity"   =>   [
-      "name"      =>"Watermerk Dekking",
-
-   ],
-   "listing_page_image"   =>   [
-      "name"      =>"Lijstpagina-afbeelding",
-
-   ],
-   "enabled_currencies"   =>   [
-      "name"      =>"Ingeschakelde valuta\'s",
-
-   ],
-   "google_statistic_code"   =>   [
-      "name"      =>"Google-statistiekcode",
-
-   ],
-
+    'default_country' => [
+        'name' => 'Standaardadvertentieland',
+    ],
+    'free_currencyconverterapi_key' => [
+        'name' => 'Valutacalculator API-sleutel'
+    ],
+    'default_city' => [
+        'name' => 'Standaard advertentiestad',
+    ],
+    'default_district' => [
+        'name' => 'Standaard advertentiedistrict',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Standaard advertentieomgeving',
+    ],
+    'default_currency' => [
+        'name' => 'Standaard advertentievaluta',
+    ],
+    'default_GET' => [
+        'name' => 'Standaardadvertentie GET',
+    ],
+    'thumbnail_width' => [
+        'name' => 'Thumbnail Width',
+    ],
+    'thumbnail_height' => [
+        'name' => 'Thumbnail Height',
+    ],
+    'picture_width' => [
+        'name' => 'Picture Width',
+    ],
+    'picture_height' => [
+        'name' => 'Picture Height',
+    ],
+    'twitter' => [
+        'name' => 'tjilpen',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+    ],
+    'youtube' => [
+        'name' => 'Youtube',
+    ],
+    'google' => [
+        'name' => 'Google',
+    ],
+    'watermark_type' => [
+        'name' => 'Watermerk Type',
+    ],
+    'watermark_text' => [
+        'name' => 'Watermerktekst',
+    ],
+    'watermark_image' => [
+        'name' => 'Watermerk afbeelding',
+    ],
+    'watermark_position' => [
+        'name' => 'Watermerkpositie',
+    ],
+    'listing_page_image' => [
+        'name' => 'Lijstpagina-afbeelding',
+    ],
+    'enabled_currencies' => [
+        'name' => 'Ingeschakelde valuta\\'s',
+    ],
+    'google_statistic_code' => [
+        'name' => 'Google-statistiekcode',
+    ],
+    'register_email_field' => [
+        'name' => 'Member Registration Email Requirement',
+    ],
 ];
