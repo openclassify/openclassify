7ea805d12e4fb065456231c219fcae4804315b19
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:30:25 +0300

    New translations section.php (Bengali)

---
+++ addons/default/visiosoft/profile-module/resources/lang/bn/section.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'প্রোফাইল',
+    ],
+    'adress' => [
+        'title' => 'adress',
+    ],
+    'general_setting' => 'সাধারণ সেটিংস',
+];
