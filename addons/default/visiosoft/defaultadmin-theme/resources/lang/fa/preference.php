c96fcb0748dd070dae7128c6970871b48bf99466
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:11:39 +0300

    New translations preference.php (Persian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/preference.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/preference.php
@@ -1,10 +1,30 @@
 <?php
 
 return [
-    'navigation' => [
+    'sidebar_hover' => [
+        'name'         => 'Sidebar Hover',
+        'instructions' => 'Expand the sidebar on hover?',
+    ],
+    'navigation'    => [
         'name'         => 'منو ها',
         'instructions' => 'ترتیب منوها را مشخص کنید',
         'warning'      => 'اولین آیتم در منو به عنوان خانه در نظر گرفته می شود',
         'reorder'      => 'برای مرتب کردن آیتم ها را در سایدبار با موس جابجا کنید',
+    ],
+    'display'       => [
+        'name'         => 'Display Density',
+        'instructions' => 'Compact display allows more content to be shown on the screen at once.',
+        'option'       => [
+            'default' => 'Default',
+            'compact' => 'Compact',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => 'Sidebar Mode',
+        'instructions' => 'Static sidebars will always be visible.',
+        'option'       => [
+            'default' => 'Default',
+            'static'  => 'Static',
+        ],
     ],
 ];
