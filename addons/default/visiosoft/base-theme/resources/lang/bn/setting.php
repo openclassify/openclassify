e6d1149246be5ecd18e4d80aad8985d7ee30a582
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:30:14 +0300

    New translations setting.php (Bengali)

---
+++ addons/default/visiosoft/base-theme/resources/lang/bn/setting.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'নেভিগেশন শিরোনাম',
+    ],
+    'navigation_action' => [
+        'name' => 'নেভিগেশন অ্যাকশন',
+    ],
+    'country_fields' => [
+        'name' => 'দেশের ক্ষেত্রসমূহ',
+    ],
+    'date_fields' => [
+        'name' => 'তারিখের ক্ষেত্রগুলি',
+    ],
+    'price_fields' => [
+        'name' => 'মূল্য ক্ষেত্রসমূহ',
+    ],
+    'breadcrumbs' => [
+        'name' => 'ব্রেডক্রম্বে',
+    ],
+    'ad_details' => [
+        'name' => 'বিজ্ঞাপনের বিবরণ',
+    ],
+    'ad_details_tab' => [
+        'name' => 'বিজ্ঞাপনের বিবরণ ট্যাব',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'সর্বশেষ এবং দেখুন সমস্ত বিটিএন',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'পৃষ্ঠা নির্দেশিকা লোগো নিবন্ধন করুন',
+    ],
+    'style' => [
+        'name' => 'স্টাইল (css)',
+    ],
+];
