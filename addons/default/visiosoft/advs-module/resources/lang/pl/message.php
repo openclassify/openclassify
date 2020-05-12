503a30f10da0f1f7351d5dbaec670905e2410afc
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:10:05 +0300

    New translations message.php (Polish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/pl/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'error_added_cart' => 'Żądany produkt jest obecnie niedostępny lub nieaktywny',
+    'please_payment' => 'Proszę zapłacić',
+    'bank_account_info' => 'informacje o koncie bankowym',
+    'agreement' => 'Klikając przycisk Post, akceptujesz nasze Warunki użytkowania i Politykę prywatności, potwierdzając, że jesteś prawowitym właścicielem tego elementu.',
+    'update_category_msg' => 'Kategoria sklasyfikowana zostanie zaktualizowana.',
+    'updated_category_msg' => 'Kategoria pomyślnie edytowana.',
+    'create_ad_with_post_cat' => [
+        'title' => "Wybór kategorii został zakończony.",
+        'success' => true,
+        'continueBtn' => "Kontyntynuj",
+    ],
+    'update_category_info' => 'Zaktualizuj kategorię.',
+    'filter_by_search_msg' => 'Filtruj według wyszukiwania.',
+    'delete_author_error' => 'Nie możesz usunąć posta, który nie należy do ciebie',
+    'success_delete' => 'Reklama usunięta',
+    'pending_ad_status' => 'Twoja reklama oczekuje na akceptację Redakcji.',
+    'extended' => ': liczba reklam została pomyślnie przedłużona.',
+    'category_not_exist' => 'Ta kategoria już nie istnieje!',
+    'currency_converted' => 'Pomyślnie przeliczono walutę.',
+    'choose_user' => 'Wybierz użytkownika',
+    'owner_updated_successfully' => 'Właściciel został zaktualizowany pomyślnie!',
+];
