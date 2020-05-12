d2f4b7f1a6292ad873abb9f05dba4150b2412781
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:12:06 +0300

    New translations stream.php (Korean)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ko/stream.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => 'Advs',
+    ],
+    'categories' => [
+        'name' => '카테고리',
+    ],
+    'village' => [
+        'name' => '마을',
+    ],
+    'custom_fields' => [
+        'name' => '맞춤 입력란',
+    ],
+    'custom_field_advs' => [
+        'name' => '맞춤 필드 광고',
+    ],
+    'cf_values' => [
+        'name' => 'CF 값',
+    ],
+];
