b803801b5849999afba6e24984d8c4eac8f9f0e4
Fatih Alp <mail@fatihalp.com>
2020-05-12 13:09:04 +0300

    New translations field.php (French)

--- addons/default/visiosoft/cats-module/resources/lang/fr/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/fr/field.php
@@ -1,21 +1,35 @@
 <?php
 
- return[
-   "name"   =>   [
-      "name"      =>"Titre",
+return [
+    'name' => [
+        'name' => 'Titre',
+        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
+    ],
+    'order' => [
+        'name' => 'Ordre',
+    ],
 
-   ],
-   "order"   =>   [
-      "name"      =>"Ordre",
+    'slug' => [
+        'name' => 'Limace',
+        'instructions' => 'Specify any organizational tags to help group your post with others.',
 
-   ],
-   "slug"   =>   [
-      "name"      =>"Limace",
+    ],
+    'files' => [
+        'name' => 'Image',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword',
+        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Description',
+        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
+    ],
+    'icon' => [
+        'name' => 'Icon',
+        'instructions' => 'It is used to add icons indicating the category type.',
+    ],
 
-   ],
-   "files"   =>   [
-      "name"      =>"Image",
-
-   ],
-   "please_wait"   =>"Veuillez patienter. Suppression des sous-catégories"
+    'please_wait' => 'Veuillez patienter. Suppression des sous-catégories',
+    'category_selection' => 'Category Selection',
 ];
