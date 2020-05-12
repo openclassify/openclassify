1c4416e32f6d50c3799172ec61ed2501bbe5b058
Fatih Alp <mail@fatihalp.com>
2020-05-12 19:28:45 +0300

    New translations setting.php (Hindi)

---
+++ addons/default/visiosoft/base-theme/resources/lang/hi/setting.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'नेविगेशन शीर्षक',
+    ],
+    'navigation_action' => [
+        'name' => 'नेविगेशन क्रिया',
+    ],
+    'country_fields' => [
+        'name' => 'देश के क्षेत्र',
+    ],
+    'date_fields' => [
+        'name' => 'दिनांक फ़ील्ड',
+    ],
+    'price_fields' => [
+        'name' => 'मूल्य क्षेत्र',
+    ],
+    'breadcrumbs' => [
+        'name' => 'ब्रेडक्रम्ब',
+    ],
+    'ad_details' => [
+        'name' => 'विज्ञापन विवरण',
+    ],
+    'ad_details_tab' => [
+        'name' => 'विज्ञापन विवरण टैब',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'नवीनतम और देखें सभी बीटीएन',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'रजिस्टर पेज इंस्ट्रक्शन लोगो',
+    ],
+    'style' => [
+        'name' => 'शैली (सीएसएस)',
+    ],
+];
