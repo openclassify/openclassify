dc6ad7f75d1bfeca78003b89df601f77ac4c6532
Diatrex <diahshalabi@hotmail.com>
2020-05-19 12:24:54 +0300

    #1393 [users] Admin - Go to users ads link

---
+++ addons/default/visiosoft/defaultadmin-theme/src/Listener/AddViewAdsButton.php
@@ -0,0 +1,53 @@
+<?php namespace Visiosoft\DefaultadminTheme\Listener;
+
+use Anomaly\Streams\Platform\Ui\Table\Event\TableIsQuerying;
+use Anomaly\UsersModule\User\Contract\UserRepositoryInterface;
+use Anomaly\UsersModule\User\Table\UserTableBuilder;
+
+class AddViewAdsButton
+{
+
+    /**
+     * @var UserRepositoryInterface
+     */
+    protected $userRepository;
+
+    /**
+     * AddViewAdsAction constructor.
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param TableIsQuerying $event
+     */
+    public function handle(TableIsQuerying $event)
+    {
+        $builder = $event->getBuilder();
+
+        if (get_class($builder) == UserTableBuilder::class) {
+            $this->addViewAdsButton($builder);
+        }
+    }
+
+    /**
+     * Add a button to view ads.
+     *
+     * @param UserTableBuilder $builder
+     */
+    protected function addViewAdsButton(UserTableBuilder $builder)
+    {
+        $buttons = $builder->getButtons();
+        $dropdown = array_merge($buttons['settings']['dropdown'], [
+            "ads" => [
+                "text" => trans('visiosoft.theme.defaultadmin::button.view_ads'),
+                "href" => "admin/advs?filter_User={entry.id}"
+            ]
+        ]);
+        $buttons['settings']['dropdown'] = $dropdown;
+        $builder->setButtons($buttons);
+    }
+}
