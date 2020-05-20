8513a48777171b6b8a88d0ba303dae99bd8bfa00
fatihalp <alp@visiosoft.com.tr>
2020-05-20 14:01:31 +0300

    Change default enc. to SSL

--- config/mail.php
+++ config/mail.php
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
 
     /*
     |--------------------------------------------------------------------------
