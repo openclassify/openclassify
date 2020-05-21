48571126f0d0195ce510ba3f9971808d631f8b05
Fatih Alp <mail@fatihalp.com>
2020-05-21 18:49:19 +0300

    New translations field.php (Hebrew)

--- addons/default/visiosoft/base-theme/resources/lang/he/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/he/field.php
@@ -50,4 +50,10 @@ return [
     'secure_e-commerce_ads' => 'מודעות מסחריות מאובטחות',
     'sms' => 'שלח הודעת טקסט (SMS)',
     'mail' => 'שלח מייל',
+
+    // Register instruction seed
+    'register_instructions' => 'Register Instructions',
+    'list' => 'List',
+    'instruction_description' => 'Instruction Description',
+    'instruction_list' => 'Instruction List',
 ];
