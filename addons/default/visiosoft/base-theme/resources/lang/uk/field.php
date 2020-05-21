c57b7bf80ea6764a1ac095894bbe99dfd25d62b8
Fatih Alp <mail@fatihalp.com>
2020-05-21 18:49:24 +0300

    New translations field.php (Ukrainian)

--- addons/default/visiosoft/base-theme/resources/lang/uk/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/uk/field.php
@@ -50,4 +50,10 @@ return [
     'secure_e-commerce_ads' => 'Захищена реклама електронної комерції',
     'sms' => 'Надіслати текстове повідомлення (SMS)',
     'mail' => 'Надіслати пошту',
+
+    // Register instruction seed
+    'register_instructions' => 'Register Instructions',
+    'list' => 'List',
+    'instruction_description' => 'Instruction Description',
+    'instruction_list' => 'Instruction List',
 ];
