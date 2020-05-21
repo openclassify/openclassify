4514decd21261ae095704a523399ef67a49f8272
Fatih Alp <mail@fatihalp.com>
2020-05-21 18:49:20 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/base-theme/resources/lang/az/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/az/field.php
@@ -50,4 +50,10 @@ return [
     'secure_e-commerce_ads' => 'Təhlükəsiz e-ticarət reklamları',
     'sms' => 'Mətn mesajı göndərin (SMS)',
     'mail' => 'Məktub göndərin',
+
+    // Register instruction seed
+    'register_instructions' => 'Register Instructions',
+    'list' => 'List',
+    'instruction_description' => 'Instruction Description',
+    'instruction_list' => 'Instruction List',
 ];
