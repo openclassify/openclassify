99ea12e7421f2c57796f9511b1474a91553abe7b
Fatih Alp <mail@fatihalp.com>
2020-06-09 12:53:30 +0300

    New translations permission.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/permission.php
@@ -49,4 +49,12 @@ return [
             'delete' => 'Kan cf-waarden worden verwijderd?',
         ],
     ],
+    'options' => [
+        'name'   => 'Options',
+        'option' => [
+            'read'   => 'Can read options?',
+            'write'  => 'Can create/edit options?',
+            'delete' => 'Can delete options?',
+        ],
+    ],
 ];
