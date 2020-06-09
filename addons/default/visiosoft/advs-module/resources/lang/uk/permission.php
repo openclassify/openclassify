9a9a1406ade2fa919272bf05884abbc768a02bac
Fatih Alp <mail@fatihalp.com>
2020-06-09 12:52:01 +0300

    New translations permission.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/permission.php
@@ -49,4 +49,12 @@ return [
             'delete' => 'Чи можна видалити значення cf?',
         ],
     ],
+    'options' => [
+        'name'   => 'Options',
+        'option' => [
+            'read'   => 'Can read options?',
+            'write'  => 'Can create/edit options?',
+            'delete' => 'Can delete options?',
+        ],
+    ],
 ];
