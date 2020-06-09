76140903068cb15cf5285085706720a2f13e3500
Fatih Alp <mail@fatihalp.com>
2020-06-09 12:52:50 +0300

    New translations permission.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/permission.php
@@ -49,4 +49,12 @@ return [
             'delete' => 'Se pot șterge valorile cf?',
         ],
     ],
+    'options' => [
+        'name'   => 'Options',
+        'option' => [
+            'read'   => 'Can read options?',
+            'write'  => 'Can create/edit options?',
+            'delete' => 'Can delete options?',
+        ],
+    ],
 ];
