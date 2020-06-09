01f69a16ed08208edf31108c9160f8bbd9284a86
Fatih Alp <mail@fatihalp.com>
2020-06-09 12:53:20 +0300

    New translations permission.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/permission.php
@@ -49,4 +49,12 @@ return [
             'delete' => 'Μπορεί να διαγράψει τιμές cf;',
         ],
     ],
+    'options' => [
+        'name'   => 'Options',
+        'option' => [
+            'read'   => 'Can read options?',
+            'write'  => 'Can create/edit options?',
+            'delete' => 'Can delete options?',
+        ],
+    ],
 ];
