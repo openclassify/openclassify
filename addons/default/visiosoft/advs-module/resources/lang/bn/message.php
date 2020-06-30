47e54a47c9e3fff60abdd50d11326cd2c84e05bc
Fatih Alp <mail@fatihalp.com>
2020-07-01 01:13:45 +0300

    New translations message.php (Bengali)

--- addons/default/visiosoft/advs-module/resources/lang/bn/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/bn/message.php
@@ -22,4 +22,5 @@ return [
     'currency_converted' => 'মুদ্রা সফলভাবে রূপান্তরিত হয়েছে।',
     'choose_user' => 'একটি ব্যবহারকারী চয়ন করুন',
     'owner_updated_successfully' => 'মালিক সফলভাবে আপডেট হয়েছে!',
+    'no_add_found' => 'No Ad Found!',
 ];
