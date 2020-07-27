c3ef171af7fd41261ce635abc4a62db70cc1f624
Fatih Alp <mail@fatihalp.com>
2020-07-27 12:46:30 +0300

    New translations field.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/field.php
@@ -19,6 +19,9 @@ return [
     'price' => [
         'name' => 'Preț'
     ],
+    'standard_price' => [
+        'name' => 'Standard Price'
+    ],
     'date' => [
         'name' => 'Data'
     ],
