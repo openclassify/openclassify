6e500595ee504f0ceb93324359609cf96b261680
Fatih Alp <mail@fatihalp.com>
2020-07-27 12:46:57 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/field.php
@@ -19,6 +19,9 @@ return [
     'price' => [
         'name' => 'Qiyməti'
     ],
+    'standard_price' => [
+        'name' => 'Standard Price'
+    ],
     'date' => [
         'name' => 'Tarix'
     ],
