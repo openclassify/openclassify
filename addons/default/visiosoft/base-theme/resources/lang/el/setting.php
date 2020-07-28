b5bd1ada2d33ddaa80d0460c4c48f8b6d15b42e1
Fatih Alp <mail@fatihalp.com>
2020-07-28 12:41:11 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/base-theme/resources/lang/el/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/el/setting.php
@@ -31,6 +31,9 @@ return [
     'register_page_instruction_logo' => [
         'name' => 'Εγγραφή λογότυπου οδηγιών σελίδας',
     ],
+    'register_page_alert_link' => [
+        'name' => 'Register Page Alert Link',
+    ],
     'style' => [
         'name' => 'Στυλ (CSS)',
     ],
