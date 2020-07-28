070668a5d9b394160d9c9f985dc9e6a47c3eaa40
Fatih Alp <mail@fatihalp.com>
2020-07-28 12:40:42 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/base-theme/resources/lang/az/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/az/setting.php
@@ -31,6 +31,9 @@ return [
     'register_page_instruction_logo' => [
         'name' => 'Səhifə Təlimatı Loqosunu qeyd edin',
     ],
+    'register_page_alert_link' => [
+        'name' => 'Register Page Alert Link',
+    ],
     'style' => [
         'name' => 'Stil (CSS)',
     ],
