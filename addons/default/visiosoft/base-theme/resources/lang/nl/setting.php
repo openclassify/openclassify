ecdc497828b8891939cdd3b3e9884865e6a20c23
Fatih Alp <mail@fatihalp.com>
2020-07-28 12:41:04 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/base-theme/resources/lang/nl/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/nl/setting.php
@@ -31,6 +31,9 @@ return [
     'register_page_instruction_logo' => [
         'name' => 'Registreer Page Instructie Logo',
     ],
+    'register_page_alert_link' => [
+        'name' => 'Register Page Alert Link',
+    ],
     'style' => [
         'name' => 'Stijl (CSS)',
     ],
