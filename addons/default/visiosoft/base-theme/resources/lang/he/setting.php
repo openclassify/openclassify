25208fa0fe673d8abea3e9b13198950d5bbdbfb0
Fatih Alp <mail@fatihalp.com>
2020-07-28 12:41:10 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/base-theme/resources/lang/he/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/he/setting.php
@@ -31,6 +31,9 @@ return [
     'register_page_instruction_logo' => [
         'name' => 'רשום לוגו של הוראות הדרכה',
     ],
+    'register_page_alert_link' => [
+        'name' => 'Register Page Alert Link',
+    ],
     'style' => [
         'name' => 'סגנון (CSS)',
     ],
