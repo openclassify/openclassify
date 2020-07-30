1f50c6d37586543fdd5288ee7503ac4db60f8d7a
Fatih Alp <mail@fatihalp.com>
2020-07-30 12:22:33 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -118,6 +118,9 @@ return [
     'listing_page_image' => [
         'name' => 'Lijstpagina-afbeelding',
     ],
+    'hide_standard_price_field' => [
+        'name' => 'Hide Standard Price Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Ingeschakelde valuta\\'s',
     ],
