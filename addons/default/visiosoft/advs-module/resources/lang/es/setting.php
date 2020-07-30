3dd05265979b6b7e3904803cf365135a3cd1de70
Fatih Alp <mail@fatihalp.com>
2020-07-30 12:22:21 +0300

    New translations setting.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/setting.php
@@ -118,6 +118,9 @@ return [
     'listing_page_image' => [
         'name' => 'Imagen de la página de listado',
     ],
+    'hide_standard_price_field' => [
+        'name' => 'Hide Standard Price Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Monedas habilitadas',
     ],
