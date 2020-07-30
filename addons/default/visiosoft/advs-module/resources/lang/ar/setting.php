a94db7932545ecef229b3bf00e3926bc6910124b
Fatih Alp <mail@fatihalp.com>
2020-07-30 12:22:22 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
@@ -118,6 +118,9 @@ return [
     'listing_page_image' => [
         'name' => 'سرد صورة الصفحة',
     ],
+    'hide_standard_price_field' => [
+        'name' => 'Hide Standard Price Field',
+    ],
     'enabled_currencies' => [
         'name' => 'العملات الممكّنة',
     ],
