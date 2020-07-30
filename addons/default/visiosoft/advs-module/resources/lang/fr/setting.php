1cb096f3a8c725b2d3959771269d955143ccd9ce
Fatih Alp <mail@fatihalp.com>
2020-07-30 12:22:18 +0300

    New translations setting.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/setting.php
@@ -118,6 +118,9 @@ return [
     'listing_page_image' => [
         'name' => 'Image de page de liste',
     ],
+    'hide_standard_price_field' => [
+        'name' => 'Hide Standard Price Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Devises activées',
     ],
