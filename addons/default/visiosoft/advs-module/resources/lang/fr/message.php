f6168b25e9f5b8f25a716333ea550c58b3c4a16c
Fatih Alp <mail@fatihalp.com>
2020-08-16 22:38:56 +0300

    New translations message.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/message.php
@@ -23,4 +23,6 @@ return [
     'choose_user' => 'Choisissez un utilisateur',
     'owner_updated_successfully' => 'Le propriétaire a été mis à jour avec succès!',
     'no_add_found' => 'No Ad Found!',
+    'replicated_success' => 'The ad has been replicated successfully!',
+    'ad_doesnt_exist' => "This ad doesn't exist!",
 ];
