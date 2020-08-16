a4ce7cd7effee609dfb0063472535e2b98dd241a
Fatih Alp <mail@fatihalp.com>
2020-08-16 22:38:50 +0300

    New translations message.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/message.php
@@ -23,4 +23,6 @@ return [
     'choose_user' => 'Välj en användare',
     'owner_updated_successfully' => 'Ägaren har uppdaterats framgångsrikt!',
     'no_add_found' => 'No Ad Found!',
+    'replicated_success' => 'The ad has been replicated successfully!',
+    'ad_doesnt_exist' => "This ad doesn't exist!",
 ];
