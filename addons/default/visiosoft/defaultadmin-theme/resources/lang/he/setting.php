75ea3ed00cd51e45a1a366508a792782a0680993
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:45 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/he/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/he/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'פריט הניווט הנגיש הראשון משמש כאזור <strong>ביתי</strong>.',
         'reorder'      => 'גרור ושחרר את הפריטים ניווט העיקרי <strong>הצדדי</strong> כדי לסדר אותן מחדש.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
