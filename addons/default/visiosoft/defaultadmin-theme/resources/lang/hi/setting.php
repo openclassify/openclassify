b24455f0056689aa7e001fa627fa5facc917a936
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:34:10 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/hi/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/hi/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'पहला सुलभ नेविगेशन आइटम <strong>होम</strong> क्षेत्र के रूप में उपयोग किया जाता है।',
         'reorder'      => 'उन्हें पुन: व्यवस्थित करने के लिए <strong>साइडबार</strong> में प्राथमिक नेविगेशन आइटम खींचें और छोड़ें।',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
