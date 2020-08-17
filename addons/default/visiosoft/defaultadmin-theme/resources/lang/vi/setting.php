4440e9b23b369dddfb46e37449b0a1e09be97b6e
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:54 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/vi/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/vi/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'Mục điều hướng có thể truy cập đầu tiên được sử dụng làm khu vực <strong>nhà</strong>.',
         'reorder'      => 'Kéo và thả các mục điều hướng chính trong thanh bên <strong></strong> để sắp xếp lại chúng.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
