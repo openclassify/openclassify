b9f63345194fa6f71b911e39f59264cfd236bb92
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:44 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/el/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/el/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'Το πρώτο προσβάσιμο στοιχείο πλοήγησης χρησιμοποιείται ως περιοχή <strong>home</strong>.',
         'reorder'      => 'Μεταφέρετε και αποθέστε τα κύρια στοιχεία πλοήγησης στην <strong>πλευρική γραμμή</strong> για να τα αναδιατάξετε.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
