7f0cdbc85339d3a0580713da3c5089a3d3a2b27b
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:51 +0300

    New translations setting.php (Bengali)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/bn/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/bn/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'প্রথম অ্যাক্সেসযোগ্য নেভিগেশন আইটেমটি <strong>হোম</strong> অঞ্চল হিসাবে ব্যবহৃত হয়।',
         'reorder'      => 'পুনরায় ক্রম করতে প্রাথমিক নেভিগেশন আইটেমগুলিকে <strong>সাইডবার</strong> টেনে আনুন।',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
