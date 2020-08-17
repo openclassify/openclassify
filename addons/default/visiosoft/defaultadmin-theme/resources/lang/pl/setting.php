07546766102c5e730d819b05e532958fd4a852e9
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:58 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/pl/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/pl/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'Pierwszy dostępny element nawigacyjny jest używany jako obszar <strong>domu</strong>.',
         'reorder'      => 'Przeciągnij i upuść podstawowe elementy nawigacji na pasku bocznym <strong></strong> aby zmienić ich kolejność.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
