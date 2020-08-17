c2b5069773b5d07c0c6feed88add5ef778122786
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:49 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/az/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/az/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'İlk əlçatan naviqasiya elementi <strong>ev</strong> sahə kimi istifadə olunur.',
         'reorder'      => 'Sıralamaq üçün <strong>yan çubuğun</strong> də ilkin naviqasiya elementlərini sürükləyin və buraxın.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
