d4c7e6d17fa7846f870c6f259b4c5892671951ce
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:39 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/ro/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ro/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'Primul articol accesibil de navigare este utilizat ca zonă <strong>acasă</strong>.',
         'reorder'      => 'Trageți și fixați elementele principale de navigație în bara laterală <strong></strong> pentru a le reordona.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
