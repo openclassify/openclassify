154b4b147553a5b94dfa2410b3e4315116075545
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:34:01 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/ru/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ru/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'Первый доступный элемент навигации используется как область <strong>home</strong>.',
         'reorder'      => 'Перетащите основные элементы навигации на боковой панели <strong></strong> чтобы изменить их порядок.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
