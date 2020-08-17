df099dba1e607ec4626084753fc8e3f5e36fb117
Fatih Alp <mail@fatihalp.com>
2020-08-17 16:33:41 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/ar/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ar/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'يتم استخدام أول عنصر تنقل يمكن الوصول إليه باعتباره منطقة <strong>home</strong>.',
         'reorder'      => 'سحب وإسقاط البنود الملاحة الابتدائية في <strong>الشريط الجانبي</strong> إلى إعادة ترتيب لهم.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
