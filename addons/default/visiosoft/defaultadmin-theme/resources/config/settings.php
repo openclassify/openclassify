9a4e01ab589f3635bd484dd625e41b7918393b1e
Diatrex <diahshalabi@hotmail.com>
2020-08-17 10:41:54 +0300

    #1898 [admin-theme] Theme Options

--- addons/default/visiosoft/defaultadmin-theme/resources/config/settings.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/config/settings.php
@@ -6,4 +6,34 @@ return [
         'type'       => 'anomaly.field_type.textarea',
         'input_view' => 'visiosoft.theme.defaultadmin::admin/navigation/settings',
     ],
+    "icon" => [
+        "type"   => "anomaly.field_type.file",
+        "config" => [
+            "folders"       => ['images'],
+        ]
+    ],
+    "title" => [
+        "type"   => "anomaly.field_type.text",
+        "config" => [
+            "default_value" => 'OpenClassify',
+        ]
+    ],
+    "sidebar_main_color" => [
+        "type"   => "anomaly.field_type.colorpicker",
+        "config" => [
+            "default_value" => '#252525',
+        ]
+    ],
+    "sidebar_secondary_color" => [
+        "type"   => "anomaly.field_type.colorpicker",
+        "config" => [
+            "default_value" => '#333333',
+        ]
+    ],
+    "active_sidebar_border_color" => [
+        "type"   => "anomaly.field_type.colorpicker",
+        "config" => [
+            "default_value" => '#c61db4',
+        ]
+    ],
 ];
--- addons/default/visiosoft/defaultadmin-theme/resources/lang/en/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/en/setting.php
@@ -11,4 +11,19 @@ return [
         'warning'      => 'The first accessible navigation item is used as the <strong>home</strong> area.',
         'reorder'      => 'Drag and drop the primary navigation items in the <strong>sidebar</strong> to reorder them.',
     ],
+    "icon" => [
+        'name' => 'Icon',
+    ],
+    "title" => [
+        'name' => 'Title',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Main Color',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Secondary Color',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Active Sidebar Border Color',
+    ]
 ];
