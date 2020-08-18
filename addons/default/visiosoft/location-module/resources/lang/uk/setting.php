660f4eeacf533392f6921fc1217c9bc58d818f1c
Fatih Alp <mail@fatihalp.com>
2020-08-18 19:23:07 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/location-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/uk/setting.php
@@ -40,4 +40,8 @@ return [
     'map_coordinates_lat' => [
         'name' => 'Ширина за замовчуванням',
     ],
+
+    'hide_location_filter' => [
+        'name' => 'Hide Location Filter',
+    ],
 ];
