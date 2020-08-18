4b8b0f62a50ca9ff80093a18ea67ffb79e182350
Fatih Alp <mail@fatihalp.com>
2020-08-18 19:23:54 +0300

    New translations section.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/section.php
@@ -40,6 +40,7 @@ return [
     'general' => 'Général',
     'ads' => 'Les publicités',
     'user' => 'Utilisateur',
+    'filter' => 'Filter',
     'options' => [
         'title' => 'Options',
     ],
