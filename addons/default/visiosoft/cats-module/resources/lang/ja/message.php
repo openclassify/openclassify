9b7336c3126f5080da4b332711dac42b26ee4a61
Fatih Alp <mail@fatihalp.com>
2020-08-21 17:46:06 +0300

    New translations message.php (Japanese)

--- addons/default/visiosoft/cats-module/resources/lang/ja/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/ja/message.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'categories_mass_delete_success' => 'Categories and related sub-categories has been deleted successfully!',
+    'categories_mass_delete_success' => 'カテゴリと関連するサブカテゴリが正常に削除されました！',
 ];
