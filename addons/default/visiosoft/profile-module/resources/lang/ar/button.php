756f10991c7f8df08877fc9d22308c0d94a391eb
Fatih Alp <mail@fatihalp.com>
2020-08-21 17:39:14 +0300

    New translations button.php (Arabic)

--- addons/default/visiosoft/profile-module/resources/lang/ar/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/ar/button.php
@@ -12,6 +12,6 @@ return [
     'edit' => 'تعديل',
     'go_profile' => 'الذهاب إلى تفاصيل الملف الشخصي',
     'go_user' => 'انتقل إلى تفاصيل المستخدم',
-    'personal' => 'Personal',
-    'corporate' => 'Corporate',
+    'personal' => 'شخصي',
+    'corporate' => 'الشركات',
 ];
