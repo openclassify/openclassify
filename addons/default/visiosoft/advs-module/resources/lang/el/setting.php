32c1debb74a63551958a041580c6b01163ec0afd
Fatih Alp <mail@fatihalp.com>
2020-08-26 15:58:20 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/setting.php
@@ -79,6 +79,9 @@ return [
     'default_GET' => [
         'name' => 'Προεπιλεγμένη διαφήμιση GET',
     ],
+    'add_canvas' => [
+        'name' => 'Add Canvas',
+    ],
     'thumbnail_width' => [
         'name' => 'Πλάτος μικρογραφιών',
     ],
@@ -145,5 +148,8 @@ return [
     ],
     'hide_map_filter' => [
         'name' => 'Απόκρυψη φίλτρου χάρτη',
+    ],
+    'show_lang_url' => [
+        'name' => 'Show Lang Parameter For URL',
     ],
 ];
