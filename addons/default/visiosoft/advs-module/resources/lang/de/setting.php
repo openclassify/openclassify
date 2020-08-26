2c465af77d60ca233aab2f0a33afb8741decca8e
Fatih Alp <mail@fatihalp.com>
2020-08-26 15:58:21 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/setting.php
@@ -79,6 +79,9 @@ return [
     'default_GET' => [
         'name' => 'Standardanzeige GET',
     ],
+    'add_canvas' => [
+        'name' => 'Add Canvas',
+    ],
     'thumbnail_width' => [
         'name' => 'Miniaturbildbreite',
     ],
@@ -145,5 +148,8 @@ return [
     ],
     'hide_map_filter' => [
         'name' => 'Kartenfilter ausblenden',
+    ],
+    'show_lang_url' => [
+        'name' => 'Show Lang Parameter For URL',
     ],
 ];
