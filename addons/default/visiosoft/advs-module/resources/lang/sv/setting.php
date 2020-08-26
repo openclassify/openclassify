76b0e40a950312c121b05d8116556c3eb5dd405c
Fatih Alp <mail@fatihalp.com>
2020-08-26 15:58:05 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
@@ -79,6 +79,9 @@ return [
     'default_GET' => [
         'name' => 'Standardannons GET',
     ],
+    'add_canvas' => [
+        'name' => 'Add Canvas',
+    ],
     'thumbnail_width' => [
         'name' => 'Miniatyrbredd',
     ],
@@ -145,5 +148,8 @@ return [
     ],
     'hide_map_filter' => [
         'name' => 'Dölj kartfilter',
+    ],
+    'show_lang_url' => [
+        'name' => 'Show Lang Parameter For URL',
     ],
 ];
