f5dd79b067a73c8380bf10bcf002995edf3f3d07
Fatih Alp <mail@fatihalp.com>
2020-08-26 15:58:22 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
@@ -79,6 +79,9 @@ return [
     'default_GET' => [
         'name' => 'الإعلان الافتراضي',
     ],
+    'add_canvas' => [
+        'name' => 'Add Canvas',
+    ],
     'thumbnail_width' => [
         'name' => 'عرض الصورة المصغرة',
     ],
@@ -145,5 +148,8 @@ return [
     ],
     'hide_map_filter' => [
         'name' => 'إخفاء عامل تصفية الخريطة',
+    ],
+    'show_lang_url' => [
+        'name' => 'Show Lang Parameter For URL',
     ],
 ];
