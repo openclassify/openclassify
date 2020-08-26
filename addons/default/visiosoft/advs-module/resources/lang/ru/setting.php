1f5d491cae455dd2b904c5d7a000190a277aaa52
Fatih Alp <mail@fatihalp.com>
2020-08-26 15:58:09 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
@@ -79,6 +79,9 @@ return [
     'default_GET' => [
         'name' => 'Реклама по умолчанию GET',
     ],
+    'add_canvas' => [
+        'name' => 'Add Canvas',
+    ],
     'thumbnail_width' => [
         'name' => 'Ширина миниатюры',
     ],
@@ -145,5 +148,8 @@ return [
     ],
     'hide_map_filter' => [
         'name' => 'Скрыть фильтр карты',
+    ],
+    'show_lang_url' => [
+        'name' => 'Show Lang Parameter For URL',
     ],
 ];
