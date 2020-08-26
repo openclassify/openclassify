40b1a71904c6d36d34e2507e5d629867b512e6ff
Fatih Alp <mail@fatihalp.com>
2020-08-26 15:58:13 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -79,6 +79,9 @@ return [
     'default_GET' => [
         'name' => 'Standaardadvertentie GET',
     ],
+    'add_canvas' => [
+        'name' => 'Add Canvas',
+    ],
     'thumbnail_width' => [
         'name' => 'Thumbnail breedte',
     ],
@@ -145,5 +148,8 @@ return [
     ],
     'hide_map_filter' => [
         'name' => 'Verberg kaartfilter',
+    ],
+    'show_lang_url' => [
+        'name' => 'Show Lang Parameter For URL',
     ],
 ];
