7c0ccfc693d18aab0d763db796b34a997f6df857
Diatrex <diahshalabi@hotmail.com>
2020-09-07 10:10:21 +0300

    #2098 admin footer copyright is static

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/en/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/en/setting.php
@@ -17,6 +17,9 @@ return [
     "title" => [
         'name' => 'Title',
     ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
     "sidebar_main_color" => [
         'name' => 'Sidebar Main Color',
     ],
