5ee82f831bd7629346d03a7229aa52e55543a241
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:12 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/pl/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/pl/setting.php
@@ -17,6 +17,9 @@ return [
     "title" => [
         'name' => 'Tytuł',
     ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
     "sidebar_main_color" => [
         'name' => 'Główny kolor paska bocznego',
     ],
