360065c1328b2b4f2ffae17ab4fa18b439628cf0
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:58 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/cats-module/resources/lang/tr/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/tr/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Site Haritası Bölme Numarası',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
