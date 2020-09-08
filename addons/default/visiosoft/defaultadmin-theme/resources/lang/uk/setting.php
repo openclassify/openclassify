aa8cdca5ae3afc7e480e252823f36654046327be
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:36 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/uk/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/uk/setting.php
@@ -17,6 +17,9 @@ return [
     "title" => [
         'name' => 'Назва',
     ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
     "sidebar_main_color" => [
         'name' => 'Основний колір бічної панелі',
     ],
