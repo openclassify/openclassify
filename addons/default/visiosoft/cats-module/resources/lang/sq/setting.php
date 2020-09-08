425074db79885ff54d9f97e21c23c8b3ccb59a5f
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:55 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/cats-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/sq/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Numri i Ndarjes së sitit',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
