a17fa16f5da02924dca6d770c69ef59b0c4716ea
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:29 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/hi/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/hi/setting.php
@@ -17,6 +17,9 @@ return [
     "title" => [
         'name' => 'शीर्षक',
     ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
     "sidebar_main_color" => [
         'name' => 'साइडबार मेन कलर',
     ],
