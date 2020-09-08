dc8150f661c7cf1d7f90c1b3a4c487cc3a4223c9
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:35 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/tr/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/tr/setting.php
@@ -17,6 +17,9 @@ return [
     "title" => [
         'name' => 'Başlık',
     ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
     "sidebar_main_color" => [
         'name' => 'Kenar Çubuğu Ana Rengi',
     ],
