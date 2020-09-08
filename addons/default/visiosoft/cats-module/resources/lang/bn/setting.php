259391494e0daf5d0255bd8917172a38786dec1c
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:04 +0300

    New translations setting.php (Bengali)

--- addons/default/visiosoft/cats-module/resources/lang/bn/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/bn/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'সাইটম্যাপ বিভাজক নম্বর',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
