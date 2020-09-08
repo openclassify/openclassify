448b04c55b404a1766ce5feaf535c5f7ca476509
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:56 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/cats-module/resources/lang/sv/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/sv/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Sitemap Delningsnummer',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
