c3906c7f7d19580a10ec16bb0787f028ef12dade
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:01 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/cats-module/resources/lang/vi/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/vi/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Số phân chia sơ đồ trang web',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
