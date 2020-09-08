f876cdb8ff69554cce3e2ffb34bda407d2d30fc5
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:44 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/cats-module/resources/lang/hi/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/hi/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'साइटमैप विभाजन संख्या',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
