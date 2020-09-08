7e7e7930c23a7ef590a4becf2872b4f70451fa5a
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:53 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/cats-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/ru/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Разделительный номер карты сайта',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
