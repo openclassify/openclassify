05e5eae30cbd3b8e61b12ea6325e768a2519bc39
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:48 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/cats-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/nl/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Sitemap Scheidingsnummer',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
