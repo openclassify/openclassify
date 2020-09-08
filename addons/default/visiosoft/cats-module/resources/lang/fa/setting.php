1292e78678f0699b107577406b47bdb3416ace42
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:03 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/cats-module/resources/lang/fa/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/fa/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'شماره تقسیم نقشه سایت',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
