b55879be5d35c16285f2c1e9c68597e3c5b3f03b
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:07 +0300

    New translations setting.php (Spanish)

--- addons/default/visiosoft/cats-module/resources/lang/es/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/es/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Número de división del mapa del sitio',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
