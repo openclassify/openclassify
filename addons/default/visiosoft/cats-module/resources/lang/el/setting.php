c709503059e4afbffb89f1cd3280a0799a247243
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:41 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/cats-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/el/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Αριθμός διαίρεσης χάρτη ιστότοπου',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
