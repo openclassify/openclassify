706ee11ecfe7f4d0137fed991fcbc964ff33a847
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:42 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/cats-module/resources/lang/he/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/he/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'מספר חלוקת מפת האתר',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
