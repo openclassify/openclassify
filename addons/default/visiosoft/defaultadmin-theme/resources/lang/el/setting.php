a39bbdc8f6ce4426ccf932dff39ed6121258f851
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:18 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/el/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/el/setting.php
@@ -17,6 +17,9 @@ return [
     "title" => [
         'name' => 'Τίτλος',
     ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
     "sidebar_main_color" => [
         'name' => 'Κύριο χρώμα πλευρικής γραμμής',
     ],
