a46d68db6c834f84c75a8e5ee2d832447ef1b3f2
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:59 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/cats-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/uk/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Номер поділу сайту',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
