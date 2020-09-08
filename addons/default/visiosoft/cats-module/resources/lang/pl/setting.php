e613a4f44d890349ae50e2b513d5a2f6e04ce2b5
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:36 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/cats-module/resources/lang/pl/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/pl/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'Mapa witryny Dzielenie liczby',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
