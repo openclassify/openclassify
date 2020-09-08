28776981859339c39c684ebbd9cf1e8f25780530
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:33:38 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/cats-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/cats-module/resources/lang/ar/setting.php
@@ -4,4 +4,7 @@ return [
     'sitemap_dividing_number' => [
         'name' => 'رقم تقسيم خريطة الموقع',
     ],
+    'include_cities_sitemap' => [
+        'name' => 'Include Cities in Sitemap',
+    ],
 ];
