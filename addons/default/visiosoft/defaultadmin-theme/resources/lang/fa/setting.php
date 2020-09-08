4b5111682f4895fa428c98ace0c6127b30c512ed
Fatih Alp <mail@fatihalp.com>
2020-09-08 15:34:40 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fa/setting.php
@@ -17,6 +17,9 @@ return [
     "title" => [
         'name' => 'عنوان',
     ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
     "sidebar_main_color" => [
         'name' => 'رنگ اصلی نوار کناری',
     ],
