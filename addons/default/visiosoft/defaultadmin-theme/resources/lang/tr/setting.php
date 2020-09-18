c9a15a8bec72a7567ce28d06fdccd4ca0d66871d
Fatih Alp <mail@fatihalp.com>
2020-09-18 16:43:36 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/tr/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/tr/setting.php
@@ -11,4 +11,22 @@ return [
         'warning'      => 'İlk erişilebilir gezinme öğesi <strong>ana</strong> alanı olarak kullanılır.',
         'reorder'      => 'Birincil gezinme öğelerini yeniden sıralamak için <strong>kenar çubuğu</strong> sürükleyip bırakın.',
     ],
+    "icon" => [
+        'name' => 'ikon',
+    ],
+    "title" => [
+        'name' => 'Başlık',
+    ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Copyright Organization Name',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Kenar Çubuğu Ana Rengi',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Kenar Çubuğu İkincil Rengi',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Etkin Kenar Çubuğu Kenarlık Rengi',
+    ]
 ];
