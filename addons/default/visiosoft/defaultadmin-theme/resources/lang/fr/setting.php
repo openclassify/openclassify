772b0ac7d613d70b8be4763e61fd91ee5fd6f098
Fatih Alp <mail@fatihalp.com>
2020-09-23 18:41:49 +0300

    New translations setting.php (French)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/setting.php
@@ -11,4 +11,22 @@ return [
         'warning'      => 'Le premier élément de navigation accessible est utilisé comme zone <strong>origine</strong>.',
         'reorder'      => 'Faites glisser et déposez les éléments de navigation principaux dans la barre latérale <strong></strong> pour les réorganiser.',
     ],
+    "icon" => [
+        'name' => 'Icône',
+    ],
+    "title" => [
+        'name' => 'Titre',
+    ],
+    "footer_copyright_org_name" => [
+        'name' => 'Nom de l\'organisation de copyright du pied de page',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Couleur principale de la barre latérale',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Couleur secondaire de la barre latérale',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Couleur de bordure de la barre latérale active',
+    ]
 ];
