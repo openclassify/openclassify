7331a68f471e6cf3f76c7e0a8fe55c00126c2574
Fatih Alp <mail@fatihalp.com>
2020-09-23 18:45:13 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/nl/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/nl/setting.php
@@ -11,4 +11,22 @@ return [
         'warning'      => 'Het eerste toegankelijke navigatie-item wordt gebruikt als het <strong>home</strong> gebied.',
         'reorder'      => 'Sleep de primaire navigatie-items in de <strong>zijbalk</strong> om ze opnieuw te ordenen.',
     ],
+    "icon" => [
+        'name' => 'Icoon',
+    ],
+    "title" => [
+        'name' => 'Titel',
+    ],
+    "footer_copyright_org_name" => [
+        'name' => 'Voettekst Copyright naam organisatie',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Hoofdkleur zijbalk',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Secundaire kleur zijbalk',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Actieve randkleur zijbalk',
+    ]
 ];
