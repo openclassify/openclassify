d133935efb5132498cd249c95c91622799562427
Fatih Alp <mail@fatihalp.com>
2020-09-24 12:39:31 +0300

    New translations setting.php (Urdu (Pakistan))

---
+++ addons/default/visiosoft/base-theme/resources/lang/ur/setting.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'نیویگیشن کا عنوان',
+    ],
+    'navigation_action' => [
+        'name' => 'نیویگیشن ایکشن',
+    ],
+    'country_fields' => [
+        'name' => 'کنٹری فیلڈز',
+    ],
+    'date_fields' => [
+        'name' => 'تاریخ کے خانے',
+    ],
+    'price_fields' => [
+        'name' => 'قیمت کے میدان',
+    ],
+    'breadcrumbs' => [
+        'name' => 'روٹی crumb',
+    ],
+    'ad_details' => [
+        'name' => 'اشتہار کی تفصیلات',
+    ],
+    'ad_details_tab' => [
+        'name' => 'اشتہار کی تفصیلات کا ٹیب',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'تازہ ترین اور تمام Btn دیکھیں',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'صفحہ ہدایت کا لوگو رجسٹر کریں',
+    ],
+    'register_page_alert_link' => [
+        'name' => 'پیج الرٹ لنک رجسٹر کریں',
+    ],
+    'style' => [
+        'name' => 'انداز (سی ایس ایس)',
+    ],
+];
