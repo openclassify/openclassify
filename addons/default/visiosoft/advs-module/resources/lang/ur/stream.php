2835ed1cae63a66a5383cb65b5ce60c4e4d243c4
Fatih Alp <mail@fatihalp.com>
2020-09-24 12:39:38 +0300

    New translations stream.php (Urdu (Pakistan))

---
+++ addons/default/visiosoft/advs-module/resources/lang/ur/stream.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => 'ایڈز',
+    ],
+    'categories' => [
+        'name' => 'اقسام',
+    ],
+    'village' => [
+        'name' => 'گاؤں',
+    ],
+    'custom_fields' => [
+        'name' => 'کسٹم فیلڈز',
+    ],
+    'custom_field_advs' => [
+        'name' => 'کسٹم فیلڈ',
+    ],
+    'cf_values' => [
+        'name' => 'CF اقدار',
+    ],
+    'options' => [
+        'name' => 'اختیارات',
+    ],
+];
