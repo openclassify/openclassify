227e8b5d34fb31e2d5294e11543932f0495ac9c0
Fatih Alp <mail@fatihalp.com>
2020-09-24 12:39:34 +0300

    New translations field.php (Urdu (Pakistan))

---
+++ addons/default/visiosoft/base-theme/resources/lang/ur/field.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    'latest_ads' => 'تازہ ترین اشتہارات',
+    'show_all' => 'سارے دکھاو',
+    'login' => [
+        'name' => 'لاگ ان کریں',
+    ],
+    'logout' => [
+        'name' => 'لاگ آوٹ',
+    ],
+    'profile' => 'پروفائل',
+    'email' => 'ای میل اڈریس',
+    'phone' => 'فون نمبر',
+    'reset_code' => 'کوڈ کو دوبارہ ترتیب دیں',
+    'password_confirmation' => 'پاسورڈ کی تو ثیق',
+    'password' => 'پاس ورڈ',
+    'first_name' => 'پہلا نام',
+    'last_name' => 'آخری نام',
+    'remember_me' => 'مجھے پہچانتے ہو',
+    'not_a_member_yet' => 'ابھی تک ممبر نہیں ہے',
+    'not_a_member_yet_message' => 'ہمارے ممبروں کو ہماری خصوصی خدمت سے استفادہ کرنے کے لئے سائن اپ کریں',
+    'forgot_password' => 'میں اپنا پاسورڈ بھول گیا',
+    'email_or_phone_number' => 'ای میل ایڈریس یا فون نمبر',
+    'phone_number' => 'فون نمبر',
+
+    // Registration instructions
+    'personal_registration_header' => 'رکنیت کے انفرادی فوائد کیا ہیں؟',
+    'personal_registration_body' => 'اپنا مکان ، کار بیچیں اور کرایہ پر دیں ، اپنی غیر استعمال شدہ اشیاء فروخت کریں ، نئی چیزیں حاصل کریں۔',
+    'personal_registration_list_1' => 'ایک مفت اشتہار پوسٹ کریں ،',
+    'personal_registration_list_2' => 'جس اشتہار میں آپ دلچسپی رکھتے ہیں ان کو شامل کریں ، قیمتوں میں ہونے والی تبدیلیوں کو اپنے پسندیدہ میں شامل کرنے کے بعد ان کی پیروی کریں ، ایسی پسندیدہ تلاشیاں بنائیں جو آپ کے معیار پر پورا اتریں ،',
+    'personal_registration_list_3' => 'سائٹ پر اشتہار مالکان کو پیغامات بھیجیں۔',
+    'register_information_note' => 'اس صفحے پر دی گئی معلومات کے ل taken لیا گیا ہے ' . env('APPLICATION_DOMAIN') . ' رکنیت آپ یہاں ذاتی معلومات کے تحفظ کے بارے میں تفصیلی معلومات حاصل کرسکتے ہیں۔',
+
+    // Register page
+    'phone_validation_error' => 'یہ فون نمبر دوسرے ممبر کے زیر استعمال ہے۔',
+
+    // Forgot Password
+    'create_new_password' => 'نیا پاس ورڈ بنائیں',
+
+    // Login page
+    'or' => 'یا',
+    'login_with_phone_number' => 'فون نمبر کے ساتھ لاگ ان کریں',
+    'login_with_email_address' => 'ای میل ایڈریس کے ساتھ لاگ ان کریں',
+
+    // Side menu links
+    'company_directory' => 'کمپنی کی ڈائرکٹری',
+    'popular_ads' => 'مشہور اشتہارات',
+    'last_48_hours' => 'آخری 48 گھنٹے',
+    'secure_e-commerce_ads' => 'ای کامرس کے اشتہارات کو محفوظ بنائیں',
+    'sms' => 'ٹیکسٹ میسج (SMS) بھیجیں',
+    'mail' => 'میل بھیجیں',
+
+    // Register instruction seed
+    'register_instructions' => 'ہدایات رجسٹر کریں',
+    'list' => 'فہرست',
+    'instruction_description' => 'ہدایات کی تفصیل',
+    'instruction_list' => 'ہدایات کی فہرست',
+];
