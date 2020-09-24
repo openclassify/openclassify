f2a67e000422602fddfb6a3517c68446445e5b63
Fatih Alp <mail@fatihalp.com>
2020-09-24 12:40:40 +0300

    New translations section.php (Hindi)

--- addons/default/visiosoft/advs-module/resources/lang/hi/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/hi/section.php
@@ -40,7 +40,8 @@ return [
     'general' => 'सामान्य',
     'ads' => 'विज्ञापन',
     'user' => 'उपयोगकर्ता',
+    'filter' => 'फ़िल्टर',
     'options' => [
-        'title' => 'Options',
+        'title' => 'विकल्प',
     ],
 ];
