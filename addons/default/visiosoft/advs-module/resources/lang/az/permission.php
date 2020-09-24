f5a871c67e9b9be99ea29e4fca176cdc6ff4818a
Fatih Alp <mail@fatihalp.com>
2020-09-24 12:41:47 +0300

    New translations permission.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/permission.php
@@ -50,11 +50,11 @@ return [
         ],
     ],
     'options' => [
-        'name'   => 'Options',
+        'name'   => 'Seçimlər',
         'option' => [
-            'read'   => 'Can read options?',
-            'write'  => 'Can create/edit options?',
-            'delete' => 'Can delete options?',
+            'read'   => 'Seçimləri oxuya bilərsinizmi?',
+            'write'  => 'Seçimlər yarada / düzəldə bilərsiniz?',
+            'delete' => 'Seçimləri silə bilərsiniz?',
         ],
     ],
 ];
