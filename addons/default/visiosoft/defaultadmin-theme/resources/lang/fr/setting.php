a854f9a0ec3e405db26d8c99f391efe1eba50833
Fatih Alp <mail@fatihalp.com>
2020-09-28 17:02:58 +0300

    New translations setting.php (French)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/fr/setting.php
@@ -11,8 +11,11 @@ return [
         'warning'      => 'Le premier élément de navigation accessible est utilisé comme zone <strong>origine</strong>.',
         'reorder'      => 'Faites glisser et déposez les éléments de navigation principaux dans la barre latérale <strong></strong> pour les réorganiser.',
     ],
-    "icon" => [
-        'name' => 'Icône',
+    "login_icon" => [
+        'name' => 'Login Icon',
+    ],
+    "panel_icon" => [
+        'name' => 'Panel Icon',
     ],
     "title" => [
         'name' => 'Titre',
