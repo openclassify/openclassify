039d1f64be53b5c3e5705390feb06a23eec45bbc
Fatih Alp <mail@fatihalp.com>
2020-10-01 15:21:36 +0300

    New translations field.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/field.php
@@ -248,6 +248,9 @@ return [
     'in_the_last_24_hours' => [
         'name' => 'За останні 24 години'
     ],
+    'in_the_last_day' => [
+        'name' => 'In The Last 24 Hours'
+    ],
     'in_the_last_week' => [
         'name' => 'В останній тиждень'
     ],
@@ -297,5 +300,10 @@ return [
     // New create page
     'options' => 'Параметри',
 
-    'free' => 'Free'
+    'free' => 'Free',
+    'ad_date' => 'Ad Date',
+    'photo_video' => 'Photo, Video',
+    'and_above' => 'and above',
+    'and_below' => 'and below',
+    'clear_all' => 'Clear All',
 ];
