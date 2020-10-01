f9758250e58bb87050b9e7375119617040445e8e
Fatih Alp <mail@fatihalp.com>
2020-10-01 15:21:43 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/field.php
@@ -249,6 +249,9 @@ return [
     'in_the_last_24_hours' => [
         'name' => 'Nas últimas 24 horas'
     ],
+    'in_the_last_day' => [
+        'name' => 'In The Last 24 Hours'
+    ],
     'in_the_last_week' => [
         'name' => 'Na última semana'
     ],
@@ -298,5 +301,10 @@ return [
     // New create page
     'options' => 'Opções',
 
-    'free' => 'Free'
+    'free' => 'Free',
+    'ad_date' => 'Ad Date',
+    'photo_video' => 'Photo, Video',
+    'and_above' => 'and above',
+    'and_below' => 'and below',
+    'clear_all' => 'Clear All',
 ];
