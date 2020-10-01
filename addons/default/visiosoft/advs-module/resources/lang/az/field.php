208ce68f864fd26d2a7cd7a4aafcab5035a2de7a
Fatih Alp <mail@fatihalp.com>
2020-10-01 15:20:33 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/field.php
@@ -248,6 +248,9 @@ return [
     'in_the_last_24_hours' => [
         'name' => 'Son 24 saatda'
     ],
+    'in_the_last_day' => [
+        'name' => 'In The Last 24 Hours'
+    ],
     'in_the_last_week' => [
         'name' => 'Son Həftədə'
     ],
@@ -297,5 +300,10 @@ return [
     // New create page
     'options' => 'Seçimlər',
 
-    'free' => 'Free'
+    'free' => 'Free',
+    'ad_date' => 'Ad Date',
+    'photo_video' => 'Photo, Video',
+    'and_above' => 'and above',
+    'and_below' => 'and below',
+    'clear_all' => 'Clear All',
 ];
