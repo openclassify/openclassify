66340e540ba358cf5ff4d6e0f8e257f57834041a
Fatih Alp <mail@fatihalp.com>
2020-10-01 15:21:48 +0300

    New translations field.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/field.php
@@ -249,6 +249,9 @@ return [
     'in_the_last_24_hours' => [
         'name' => 'In de afgelopen 24 uur'
     ],
+    'in_the_last_day' => [
+        'name' => 'In The Last 24 Hours'
+    ],
     'in_the_last_week' => [
         'name' => 'In de laatste week'
     ],
@@ -298,5 +301,10 @@ return [
     // New create page
     'options' => 'Opties',
 
-    'free' => 'Free'
+    'free' => 'Free',
+    'ad_date' => 'Ad Date',
+    'photo_video' => 'Photo, Video',
+    'and_above' => 'and above',
+    'and_below' => 'and below',
+    'clear_all' => 'Clear All',
 ];
