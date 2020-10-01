80ff7e2aeb2e94e043eee261016d06a48197e462
Fatih Alp <mail@fatihalp.com>
2020-10-01 15:21:59 +0300

    New translations field.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/field.php
@@ -248,6 +248,9 @@ return [
     'in_the_last_24_hours' => [
         'name' => 'في ال 24 ساعة الماضية'
     ],
+    'in_the_last_day' => [
+        'name' => 'In The Last 24 Hours'
+    ],
     'in_the_last_week' => [
         'name' => 'في الأسبوع الماضي'
     ],
@@ -297,5 +300,10 @@ return [
     // New create page
     'options' => 'خيارات',
 
-    'free' => 'Free'
+    'free' => 'Free',
+    'ad_date' => 'Ad Date',
+    'photo_video' => 'Photo, Video',
+    'and_above' => 'and above',
+    'and_below' => 'and below',
+    'clear_all' => 'Clear All',
 ];
