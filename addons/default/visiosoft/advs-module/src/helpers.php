a65dea3e1f057ab92dd681017d8ff97ffb11d5bf
Muammer Top <muammer@visiosoft.com.tr>
2020-10-02 17:31:51 +0300

    The installation has been fixed.

--- addons/default/visiosoft/advs-module/src/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Contracts\Bus\Dispatcher;
-use Visiosoft\AdvsModule\Adv\Command\appendRequestURL;
-
-function fullLink($request, $url, $newParameters = array()) {
-    return app(Dispatcher::class)->dispatch(new appendRequestURL($request, $url, $newParameters));
-}
