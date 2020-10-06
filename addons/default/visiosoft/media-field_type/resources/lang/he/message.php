32d6c1befbbe9076e15294bc3b478b92edf7a971
Fatih Alp <mail@fatihalp.com>
2020-10-06 17:33:45 +0300

    New translations message.php (Hebrew)

--- addons/default/visiosoft/media-field_type/resources/lang/he/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/he/message.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'choose_folder'     => 'לאיזו תיקיה תרצה להעלות?',
-    'upload'            => 'הוסף תמונות | העלה תמונות | לצרף תמונה',
-    'choose_files'      => 'באילו קבצים תרצה להשתמש?',
+    'choose_folder' => 'לאיזו תיקיה תרצה להעלות?',
+    'upload' => 'הוסף תמונות | העלה תמונות | לצרף תמונה',
+    'choose_files' => 'באילו קבצים תרצה להשתמש?',
     'no_files_selected' => 'לא נבחרו קבצים.',
-    'no_uploads'        => 'לא הועלו קבצים.',
-    'overwrite'         => 'כבר הועלה. האם תרצה להחליף אותו?',
-    'uploading'         => 'מעלה',
-    'loading'           => 'טעינה',
+    'no_uploads' => 'לא הועלו קבצים.',
+    'overwrite' => 'כבר הועלה. האם תרצה להחליף אותו?',
+    'uploading' => 'מעלה',
+    'loading' => 'טעינה',
+    'error_upload' => 'There was a problem uploading the file.',
 ];
