7f65e3cc5e3ef096ce7b49adb736e21bdbd841c2
Fatih Alp <mail@fatihalp.com>
2020-10-06 13:58:59 +0300

    New translations button.php (Turkish)

--- addons/default/visiosoft/base-theme/resources/lang/tr/button.php
+++ addons/default/visiosoft/base-theme/resources/lang/tr/button.php
@@ -10,5 +10,5 @@ return [
     'reset_password' => 'Şifremi Güncelle',
     'email' => 'E-posta',
     'phone' => 'Telefon',
-    'show_all' => 'Show All',
+    'show_all' => 'Hepsini Göster ↓',
 ];
