85332483b2a362b61562d727a8f5495741ca97f0
Fatih Alp <mail@fatihalp.com>
2020-10-06 13:51:47 +0300

    New translations button.php (Spanish)

--- addons/default/visiosoft/base-theme/resources/lang/es/button.php
+++ addons/default/visiosoft/base-theme/resources/lang/es/button.php
@@ -10,5 +10,5 @@ return [
     'reset_password' => 'Restablecer la contraseña',
     'email' => 'Correo electrónico',
     'phone' => 'Teléfono',
-    'show_all' => 'Show All',
+    'show_all' => 'Mostrar todo',
 ];
