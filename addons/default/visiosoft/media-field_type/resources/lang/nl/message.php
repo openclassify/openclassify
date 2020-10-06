dc6799c5d34629dd75667a8afbd1a7fc65c990e1
Fatih Alp <mail@fatihalp.com>
2020-10-06 17:25:36 +0300

    New translations message.php (Dutch)

--- addons/default/visiosoft/media-field_type/resources/lang/nl/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/nl/message.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'choose_folder'     => 'Naar welke map wil je uploaden?',
-    'upload'            => 'İmages | İmages uploaden | Voeg een foto toe',
-    'choose_files'      => 'Welke bestanden wil je gebruiken?',
+    'choose_folder' => 'Naar welke map wil je uploaden?',
+    'upload' => 'İmages | İmages uploaden | Voeg een foto toe',
+    'choose_files' => 'Welke bestanden wil je gebruiken?',
     'no_files_selected' => 'Geen bestanden geselecteerd.',
-    'no_uploads'        => 'Geen bestanden geüpload.',
-    'overwrite'         => 'is al geüpload. Wil je het overschrijven?',
-    'uploading'         => 'Uploaden',
-    'loading'           => 'Bezig met laden',
+    'no_uploads' => 'Geen bestanden geüpload.',
+    'overwrite' => 'is al geüpload. Wil je het overschrijven?',
+    'uploading' => 'Uploaden',
+    'loading' => 'Bezig met laden',
+    'error_upload' => 'There was a problem uploading the file.',
 ];
