c2e1d4628f4161a5feae738bcb95dc2a399f2b06
Fatih Alp <mail@fatihalp.com>
2020-10-06 17:31:09 +0300

    New translations message.php (Spanish)

--- addons/default/visiosoft/media-field_type/resources/lang/es/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/es/message.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'choose_folder'     => '¿A qué carpeta le gustaría subir?',
-    'upload'            => 'Añadir imágenes | Subir imágenes | Adjuntar una foto',
-    'choose_files'      => '¿Qué archivos te gustaría usar?',
+    'choose_folder' => '¿A qué carpeta le gustaría subir?',
+    'upload' => 'Añadir imágenes | Subir imágenes | Adjuntar una foto',
+    'choose_files' => '¿Qué archivos te gustaría usar?',
     'no_files_selected' => 'No hay archivos seleccionados.',
-    'no_uploads'        => 'No hay archivos cargados.',
-    'overwrite'         => 'ya ha sido subido ¿Te gustaría sobrescribirlo?',
-    'uploading'         => 'Cargando',
-    'loading'           => 'Cargando',
+    'no_uploads' => 'No hay archivos cargados.',
+    'overwrite' => 'ya ha sido subido ¿Te gustaría sobrescribirlo?',
+    'uploading' => 'Cargando',
+    'loading' => 'Cargando',
+    'error_upload' => 'There was a problem uploading the file.',
 ];
