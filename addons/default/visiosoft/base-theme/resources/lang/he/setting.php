90baa3e5683460385a77abc6e3b9f0e54b2f4abd
Fatih Alp <mail@fatihalp.com>
2020-10-06 13:56:45 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/base-theme/resources/lang/he/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/he/setting.php
@@ -38,6 +38,6 @@ return [
         'name' => 'סגנון (CSS)',
     ],
     'home_page_sub_categories_limit' => [
-        'name' => 'Homepage Sub Categories Limit',
+        'name' => 'מגבלת קטגוריות המשנה לדף הבית',
     ],
 ];
