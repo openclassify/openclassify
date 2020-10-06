0ac7d40a1b41652b14656a43f51cafd25c4b9c88
Fatih Alp <mail@fatihalp.com>
2020-10-06 17:36:52 +0300

    New translations message.php (Turkish)

--- addons/default/visiosoft/media-field_type/resources/lang/tr/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/tr/message.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'choose_folder'     => 'Hangi klasöre yüklemek istersiniz?',
-    'upload'            => 'Yüklemek için buraya tıklayın veya bırakın.',
-    'choose_files'      => 'Hangi dosyaları kullanmak istersiniz?',
+    'choose_folder' => 'Hangi klasöre yüklemek istersiniz?',
+    'upload' => 'Yüklemek için buraya tıklayın veya bırakın.',
+    'choose_files' => 'Hangi dosyaları kullanmak istersiniz?',
     'no_files_selected' => 'Seçili dosya yok.',
-    'no_uploads'        => 'Dosya yüklenmedi.',
-    'overwrite'         => 'zaten yüklendi. Üzerine yazmak ister misiniz?',
-    'uploading'         => 'Yükleme',
-    'loading'           => 'Yükleniyor',
+    'no_uploads' => 'Dosya yüklenmedi.',
+    'overwrite' => 'zaten yüklendi. Üzerine yazmak ister misiniz?',
+    'uploading' => 'Yükleme',
+    'loading' => 'Yükleniyor',
+    'error_upload' => 'There was a problem uploading the file.',
 ];
