e80f9b9cb0cb90e2bf1ac493a92b38f7b29f91d3
Diatrex <diahshalabi@hotmail.com>
2020-10-08 13:56:38 +0300

    #2245 Add points-module as payment method

--- addons/default/visiosoft/advs-module/src/Support/_helpers.php
+++ addons/default/visiosoft/advs-module/src/Support/_helpers.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Contracts\Bus\Dispatcher;
 use Visiosoft\AdvsModule\Adv\Command\appendRequestURL;
+use Visiosoft\AdvsModule\Support\Command\CheckModuleInstalled;
 
-if (!function_exists('fullLink')) {
+if (!function_exists('fullLink'))
+{
 	function fullLink($request, $url, $newParameters = array()) {
 		return app(Dispatcher::class)->dispatch(new appendRequestURL($request, $url, $newParameters));
+	}
+}
+
+if (!function_exists('is_module_installed'))
+{
+	function is_module_installed($moduleNamespace, $checkEnabled = true) {
+		return dispatch_now(new CheckModuleInstalled($moduleNamespace, $checkEnabled));
 	}
 }
