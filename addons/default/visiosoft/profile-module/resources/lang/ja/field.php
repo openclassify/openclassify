bc98ed455e78856c6ab099cf8923d995111374c2
Fatih Alp <mail@fatihalp.com>
2020-10-14 11:55:01 +0300

    New translations field.php (Japanese)

--- addons/default/visiosoft/profile-module/resources/lang/ja/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ja/field.php
@@ -55,6 +55,9 @@ return [
     'register_type' => [
         'name' => '登録タイプ'
     ],
+    'birthday' => [
+        'name' => 'Birthday'
+    ],
     'identification_number' => [
         'name' => '識別番号'
     ],
