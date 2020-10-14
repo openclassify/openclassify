b3b86be9e32757c641f6486aa34a87045772fd7d
Fatih Alp <mail@fatihalp.com>
2020-10-14 11:55:15 +0300

    New translations field.php (Hindi)

--- addons/default/visiosoft/profile-module/resources/lang/hi/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/hi/field.php
@@ -55,6 +55,9 @@ return [
     'register_type' => [
         'name' => 'रजिस्टर प्रकार'
     ],
+    'birthday' => [
+        'name' => 'Birthday'
+    ],
     'identification_number' => [
         'name' => 'पहचान संख्या'
     ],
