3913a977c6c21c61e8ef1a91f20bc3da5b574647
Fatih Alp <mail@fatihalp.com>
2020-10-14 11:55:08 +0300

    New translations field.php (Greek)

--- addons/default/visiosoft/profile-module/resources/lang/el/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/el/field.php
@@ -55,6 +55,9 @@ return [
     'register_type' => [
         'name' => 'Τύπος μητρώου'
     ],
+    'birthday' => [
+        'name' => 'Birthday'
+    ],
     'identification_number' => [
         'name' => 'Αριθμός αναγνώρισης'
     ],
