bae96e837fa7d4b322d2db6aae776d27dbab6da6
Fatih Alp <mail@fatihalp.com>
2020-10-14 11:54:48 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/profile-module/resources/lang/tr/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/tr/field.php
@@ -55,6 +55,9 @@ return [
     'register_type' => [
         'name' => 'Üyelik Tipi'
     ],
+    'birthday' => [
+        'name' => 'Birthday'
+    ],
     'identification_number' => [
         'name' => 'Kimlik Numarası'
     ],
