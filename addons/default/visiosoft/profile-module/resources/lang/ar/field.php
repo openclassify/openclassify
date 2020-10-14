69885d408a2dec148c538fcfd5c49bad34d496d0
Fatih Alp <mail@fatihalp.com>
2020-10-14 11:55:12 +0300

    New translations field.php (Arabic)

--- addons/default/visiosoft/profile-module/resources/lang/ar/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ar/field.php
@@ -55,6 +55,9 @@ return [
     'register_type' => [
         'name' => 'نوع السجل'
     ],
+    'birthday' => [
+        'name' => 'Birthday'
+    ],
     'identification_number' => [
         'name' => 'رقم الهوية'
     ],
