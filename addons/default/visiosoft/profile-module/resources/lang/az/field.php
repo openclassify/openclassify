616ce781bd27b5ca6080ebca229e1073569b44c7
Fatih Alp <mail@fatihalp.com>
2020-10-14 11:54:39 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/profile-module/resources/lang/az/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/az/field.php
@@ -55,6 +55,9 @@ return [
     'register_type' => [
         'name' => 'Qeyd Növü'
     ],
+    'birthday' => [
+        'name' => 'Birthday'
+    ],
     'identification_number' => [
         'name' => 'İdentifikasiya nömrəsi'
     ],
