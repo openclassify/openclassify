0af9c34e8dbc2fd028e5fa5897115322a79cb4d2
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:31 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/base-theme/resources/lang/el/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/el/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'Όριο υποκατηγοριών αρχικής σελίδας',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
