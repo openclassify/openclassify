4737702f5aaab3f5281efc635421c845df5bf626
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:21 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/base-theme/resources/lang/pl/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/pl/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'Limit podkategorii strony głównej',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
