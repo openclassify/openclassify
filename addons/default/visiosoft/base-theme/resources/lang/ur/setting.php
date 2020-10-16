b1e91c3a860027e63cddf0df6e237b5d1bad1bf1
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:10 +0300

    New translations setting.php (Urdu (Pakistan))

--- addons/default/visiosoft/base-theme/resources/lang/ur/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/ur/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'مرکزی صفحہ ذیلی زمرے کی حد',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
