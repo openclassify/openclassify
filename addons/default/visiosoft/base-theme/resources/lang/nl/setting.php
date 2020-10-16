9b94e3643e3ce2c7bf614dc1818938c989b4415d
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:23 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/base-theme/resources/lang/nl/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/nl/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'Limiet subcategorieën op homepage',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
