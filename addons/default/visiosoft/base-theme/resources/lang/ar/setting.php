94d8ccbc6f50619e3c0556fc7f3010ef7e6c5190
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:34 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/base-theme/resources/lang/ar/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/ar/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'حد الفئات الفرعية للصفحة الرئيسية',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
