88a2dccca466b231e6d92c57a918a3d29153e93c
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:15 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/base-theme/resources/lang/sv/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/sv/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'Hemsida Underkategorier Begränsning',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
