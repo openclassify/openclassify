211a62fc6e6ea90aa485a5a45c646b5ddaf33b95
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:09 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/base-theme/resources/lang/vi/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/vi/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'Giới hạn danh mục phụ của trang chủ',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
