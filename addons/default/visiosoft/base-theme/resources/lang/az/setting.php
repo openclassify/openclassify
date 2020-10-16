4132ca3b362cc72338ddc1fa6c34d0b2254fc2e2
Fatih Alp <mail@fatihalp.com>
2020-10-16 12:16:04 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/base-theme/resources/lang/az/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/az/setting.php
@@ -40,4 +40,7 @@ return [
     'home_page_sub_categories_limit' => [
         'name' => 'Əsas alt kateqoriyalar məhdudiyyəti',
     ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
 ];
