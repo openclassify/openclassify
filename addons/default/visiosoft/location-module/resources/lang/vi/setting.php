1ac040d000e0b6eaefbb5ea6fbdc5fb55682d03a
Fatih Alp <mail@fatihalp.com>
2020-10-20 16:25:43 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/location-module/resources/lang/vi/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/vi/setting.php
@@ -44,4 +44,7 @@ return [
     'hide_location_filter' => [
         'name' => 'Ẩn Bộ lọc Vị trí',
     ],
+	'country_for_phone_field' => [
+		'name' => 'Default Country For Register Phone Field'
+	]
 ];
