88525bee496f31a12dfd7e6a3e5d30d9e61b52f2
Fatih Alp <mail@fatihalp.com>
2020-10-20 16:25:58 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/location-module/resources/lang/pl/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/pl/setting.php
@@ -44,4 +44,7 @@ return [
     'hide_location_filter' => [
         'name' => 'Ukryj filtr lokalizacji',
     ],
+	'country_for_phone_field' => [
+		'name' => 'Default Country For Register Phone Field'
+	]
 ];
