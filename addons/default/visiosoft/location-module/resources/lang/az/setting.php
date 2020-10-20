40968613e1814a3c7df1e2df16f767a62618df9c
Fatih Alp <mail@fatihalp.com>
2020-10-20 16:25:38 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/location-module/resources/lang/az/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/az/setting.php
@@ -44,4 +44,7 @@ return [
     'hide_location_filter' => [
         'name' => 'Yer filtrini gizlət',
     ],
+	'country_for_phone_field' => [
+		'name' => 'Default Country For Register Phone Field'
+	]
 ];
