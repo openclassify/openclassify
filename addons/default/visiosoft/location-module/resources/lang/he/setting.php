0dc88eef4414b7ce2ab60cd8f8ed076292057ee7
Fatih Alp <mail@fatihalp.com>
2020-10-21 12:35:54 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/location-module/resources/lang/he/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/he/setting.php
@@ -45,6 +45,6 @@ return [
         'name' => 'הסתר מסנן מיקום',
     ],
 	'country_for_phone_field' => [
-		'name' => 'Default Country For Register Phone Field'
+		'name' => 'מדינה ברירת מחדל לשדה טלפון רישום'
 	]
 ];
