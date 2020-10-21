79bec9fc90d111d525ff857a5b2ccfb0fc6c0c1b
Fatih Alp <mail@fatihalp.com>
2020-10-21 12:38:15 +0300

    New translations message.php (Korean)

--- addons/default/visiosoft/profile-module/resources/lang/ko/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/ko/message.php
@@ -28,7 +28,7 @@ return [
     'ajax_address_error' => '주소가 없거나 볼 권한이 없습니다.',
     'empty_password_sms_message' => '보안 문제로 인해 비밀번호가 변경되었습니다. 새 비밀번호는 다음과 같습니다.',
     'required_all' => "모든 필드는 필수입니다!",
-    'can_not_remove_filled_fields' => "You can not remove filled fields!",
+    'can_not_remove_filled_fields' => "채워진 필드를 제거 할 수 없습니다!",
 
     // Forgot Password
     'email_phone_not_found' => '이메일, 전화 번호가 올바르지 않습니다!',
