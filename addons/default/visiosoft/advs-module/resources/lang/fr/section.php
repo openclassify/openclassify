dc92af755669bbf0b7e10ca32fea9bcfd1b57ee7
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:01:53 +0300

    New translations section.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/section.php
@@ -45,4 +45,13 @@ return [
         'title' => 'Options',
     ],
     'ads_image' => 'Image d\'annonces',
+    'product_options' => [
+        'title' => 'Options',
+    ],
+    'productoptions_value' => [
+        'title' => 'Options Value',
+    ],
+    'option_configuration' => [
+        'title' => 'Configuration',
+    ],
 ];
