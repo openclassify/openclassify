e82b921c8942d4c5d09fd8f3a5fa9d9c01717fd6
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:02:16 +0300

    New translations stream.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/stream.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/stream.php
@@ -22,4 +22,13 @@ return [
     'options' => [
         'name' => 'Opties',
     ],
+    'product_options' => [
+        'name' => 'Product Options',
+    ],
+    'productoptions_value' => [
+        'name' => 'Options value',
+    ],
+    'option_configuration' => [
+        'name' => 'Configuration',
+    ],
 ];
