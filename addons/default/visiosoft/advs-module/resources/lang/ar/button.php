e30023411ec1d80a0db8b03aacb7e9e60c15326d
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:01:21 +0300

    New translations button.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/button.php
@@ -32,5 +32,11 @@ return [
         'name' => 'تحديث الفئة'
     ],
     'new_option' => 'خيار جديد',
-	'export' => 'تصدير'
+	'export' => 'تصدير',
+    'new_productoption' => 'New Productoption',
+    'new_productoptions_value' => 'New Productoptions value',
+    'new_options_configuration' => 'New Options configuration',
+    'new_option_configuration' => 'New Option configuration',
+    'new_option_configuration' => 'New Option configuration',
+	'create_configurations' => 'Create Configurations',
 ];
