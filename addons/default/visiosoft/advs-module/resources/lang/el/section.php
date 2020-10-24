9a89afb84dbc343eba31b10ce955daf5db38180d
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:02:03 +0300

    New translations section.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/section.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/section.php
@@ -45,4 +45,13 @@ return [
         'title' => 'Επιλογές',
     ],
     'ads_image' => 'Εικόνα διαφημίσεων',
+    'product_options' => [
+        'title' => 'Options',
+    ],
+    'productoptions_value' => [
+        'title' => 'Options Value',
+    ],
+    'option_configuration' => [
+        'title' => 'Configuration',
+    ],
 ];
