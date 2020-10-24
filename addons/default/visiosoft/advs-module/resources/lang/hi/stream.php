99ed7913f12f81a201f51cabb23b3de2905ec1ee
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:02:51 +0300

    New translations stream.php (Hindi)

--- addons/default/visiosoft/advs-module/resources/lang/hi/stream.php
+++ addons/default/visiosoft/advs-module/resources/lang/hi/stream.php
@@ -22,4 +22,13 @@ return [
     'options' => [
         'name' => 'विकल्प',
     ],
+    'product_options' => [
+        'name' => 'Product Options',
+    ],
+    'productoptions_value' => [
+        'name' => 'Options value',
+    ],
+    'option_configuration' => [
+        'name' => 'Configuration',
+    ],
 ];
