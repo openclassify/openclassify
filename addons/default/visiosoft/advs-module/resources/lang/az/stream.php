2a2b20de9911e9fa38a0f7b16af9dcdbcf926e94
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:00:42 +0300

    New translations stream.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/stream.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/stream.php
@@ -22,4 +22,13 @@ return [
     'options' => [
         'name' => 'Seçimlər',
     ],
+    'product_options' => [
+        'name' => 'Product Options',
+    ],
+    'productoptions_value' => [
+        'name' => 'Options value',
+    ],
+    'option_configuration' => [
+        'name' => 'Configuration',
+    ],
 ];
