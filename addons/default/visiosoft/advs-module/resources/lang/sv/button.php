e40f320ccf9a049d39073439c15db134b1698822
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:00:06 +0300

    New translations button.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/button.php
@@ -32,5 +32,11 @@ return [
         'name' => 'Uppdatera kategori'
     ],
     'new_option' => 'Nytt alternativ',
-	'export' => 'Exportera'
+	'export' => 'Exportera',
+    'new_productoption' => 'New Productoption',
+    'new_productoptions_value' => 'New Productoptions value',
+    'new_options_configuration' => 'New Options configuration',
+    'new_option_configuration' => 'New Option configuration',
+    'new_option_configuration' => 'New Option configuration',
+	'create_configurations' => 'Create Configurations',
 ];
