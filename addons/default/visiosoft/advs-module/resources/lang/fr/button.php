71f75fc43c623e76a2bab8f0a59e30346ad5e243
Fatih Alp <mail@fatihalp.com>
2020-10-25 00:01:57 +0300

    New translations button.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/button.php
@@ -32,5 +32,11 @@ return [
         'name' => 'Mettre à jour la catégorie'
     ],
     'new_option' => 'Nouvelle option',
-	'export' => 'Exportation'
+	'export' => 'Exportation',
+    'new_productoption' => 'New Productoption',
+    'new_productoptions_value' => 'New Productoptions value',
+    'new_options_configuration' => 'New Options configuration',
+    'new_option_configuration' => 'New Option configuration',
+    'new_option_configuration' => 'New Option configuration',
+	'create_configurations' => 'Create Configurations',
 ];
