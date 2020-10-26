1f1f2c7cf1539ca0fe4ac2db3aaf05212390cb96
Fatih Alp <mail@fatihalp.com>
2020-10-26 19:51:50 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/setting.php
@@ -176,4 +176,8 @@ return [
     'watermark_position' => [
         'name' => 'Posizione della filigrana',
     ],
+    'user_filter_limit' => [
+        'name' => 'User Filter Limit',
+        'instructions' => 'In the admin panel'
+    ]
 ];
