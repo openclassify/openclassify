9a23f13aeaf18efae5ecd1a98138f4719914df42
Fatih Alp <mail@fatihalp.com>
2020-10-26 19:51:43 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/advs-module/resources/lang/pl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/pl/setting.php
@@ -176,4 +176,8 @@ return [
     'watermark_position' => [
         'name' => 'Pozycja znaku wodnego',
     ],
+    'user_filter_limit' => [
+        'name' => 'User Filter Limit',
+        'instructions' => 'In the admin panel'
+    ]
 ];
