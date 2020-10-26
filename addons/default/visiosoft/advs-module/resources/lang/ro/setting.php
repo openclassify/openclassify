8d94028f7739e751a3a59212bb15ad6a17859416
Fatih Alp <mail@fatihalp.com>
2020-10-26 19:51:21 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
@@ -176,4 +176,8 @@ return [
     'watermark_position' => [
         'name' => 'Poziția filigranului',
     ],
+    'user_filter_limit' => [
+        'name' => 'User Filter Limit',
+        'instructions' => 'In the admin panel'
+    ]
 ];
