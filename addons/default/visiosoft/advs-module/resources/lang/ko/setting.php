65a23eddffd20752f143543bb480ec4c9e9fe915
Fatih Alp <mail@fatihalp.com>
2020-10-26 19:51:46 +0300

    New translations setting.php (Korean)

--- addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
@@ -176,4 +176,8 @@ return [
     'watermark_position' => [
         'name' => '워터 마크 위치',
     ],
+    'user_filter_limit' => [
+        'name' => 'User Filter Limit',
+        'instructions' => 'In the admin panel'
+    ]
 ];
