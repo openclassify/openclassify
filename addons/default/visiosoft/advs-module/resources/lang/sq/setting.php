5b15b841577cfa01826fd6eb8cc067a471c8c2e9
Fatih Alp <mail@fatihalp.com>
2020-10-26 19:51:38 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
@@ -176,4 +176,8 @@ return [
     'watermark_position' => [
         'name' => 'Pozicioni i Filigranit',
     ],
+    'user_filter_limit' => [
+        'name' => 'User Filter Limit',
+        'instructions' => 'In the admin panel'
+    ]
 ];
