17225baa807b7179b933c23542daf1675829bce2
Fatih Alp <mail@fatihalp.com>
2020-10-27 17:44:19 +0300

    New translations stream.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/stream.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/stream.php
@@ -22,4 +22,13 @@ return [
     'options' => [
         'name' => 'אפשרויות',
     ],
+    'product_options' => [
+        'name' => 'אפשרויות מוצר',
+    ],
+    'productoptions_value' => [
+        'name' => 'ערך אפשרויות',
+    ],
+    'option_configuration' => [
+        'name' => 'תְצוּרָה',
+    ],
 ];
