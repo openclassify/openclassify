5ac624c3880622c72bbba7e0e2eebf1ca0156a69
Fatih Alp <mail@fatihalp.com>
2020-10-27 12:19:48 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/setting.php
@@ -177,7 +177,7 @@ return [
         'name' => 'Posizione della filigrana',
     ],
     'user_filter_limit' => [
-        'name' => 'User Filter Limit',
-        'instructions' => 'In the admin panel'
+        'name' => 'Limite filtro utente',
+        'instructions' => 'Nel pannello di amministrazione'
     ]
 ];
