6d5330385735fedde141443b3f8a7f76ffbe9c4e
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:45 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'إخفاء حقل السعر القياسي',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'العملات الممكّنة',
     ],
