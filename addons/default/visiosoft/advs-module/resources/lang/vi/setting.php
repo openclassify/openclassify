ec11f7d80f31aa24b6bba79bf8576849ce974fbe
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:19 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/advs-module/resources/lang/vi/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/vi/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Ẩn trường giá chuẩn',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Tiền tệ được kích hoạt',
     ],
