8d81d5b8f7d42dc80583fdf94da563fd93a9b799
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:14 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Standart qiymət sahəsini gizlət',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Aktiv valyutalar',
     ],
