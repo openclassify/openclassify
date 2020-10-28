d6a6ebe93f2316d586bd2169ed49b540101ff1e5
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:34 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Verberg standaardprijsveld',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Ingeschakelde valuta\\'s',
     ],
