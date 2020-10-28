ee6c43ece279d70ec990819a760912ed3d3a8057
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:23 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Сховати поле стандартної ціни',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Увімкнено валюти',
     ],
