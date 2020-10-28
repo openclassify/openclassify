32297738a24ddea575473d042308fe8ec02af180
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:39 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Nascondi campo prezzo standard',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Valute abilitate',
     ],
