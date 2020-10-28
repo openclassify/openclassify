a04bad352206a9d88fc36fbfc7e5ee203a5c94bd
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:11 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Ascundeți câmpul de preț standard',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Valute activate',
     ],
