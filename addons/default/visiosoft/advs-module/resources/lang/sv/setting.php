c5a2304788f0477dddcee607efab9e7cdb3edb67
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:26 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Dölj standardprisfältet',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Aktiverade valutor',
     ],
