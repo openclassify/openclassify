7ca12e32582320b93214f14e53f7a618306d17d7
Fatih Alp <mail@fatihalp.com>
2020-10-28 14:54:24 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
@@ -97,6 +97,9 @@ return [
     'hide_standard_price_field' => [
         'name' => 'Standart Fiyat Alanını Gizle',
     ],
+    'hide_options_field' => [
+        'name' => 'Hide Options Field',
+    ],
     'enabled_currencies' => [
         'name' => 'Etkin Para Birimleri',
     ],
