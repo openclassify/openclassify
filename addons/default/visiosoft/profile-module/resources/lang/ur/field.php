0dcffa159a7e66582df3ce6fd32761c5b9d6694b
Fatih Alp <mail@fatihalp.com>
2020-11-04 15:46:13 +0300

    New translations field.php (Urdu (Pakistan))

--- addons/default/visiosoft/profile-module/resources/lang/ur/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ur/field.php
@@ -446,4 +446,10 @@ return [
     'edit_details' => 'تفصیلات میں ترمیم کریں',
     'update' => 'اپ ڈیٹ',
     'change_password' => 'پاس ورڈ تبدیل کریں',
+
+    // Register page
+    'accept_terms_label' => 'I accept the terms.',
+    'accept_protection_law_label' => 'I accept the terms of the Law on Protection of Personal Data.',
+    'accept_privacy_terms_label' => 'I accept privacy terms.',
+    'receive_sms_emails_label' => 'I want to receive e-mail & sms.',
 ];
