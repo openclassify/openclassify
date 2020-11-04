4de7b852ce0f02bb5fe06c2515e2c941279dd31f
Fatih Alp <mail@fatihalp.com>
2020-11-04 15:45:58 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/profile-module/resources/lang/tr/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/tr/field.php
@@ -445,4 +445,10 @@ return [
     'edit_details' => 'Detayları düzenle',
     'update' => 'Güncelleme',
     'change_password' => 'Şifre değiştir',
+
+    // Register page
+    'accept_terms_label' => 'I accept the terms.',
+    'accept_protection_law_label' => 'I accept the terms of the Law on Protection of Personal Data.',
+    'accept_privacy_terms_label' => 'I accept privacy terms.',
+    'receive_sms_emails_label' => 'I want to receive e-mail & sms.',
 ];
