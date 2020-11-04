fe6fa24da3d1794884c5f81760d8c9bec09595b2
Fatih Alp <mail@fatihalp.com>
2020-11-04 15:46:05 +0300

    New translations field.php (Hindi)

--- addons/default/visiosoft/profile-module/resources/lang/hi/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/hi/field.php
@@ -446,4 +446,10 @@ return [
     'edit_details' => 'विवरण संपादित करें',
     'update' => 'अपडेट करें',
     'change_password' => 'पासवर्ड बदलें',
+
+    // Register page
+    'accept_terms_label' => 'I accept the terms.',
+    'accept_protection_law_label' => 'I accept the terms of the Law on Protection of Personal Data.',
+    'accept_privacy_terms_label' => 'I accept privacy terms.',
+    'receive_sms_emails_label' => 'I want to receive e-mail & sms.',
 ];
