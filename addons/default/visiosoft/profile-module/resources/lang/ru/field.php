a0e011898906084550a81e118568d58336c185aa
Fatih Alp <mail@fatihalp.com>
2020-11-04 15:45:43 +0300

    New translations field.php (Russian)

--- addons/default/visiosoft/profile-module/resources/lang/ru/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ru/field.php
@@ -447,4 +447,10 @@ return [
     'edit_details' => 'Редактировать детали',
     'update' => 'Обновить',
     'change_password' => 'Сменить пароль',
+
+    // Register page
+    'accept_terms_label' => 'I accept the terms.',
+    'accept_protection_law_label' => 'I accept the terms of the Law on Protection of Personal Data.',
+    'accept_privacy_terms_label' => 'I accept privacy terms.',
+    'receive_sms_emails_label' => 'I want to receive e-mail & sms.',
 ];
