356d79dffdba964b908b81d2f7bb70fdf7453aff
Diatrex <diahshalabi@hotmail.com>
2020-11-05 17:52:32 +0300

    #2592 üye kayıt formuna capthca eklenmesi

--- addons/default/visiosoft/profile-module/resources/assets/js/captcha.js
+++ addons/default/visiosoft/profile-module/resources/assets/js/captcha.js
@@ -1,6 +1,6 @@
 grecaptcha.ready(function() {
     grecaptcha.execute(reCAPTCHASiteKey)
         .then(function(token) {
-            document.getElementById("recaptcha_token").value = token;
+            $('.recaptcha-token').val(token)
         });
 });
