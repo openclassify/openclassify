f905d6fee5f2ee4a725505003012311a229d0463
Fatih Alp <mail@fatihalp.com>
2020-11-05 12:45:13 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/location-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/it/setting.php
@@ -46,5 +46,11 @@ return [
     ],
 	'country_for_phone_field' => [
 		'name' => 'Paese predefinito per il campo del telefono di registrazione'
-	]
+	],
+    'sorting_column' => [
+        'name' => 'Sorting Column',
+    ],
+    'sorting_type' => [
+        'name' => 'Sorting Type',
+    ],
 ];
