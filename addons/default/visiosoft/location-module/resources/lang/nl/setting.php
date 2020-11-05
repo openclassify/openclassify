4ab029e5bb016995090aec1887b164d02a9c86c1
Fatih Alp <mail@fatihalp.com>
2020-11-05 12:45:08 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/location-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/nl/setting.php
@@ -46,5 +46,11 @@ return [
     ],
 	'country_for_phone_field' => [
 		'name' => 'Standaardland voor register telefoonveld'
-	]
+	],
+    'sorting_column' => [
+        'name' => 'Sorting Column',
+    ],
+    'sorting_type' => [
+        'name' => 'Sorting Type',
+    ],
 ];
