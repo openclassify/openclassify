577c70652bb4105c2df100ffa04b93883bcf2efc
Fatih Alp <mail@fatihalp.com>
2020-11-05 12:45:11 +0300

    New translations setting.php (Japanese)

--- addons/default/visiosoft/location-module/resources/lang/ja/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/ja/setting.php
@@ -46,5 +46,11 @@ return [
     ],
 	'country_for_phone_field' => [
 		'name' => '電話登録フィールドのデフォルトの国'
-	]
+	],
+    'sorting_column' => [
+        'name' => 'Sorting Column',
+    ],
+    'sorting_type' => [
+        'name' => 'Sorting Type',
+    ],
 ];
