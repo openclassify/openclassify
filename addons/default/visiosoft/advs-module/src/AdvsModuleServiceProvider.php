a3352013edf5840a59ae3de85f010dd6844507db
Diatrex <diahshalabi@hotmail.com>
2020-11-05 12:53:04 +0300

    remove unused properties and methods

--- addons/default/visiosoft/advs-module/src/AdvsModuleServiceProvider.php
+++ addons/default/visiosoft/advs-module/src/AdvsModuleServiceProvider.php
@@ -33,42 +33,10 @@ use Visiosoft\LocationModule\Country\CountryRepository;
 
 class AdvsModuleServiceProvider extends AddonServiceProvider
 {
-
-    /**
-     * Additional addon plugins.
-     *
-     * @type array|null
-     */
     protected $plugins = [
         AdvsModulePlugin::class,
     ];
 
-    /**
-     * The addon Artisan commands.
-     *
-     * @type array|null
-     */
-    protected $commands = [];
-
-    /**
-     * The addon's scheduled commands.
-     *
-     * @type array|null
-     */
-    protected $schedules = [];
-
-    /**
-     * The addon API routes.
-     *
-     * @type array|null
-     */
-    protected $api = [];
-
-    /**
-     * The addon routes.
-     *
-     * @type array|null
-     */
     protected $routes = [
         // Admin AdvsController
         'admin/advs' => [
@@ -244,59 +212,17 @@ class AdvsModuleServiceProvider extends AddonServiceProvider
 
     ];
 
-    /**
-     * The addon middleware.
-     *
-     * @type array|null
-     */
     protected $middleware = [
         SetLang::class,
         redirectDiffrentLang::class,
     ];
 
-    /**
-     * Addon group middleware.
-     *
-     * @var array
-     */
-    protected $groupMiddleware = [
-        //'web' => [
-        //    Visiosoft\AdvsModule\Http\Middleware\ExampleMiddleware::class,
-        //],
-    ];
-
-    /**
-     * Addon route middleware.
-     *
-     * @type array|null
-     */
-    protected $routeMiddleware = [];
-
-    /**
-     * The addon event listeners.
-     *
-     * @type array|null
-     */
     protected $listeners = [
         TableIsQuerying::class => [
             AddAdvsSettingsScript::class,
         ],
     ];
 
-    /**
-     * The addon alias bindings.
-     *
-     * @type array|null
-     */
-    protected $aliases = [
-        //'Example' => Visiosoft\AdvsModule\Example::class
-    ];
-
-    /**
-     * The addon class bindings.
-     *
-     * @type array|null
-     */
     protected $bindings = [
         // AdvsCfValuesEntryModel::class => CfValueModel::class,
         // AdvsCustomFieldAdvsEntryModel::class => CustomFieldAdvModel::class,
@@ -327,50 +253,12 @@ class AdvsModuleServiceProvider extends AddonServiceProvider
 	    ProductoptionsValueRepositoryInterface::class => ProductoptionsValueRepository::class,
     ];
 
-    /**
-     * Additional service providers.
-     *
-     * @type array|null
-     */
-    protected $providers = [
-        //\ExamplePackage\Provider\ExampleProvider::class
-    ];
-
-    /**
-     * The addon view overrides.
-     *
-     * @type array|null
-     */
     protected $overrides = [
         'streams::form/form' => 'visiosoft.module.advs::form/form',
         //'streams::errors/404' => 'module::errors/404',
         //'streams::errors/500' => 'module::errors/500',
     ];
 
-    /**
-     * The addon mobile-only view overrides.
-     *
-     * @type array|null
-     */
-    protected $mobile = [
-        //'streams::errors/404' => 'module::mobile/errors/404',
-        //'streams::errors/500' => 'module::mobile/errors/500',
-    ];
-
-    /**
-     * Register the addon.
-     */
-    public function register()
-    {
-        // Run extra pre-boot registration logic here.
-        // Use method injection or commands to bring in services.
-    }
-
-    /**
-     * Boot the addon.
-     * @param AddonCollection $addonCollection
-     * @param FileModel $fileModel
-     */
     public function boot(AddonCollection $addonCollection, FileModel $fileModel)
     {
         // Run extra post-boot registration logic here.
@@ -404,19 +292,5 @@ class AdvsModuleServiceProvider extends AddonServiceProvider
 
 	    // Disable file versioning
         $fileModel->disableVersioning();
-    }
-
-    /**
-     * Map additional addon routes.
-     *
-     * @param Router $router
-     */
-    // public function map(Router $router)
-    // {
-    //     // Register dynamic routes here for example.
-    //     // Use method injection or commands to bring in services.
-    // }
-    public function map(Router $router)
-    {
     }
 }
