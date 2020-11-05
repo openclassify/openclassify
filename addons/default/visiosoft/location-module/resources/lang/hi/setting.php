f91b190d024c5303d207fb45797ee31c6662818a
Fatih Alp <mail@fatihalp.com>
2020-11-05 12:45:25 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/location-module/resources/lang/hi/setting.php
+++ addons/default/visiosoft/location-module/resources/lang/hi/setting.php
@@ -46,5 +46,11 @@ return [
     ],
 	'country_for_phone_field' => [
 		'name' => 'डिफ़ॉल्ट देश रजिस्टर फोन क्षेत्र के लिए'
-	]
+	],
+    'sorting_column' => [
+        'name' => 'Sorting Column',
+    ],
+    'sorting_type' => [
+        'name' => 'Sorting Type',
+    ],
 ];
