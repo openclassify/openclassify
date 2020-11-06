43c5e66e8d8219cca94c4ce271340ee7102ef97f
Fatih Alp <mail@fatihalp.com>
2020-11-06 20:15:23 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/profile-module/resources/lang/ro/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/ro/setting.php
@@ -11,12 +11,12 @@ return [
         'name' => 'Arată câmpul Oficiului Fiscal',
     ],
     'show_checkbox_terms_on_register' => [
-        'name' => 'Show Checkbox Terms on Register',
-        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+        'name' => 'Afișați termenii casetei de selectare la Înregistrare',
+        'instructions' => 'Afișați „Acceptați termenul”, „legea protecției”, „termenul de confidențialitate”, „sms și e-mailuri” în formularul de înregistrare'
     ],
     'show_education_profession' => [
-        'name' => 'Show Education and Profession Fields',
-        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+        'name' => 'Afișați domeniile Educație și profesie',
+        'instructions' => 'Afișați „Educație”, „Starea educației” și „Profesie” pe pagina de editare a profilului'
     ],
     'google_captcha_site_key' => [
         'name' => 'Cheia site-ului Google Captcha',
@@ -29,12 +29,12 @@ return [
         'instructions' => 'O valoare cuprinsă între 0 și 1. Cu cât pragul este mai mare, cu atât ReCaptcha este mai strict în încercarea de a determina dacă un utilizator este sau nu un bot.',
     ],
     'education' => [
-        'name' => 'Education'
+        'name' => 'Educaţie'
     ],
     'state_of_education' => [
-        'name' => 'State of Education'
+        'name' => 'Starea educației'
     ],
     'profession' => [
-        'name' => 'Profession'
+        'name' => 'Profesie'
     ],
 ];
