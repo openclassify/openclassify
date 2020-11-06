123f2555bb0f5df17f9aa55f1008cfc8151e6c34
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:13:47 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/profile-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/uk/setting.php
@@ -7,8 +7,17 @@ return [
     'upload_avatar' => [
         'name' => 'Завантажити аватар',
     ],
-
     'show_tax_office' => [
         'name' => 'Показати поле податкової служби',
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha Site Key',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha Secret Key',
+    ],
+    'score_threshold' => [
+        'name' => 'Score Threshold',
+        'instructions' => 'A value between 0 and 1. The higher the threshold the more strict ReCaptcha is in trying to determine if a user is a bot or not.',
     ],
 ];
