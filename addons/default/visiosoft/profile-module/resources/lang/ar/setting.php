0d698f5f93b1bd7f575fffb9270c10f1bf6ff3d6
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:50:48 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/profile-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/ar/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'الصورة الرمزية تحميل',
     ],
-
     'show_tax_office' => [
         'name' => 'إظهار حقل مكتب الضرائب',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'مفتاح موقع Google Captcha',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'مفتاح Google Captcha السري',
+    ],
+    'score_threshold' => [
+        'name' => 'عتبة النتيجة',
+        'instructions' => 'قيمة بين 0 و 1. كلما زادت العتبة ، زادت صرامة ReCaptcha في محاولة تحديد ما إذا كان المستخدم روبوتًا أم لا.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
