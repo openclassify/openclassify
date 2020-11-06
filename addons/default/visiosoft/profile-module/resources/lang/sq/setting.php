28f0e68ec570d40ce41f58c69584f3eec889ca77
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:12:51 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/profile-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/sq/setting.php
@@ -7,8 +7,17 @@ return [
     'upload_avatar' => [
         'name' => 'Foto e Profilit',
     ],
-
     'show_tax_office' => [
         'name' => 'Trego Fushën e Zyrës së Taksave',
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha Site Key',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha Secret Key',
+    ],
+    'score_threshold' => [
+        'name' => 'Score Threshold',
+        'instructions' => 'A value between 0 and 1. The higher the threshold the more strict ReCaptcha is in trying to determine if a user is a bot or not.',
     ],
 ];
