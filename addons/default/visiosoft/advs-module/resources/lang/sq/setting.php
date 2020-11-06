1b517c58733063a51d9e05b6d7415f82a6aeebfa
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:12:56 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
@@ -179,5 +179,8 @@ return [
     'user_filter_limit' => [
         'name' => 'Kufiri i filtrit të përdoruesit',
         'instructions' => 'Në panelin e administratorit'
-    ]
+    ],
+    'show_breadcrumb_when_creating_ad' => [
+        'name' => 'Show Breadcrumb When Creating an Ad',
+    ],
 ];
