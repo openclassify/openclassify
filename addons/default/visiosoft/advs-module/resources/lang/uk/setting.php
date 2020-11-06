ff2b2e25ef3c41820959f0d814660726dd63d015
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:13:52 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
@@ -179,5 +179,8 @@ return [
     'user_filter_limit' => [
         'name' => 'Обмеження фільтра користувача',
         'instructions' => 'На панелі адміністратора'
-    ]
+    ],
+    'show_breadcrumb_when_creating_ad' => [
+        'name' => 'Show Breadcrumb When Creating an Ad',
+    ],
 ];
