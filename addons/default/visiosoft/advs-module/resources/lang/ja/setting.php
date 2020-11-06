73a5b05487956676c358c6805396cfeca61dc9f8
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:12:14 +0300

    New translations setting.php (Japanese)

--- addons/default/visiosoft/advs-module/resources/lang/ja/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ja/setting.php
@@ -179,5 +179,8 @@ return [
     'user_filter_limit' => [
         'name' => 'ユーザーフィルターの制限',
         'instructions' => '管理パネルで'
-    ]
+    ],
+    'show_breadcrumb_when_creating_ad' => [
+        'name' => 'Show Breadcrumb When Creating an Ad',
+    ],
 ];
