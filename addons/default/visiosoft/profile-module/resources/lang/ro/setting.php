fe456238d9805a2371f9bf7e70ff86d7db3162e9
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:14:24 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/profile-module/resources/lang/ro/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/ro/setting.php
@@ -7,8 +7,17 @@ return [
     'upload_avatar' => [
         'name' => 'Încărcare Avatar',
     ],
-
     'show_tax_office' => [
         'name' => 'Arată câmpul Oficiului Fiscal',
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha Site Key',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha Secret Key',
+    ],
+    'score_threshold' => [
+        'name' => 'Score Threshold',
+        'instructions' => 'A value between 0 and 1. The higher the threshold the more strict ReCaptcha is in trying to determine if a user is a bot or not.',
     ],
 ];
