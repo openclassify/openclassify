9a6caafd45b55244bb554cbd1deac75896e48bd5
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:45:37 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/profile-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/sq/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'Foto e Profilit',
     ],
-
     'show_tax_office' => [
         'name' => 'Trego Fushën e Zyrës së Taksave',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Çelësi i faqes Google Captcha',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Çelësi sekret i Google Captcha',
+    ],
+    'score_threshold' => [
+        'name' => 'Pragu i Rezultateve',
+        'instructions' => 'Një vlerë ndërmjet 0 dhe 1. Sa më i lartë pragu, aq më i rreptë është ReCaptcha në përpjekjen për të përcaktuar nëse një përdorues është bot apo jo.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
