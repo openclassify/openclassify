98bc06088c5a2864af662a00db60827bc6e2983f
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:13:33 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/profile-module/resources/lang/vi/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/vi/setting.php
@@ -7,8 +7,17 @@ return [
     'upload_avatar' => [
         'name' => 'Tải lên hình đại diện',
     ],
-
     'show_tax_office' => [
         'name' => 'Hiển thị Trường Văn phòng Thuế',
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha Site Key',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha Secret Key',
+    ],
+    'score_threshold' => [
+        'name' => 'Score Threshold',
+        'instructions' => 'A value between 0 and 1. The higher the threshold the more strict ReCaptcha is in trying to determine if a user is a bot or not.',
     ],
 ];
