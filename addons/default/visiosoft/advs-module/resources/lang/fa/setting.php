842bd5d427a2d5b4302a6efcd5828686455f93e0
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:13:31 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
@@ -180,5 +180,8 @@ return [
     'user_filter_limit' => [
         'name' => 'محدودیت فیلتر کاربر',
         'instructions' => 'در پنل مدیریت'
-    ]
+    ],
+    'show_breadcrumb_when_creating_ad' => [
+        'name' => 'Show Breadcrumb When Creating an Ad',
+    ],
 ];
