c81fdb5113718292c9cb01f751032a6dcc97584b
Fatih Alp <mail@fatihalp.com>
2020-11-06 20:13:49 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/profile-module/resources/lang/hi/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/hi/setting.php
@@ -11,12 +11,12 @@ return [
         'name' => 'टैक्स ऑफिस फील्ड दिखाएं',
     ],
     'show_checkbox_terms_on_register' => [
-        'name' => 'Show Checkbox Terms on Register',
-        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+        'name' => 'रजिस्टर पर चेकबॉक्स की शर्तें दिखाएं',
+        'instructions' => 'रजिस्टर फॉर्म पर "स्वीकार करें", "सुरक्षा कानून", "गोपनीयता शब्द", "एसएमएस और ईमेल" दिखाएं'
     ],
     'show_education_profession' => [
-        'name' => 'Show Education and Profession Fields',
-        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+        'name' => 'शिक्षा और पेशा क्षेत्र दिखाएँ',
+        'instructions' => 'प्रोफ़ाइल संपादन पृष्ठ पर "शिक्षा", "शिक्षा की स्थिति" और "पेशा" दिखाएं'
     ],
     'google_captcha_site_key' => [
         'name' => 'Google कैप्चा साइट कुंजी',
@@ -29,12 +29,12 @@ return [
         'instructions' => '0 और 1. के बीच का मान जितना अधिक होता है उतना ही अधिक ReCaptcha यह निर्धारित करने की कोशिश में होता है कि उपयोगकर्ता बॉट है या नहीं।',
     ],
     'education' => [
-        'name' => 'Education'
+        'name' => 'शिक्षा'
     ],
     'state_of_education' => [
-        'name' => 'State of Education'
+        'name' => 'शिक्षा की स्थिति'
     ],
     'profession' => [
-        'name' => 'Profession'
+        'name' => 'व्यवसाय'
     ],
 ];
