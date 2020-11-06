df950a0858f55d4327afa8254ba301c6d1761920
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:12:35 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/setting.php
@@ -179,5 +179,8 @@ return [
     'user_filter_limit' => [
         'name' => 'Benutzerfilterlimit',
         'instructions' => 'Im Admin-Bereich'
-    ]
+    ],
+    'show_breadcrumb_when_creating_ad' => [
+        'name' => 'Show Breadcrumb When Creating an Ad',
+    ],
 ];
