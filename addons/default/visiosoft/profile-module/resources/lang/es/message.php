a37670b780befa7ddb48afa304615bf7ec6c9a9d
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:14:12 +0300

    New translations message.php (Spanish)

--- addons/default/visiosoft/profile-module/resources/lang/es/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/es/message.php
@@ -39,4 +39,8 @@ return [
     // Mail
     'update_email_mail_subject' => '¡Correo electrónico actualizado!',
     'update_email_mail_message' => '¡Su correo electrónico ha sido actualizado!',
+
+    // Register
+    'recaptcha_field_is_required' => 'reCaptcha field is required.',
+    'failed_to_validate_captcha' => 'Failed to validate captcha.',
 ];
