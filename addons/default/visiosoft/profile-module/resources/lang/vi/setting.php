2408ffd2b567be6693e66282def9d9cf20d4c19d
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:49:46 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/profile-module/resources/lang/vi/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/vi/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'Tải lên hình đại diện',
     ],
-
     'show_tax_office' => [
         'name' => 'Hiển thị Trường Văn phòng Thuế',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Khóa trang web Captcha của Google',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Khóa bí mật Captcha của Google',
+    ],
+    'score_threshold' => [
+        'name' => 'Ngưỡng điểm',
+        'instructions' => 'Giá trị từ 0 đến 1. Ngưỡng càng cao thì ReCaptcha càng nghiêm ngặt trong việc cố gắng xác định xem người dùng có phải là bot hay không.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
