a3ed8de9f95399f75810d4dfd8781121c2715e74
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:12:29 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/profile-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/de/setting.php
@@ -7,8 +7,17 @@ return [
     'upload_avatar' => [
         'name' => 'Avatar hochladen',
     ],
-
     'show_tax_office' => [
         'name' => 'Feld des Finanzamtes anzeigen',
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha Site Key',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha Secret Key',
+    ],
+    'score_threshold' => [
+        'name' => 'Score Threshold',
+        'instructions' => 'A value between 0 and 1. The higher the threshold the more strict ReCaptcha is in trying to determine if a user is a bot or not.',
     ],
 ];
