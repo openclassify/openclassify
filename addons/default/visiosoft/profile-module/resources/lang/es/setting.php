e8c6c21a13024e67dd40ba24b2052b7543deb951
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:52:58 +0300

    New translations setting.php (Spanish)

--- addons/default/visiosoft/profile-module/resources/lang/es/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/es/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'Carga de avatar',
     ],
-
     'show_tax_office' => [
         'name' => 'Mostrar campo de la oficina de impuestos',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Clave de sitio de Google Captcha',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Clave secreta de Google Captcha',
+    ],
+    'score_threshold' => [
+        'name' => 'Umbral de puntuación',
+        'instructions' => 'Un valor entre 0 y 1. Cuanto mayor sea el umbral, más estricto será ReCaptcha al tratar de determinar si un usuario es un bot o no.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
