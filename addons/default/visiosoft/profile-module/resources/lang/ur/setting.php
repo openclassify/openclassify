dc6b2ecd328ba4c91fbd4bcdbc1427550567da25
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:48:35 +0300

    New translations setting.php (Urdu (Pakistan))

--- addons/default/visiosoft/profile-module/resources/lang/ur/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/ur/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'اوتار اپ لوڈ',
     ],
-
     'show_tax_office' => [
         'name' => 'ٹیکس آفس فیلڈ دکھائیں',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'گوگل کیپچا سائٹ کی',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'گوگل کیپچا سیکرٹ کی',
+    ],
+    'score_threshold' => [
+        'name' => 'اسکور کی حد',
+        'instructions' => '0 اور 1 کے درمیان ایک قدر جس کی حد زیادہ ہے اس سے زیادہ سخت ری کیپچا اس بات کا تعین کرنے کی کوشش میں ہے کہ آیا صارف بوٹ ہے یا نہیں۔',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
