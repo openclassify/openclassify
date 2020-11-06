efc9aeda20f42dcddec4d8bc6673221ac5ae40e2
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:54:29 +0300

    New translations setting.php (Korean)

--- addons/default/visiosoft/profile-module/resources/lang/ko/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/ko/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => '아바타 업로드',
     ],
-
     'show_tax_office' => [
         'name' => '세무서 필드 표시',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha 사이트 키',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha 비밀 키',
+    ],
+    'score_threshold' => [
+        'name' => '점수 임계 값',
+        'instructions' => '0과 1 사이의 값입니다. 임계 값이 높을수록 ReCaptcha는 사용자가 봇인지 아닌지 확인하는 데 더 엄격한 기준을 적용합니다.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
