22d86405f93768b53e643a973fb42884e54b8d19
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:51:11 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/profile-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/el/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'Μεταφόρτωση Avatar',
     ],
-
     'show_tax_office' => [
         'name' => 'Εμφάνιση πεδίου φορολογικής υπηρεσίας',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Κλειδί ιστότοπου Google Captcha',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Μυστικό κλειδί Google Captcha',
+    ],
+    'score_threshold' => [
+        'name' => 'Όριο βαθμολογίας',
+        'instructions' => 'Μια τιμή μεταξύ 0 και 1. Όσο υψηλότερο είναι το όριο, τόσο πιο αυστηρό ReCaptcha προσπαθεί να προσδιορίσει εάν ένας χρήστης είναι bot ή όχι.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
