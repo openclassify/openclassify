6dbb0ec88c6aa394b452dce3763077a36eda7997
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:14:31 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/profile-module/resources/lang/hi/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/hi/setting.php
@@ -7,8 +7,17 @@ return [
     'upload_avatar' => [
         'name' => 'अवतार अपलोड',
     ],
-
     'show_tax_office' => [
         'name' => 'टैक्स ऑफिस फील्ड दिखाएं',
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha Site Key',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha Secret Key',
+    ],
+    'score_threshold' => [
+        'name' => 'Score Threshold',
+        'instructions' => 'A value between 0 and 1. The higher the threshold the more strict ReCaptcha is in trying to determine if a user is a bot or not.',
     ],
 ];
