01b4ddc788981f5ce5f3aa97eb5b11edb96b1343
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:54:11 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/profile-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/nl/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'Avatar uploaden',
     ],
-
     'show_tax_office' => [
         'name' => 'Toon belastingkantoor veld',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha-sitesleutel',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha geheime sleutel',
+    ],
+    'score_threshold' => [
+        'name' => 'Score drempel',
+        'instructions' => 'Een waarde tussen 0 en 1. Hoe hoger de drempel, hoe strikter ReCaptcha is om te bepalen of een gebruiker een bot is of niet.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
