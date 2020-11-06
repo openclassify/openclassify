688362a98ac2728146a4ff1e068efc4709a72049
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:46:24 +0300

    New translations setting.php (Bengali)

--- addons/default/visiosoft/profile-module/resources/lang/bn/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/bn/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'অবতার আপলোড',
     ],
-
     'show_tax_office' => [
         'name' => 'কর অফিস ক্ষেত্র প্রদর্শন করুন',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'গুগল ক্যাপচা সাইট কী',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'গুগল ক্যাপচা সিক্রেট কী',
+    ],
+    'score_threshold' => [
+        'name' => 'স্কোর থ্রেশহোল্ড',
+        'instructions' => '0 এবং 1 এর মধ্যে একটি মান একটি প্রারম্ভিক যত বেশি তত বেশি কঠোর রিক্যাপ্টচা নির্ধারণ করার চেষ্টা করছে যে কোনও ব্যবহারকারী বট বা না।',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
