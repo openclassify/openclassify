b66095ae46a1280ded623e4a1fc22b81e77788d5
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:13:00 +0300

    New translations message.php (Russian)

--- addons/default/visiosoft/profile-module/resources/lang/ru/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/ru/message.php
@@ -41,4 +41,8 @@ return [
     // Mail
     'update_email_mail_subject' => 'Электронная почта обновлена!',
     'update_email_mail_message' => 'Ваш адрес электронной почты обновлен!',
+
+    // Register
+    'recaptcha_field_is_required' => 'reCaptcha field is required.',
+    'failed_to_validate_captcha' => 'Failed to validate captcha.',
 ];
