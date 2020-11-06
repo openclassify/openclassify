0084b01f62fd711d8fa58f75501c26cbb22f19f7
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:57:00 +0300

    New translations setting.php (Japanese)

--- addons/default/visiosoft/profile-module/resources/lang/ja/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/ja/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'アバターのアップロード',
     ],
-
     'show_tax_office' => [
         'name' => '税務署フィールドを表示',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'GoogleCaptchaサイトキー',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'GoogleCaptchaシークレットキー',
+    ],
+    'score_threshold' => [
+        'name' => 'スコアのしきい値',
+        'instructions' => '0から1までの値。しきい値が高いほど、ReCaptchaはユーザーがボットであるかどうかを判断する際に厳密になります。',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
