292b73d28fc63aee8ccbe97db4bce427855ea3c1
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:12:19 +0300

    New translations message.php (Dutch)

--- addons/default/visiosoft/profile-module/resources/lang/nl/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/nl/message.php
@@ -38,4 +38,8 @@ return [
     // Mail
     'update_email_mail_subject' => 'E-mail bijgewerkt!',
     'update_email_mail_message' => 'Uw e-mail is bijgewerkt!',
+
+    // Register
+    'recaptcha_field_is_required' => 'reCaptcha field is required.',
+    'failed_to_validate_captcha' => 'Failed to validate captcha.',
 ];
