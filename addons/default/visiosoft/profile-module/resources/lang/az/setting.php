4e6c8b503aa68aac7f52905f045b69c126d3feae
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:47:58 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/profile-module/resources/lang/az/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/az/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'Avatar yüklə',
     ],
-
     'show_tax_office' => [
         'name' => 'Vergi Dairəsi Sahəsini göstərin',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Google Captcha Sayt Açarı',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Google Captcha Gizli Açar',
+    ],
+    'score_threshold' => [
+        'name' => 'Hesab ərəfəsi',
+        'instructions' => '0 ilə 1 arasındakı dəyər. Eşik nə qədər yüksək olsa, ReCaptcha istifadəçinin bot olub olmadığını müəyyənləşdirməyə çalışır.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
