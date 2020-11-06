df2063d6a17f29dc89dba944ff167e73760072de
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:49:28 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/profile-module/resources/lang/fa/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/fa/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'آپلود آواتار',
     ],
-
     'show_tax_office' => [
         'name' => 'نمایش زمینه اداره مالیات',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'کلید سایت Google Captcha',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'رمز مخفی Google Captcha',
+    ],
+    'score_threshold' => [
+        'name' => 'آستانه امتیاز',
+        'instructions' => 'مقداری بین 0 تا 1. هرچه آستانه بالاتر باشد ReCaptcha سختگیرانه تر است تا تلاش کند ربات را رد کند یا خیر.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
