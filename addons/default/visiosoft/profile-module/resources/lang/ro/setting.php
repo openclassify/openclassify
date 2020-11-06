86b586a52b20d6116449266e65e22414055763b6
Fatih Alp <mail@fatihalp.com>
2020-11-06 19:52:34 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/profile-module/resources/lang/ro/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/ro/setting.php
@@ -7,8 +7,34 @@ return [
     'upload_avatar' => [
         'name' => 'Încărcare Avatar',
     ],
-
     'show_tax_office' => [
         'name' => 'Arată câmpul Oficiului Fiscal',
+    ],
+    'show_checkbox_terms_on_register' => [
+        'name' => 'Show Checkbox Terms on Register',
+        'instructions' => 'Show the "Accept term", "protection law", "privacy term", "sms & emails" on the register form'
+    ],
+    'show_education_profession' => [
+        'name' => 'Show Education and Profession Fields',
+        'instructions' => 'Show the "Education", "State of Education" and "Profession" on the profile edit page'
+    ],
+    'google_captcha_site_key' => [
+        'name' => 'Cheia site-ului Google Captcha',
+    ],
+    'google_captcha_secret_key' => [
+        'name' => 'Cheia secretă Google Captcha',
+    ],
+    'score_threshold' => [
+        'name' => 'Prag de scor',
+        'instructions' => 'O valoare cuprinsă între 0 și 1. Cu cât pragul este mai mare, cu atât ReCaptcha este mai strict în încercarea de a determina dacă un utilizator este sau nu un bot.',
+    ],
+    'education' => [
+        'name' => 'Education'
+    ],
+    'state_of_education' => [
+        'name' => 'State of Education'
+    ],
+    'profession' => [
+        'name' => 'Profession'
     ],
 ];
