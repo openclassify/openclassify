4e261f1bc9f47f81dddb4d821c5aab29ca94e882
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:14:42 +0300

    New translations message.php (Azerbaijani)

--- addons/default/visiosoft/profile-module/resources/lang/az/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/az/message.php
@@ -38,4 +38,8 @@ return [
     // Mail
     'update_email_mail_subject' => 'E-poçt Yenilənib!',
     'update_email_mail_message' => 'E-poçtunuz Yeniləndi!',
+
+    // Register
+    'recaptcha_field_is_required' => 'reCaptcha field is required.',
+    'failed_to_validate_captcha' => 'Failed to validate captcha.',
 ];
