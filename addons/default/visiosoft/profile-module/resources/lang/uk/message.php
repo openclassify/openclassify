cbc687214baeb2b3d362f9c5999db403093f14ab
Fatih Alp <mail@fatihalp.com>
2020-11-06 12:13:51 +0300

    New translations message.php (Ukrainian)

--- addons/default/visiosoft/profile-module/resources/lang/uk/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/uk/message.php
@@ -38,4 +38,8 @@ return [
     // Mail
     'update_email_mail_subject' => 'Оновлено електронний лист!',
     'update_email_mail_message' => 'Ваш електронний лист оновлено!',
+
+    // Register
+    'recaptcha_field_is_required' => 'reCaptcha field is required.',
+    'failed_to_validate_captcha' => 'Failed to validate captcha.',
 ];
