9e0c30fd49fc8690fc51b8605f6f967ec42707b1
Fatih Alp <mail@fatihalp.com>
2020-11-11 20:26:47 +0300

    New translations field.php (Ukrainian)

--- addons/default/visiosoft/profile-module/resources/lang/uk/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/uk/field.php
@@ -428,7 +428,7 @@ return [
         'name' => 'індивідуальний',
     ],
     'corporate' => [
-        'name' => 'Corporate',
+        'name' => 'Корпоративні',
     ],
 
     'details' => [
@@ -464,7 +464,7 @@ return [
 
     // Register Type
     'personal' => [
-        'name' => 'Personal'
+        'name' => 'Особистий'
     ],
 
 ];
