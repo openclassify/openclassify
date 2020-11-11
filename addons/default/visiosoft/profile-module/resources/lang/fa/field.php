bd1a8c820f2cd489cbb0854ba5629321a8113b45
Fatih Alp <mail@fatihalp.com>
2020-11-11 15:40:50 +0300

    New translations field.php (Persian)

--- addons/default/visiosoft/profile-module/resources/lang/fa/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/fa/field.php
@@ -427,7 +427,7 @@ return [
         'name' => 'شخصی',
     ],
     'corporate' => [
-        'name' => 'شرکت های بزرگ، دارای شخصیت حقوقی',
+        'name' => 'Corporate',
     ],
 
     'details' => [
@@ -460,4 +460,10 @@ return [
     'accept_protection_law_label' => 'من شرایط قانون حمایت از داده های شخصی را می پذیرم.',
     'accept_privacy_terms_label' => 'من شرایط حریم خصوصی را می پذیرم.',
     'receive_sms_emails_label' => 'من می خواهم ایمیل و پیام کوتاه دریافت کنم.',
+
+    // Register Type
+    'personal' => [
+        'name' => 'Personal'
+    ],
+
 ];
