0e65dfed84ed53a97bb6305d2271176faa079001
Fatih Alp <mail@fatihalp.com>
2020-11-11 20:26:01 +0300

    New translations field.php (Vietnamese)

--- addons/default/visiosoft/cats-module/resources/lang/vi/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/vi/field.php
@@ -32,5 +32,5 @@ return [
 
     'please_wait' => 'Xin vui lòng chờ. Xóa danh mục phụ',
     'category_selection' => 'Lựa chọn danh mục',
-    'go_to_parent' => 'Go To Parent',
+    'go_to_parent' => 'Đi tới cha mẹ',
 ];
