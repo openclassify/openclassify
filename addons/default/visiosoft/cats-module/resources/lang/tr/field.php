922c3b5be5271fba890e8c8a5fb0416b5aafd0ab
Fatih Alp <mail@fatihalp.com>
2020-11-11 20:24:24 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/cats-module/resources/lang/tr/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/tr/field.php
@@ -32,5 +32,5 @@ return [
 
     'please_wait' => 'Lütfen bekleyin. Alt Kategoriler Siliniyor',
     'category_selection' => 'Kategori Seçimi',
-    'go_to_parent' => 'Go To Parent',
+    'go_to_parent' => 'Ebeveyne Git',
 ];
