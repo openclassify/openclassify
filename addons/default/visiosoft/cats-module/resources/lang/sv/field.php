709d6d3cd7b02b85308a4da82ffcc8117982c17f
Fatih Alp <mail@fatihalp.com>
2020-11-11 20:25:50 +0300

    New translations field.php (Swedish)

--- addons/default/visiosoft/cats-module/resources/lang/sv/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/sv/field.php
@@ -32,5 +32,5 @@ return [
 
     'please_wait' => 'Vänta. Ta bort underkategorier',
     'category_selection' => 'Val av kategori',
-    'go_to_parent' => 'Go To Parent',
+    'go_to_parent' => 'Gå till förälder',
 ];
