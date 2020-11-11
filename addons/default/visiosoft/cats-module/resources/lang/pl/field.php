5c34c5168730eed943129e20b1cf4dba76cc340e
Fatih Alp <mail@fatihalp.com>
2020-11-11 20:31:38 +0300

    New translations field.php (Polish)

--- addons/default/visiosoft/cats-module/resources/lang/pl/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/pl/field.php
@@ -32,5 +32,5 @@ return [
 
     'please_wait' => 'Proszę czekać. Usuwanie podkategorii',
     'category_selection' => 'Wybór kategorii',
-    'go_to_parent' => 'Go To Parent',
+    'go_to_parent' => 'Idź do rodzica',
 ];
