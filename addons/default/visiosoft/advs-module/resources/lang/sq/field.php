fc1d617038651264675dd1b76b6009369a0a148e
Fatih Alp <mail@fatihalp.com>
2020-11-12 11:44:11 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/field.php
@@ -302,6 +302,10 @@ return [
 
     // New create page
     'options' => 'Opsione',
+    'category_selection' => 'Category Selection',
+    'listing_details' => 'Listing Details',
+    'preview' => 'Preview',
+    'congratulations' => 'Congratulations',
 
     'free' => 'Falas',
     'ad_date' => 'Data e shpalljes',
