5a58293318cf6888b70eca91786578148cc260ae
Fatih Alp <mail@fatihalp.com>
2020-11-12 17:52:30 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
@@ -101,6 +101,20 @@ return [
     'hide_options_field' => [
         'name' => 'قسمت گزینه ها را پنهان کنید',
     ],
+    'hide_village_field' => [
+        'name' => 'Hide Village Field',
+    ],
+    'hide_configurations' => [
+        'name' => 'Hide the Configurations Button',
+        'instructions' => 'Hide the Configurations Button in the Ad Preview Page',
+    ],
+    'make_map_required' => [
+        'name' => 'Make Map Required',
+        'instructions' => 'Require the user to place a pin on the map when creating an ad',
+    ],
+    'hide_listing_standard_price' => [
+        'name' => 'Hide Listing Standard Price',
+    ],
     'enabled_currencies' => [
         'name' => 'ارزهای فعال شده',
     ],
