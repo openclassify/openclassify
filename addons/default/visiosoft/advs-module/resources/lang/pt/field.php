04c4a48c7969cba2a4583e540fedafc1b2bf331c
Fatih Alp <mail@fatihalp.com>
2020-11-12 11:44:13 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/field.php
@@ -302,6 +302,10 @@ return [
 
     // New create page
     'options' => 'Opções',
+    'category_selection' => 'Category Selection',
+    'listing_details' => 'Listing Details',
+    'preview' => 'Preview',
+    'congratulations' => 'Congratulations',
 
     'free' => 'Livre',
     'ad_date' => 'Data do Anúncio',
