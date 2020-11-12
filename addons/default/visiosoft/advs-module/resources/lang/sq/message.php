2816b31d720505ca164292165ae91f7d7ca27c0e
Fatih Alp <mail@fatihalp.com>
2020-11-12 17:53:29 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/message.php
@@ -25,4 +25,5 @@ return [
     'no_add_found' => 'Asnjë shpallje nuk u gjet!',
     'replicated_success' => 'Shpallja është përsëritur me sukses!',
     'ad_doesnt_exist' => "Kjo shpallje nuk ekziston!",
+    'select_location_error' => "Please select a location on the map!",
 ];
