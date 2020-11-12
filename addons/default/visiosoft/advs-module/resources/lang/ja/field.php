b74b75657a6da29ff1c2baa956419d8892547aed
Fatih Alp <mail@fatihalp.com>
2020-11-12 17:38:56 +0300

    New translations field.php (Japanese)

--- addons/default/visiosoft/advs-module/resources/lang/ja/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ja/field.php
@@ -301,10 +301,10 @@ return [
 
     // New create page
     'options' => 'オプション',
-    'category_selection' => 'Category Selection',
-    'listing_details' => 'Listing Details',
-    'preview' => 'Preview',
-    'congratulations' => 'Congratulations',
+    'category_selection' => 'カテゴリの選択',
+    'listing_details' => 'リストの詳細',
+    'preview' => 'プレビュー',
+    'congratulations' => 'おめでとう',
 
     'free' => '自由',
     'ad_date' => '広告日',
