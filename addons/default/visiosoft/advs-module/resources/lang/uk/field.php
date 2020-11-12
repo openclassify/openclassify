4eab885eff3864ce66fe59e5bca74a7717875b6c
Fatih Alp <mail@fatihalp.com>
2020-11-12 17:53:45 +0300

    New translations field.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/field.php
@@ -301,10 +301,10 @@ return [
 
     // New create page
     'options' => 'Параметри',
-    'category_selection' => 'Category Selection',
-    'listing_details' => 'Listing Details',
-    'preview' => 'Preview',
-    'congratulations' => 'Congratulations',
+    'category_selection' => 'Вибір категорії',
+    'listing_details' => 'Деталі переліку',
+    'preview' => 'Попередній перегляд',
+    'congratulations' => 'Вітаю',
 
     'free' => 'Безкоштовно',
     'ad_date' => 'Дата оголошення',
