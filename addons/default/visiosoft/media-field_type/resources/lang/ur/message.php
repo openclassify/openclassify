381e1cc1413bc4af363391cf1341232053494c14
Fatih Alp <mail@fatihalp.com>
2020-11-16 18:23:06 +0300

    New translations message.php (Urdu (Pakistan))

--- addons/default/visiosoft/media-field_type/resources/lang/ur/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/ur/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'آپ کس فولڈر میں اپ لوڈ کرنا چاہیں گے؟',
-    'upload' => 'İmages شامل کریں اپ لوڈ İmages | تصویر منسلک کریں',
+    'upload' => 'Add images | Upload images | Attach a photo',
     'choose_files' => 'آپ کون سی فائلیں استعمال کرنا چاہیں گے؟',
     'no_files_selected' => 'کوئی فائلیں منتخب نہیں کی گئیں۔',
     'no_uploads' => 'کوئی فائلیں اپ لوڈ نہیں کی گئیں۔',
