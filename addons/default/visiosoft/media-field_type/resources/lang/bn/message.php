bb56e88b0818c1f2da9d27149c286e92a86234ac
Fatih Alp <mail@fatihalp.com>
2020-11-16 18:23:01 +0300

    New translations message.php (Bengali)

--- addons/default/visiosoft/media-field_type/resources/lang/bn/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/bn/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'আপনি কোন ফোল্ডারে আপলোড করতে চান?',
-    'upload' => 'যোগ করুন İmages | আপলোড করুন | একটি ছবি সংযুক্ত করুন',
+    'upload' => 'Add images | Upload images | Attach a photo',
     'choose_files' => 'আপনি কোন ফাইলগুলি ব্যবহার করতে চান?',
     'no_files_selected' => 'কোনও ফাইল নির্বাচিত হয়নি।',
     'no_uploads' => 'কোনও ফাইল আপলোড করা হয়নি।',
