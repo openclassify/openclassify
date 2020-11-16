64df053914f6223138297faf9c322ee88b47e824
Fatih Alp <mail@fatihalp.com>
2020-11-16 18:23:11 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/media-field_type/resources/lang/sq/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/sq/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'Në cilën dosje dëshironi të ngarkoni?',
-    'upload' => 'Shtoni agesmages | Ngarko İmages | Bashkangjit një fotografi',
+    'upload' => 'Add images | Upload images | Attach a photo',
     'choose_files' => 'Cilat skedarë dëshironi të përdorni?',
     'no_files_selected' => 'Asnjë skedar i zgjedhur.',
     'no_uploads' => 'Asnjë fotografi të ngarkuar.',
