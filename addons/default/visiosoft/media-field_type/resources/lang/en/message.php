3ddb65b4a7c31dbdc7cdce0d6f9ea2a478ac72ae
Diatrex <diahshalabi@hotmail.com>
2020-11-16 17:42:38 +0300

    fix trans

--- addons/default/visiosoft/media-field_type/resources/lang/en/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/en/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'Which folder would you like to upload to?',
-    'upload' => 'Add İmages | Upload İmages | Attach a photo',
+    'upload' => 'Add images | Upload images | Attach a photo',
     'choose_files' => 'Which files would you like to use?',
     'no_files_selected' => 'No files selected.',
     'no_uploads' => 'No files uploaded.',
