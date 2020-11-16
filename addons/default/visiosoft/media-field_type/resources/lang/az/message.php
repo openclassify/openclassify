13816bd20db1235cf8e989d269baa33b6eed0d56
Fatih Alp <mail@fatihalp.com>
2020-11-16 18:22:59 +0300

    New translations message.php (Azerbaijani)

--- addons/default/visiosoft/media-field_type/resources/lang/az/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/az/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'Hansı qovluğa yükləmək istərdiniz?',
-    'upload' => 'İmages əlavə edin | İmages yüklə | Bir şəkil əlavə edin',
+    'upload' => 'Add images | Upload images | Attach a photo',
     'choose_files' => 'Hansı fayllardan istifadə etmək istərdiniz?',
     'no_files_selected' => 'Fayl seçilməyib.',
     'no_uploads' => 'Fayl yüklənməyib.',
