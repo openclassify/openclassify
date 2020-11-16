e6923826cea699a64623959a1589fa4b495f7297
Fatih Alp <mail@fatihalp.com>
2020-11-16 14:54:55 +0300

    New translations message.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/message.php
@@ -25,5 +25,5 @@ return [
     'no_add_found' => 'Nu a fost găsit niciun anunț!',
     'replicated_success' => 'Anunțul a fost reprodus cu succes!',
     'ad_doesnt_exist' => "Acest anunț nu există!",
-    'select_location_error' => "Please select a location on the map!",
+    'select_location_error' => "Vă rugăm să selectați o locație pe hartă!",
 ];
