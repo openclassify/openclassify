e625009356e036d8d67f54e63947ce574beefdca
Fatih Alp <mail@fatihalp.com>
2020-11-16 16:27:21 +0300

    New translations message.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/message.php
@@ -26,4 +26,5 @@ return [
     'replicated_success' => 'Die Anzeige wurde erfolgreich repliziert!',
     'ad_doesnt_exist' => "Diese Anzeige existiert nicht!",
     'select_location_error' => "Bitte wählen Sie einen Ort auf der Karte!",
+    'this_ad_is_not_valid_anymore' => "This ad is not valid anymore!",
 ];
