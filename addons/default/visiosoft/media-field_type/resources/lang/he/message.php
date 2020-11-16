ef66dbed5be591e216741fed6c73f4596536f5c5
Fatih Alp <mail@fatihalp.com>
2020-11-16 18:23:27 +0300

    New translations message.php (Hebrew)

--- addons/default/visiosoft/media-field_type/resources/lang/he/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/he/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'לאיזו תיקיה תרצה להעלות?',
-    'upload' => 'הוסף תמונות | העלה תמונות | לצרף תמונה',
+    'upload' => 'Add images | Upload images | Attach a photo',
     'choose_files' => 'באילו קבצים תרצה להשתמש?',
     'no_files_selected' => 'לא נבחרו קבצים.',
     'no_uploads' => 'לא הועלו קבצים.',
