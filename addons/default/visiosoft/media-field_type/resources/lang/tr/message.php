5f6ed93dc43306aa4a9c621f253f262ec3c711c9
Fatih Alp <mail@fatihalp.com>
2020-11-17 19:08:14 +0300

    New translations message.php (Turkish)

--- addons/default/visiosoft/media-field_type/resources/lang/tr/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/tr/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'Hangi klasöre yüklemek istersiniz?',
-    'upload' => 'Add images | Upload images | Attach a photo',
+    'upload' => 'Resim ekle | Resim yükle | Fotoğraf ekle',
     'choose_files' => 'Hangi dosyaları kullanmak istersiniz?',
     'no_files_selected' => 'Seçili dosya yok.',
     'no_uploads' => 'Dosya yüklenmedi.',
