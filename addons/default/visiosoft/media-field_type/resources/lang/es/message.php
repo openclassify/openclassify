9b9a516beb30f5b29b195af6b1d46e1745a32774
Fatih Alp <mail@fatihalp.com>
2020-11-17 18:55:48 +0300

    New translations message.php (Spanish)

--- addons/default/visiosoft/media-field_type/resources/lang/es/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/es/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => '¿A qué carpeta le gustaría subir?',
-    'upload' => 'Add images | Upload images | Attach a photo',
+    'upload' => 'Agregar imágenes | Subir imágenes | Adjuntar una foto',
     'choose_files' => '¿Qué archivos te gustaría usar?',
     'no_files_selected' => 'No hay archivos seleccionados.',
     'no_uploads' => 'No hay archivos cargados.',
