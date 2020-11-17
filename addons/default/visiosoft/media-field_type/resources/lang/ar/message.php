14e8c6a97c6e00c8f751003fc3fe223db0c86393
Fatih Alp <mail@fatihalp.com>
2020-11-17 18:55:31 +0300

    New translations message.php (Arabic)

--- addons/default/visiosoft/media-field_type/resources/lang/ar/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/ar/message.php
@@ -2,7 +2,7 @@
 
 return [
     'choose_folder' => 'ماهو المجلد الذي تريد الرفع إليه?',
-    'upload' => 'Add images | Upload images | Attach a photo',
+    'upload' => 'إضافة الصور | تحميل الصور | ارفاق الصورة',
     'choose_files' => 'ما هي الملفات التي تريد استخدامها?',
     'no_files_selected' => 'لم يتم اختيار الملفات.',
     'no_uploads' => 'لم يتم رفع الملفات.',
