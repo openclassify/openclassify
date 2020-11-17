80e8821bfc1828c3729fd10dcbff9aed999eaf6f
Fatih Alp <mail@fatihalp.com>
2020-11-17 18:47:47 +0300

    New translations message.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/message.php
@@ -26,5 +26,5 @@ return [
     'replicated_success' => '¡El anuncio se ha replicado correctamente!',
     'ad_doesnt_exist' => "¡Este anuncio no existe!",
     'select_location_error' => "Seleccione una ubicación en el mapa.",
-    'this_ad_is_not_valid_anymore' => "This ad is not valid anymore!",
+    'this_ad_is_not_valid_anymore' => "¡Este anuncio ya no es válido!",
 ];
