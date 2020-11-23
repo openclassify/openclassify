fbc83253959242c74561b6071f4d4ff0655ed259
Fatih Alp <mail@fatihalp.com>
2020-11-23 12:22:15 +0300

    New translations field.php (Dutch)

--- addons/default/visiosoft/profile-module/resources/lang/nl/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/nl/field.php
@@ -457,6 +457,7 @@ return [
     'edit_details' => 'Details bewerken',
     'update' => 'Bijwerken',
     'change_password' => 'Wachtwoord wijzigen',
+    'choose_an_option' => 'Choose an option...',
 
     // Register page
     'accept_terms_label' => 'Ik ga akkoord met de voorwaarden.',
@@ -468,5 +469,4 @@ return [
     'personal' => [
         'name' => 'Persoonlijk'
     ],
-
 ];
