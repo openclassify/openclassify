085395c1844c674cb712e3918fada473b3fa8d93
Fatih Alp <mail@fatihalp.com>
2020-11-24 15:28:25 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/profile-module/resources/lang/sv/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/sv/setting.php
@@ -14,6 +14,12 @@ return [
         'name' => 'Visa kryssrutans villkor i registret',
         'instructions' => 'Visa "Acceptera term", "skyddslag", "sekretessbegrepp", "sms & e-post" på registerformuläret'
     ],
+    'register_protection_url' => [
+        'name' => 'Register Protection URL',
+    ],
+    'register_privacy_url' => [
+        'name' => 'Register Privacy URL',
+    ],
     'show_education_profession' => [
         'name' => 'Visa utbildnings- och yrkesfält',
         'instructions' => 'Visa "Utbildning", "Utbildningsläge" och "Yrke" på profilredigeringssidan'
