3c976e6038c219c019f79762ecc3dc94be319553
Fatih Alp <mail@fatihalp.com>
2020-11-24 15:29:35 +0300

    New translations field.php (Korean)

--- addons/default/visiosoft/profile-module/resources/lang/ko/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ko/field.php
@@ -459,8 +459,8 @@ return [
 
     // Register page
     'accept_terms_label' => '약관에 동의합니다.',
-    'accept_protection_law_label' => '개인 데이터 보호에 관한 법률 조항에 동의합니다.',
-    'accept_privacy_terms_label' => '개인 정보 보호 약관에 동의합니다.',
+    'accept_protection_law_label' => 'I accept the terms of the Law on <a href="' . setting_value('visiosoft.module.profile::register_protection_url') . '" target="_blank">Protection of Personal Data.</a>',
+    'accept_privacy_terms_label' => 'I accept <a href="' . setting_value('visiosoft.module.profile::register_privacy_url') . '" target="_blank">privacy terms</a>.',
     'receive_sms_emails_label' => '이메일과 SMS를 받고 싶습니다.',
 
     // Register Type
