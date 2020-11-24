709967800d423a6f195cb48e674056e1fced9e80
Fatih Alp <mail@fatihalp.com>
2020-11-24 15:29:20 +0300

    New translations field.php (Greek)

--- addons/default/visiosoft/profile-module/resources/lang/el/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/el/field.php
@@ -461,8 +461,8 @@ return [
 
     // Register page
     'accept_terms_label' => 'Αποδέχομαι τους όρους.',
-    'accept_protection_law_label' => 'Αποδέχομαι τους όρους του Νόμου περί Προστασίας Προσωπικών Δεδομένων.',
-    'accept_privacy_terms_label' => 'Αποδέχομαι τους όρους απορρήτου.',
+    'accept_protection_law_label' => 'I accept the terms of the Law on <a href="' . setting_value('visiosoft.module.profile::register_protection_url') . '" target="_blank">Protection of Personal Data.</a>',
+    'accept_privacy_terms_label' => 'I accept <a href="' . setting_value('visiosoft.module.profile::register_privacy_url') . '" target="_blank">privacy terms</a>.',
     'receive_sms_emails_label' => 'Θέλω να λαμβάνω e-mail & sms.',
 
     // Register Type
