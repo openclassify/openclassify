47407ae70f1e84902232c5f02b2d71e6b088175a
Fatih Alp <mail@fatihalp.com>
2020-11-27 16:17:44 +0300

    New translations button.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/button.php
@@ -2,6 +2,7 @@
 
 return [
     'new_adv' => 'Novo Adv',
+    'new_classified' => 'New classified',
     'new_category' => 'Nova categoria',
     'new_country' => 'Novo país',
     'new_city' => 'Nova cidade',
@@ -39,4 +40,5 @@ return [
     'new_option_configuration' => 'Configuração de nova opção',
 	'create_configurations' => 'Criar configurações',
 	'replicate' => 'Replicar',
+	'fast_create' => 'Fast create',
 ];
