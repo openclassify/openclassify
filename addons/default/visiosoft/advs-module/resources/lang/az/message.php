6dd060298953cc52665a6d40de8ca7f3686f5a37
Fatih Alp <mail@fatihalp.com>
2020-11-30 18:00:57 +0300

    New translations message.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/message.php
@@ -27,4 +27,6 @@ return [
     'ad_doesnt_exist' => "Bu reklam mövcud deyil!",
     'select_location_error' => "Xahiş edirəm xəritədə bir yer seçin!",
     'this_ad_is_not_valid_anymore' => "Bu reklam artıq etibarlı deyil!",
+    'approve_status_change' => "Your Ad's Status Has Been Set to Active!",
+    'passive_status_change' => "Your Ad's Status Has Been Set to Passive!",
 ];
