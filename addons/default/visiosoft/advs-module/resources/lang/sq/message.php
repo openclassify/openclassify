3fcd1a9601d323a7fb98c68e8217dc9fb2e8032a
Fatih Alp <mail@fatihalp.com>
2020-11-30 18:01:15 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/message.php
@@ -27,4 +27,6 @@ return [
     'ad_doesnt_exist' => "Kjo shpallje nuk ekziston!",
     'select_location_error' => "Ju lutemi zgjidhni një vendndodhje në hartë!",
     'this_ad_is_not_valid_anymore' => "Kjo reklamë nuk është më e vlefshme!",
+    'approve_status_change' => "Your Ad's Status Has Been Set to Active!",
+    'passive_status_change' => "Your Ad's Status Has Been Set to Passive!",
 ];
