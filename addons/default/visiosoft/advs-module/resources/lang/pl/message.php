4e63f50fa8a1c4a0c3b08f7b20ad12c6573d8d88
Fatih Alp <mail@fatihalp.com>
2020-11-30 18:01:20 +0300

    New translations message.php (Polish)

--- addons/default/visiosoft/advs-module/resources/lang/pl/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/pl/message.php
@@ -27,4 +27,6 @@ return [
     'ad_doesnt_exist' => "Ta reklama nie istnieje!",
     'select_location_error' => "Wybierz lokalizację na mapie!",
     'this_ad_is_not_valid_anymore' => "Ta reklama jest już nieważna!",
+    'approve_status_change' => "Your Ad's Status Has Been Set to Active!",
+    'passive_status_change' => "Your Ad's Status Has Been Set to Passive!",
 ];
