724b68186582bff78528e4adbe1ca8f0599e8b3b
Fatih Alp <mail@fatihalp.com>
2020-11-30 18:01:30 +0300

    New translations message.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/message.php
@@ -27,4 +27,6 @@ return [
     'ad_doesnt_exist' => "מודעה זו אינה קיימת!",
     'select_location_error' => "אנא בחר מיקום במפה!",
     'this_ad_is_not_valid_anymore' => "מודעה זו אינה תקפה יותר!",
+    'approve_status_change' => "Your Ad's Status Has Been Set to Active!",
+    'passive_status_change' => "Your Ad's Status Has Been Set to Passive!",
 ];
