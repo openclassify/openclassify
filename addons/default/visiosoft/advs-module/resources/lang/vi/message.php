460100c0d7498b9a33bfee4db87093bed66a8bf2
Fatih Alp <mail@fatihalp.com>
2020-11-30 18:01:03 +0300

    New translations message.php (Vietnamese)

--- addons/default/visiosoft/advs-module/resources/lang/vi/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/vi/message.php
@@ -27,4 +27,6 @@ return [
     'ad_doesnt_exist' => "Quảng cáo này không tồn tại!",
     'select_location_error' => "Vui lòng chọn một vị trí trên bản đồ!",
     'this_ad_is_not_valid_anymore' => "Quảng cáo này không hợp lệ nữa!",
+    'approve_status_change' => "Your Ad's Status Has Been Set to Active!",
+    'passive_status_change' => "Your Ad's Status Has Been Set to Passive!",
 ];
