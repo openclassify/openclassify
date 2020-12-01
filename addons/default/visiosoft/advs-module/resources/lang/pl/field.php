860a7c862d985145f7e81ae4450a637185c62d44
Fatih Alp <mail@fatihalp.com>
2020-12-01 18:23:32 +0300

    New translations field.php (Polish)

--- addons/default/visiosoft/advs-module/resources/lang/pl/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/pl/field.php
@@ -82,6 +82,15 @@ return [
     'cat7' => [
         'name' => 'Podkategoria 6'
     ],
+    'cat8' => [
+        'name' => 'Podkategoria 7'
+    ],
+    'cat9' => [
+        'name' => 'Podkategoria 8'
+    ],
+    'cat10' => [
+        'name' => 'Podkategoria 9'
+    ],
     'currency' => [
         'name' => 'Waluta'
     ],
