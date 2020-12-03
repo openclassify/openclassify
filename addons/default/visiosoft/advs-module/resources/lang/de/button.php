5a57501423858c4e871c79514abdc1e1da328dfc
Fatih Alp <mail@fatihalp.com>
2020-12-03 17:48:43 +0300

    New translations button.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/button.php
@@ -41,5 +41,5 @@ return [
 	'create_configurations' => 'Konfigurationen erstellen',
 	'replicate' => 'Replizieren',
 	'fast_create' => 'Schnell erstellen',
-	'publish' => 'Publish',
+	'publish' => 'Veröffentlichen',
 ];
