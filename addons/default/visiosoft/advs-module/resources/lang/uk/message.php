d4c5061ba19619208c2508ce2f3795f4c2001aea
Fatih Alp <mail@fatihalp.com>
2020-12-03 18:13:04 +0300

    New translations message.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/message.php
@@ -4,7 +4,7 @@ return [
     'error_added_cart' => 'На даний момент запитуваний продукт відсутній або неактивний',
     'please_payment' => 'Будь ласка, платіть',
     'bank_account_info' => 'Інформація про банківський рахунок',
-    'agreement' => 'By clicking Publish, you agree to our Terms of Use and Privacy Policy acknowledge that you are the rightful owner of this item.',
+    'agreement' => 'Натискаючи Опублікувати, ви погоджуєтесь з нашими Умовами використання та Політикою конфіденційності підтверджуєте, що ви є законним власником цього товару.',
     'update_category_msg' => 'Класифікована категорія буде оновлена.',
     'updated_category_msg' => 'Категорію успішно відредаговано.',
     'create_ad_with_post_cat' => [
