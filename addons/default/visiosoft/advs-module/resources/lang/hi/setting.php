263aff934991470e174568f2c7dd0346e3425b54
Fatih Alp <mail@fatihalp.com>
2020-12-03 16:54:13 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/advs-module/resources/lang/hi/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/hi/setting.php
@@ -197,4 +197,12 @@ return [
     'show_breadcrumb_when_creating_ad' => [
         'name' => 'विज्ञापन बनाते समय ब्रेडक्रंब दिखाएं',
     ],
+    'show_ads_count' => [
+        'name' => 'Show Ads Count',
+        'instructions' => 'Show ads count on the left categories list',
+    ],
+    'show_post_ad_agreement' => [
+        'name' => 'Show Post Ad Agreement',
+        'instructions' => 'Turn this on if you want to display a privacy policy agreement on the ad creation page',
+    ],
 ];
