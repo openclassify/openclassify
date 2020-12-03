4e400c6cb694f5aba6757486e36bc69d04bb69b3
Fatih Alp <mail@fatihalp.com>
2020-12-03 16:53:25 +0300

    New translations message.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/message.php
@@ -4,7 +4,7 @@ return [
     'error_added_cart' => 'Tələb olunan məhsul hazırda itkin və ya hərəkətsizdir',
     'please_payment' => 'Zəhmət olmasa ödəyin',
     'bank_account_info' => 'Bank hesabı məlumatları',
-    'agreement' => 'Post\'u tıklayarak, İstifadə şərtlərimizlə razılaşırsınız və Məxfilik Siyasəti bu maddənin qanuni sahibi olduğunuzu qəbul edirsiniz.',
+    'agreement' => 'By clicking Publish, you agree to our Terms of Use and Privacy Policy acknowledge that you are the rightful owner of this item.',
     'update_category_msg' => 'Təsnif edilmiş kateqoriya yenilənəcəkdir.',
     'updated_category_msg' => 'Kateqoriya uğurla düzəldildi.',
     'create_ad_with_post_cat' => [
