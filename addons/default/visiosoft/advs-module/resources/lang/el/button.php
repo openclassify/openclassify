4affdf1fe33b14689fed1fc502cadb17204b4b09
Fatih Alp <mail@fatihalp.com>
2020-12-03 17:48:16 +0300

    New translations button.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/button.php
@@ -41,5 +41,5 @@ return [
 	'create_configurations' => 'Δημιουργήστε διαμορφώσεις',
 	'replicate' => 'Αντιγραφή',
 	'fast_create' => 'Γρήγορη δημιουργία',
-	'publish' => 'Publish',
+	'publish' => 'Δημοσιεύω',
 ];
