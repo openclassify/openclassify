986fc58272a9812e1b2633e7f52458823abaff66
Fatih Alp <mail@fatihalp.com>
2020-12-05 16:12:15 +0300

    New translations permission.php (Kurdish)

---
+++ addons/default/visiosoft/profile-module/resources/lang/ku/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'profile' => [
+        'name'   => 'Profile',
+        'option' => [
+            'read'   => 'Can read profile?',
+            'write'  => 'Can create/edit profile?',
+            'delete' => 'Can delete profile?',
+        ],
+    ],
+    'adress' => [
+        'name'   => 'Adress',
+        'option' => [
+            'read'   => 'Can read adress?',
+            'write'  => 'Can create/edit adress?',
+            'delete' => 'Can delete adress?',
+        ],
+    ],
+];
