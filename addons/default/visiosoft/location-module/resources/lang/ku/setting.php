40a0d678fa60efe342c1d70dbb6519f962716f4e
Fatih Alp <mail@fatihalp.com>
2020-12-05 16:12:42 +0300

    New translations setting.php (Kurdish)

---
+++ addons/default/visiosoft/location-module/resources/lang/ku/setting.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => 'Home Page Location Field',
+    ],
+    'list_page_location' => [
+        'name' => 'List Page Location Field',
+    ],
+    'detail_page_location' => [
+        'name' => 'Detail Page Location Field',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'Show Search Location Button',
+    ],
+    'default_country' => [
+        'name' => 'Default Country',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'Create Ad Page Location',
+    ],
+    'default_city' => [
+        'name' => 'Default City',
+    ],
+    'default_district' => [
+        'name' => 'Default District',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Default Neighborhood',
+    ],
+
+    'google_map_key' => [
+        'name' => 'Google Maps Api Key',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'Default Long',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'Default Lat',
+    ],
+
+    'hide_location_filter' => [
+        'name' => 'Hide Location Filter',
+    ],
+	'country_for_phone_field' => [
+		'name' => 'Default Country For Register Phone Field'
+	],
+    'sorting_column' => [
+        'name' => 'Sorting Column',
+    ],
+    'sorting_type' => [
+        'name' => 'Sorting Type',
+    ],
+];
