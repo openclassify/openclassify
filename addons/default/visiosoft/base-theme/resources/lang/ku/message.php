d4a4ed7c7e8b9b1e2b7c20f74ae0642269614201
Fatih Alp <mail@fatihalp.com>
2020-12-08 13:38:39 +0300

    New translations message.php (Kurdish)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ku/message.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'send_forgot_sms' => 'SMS ji telefona xweya tomarkirî re şandiye. Ji kerema xwe venêrin.',
+    'found_phone' => 'Jimara têlefona ku di pergalê de hatî tomar kirin nehat dîtin.',
+];
