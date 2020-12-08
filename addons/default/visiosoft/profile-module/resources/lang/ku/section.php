6fa1aa2d30c387cb48d8e2b74540c62b71e856db
Fatih Alp <mail@fatihalp.com>
2020-12-08 13:38:52 +0300

    New translations section.php (Kurdish)

---
+++ addons/default/visiosoft/profile-module/resources/lang/ku/section.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'Tengal',
+    ],
+    'adress' => [
+        'title' => 'Navnîşan',
+    ],
+    'general_setting' => 'Mîhengên Giştî',
+    'recaptcha' => 'reCAPTCHA',
+];
