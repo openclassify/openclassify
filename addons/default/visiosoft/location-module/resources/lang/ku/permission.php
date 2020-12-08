54607d9d6039279ae3992a6dd218bf8fc123f936
Fatih Alp <mail@fatihalp.com>
2020-12-08 13:40:07 +0300

    New translations permission.php (Kurdish)

---
+++ addons/default/visiosoft/location-module/resources/lang/ku/permission.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'village' => [
+        'name'   => 'Gûnd',
+        'option' => [
+            'read'   => 'Dikarin gund bixwînin?',
+            'write'  => 'Ma dikare gund çêbike / sererast bike?',
+            'delete' => 'Ma dikare gund jê bibe?',
+        ],
+    ],
+];
