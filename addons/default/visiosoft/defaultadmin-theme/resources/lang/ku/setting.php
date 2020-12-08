ca9baee673741e9070b268b4a4b4122525efd759
Fatih Alp <mail@fatihalp.com>
2020-12-08 13:40:16 +0300

    New translations setting.php (Kurdish)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ku/setting.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Sidebar Hover',
+        'instructions' => 'Zivirandina kêleka kêlekê?',
+    ],
+    'navigation'    => [
+        'name'         => 'Rêserbirinî',
+        'instructions' => 'Hên ji <em>default</em> fermana navîgasyon.',
+        'warning'      => 'Yekem tişta navîgasyona gihînbar wekî qada <strong>mal</strong> tê bikar anîn.',
+        'reorder'      => 'Tiştên navîgasyonên sereke di <strong>tenişta</strong> bikişînin û bavêjin da ku wan ji nû ve rêz bikin.',
+    ],
+    "login_icon" => [
+        'name' => 'Icon-ê têkevê',
+    ],
+    "panel_icon" => [
+        'name' => 'Conkona panelê',
+    ],
+    "title" => [
+        'name' => 'Nav',
+    ],
+    "footer_copyright_org_name" => [
+        'name' => 'Footer Navê Rêxistina Copyright',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Rengdêr Rengîniya Sidebar',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Rengê Duyemîn Rengdêr',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Rengê Sînorê Sidebar-ê Çalak',
+    ]
+];
