5f0eff67c5fcf8dce99c86bf29fbeddc7674d64a
Fatih Alp <mail@fatihalp.com>
2020-12-08 12:47:17 +0300

    New translations setting.php (Kurdish)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/ku/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ku/setting.php
@@ -3,33 +3,33 @@
 return [
     'sidebar_hover' => [
         'name'         => 'Sidebar Hover',
-        'instructions' => 'Expand the sidebar on hover?',
+        'instructions' => 'Zivirandina kêleka kêlekê?',
     ],
     'navigation'    => [
-        'name'         => 'Navigation',
-        'instructions' => 'Specify the <em>default</em> order of navigation.',
-        'warning'      => 'The first accessible navigation item is used as the <strong>home</strong> area.',
-        'reorder'      => 'Drag and drop the primary navigation items in the <strong>sidebar</strong> to reorder them.',
+        'name'         => 'Rêserbirinî',
+        'instructions' => 'Hên ji <em>default</em> fermana navîgasyon.',
+        'warning'      => 'Yekem tişta navîgasyona gihînbar wekî qada <strong>mal</strong> tê bikar anîn.',
+        'reorder'      => 'Tiştên navîgasyonên sereke di <strong>tenişta</strong> bikişînin û bavêjin da ku wan ji nû ve rêz bikin.',
     ],
     "login_icon" => [
-        'name' => 'Login Icon',
+        'name' => 'Icon-ê têkevê',
     ],
     "panel_icon" => [
-        'name' => 'Panel Icon',
+        'name' => 'Conkona panelê',
     ],
     "title" => [
-        'name' => 'Title',
+        'name' => 'Nav',
     ],
     "footer_copyright_org_name" => [
-        'name' => 'Footer Copyright Organization Name',
+        'name' => 'Footer Navê Rêxistina Copyright',
     ],
     "sidebar_main_color" => [
-        'name' => 'Sidebar Main Color',
+        'name' => 'Rengdêr Rengîniya Sidebar',
     ],
     "sidebar_secondary_color" => [
-        'name' => 'Sidebar Secondary Color',
+        'name' => 'Rengê Duyemîn Rengdêr',
     ],
     "active_sidebar_border_color" => [
-        'name' => 'Active Sidebar Border Color',
+        'name' => 'Rengê Sînorê Sidebar-ê Çalak',
     ]
 ];
