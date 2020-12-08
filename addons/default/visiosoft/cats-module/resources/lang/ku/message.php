141c84cb020411d4a856c0e0e23a62f64900ee41
Fatih Alp <mail@fatihalp.com>
2020-12-08 12:47:27 +0300

    New translations message.php (Kurdish)

--- addons/default/visiosoft/cats-module/resources/lang/ku/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/ku/message.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'categories_mass_delete_success' => 'Categories and related sub-categories has been deleted successfully!',
+    'categories_mass_delete_success' => 'Kategorî û jêr-kategorîyên têkildar bi serfirazî hatin jêbirin!',
 ];
