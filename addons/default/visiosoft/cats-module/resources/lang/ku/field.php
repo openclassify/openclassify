8f728b1ed23df7370ec9361f289f0026a4287a5c
Fatih Alp <mail@fatihalp.com>
2020-12-08 12:46:40 +0300

    New translations field.php (Kurdish)

--- addons/default/visiosoft/cats-module/resources/lang/ku/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ku/field.php
@@ -2,35 +2,35 @@
 
 return [
     'name' => [
-        'name' => 'Title',
-        'instructions' => 'It is the name of the main category that you will define. It allows the user to easily find the product they are looking for.',
+        'name' => 'Nav',
+        'instructions' => 'Ew navê kategoriya sereke ye ku hûn ê diyar bikin. Ew dihêle bikarhêner bi hêsanî hilbera ku lê digerin bibîne.',
     ],
     'order' => [
-        'name' => 'Order',
+        'name' => 'Emir',
     ],
 
     'slug' => [
-        'name' => 'Slug',
-        'instructions' => 'Specify any organizational tags to help group your post with others.',
+        'name' => 'Şeytanok',
+        'instructions' => 'Ji bo ku koma posta xwe bi yên din re bike alîkar her tagên rêxistinê diyar bikin.',
 
     ],
     'files' => [
-        'name' => 'Image',
+        'name' => 'Wêne',
     ],
     'seo_keyword' => [
         'name' => 'Seo Keyword',
-        'instructions' => 'Words added to the site in the Google browser to appear above the search results.',
+        'instructions' => 'Gotinên ku di geroka Google-ê de li malperê hatine zêdekirin da ku li jorê encamên lêgerînê xuya bibin.',
     ],
     'seo_description' => [
-        'name' => 'Seo Description',
-        'instructions' => 'It can affect your visitors\' decision on whether they want to click on the content in the search results.',
+        'name' => 'Seo Danasîn',
+        'instructions' => 'Ew dikare li ser biryara mêvanên we bandor bike ka ew dixwazin naverokê di encamên lêgerînê de bikirtînin.',
     ],
     'icon' => [
         'name' => 'Icon',
-        'instructions' => 'It is used to add icons indicating the category type.',
+        'instructions' => 'Ew tê bikaranîn ku îkonên ku celebê kategoriyê nîşan dikin zêde bike.',
     ],
 
-    'please_wait' => 'Please wait.Deleting Sub Categories',
-    'category_selection' => 'Category Selection',
-    'go_to_parent' => 'Go To Parent',
+    'please_wait' => 'Ji kerema xwe li bendê bimînin. Jêbirina Kategoriyên Bin',
+    'category_selection' => 'Hilbijartina Kategorî',
+    'go_to_parent' => 'Herin cem dêûbavan',
 ];
