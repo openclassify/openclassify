49030720ba6460243b16c8af1b593dd85649f00b
Fatih Alp <mail@fatihalp.com>
2020-12-08 12:46:26 +0300

    New translations help.php (Kurdish)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/ku/help.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ku/help.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'title'                     => 'Helpful Resources',
-    'description'               => 'Find help and discover features with these helpful resources.',
+    'title'                     => 'Çavkaniyên Alîkar',
+    'description'               => 'Bi van çavkaniyên alîkar alîkariyê bibînin û taybetmendiyan bibînin.',
     'documentation_link'        => 'OpenClassify Documentation',
-    'documentation_description' => 'Find documentation for OpenClassify, developing addons, and using addons too.',
-    'slack_link'                => 'Slack Team',
-    'slack_description'         => 'Find and communicate with other OpenClassify users and developers.',
-    'forum_link'                => 'Discussion Forum',
-    'forum_description'         => 'Find answers and post questions about using and developing with OpenClassify.',
-    'addons_link'               => 'Available Addons',
-    'addons_description'        => 'Discover addons available for OpenClassify.',
+    'documentation_description' => 'Ji bo OpenClassify, pêşvexistina pêvekan, û pêvekan jî belgekirinê bibînin.',
+    'slack_link'                => 'Tîmê Slack',
+    'slack_description'         => 'Bikarhêner û pêşdebirên din ên OpenClassify bibînin û pê re têkilî daynin.',
+    'forum_link'                => 'Forum Gotûbêj',
+    'forum_description'         => 'Li ser karanîn û pêşvebirina OpenClassify bersivan bibînin û pirsan bişînin.',
+    'addons_link'               => 'Addonên Heye',
+    'addons_description'        => 'Addonên ku ji bo OpenClassify hene peyda bikin.',
 ];
