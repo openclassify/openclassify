9c774c63b4cc713c5ba3375b785491b754cd43ea
Fatih Alp <mail@fatihalp.com>
2020-12-08 13:40:18 +0300

    New translations field.php (Kurdish)

---
+++ addons/default/visiosoft/cats-module/resources/lang/ku/field.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Nav',
+        'instructions' => 'Ew navê kategoriya sereke ye ku hûn ê diyar bikin. Ew dihêle bikarhêner bi hêsanî hilbera ku lê digerin bibîne.',
+    ],
+    'order' => [
+        'name' => 'Emir',
+    ],
+
+    'slug' => [
+        'name' => 'Şeytanok',
+        'instructions' => 'Ji bo ku koma posta xwe bi yên din re bike alîkar her tagên rêxistinê diyar bikin.',
+
+    ],
+    'files' => [
+        'name' => 'Wêne',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Keyword',
+        'instructions' => 'Gotinên ku di geroka Google-ê de li malperê hatine zêdekirin da ku li jorê encamên lêgerînê xuya bibin.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Danasîn',
+        'instructions' => 'Ew dikare li ser biryara mêvanên we bandor bike ka ew dixwazin naverokê di encamên lêgerînê de bikirtînin.',
+    ],
+    'icon' => [
+        'name' => 'Icon',
+        'instructions' => 'Ew tê bikaranîn ku îkonên ku celebê kategoriyê nîşan dikin zêde bike.',
+    ],
+
+    'please_wait' => 'Ji kerema xwe li bendê bimînin. Jêbirina Kategoriyên Bin',
+    'category_selection' => 'Hilbijartina Kategorî',
+    'go_to_parent' => 'Herin cem dêûbavan',
+];
