56a782eee77b5a75775c5a580840ea80d664d1aa
Fatih Alp <mail@fatihalp.com>
2020-12-08 13:40:19 +0300

    New translations preference.php (Kurdish)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ku/preference.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Sidebar Hover',
+        'instructions' => 'Zivirandina kêleka kêlekê?',
+    ],
+    'navigation'    => [
+        'name'         => 'Rêserbirinî',
+        'instructions' => 'Hên xwe <em>şexsî</em> fermana navîgasyon.',
+        'warning'      => 'Yekem tişta navîgasyonê ya gihînbar wekî qada <strong>xaniyê we</strong> tê bikar anîn.',
+        'reorder'      => 'Tiştên navîgasyonên sereke di <strong>tenişta</strong> bikişînin û bavêjin da ku wan ji nû ve rêz bikin.',
+    ],
+    'display'       => [
+        'name'         => 'Density Display',
+        'instructions' => 'Dîmendera kompakt dihêle ku naverok bêtir bi carekê li ser dîmenderê were xuyang kirin.',
+        'option'       => [
+            'default' => 'Destçûnî',
+            'compact' => 'Gişt',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => 'Modeya Sidebar',
+        'instructions' => 'Dê teniştên statik her dem xuya bibin.',
+        'option'       => [
+            'default' => 'Destçûnî',
+            'static'  => 'Statik',
+        ],
+    ],
+];
