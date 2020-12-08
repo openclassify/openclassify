18821518965208161dd94c11397ab56496e3ee56
Diatrex <diahshalabi@hotmail.com>
2020-12-08 10:05:14 +0300

    #2831 emlak24 requirements

--- addons/default/visiosoft/advs-module/resources/lang/tr/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/message.php
@@ -27,4 +27,6 @@ return [
     'ad_doesnt_exist' => "Bu İlan mevcut değil!",
     'select_location_error' => "Lütfen haritada bir yer seçin!",
     'this_ad_is_not_valid_anymore' => "Bu ürün veya ilan artık geçerli değil!",
+    'approve_status_change' => "İlanınızın Durumu Etkin Olarak Ayarlandı!",
+    'passive_status_change' => "İlanınızın Durumu Pasif Olarak Ayarlandı!",
 ];
