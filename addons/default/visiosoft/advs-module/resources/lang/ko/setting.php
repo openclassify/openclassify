f6d3f330893b9c566569bcda802120a5709d860e
Fatih Alp <mail@fatihalp.com>
2020-12-08 12:50:43 +0300

    New translations setting.php (Korean)

--- addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
@@ -206,7 +206,7 @@ return [
         'instructions' => '광고 작성 페이지에 개인 정보 보호 정책을 표시하려면이 옵션을 켜십시오.',
     ],
     'override_text' => [
-        'name' => 'Override Text',
-        'instructions' => 'Old Value:New Value'
+        'name' => '텍스트 무시',
+        'instructions' => '기존 가치 : 새로운 가치'
     ],
 ];
