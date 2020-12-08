eb3fd4a55a8cf828e95212e4bffc8d068e0812a1
Fatih Alp <mail@fatihalp.com>
2020-12-08 12:50:15 +0300

    New translations field.php (Japanese)

--- addons/default/visiosoft/profile-module/resources/lang/ja/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ja/field.php
@@ -375,6 +375,9 @@ return [
     'password' => [
         'name' => 'パスワード',
     ],
+    'old_password' => [
+        'name' => 'Old password',
+    ],
     'new_password' => [
         'name' => '新しいパスワード',
     ],
