5a566512581bf5821475bb78a1314cb36bb6ecfb
Fatih Alp <mail@fatihalp.com>
2020-12-08 12:40:15 +0300

    New translations field.php (French)

--- addons/default/visiosoft/profile-module/resources/lang/fr/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/fr/field.php
@@ -377,6 +377,9 @@ return [
     'password' => [
         'name' => 'Mot de passe',
     ],
+    'old_password' => [
+        'name' => 'Old password',
+    ],
     'new_password' => [
         'name' => 'Nouveau mot de passe',
     ],
