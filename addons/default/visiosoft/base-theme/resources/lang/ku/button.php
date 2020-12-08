fac1be900241054e23312c38d47de69c1f351cdc
Fatih Alp <mail@fatihalp.com>
2020-12-08 13:39:38 +0300

    New translations button.php (Kurdish)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ku/button.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'post_ad' => [
+        'name' => 'Post Ad',
+    ],
+    'login' => 'Têkevin',
+    'register' => 'Fêhrist',
+    'continue' => 'Berdewamkirin',
+    'reset_password' => 'Reset Passwordîfreyê',
+    'email' => 'E-nameyê bişînin',
+    'phone' => 'Telefon',
+    'show_all' => 'Hemî nîşan bide',
+];
