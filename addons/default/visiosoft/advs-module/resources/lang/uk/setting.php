7d9b65fbbce495245452c0f32b5f7d241fae8bed
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:02 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Ринок',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'Сховати фільтр цін',
     ],
