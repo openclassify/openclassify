4b092cf580c7ac8a7c4138b4ef3aecfc87b66d2c
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:25 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/advs-module/resources/lang/hi/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/hi/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'मार्केट प्लेस',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'मूल्य फ़िल्टर छिपाएँ',
     ],
