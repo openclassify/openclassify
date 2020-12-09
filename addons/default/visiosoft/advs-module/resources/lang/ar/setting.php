daa491ce8e28ce28d8387c8b0904d61cb685c48e
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:41 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'المتجر',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'إخفاء عامل تصفية السعر',
     ],
