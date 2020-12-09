5a1b95fd3bd39b2e1bd4425519e25f25dc79fec9
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:41:58 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Pazar Yeri',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'Fiyat Filtresini Gizle',
     ],
