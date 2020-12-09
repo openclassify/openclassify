4689b1c118048d8cbb0da275fc2156ada6cf88d3
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:51 +0300

    New translations field.php (Greek)

--- addons/default/visiosoft/profile-module/resources/lang/el/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/el/field.php
@@ -58,6 +58,12 @@ return [
     'birthday' => [
         'name' => 'Γενέθλια'
     ],
+	'facebook_address' => [
+		'name' => 'Facebook Address'
+	],
+	'google_address' => [
+		'name' => 'Google Address'
+	],
     'education' => [
         'name' => 'Εκπαίδευση'
     ],
