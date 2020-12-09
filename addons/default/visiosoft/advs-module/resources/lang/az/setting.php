e825787ec65bad452d123b22dc67b6cda97b94bd
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:21 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Bazar yeri',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'Qiymət filtrini gizlət',
     ],
