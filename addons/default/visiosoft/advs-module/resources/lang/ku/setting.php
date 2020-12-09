8ff3e54518d42807d6929918fd62c92836a52a2a
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:43:07 +0300

    New translations setting.php (Kurdish)

--- addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Cihê Sûkê',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'Parzûna Bihayê Veşêre',
     ],
