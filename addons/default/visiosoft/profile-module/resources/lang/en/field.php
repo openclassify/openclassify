9db731dc6cdef4df4c5ae15afc1b3c6d0467bb1e
Muammer Top <muammer@visiosoft.com.tr>
2020-12-09 12:47:15 +0300

    #2819 minor fix (google & facebook address bonus)

--- addons/default/visiosoft/profile-module/resources/lang/en/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/en/field.php
@@ -58,6 +58,12 @@ return [
     'birthday' => [
         'name' => 'Birthday'
     ],
+	'facebook_address' => [
+		'name' => 'Facebook Address'
+	],
+	'google_address' => [
+		'name' => 'Google Address'
+	],
     'education' => [
         'name' => 'Education'
     ],
