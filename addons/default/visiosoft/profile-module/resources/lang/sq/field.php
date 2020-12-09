316fd111812b1ac13643216a6229a03588d0fde8
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:41:52 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/profile-module/resources/lang/sq/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/sq/field.php
@@ -58,6 +58,12 @@ return [
     'birthday' => [
         'name' => 'Ditëlindja'
     ],
+	'facebook_address' => [
+		'name' => 'Facebook Address'
+	],
+	'google_address' => [
+		'name' => 'Google Address'
+	],
     'education' => [
         'name' => 'Arsimi'
     ],
