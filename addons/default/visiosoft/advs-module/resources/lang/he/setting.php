528173e4b85b6a045ec1f44f8eef4725e73bc843
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:32 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'זירת מסחר',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'הסתר מסנן מחירים',
     ],
