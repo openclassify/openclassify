96ff26551b81b02cfc4e83896bfcf742952f5f5f
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:10 +0300

    New translations setting.php (Vietnamese)

--- addons/default/visiosoft/advs-module/resources/lang/vi/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/vi/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Thương trường',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'Ẩn bộ lọc giá',
     ],
