e4f504d8f7fe97a66d06331812db823eb418e651
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:05 +0300

    New translations setting.php (Urdu (Pakistan))

--- addons/default/visiosoft/advs-module/resources/lang/ur/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ur/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'مارکیٹ پلیس',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'پرائس فلٹر چھپائیں',
     ],
