e1105626136bcb8ef2307cd65eb9936dd5bf91e0
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:56 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Piazza del mercato',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'Nascondi filtro prezzo',
     ],
