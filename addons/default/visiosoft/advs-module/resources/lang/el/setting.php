c8a3437518f1bebe9083f8b2a1499c7e797558e9
Fatih Alp <mail@fatihalp.com>
2020-12-09 16:42:49 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Αγορά',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Price Area Hidden',
+	],
     'hide_price_filter' => [
         'name' => 'Απόκρυψη φίλτρου τιμών',
     ],
