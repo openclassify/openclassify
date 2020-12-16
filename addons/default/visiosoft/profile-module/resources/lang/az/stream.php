501e308c1f28e1b1787302dea352812f675a78f2
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:53:34 +0300

    New translations stream.php (Azerbaijani)

--- addons/default/visiosoft/profile-module/resources/lang/az/stream.php
+++ addons/default/visiosoft/profile-module/resources/lang/az/stream.php
@@ -7,4 +7,10 @@ return [
     'adress' => [
         'name' => 'Ünvan',
     ],
+    'education' => [
+        'name' => 'Education',
+    ],
+    'education_part' => [
+        'name' => 'Education part',
+    ],
 ];
