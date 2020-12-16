5070b1a1140985541e43edb3386cd5561ab6e943
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:50:51 +0300

    New translations section.php (Albanian)

--- addons/default/visiosoft/profile-module/resources/lang/sq/section.php
+++ addons/default/visiosoft/profile-module/resources/lang/sq/section.php
@@ -9,4 +9,10 @@ return [
     ],
     'general_setting' => 'Cilësimet e përgjithshme',
     'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Education',
+    ],
+    'education_part' => [
+        'title' => 'Education part',
+    ],
 ];
