50ad0a4718869a3460117ef887a579072fefada3
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:53:57 +0300

    New translations button.php (Turkish)

--- addons/default/visiosoft/profile-module/resources/lang/tr/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/tr/button.php
@@ -15,4 +15,6 @@ return [
     'personal' => 'Kişisel',
     'corporate' => 'Kurumsal',
     'export' => 'Dışa aktar',
+    'new_education' => 'New Education',
+    'new_education_part' => 'New Education part',
 ];
