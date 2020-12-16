82fe05da1686a7bd502c8d0bd46e5f8b791e13b2
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:53:17 +0300

    New translations field.php (Bengali)

--- addons/default/visiosoft/profile-module/resources/lang/bn/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/bn/field.php
@@ -58,6 +58,12 @@ return [
     'birthday' => [
         'name' => 'জন্মদিন'
     ],
+	'facebook_address' => [
+		'name' => 'Facebook Address'
+	],
+	'google_address' => [
+		'name' => 'Google Address'
+	],
     'education' => [
         'name' => 'শিক্ষা'
     ],
@@ -115,6 +121,9 @@ return [
     'user' => [
         'name' => 'ব্যবহারকারী'
     ],
+	'name' => [
+		'name' => 'Name'
+	],
 
     /*Menu Button*/
     'profile' => [
@@ -375,6 +384,9 @@ return [
     'password' => [
         'name' => 'পাসওয়ার্ড',
     ],
+    'old_password' => [
+        'name' => 'পুরানো পাসওয়ার্ড',
+    ],
     'new_password' => [
         'name' => 'নতুন পাসওয়ার্ড',
     ],
@@ -467,4 +479,8 @@ return [
     'personal' => [
         'name' => 'ব্যক্তিগত'
     ],
+
+	'education_part' => [
+		'name' => 'State of Education'
+	],
 ];
