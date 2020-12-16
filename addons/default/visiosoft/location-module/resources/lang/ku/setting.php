ca0698438089eba5314b83fefebff13b1eacb365
Fatih Alp <mail@fatihalp.com>
2020-12-16 14:04:17 +0300

    New translations setting.php (Kurdish)

---
+++ addons/default/visiosoft/location-module/resources/lang/ku/setting.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    'home_page_location' => [
+        'name' => 'Zeviyê Cihê Rûpela Rûpelê',
+    ],
+    'list_page_location' => [
+        'name' => 'Qada Rûpelê Rûpela Rûpelê',
+    ],
+    'detail_page_location' => [
+        'name' => 'Zeviya Cihê Rûpelê Detail',
+    ],
+    'show_search_location_btn' => [
+        'name' => 'Bişkoja Cihê Lêgerînê nîşan bide',
+    ],
+    'default_country' => [
+        'name' => 'Welatê Default',
+    ],
+    'create_ad_page_location' => [
+        'name' => 'Cihê Rûpela Rûpelê biafirînin',
+    ],
+    'default_city' => [
+        'name' => 'Default City',
+    ],
+    'default_district' => [
+        'name' => 'Navçeya Default',
+    ],
+    'default_neighborhood' => [
+        'name' => 'Taxa Default',
+    ],
+
+    'google_map_key' => [
+        'name' => 'Google Maps Api Key',
+    ],
+
+    'map_coordinates_long' => [
+        'name' => 'Default Long',
+    ],
+
+    'map_coordinates_lat' => [
+        'name' => 'Default Lat',
+    ],
+
+    'hide_location_filter' => [
+        'name' => 'Parzûna Cihê Veşêre',
+    ],
+	'country_for_phone_field' => [
+		'name' => 'Welatê Default Ji Bo Tomarbûna Qada Telefonê'
+	],
+    'sorting_column' => [
+        'name' => 'Stûnê Rêzkirin',
+    ],
+    'sorting_type' => [
+        'name' => 'Cureyê Rêzkirinê',
+    ],
+];
