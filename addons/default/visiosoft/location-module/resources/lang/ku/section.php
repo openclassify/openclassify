5d5b8ab528223817d325fd73bb36528effb7c828
Fatih Alp <mail@fatihalp.com>
2020-12-16 14:04:19 +0300

    New translations section.php (Kurdish)

---
+++ addons/default/visiosoft/location-module/resources/lang/ku/section.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'countries' => [
+        'title' => 'Welat',
+    ],
+    'cities' => [
+        'title' => 'Bajar',
+    ],
+    'districts' => [
+        'title' => 'Navçe',
+    ],
+    'neighborhoods' => [
+        'title' => 'Taxan',
+    ],
+    'village' => [
+        'title' => 'Gûnd',
+    ],
+    'general' => 'Giştî',
+    'map' => 'Qert',
+    'setting' => 'Sazkirin',
+    'filter' => 'Parzûn',
+];
