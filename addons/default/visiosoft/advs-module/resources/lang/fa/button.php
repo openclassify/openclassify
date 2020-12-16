efd2b6dcc07f63e31cc6a2e6bf783782d4dd7d74
Fatih Alp <mail@fatihalp.com>
2020-12-16 14:17:22 +0300

    New translations button.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/button.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/button.php
@@ -2,6 +2,7 @@
 
 return [
     'new_adv' => 'جدید جدید',
+    'new_classified' => 'طبقه بندی جدید',
     'new_category' => 'دسته بندی جدید',
     'new_country' => 'کشور جدید',
     'new_city' => 'شهر جدید',
@@ -39,4 +40,7 @@ return [
     'new_option_configuration' => 'پیکربندی گزینه جدید',
 	'create_configurations' => 'تنظیمات را ایجاد کنید',
 	'replicate' => 'تکثیر',
+	'fast_create' => 'سریع ایجاد کنید',
+	'publish' => 'انتشار',
+	'import' => 'وارد كردن',
 ];
