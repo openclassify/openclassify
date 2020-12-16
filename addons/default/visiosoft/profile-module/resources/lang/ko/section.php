0c105b3fca5a7c3a2f6019202c1e972884b5894b
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:50:20 +0300

    New translations section.php (Korean)

--- addons/default/visiosoft/profile-module/resources/lang/ko/section.php
+++ addons/default/visiosoft/profile-module/resources/lang/ko/section.php
@@ -9,4 +9,10 @@ return [
     ],
     'general_setting' => '일반 설정',
     'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Education',
+    ],
+    'education_part' => [
+        'title' => 'Education part',
+    ],
 ];
