4de77f552ec07b31c07c73019dc8e40ac6ca88a8
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:54:22 +0300

    New translations permission.php (Urdu (Pakistan))

--- addons/default/visiosoft/profile-module/resources/lang/ur/permission.php
+++ addons/default/visiosoft/profile-module/resources/lang/ur/permission.php
@@ -17,4 +17,20 @@ return [
             'delete' => 'ایڈریس کو حذف کرسکتے ہیں؟',
         ],
     ],
+    'education' => [
+        'name'   => 'Education',
+        'option' => [
+            'read'   => 'Can read education?',
+            'write'  => 'Can create/edit education?',
+            'delete' => 'Can delete education?',
+        ],
+    ],
+    'education_part' => [
+        'name'   => 'Education part',
+        'option' => [
+            'read'   => 'Can read education part?',
+            'write'  => 'Can create/edit education part?',
+            'delete' => 'Can delete education part?',
+        ],
+    ],
 ];
