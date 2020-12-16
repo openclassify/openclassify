51167258e7b1d0e2932010f436c84f50c63729ee
Fatih Alp <mail@fatihalp.com>
2020-12-16 14:10:56 +0300

    New translations button.php (Turkish)

--- addons/default/visiosoft/location-module/resources/lang/tr/button.php
+++ addons/default/visiosoft/location-module/resources/lang/tr/button.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'new_country' => 'Yeni ülke',
+    'new_country' => 'Yeni Ülke',
     'new_city' => 'Yeni İl',
     'new_district' => 'Yeni İlçe',
     'new_neighborhood' => 'Yeni mahalle',
-    'new_village' => 'Yeni köy',
+    'new_village' => 'Yeni Köy',
     'sub_cities' => 'Alt Şehirler',
     'add_sub_cities' => 'Şehir Ekle',
     'add_sub_districts' => 'İlçe Ekle',
