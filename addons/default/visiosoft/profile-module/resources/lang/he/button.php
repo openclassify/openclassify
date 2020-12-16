326dd2c531599329e3330ac63225dd5b7ad9be50
Fatih Alp <mail@fatihalp.com>
2020-12-16 13:47:26 +0300

    New translations button.php (Hebrew)

--- addons/default/visiosoft/profile-module/resources/lang/he/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/he/button.php
@@ -15,6 +15,6 @@ return [
     'personal' => 'אישי',
     'corporate' => 'תאגידי',
     'export' => 'יְצוּא',
-    'new_education' => 'New Education',
-    'new_education_part' => 'New Education part',
+    'new_education' => 'חינוך חדש',
+    'new_education_part' => 'חלק חינוך חדש',
 ];
