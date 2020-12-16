29c49f466be91783599cce31081b077b80f36e95
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:53:18 +0300

    New translations permission.php (Bengali)

--- addons/default/visiosoft/profile-module/resources/lang/bn/permission.php
+++ addons/default/visiosoft/profile-module/resources/lang/bn/permission.php
@@ -17,4 +17,20 @@ return [
             'delete' => 'ঠিকানা মুছতে পারেন?',
         ],
     ],
+    'education' => [
+        'name'   => 'Education',
+        'option' => [
+            'read'   => 'Can read education?',
+            'write'  => 'Can create/edit education?',
+            'delete' => 'Can delete education?',
+        ],
+    ],
+    'education_part' => [
+        'name'   => 'Education part',
+        'option' => [
+            'read'   => 'Can read education part?',
+            'write'  => 'Can create/edit education part?',
+            'delete' => 'Can delete education part?',
+        ],
+    ],
 ];
