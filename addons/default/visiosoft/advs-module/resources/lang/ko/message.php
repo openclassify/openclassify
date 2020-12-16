96cf11ad4fb5e6e3d8ffc911898086eec73da202
Fatih Alp <mail@fatihalp.com>
2020-12-16 13:50:07 +0300

    New translations message.php (Korean)

--- addons/default/visiosoft/advs-module/resources/lang/ko/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ko/message.php
@@ -27,4 +27,6 @@ return [
     'ad_doesnt_exist' => "이 광고는 존재하지 않습니다!",
     'select_location_error' => "지도에서 위치를 선택하세요!",
     'this_ad_is_not_valid_anymore' => "이 광고는 더 이상 유효하지 않습니다!",
+    'approve_status_change' => "귀하의 광고 상태가 활성으로 설정되었습니다!",
+    'passive_status_change' => "귀하의 광고 상태가 수동으로 설정되었습니다!",
 ];
