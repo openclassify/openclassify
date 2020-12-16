184c1329fb148647c02f0e060853cfc9f600cca4
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:54:24 +0300

    New translations section.php (Urdu (Pakistan))

--- addons/default/visiosoft/profile-module/resources/lang/ur/section.php
+++ addons/default/visiosoft/profile-module/resources/lang/ur/section.php
@@ -9,4 +9,10 @@ return [
     ],
     'general_setting' => 'عام ترتیبات',
     'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Education',
+    ],
+    'education_part' => [
+        'title' => 'Education part',
+    ],
 ];
