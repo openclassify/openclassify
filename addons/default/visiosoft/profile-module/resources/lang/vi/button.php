0627cba33c8a90e90bac65473c4920a5619d643f
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:54:30 +0300

    New translations button.php (Vietnamese)

--- addons/default/visiosoft/profile-module/resources/lang/vi/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/vi/button.php
@@ -15,4 +15,6 @@ return [
     'personal' => 'Cá nhân',
     'corporate' => 'Công ty',
     'export' => 'Xuất khẩu',
+    'new_education' => 'New Education',
+    'new_education_part' => 'New Education part',
 ];
