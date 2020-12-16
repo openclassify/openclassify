3bdeb3be5bbfc02e309704ce741906ec81187a2a
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:50:22 +0300

    New translations permission.php (Korean)

--- addons/default/visiosoft/profile-module/resources/lang/ko/permission.php
+++ addons/default/visiosoft/profile-module/resources/lang/ko/permission.php
@@ -17,4 +17,20 @@ return [
             'delete' => '주소를 삭제할 수 있습니까?',
         ],
     ],
+    'education' => [
+        'name'   => 'Education',
+        'option' => [
+            'read'   => 'Can read education?',
+            'write'  => 'Can create/edit education?',
+            'delete' => 'Can delete education?',
+        ],
+    ],
+    'education_part' => [
+        'name'   => 'Education part',
+        'option' => [
+            'read'   => 'Can read education part?',
+            'write'  => 'Can create/edit education part?',
+            'delete' => 'Can delete education part?',
+        ],
+    ],
 ];
