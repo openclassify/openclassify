ca03929e91e0c7b948871ffdfd452f097dcc2234
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:54:36 +0300

    New translations stream.php (Hindi)

--- addons/default/visiosoft/profile-module/resources/lang/hi/stream.php
+++ addons/default/visiosoft/profile-module/resources/lang/hi/stream.php
@@ -7,4 +7,10 @@ return [
     'adress' => [
         'name' => 'पता',
     ],
+    'education' => [
+        'name' => 'Education',
+    ],
+    'education_part' => [
+        'name' => 'Education part',
+    ],
 ];
