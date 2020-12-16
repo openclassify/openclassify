0303e5278e8b89f2510dd41e8cb0ac7fd826c37a
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:50:03 +0300

    New translations button.php (Dutch)

--- addons/default/visiosoft/profile-module/resources/lang/nl/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/nl/button.php
@@ -15,4 +15,6 @@ return [
     'personal' => 'Persoonlijk',
     'corporate' => 'Zakelijk',
     'export' => 'Exporteren',
+    'new_education' => 'New Education',
+    'new_education_part' => 'New Education part',
 ];
