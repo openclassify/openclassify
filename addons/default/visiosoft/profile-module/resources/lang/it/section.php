6574865283ba3ff6d3a3501b13d630b75bb024b4
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:52:26 +0300

    New translations section.php (Italian)

--- addons/default/visiosoft/profile-module/resources/lang/it/section.php
+++ addons/default/visiosoft/profile-module/resources/lang/it/section.php
@@ -9,4 +9,10 @@ return [
     ],
     'general_setting' => 'impostazioni generali',
     'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Education',
+    ],
+    'education_part' => [
+        'title' => 'Education part',
+    ],
 ];
