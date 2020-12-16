c85db7af531dab505bbdc904e43ecf8d8069957a
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:53:43 +0300

    New translations section.php (Hindi)

--- addons/default/visiosoft/profile-module/resources/lang/hi/section.php
+++ addons/default/visiosoft/profile-module/resources/lang/hi/section.php
@@ -9,4 +9,10 @@ return [
     ],
     'general_setting' => 'सामान्य सेटिंग्स',
     'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Education',
+    ],
+    'education_part' => [
+        'title' => 'Education part',
+    ],
 ];
