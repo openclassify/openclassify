80a313f5a00873d995e28f8522e40190727cd31c
Fatih Alp <mail@fatihalp.com>
2020-12-16 12:54:17 +0300

    New translations stream.php (Ukrainian)

--- addons/default/visiosoft/profile-module/resources/lang/uk/stream.php
+++ addons/default/visiosoft/profile-module/resources/lang/uk/stream.php
@@ -7,4 +7,10 @@ return [
     'adress' => [
         'name' => 'Адреса',
     ],
+    'education' => [
+        'name' => 'Education',
+    ],
+    'education_part' => [
+        'name' => 'Education part',
+    ],
 ];
