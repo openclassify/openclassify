7982a528ea313049c6a811735b23685f33f38906
Fatih Alp <mail@fatihalp.com>
2020-12-16 14:10:03 +0300

    New translations message.php (Swedish)

--- addons/default/visiosoft/profile-module/resources/lang/sv/message.php
+++ addons/default/visiosoft/profile-module/resources/lang/sv/message.php
@@ -29,6 +29,7 @@ return [
     'empty_password_sms_message' => 'På grund av säkerhetsproblem har vi ändrat ditt lösenord! Ditt nya lösenord är:',
     'required_all' => "Allt fält krävs!",
     'can_not_remove_filled_fields' => "Du kan inte ta bort fyllda fält!",
+    'wrong_password' => "Lösenordet du angav är inte korrekt!",
 
     // Forgot Password
     'email_phone_not_found' => 'E-post, telefonnummer är inte korrekt!',
