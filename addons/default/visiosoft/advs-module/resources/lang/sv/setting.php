7c282927094efef46dd491be1cc31271277945da
Fatih Alp <mail@fatihalp.com>
2020-12-18 16:59:21 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
@@ -126,7 +126,9 @@ return [
     'market_place' => [
         'name' => 'Market Place',
     ],
-
+	'price_area_hidden' => [
+		'name' => 'Prisområde dolt',
+	],
     'hide_price_filter' => [
         'name' => 'Dölj prisfilter',
     ],
@@ -196,5 +198,25 @@ return [
     ],
     'show_breadcrumb_when_creating_ad' => [
         'name' => 'Visa brödsmulan när du skapar en annons',
+    ],
+    'show_ads_count' => [
+        'name' => 'Visa antal annonser',
+        'instructions' => 'Visa annonser räknas på listan till vänster',
+    ],
+    'show_post_ad_agreement' => [
+        'name' => 'Visa annonsannonsavtal',
+        'instructions' => 'Aktivera detta om du vill visa ett integritetspolicyavtal på sidan för skapande av annonser',
+    ],
+    'override_text' => [
+        'name' => 'Åsidosätt text',
+        'instructions' => 'Old Value: New Value'
+    ],
+    'steps_color' => [
+        'name' => 'Steps Color',
+        'instructions' => 'Changes the progressive steps color.'
+    ],
+    'create_ad_button_color' => [
+        'name' => 'Create Ad Button Color',
+        'instructions' => 'Changes the button that shows up when the new ad category selection is finished.'
     ],
 ];
