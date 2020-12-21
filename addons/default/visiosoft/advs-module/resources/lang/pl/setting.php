a273d0ed5b26d9d4c79c58405fa52e2dba4156a4
Fatih Alp <mail@fatihalp.com>
2020-12-21 13:43:42 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/advs-module/resources/lang/pl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/pl/setting.php
@@ -212,11 +212,11 @@ return [
         'instructions' => 'Stara wartość: nowa wartość'
     ],
     'steps_color' => [
-        'name' => 'Steps Color',
-        'instructions' => 'Changes the progressive steps color.'
+        'name' => 'Kolor kroków',
+        'instructions' => 'Zmienia kolor stopniowych kroków.'
     ],
     'create_ad_button_color' => [
-        'name' => 'Create Ad Button Color',
-        'instructions' => 'Changes the button that shows up when the new ad category selection is finished.'
+        'name' => 'Utwórz kolor przycisku reklamy',
+        'instructions' => 'Zmienia przycisk wyświetlany po zakończeniu wyboru nowej kategorii reklam.'
     ],
 ];
