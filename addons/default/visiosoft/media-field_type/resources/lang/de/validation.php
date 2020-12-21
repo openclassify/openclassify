d0815d8a84ad3d97382fb9db5b3f22d9ca804069
Fatih Alp <mail@fatihalp.com>
2020-12-21 16:05:49 +0300

    New translations validation.php (German)

--- addons/default/visiosoft/media-field_type/resources/lang/de/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/de/validation.php
@@ -2,4 +2,5 @@
 
 return [
     'valid_disk' => 'Die gewählte Upload-Disk für :attribute existiert nicht.',
+	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
 ];
