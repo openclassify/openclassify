31635ab0136b159d921fd61cb87fe00c79cb55a7
Fatih Alp <mail@fatihalp.com>
2020-12-21 16:05:51 +0300

    New translations validation.php (Arabic)

--- addons/default/visiosoft/media-field_type/resources/lang/ar/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/ar/validation.php
@@ -2,4 +2,5 @@
 
 return [
     'valid_disk' => 'القرص المخصص لـ :attribute غير موجود.',
+	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
 ];
