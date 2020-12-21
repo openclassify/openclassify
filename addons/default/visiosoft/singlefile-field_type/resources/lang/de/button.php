9c48e74e6c229761d4d213631d98be17e1f2a4d7
Fatih Alp <mail@fatihalp.com>
2020-12-21 13:25:49 +0300

    New translations button.php (German)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/de/button.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/de/button.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'select_file' => 'Datei auswählen',
-    'upload'      => 'Hochladen',
-    'remove'      => 'Entfernen',
+    'select_file' => 'Datei auswählenException: no protocol: Head - Url: http://Exception: unknown protocol: exception Index 015164663380.
+Tracklist aimcart',
+    'upload'      => 'Hochladen id Lockscreen ans lockaion',
+    'remove'      => 'Entferne Blockierungen',
     'change'      => 'Ändern',
 ];
