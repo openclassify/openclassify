d6eebf66f4df6318f867c41c4e32e81bb02e3896
Fatih Alp <mail@fatihalp.com>
2020-12-21 13:36:33 +0300

    New translations field.php (Kurdish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/ku/field.php
@@ -0,0 +1,344 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Nav'
+    ],
+    'order' => [
+        'name' => 'Emir'
+    ],
+    'slug' => [
+        'name' => 'Şeytanok'
+    ],
+    'parent_category' => [
+        'name' => 'Dêûbav'
+    ],
+    'category' => [
+        'name' => 'Title Kategorî'
+    ],
+    'price' => [
+        'name' => 'Biha'
+    ],
+    'standard_price' => [
+        'name' => 'Bihayê Standard'
+    ],
+    'date' => [
+        'name' => 'Rojek'
+    ],
+    'photo' => [
+        'name' => 'Wêne'
+    ],
+    'video' => [
+        'name' => 'Vîdyo'
+    ],
+    'map' => [
+        'name' => 'Qert'
+    ],
+    'stock' => [
+        'name' => 'Embar'
+    ],
+    'advs_desc' => [
+        'name' => 'Terîf'
+    ],
+    'online_payment' => [
+        'name' => 'Tezmînata Serhêl'
+    ],
+    'city' => [
+        'name' => 'Bajar'
+    ],
+    'country' => [
+        'name' => 'Welat'
+    ],
+    'district' => [
+        'name' => 'Herêm'
+    ],
+    'neighborhood' => [
+        'name' => 'Cînarî'
+    ],
+    'village' => [
+        'name' => 'Gûnd'
+    ],
+    'files' => [
+        'name' => 'Wêne'
+    ],
+    'cat1' => [
+        'name' => 'Kategorî Sereke'
+    ],
+    'cat2' => [
+        'name' => 'Kategoriya Jêrîn 1'
+    ],
+    'cat3' => [
+        'name' => 'Kategoriya Jêrîn 2'
+    ],
+    'cat4' => [
+        'name' => 'Kategoriya Jêrîn 3'
+    ],
+    'cat5' => [
+        'name' => 'Kategoriya Jêrîn 4'
+    ],
+    'cat6' => [
+        'name' => 'Kategoriya Jêrîn 5'
+    ],
+    'cat7' => [
+        'name' => 'Bine Kategorî 6'
+    ],
+    'cat8' => [
+        'name' => 'Kategoriya Jêrîn 7'
+    ],
+    'cat9' => [
+        'name' => 'Kategoriya Jêrîn 8'
+    ],
+    'cat10' => [
+        'name' => 'Kategoriya Jêrîn 9'
+    ],
+    'currency' => [
+        'name' => 'Diravcins'
+    ],
+    'status' => [
+        'name' => 'Cî',
+        'option' => [
+            'approved' => 'Pejirandin',
+            'expired' => 'Xilas bû',
+            'unpublished' => 'Neweşandî',
+            'pending_admin' => 'Li bendê Admin',
+            'pending_user' => 'Bikarhênera bendewar'
+        ]
+    ],
+    'pending_adv' => [
+        'name' => 'Neweşandî'
+    ],
+    'finish_at' => [
+        'name' => 'Dawî Li'
+    ],
+    'cover_photo' => [
+        'name' => 'Cover Photo'
+    ],
+    'favs_adv' => [
+        'name' => 'Fav Ads'
+    ],
+    'my_adv' => [
+        'name' => 'ADVS'
+    ],
+    'description' =>  'Terîf',
+    'explanation' =>  'Daxûyanî',
+    'type' => [
+        'name' => 'Awa'
+    ],
+    'parent_adv' => [
+        'name' => 'Girêdayî Ad'
+    ],
+    'value' => [
+        'name' => 'Giranî'
+    ],
+    'list' => 'Rêzok',
+    'home' => 'Xane',
+    'list-page' => 'Rûpela Rûpelê',
+    'search' => 'Gerr',
+    'recommended' => 'Ji bo We Reklamên Pêşniyar kirin',
+    'filter' => 'Parzûn',
+    'sort' => 'Jiberhevxistin',
+    'sort_by' => 'Sort By',
+    'pick_option' => 'Vebijarek hilbijêrin',
+    'pick_ordering' => 'Rêzkirin',
+    'price_high' => 'Bihayê Bilind heya Nizm',
+    'price_low' => 'Bihayê Kêm heya Bilind',
+    'newest' => 'Nûtirîn',
+    'oldest' => 'Kevintirîn',
+    'address_a_z' => 'Navnîşan (A-Z)',
+    'address_z_a' => 'Navnîşan (ji Z-A)',
+	'categories' => [
+		'name' => 'Kategorî'
+	],
+    'all_categories' => 'Hemî Kategorî',
+    'location' => 'Cîh',
+    'send_message' => 'Peyam bişînin',
+    'phone_gsm' => 'Telefonê Gsm',
+    'phone_office' => 'Telefonê Ofîsê',
+    'add_to_favorites' => "Add to Favorites",
+    'send' => 'Şandin',
+    'search_name' => 'Navê Lêgerînê',
+    'my_favorite_searches' => 'Lêgerînên Bijare',
+    'search_desc' => 'Hûn dikarin li vir vebijarkên lêgerîna xweya bijare zêde bikin û rêve bibin.',
+    'complaint_placeholder' => 'Agahdariyên giliyê xwe li vir binivîsin.',
+    'show_details' => 'Agahdariyên Nîşan bidin',
+    'ad_actions' => 'Çalakiyên Ad',
+    'edit' => 'Weşandin',
+    'approve' => 'Destûrdan',
+    'passive' => "Nejîr",
+    'offered_by' => 'Ji hêla Pêşniyar kirin',
+    'more_ads_by' => 'More Ads By',
+    'click_phone' => 'Hejmara têlefonê nîşan bide',
+    'share' => 'Par',
+    'report' => 'Vê reklamê ragihînin',
+    'features' => 'Taybetmendî',
+    "streetview" => "Dîmena Kolanê",
+    'recommended_ads' => 'Ji bo We Reklamên Pêşniyar kirin',
+    'new' => 'Nşh',
+    'short_info' => 'Kurte Agahdarî',
+    'secure_trading' => 'Bazirganiya Ewle',
+    '24/7' => '24/7 Piştgirî',
+    'easy_trading' => 'Bazirganiya Hêsan',
+    'need_help' => 'Alîkarî hewce dike?',
+    'give_a_call' => 'Gazî bikin',
+    'message_sent' => 'Peyam entandin!',
+    'asap' => 'Peyam şandî, dê firotkar asap bi we re têkilî dayne.',
+    'ad_id' => 'Ad Id',
+    'contact_with' => 'Têkilî Bi',
+    'create_adv' => 'Ad biafirînin',
+    'edit_adv' => 'Ad Adrast bike',
+    'mandatory_fields' => 'Zeviyên Mecbûrî',
+    'ad_post' => 'Ad Post',
+    'sell_item' => 'Tiştek an karûbarek bifroşin',
+    'search_save' => 'Lêgerîna bijare bi serkeftî hate tomarkirin',
+    'address' => 'Navnîşan',
+    'email' => 'E-name',
+    'phone' => 'Telefon',
+    'send_us_your_feedback' => 'Bersiva xwe ji me re bişînin',
+    'online_shopping' => 'STENDIN',
+    'approved' => 'Bi Serkeftin Hat Pejirandin',
+    'extended' => 'Bi Serkeftin Berfireh kirin',
+    'declined' => 'Bi Serkeftin Daketin',
+    'search_on_map' => 'Li Nexşeyê bigerin',
+    'post_free_ad' => 'Reklamek Belaş bişînin',
+    'select_a_category' => 'Kategoriyek hilbijêrin',
+    'select_a_sub_category' => 'Kategoriyek Jêrîn hilbijêrin',
+    'all_dopings_added' => 'Hemî celebên dopîngê ji bo vê reklamê têne zêdekirin.',
+    'is_get_adv' => [
+        'name' => 'STENDIN'
+    ],
+    'corporate_info' => 'Agahdariya Pargîdanî',
+    'featured_ads' => "Reklamên Taybetmendî",
+    'trending_ads' => 'Reklamên Trendind',
+    'search_ads_result_message' => [
+        'name' => ': reklamên hejmartin ji bo: lêgerîna navan hate dîtin',
+    ],
+    'total' => 'Hemî',
+    'quantity' => 'Jimarî',
+    'view_cart' => 'Cart View',
+    'no_ads' => 'Reklam nehat dîtin.',
+    'show_display_mode' => [
+        'map' => 'Qert',
+        'list' => 'Rêzok',
+    ],
+    'advs_list_table_thead' => [
+        'name' => 'Navê Ad',
+        'price' => 'Biha',
+        'publish_at' => 'Dîroka Ad',
+        'location_city' => 'Bajar',
+        'location_country' => 'Welat',
+    ],
+    'show_list_view' => [
+        'name' => 'Rêzok',
+    ],
+    'show_table_view' => [
+        'name' => 'Mêz',
+    ],
+    'showcase' => [
+        'name' => 'Pêşangeh',
+    ],
+    'new_ad' => [
+        'name' => 'Reklama Nû',
+    ],
+    'profile' => [
+        'name' => 'Tengal',
+    ],
+    'my_ads' => [
+        'name' => 'Reklamên min',
+    ],
+    'sold_out' => [
+        'name' => 'Hate firotin',
+    ],
+    'edit_category' => 'Kategorî biguherîne',
+    'set_category' => 'Kategorî danîn',
+    'max' => [
+        'name' => 'Max',
+    ],
+    'min' => [
+        'name' => 'Min',
+    ],
+    'in_the_last_24_hours' => [
+        'name' => 'Di 24 Demjimêrên Dawîn de'
+    ],
+    'in_the_last_day' => [
+        'name' => 'Di 24 Demjimêrên Dawîn de'
+    ],
+    'in_the_last_week' => [
+        'name' => 'Di Hefteya Dawîn de'
+    ],
+    'in_the_last_month' => [
+        'name' => 'Di Meha Dawîn de'
+    ],
+    'ads_with_photo' => [
+        'name' => 'Reklamên Bi Wêne'
+    ],
+    'ads_with_video' => [
+        'name' => 'Reklamên Bi Vîdyo'
+    ],
+    'yes' => [
+        'name' => 'Erê'
+    ],
+    "no_location" => "Cîh nayê hilbijartin.",
+    "continue" => 'Berdewamkirin',
+    "gallery" => 'Galerya',
+    "ads" => 'Reklam',
+    "were_found" => 'hatin dîtin.',
+
+    // Pending screen
+    'pending_message_1' => 'Reklam di Pêvajoya Pejirandinê de ye',
+    'pending_message_2' => "Dîroka Nûvekirina Ad",
+    'pending_message_3' => 'Wextê Bendewariyê Texmîn kirin',
+    'pending_message_4' => 'Dema ku di pêvajoya pejirandinê de derbas bûye dê li dema dopîngê were zêdekirin.',
+    'pending_message_5' => 'Di pêvajoya pejirandinê de têkildarî reklamê ji navenda bangê çu danûstandin nayê kirin.',
+    'hours' => 'saetan',
+
+    // Preview page
+    'preview_text' => [
+        'part_1' => 'Heke di derheqê reklama we de agahdariya jêrîn rast e, bişkoja "Berdewam" bikirtînin û biçin gava paşîn.',
+        'part_2' => 'Heke na, bişkoja "Biguherîne" bikirtînin.',
+    ],
+    'views' => 'Dîtin',
+
+    // Detail page
+    "no_street_view" => "Li vî cîhî wêneyek dîtina kolanan tune",
+    'edit_my_contact_info' => 'Agahdariya Têkiliya Min Biguherînin',
+    'update_my_contact_info' => 'agahdariya têkiliya min nûve bikin',
+    'contact_info' => 'Agahdariya têkiliyê',
+    'ad_info' => 'Agahdariya Ad',
+
+    // Select cat
+    'select_category_step_by_step' => 'Gav bi Gav Kategorî hilbijêrin',
+
+    // New create page
+    'options' => 'Vebijarkên',
+    'category_selection' => 'Hilbijartina Kategorî',
+    'listing_details' => 'Details hûrgulî',
+    'preview' => 'Pêşnerîn',
+    'congratulations' => 'Pîrozbahî',
+    'ad_desc' => 'Ad Description',
+    'upload_photos' => 'Wêneyan bar bikin',
+    'additional_fields' => 'Zeviyên Zêdeyî',
+    'ad_location' => 'Cihê Ad',
+
+    'free' => 'Belaş',
+    'ad_date' => 'Dîroka Ad',
+    'photo_video' => 'Wêne, Vîdyo',
+    'and_above' => 'û li jor',
+    'and_below' => 'û li jêr',
+    'clear_all' => 'Hemî Paqij Bikin',
+    'balances' => 'Hevsengî',
+    'packages' => 'Packages',
+    'dopings' => 'Dopîng',
+    'advs' => 'Advs',
+    'site' => 'Malper',
+    'subscription' => 'Abonetî',
+	'created' => 'Afirandin',
+	'product_option' => [
+		'name' => 'Hilbijarka Hilberê'
+	],
+	'option_json' => [
+		'name' => 'Dibe'
+	],
+    'old_price' => [
+        'name' => 'Bihayê Kevn'
+    ],
+];
