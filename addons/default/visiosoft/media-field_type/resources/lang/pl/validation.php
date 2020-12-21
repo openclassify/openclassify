39b4f34aaa93ec876454a6e8c7037e8f4692df0b
Fatih Alp <mail@fatihalp.com>
2020-12-21 16:05:33 +0300

    New translations validation.php (Polish)

--- addons/default/visiosoft/media-field_type/resources/lang/pl/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/pl/validation.php
@@ -2,4 +2,5 @@
 
 return [
     'valid_disk' => 'Skonfigurowany dysk do przesyłania dla: atrybut nie istnieje.',
+	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
 ];
