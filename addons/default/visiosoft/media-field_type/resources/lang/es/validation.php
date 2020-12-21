edf774eea66b0bab7f2739a1e8267f6c750f70e3
Fatih Alp <mail@fatihalp.com>
2020-12-21 19:05:53 +0300

    New translations validation.php (Spanish)

--- addons/default/visiosoft/media-field_type/resources/lang/es/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/es/validation.php
@@ -2,5 +2,5 @@
 
 return [
     'valid_disk' => 'El disco de carga configurado para: atributo no existe.',
-	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
+	'unsupported' => 'Esta área es solo para archivos de imagen. Puede agregar diferentes tipos de archivos en el campo de descripción.'
 ];
