37f0052de49a6fa79199fb9fcaef2e508b09f013
Fatih Alp <mail@fatihalp.com>
2020-12-21 13:32:54 +0300

    New translations message.php (Kurdish)

---
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/ku/message.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'choose_file'      => 'Hûn dixwazin kîjan pelê bikar bînin?',
+    'choose_folder'    => 'Hûn dixwazin li kîjan peldankê bar bikin?',
+    'upload'           => 'Ji bo barkirinê pelan li vir bikirtînin an bavêjin.',
+    'no_file_selected' => 'Tu pel nehat hilbijartin.',
+    'no_uploads'       => 'Tu pel nehatin barkirin.',
+    'overwrite'        => 'berê hate barkirin. Ma hûn dixwazin wê binivîsin?',
+    'uploading'        => 'Uploading',
+    'loading'          => 'Barkirin',
+];
