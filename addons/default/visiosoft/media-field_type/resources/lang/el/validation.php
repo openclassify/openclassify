34d4cb8d2a26f16b65e5918771ecb03d2ff88b93
Fatih Alp <mail@fatihalp.com>
2020-12-21 16:05:47 +0300

    New translations validation.php (Greek)

--- addons/default/visiosoft/media-field_type/resources/lang/el/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/el/validation.php
@@ -2,4 +2,5 @@
 
 return [
     'valid_disk' => 'Ο διαμορφωμένος δίσκος μεταφόρτωσης για: χαρακτηριστικό δεν υπάρχει.',
+	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
 ];
