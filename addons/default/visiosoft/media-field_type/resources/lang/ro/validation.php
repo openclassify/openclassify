a4f04bc42f7825a28e6077ab1d54a162450cec51
Fatih Alp <mail@fatihalp.com>
2020-12-21 19:04:18 +0300

    New translations validation.php (Romanian)

--- addons/default/visiosoft/media-field_type/resources/lang/ro/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/ro/validation.php
@@ -2,5 +2,5 @@
 
 return [
     'valid_disk' => 'Discul de încărcare configurat pentru: atribut nu există.',
-	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
+	'unsupported' => 'Această zonă este doar pentru fișiere imagine. Puteți adăuga diferite tipuri de fișiere în câmpul de descriere.'
 ];
