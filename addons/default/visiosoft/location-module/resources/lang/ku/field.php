818927b0b6f1a92263915b1a9dd194d53b241592
Fatih Alp <mail@fatihalp.com>
2020-12-21 13:30:42 +0300

    New translations field.php (Kurdish)

---
+++ addons/default/visiosoft/location-module/resources/lang/ku/field.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Nav'
+    ],
+    'slug' => [
+        'name' => 'Şeytanok'
+    ],
+    'city' => [
+        'name' => 'Bajar'
+    ],
+    'country' => [
+        'name' => 'Welat'
+    ],
+    'district' => [
+        'name' => 'Herêm'
+    ],
+    'order' => [
+        'name' => 'Rêzê Rêz bike'
+    ],
+    'neighborhood' => [
+        'name' => 'Cînarî'
+    ],
+    'village' => [
+        'name' => 'Gûnd'
+    ],
+    'description' => [
+        'name' => 'Terîf'
+    ],
+    'pick_option' => [
+        'name' => 'Vebijarek hilbijêrin',
+    ],
+    'address' => [
+        'name' => 'Navnîşan',
+    ],
+    'selected' => 'Hilbijartin',
+    'choose_an_option' => 'Vebijarek hilbijêrin ...',
+];
