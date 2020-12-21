6a443c92175400a5a238bae2b4c3a5f34711350f
Fatih Alp <mail@fatihalp.com>
2020-12-21 16:05:39 +0300

    New translations validation.php (Azerbaijani)

--- addons/default/visiosoft/media-field_type/resources/lang/az/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/az/validation.php
@@ -2,4 +2,5 @@
 
 return [
     'valid_disk' => 'Konfiqurasiya edilmiş yükləmə diski: atribut mövcud deyil.',
+	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
 ];
