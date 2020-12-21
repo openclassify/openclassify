6ef3908a6951cfe17b0653b0f2c2f75942bda10e
Fatih Alp <mail@fatihalp.com>
2020-12-21 16:05:14 +0300

    New translations validation.php (Turkish)

--- addons/default/visiosoft/media-field_type/resources/lang/tr/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/tr/validation.php
@@ -2,4 +2,5 @@
 
 return [
     'valid_disk' => 'Yükleme diski :attribute için mevcut değil.',
+	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
 ];
