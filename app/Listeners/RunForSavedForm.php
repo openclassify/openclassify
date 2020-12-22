59acc048316a78708cea1375a6eac5dba27ae7ef
vedatakdogan <vedat@vebze.com>
2020-12-22 19:04:56 +0300

    fixed maintenance

---
+++ app/Listeners/RunForSavedForm.php
@@ -0,0 +1,21 @@
+<?php namespace App\Listeners;
+
+use Anomaly\SettingsModule\Setting\Form\SettingFormRepository;
+use Anomaly\Streams\Platform\Ui\Form\Event\FormWasSaved;
+
+class RunForSavedForm
+{
+
+    public function handle(FormWasSaved $event)
+    {
+        $builder = $event->getBuilder();
+
+        if (get_class($builder->getRepository()) === SettingFormRepository::class) {
+            if ($builder->getFormValues()->has('maintenance') and $builder->getFormValues()->get('maintenance')) {
+                file_put_contents(storage_path('framework/down'),
+                    json_encode(['time' => '', 'retry' => null, 'message' => null],
+                        JSON_PRETTY_PRINT));
+            }
+        }
+    }
+}
