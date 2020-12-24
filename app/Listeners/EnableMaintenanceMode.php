e43ec4f57b639798dde4b71bee769d5dadbe17c6
vedatakdogan <vedat@vebze.com>
2020-12-24 17:23:13 +0300

    fixed comment

--- app/Listeners/EnableMaintenanceMode.php
+++ app/Listeners/EnableMaintenanceMode.php
@@ -2,6 +2,7 @@
 
 use Anomaly\SettingsModule\Setting\Form\SettingFormRepository;
 use Anomaly\Streams\Platform\Ui\Form\Event\FormWasSaved;
+use Illuminate\Support\Facades\Artisan;
 
 class EnableMaintenanceMode
 {
@@ -9,12 +10,9 @@ class EnableMaintenanceMode
     public function handle(FormWasSaved $event)
     {
         $builder = $event->getBuilder();
-
         if (get_class($builder->getRepository()) === SettingFormRepository::class) {
             if ($builder->getFormValues()->has('maintenance') and $builder->getFormValues()->get('maintenance')) {
-                file_put_contents(storage_path('framework/down'),
-                    json_encode(['time' => '', 'retry' => null, 'message' => null],
-                        JSON_PRETTY_PRINT));
+                Artisan::call('down');
             }
         }
     }
