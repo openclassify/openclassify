adeaff40e14545a4e28d0e96c3a6293401227c8c
Fatih Alp <mail@fatihalp.com>
2020-12-29 18:14:52 +0300

    New translations permission.php (Hindi)

--- addons/default/visiosoft/advs-module/resources/lang/hi/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/hi/permission.php
@@ -97,4 +97,12 @@ return [
             'delete' => 'विकल्प कॉन्फ़िगरेशन हटा सकते हैं?',
         ],
     ],
+    'status' => [
+        'name'   => 'Status',
+        'option' => [
+            'read'   => 'Can read status?',
+            'write'  => 'Can create/edit status?',
+            'delete' => 'Can delete status?',
+        ],
+    ],
 ];
