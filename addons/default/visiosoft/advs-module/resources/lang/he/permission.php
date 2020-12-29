6a3a8cd6affa47ad5bba9f3e1347c49e17053fbe
Fatih Alp <mail@fatihalp.com>
2020-12-29 18:12:53 +0300

    New translations permission.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/permission.php
@@ -97,4 +97,12 @@ return [
             'delete' => 'האם ניתן למחוק את תצורת האפשרויות?',
         ],
     ],
+    'status' => [
+        'name'   => 'Status',
+        'option' => [
+            'read'   => 'Can read status?',
+            'write'  => 'Can create/edit status?',
+            'delete' => 'Can delete status?',
+        ],
+    ],
 ];
