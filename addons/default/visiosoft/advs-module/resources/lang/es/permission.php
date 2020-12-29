3ec36a9efd2751f81997631dee78410058fa678e
Fatih Alp <mail@fatihalp.com>
2020-12-29 18:13:09 +0300

    New translations permission.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/permission.php
@@ -97,4 +97,12 @@ return [
             'delete' => '¿Se puede eliminar la configuración de opciones?',
         ],
     ],
+    'status' => [
+        'name'   => 'Status',
+        'option' => [
+            'read'   => 'Can read status?',
+            'write'  => 'Can create/edit status?',
+            'delete' => 'Can delete status?',
+        ],
+    ],
 ];
