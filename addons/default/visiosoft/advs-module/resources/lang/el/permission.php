79c6d8fae92fa7849c446415fe6160944adb2696
Fatih Alp <mail@fatihalp.com>
2020-12-29 18:12:25 +0300

    New translations permission.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/permission.php
@@ -97,4 +97,12 @@ return [
             'delete' => 'Μπορεί να διαγραφεί η διαμόρφωση επιλογών;',
         ],
     ],
+    'status' => [
+        'name'   => 'Status',
+        'option' => [
+            'read'   => 'Can read status?',
+            'write'  => 'Can create/edit status?',
+            'delete' => 'Can delete status?',
+        ],
+    ],
 ];
