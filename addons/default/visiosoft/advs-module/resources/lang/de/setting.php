f3231949577f6ec1856cf9c074f58b4763d81b81
Fatih Alp <mail@fatihalp.com>
2020-12-30 17:00:50 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/setting.php
@@ -219,4 +219,8 @@ return [
         'name' => 'Farbe der Anzeigenschaltfläche erstellen',
         'instructions' => 'Ändert die Schaltfläche, die angezeigt wird, wenn die Auswahl der neuen Anzeigenkategorie abgeschlossen ist.'
     ],
+    'lang_switcher_for_browser' => [
+        'name' => 'Lang Switcher For Browser',
+        'instructions' => 'Allows you to choose browser language as active language'
+    ],
 ];
