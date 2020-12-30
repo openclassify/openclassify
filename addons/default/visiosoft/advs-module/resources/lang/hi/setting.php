f65dffb20fed34ef11146bfd4b35d48ba9ce8af1
Fatih Alp <mail@fatihalp.com>
2020-12-30 17:00:19 +0300

    New translations setting.php (Hindi)

--- addons/default/visiosoft/advs-module/resources/lang/hi/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/hi/setting.php
@@ -219,4 +219,8 @@ return [
         'name' => 'विज्ञापन बटन रंग बनाएँ',
         'instructions' => 'नया विज्ञापन श्रेणी चयन समाप्त होने पर दिखाई देने वाले बटन को बदल देता है।'
     ],
+    'lang_switcher_for_browser' => [
+        'name' => 'Lang Switcher For Browser',
+        'instructions' => 'Allows you to choose browser language as active language'
+    ],
 ];
