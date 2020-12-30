a780b520dc0ea8761e4e4f58a127f97626638b08
Fatih Alp <mail@fatihalp.com>
2020-12-30 17:01:08 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/setting.php
@@ -219,4 +219,8 @@ return [
         'name' => 'Reklam Düyməsini Rəng Yaradın',
         'instructions' => 'Yeni reklam kateqoriyası seçimi bitdikdə görünən düyməni dəyişdirir.'
     ],
+    'lang_switcher_for_browser' => [
+        'name' => 'Lang Switcher For Browser',
+        'instructions' => 'Allows you to choose browser language as active language'
+    ],
 ];
