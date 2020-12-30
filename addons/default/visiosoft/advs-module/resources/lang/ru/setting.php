33f92b4baa07d6a7cbfd5153347ec0322ce6a418
Fatih Alp <mail@fatihalp.com>
2020-12-30 17:01:10 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
@@ -219,4 +219,8 @@ return [
         'name' => 'Создать цвет кнопки объявления',
         'instructions' => 'Изменяет кнопку, которая появляется после завершения выбора новой категории объявлений.'
     ],
+    'lang_switcher_for_browser' => [
+        'name' => 'Lang Switcher For Browser',
+        'instructions' => 'Allows you to choose browser language as active language'
+    ],
 ];
