b166a1b9bcec073d8cbae1e644c73075789c7c61
Fatih Alp <mail@fatihalp.com>
2020-12-30 17:01:03 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -219,4 +219,8 @@ return [
         'name' => 'Kleur van advertentieknop maken',
         'instructions' => 'Wijzigt de knop die wordt weergegeven wanneer de selectie van de nieuwe advertentiecategorie is voltooid.'
     ],
+    'lang_switcher_for_browser' => [
+        'name' => 'Lang Switcher For Browser',
+        'instructions' => 'Allows you to choose browser language as active language'
+    ],
 ];
