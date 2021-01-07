2f448104c7befa30a66b714c70e2ca4e84cdf1c4
Fatih Alp <mail@fatihalp.com>
2021-01-07 14:33:46 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
@@ -219,4 +219,12 @@ return [
         'name' => 'Krijo ngjyrën e butonit të reklamës',
         'instructions' => 'Ndryshon butonin që shfaqet kur të përfundojë zgjedhja e kategorisë së re të reklamës.'
     ],
+    'lang_switcher_for_browser' => [
+        'name' => 'Lang Switcher For Browser',
+        'instructions' => 'Allows you to choose browser language as active language'
+    ],
+    'get_categories' => [
+        'name' => 'GET Categories',
+        'instructions' => 'It represents the categories in which the GET property will be valid.'
+    ],
 ];
