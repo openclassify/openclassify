3a1642435a0fb067b508179007519d878325d472
Diatrex <diahshalabi@hotmail.com>
2021-01-07 14:43:54 +0300

    #6 Notation and information part design on the map

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,5 @@ let mix = require('laravel-mix');
 
 mix
     .js('resources/assets/js/app.js', 'public/js')
+    .js('addons/default/visiosoft/*/resources/js/init.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
