ff723265c6393a2a39be35ac8c930b28269ac2c3
Fatih Alp <mail@fatihalp.com>
2021-01-07 14:33:27 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
@@ -219,4 +219,12 @@ return [
         'name' => 'Creați culoarea butonului publicitar',
         'instructions' => 'Schimbă butonul care se afișează la finalizarea selecției noii categorii de anunțuri.'
     ],
+    'lang_switcher_for_browser' => [
+        'name' => 'Lang Switcher For Browser',
+        'instructions' => 'Allows you to choose browser language as active language'
+    ],
+    'get_categories' => [
+        'name' => 'GET Categories',
+        'instructions' => 'It represents the categories in which the GET property will be valid.'
+    ],
 ];
