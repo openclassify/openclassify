fef2c51316a45677d8c6765493c8f3adee5a2a6a
Diatrex <diahshalabi@hotmail.com>
2021-01-07 16:08:34 +0300

    revert

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,4 @@ let mix = require('laravel-mix');
 
 mix
     .js('resources/assets/js/app.js', 'public/js')
-    .js('addons/default/visiosoft/*/resources/js/init.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css');
