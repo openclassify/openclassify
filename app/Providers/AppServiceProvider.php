2b00f1029f870848de159c7cffbaadc1283b3fd2
Muammer Top <muammer@visiosoft.com.tr>
2021-01-08 17:42:02 +0300

    #3049 Modules to Hide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Auto Language Switcher
-        view()->composer('*', function ($view) {
-            if (config('advs.lang_switcher_for_browser') and is_null(Request()->session()->get('_locale'))) {
+	    view()->composer('*', function ($view) {
+	    	//Hidden menu items in sidebar on dashboard
+		    if (auth()->check() and template()->get('cp')){
+			    template()->get('cp')->getNavigation()->get('anomaly.module.variables')->setClass('hidden');
+			    template()->get('cp')->getNavigation()->get('anomaly.module.system')->setClass('hidden');
+			    template()->get('cp')->getNavigation()->get('anomaly.module.redirects')->setClass('hidden');
+			    template()->get('cp')->getNavigation()->get('anomaly.module.repeaters')->setClass('hidden');
+		    }
+		    //Auto Language Switcher
+		    if (config('advs.lang_switcher_for_browser') and is_null(Request()->session()->get('_locale'))) {
                 $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);//Get Browser Language
                 $acceptLang = config('streams::locales.enabled'); //Supported Language
                 $lang = in_array($lang, $acceptLang) ? $lang : config('streams::locales.default', 'en');
