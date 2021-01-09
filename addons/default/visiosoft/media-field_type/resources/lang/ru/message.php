bb4f0a6bfd86140535c20c028d0a63592f3854f5
Fatih Alp <mail@fatihalp.com>
2021-01-09 13:05:52 +0300

    New translations message.php (Russian)

--- addons/default/visiosoft/media-field_type/resources/lang/ru/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/ru/message.php
@@ -10,4 +10,5 @@ return [
     'uploading' => 'Выгрузка',
     'loading' => 'загрузка',
     'error_upload' => 'При загрузке файла возникла проблема.',
+    'error_upload_docs' => 'There is no document upload support. Please contact your system administrator!',
 ];
