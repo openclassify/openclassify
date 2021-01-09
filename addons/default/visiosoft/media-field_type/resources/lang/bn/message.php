68bf7f134a54941beb0cb1a86a4daf73c4b89caa
Fatih Alp <mail@fatihalp.com>
2021-01-09 13:05:38 +0300

    New translations message.php (Bengali)

--- addons/default/visiosoft/media-field_type/resources/lang/bn/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/bn/message.php
@@ -10,4 +10,5 @@ return [
     'uploading' => 'আপলোড হচ্ছে',
     'loading' => 'বোঝাই',
     'error_upload' => 'ফাইলটি আপলোড করতে সমস্যা হয়েছিল।',
+    'error_upload_docs' => 'There is no document upload support. Please contact your system administrator!',
 ];
