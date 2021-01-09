3614ee33414a2a152a918b4d12010936c7f3cfc1
Fatih Alp <mail@fatihalp.com>
2021-01-09 13:06:11 +0300

    New translations message.php (Arabic)

--- addons/default/visiosoft/media-field_type/resources/lang/ar/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/ar/message.php
@@ -10,4 +10,5 @@ return [
     'uploading' => 'جاري الرفع',
     'loading' => 'جاري التحميل',
     'error_upload' => 'كانت هناك مشكلة في تحميل الملف.',
+    'error_upload_docs' => 'There is no document upload support. Please contact your system administrator!',
 ];
