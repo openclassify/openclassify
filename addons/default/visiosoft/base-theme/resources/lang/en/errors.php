8f163f457a1570b804851c8dc63579538e2c17c1
vedatakd <vedat@vebze.com>
2021-01-09 14:12:08 +0300

    added 403 error page

--- addons/default/visiosoft/base-theme/resources/lang/en/errors.php
+++ addons/default/visiosoft/base-theme/resources/lang/en/errors.php
@@ -2,6 +2,8 @@
 
 return [
     '404_title' => 'Not Found',
+    '403_title' => 'Forbidden',
     '404_message' => 'The page you were looking for was not found.',
+    '403_message' => 'Access to this resources on the site is denied!',
     'home_page' => 'Go to Home Page',
 ];
--- addons/default/visiosoft/base-theme/src/BaseThemeServiceProvider.php
+++ addons/default/visiosoft/base-theme/src/BaseThemeServiceProvider.php
@@ -128,6 +128,7 @@ class BaseThemeServiceProvider extends AddonServiceProvider
      */
     protected $mobile = [
         'streams::errors/404' => 'visiosoft.theme.base::errors/404',
+        'streams::errors/403' => 'visiosoft.theme.base::errors/403',
         'anomaly.module.users::login' => 'visiosoft.theme.base::addons/anomaly/users-module/login',
         'anomaly.module.users::register' => 'visiosoft.theme.base::addons/anomaly/users-module/register',
         'anomaly.module.users::password/forgot' => 'visiosoft.theme.base::addons/anomaly/users-module/password/forgot',
