2d8983f5ea4f7fad1425fd7c0634980692932e53
Fatih Alp <mail@fatihalp.com>
2021-01-09 19:18:34 +0300

    New translations errors.php (Albanian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/sq/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '404_title' => 'Not Found',
+    '403_title' => 'Forbidden',
+    '404_message' => 'The page you were looking for was not found.',
+    '403_message' => 'Access to this resources on the site is denied!',
+    'home_page' => 'Go to Home Page',
+];
