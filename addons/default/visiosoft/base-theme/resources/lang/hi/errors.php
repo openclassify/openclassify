e7dff549e1e6a0c0632ec27132607014ab0dcd17
Fatih Alp <mail@fatihalp.com>
2021-01-17 18:02:49 +0300

    New translations errors.php (Hindi)

--- addons/default/visiosoft/base-theme/resources/lang/hi/errors.php
+++ addons/default/visiosoft/base-theme/resources/lang/hi/errors.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-    '404_title' => 'Not Found',
-    '403_title' => 'Forbidden',
-    '404_message' => 'The page you were looking for was not found.',
-    '403_message' => 'Access to this resources on the site is denied!',
-    'home_page' => 'Go to Home Page',
+    '404_title' => 'Nahin Mila',
+    '403_title' => 'Mana kiya hua',
+    '404_message' => 'Vah prshth jise aap khoj rahe the, vah nahin mila.',
+    '403_message' => 'Site par is sansaadhanon tak pahunch se vanchit hai!',
+    'home_page' => 'Mukhaprshth pr jaen
+',
 ];
