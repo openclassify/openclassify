17f5bc1ff901568f8a57b11847dd9f69d0ca6071
Fatih Alp <mail@fatihalp.com>
2021-01-17 03:41:43 +0300

    New translations button.php (Kurdish)

--- addons/default/visiosoft/profile-module/resources/lang/ku/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/ku/button.php
@@ -6,17 +6,22 @@ return [
     'show' => 'ناونیشانی پیشانبدە',
     'update_password' => ' تێپەڕەوشەی نوێ بکەرەوە
 ',
-    'update_profile' => 'Nûvekirina Profîlê',
-    'delete' => 'Jêbirin',
-    'extend' => 'N',
-    'extend_all' => 'Hemî Dirêj Bikin',
-    'extend_unpublished' => 'Berfireh Nedayîn',
-    'edit' => 'Weşandin',
-    'go_profile' => 'Herin Detailê Profîl',
-    'go_user' => 'Herin Berfirehiya Bikarhêner',
-    'personal' => 'Şexsî',
+    'update_profile' => ' پرۆفایلی نوێکردنەوە',
+    'delete' => 'سڕینەوە',
+    'extend' => 'درێژ بکە',
+    'extend_all' => 'هەموو درێژ بکە',
+    'extend_unpublished' => 'بڵاونەکراوەتەوەی درێژ بکە
+',
+    'edit' => 'دەستکاری بکە',
+    'go_profile' => 'بچۆ ناو پڕۆفایل وردەکاری
+',
+    'go_user' => 'بچۆ بۆ بەکارهێنەر وردەکاری
+',
+    'personal' => 'شه‌خسی',
     'corporate' => 'Pargîdanî',
-    'export' => 'Eksport',
-    'new_education' => 'Perwerdehiya Nû',
-    'new_education_part' => 'Beşa Perwerdehiya Nû',
+    'export' => 'هەناردن',
+    'new_education' => 'په روه رده ی نوێ
+',
+    'new_education_part' => 'بەشی پەروەردەی نوێ
+',
 ];
