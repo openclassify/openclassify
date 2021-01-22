9acc66ec79237efb1891e6bbb9f6ffdb2d158b6c
Fatih Alp <mail@fatihalp.com>
2021-01-22 20:22:47 +0300

    New translations field.php (Kurdish)

--- addons/default/visiosoft/cats-module/resources/lang/ku/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ku/field.php
@@ -21,14 +21,15 @@ return [
     ],
     'seo_keyword' => [
         'name' => 'Seo Keyword',
-        'instructions' => 'Gotinên ku di geroka Google-ê de li malperê hatine zêdekirin da ku li jorê encamên lêgerînê xuya bibin.',
+        'instructions' => 'وشەی زیادکراو بۆ سایتەکە لە گۆگڵ بۆ ئەوەی لەسەر ئەنجامی گەڕانەکان دەربکەوێت.
+',
     ],
     'seo_description' => [
         'name' => 'Seo Danasîn',
         'instructions' => 'Ew dikare li ser biryara mêvanên we bandor bike ka ew dixwazin naverokê di encamên lêgerînê de bikirtînin.',
     ],
     'icon' => [
-        'name' => 'Icon',
+        'name' => 'وێنۆچکە',
         'instructions' => 'Ew tê bikaranîn ku îkonên ku celebê kategoriyê nîşan dikin zêde bike.',
     ],
 
