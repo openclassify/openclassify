47af717696411236b22450b2a7e61d2630149a86
Fatih Alp <mail@fatihalp.com>
2021-01-22 18:29:33 +0300

    New translations errors.php (Kurdish)

--- addons/default/visiosoft/base-theme/resources/lang/ku/errors.php
+++ addons/default/visiosoft/base-theme/resources/lang/ku/errors.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    '404_title' => 'Peyda nebû',
+    '404_title' => 'نەدۆزراوەتەوە
+',
     '403_title' => 'Qedexekirî',
     '404_message' => 'Rûpela ku hûn lê digerin nehat dîtin.',
     '403_message' => 'Li ser malperê gihîştina van çavkaniyan nayê qedexekirin!',
