09ab60728b51689f8fb3db8e953fe70752a15b42
Fatih Alp <mail@fatihalp.com>
2021-01-22 19:05:08 +0300

    New translations errors.php (Kurdish)

--- addons/default/visiosoft/base-theme/resources/lang/ku/errors.php
+++ addons/default/visiosoft/base-theme/resources/lang/ku/errors.php
@@ -3,8 +3,10 @@
 return [
     '404_title' => 'نەدۆزراوەتەوە
 ',
-    '403_title' => 'Qedexekirî',
-    '404_message' => 'Rûpela ku hûn lê digerin nehat dîtin.',
-    '403_message' => 'Li ser malperê gihîştina van çavkaniyan nayê qedexekirin!',
-    'home_page' => 'Herin Serûpelê',
+    '403_title' => 'قەدەغەکراو',
+    '404_message' => 'ئەو پەیجەی تۆ بەدوایدا دەگەڕایت نەدۆزرایەوە',
+    '403_message' => '! دەستکەوت بۆ ئەم سەرچاوەیە لە سایتەکە ڕەتدەکرێتەوە
+',
+    'home_page' => 'بڕۆ بۆ ماڵپەڕ
+',
 ];
