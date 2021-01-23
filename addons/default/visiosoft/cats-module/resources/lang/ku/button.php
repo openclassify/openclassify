fe84359b871f0cc88049f5087d1203bcecf2ad12
Fatih Alp <mail@fatihalp.com>
2021-01-23 15:29:56 +0300

    New translations button.php (Kurdish)

--- addons/default/visiosoft/cats-module/resources/lang/ku/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/ku/button.php
@@ -3,7 +3,8 @@
 return [
     'new_category' => 'کەتێگۆریی نوێ
 ',
-    'add_sub_category' => 'Kategoriya Jêrîn zêde bikin',
-    'sub_category' => 'Kategoriya Jêrîn nîşan bide',
-    'new_placeholderforsearch' => 'New Placeholderforsearch',
+    'add_sub_category' => 'کەتێگۆریی ژێرەوە زیاد بکەن',
+    'sub_category' => 'کەتێگۆریی ژێرەوە نیشاندە',
+    'new_placeholderforsearch' => 'شوێنی نوێ بۆ گەڕان
+',
 ];
