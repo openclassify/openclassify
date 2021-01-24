9120416dd4c0d203e210b6a43bb922bce4b1e233
Fatih Alp <mail@fatihalp.com>
2021-01-24 18:42:18 +0300

    New translations control_panel.php (Serbian (Latin))

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/sr/control_panel.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'help'      => 'Help',
+    'search'    => 'Search',
+    'logout'    => 'Logout',
+    'view_site' => 'View Site',
+    'title'     => 'Control Panel',
+    'search_placeholder'     => 'Type to search',
+
+    // Users Module
+    'search_by_gsm_number'     => 'Search By GSM Number',
+];
