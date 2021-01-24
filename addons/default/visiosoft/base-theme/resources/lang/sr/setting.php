a9bb832733a2563a3e112cd9baa6f5091a5f94fd
Fatih Alp <mail@fatihalp.com>
2021-01-24 18:42:33 +0300

    New translations setting.php (Serbian (Latin))

---
+++ addons/default/visiosoft/base-theme/resources/lang/sr/setting.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Navigation Title',
+    ],
+    'navigation_action' => [
+        'name' => 'Navigation Action',
+    ],
+    'country_fields' => [
+        'name' => 'Country Fields',
+    ],
+    'date_fields' => [
+        'name' => 'Date Fields',
+    ],
+    'price_fields' => [
+        'name' => 'Price Fields',
+    ],
+    'breadcrumbs' => [
+        'name' => 'Breadcrumb',
+    ],
+    'ad_details' => [
+        'name' => 'Ad Details',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Ad Details Tab',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Latest and View All Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'Register Page Instruction Logo',
+    ],
+    'register_page_alert_link' => [
+        'name' => 'Register Page Alert Link',
+    ],
+    'style' => [
+        'name' => 'Style(CSS)',
+    ],
+    'home_page_sub_categories_limit' => [
+        'name' => 'Homepage Sub Categories Limit',
+    ],
+	'default_country' => [
+		'name' => 'Register page, default country for phone',
+	]
+];
