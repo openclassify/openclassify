d03afdf0213b2b5a503a36516833e4e44b72b372
Fatih Alp <mail@fatihalp.com>
2021-01-24 18:42:41 +0300

    New translations stream.php (Serbian (Latin))

---
+++ addons/default/visiosoft/advs-module/resources/lang/sr/stream.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => 'Advs',
+    ],
+    'categories' => [
+        'name' => 'Categories',
+    ],
+    'village' => [
+        'name' => 'Village',
+    ],
+    'custom_fields' => [
+        'name' => 'Custom fields',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Custom field advs',
+    ],
+    'cf_values' => [
+        'name' => 'Cf values',
+    ],
+    'options' => [
+        'name' => 'Options',
+    ],
+    'product_options' => [
+        'name' => 'Product Options',
+    ],
+    'productoptions_value' => [
+        'name' => 'Options value',
+    ],
+    'option_configuration' => [
+        'name' => 'Configuration',
+    ],
+    'status' => [
+        'name' => 'Status',
+    ],
+];
