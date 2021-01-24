c77d6793a5cc36a2419cad2ed14b49711893cede
Fatih Alp <mail@fatihalp.com>
2021-01-24 18:42:38 +0300

    New translations button.php (Serbian (Latin))

---
+++ addons/default/visiosoft/base-theme/resources/lang/sr/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'post_ad' => [
+        'name' => 'Post Ad',
+    ],
+    'login' => 'Login',
+    'register' => 'Register',
+    'continue' => 'Continue',
+    'reset_password' => 'Reset Password',
+    'email' => 'Email',
+    'phone' => 'Phone',
+    'show_all' => 'Show All',
+    'hide_all' => 'Hide All',
+];
