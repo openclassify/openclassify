1372ba16c633e510112c9edaee454346dd35071c
Fatih Alp <mail@fatihalp.com>
2021-01-28 20:22:09 +0300

    New translations section.php (Bulgarian)

---
+++ addons/default/visiosoft/profile-module/resources/lang/bg/section.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'Profile',
+    ],
+    'adress' => [
+        'title' => 'Adress',
+    ],
+    'general_setting' => 'General Settings',
+    'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Education',
+    ],
+    'education_part' => [
+        'title' => 'Education part',
+    ],
+];
