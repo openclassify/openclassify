3b8f71669d9f9ad17bd87446f962dcc260a0e051
Fatih Alp <mail@fatihalp.com>
2021-01-28 20:20:45 +0300

    New translations section.php (Bulgarian)

---
+++ addons/default/visiosoft/location-module/resources/lang/bg/section.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'countries' => [
+        'title' => 'Countries',
+    ],
+    'cities' => [
+        'title' => 'Cities',
+    ],
+    'districts' => [
+        'title' => 'Districts',
+    ],
+    'neighborhoods' => [
+        'title' => 'Neighborhoods',
+    ],
+    'village' => [
+        'title' => 'Village',
+    ],
+    'general' => 'General',
+    'map' => 'Map',
+    'setting' => 'Setting',
+    'filter' => 'Filter',
+];
