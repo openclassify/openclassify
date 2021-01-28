ac293c00e887f09f6db594817f403e7a7da52c61
Fatih Alp <mail@fatihalp.com>
2021-01-28 20:20:27 +0300

    New translations control_panel.php (Bulgarian)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/bg/control_panel.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'help'      => 'Помогне',
+    'search'    => 'Търсене',
+    'logout'    => 'Излез от профила си',
+    'view_site' => 'Преглед на сайта',
+    'title'     => 'Контролен панел',
+    'search_placeholder'     => 'Въведете за търсене',
+
+    // Users Module
+    'search_by_gsm_number'     => 'Търсене по GSM номер',
+];
