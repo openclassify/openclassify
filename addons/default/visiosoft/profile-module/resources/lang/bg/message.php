268b9deeba594aa34312b828d735bd13712953f3
Fatih Alp <mail@fatihalp.com>
2021-01-28 20:21:53 +0300

    New translations message.php (Bulgarian)

---
+++ addons/default/visiosoft/profile-module/resources/lang/bg/message.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    "email"      => "E-Mail Field is Required!",
+    "username"      => "Username Field is Required!",
+    "success_update"      => "Profile Update Successfully!",
+    "adress_success_update"      => "Adress Update Successfully!",
+    "adress_success_create"      => "Adress Create Successfully!",
+    "login_error" => "Incorrect login or password.",
+    "login_noMail_old_user" => "The email address you entered couldn’t be found.",
+    "login_noMail_old_user2" => "Please check the email address and try again.",
+    "login_noMail_old_user3" => "If the problem continues, please contact at 'info@openclassify.com'.",
+    "login_info_old_user" => "A new password has been sent to your e-mail address.",
+    "success" => "Success",
+    "notified_new_updates" => "I want to be notified of new updates",
+    "notified_about_ads" => "Allow notifications about my ads",
+    "receive_messages_email" => "I want to receive messages as email",
+    "no_packages_module" => "No Packages Module!",
+    "required_add"=> "Please fill in all required lines.",
+    "no_extend_package" => "No Extend Ad Packages",
+    "saved" => "Saved!",
+    'please_confirm_transaction' => 'Please Confirm Transaction',
+    'password_do_not_match' => 'Passwords do not match',
+    'your_password_changed' => 'Your password was successfully updated',
+    'error_valid_email_or_phone' => 'Phone number or E-mail address format is not correct.',
+    'error_valid_phone' => 'Phone number format is not correct.',
+    'registered_phone' => 'This phone number has already been registered.',
+    'ajax_address_error' => 'No address or not authorized to view.',
+    'empty_password_sms_message' => 'Due to security issues, we changed your password! Your new password is:',
+    'required_all' => "All Field is Required!",
+    'can_not_remove_filled_fields' => "You can not remove filled fields!",
+    'wrong_password' => "The password you entered is not correct!",
+
+    // Forgot Password
+    'email_phone_not_found' => 'The E-mail, Phone Number is not correct!',
+
+    'disable_account' => 'Do you want to close your account? This operation cannot be undone.',
+
+    // Mail
+    'update_email_mail_subject' => 'Email Updated!',
+    'update_email_mail_message' => 'Your Email Has Been Updated!',
+
+    // Register
+    'recaptcha_field_is_required' => 'reCaptcha field is required.',
+    'failed_to_validate_captcha' => 'Failed to validate captcha.',
+];
