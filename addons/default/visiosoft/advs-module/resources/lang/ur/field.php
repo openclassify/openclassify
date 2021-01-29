98c71e7fdee5a3a09fe9321d958807bc3afc7822
Fatih Alp <mail@fatihalp.com>
2021-01-29 16:18:55 +0300

    New translations field.php (Urdu (Pakistan))

--- addons/default/visiosoft/advs-module/resources/lang/ur/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ur/field.php
@@ -322,6 +322,8 @@ return [
     'upload_files' => 'فائلیں اپ لوڈ کرو',
     'additional_fields' => 'اضافی فیلڈز',
     'ad_location' => 'اشتہار کی جگہ',
+    'owner' => 'Owner',
+    'default_owner_instruction' => 'The owner will default to the current user if no user is selected',
 
     'free' => 'مفت',
     'ad_date' => 'اشتہاری تاریخ',
