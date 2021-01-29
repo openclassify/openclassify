14fb6c1e4c05ea2887d0787c8f7a5d79c2622a2c
Fatih Alp <mail@fatihalp.com>
2021-01-29 16:19:19 +0300

    New translations field.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/field.php
@@ -322,6 +322,8 @@ return [
     'upload_files' => 'העלה קבצים',
     'additional_fields' => 'שדות נוספים',
     'ad_location' => 'מיקום מודעה',
+    'owner' => 'Owner',
+    'default_owner_instruction' => 'The owner will default to the current user if no user is selected',
 
     'free' => 'חינם',
     'ad_date' => 'תאריך מודעה',
