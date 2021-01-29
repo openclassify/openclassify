a5ba580cde09b9c370045bc542d8b32bfc8e747d
Fatih Alp <mail@fatihalp.com>
2021-01-29 16:19:04 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/field.php
@@ -323,6 +323,8 @@ return [
     'upload_files' => 'Fazer upload de arquivos',
     'additional_fields' => 'Campos Adicionais',
     'ad_location' => 'Localização do anúncio',
+    'owner' => 'Owner',
+    'default_owner_instruction' => 'The owner will default to the current user if no user is selected',
 
     'free' => 'Livre',
     'ad_date' => 'Data do Anúncio',
