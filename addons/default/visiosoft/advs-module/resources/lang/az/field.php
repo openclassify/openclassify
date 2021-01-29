3a4102c206bb5a04daa81073397e19b7638fb9da
Fatih Alp <mail@fatihalp.com>
2021-01-29 16:18:47 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/field.php
@@ -323,6 +323,8 @@ return [
     'upload_files' => 'Faylları yükləyin',
     'additional_fields' => 'Əlavə sahələr',
     'ad_location' => 'Ünvan əlavə et',
+    'owner' => 'Owner',
+    'default_owner_instruction' => 'The owner will default to the current user if no user is selected',
 
     'free' => 'Pulsuz',
     'ad_date' => 'Elan Tarixi',
