f62512d9b161b4d98026c325924f42b828842812
Fatih Alp <mail@fatihalp.com>
2021-02-03 18:33:43 +0300

    New translations message.php (Bulgarian)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/bg/message.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/bg/message.php
@@ -1,13 +1,13 @@
 <?php
 
 return [
-    'choose_file' => 'Which file would you like to use?',
-    'choose_folder' => 'Which folder would you like to upload to?',
-    'upload' => 'Click or drop files here to upload.',
-    'no_file_selected' => 'No file selected.',
-    'no_uploads' => 'No files uploaded.',
-    'overwrite' => 'has already been uploaded. Would you like to overwrite it?',
-    'uploading' => 'Uploading',
-    'loading' => 'Loading',
-    'choose_file_option' => 'Choose File Option',
+    'choose_file' => 'Кой файл бихте искали да използвате?',
+    'choose_folder' => 'В коя папка искате да качите?',
+    'upload' => 'Щракнете или пуснете файлове тук, за да ги качите.',
+    'no_file_selected' => 'Не е избран файл.',
+    'no_uploads' => 'Няма качени файлове.',
+    'overwrite' => 'вече е качено. Искате ли да го презапишете?',
+    'uploading' => 'Качване',
+    'loading' => 'Зареждане',
+    'choose_file_option' => 'Изберете File Option',
 ];
