e10e8e45b9d3b9e88f93f7a37973ff80b2dcdd4f
Fatih Alp <mail@fatihalp.com>
2021-02-03 18:33:10 +0300

    New translations message.php (Bulgarian)

--- addons/default/visiosoft/location-module/resources/lang/bg/message.php
+++ addons/default/visiosoft/location-module/resources/lang/bg/message.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Type here to search',
-    'null_msg' => 'Not Found',
+    'placeholder' => 'Въведете тук, за да търсите',
+    'null_msg' => 'Не е намерен',
 ];
