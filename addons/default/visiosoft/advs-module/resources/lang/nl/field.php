1919c664601f9402541e9a6be5d12ef07ceefc56
Fatih Alp <mail@fatihalp.com>
2021-02-03 18:44:45 +0300

    New translations field.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/field.php
@@ -323,8 +323,8 @@ return [
     'upload_files' => 'Upload bestanden',
     'additional_fields' => 'Extra velden',
     'ad_location' => 'Advertentielocatie',
-    'owner' => 'Owner',
-    'default_owner_instruction' => 'The owner will default to the current user if no user is selected',
+    'owner' => 'Eigenaar',
+    'default_owner_instruction' => 'De eigenaar wordt standaard de huidige gebruiker als er geen gebruiker is geselecteerd',
 
     'free' => 'Vrij',
     'ad_date' => 'Advertentiedatum',
