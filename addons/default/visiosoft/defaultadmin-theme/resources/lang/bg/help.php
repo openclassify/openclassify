9299e51401591e9306c159811ce6c74b352802f8
Fatih Alp <mail@fatihalp.com>
2021-02-03 18:32:37 +0300

    New translations help.php (Bulgarian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/bg/help.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/bg/help.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'title'                     => 'Helpful Resources',
-    'description'               => 'Find help and discover features with these helpful resources.',
-    'documentation_link'        => 'OpenClassify Documentation',
-    'documentation_description' => 'Find documentation for OpenClassify, developing addons, and using addons too.',
-    'slack_link'                => 'Slack Team',
-    'slack_description'         => 'Find and communicate with other OpenClassify users and developers.',
-    'forum_link'                => 'Discussion Forum',
-    'forum_description'         => 'Find answers and post questions about using and developing with OpenClassify.',
-    'addons_link'               => 'Available Addons',
-    'addons_description'        => 'Discover addons available for OpenClassify.',
+    'title'                     => 'Полезни ресурси',
+    'description'               => 'Намерете помощ и открийте функции с тези полезни ресурси.',
+    'documentation_link'        => 'Документация на OpenClassify',
+    'documentation_description' => 'Намерете документация за OpenClassify, разработване на добавки и използване на добавки също.',
+    'slack_link'                => 'Слаб отбор',
+    'slack_description'         => 'Намерете и комуникирайте с други потребители и разработчици на OpenClassify.',
+    'forum_link'                => 'Дискусионен форум',
+    'forum_description'         => 'Намерете отговори и публикувайте въпроси относно използването и разработването с OpenClassify.',
+    'addons_link'               => 'Налични добавки',
+    'addons_description'        => 'Открийте добавките, налични за OpenClassify.',
 ];
