0994bcfca8a95a9bd0065078ffbebc66066b4c33
Fatih Alp <mail@fatihalp.com>
2021-02-04 10:44:59 +0300

    New translations permission.php (Indonesian)

---
+++ addons/default/visiosoft/location-module/resources/lang/id/permission.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'village' => [
+        'name'   => 'Desa',
+        'option' => [
+            'read'   => 'Bisa membaca desa?',
+            'write'  => 'Bisakah membuat / mengedit desa?',
+            'delete' => 'Bisakah menghapus desa?',
+        ],
+    ],
+];
