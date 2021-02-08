067e4e9d2dcc5e5c269273a9246bc9c863af4303
vedatakd <vedat@vebze.com>
2021-02-08 18:19:09 +0300

    filter user

--- addons/default/visiosoft/advs-module/resources/js/list.js
+++ addons/default/visiosoft/advs-module/resources/js/list.js
@@ -145,29 +145,6 @@ $(document).ready(function () {
         filter.checkUser();
     });
 
-    // User filter
-    $("select[name=filter_User]").select2({
-        placeholder: $('select[name=filter_User] option:first-child').text(),
-        ajax: {
-            url: '/api/profile/query-users',
-            dataType: 'json',
-            processResults: function (data) {
-                let formattedData = [];
-
-                Object.keys(data).forEach(function (id) {
-                    formattedData.push({
-                        'id': id,
-                        'text': data[id]
-                    })
-                })
-
-                return {
-                    results: formattedData
-                }
-            }
-        }
-    });
-
     // Country filter
     $("select[name=filter_country]").select2({
         placeholder: $('select[name=filter_country] option:first-child').text()
