4dfa0a0753423d83ddb5c3b9e208da305e5b2f87
Fatih Alp <mail@fatihalp.com>
2021-02-17 16:50:59 +0300

    New translations addon.php (Serbian (Cyrillic))

--- addons/default/visiosoft/media-field_type/resources/lang/sr/addon.php
+++ addons/default/visiosoft/media-field_type/resources/lang/sr/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Media Field Type',
-    'name'        => 'Files Field Type',
-    'description' => 'A multiple files upload field type.',
+    'title'       => 'Врста медијског поља',
+    'name'        => 'Тип поља датотека',
+    'description' => 'Тип поља за отпремање више датотека.',
 ];
