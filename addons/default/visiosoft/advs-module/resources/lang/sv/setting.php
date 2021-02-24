b6cfe9731218c7005771a26a3ab8a24fa42d7dab
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:54:40 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'Visa antal annonser',
         'instructions' => 'Visa annonser räknas på listan till vänster',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'Visa annonsannonsavtal',
         'instructions' => 'Aktivera detta om du vill visa ett integritetspolicyavtal på sidan för skapande av annonser',
