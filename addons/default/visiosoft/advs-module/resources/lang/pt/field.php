0901b7c2c06ad4a950d5252d498397a480021130
Fatih Alp <mail@fatihalp.com>
2021-02-24 12:55:08 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/field.php
@@ -280,6 +280,17 @@ return [
     'yes' => [
         'name' => 'sim'
     ],
+    'no' => [
+        'name' => 'No'
+    ],
+    'is_system' => [
+        'name' => 'Is System',
+        'instructions' => 'System status are required and should not be changed',
+    ],
+    'user_access' => [
+        'name' => 'User Access',
+        'instructions' => 'Can a user use this status on his ads?',
+    ],
     "no_location" => "Nenhum local está selecionado.",
     "continue" => 'Continuar',
     "gallery" => 'Galeria',
