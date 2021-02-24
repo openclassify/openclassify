fa103d1d45837cad132492b02a4848ced9bbb4c0
Fatih Alp <mail@fatihalp.com>
2021-02-24 12:53:12 +0300

    New translations field.php (Urdu (Pakistan))

--- addons/default/visiosoft/advs-module/resources/lang/ur/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ur/field.php
@@ -279,6 +279,17 @@ return [
     'yes' => [
         'name' => 'جی ہاں'
     ],
+    'no' => [
+        'name' => 'No'
+    ],
+    'is_system' => [
+        'name' => 'Is System',
+        'instructions' => 'System status are required and should not be changed',
+    ],
+    'user_access' => [
+        'name' => 'User Access',
+        'instructions' => 'Can a user use this status on his ads?',
+    ],
     "no_location" => "کوئی مقام منتخب نہیں ہوا ہے۔",
     "continue" => 'جاری رہے',
     "gallery" => 'گیلری',
