4edc6ebbfbbbfe1db4ed0dca64bbbb98883f17ce
Fatih Alp <mail@fatihalp.com>
2021-02-24 12:55:22 +0300

    New translations field.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/field.php
@@ -280,6 +280,17 @@ return [
     'yes' => [
         'name' => 'sì'
     ],
+    'no' => [
+        'name' => 'No'
+    ],
+    'is_system' => [
+        'name' => 'Is System',
+        'instructions' => 'System status are required and should not be changed',
+    ],
+    'user_access' => [
+        'name' => 'User Access',
+        'instructions' => 'Can a user use this status on his ads?',
+    ],
     "no_location" => "Nessuna posizione selezionata.",
     "continue" => 'Continua',
     "gallery" => 'Galleria',
