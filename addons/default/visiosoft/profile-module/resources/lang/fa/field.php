a032d9f861da7a2eb1ae0e805c60d56465e2c69f
Fatih Alp <mail@fatihalp.com>
2021-02-24 12:54:18 +0300

    New translations field.php (Persian)

--- addons/default/visiosoft/profile-module/resources/lang/fa/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/fa/field.php
@@ -467,6 +467,7 @@ return [
     'update' => 'به روز رسانی',
     'change_password' => 'تغییر رمز عبور',
     'choose_an_option' => 'گزینه ای را انتخاب کنید ...',
+    'change_status' => 'Change Status',
 
     // Register page
     'accept_terms_label' => 'من شرایط را قبول می کنم',
