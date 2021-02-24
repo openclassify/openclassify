031f253f0686bb5a8d24de3e679d9e4938852d30
Fatih Alp <mail@fatihalp.com>
2021-02-24 12:54:10 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/field.php
@@ -280,6 +280,17 @@ return [
     'yes' => [
         'name' => 'Bəli'
     ],
+    'no' => [
+        'name' => 'No'
+    ],
+    'is_system' => [
+        'name' => 'Is System',
+        'instructions' => 'System status are required and should not be changed',
+    ],
+    'user_access' => [
+        'name' => 'User Access',
+        'instructions' => 'Can a user use this status on his ads?',
+    ],
     "no_location" => "Heç bir yer seçilməyib.",
     "continue" => 'Davam et',
     "gallery" => 'Qalereya',
