c65b2ce60ec291ec210e9afd2033c9e3abfc03f6
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:55:35 +0300

    New translations setting.php (Kurdish)

--- addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'Nîşan Ads Count',
         'instructions' => 'Reklamên li ser lîsteya kategoriyên çepê hesab bikin nîşan bikin',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'Peymana Ad Ad-ê nîşan bide',
         'instructions' => 'Heke hûn dixwazin li ser rûpela afirandina reklamê peymana siyaseta nepenîtiyê nîşan bikin vê yekê vekin',
