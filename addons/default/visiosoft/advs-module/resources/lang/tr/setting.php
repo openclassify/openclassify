a6d9b4d7a20583758297ca57e57a3f759b005f26
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:54:42 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'İlan Sayısını Göster',
         'instructions' => 'Soldaki kategori listesinde ilan sayısını göster',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'İlan Sonrası Sözleşmesini Göster',
         'instructions' => 'Reklam oluşturma sayfasında bir gizlilik politikası sözleşmesi görüntülemek istiyorsanız bunu açın',
