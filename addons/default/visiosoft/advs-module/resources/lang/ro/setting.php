7f5e6df8c7503fc55e95960366abb9d42a89ee0a
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:55:11 +0300

    New translations setting.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'Afișați numărul de anunțuri',
         'instructions' => 'Afișați anunțurile în lista de categorii din stânga',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'Afișați acordul de postare a anunțului',
         'instructions' => 'Activați acest lucru dacă doriți să afișați un acord privind politica de confidențialitate pe pagina de creare a anunțului',
