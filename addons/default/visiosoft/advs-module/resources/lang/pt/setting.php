a1ff0f8eb0f91533ae81d3adfb2e56ec47009a6d
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:55:27 +0300

    New translations setting.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'Mostrar contagem de anúncios',
         'instructions' => 'Exibir contagem de anúncios na lista de categorias à esquerda',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'Mostrar Acordo de Postagem',
         'instructions' => 'Ative-o se quiser exibir um acordo de política de privacidade na página de criação de anúncios',
