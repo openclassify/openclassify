581405012ce9e059ac22ae46b3768dbbcc3df4d2
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:55:12 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'ספירת מודעות הצגה',
         'instructions' => 'הצגת מודעות נספרת ברשימת הקטגוריות השמאלית',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'הצג הסכם מודעות לפרסום',
         'instructions' => 'הפעל זאת אם ברצונך להציג הסכם מדיניות פרטיות בדף יצירת המודעות',
