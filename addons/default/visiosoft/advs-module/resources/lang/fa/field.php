10a381e8f1184886096f5ecf23b751e5b5141b38
Fatih Alp <mail@fatihalp.com>
2021-02-24 12:53:41 +0300

    New translations field.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/field.php
@@ -279,6 +279,17 @@ return [
     'yes' => [
         'name' => 'بلی'
     ],
+    'no' => [
+        'name' => 'No'
+    ],
+    'is_system' => [
+        'name' => 'Is System',
+        'instructions' => 'System status are required and should not be changed',
+    ],
+    'user_access' => [
+        'name' => 'User Access',
+        'instructions' => 'Can a user use this status on his ads?',
+    ],
     "no_location" => "هیچ موقعیتی انتخاب نشده است.",
     "continue" => 'ادامه',
     "gallery" => 'عکس ها',
