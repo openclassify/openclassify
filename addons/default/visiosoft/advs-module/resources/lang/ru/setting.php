20b0fbea576115f92339961b550bffe3abb0ca87
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:54:51 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'Показать количество объявлений',
         'instructions' => 'Показывать количество объявлений в левом списке категорий',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'Показать соглашение о публикации рекламы',
         'instructions' => 'Включите это, если вы хотите отображать соглашение о политике конфиденциальности на странице создания рекламы.',
