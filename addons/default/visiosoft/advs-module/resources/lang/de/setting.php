2dad892204bdc4701d941471a6b03919165b5288
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:55:21 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'Anzeigenanzahl anzeigen',
         'instructions' => 'Anzeigenanzahl in der linken Kategorieliste anzeigen',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'Show Post Ad Agreement anzeigen',
         'instructions' => 'Aktivieren Sie diese Option, wenn Sie auf der Seite zur Anzeigenerstellung eine Datenschutzrichtlinie anzeigen möchten',
