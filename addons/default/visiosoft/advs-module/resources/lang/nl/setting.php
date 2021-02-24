31c972a6804e4f988095b9e273b5f892f410e777
Fatih Alp <mail@fatihalp.com>
2021-02-24 10:55:38 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -203,6 +203,22 @@ return [
         'name' => 'Toon het aantal advertenties',
         'instructions' => 'Toon het aantal advertenties in de lijst met linkercategorieën',
     ],
+    'ads_id_date_hidden' => [
+        'name' => 'Hide ID and Created Date',
+        'instructions' => 'Hide ID and created date in ads detail page',
+    ],
+	'hide_listing_header' => [
+		'name' => 'Hide Listing Header',
+		'instructions' => 'Hiding to ordering and view types',
+	],
+	'hide_filter_section' => [
+		'name' => 'Hide Filter Section',
+		'instructions' => 'Hiding to left filter area in listing page',
+	],
+	'hide_seller_info' => [
+		'name' => 'Hide Seller Information',
+		'instructions' => 'Hiding to seller infos in ads detail page',
+	],
     'show_post_ad_agreement' => [
         'name' => 'Toon advertentieovereenkomst',
         'instructions' => 'Schakel dit in als u een privacybeleidsovereenkomst wilt weergeven op de pagina voor het maken van advertenties',
