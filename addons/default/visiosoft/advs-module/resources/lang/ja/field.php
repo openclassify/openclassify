41de76fac99cc04f83b23b6536532f6710eff96b
Fatih Alp <mail@fatihalp.com>
2021-02-24 12:55:35 +0300

    New translations field.php (Japanese)

--- addons/default/visiosoft/advs-module/resources/lang/ja/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ja/field.php
@@ -279,6 +279,17 @@ return [
     'yes' => [
         'name' => 'はい'
     ],
+    'no' => [
+        'name' => 'No'
+    ],
+    'is_system' => [
+        'name' => 'Is System',
+        'instructions' => 'System status are required and should not be changed',
+    ],
+    'user_access' => [
+        'name' => 'User Access',
+        'instructions' => 'Can a user use this status on his ads?',
+    ],
     "no_location" => "場所が選択されていません。",
     "continue" => '継続する',
     "gallery" => 'ギャラリー',
