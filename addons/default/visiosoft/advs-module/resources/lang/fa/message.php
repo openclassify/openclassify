271f68bcefd0d9cee9cb705cf6c2f153d4ee8721
Fatih Alp <mail@fatihalp.com>
2021-02-25 20:11:26 +0300

    New translations message.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/message.php
@@ -30,5 +30,5 @@ return [
     'approve_status_change' => "وضعیت آگهی شما روی فعال تنظیم شده است!",
     'passive_status_change' => "وضعیت آگهی شما منفعل تنظیم شده است!",
 	'sold_status_change' => "وضعیت آگهی شما به فروش رسیده است!",
-	'status_change' => "Your Ad's Status Has Been Set to :status!",
+	'status_change' => "وضعیت آگهی شما روی: status تنظیم شده است!",
 ];
