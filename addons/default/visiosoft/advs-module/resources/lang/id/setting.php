5b896c52067258bb1856135519ee4798bbd3ca05
Fatih Alp <mail@fatihalp.com>
2021-02-25 20:06:32 +0300

    New translations setting.php (Indonesian)

--- addons/default/visiosoft/advs-module/resources/lang/id/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/id/setting.php
@@ -204,20 +204,20 @@ return [
         'instructions' => 'Tampilkan jumlah iklan di daftar kategori kiri',
     ],
     'ads_id_date_hidden' => [
-        'name' => 'Hide ID and Created Date',
-        'instructions' => 'Hide ID and created date in ads detail page',
+        'name' => 'Sembunyikan ID dan Tanggal Dibuat',
+        'instructions' => 'Sembunyikan ID dan tanggal pembuatan di halaman detail iklan',
     ],
 	'hide_listing_header' => [
-		'name' => 'Hide Listing Header',
-		'instructions' => 'Hiding to ordering and view types',
+		'name' => 'Sembunyikan Judul Cantuman',
+		'instructions' => 'Menyembunyikan jenis pemesanan dan tampilan',
 	],
 	'hide_filter_section' => [
-		'name' => 'Hide Filter Section',
-		'instructions' => 'Hiding to left filter area in listing page',
+		'name' => 'Sembunyikan Bagian Filter',
+		'instructions' => 'Bersembunyi di area filter kiri di halaman daftar',
 	],
 	'hide_seller_info' => [
-		'name' => 'Hide Seller Information',
-		'instructions' => 'Hiding to seller infos in ads detail page',
+		'name' => 'Sembunyikan Informasi Penjual',
+		'instructions' => 'Menyembunyikan info penjual di halaman detail iklan',
 	],
     'show_post_ad_agreement' => [
         'name' => 'Show Post Ad Agreement',
