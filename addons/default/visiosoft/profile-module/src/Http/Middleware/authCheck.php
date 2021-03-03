887c19a12e83fcef7876400c875cc1f7f8fea089
Diatrex <diahshalabi@hotmail.com>
2021-03-03 11:31:25 +0300

    #3411 [references] Add image to share link to settings

--- addons/default/visiosoft/profile-module/src/Http/Middleware/authCheck.php
+++ addons/default/visiosoft/profile-module/src/Http/Middleware/authCheck.php
@@ -1,32 +1,40 @@
-<?php
+<?php namespace Visiosoft\ProfileModule\Http\Middleware;
 
-namespace Visiosoft\ProfileModule\Http\Middleware;
-
+use Anomaly\FilesModule\File\Command\GetFile;
+use Anomaly\Streams\Platform\View\ViewTemplate;
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
 
 class authCheck
 {
+    use DispatchesJobs;
 
     private $auth;
     private $request;
+    private $template;
 
-    public function __construct(Guard $auth,Request $request)
+    public function __construct(Guard $auth,Request $request, ViewTemplate $template)
     {
         $this->auth = $auth;
         $this->request = $request;
+        $this->template = $template;
     }
 
-    /**
-     * @param Guard $auth
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
     public function handle(Request $request, Closure $next)
     {
         if ($this->auth->check()) {
             return redirect($this->request->get('redirect', '/'));
         }
+
+        if (($ogImage = session()->get('ogImage')) && ($file = $this->dispatch(new GetFile($ogImage)))) {
+            $this->template->set(
+                'og_image',
+                $file->make()->url()
+            );
+        }
+
         return $next($request);
-                            }
+    }
 }
