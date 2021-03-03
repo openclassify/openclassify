676e47f99c53e29ce9590bd5b9761f68c8ad2a8f
Diatrex <diahshalabi@hotmail.com>
2021-03-03 16:24:00 +0300

    #3411 [references] Add image to share link to settings

--- addons/default/visiosoft/profile-module/src/Http/Middleware/authCheck.php
+++ addons/default/visiosoft/profile-module/src/Http/Middleware/authCheck.php
@@ -2,16 +2,12 @@
 
 use Closure;
 use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Http\Request;
 
 class authCheck
 {
-    use DispatchesJobs;
-
     private $auth;
     private $request;
-    private $template;
 
     public function __construct(Guard $auth,Request $request)
     {
