43c457a048f592cd7374c16a68a2cea988867354
Diatrex <diahshalabi@hotmail.com>
2021-03-04 16:27:12 +0300

    #3481 [advs-module] Currency error when viewing an ad

--- addons/default/visiosoft/advs-module/src/Http/Controller/advsController.php
+++ addons/default/visiosoft/advs-module/src/Http/Controller/advsController.php
@@ -257,7 +257,7 @@ class AdvsController extends PublicController
             $advs[$index] = $this->adv_model->AddAdsDefaultCoverImage($ad);
 
 	        $foreign_currencies = json_decode($advs[$index]->foreign_currencies, true);
-	        if ($_COOKIE['currency'] && $advs[$index]->foreign_currencies && array_key_exists($_COOKIE['currency'], $foreign_currencies)) {
+	        if (isset($_COOKIE['currency']) && $_COOKIE['currency'] && $advs[$index]->foreign_currencies && array_key_exists($_COOKIE['currency'], $foreign_currencies)) {
 		        $advs[$index]->currency = $_COOKIE['currency'];
 		        $advs[$index]->price = $foreign_currencies[$_COOKIE['currency']];
 	        }
