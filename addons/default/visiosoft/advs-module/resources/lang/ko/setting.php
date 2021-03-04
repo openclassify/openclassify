4b9c716844657e33dc6e9bb2dc3987735e20b2a4
Fatih Alp <mail@fatihalp.com>
2021-03-04 15:39:35 +0300

    New translations setting.php (Korean)

--- addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
@@ -61,6 +61,10 @@ return [
     'default_country' => [
         'name' => '기본 광고 국가',
     ],
+    'hide_price_categories' => [
+        'name' => 'Hide Price On Categories',
+        'instructions' => 'The price will be hidden when you create an ad or view an ad under these categories.'
+    ],
     'free_currencyconverterapi_key' => [
         'name' => '통화 변환기 API 키'
     ],
@@ -204,8 +208,8 @@ return [
         'instructions' => '왼쪽 카테고리 목록에 광고 개수 표시',
     ],
     'ads_date_hidden' => [
-        'name' => 'Hide Created Date',
-        'instructions' => 'Hide created date in ads detail page',
+        'name' => '만든 날짜 숨기기',
+        'instructions' => '광고 세부 정보 페이지에서 만든 날짜 숨기기',
     ],
 	'hide_listing_header' => [
 		'name' => '목록 헤더 숨기기',
