dbe9be26ae66f5e431a30822c14b9dc6d868ba84
Fatih Alp <mail@fatihalp.com>
2021-03-04 15:41:32 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/setting.php
@@ -61,6 +61,10 @@ return [
     'default_country' => [
         'name' => 'Paese annuncio predefinito',
     ],
+    'hide_price_categories' => [
+        'name' => 'Hide Price On Categories',
+        'instructions' => 'The price will be hidden when you create an ad or view an ad under these categories.'
+    ],
     'free_currencyconverterapi_key' => [
         'name' => 'Chiave API del convertitore di valuta'
     ],
@@ -204,8 +208,8 @@ return [
         'instructions' => 'Mostra il conteggio degli annunci nell\'elenco delle categorie a sinistra',
     ],
     'ads_date_hidden' => [
-        'name' => 'Hide Created Date',
-        'instructions' => 'Hide created date in ads detail page',
+        'name' => 'Nascondi data di creazione',
+        'instructions' => 'Nascondi la data di creazione nella pagina dei dettagli degli annunci',
     ],
 	'hide_listing_header' => [
 		'name' => 'Nascondi intestazione elenco',
