575a9a9dbf71f77e5dd5bf03d9d3bc44668d6cc9
Fatih Alp <mail@fatihalp.com>
2021-03-04 15:45:34 +0300

    New translations setting.php (Serbian (Cyrillic))

--- addons/default/visiosoft/advs-module/resources/lang/sr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sr/setting.php
@@ -61,6 +61,10 @@ return [
     'default_country' => [
         'name' => 'Подразумевана земља огласа',
     ],
+    'hide_price_categories' => [
+        'name' => 'Hide Price On Categories',
+        'instructions' => 'The price will be hidden when you create an ad or view an ad under these categories.'
+    ],
     'free_currencyconverterapi_key' => [
         'name' => 'АПИ кључ конвертера валута'
     ],
@@ -204,8 +208,8 @@ return [
         'instructions' => 'Прикажи огласе рачунајте на левој листи категорија',
     ],
     'ads_date_hidden' => [
-        'name' => 'Hide Created Date',
-        'instructions' => 'Hide created date in ads detail page',
+        'name' => 'Сакриј креирани датум',
+        'instructions' => 'Сакриј датум креирања на страници са детаљима огласа',
     ],
 	'hide_listing_header' => [
 		'name' => 'Сакриј заглавље уноса',
