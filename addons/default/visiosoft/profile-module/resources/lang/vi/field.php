b15885f8568812199ed8aee1ec9093a3b33f6735
Fatih Alp <mail@fatihalp.com>
2021-03-08 13:16:39 +0300

    New translations field.php (Vietnamese)

--- addons/default/visiosoft/profile-module/resources/lang/vi/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/vi/field.php
@@ -484,4 +484,14 @@ return [
 	'education_part' => [
 		'name' => 'Bang giáo dục'
 	],
+
+    'notified_new_updates' => [
+        'name' => 'Notified New Updates',
+    ],
+    'notified_about_ads' => [
+        'name' => 'Notified About Ads',
+    ],
+    'receive_messages_email' => [
+        'name' => 'Receive Messages Email',
+    ],
 ];
