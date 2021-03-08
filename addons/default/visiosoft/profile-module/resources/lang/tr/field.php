0a8a2f9694211764823581fabe26dbc79fe90beb
Fatih Alp <mail@fatihalp.com>
2021-03-08 13:16:43 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/profile-module/resources/lang/tr/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/tr/field.php
@@ -483,4 +483,14 @@ return [
 	'education_part' => [
 		'name' => 'Eğitim Durumu'
 	],
+
+    'notified_new_updates' => [
+        'name' => 'Notified New Updates',
+    ],
+    'notified_about_ads' => [
+        'name' => 'Notified About Ads',
+    ],
+    'receive_messages_email' => [
+        'name' => 'Receive Messages Email',
+    ],
 ];
