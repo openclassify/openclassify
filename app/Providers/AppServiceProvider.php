9136b20a4f70b441ae853b6286f359b4477fa83b
Muammer Top <muammer@visiosoft.com.tr>
2021-03-09 12:03:19 +0300

    #3283 added theme settings in left navigation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,12 +17,31 @@ class AppServiceProvider extends ServiceProvider
 	public function boot(ControlPanelBuilder $builder, NavigationFactory $factory)
     {
         view()->composer('*', function ($view) use ($builder, $factory) {
-            //Hidden menu items in sidebar on dashboard
+
             if (auth()->check() and template()->get('cp')) {
-                ($navigation = template()->get('cp')->getNavigation()->get('anomaly.module.variables')) ? $navigation->setClass('hidden') : false;
+	            //Hidden menu items in sidebar on dashboard
+	            ($navigation = template()->get('cp')->getNavigation()->get('anomaly.module.variables')) ? $navigation->setClass('hidden') : false;
                 ($navigation = template()->get('cp')->getNavigation()->get('anomaly.module.system')) ? $navigation->setClass('hidden') : false;
                 ($navigation = template()->get('cp')->getNavigation()->get('anomaly.module.redirects')) ? $navigation->setClass('hidden') : false;
                 ($navigation = template()->get('cp')->getNavigation()->get('anomaly.module.repeaters')) ? $navigation->setClass('hidden') : false;
+
+                // Add new menu items in sidebar on dashboard
+	            $newNavigations = [
+		            [
+			            'slug' => setting_value("streams::standard_theme"),
+			            'icon' => 'fa fa-pencil-square-o',
+			            'title' => 'visiosoft.theme.defaultadmin::section.theme_settings.name',
+			            'attributes' => [
+				            'href' => url("admin/settings/themes/" . setting_value("streams::standard_theme"))
+			            ]
+		            ]
+	            ];
+	            $cp = $builder->getControlPanel();
+	            foreach ($newNavigations as $newNavigation) {
+		            if (!template()->get('cp')->getNavigation()->get($newNavigation['slug'])){
+			            $cp->addNavigationLink($factory->make($newNavigation));
+		            }
+	            }
             }
             //Auto Language Switcher
             if (config('advs.lang_switcher_for_browser') and is_null(Request()->session()->get('_locale')) and isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
@@ -32,23 +51,6 @@ class AppServiceProvider extends ServiceProvider
                 App()->setLocale($lang);
                 Request()->session()->put('_locale', $lang);
             }
-
-	        $newNavigations = [
-		        [
-		        	'slug' => setting_value("streams::standard_theme"),
-			        'icon' => 'fa fa-pencil-square-o',
-			        'title' => 'visiosoft.theme.defaultadmin::section.theme_settings.name',
-			        'attributes' => [
-				        'href' => url("admin/settings/themes/" . setting_value("streams::standard_theme"))
-			        ]
-		        ]
-	        ];
-	        $cp = $builder->getControlPanel();
-	        foreach ($newNavigations as $newNavigation) {
-		        if (!template()->get('cp')->getNavigation()->get($newNavigation['slug'])){
-			        $cp->addNavigationLink($factory->make($newNavigation));
-		        }
-	        }
         });
     }
 
