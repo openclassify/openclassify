a4f58f0a8d4c96366c5f2db876e69204735a0ca5
vedatakd <vedat@vebze.com>
2021-03-10 11:31:23 +0300

    remove

--- addons/default/visiosoft/advs-module/src/ProductoptionsValue/Support/MultipleFieldType/SelectedTableBuilder.php
+++ addons/default/visiosoft/advs-module/src/ProductoptionsValue/Support/MultipleFieldType/SelectedTableBuilder.php
@@ -1,20 +1,7 @@
 <?php namespace Visiosoft\AdvsModule\ProductoptionsValue\Support\MultipleFieldType;
 
-/**
- * Class SelectedTableBuilder
- *
- * @link          http://pyrocms.com/
- * @author        PyroCMS, Inc. <support@pyrocms.com>
- * @author        Ryan Thompson <ryan@pyrocms.com>
- */
 class SelectedTableBuilder extends \Anomaly\MultipleFieldType\Table\SelectedTableBuilder
 {
-
-    /**
-     * The table columns.
-     *
-     * @var array
-     */
     protected $columns = [
 	    'name', 'product_option'
     ];
