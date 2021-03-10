8da58fc3d1d4773e4c778e4f370df22d7ea03ba2
Fatih Alp <mail@fatihalp.com>
2021-03-10 14:21:58 +0300

    New translations setting.php (Kurdish)

--- addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
@@ -62,8 +62,8 @@ return [
         'name' => 'Default Ad Country',
     ],
     'hide_price_categories' => [
-        'name' => 'Hide Price On Categories',
-        'instructions' => 'The price will be hidden when you create an ad or view an ad under these categories.'
+        'name' => 'Li Ser Kategoriyan Veşêre',
+        'instructions' => 'Gava ku hûn reklamek biafirînin an reklamek di bin van kategoriyan de bibînin wê bihayê veşêrin.'
     ],
     'free_currencyconverterapi_key' => [
         'name' => 'Mifteya API-ya veguherînerê Pereyê'
