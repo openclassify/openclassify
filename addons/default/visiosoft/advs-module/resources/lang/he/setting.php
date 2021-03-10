8ef2ee5750cfcc0b67afd4fea36901e6804eb4b5
Fatih Alp <mail@fatihalp.com>
2021-03-10 14:20:38 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/setting.php
@@ -62,8 +62,8 @@ return [
         'name' => 'מדינה ברירת מחדל',
     ],
     'hide_price_categories' => [
-        'name' => 'Hide Price On Categories',
-        'instructions' => 'The price will be hidden when you create an ad or view an ad under these categories.'
+        'name' => 'הסתר מחיר בקטגוריות',
+        'instructions' => 'המחיר יוסתר כאשר תיצור מודעה או תראה מודעה בקטגוריות אלה.'
     ],
     'free_currencyconverterapi_key' => [
         'name' => 'מפתח API של ממיר מטבע'
