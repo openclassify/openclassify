4add58777daa83b703b460db07ed32855db1a50f
Fatih Alp <mail@fatihalp.com>
2021-03-10 14:20:16 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/setting.php
@@ -62,8 +62,8 @@ return [
         'name' => 'Προεπιλεγμένη χώρα διαφήμισης',
     ],
     'hide_price_categories' => [
-        'name' => 'Hide Price On Categories',
-        'instructions' => 'The price will be hidden when you create an ad or view an ad under these categories.'
+        'name' => 'Απόκρυψη τιμής στις κατηγορίες',
+        'instructions' => 'Η τιμή θα είναι κρυφή όταν δημιουργείτε μια διαφήμιση ή προβάλλετε μια διαφήμιση σε αυτές τις κατηγορίες.'
     ],
     'free_currencyconverterapi_key' => [
         'name' => 'Κλειδί API μετατροπέα νομισμάτων'
