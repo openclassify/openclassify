7d214af3be60cbdc26d6099c3511f83cace1fe3d
Fatih Alp <mail@fatihalp.com>
2021-03-10 14:33:14 +0300

    New translations field.php (Ukrainian)

--- addons/default/visiosoft/profile-module/resources/lang/uk/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/uk/field.php
@@ -486,12 +486,12 @@ return [
 	],
 
     'notified_new_updates' => [
-        'name' => 'Notified New Updates',
+        'name' => 'Сповіщені нові оновлення',
     ],
     'notified_about_ads' => [
-        'name' => 'Notified About Ads',
+        'name' => 'Повідомлення про рекламу',
     ],
     'receive_messages_email' => [
-        'name' => 'Receive Messages Email',
+        'name' => 'Отримувати повідомлення електронною поштою',
     ],
 ];
