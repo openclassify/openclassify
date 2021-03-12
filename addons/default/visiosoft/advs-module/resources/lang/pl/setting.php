15515c50b74545129cdcc6d14570968be2d9b7ad
Fatih Alp <mail@fatihalp.com>
2021-03-12 14:22:10 +0300

    New translations setting.php (Polish)

--- addons/default/visiosoft/advs-module/resources/lang/pl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/pl/setting.php
@@ -247,6 +247,9 @@ return [
         'name' => 'POBIERZ kategorie',
         'instructions' => 'Reprezentuje kategorie, w których właściwość GET będzie ważna.'
     ],
+	'disable_sentry' => [
+		'name' => 'Disable Sentry'
+	],
     'watermark' => [
         'name' => 'Watermark',
         'instructions' => 'Is watermark active?'
