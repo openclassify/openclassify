38bb012abe934c2cbe9390d94a1dea3f54b1ea37
Fatih Alp <mail@fatihalp.com>
2021-03-12 14:21:49 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
@@ -247,6 +247,9 @@ return [
         'name' => 'GET Kategoritë',
         'instructions' => 'Ai përfaqëson kategoritë në të cilat prona GET do të jetë e vlefshme.'
     ],
+	'disable_sentry' => [
+		'name' => 'Disable Sentry'
+	],
     'watermark' => [
         'name' => 'Watermark',
         'instructions' => 'Is watermark active?'
