dda95127ee7da0d516e8b8f36b126de1e021ed24
Fatih Alp <mail@fatihalp.com>
2021-03-12 14:21:55 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/setting.php
@@ -247,6 +247,9 @@ return [
         'name' => 'Kateqoriyalar alın',
         'instructions' => 'GET mülkiyyətinin etibarlı olacağı kateqoriyanı təmsil edir.'
     ],
+	'disable_sentry' => [
+		'name' => 'Disable Sentry'
+	],
     'watermark' => [
         'name' => 'Watermark',
         'instructions' => 'Is watermark active?'
