85a5e0084e0b6da0af6eff031a0f2194ac19a57a
Fatih Alp <mail@fatihalp.com>
2021-03-12 14:21:51 +0300

    New translations setting.php (Indonesian)

--- addons/default/visiosoft/advs-module/resources/lang/id/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/id/setting.php
@@ -247,6 +247,9 @@ return [
         'name' => 'DAPATKAN Kategori',
         'instructions' => 'Ini mewakili kategori di mana properti GET akan berlaku.'
     ],
+	'disable_sentry' => [
+		'name' => 'Disable Sentry'
+	],
     'watermark' => [
         'name' => 'Watermark',
         'instructions' => 'Is watermark active?'
