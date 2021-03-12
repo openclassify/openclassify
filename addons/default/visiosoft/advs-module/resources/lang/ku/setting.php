b0f3c6c84b9a8d5b15e71f02a7623607568c3738
Fatih Alp <mail@fatihalp.com>
2021-03-12 14:22:12 +0300

    New translations setting.php (Kurdish)

--- addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
@@ -247,6 +247,9 @@ return [
         'name' => 'Kategoriyan bistînin',
         'instructions' => 'Ew kategoriyên ku tê de taybetmendiya GET dê derbasdar be temsîl dike.'
     ],
+	'disable_sentry' => [
+		'name' => 'Disable Sentry'
+	],
     'watermark' => [
         'name' => 'Watermark',
         'instructions' => 'Is watermark active?'
