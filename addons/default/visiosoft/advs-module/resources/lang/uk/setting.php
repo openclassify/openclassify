f3484c58b00a4d35aecb79072d353efc2c399470
Fatih Alp <mail@fatihalp.com>
2021-03-12 14:22:01 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
@@ -247,6 +247,9 @@ return [
         'name' => 'ОТРИМАТИ Категорії',
         'instructions' => 'Він представляє категорії, в яких буде дійсним властивість GET.'
     ],
+	'disable_sentry' => [
+		'name' => 'Disable Sentry'
+	],
     'watermark' => [
         'name' => 'Watermark',
         'instructions' => 'Is watermark active?'
