59e056829e71c2a851e77c6f26aa79602c490c24
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:39:26 +0300

    New translations setting.php (Czech)

---
+++ addons/default/visiosoft/cats-module/resources/lang/cs/setting.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'sitemap_dividing_number' => [
+        'name' => 'Dělicí číslo souboru Sitemap',
+    ],
+    'include_cities_sitemap' => [
+        'name' => 'Zahrnout města do souboru Sitemap',
+    ],
+];
