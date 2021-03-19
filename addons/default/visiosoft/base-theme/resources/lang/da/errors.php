f81d57eda3b13e2b73d7669123fb29dcd5c150f1
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:40:50 +0300

    New translations errors.php (Danish)

---
+++ addons/default/visiosoft/base-theme/resources/lang/da/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '404_title' => 'Ikke fundet',
+    '403_title' => 'Forbudt',
+    '404_message' => 'Den side, du ledte efter, blev ikke fundet.',
+    '403_message' => 'Adgang til disse ressourcer på webstedet nægtes!',
+    'home_page' => 'Gå til startsiden',
+];
