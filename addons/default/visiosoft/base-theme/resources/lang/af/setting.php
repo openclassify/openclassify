b756b2d5a982df1fc418dde8abe84fb850237aa1
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:35:51 +0300

    New translations setting.php (Afrikaans)

---
+++ addons/default/visiosoft/base-theme/resources/lang/af/setting.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Navigasietitel',
+    ],
+    'navigation_action' => [
+        'name' => 'Navigasie-aksie',
+    ],
+    'country_fields' => [
+        'name' => 'Landvelde',
+    ],
+    'date_fields' => [
+        'name' => 'Datumvelde',
+    ],
+    'price_fields' => [
+        'name' => 'Prysvelde',
+    ],
+    'breadcrumbs' => [
+        'name' => 'Broodkrummels',
+    ],
+    'ad_details' => [
+        'name' => 'Advertensiebesonderhede',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Advertensiebesonderhede Tab',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Nuutste en bekyk alles Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'Registreer bladsy-instruksie-logo',
+    ],
+    'register_page_alert_link' => [
+        'name' => 'Registreer bladsy-waarskuwingskakel',
+    ],
+    'style' => [
+        'name' => 'Styl (CSS)',
+    ],
+    'home_page_sub_categories_limit' => [
+        'name' => 'Tuisblad Subkategorieë Beperk',
+    ],
+	'default_country' => [
+		'name' => 'Registreer bladsy, standaardland vir telefoon',
+	]
+];
