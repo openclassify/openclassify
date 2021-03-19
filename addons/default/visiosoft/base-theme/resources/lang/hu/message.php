f536e5d493d6fae34faa4774800b90dc8bc1ab87
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:28 +0300

    New translations message.php (Hungarian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/hu/message.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'send_forgot_sms' => 'SMS-t küldünk a regisztrált telefonjára. Kérem ellenőrizze.',
+    'found_phone' => 'A rendszerben regisztrált telefonszám nem található.',
+];
