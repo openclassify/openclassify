1253db0b0ad04e70039c540314c159eaa3c7b1b4
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:34:43 +0300

    New translations section.php (Catalan)

---
+++ addons/default/visiosoft/profile-module/resources/lang/ca/section.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'Perfil',
+    ],
+    'adress' => [
+        'title' => 'Adreça',
+    ],
+    'general_setting' => 'Configuració general',
+    'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Educació',
+    ],
+    'education_part' => [
+        'title' => 'Part d’educació',
+    ],
+];
