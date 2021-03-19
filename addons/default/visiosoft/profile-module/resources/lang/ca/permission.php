81dcee47f82688590bfe76b65f18a9c324fc6a6a
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:34:41 +0300

    New translations permission.php (Catalan)

---
+++ addons/default/visiosoft/profile-module/resources/lang/ca/permission.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'profile' => [
+        'name'   => 'Perfil',
+        'option' => [
+            'read'   => 'Es pot llegir el perfil?',
+            'write'  => 'Es pot crear / editar el perfil?',
+            'delete' => 'Es pot suprimir el perfil?',
+        ],
+    ],
+    'adress' => [
+        'name'   => 'Adreça',
+        'option' => [
+            'read'   => 'Podeu llegir l\'adreça?',
+            'write'  => 'Es pot crear / editar l\'adreça?',
+            'delete' => 'Es pot suprimir l\'adreça?',
+        ],
+    ],
+    'education' => [
+        'name'   => 'Educació',
+        'option' => [
+            'read'   => 'Saps llegir l’educació?',
+            'write'  => 'Es pot crear / editar educació?',
+            'delete' => 'Es pot suprimir l\'educació?',
+        ],
+    ],
+    'education_part' => [
+        'name'   => 'Part d’educació',
+        'option' => [
+            'read'   => 'Podeu llegir part de l’educació?',
+            'write'  => 'Es pot crear / editar una part educativa?',
+            'delete' => 'Es pot suprimir la part educativa?',
+        ],
+    ],
+];
