a9e9fc528b9de6af4c04e863471fedce015d8a49
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:24 +0300

    New translations config.php (Hungarian)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/hu/config.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/hu/config.php
@@ -2,8 +2,22 @@
 
 return [
     'folders' => [
-        'name'     => 'Mappák',
-    'instructions' => 'Határozd meg az elérhető mappákat a mezőhöz. Hagyd üresen az összes mappához.',
-    'warning'      => 'A meglévő mappa engedélyek elsőbbséget élveznek a kiválasztott mappáknál.',
+        'name'         => 'Mappák',
+        'instructions' => 'Adja meg, mely mappák állnak rendelkezésre ehhez a mezőhöz. Az összes mappa megjelenítéséhez hagyja üresen.',
+        'warning'      => 'A meglévő mappaengedélyek elsőbbséget élveznek a kiválasztott mappákkal szemben.',
+    ],
+    'max'     => [
+        'name'         => 'Maximális feltöltési méret',
+        'instructions' => 'Adja meg a maximális feltöltési méretet <strong>megabájtban</strong>.',
+        'warning'      => 'Ha nincs megadva, akkor a max mappa, majd a server max lesz használva.',
+    ],
+    'mode'    => [
+        'name'         => 'Bemeneti mód',
+        'instructions' => 'Hogyan kell a felhasználóknak megadniuk a fájlbevitelt?',
+        'option'       => [
+            'default' => 'Fájlok feltöltése és / vagy kiválasztása.',
+            'select'  => 'Csak fájlokat válasszon.',
+            'upload'  => 'Csak fájlokat tölthet fel.',
+        ],
     ],
 ];
