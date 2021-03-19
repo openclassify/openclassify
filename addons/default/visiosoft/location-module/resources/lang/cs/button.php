d4cbbc6180aa50b53c16b2aa3160b1a3f7e6996a
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:40:01 +0300

    New translations button.php (Czech)

---
+++ addons/default/visiosoft/location-module/resources/lang/cs/button.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'new_country' => 'Nová země',
+    'new_city' => 'Nové Město',
+    'new_district' => 'Nová čtvrť',
+    'new_neighborhood' => 'Nové sousedství',
+    'new_village' => 'New Village',
+    'sub_cities' => 'Dílčí města',
+    'add_sub_cities' => 'Přidat dílčí město',
+    'add_sub_districts' => 'Přidat dílčí obvod',
+    'add_sub_neighborhoods' => 'Přidat sousední sousedství',
+    'add_sub_village' => 'Přidat Sub Village',
+    'sub_districts' => 'Dílčí okresy',
+    'sub_neighborhoods' => 'Sub sousedství',
+    'sub_village' => 'Sub Village',
+];
