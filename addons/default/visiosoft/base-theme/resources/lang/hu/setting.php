560a358ede09537e28580eb9cfcb6a2653cfd2bf
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:49 +0300

    New translations setting.php (Hungarian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/hu/setting.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Navigációs cím',
+    ],
+    'navigation_action' => [
+        'name' => 'Navigációs művelet',
+    ],
+    'country_fields' => [
+        'name' => 'Ország mezők',
+    ],
+    'date_fields' => [
+        'name' => 'Dátummezők',
+    ],
+    'price_fields' => [
+        'name' => 'Ármezők',
+    ],
+    'breadcrumbs' => [
+        'name' => 'Kenyérmorzsa',
+    ],
+    'ad_details' => [
+        'name' => 'Hirdetés részletei',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Hirdetés részletei lap',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Legfrissebb és az összes megtekintése Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'Regisztráljon oldal utasítás logót',
+    ],
+    'register_page_alert_link' => [
+        'name' => 'Oldal figyelmeztető link regisztrálása',
+    ],
+    'style' => [
+        'name' => 'Stílus (CSS)',
+    ],
+    'home_page_sub_categories_limit' => [
+        'name' => 'Honlap alkategóriák Limit',
+    ],
+	'default_country' => [
+		'name' => 'Regisztrációs oldal, a telefon alapértelmezett országa',
+	]
+];
