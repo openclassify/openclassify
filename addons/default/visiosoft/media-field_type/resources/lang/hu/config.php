4a49371f2cdbd0cf4601ff892bbd51b9e9d0de74
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:07 +0300

    New translations config.php (Hungarian)

--- addons/default/visiosoft/media-field_type/resources/lang/hu/config.php
+++ addons/default/visiosoft/media-field_type/resources/lang/hu/config.php
@@ -2,16 +2,25 @@
 
 return [
     'folders' => [
-        'name'     => 'Mappák',
-    'instructions' => 'Határozd meg az elérhető mappákat a mezőhöz. Hagyd üresen az összes mappához.',
-    'warning'      => 'A meglévő mappa engedélyek elsőbbséget élveznek a kiválasztott mappáknál.',
+        'name'         => 'Mappák',
+        'instructions' => 'Adja meg, mely mappák állnak rendelkezésre ehhez a mezőhöz. Az összes mappa megjelenítéséhez hagyja üresen.',
+        'warning'      => 'A meglévő mappaengedélyek elsőbbséget élveznek a kiválasztott mappákkal szemben.',
     ],
-    'min' => [
-        'label'    => 'Minimális Kiválasztás',
-    'instructions' => 'Határozd meg a minimálisan kiválasztható elemek számát.',
+    'min'     => [
+        'label'        => 'Minimális választások',
+        'instructions' => 'Adja meg a megengedett kiválasztások minimális számát.',
     ],
-    'max' => [
-        'label'    => 'Minimális Kiválasztás',
-    'instructions' => 'Határozd meg a maximálisan kiválasztható elemek számát.',
+    'max'     => [
+        'label'        => 'Maximális választási lehetőségek',
+        'instructions' => 'Adja meg a megengedett kiválasztások maximális számát.',
+    ],
+    'mode'    => [
+        'name'         => 'Bemeneti mód',
+        'instructions' => 'Hogyan kell a felhasználóknak megadniuk a fájlbevitelt?',
+        'option'       => [
+            'default' => 'Fájlok feltöltése és / vagy kiválasztása.',
+            'select'  => 'Csak fájlokat válasszon.',
+            'upload'  => 'Csak fájlokat tölthet fel.',
+        ],
     ],
 ];
