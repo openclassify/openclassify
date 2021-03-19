a3176aade4922c881603cf2b363ffe7389b546aa
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:40:08 +0300

    New translations section.php (Czech)

---
+++ addons/default/visiosoft/profile-module/resources/lang/cs/section.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'Profil',
+    ],
+    'adress' => [
+        'title' => 'Adresa',
+    ],
+    'general_setting' => 'Obecné nastavení',
+    'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Vzdělání',
+    ],
+    'education_part' => [
+        'title' => 'Vzdělávací část',
+    ],
+];
