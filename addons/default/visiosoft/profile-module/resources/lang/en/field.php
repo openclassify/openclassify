df8bb05f1be71c5e35a9cf1db43df8ec33e41222
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:43:25 +0300

    New translations field.php (English)

--- addons/default/visiosoft/profile-module/resources/lang/en/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/en/field.php
@@ -468,6 +468,7 @@ return [
     'update' => 'Update',
     'change_password' => 'Change Password',
     'choose_an_option' => 'Choose an option...',
+    'change_status' => 'Change Status',
 
     // Register page
     'accept_terms_label' => 'I accept the terms.',
@@ -483,4 +484,14 @@ return [
 	'education_part' => [
 		'name' => 'State of Education'
 	],
+
+    'notified_new_updates' => [
+        'name' => 'Notified New Updates',
+    ],
+    'notified_about_ads' => [
+        'name' => 'Notified About Ads',
+    ],
+    'receive_messages_email' => [
+        'name' => 'Receive Messages Email',
+    ],
 ];
