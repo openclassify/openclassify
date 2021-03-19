6a0343e771c44dab830654dfcf29eca39ddd0419
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:44:52 +0300

    New translations button.php (Chinese Simplified)

--- addons/default/visiosoft/profile-module/resources/lang/zh/button.php
+++ addons/default/visiosoft/profile-module/resources/lang/zh/button.php
@@ -9,7 +9,13 @@ return [
     'delete' => '删除',
     'extend' => '延伸',
     'extend_all' => '全部延伸',
+    'extend_unpublished' => '扩展未发布',
     'edit' => '编辑',
     'go_profile' => '转到个人资料详细信息',
     'go_user' => '转到用户详细信息',
+    'personal' => '个人的',
+    'corporate' => '公司的',
+    'export' => '出口',
+    'new_education' => '新教育',
+    'new_education_part' => '新教育部分',
 ];
