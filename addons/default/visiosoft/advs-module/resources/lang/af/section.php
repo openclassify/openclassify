4d20a35b58ccd1d04c58f9af4e4024fed60701f7
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:35:57 +0300

    New translations section.php (Afrikaans)

---
+++ addons/default/visiosoft/advs-module/resources/lang/af/section.php
@@ -0,0 +1,64 @@
+<?php
+
+return [
+    'advs' => [
+        'title' => 'Advertensies',
+    ],
+    'categories' => [
+        'title' => 'Kategorieë',
+    ],
+    'countries' => [
+        'title' => 'Lande',
+    ],
+    'cities' => [
+        'title' => 'Stede',
+    ],
+    'districts' => [
+        'title' => 'Distrikte',
+    ],
+    'neighborhoods' => [
+        'title' => 'Wyk',
+    ],
+    'village' => [
+        'title' => 'Dorp',
+    ],
+    'custom_fields' => [
+        'title' => 'Pasgemaakte velde',
+    ],
+    'custom_field_advs' => [
+        'title' => 'Pasgemaakte veldadvertensies',
+    ],
+    'cf_values' => [
+        'title' => 'Cf waardes',
+    ],
+    'fields' => [
+        'title' => 'Pasgemaakte velde'
+    ],
+    'assets_clear' => [
+        'name' => 'Bates duidelik'
+    ],
+    'general' => 'Generaal',
+    'ads' => 'Advertensies',
+    'user' => 'Gebruiker',
+    'filter' => 'Filtreer',
+    'create_ad' => 'Skep advertensie',
+    'options' => [
+        'title' => 'Opsies',
+    ],
+    'ads_image' => 'Advertensieprent',
+    'product_options' => [
+        'title' => 'Opsies',
+    ],
+    'productoptions_value' => [
+        'title' => 'Opsies Waarde',
+    ],
+    'option_configuration' => [
+        'title' => 'Konfigurasie',
+    ],
+    'translations' => 'Vertalings',
+    'status' => [
+        'title' => 'Status',
+    ],
+	'ads_detail' => 'Advertensiebesonderhede',
+
+];
