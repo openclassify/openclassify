c77c8f5be56883b885dab3826221dfaa40c5d7ab
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:33:08 +0300

    New translations config.php (Afrikaans)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/af/config.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'folders' => [
+        'name'         => 'Gidse',
+        'instructions' => 'Spesifiseer watter vouers vir hierdie veld beskikbaar is. Laat leeg om alle vouers te vertoon.',
+        'warning'      => 'Bestaande vouertoestemmings geniet voorkeur bo geselekteerde vouers.',
+    ],
+    'min'     => [
+        'label'        => 'Minimum keuses',
+        'instructions' => 'Voer die minimum aantal toegelate keuses in.',
+    ],
+    'max'     => [
+        'label'        => 'Maksimum keuses',
+        'instructions' => 'Voer die maksimum aantal toegelate keuses in.',
+    ],
+    'mode'    => [
+        'name'         => 'Invoermodus',
+        'instructions' => 'Hoe moet gebruikers lêerinvoer verskaf?',
+        'option'       => [
+            'default' => 'Laai lêers op en / of kies dit.',
+            'select'  => 'Kies slegs lêers.',
+            'upload'  => 'Laai slegs lêers op.',
+        ],
+    ],
+];
