e0ff4571e9ceb2c6fc0d87714195e5e463ae411e
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:52 +0300

    New translations field.php (Hungarian)

---
+++ addons/default/visiosoft/cats-module/resources/lang/hu/field.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Cím',
+        'instructions' => 'Ez a fő kategória neve, amelyet meghatározni fog. Ez lehetővé teszi a felhasználó számára, hogy könnyedén megtalálja a keresett terméket.',
+    ],
+    'order' => [
+        'name' => 'Rendelés',
+    ],
+
+    'slug' => [
+        'name' => 'Meztelen csiga',
+        'instructions' => 'Adjon meg szervezeti címkéket, amelyek segítenek csoportosítani a bejegyzését másokkal.',
+
+    ],
+    'files' => [
+        'name' => 'Kép',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo Kulcsszó',
+        'instructions' => 'A webhelyre a Google böngészőben hozzáadott szavak, amelyek a keresési eredmények felett jelennek meg.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Leírás',
+        'instructions' => 'Ez befolyásolhatja a látogatók döntését arról, hogy rákattintanak-e a keresési eredmények tartalmára.',
+    ],
+    'icon' => [
+        'name' => 'Ikon',
+        'instructions' => 'A kategória típusát jelző ikonok hozzáadására szolgál.',
+    ],
+
+    'please_wait' => 'Kérjük, várjon. Alkategóriák törlése',
+    'category_selection' => 'Kategória kiválasztása',
+    'go_to_parent' => 'Menjen a Szülőhöz',
+];
