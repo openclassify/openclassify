9c7b3d5a8711a64ac6b0dd36b15f276711f4b276
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:55:33 +0300

    New translations permission.php (Portuguese, Brazilian)

--- addons/default/visiosoft/cats-module/resources/lang/pt/permission.php
+++ addons/default/visiosoft/cats-module/resources/lang/pt/permission.php
@@ -4,17 +4,17 @@ return [
     'category' => [
         'name'   => 'Categoria',
         'option' => [
-            'read'   => 'Pode ler categoria?        ',
-            'write'  => 'Pode criar / editar categoria?        ',
-            'delete' => 'Pode excluir categoria?',
+            'read'   => 'Pode ler a categoria?',
+            'write'  => 'Pode criar / editar categoria?',
+            'delete' => 'Pode excluir a categoria?',
         ],
     ],
     'placeholderforsearch' => [
-        'name'   => 'Placeholderforsearch',
+        'name'   => 'Espaço reservado para pesquisa',
         'option' => [
-            'read'   => 'Pode ler o placeholderforsearch?        ',
-            'write'  => 'Pode criar / editar placeholderforsearch?        ',
-            'delete' => 'Pode excluir o placeholderforsearch?',
+            'read'   => 'Pode ler placeholderforsearch?',
+            'write'  => 'Pode criar / editar espaço reservado para pesquisa?',
+            'delete' => 'Pode excluir placeholderforsearch?',
         ],
     ],
 ];
