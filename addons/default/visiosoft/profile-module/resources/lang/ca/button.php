cd10cb9915e0cee257d37104cf8a5b39a31f88c2
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:34:37 +0300

    New translations button.php (Catalan)

---
+++ addons/default/visiosoft/profile-module/resources/lang/ca/button.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'new_profile' => 'Perfil nou',
+    'new_adress' => 'Nova adreça',
+    'show' => 'Mostra l’adreça',
+    'update_password' => 'Actualitza la contrasenya',
+    'update_profile' => 'Actualitza el perfil',
+    'delete' => 'Suprimeix',
+    'extend' => 'Estendre',
+    'extend_all' => 'Amplieu-ho tot',
+    'extend_unpublished' => 'Amplia sense publicar',
+    'edit' => 'Edita',
+    'go_profile' => 'Aneu a Detall del perfil',
+    'go_user' => 'Aneu a Detalls de l\'usuari',
+    'personal' => 'Personal',
+    'corporate' => 'Corporatiu',
+    'export' => 'Exporta',
+    'new_education' => 'Nova educació',
+    'new_education_part' => 'Nova part d’Educació',
+];
