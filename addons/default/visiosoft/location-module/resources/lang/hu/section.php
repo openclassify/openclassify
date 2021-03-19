bcd17be46b68330e2bd483fee45d16a43698e7f2
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:39:13 +0300

    New translations section.php (Hungarian)

---
+++ addons/default/visiosoft/location-module/resources/lang/hu/section.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'countries' => [
+        'title' => 'Országok',
+    ],
+    'cities' => [
+        'title' => 'Városok',
+    ],
+    'districts' => [
+        'title' => 'Kerületek',
+    ],
+    'neighborhoods' => [
+        'title' => 'Környékek',
+    ],
+    'village' => [
+        'title' => 'Falu',
+    ],
+    'general' => 'Tábornok',
+    'map' => 'Térkép',
+    'setting' => 'Beállítás',
+    'filter' => 'Szűrő',
+];
