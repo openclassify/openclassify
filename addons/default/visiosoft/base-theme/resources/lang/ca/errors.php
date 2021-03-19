967f3eef3e416fb503850f24359ee3e8381ebf61
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:33:46 +0300

    New translations errors.php (Catalan)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ca/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '404_title' => 'No trobat',
+    '403_title' => 'Prohibit',
+    '404_message' => 'No s\'ha trobat la pàgina que cercàveu.',
+    '403_message' => 'Es denega l\'accés a aquests recursos del lloc.',
+    'home_page' => 'Aneu a la pàgina inicial',
+];
