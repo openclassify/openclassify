7e0cf10f985a7094c807bd475f2af15d1b0c1c3e
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:39:34 +0300

    New translations stream.php (Czech)

---
+++ addons/default/visiosoft/profile-module/resources/lang/cs/stream.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'profile' => [
+        'name' => 'Profil',
+    ],
+    'adress' => [
+        'name' => 'Adresa',
+    ],
+    'education' => [
+        'name' => 'Vzdělání',
+    ],
+    'education_part' => [
+        'name' => 'Vzdělávací část',
+    ],
+];
