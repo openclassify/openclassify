fc5ad929b23018262dfa53f9a6e14c3ea96ce19c
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:33:18 +0300

    New translations message.php (Afrikaans)

---
+++ addons/default/visiosoft/profile-module/resources/lang/af/message.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    "email"      => "E-pos veld is verpligtend!",
+    "username"      => "Gebruikersnaamveld is verpligtend!",
+    "success_update"      => "Profielopdatering suksesvol!",
+    "adress_success_update"      => "Adresopdatering suksesvol!",
+    "adress_success_create"      => "Adres Skep suksesvol!",
+    "login_error" => "Verkeerde aanmelding of wagwoord.",
+    "login_noMail_old_user" => "Die e-posadres wat u ingevoer het, kon nie gevind word nie.",
+    "login_noMail_old_user2" => "Gaan asseblief die e-posadres na en probeer weer.",
+    "login_noMail_old_user3" => "As die probleem voortduur, kontak asseblief 'info@openclassify.com'.",
+    "login_info_old_user" => "'N Nuwe wagwoord is na u e-posadres gestuur.",
+    "success" => "Sukses",
+    "notified_new_updates" => "Ek wil in kennis gestel word van nuwe opdaterings",
+    "notified_about_ads" => "Laat kennisgewings oor my advertensies toe",
+    "receive_messages_email" => "Ek wil boodskappe per e-pos ontvang",
+    "no_packages_module" => "Geen pakketmodule nie!",
+    "required_add"=> "Vul asseblief alle vereiste lyne in.",
+    "no_extend_package" => "Geen verleng advertensie pakkette nie",
+    "saved" => "Gestoor!",
+    'please_confirm_transaction' => 'Bevestig asseblief die transaksie',
+    'password_do_not_match' => 'Wagwoorde stem nie ooreen nie',
+    'your_password_changed' => 'U wagwoord is suksesvol opgedateer',
+    'error_valid_email_or_phone' => 'Telefoonnommer of e-posadresformaat is nie korrek nie.',
+    'error_valid_phone' => 'Telefoonnommer-formaat is nie korrek nie.',
+    'registered_phone' => 'Hierdie telefoonnommer is reeds geregistreer.',
+    'ajax_address_error' => 'Geen adres of magtig om nie te besigtig nie.',
+    'empty_password_sms_message' => 'As gevolg van veiligheidskwessies het ons u wagwoord verander! U nuwe wagwoord is:',
+    'required_all' => "Alle veld is verpligtend!",
+    'can_not_remove_filled_fields' => "U kan nie gevulde velde verwyder nie!",
+    'wrong_password' => "Die wagwoord wat u ingevoer het, is nie korrek nie!",
+
+    // Forgot Password
+    'email_phone_not_found' => 'Die e-pos, telefoonnommer is nie korrek nie!',
+
+    'disable_account' => 'Wil u u rekening sluit? Hierdie bewerking kan nie ontdoen word nie.',
+
+    // Mail
+    'update_email_mail_subject' => 'E-pos opgedateer!',
+    'update_email_mail_message' => 'U e-pos is opgedateer!',
+
+    // Register
+    'recaptcha_field_is_required' => 'reCaptcha-veld is verpligtend.',
+    'failed_to_validate_captcha' => 'Kon captcha nie bekragtig nie.',
+];
