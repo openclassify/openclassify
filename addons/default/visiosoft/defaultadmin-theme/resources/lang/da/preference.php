7cd24f937fc101a978d2cb15c9a777fe2b0964c6
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:41:15 +0300

    New translations preference.php (Danish)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/da/preference.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Sidepanel svæver',
+        'instructions' => 'Udvide sidepanelet, når du holder markøren?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigation',
+        'instructions' => 'Angiv din <em>personlige</em> rækkefølge for navigation.',
+        'warning'      => 'Det første tilgængelige navigationselement bruges som dit <strong>hjem</strong> område.',
+        'reorder'      => 'Træk og slip de primære navigations elementer i <strong>sidebar</strong> for at omorganisere dem.',
+    ],
+    'display'       => [
+        'name'         => 'Vis tæthed',
+        'instructions' => 'Kompakt visning gør det muligt at vise mere indhold på skærmen på én gang.',
+        'option'       => [
+            'default' => 'Standard',
+            'compact' => 'Kompakt',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => 'Sidebar-tilstand',
+        'instructions' => 'Statiske sidebjælker vil altid være synlige.',
+        'option'       => [
+            'default' => 'Standard',
+            'static'  => 'Statisk',
+        ],
+    ],
+];
