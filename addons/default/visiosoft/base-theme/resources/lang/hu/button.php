1c6193cffba5e848f025b30f79d156d3b96c468b
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:45 +0300

    New translations button.php (Hungarian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/hu/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'post_ad' => [
+        'name' => 'Feladás hirdetése',
+    ],
+    'login' => 'Belépés',
+    'register' => 'Regisztráció',
+    'continue' => 'Folytatni',
+    'reset_password' => 'Jelszó visszaállítása',
+    'email' => 'Email',
+    'phone' => 'Telefon',
+    'show_all' => 'Mutasd az összeset',
+    'hide_all' => 'Rejtsd el mindet',
+];
