24e67b46e6c5f5f6b75d5d861164b5bcf6f10d4e
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:44:21 +0300

    New translations button.php (Norwegian)

---
+++ addons/default/visiosoft/profile-module/resources/lang/no/button.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'new_profile' => 'Ny profil',
+    'new_adress' => 'Ny adresse',
+    'show' => 'Vis adresse',
+    'update_password' => 'Oppdater passord',
+    'update_profile' => 'Oppdater profil',
+    'delete' => 'Slett',
+    'extend' => 'Forlenge',
+    'extend_all' => 'Utvid alle',
+    'extend_unpublished' => 'Utvid upublisert',
+    'edit' => 'Redigere',
+    'go_profile' => 'Gå til Profildetaljer',
+    'go_user' => 'Gå til brukerdetaljer',
+    'personal' => 'Personlig',
+    'corporate' => 'Bedrift',
+    'export' => 'Eksport',
+    'new_education' => 'Ny utdanning',
+    'new_education_part' => 'Ny utdanning del',
+];
