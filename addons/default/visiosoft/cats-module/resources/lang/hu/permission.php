17b8688f416137ce98436ee651d3ee026497cf71
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:53 +0300

    New translations permission.php (Hungarian)

---
+++ addons/default/visiosoft/cats-module/resources/lang/hu/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'category' => [
+        'name'   => 'Kategória',
+        'option' => [
+            'read'   => 'Tud olvasni kategóriát?',
+            'write'  => 'Létrehozhat / szerkeszthet kategóriát?',
+            'delete' => 'Törölheti a kategóriát?',
+        ],
+    ],
+    'placeholderforsearch' => [
+        'name'   => 'Helyőrző keresés',
+        'option' => [
+            'read'   => 'Tud olvasni helyőrző keresést?',
+            'write'  => 'Létrehozhat / szerkeszthet helyőrző keresést?',
+            'delete' => 'Törölheti a helyőrző keresést?',
+        ],
+    ],
+];
