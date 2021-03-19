dbe5c90c63069475eef43126d1925e6d3011eec3
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:40:17 +0300

    New translations validation.php (Czech)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/cs/validation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'valid_disk' => 'Konfigurovaný disk pro nahrávání pro: atribut neexistuje.',
+	'unsupported' => 'Tato oblast je pouze pro obrazové soubory. Do pole pro popis můžete přidat různé typy souborů.'
+];
