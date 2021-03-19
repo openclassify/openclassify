6a56b61e5f1937d1d038dddeeb1c0b4d46df2795
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:36:05 +0300

    New translations field.php (Afrikaans)

---
+++ addons/default/visiosoft/base-theme/resources/lang/af/field.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    'latest_ads' => 'Nuutste advertensies',
+    'show_all' => 'Wys almal',
+    'login' => [
+        'name' => 'Teken aan',
+    ],
+    'logout' => [
+        'name' => 'Teken uit',
+    ],
+    'profile' => 'Profiel',
+    'email' => 'E-pos adres',
+    'phone' => 'Telefoon nommer',
+    'reset_code' => 'Stel kode terug',
+    'password_confirmation' => 'Wagwoord bevestiging',
+    'password' => 'Wagwoord',
+    'first_name' => 'Eerste naam',
+    'last_name' => 'Van',
+    'remember_me' => 'Onthou my',
+    'not_a_member_yet' => 'Nog nie \'n lid nie',
+    'not_a_member_yet_message' => 'Teken in vir ons lede om voordeel te trek uit ons spesiale diens',
+    'forgot_password' => 'Ek het my wagwoord vergeet',
+    'email_or_phone_number' => 'E-posadres of telefoonnommer',
+    'phone_number' => 'Telefoon nommer',
+
+    // Registration instructions
+    'personal_registration_header' => 'Wat is die voordele vir individuele lidmaatskap?',
+    'personal_registration_body' => 'Verkoop en huur u huis, motor, verkoop u ongebruikte items, kry nuwe.',
+    'personal_registration_list_1' => 'Plaas \'n gratis advertensie,',
+    'personal_registration_list_2' => 'Voeg die advertensies by waarin u belangstel, volg die prysveranderings nadat u dit by u gunstelinge gevoeg het, skep gunsteling soektogte wat aan u kriteria voldoen,',
+    'personal_registration_list_3' => 'Stuur boodskappe aan die advertensie-eienaars op die webwerf.',
+    'register_information_note' => 'Die inligting op hierdie bladsy word gebruik vir ' . env('APPLICATION_DOMAIN') . ' lidmaatskap. U kan gedetailleerde inligting oor die beskerming van persoonlike data hier vind.',
+
+    // Register page
+    'phone_validation_error' => 'Hierdie telefoonnommer word deur \'n ander lid gebruik.',
+
+    // Forgot Password
+    'create_new_password' => 'Skep nuwe wagwoord',
+
+    // Login page
+    'or' => 'of',
+    'login_with_phone_number' => 'Teken in met telefoonnommer',
+    'login_with_email_address' => 'Teken in met e-posadres',
+
+    // Side menu links
+    'company_directory' => 'Maatskappy gids',
+    'popular_ads' => 'Gewilde advertensies',
+    'last_48_hours' => 'Laaste 48 uur',
+    'secure_e-commerce_ads' => 'Veilige e-handelsadvertensies',
+    'sms' => 'Stuur teksboodskap (SMS)',
+    'mail' => 'Stuur pos',
+
+    // Register instruction seed
+    'register_instructions' => 'Registreer instruksies',
+    'list' => 'Lys',
+    'instruction_description' => 'Instruksie Beskrywing',
+    'instruction_list' => 'Instruksielys',
+];
