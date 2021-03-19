24908fab0cc9347c0fe7393bbe86bede84ef4f9e
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:54:08 +0300

    New translations setting.php (Norwegian)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/no/setting.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Sidepanel Hover',
+        'instructions' => 'Utvide sidefeltet når du holder markøren?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigasjon',
+        'instructions' => 'Spesifiser <em>standard</em> rekkefølge for navigering.',
+        'warning'      => 'Det første tilgjengelige navigasjonselementet brukes som <strong>hjem</strong> område.',
+        'reorder'      => 'Dra og slipp de primære navigasjons elementene i <strong>sidebar</strong> å endre rekkefølgen på dem.',
+    ],
+    "login_icon" => [
+        'name' => 'Påloggingsikon',
+    ],
+    "panel_icon" => [
+        'name' => 'Panelikon',
+    ],
+    "title" => [
+        'name' => 'Tittel',
+    ],
+    "footer_copyright_org_name" => [
+        'name' => 'Bunntekst Opphavsrettsorganisasjonsnavn',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidefelt Hovedfarge',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Sekundær farge',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Aktiv sidefelt kantfarge',
+    ]
+];
