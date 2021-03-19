d722559b5d3c159ce13b95ce62dbbdd7430164fa
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:41:24 +0300

    New translations permission.php (Danish)

---
+++ addons/default/visiosoft/location-module/resources/lang/da/permission.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'village' => [
+        'name'   => 'Landsby',
+        'option' => [
+            'read'   => 'Kan læse landsby?',
+            'write'  => 'Kan du oprette / redigere landsby?',
+            'delete' => 'Kan du slette landsby?',
+        ],
+    ],
+];
