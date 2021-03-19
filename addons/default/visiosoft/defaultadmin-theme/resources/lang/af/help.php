2d68df0f526770758f58fb3a9906412bcb86dba4
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:36:20 +0300

    New translations help.php (Afrikaans)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/af/help.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'                     => 'Nuttige hulpbronne',
+    'description'               => 'Vind hulp en ontdek funksies met hierdie nuttige hulpbronne.',
+    'documentation_link'        => 'OpenClassify-dokumentasie',
+    'documentation_description' => 'Soek dokumentasie vir OpenClassify, ontwikkel byvoegings en gebruik ook byvoegings.',
+    'slack_link'                => 'Slap span',
+    'slack_description'         => 'Soek en kommunikeer met ander OpenClassify-gebruikers en ontwikkelaars.',
+    'forum_link'                => 'Besprekingsforum',
+    'forum_description'         => 'Vind antwoorde en plaas vrae oor die gebruik en ontwikkeling met OpenClassify.',
+    'addons_link'               => 'Beskikbare byvoegings',
+    'addons_description'        => 'Ontdek byvoegings wat beskikbaar is vir OpenClassify.',
+];
