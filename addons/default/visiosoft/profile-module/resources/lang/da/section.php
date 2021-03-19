9cb29ba58e0dd9988d917cbc1d6c6548e3e44b53
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:40:30 +0300

    New translations section.php (Danish)

---
+++ addons/default/visiosoft/profile-module/resources/lang/da/section.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'profile' => [
+        'title' => 'Profil',
+    ],
+    'adress' => [
+        'title' => 'Adresse',
+    ],
+    'general_setting' => 'Generelle indstillinger',
+    'recaptcha' => 'reCAPTCHA',
+    'education' => [
+        'title' => 'Uddannelse',
+    ],
+    'education_part' => [
+        'title' => 'Uddannelsesdel',
+    ],
+];
