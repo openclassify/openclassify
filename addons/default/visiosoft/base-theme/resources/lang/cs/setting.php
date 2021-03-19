51507a932ef9efd4136db0b0803994f7b74ee993
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:34:04 +0300

    New translations setting.php (Czech)

---
+++ addons/default/visiosoft/base-theme/resources/lang/cs/setting.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'navigation_title' => [
+        'name' => 'Název navigace',
+    ],
+    'navigation_action' => [
+        'name' => 'Akce navigace',
+    ],
+    'country_fields' => [
+        'name' => 'Země pole',
+    ],
+    'date_fields' => [
+        'name' => 'Pole data',
+    ],
+    'price_fields' => [
+        'name' => 'Cenová pole',
+    ],
+    'breadcrumbs' => [
+        'name' => 'Strouhanka',
+    ],
+    'ad_details' => [
+        'name' => 'Podrobnosti o reklamě',
+    ],
+    'ad_details_tab' => [
+        'name' => 'Karta Podrobnosti reklamy',
+    ],
+    'latest_and_view_all_btn' => [
+        'name' => 'Nejnovější a Zobrazit vše Btn',
+    ],
+    'register_page_instruction_logo' => [
+        'name' => 'Zaregistrujte si logo instrukce pro stránku',
+    ],
+    'register_page_alert_link' => [
+        'name' => 'Zaregistrovat odkaz na upozornění na stránku',
+    ],
+    'style' => [
+        'name' => 'Styl (CSS)',
+    ],
+    'home_page_sub_categories_limit' => [
+        'name' => 'Limit podkategorií domovské stránky',
+    ],
+	'default_country' => [
+		'name' => 'Stránka registrace, výchozí země pro telefon',
+	]
+];
