5c86d6cb5f28024ed658a0af664d34bfeb308b08
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:40:16 +0300

    New translations message.php (Czech)

---
+++ addons/default/visiosoft/media-field_type/resources/lang/cs/message.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'choose_folder' => 'Do které složky chcete nahrát?',
+    'upload' => 'Přidat obrázky | Nahrát obrázky | Připojte fotografii',
+    'choose_files' => 'Které soubory byste chtěli použít?',
+    'no_files_selected' => 'Nejsou vybrány žádné soubory.',
+    'no_uploads' => 'Nebyly nahrány žádné soubory.',
+    'overwrite' => 'již bylo nahráno. Chcete jej přepsat?',
+    'uploading' => 'Nahrávám',
+    'loading' => 'načítání',
+    'error_upload' => 'Při nahrávání souboru došlo k problému.',
+    'error_upload_docs' => 'Neexistuje žádná podpora nahrávání dokumentů. Kontaktujte svého správce systému!',
+];
