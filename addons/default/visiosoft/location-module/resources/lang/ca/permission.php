07a61dafd46123c53685a23d911f5be8a063162d
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:34:22 +0300

    New translations permission.php (Catalan)

---
+++ addons/default/visiosoft/location-module/resources/lang/ca/permission.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'village' => [
+        'name'   => 'Poble',
+        'option' => [
+            'read'   => 'Sabeu llegir poble?',
+            'write'  => 'Es pot crear / editar un poble?',
+            'delete' => 'Es pot suprimir el poble?',
+        ],
+    ],
+];
