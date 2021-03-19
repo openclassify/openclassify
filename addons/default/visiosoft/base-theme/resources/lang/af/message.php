f377a3eaf1a2cd9b6bc1cad0500532ad8e06ad78
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:33:15 +0300

    New translations message.php (Afrikaans)

---
+++ addons/default/visiosoft/base-theme/resources/lang/af/message.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'send_forgot_sms' => 'SMS gestuur na u geregistreerde foon. Kyk asb.',
+    'found_phone' => 'Die telefoonnommer wat in die stelsel geregistreer is, is nie gevind nie.',
+];
