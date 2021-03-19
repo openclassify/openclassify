676d80453e84259b4afd15aa08aa781c5405b963
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:36:22 +0300

    New translations setting.php (Afrikaans)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/af/setting.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Sidebar Hover',
+        'instructions' => 'Brei die sybalk uit as jy dit hou?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigasie',
+        'instructions' => 'Spesifiseer die <em>standaard</em> volgorde van navigasie.',
+        'warning'      => 'Die eerste toeganklike navigasie-item word gebruik as die <strong>huis</strong> gebied.',
+        'reorder'      => 'Sleep en sleep die primêre navigasie-items in die <strong>sybalk</strong> om dit te herorden.',
+    ],
+    "login_icon" => [
+        'name' => 'Aanmeldingsikoon',
+    ],
+    "panel_icon" => [
+        'name' => 'Paneelikoon',
+    ],
+    "title" => [
+        'name' => 'Titel',
+    ],
+    "footer_copyright_org_name" => [
+        'name' => 'Voetskrif Kopiereg Organisasie Naam',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Sidebar Hoofkleur',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sidebar Sekondêre Kleur',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Aktiewe sybalkantkleur',
+    ]
+];
