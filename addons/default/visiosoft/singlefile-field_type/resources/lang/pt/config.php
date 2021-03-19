11651ffd15201192e4f9e92de4ba4c30a4a367f9
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:55:40 +0300

    New translations config.php (Portuguese, Brazilian)

--- addons/default/visiosoft/singlefile-field_type/resources/lang/pt/config.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/pt/config.php
@@ -9,15 +9,15 @@ return [
     'max'     => [
         'name'         => 'Tamanho máximo de upload',
         'instructions' => 'Especifique o tamanho máximo de upload em <strong>megabytes</strong>.',
-        'warning'      => 'Se não for especificada, a pasta max e o servidor max serão usados.',
+        'warning'      => 'Se não for especificado, a pasta max e o server max serão usados.',
     ],
     'mode'    => [
         'name'         => 'Modo de entrada',
         'instructions' => 'Como os usuários devem fornecer entrada de arquivo?',
         'option'       => [
-            'default' => 'Carregar e / ou selecionar arquivos.',
+            'default' => 'Faça upload e / ou selecione arquivos.',
             'select'  => 'Selecione apenas arquivos.',
-            'upload'  => 'Carregar apenas arquivos.',
+            'upload'  => 'Faça upload de arquivos apenas.',
         ],
     ],
 ];
