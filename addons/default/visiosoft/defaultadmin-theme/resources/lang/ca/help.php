a3b4c20f91d3ed0b6a62c12920f6f28a825c786c
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:33:34 +0300

    New translations help.php (Catalan)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ca/help.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'title'                     => 'Recursos útils',
+    'description'               => 'Trobeu ajuda i descobriu funcions amb aquests recursos útils.',
+    'documentation_link'        => 'Documentació OpenClassify',
+    'documentation_description' => 'Cerqueu documentació per a OpenClassify, desenvolupeu complements i també feu servir complements.',
+    'slack_link'                => 'Slack Team',
+    'slack_description'         => 'Cerqueu i comuniqueu-vos amb altres usuaris i desenvolupadors d\'OpenClassify.',
+    'forum_link'                => 'Fòrum de debat',
+    'forum_description'         => 'Cerqueu respostes i publiqueu preguntes sobre l’ús i el desenvolupament amb OpenClassify.',
+    'addons_link'               => 'Complements disponibles',
+    'addons_description'        => 'Descobriu els complements disponibles per a OpenClassify.',
+];
