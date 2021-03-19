cc0bff72022a7578b8744c41deea74aa4babdabf
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:54:25 +0300

    New translations field.php (Norwegian)

---
+++ addons/default/visiosoft/cats-module/resources/lang/no/field.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Tittel',
+        'instructions' => 'Det er navnet på hovedkategorien du vil definere. Det lar brukeren enkelt finne produktet de leter etter.',
+    ],
+    'order' => [
+        'name' => 'Rekkefølge',
+    ],
+
+    'slug' => [
+        'name' => 'Mordersnegle',
+        'instructions' => 'Spesifiser eventuelle organisasjonskoder for å gruppere innlegget ditt med andre.',
+
+    ],
+    'files' => [
+        'name' => 'Bilde',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo nøkkelord',
+        'instructions' => 'Ord lagt til nettstedet i Google-nettleseren for å vises over søkeresultatene.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Beskrivelse',
+        'instructions' => 'Det kan påvirke de besøkendes beslutning om de vil klikke på innholdet i søkeresultatene.',
+    ],
+    'icon' => [
+        'name' => 'Ikon',
+        'instructions' => 'Den brukes til å legge til ikoner som angir kategoritypen.',
+    ],
+
+    'please_wait' => 'Vent. Sletter underkategorier',
+    'category_selection' => 'Kategorivalg',
+    'go_to_parent' => 'Gå til foreldre',
+];
