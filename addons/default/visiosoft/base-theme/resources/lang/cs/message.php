043f859b2a666fc55d763f30b14e82670bc8ba0e
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:39:23 +0300

    New translations message.php (Czech)

---
+++ addons/default/visiosoft/base-theme/resources/lang/cs/message.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'send_forgot_sms' => 'SMS zaslané na váš registrovaný telefon. Prosím zkontrolujte.',
+    'found_phone' => 'Telefonní číslo registrované v systému nebylo nalezeno.',
+];
