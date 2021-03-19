73474d9335aa48fd86ba5f9128c9edfbe11941d0
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:33:02 +0300

    New translations section.php (Afrikaans)

---
+++ addons/default/visiosoft/location-module/resources/lang/af/section.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'countries' => [
+        'title' => 'Lande',
+    ],
+    'cities' => [
+        'title' => 'Stede',
+    ],
+    'districts' => [
+        'title' => 'Distrikte',
+    ],
+    'neighborhoods' => [
+        'title' => 'Wyk',
+    ],
+    'village' => [
+        'title' => 'Dorp',
+    ],
+    'general' => 'Generaal',
+    'map' => 'Kaart',
+    'setting' => 'Instelling',
+    'filter' => 'Filtreer',
+];
