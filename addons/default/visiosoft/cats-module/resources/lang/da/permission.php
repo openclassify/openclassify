0807a7f9eab91b9aec23ec51d933baea86a314ad
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:41:06 +0300

    New translations permission.php (Danish)

---
+++ addons/default/visiosoft/cats-module/resources/lang/da/permission.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'category' => [
+        'name'   => 'Kategori',
+        'option' => [
+            'read'   => 'Kan læse kategori?',
+            'write'  => 'Kan du oprette / redigere kategori?',
+            'delete' => 'Kan slette kategori?',
+        ],
+    ],
+    'placeholderforsearch' => [
+        'name'   => 'Pladsholderforskning',
+        'option' => [
+            'read'   => 'Kan læse pladsholderesøgning?',
+            'write'  => 'Kan du oprette / redigere pladsholderforsøg?',
+            'delete' => 'Kan slette pladsholderesøgning?',
+        ],
+    ],
+];
