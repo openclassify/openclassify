b708faa45284ebcbc93eb8987179126b53bdbf8d
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:37:23 +0300

    New translations errors.php (Finnish)

---
+++ addons/default/visiosoft/base-theme/resources/lang/fi/errors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '404_title' => 'Ei löydetty',
+    '403_title' => 'Kielletty',
+    '404_message' => 'Hakemaasi sivua ei löytynyt.',
+    '403_message' => 'Pääsy tämän sivuston resursseihin evätään!',
+    'home_page' => 'Mene kotisivulle',
+];
