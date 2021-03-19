5158f3d315dac09ceac901e0061a4210fe0f8417
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:39:38 +0300

    New translations permission.php (Danish)

---
+++ addons/default/visiosoft/advs-module/resources/lang/da/permission.php
@@ -0,0 +1,108 @@
+<?php
+
+return [
+    'advs' => [
+        'name'   => 'Annoncer',
+        'option' => [
+            'read'   => 'Kan læse annoncer?',
+            'write'  => 'Kan oprette / redigere annoncer?',
+            'delete' => 'Kan slette annoncer?',
+        ],
+    ],
+    'categories' => [
+        'name'   => 'Kategorier',
+        'option' => [
+            'read'   => 'Kan læse kategorier?',
+            'write'  => 'Kan du oprette / redigere kategorier?',
+            'delete' => 'Kan slette kategorier?',
+        ],
+    ],
+    'village' => [
+        'name'   => 'Landsby',
+        'option' => [
+            'read'   => 'Kan læse landsby?',
+            'write'  => 'Kan du oprette / redigere landsby?',
+            'delete' => 'Kan du slette landsby?',
+        ],
+    ],
+    'custom_fields' => [
+        'name'   => 'Brugerdefinerede felter',
+        'option' => [
+            'read'   => 'Kan læse brugerdefinerede felter?',
+            'write'  => 'Kan du oprette / redigere brugerdefinerede felter?',
+            'delete' => 'Kan du slette brugerdefinerede felter?',
+        ],
+    ],
+    'custom_field_advs' => [
+        'name'   => 'Tilpassede feltannoncer',
+        'option' => [
+            'read'   => 'Kan læse tilpassede feltannoncer?',
+            'write'  => 'Kan oprette / redigere tilpassede feltannoncer?',
+            'delete' => 'Kan slette tilpassede feltannoncer?',
+        ],
+    ],
+    'cf_values' => [
+        'name'   => 'Cf-værdier',
+        'option' => [
+            'read'   => 'Kan læse CF-værdier?',
+            'write'  => 'Kan oprette / redigere CF-værdier?',
+            'delete' => 'Kan slette cf-værdier?',
+        ],
+    ],
+    'options' => [
+        'name'   => 'Muligheder',
+        'option' => [
+            'read'   => 'Kan læse muligheder?',
+            'write'  => 'Kan oprette / redigere indstillinger?',
+            'delete' => 'Kan slette indstillinger?',
+        ],
+    ],
+    'productoptions' => [
+        'name'   => 'Produktoptioner',
+        'option' => [
+            'read'   => 'Kan læse produktoptioner?',
+            'write'  => 'Kan oprette / redigere produktoptioner?',
+            'delete' => 'Kan slette produktvalg?',
+        ],
+    ],
+    'productoptions_value' => [
+        'name'   => 'Productoptions værdi',
+        'option' => [
+            'read'   => 'Kan læse produktvalgets værdi?',
+            'write'  => 'Kan oprette / redigere produktoptionsværdi?',
+            'delete' => 'Kan slette produktvalgets værdi?',
+        ],
+    ],
+    'options_configuration' => [
+        'name'   => 'Valgkonfiguration',
+        'option' => [
+            'read'   => 'Kan konfiguration af læsemuligheder?',
+            'write'  => 'Kan oprette / redigere konfiguration af indstillinger?',
+            'delete' => 'Kan slette konfiguration af indstillinger?',
+        ],
+    ],
+    'option_configuration' => [
+        'name'   => 'Valgkonfiguration',
+        'option' => [
+            'read'   => 'Kan læse option konfiguration?',
+            'write'  => 'Kan oprette / redigere konfiguration af indstillinger?',
+            'delete' => 'Kan slette indstillingskonfiguration?',
+        ],
+    ],
+    'option_configuration' => [
+        'name'   => 'Valgkonfiguration',
+        'option' => [
+            'read'   => 'Kan læse option konfiguration?',
+            'write'  => 'Kan oprette / redigere konfiguration af indstillinger?',
+            'delete' => 'Kan slette indstillingskonfiguration?',
+        ],
+    ],
+    'status' => [
+        'name'   => 'Status',
+        'option' => [
+            'read'   => 'Kan læse status?',
+            'write'  => 'Kan oprette / redigere status?',
+            'delete' => 'Kan slette status?',
+        ],
+    ],
+];
