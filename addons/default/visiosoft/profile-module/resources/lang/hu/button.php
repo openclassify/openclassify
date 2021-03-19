f2360bdf2d7b3b2df6ac64aeec6da7db3bc6cf19
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:38:13 +0300

    New translations button.php (Hungarian)

---
+++ addons/default/visiosoft/profile-module/resources/lang/hu/button.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'new_profile' => 'Új profil',
+    'new_adress' => 'Új cím',
+    'show' => 'Cím megjelenítése',
+    'update_password' => 'Jelszó frissítése',
+    'update_profile' => 'Profil frissítése',
+    'delete' => 'Töröl',
+    'extend' => 'Kiterjeszt',
+    'extend_all' => 'Hosszabbítsa az összeset',
+    'extend_unpublished' => 'Hosszabbítsa a közzététel nélküli',
+    'edit' => 'Szerkesztés',
+    'go_profile' => 'Lépjen a Profil részleteihez',
+    'go_user' => 'Lépjen a Felhasználói adatok részhez',
+    'personal' => 'Személyes',
+    'corporate' => 'Társasági',
+    'export' => 'Export',
+    'new_education' => 'Új oktatás',
+    'new_education_part' => 'Új oktatás rész',
+];
