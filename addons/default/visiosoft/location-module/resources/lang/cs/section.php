69bdd04114501e77f691322948e1d0a1ca5dadf2
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:39:51 +0300

    New translations section.php (Czech)

---
+++ addons/default/visiosoft/location-module/resources/lang/cs/section.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'countries' => [
+        'title' => 'Země',
+    ],
+    'cities' => [
+        'title' => 'Města',
+    ],
+    'districts' => [
+        'title' => 'Okresy',
+    ],
+    'neighborhoods' => [
+        'title' => 'Sousedství',
+    ],
+    'village' => [
+        'title' => 'Vesnice',
+    ],
+    'general' => 'Všeobecné',
+    'map' => 'Mapa',
+    'setting' => 'Nastavení',
+    'filter' => 'Filtr',
+];
