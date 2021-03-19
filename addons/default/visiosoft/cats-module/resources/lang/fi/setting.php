a83c2c4e36dc08c9e1b199f6465af6c3299c6c2e
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:37:16 +0300

    New translations setting.php (Finnish)

---
+++ addons/default/visiosoft/cats-module/resources/lang/fi/setting.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'sitemap_dividing_number' => [
+        'name' => 'Sivukartan jakoluku',
+    ],
+    'include_cities_sitemap' => [
+        'name' => 'Sisällytä kaupungit sivustokarttaan',
+    ],
+];
