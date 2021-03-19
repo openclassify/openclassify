1ea840cfa1d533a89e5ee080b883daa13d869363
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:32:40 +0300

    New translations field.php (Catalan)

---
+++ addons/default/visiosoft/base-theme/resources/lang/ca/field.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    'latest_ads' => 'Últims anuncis',
+    'show_all' => 'Mostrar tots',
+    'login' => [
+        'name' => 'iniciar Sessió',
+    ],
+    'logout' => [
+        'name' => 'Tancar sessió',
+    ],
+    'profile' => 'Perfil',
+    'email' => 'Correu electrònic',
+    'phone' => 'Número de telèfon',
+    'reset_code' => 'Restableix el codi',
+    'password_confirmation' => 'Confirmació de la contrasenya',
+    'password' => 'Contrasenya',
+    'first_name' => 'Nom',
+    'last_name' => 'Cognom',
+    'remember_me' => 'Recorda\'m',
+    'not_a_member_yet' => 'Encara no és membre',
+    'not_a_member_yet_message' => 'Registreu-vos perquè els nostres membres es puguin beneficiar del nostre servei especial',
+    'forgot_password' => 'He oblidat la meva contrasenya',
+    'email_or_phone_number' => 'Adreça de correu electrònic o número de telèfon',
+    'phone_number' => 'Número de telèfon',
+
+    // Registration instructions
+    'personal_registration_header' => 'Quins són els avantatges de la pertinença individual?',
+    'personal_registration_body' => 'Ven i lloga la teva llar, el teu cotxe, ven els articles que no utilitzis i aconsegueix-ne de nous.',
+    'personal_registration_list_1' => 'Publiqueu un anunci gratuït,',
+    'personal_registration_list_2' => 'Afegiu els anuncis que us interessin, seguiu els canvis de preu després d\'afegir-los als vostres preferits, creeu cerques que compleixin els vostres criteris,',
+    'personal_registration_list_3' => 'Envieu missatges als propietaris d’anuncis del lloc.',
+    'register_information_note' => 'La informació d\'aquesta pàgina es pren per a ' . env('APPLICATION_DOMAIN') . ' membresía. Podeu trobar informació detallada sobre la protecció de dades personals aquí.',
+
+    // Register page
+    'phone_validation_error' => 'Aquest número de telèfon l’utilitza un altre membre.',
+
+    // Forgot Password
+    'create_new_password' => 'Crea una nova contrasenya',
+
+    // Login page
+    'or' => 'o bé',
+    'login_with_phone_number' => 'Inicieu la sessió amb el número de telèfon',
+    'login_with_email_address' => 'Inicieu la sessió amb l\'adreça de correu electrònic',
+
+    // Side menu links
+    'company_directory' => 'Directori d\'empreses',
+    'popular_ads' => 'Anuncis populars',
+    'last_48_hours' => 'Darreres 48 hores',
+    'secure_e-commerce_ads' => 'Anuncis de comerç electrònic segurs',
+    'sms' => 'Enviar missatges de text (SMS)',
+    'mail' => 'Envia un correu',
+
+    // Register instruction seed
+    'register_instructions' => 'Registre d’instruccions',
+    'list' => 'Llista',
+    'instruction_description' => 'Descripció de la instrucció',
+    'instruction_list' => 'Llista d’instruccions',
+];
