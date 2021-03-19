baea2df9a591c003ef14e9dc5400ffe81665718c
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:54:07 +0300

    New translations preference.php (Norwegian)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/no/preference.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Sidepanel Hover',
+        'instructions' => 'Utvide sidefeltet når du holder markøren?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigasjon',
+        'instructions' => 'Spesifiser <em>personlige</em> rekkefølge for navigering.',
+        'warning'      => 'Det første tilgjengelige navigasjonselementet brukes som ditt <strong>hjem</strong> område.',
+        'reorder'      => 'Dra og slipp de primære navigasjons elementene i <strong>sidebar</strong> å endre rekkefølgen på dem.',
+    ],
+    'display'       => [
+        'name'         => 'Vis tetthet',
+        'instructions' => 'Kompakt visning gjør at mer innhold kan vises på skjermen samtidig.',
+        'option'       => [
+            'default' => 'Misligholde',
+            'compact' => 'Kompakt',
+        ],
+    ],
+    'sidebars'      => [
+        'name'         => 'Sidefeltmodus',
+        'instructions' => 'Statiske sidefelt vil alltid være synlige.',
+        'option'       => [
+            'default' => 'Misligholde',
+            'static'  => 'Statisk',
+        ],
+    ],
+];
