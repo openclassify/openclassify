8377a493e50a452d064a4aedba73d252aa823998
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:54:46 +0300

    New translations button.php (Norwegian)

---
+++ addons/default/visiosoft/base-theme/resources/lang/no/button.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'post_ad' => [
+        'name' => 'Legg ut annonse',
+    ],
+    'login' => 'Logg Inn',
+    'register' => 'Registrere',
+    'continue' => 'Fortsette',
+    'reset_password' => 'Tilbakestille passord',
+    'email' => 'E-post',
+    'phone' => 'Telefon',
+    'show_all' => 'Vis alt',
+    'hide_all' => 'Gjem alle',
+];
