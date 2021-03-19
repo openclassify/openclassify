64e1c6feab2056744a9642e74a6ab685a19bd18d
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:45:04 +0300

    New translations setting.php (Chinese Simplified)

--- addons/default/visiosoft/base-theme/resources/lang/zh/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/zh/setting.php
@@ -31,7 +31,16 @@ return [
     'register_page_instruction_logo' => [
         'name' => '注册页面说明徽标',
     ],
+    'register_page_alert_link' => [
+        'name' => '注册页面警报链接',
+    ],
     'style' => [
         'name' => '样式（CSS）',
     ],
+    'home_page_sub_categories_limit' => [
+        'name' => '主页子类别限制',
+    ],
+	'default_country' => [
+		'name' => '注册页面，电话的默认国家/地区',
+	]
 ];
