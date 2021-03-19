7bb08af8a738973e368e8173f98fd34cd045f889
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:32:47 +0300

    New translations field.php (Catalan)

---
+++ addons/default/visiosoft/cats-module/resources/lang/ca/field.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Títol',
+        'instructions' => 'És el nom de la categoria principal que definireu. Permet a l’usuari trobar fàcilment el producte que busca.',
+    ],
+    'order' => [
+        'name' => 'Comanda',
+    ],
+
+    'slug' => [
+        'name' => 'Llimac',
+        'instructions' => 'Especifiqueu qualsevol etiqueta organitzativa per ajudar a agrupar la vostra publicació amb altres persones.',
+
+    ],
+    'files' => [
+        'name' => 'Imatge',
+    ],
+    'seo_keyword' => [
+        'name' => 'Paraula clau SEO',
+        'instructions' => 'Paraules afegides al lloc al navegador Google per aparèixer a sobre dels resultats de la cerca.',
+    ],
+    'seo_description' => [
+        'name' => 'Descripció SEO',
+        'instructions' => 'Pot afectar la decisió dels visitants sobre si volen fer clic al contingut dels resultats de la cerca.',
+    ],
+    'icon' => [
+        'name' => 'Icona',
+        'instructions' => 'S\'utilitza per afegir icones que indiquen el tipus de categoria.',
+    ],
+
+    'please_wait' => 'Si us plau, espereu. Esborra les subcategories',
+    'category_selection' => 'Selecció de categories',
+    'go_to_parent' => 'Ves al pare',
+];
