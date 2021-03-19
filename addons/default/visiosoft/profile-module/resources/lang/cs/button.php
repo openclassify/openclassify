2a2c949f50ef091f895857f751fa542689abdd38
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:40:19 +0300

    New translations button.php (Czech)

---
+++ addons/default/visiosoft/profile-module/resources/lang/cs/button.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'new_profile' => 'Nový profil',
+    'new_adress' => 'Nová adresa',
+    'show' => 'Zobrazit adresu',
+    'update_password' => 'Aktualizujte heslo',
+    'update_profile' => 'Aktualizovat profil',
+    'delete' => 'Vymazat',
+    'extend' => 'Rozšířit',
+    'extend_all' => 'Rozšířit vše',
+    'extend_unpublished' => 'Rozšířit nepublikované',
+    'edit' => 'Upravit',
+    'go_profile' => 'Přejít na Detail profilu',
+    'go_user' => 'Přejít na Detail uživatele',
+    'personal' => 'Osobní',
+    'corporate' => 'Firemní',
+    'export' => 'Vývozní',
+    'new_education' => 'Nové vzdělání',
+    'new_education_part' => 'Nová část vzdělávání',
+];
