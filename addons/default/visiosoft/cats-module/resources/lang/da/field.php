daf3ea8c00c7ce8c2942d63d34052f96dfd77404
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:41:04 +0300

    New translations field.php (Danish)

---
+++ addons/default/visiosoft/cats-module/resources/lang/da/field.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'name' => [
+        'name' => 'Titel',
+        'instructions' => 'Det er navnet på den hovedkategori, du vil definere. Det giver brugeren mulighed for let at finde det produkt, de leder efter.',
+    ],
+    'order' => [
+        'name' => 'Bestille',
+    ],
+
+    'slug' => [
+        'name' => 'Slug',
+        'instructions' => 'Angiv eventuelle organisatoriske tags for at hjælpe med at gruppere dit indlæg med andre.',
+
+    ],
+    'files' => [
+        'name' => 'Billede',
+    ],
+    'seo_keyword' => [
+        'name' => 'Seo nøgleord',
+        'instructions' => 'Ord, der føjes til webstedet i Google-browseren, vises over søgeresultaterne.',
+    ],
+    'seo_description' => [
+        'name' => 'Seo Beskrivelse',
+        'instructions' => 'Det kan påvirke dine besøgendes beslutning om, hvorvidt de vil klikke på indholdet i søgeresultaterne.',
+    ],
+    'icon' => [
+        'name' => 'Ikon',
+        'instructions' => 'Det bruges til at tilføje ikoner, der angiver kategoritypen.',
+    ],
+
+    'please_wait' => 'Vent venligst. Sletning af underkategorier',
+    'category_selection' => 'Valg af kategori',
+    'go_to_parent' => 'Gå til forælder',
+];
