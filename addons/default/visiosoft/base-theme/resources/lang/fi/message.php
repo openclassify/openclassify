6ca9c413bd6f3ca49662cc40571ada78d2558956
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:37:14 +0300

    New translations message.php (Finnish)

---
+++ addons/default/visiosoft/base-theme/resources/lang/fi/message.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'send_forgot_sms' => 'Tekstiviesti lähetetty rekisteröityyn puhelimeesi. Tarkista.',
+    'found_phone' => 'Järjestelmään rekisteröityä puhelinnumeroa ei löytynyt.',
+];
