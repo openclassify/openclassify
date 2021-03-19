d65b61ca63daf6ce37fa07df2f0cfc5701120001
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:36:53 +0300

    New translations setting.php (Czech)

---
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/cs/setting.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    'sidebar_hover' => [
+        'name'         => 'Umístěte ukazatel myši na boční panel',
+        'instructions' => 'Rozbalit postranní panel při najetí myší?',
+    ],
+    'navigation'    => [
+        'name'         => 'Navigace',
+        'instructions' => 'Určete <em>výchozí</em> pořadí navigace.',
+        'warning'      => 'První přístupná položka navigace se používá jako oblast <strong>domů</strong>.',
+        'reorder'      => 'Drag and drop primární navigační položky v <strong>postranním panelu</strong> , aby jejich pořadí.',
+    ],
+    "login_icon" => [
+        'name' => 'Ikona přihlášení',
+    ],
+    "panel_icon" => [
+        'name' => 'Ikona panelu',
+    ],
+    "title" => [
+        'name' => 'Titul',
+    ],
+    "footer_copyright_org_name" => [
+        'name' => 'Název organizace pro zápatí autorských práv',
+    ],
+    "sidebar_main_color" => [
+        'name' => 'Hlavní barva postranního panelu',
+    ],
+    "sidebar_secondary_color" => [
+        'name' => 'Sekundární barva postranního panelu',
+    ],
+    "active_sidebar_border_color" => [
+        'name' => 'Aktivní barva okraje postranního panelu',
+    ]
+];
