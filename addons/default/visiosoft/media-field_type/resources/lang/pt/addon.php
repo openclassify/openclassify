000b8a911364737fd019456b4f57db9f2c58a911
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:55:09 +0300

    New translations addon.php (Portuguese, Brazilian)

--- addons/default/visiosoft/media-field_type/resources/lang/pt/addon.php
+++ addons/default/visiosoft/media-field_type/resources/lang/pt/addon.php
@@ -2,6 +2,6 @@
 
 return [
     'title'       => 'Tipo de campo de mídia',
-    'name'        => 'Tipo de campo Arquivos',
+    'name'        => 'Tipo de campo de arquivos',
     'description' => 'Um tipo de campo de upload de vários arquivos.',
 ];
