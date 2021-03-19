90673ff968a16036db237efc0f4acca59a6ed427
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:43:48 +0300

    New translations addon.php (Chinese Simplified)

--- addons/default/visiosoft/advs-module/resources/lang/zh/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/zh/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => '廣告',
-    'name'        => '廣告模塊',
-    'description' => '廣告模塊',
+    'title'       => '广告',
+    'name'        => '广告模块',
+    'description' => '广告模块',
 ];
