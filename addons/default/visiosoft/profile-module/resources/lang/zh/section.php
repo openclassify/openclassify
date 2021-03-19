67254e84fda823e63bde73dbafee914222624eec
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:44:57 +0300

    New translations section.php (Chinese Simplified)

--- addons/default/visiosoft/profile-module/resources/lang/zh/section.php
+++ addons/default/visiosoft/profile-module/resources/lang/zh/section.php
@@ -8,4 +8,11 @@ return [
         'title' => '地址',
     ],
     'general_setting' => '常规设置',
+    'recaptcha' => '验证码',
+    'education' => [
+        'title' => '教育',
+    ],
+    'education_part' => [
+        'title' => '教育部分',
+    ],
 ];
