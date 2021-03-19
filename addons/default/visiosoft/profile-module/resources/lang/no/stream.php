39108529b67499cf54f966622dc1e5f4169d90b9
Fatih Alp <mail@fatihalp.com>
2021-03-19 15:44:30 +0300

    New translations stream.php (Norwegian)

---
+++ addons/default/visiosoft/profile-module/resources/lang/no/stream.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'profile' => [
+        'name' => 'Profil',
+    ],
+    'adress' => [
+        'name' => 'Adresse',
+    ],
+    'education' => [
+        'name' => 'utdanning',
+    ],
+    'education_part' => [
+        'name' => 'Utdanning del',
+    ],
+];
