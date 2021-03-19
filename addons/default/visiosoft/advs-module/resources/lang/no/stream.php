1c799ad9172a5eafbd3cb6250c92a021b021af86
Fatih Alp <mail@fatihalp.com>
2021-03-19 14:54:43 +0300

    New translations stream.php (Norwegian)

---
+++ addons/default/visiosoft/advs-module/resources/lang/no/stream.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'advs' => [
+        'name' => 'Adv',
+    ],
+    'categories' => [
+        'name' => 'Kategorier',
+    ],
+    'village' => [
+        'name' => 'Landsby',
+    ],
+    'custom_fields' => [
+        'name' => 'Egendefinerte felt',
+    ],
+    'custom_field_advs' => [
+        'name' => 'Tilpasset feltadv',
+    ],
+    'cf_values' => [
+        'name' => 'Cf-verdier',
+    ],
+    'options' => [
+        'name' => 'Alternativer',
+    ],
+    'product_options' => [
+        'name' => 'Produktalternativer',
+    ],
+    'productoptions_value' => [
+        'name' => 'Alternativer verdi',
+    ],
+    'option_configuration' => [
+        'name' => 'Konfigurasjon',
+    ],
+    'status' => [
+        'name' => 'Status',
+    ],
+];
