014ded57b5f52cfa1a2d4058292d9f90fb6a3ec0
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:01:08 +0300

    New translations setting.php (French)

--- addons/default/visiosoft/advs-module/resources/lang/fr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fr/setting.php
@@ -258,4 +258,8 @@ return [
         'name' => 'Filigrane',
         'instructions' => 'Le filigrane est-il actif?'
     ],
+    'detailed_product_options' => [
+        'name' => 'Detailed Product Options',
+        'instructions' => 'Allows you to enter stock and price information for the product'
+    ],
 ];
