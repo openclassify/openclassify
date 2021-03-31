bf6a65c790b28b8ca34b051a3ebb43a7b88e81ec
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:01:13 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/setting.php
@@ -258,4 +258,8 @@ return [
         'name' => 'Водяний знак',
         'instructions' => 'Активний водяний знак?'
     ],
+    'detailed_product_options' => [
+        'name' => 'Detailed Product Options',
+        'instructions' => 'Allows you to enter stock and price information for the product'
+    ],
 ];
