3e03812db323aea76de5b515c9de0ecbedf683a6
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:52:33 +0300

    New translations message.php (Portuguese)

--- addons/default/visiosoft/media-field_type/resources/lang/pt/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/pt/message.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'choose_folder' => 'Para qual pasta você gostaria de enviar?',
+    'choose_folder' => 'Para qual pasta você deseja enviar?',
     'upload' => 'Adicionar imagens | Carregar imagens | Anexe uma foto',
     'choose_files' => 'Quais arquivos você gostaria de usar?',
     'no_files_selected' => 'Nenhum arquivo selecionado.',
-    'no_uploads' => 'Nenhum arquivo foi enviado.',
-    'overwrite' => 'já foi carregado. Você gostaria de substituí-lo?',
+    'no_uploads' => 'Nenhum arquivo enviado.',
+    'overwrite' => 'já foi enviado. Deseja substituí-lo?',
     'uploading' => 'Enviando',
     'loading' => 'Carregando',
     'error_upload' => 'Ocorreu um problema ao enviar o arquivo.',
