9bd5580902f83a579d0bd483fef48aba729742e7
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:01:17 +0300

    New translations setting.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/setting.php
@@ -258,4 +258,8 @@ return [
         'name' => 'Filigran',
         'instructions' => 'Filigran etkin mi?'
     ],
+    'detailed_product_options' => [
+        'name' => 'Detailed Product Options',
+        'instructions' => 'Allows you to enter stock and price information for the product'
+    ],
 ];
