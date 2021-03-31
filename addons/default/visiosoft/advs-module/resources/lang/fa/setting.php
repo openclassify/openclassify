df96d9628a1665a9e7d58845726d28023e94fccd
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:00:15 +0300

    New translations setting.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/setting.php
@@ -259,4 +259,8 @@ return [
         'name' => 'علامت',
         'instructions' => 'آیا علامت علامت فعال است؟'
     ],
+    'detailed_product_options' => [
+        'name' => 'Detailed Product Options',
+        'instructions' => 'Allows you to enter stock and price information for the product'
+    ],
 ];
