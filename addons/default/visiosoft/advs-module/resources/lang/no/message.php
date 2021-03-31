0c244468dea2bd4afc42b4acd92bb04a6b072bfb
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:51:41 +0300

    New translations message.php (Norwegian)

--- addons/default/visiosoft/advs-module/resources/lang/no/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/no/message.php
@@ -31,5 +31,5 @@ return [
     'passive_status_change' => "Annonsens status er satt til passiv!",
 	'sold_status_change' => "Annonsens status er satt til solgt!",
 	'status_change' => "Annonsens status er satt til: status!",
-	'disabled_detailed_options_for_admin_role' => "Detailed product options are disabled.",
+	'disabled_detailed_options_for_admin_role' => "Detaljerte produktalternativer er deaktivert.",
 ];
