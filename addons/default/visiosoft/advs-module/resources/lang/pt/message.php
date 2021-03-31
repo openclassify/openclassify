3061fe67496224031f21b61450f957ac13d7056e
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:01:31 +0300

    New translations message.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/message.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'error_added_cart' => 'O produto solicitado está ausente ou inativo',
+    'error_added_cart' => 'O produto solicitado está ausente ou inativo no momento',
     'please_payment' => 'Por favor pague',
     'bank_account_info' => 'informações de conta bancária',
-    'agreement' => 'Ao clicar em Publicar, você concorda com nossos Termos de Uso e Política de Privacidade e reconhece que você é o legítimo proprietário deste item.',
-    'update_category_msg' => 'A categoria de classificados será atualizada.',
+    'agreement' => 'Ao clicar em Publicar, você concorda com nossos Termos de Uso e Política de Privacidade, reconhecendo que você é o legítimo proprietário deste item.',
+    'update_category_msg' => 'A categoria classificada será atualizada.',
     'updated_category_msg' => 'Categoria editada com sucesso.',
     'create_ad_with_post_cat' => [
         'title' => "A seleção da categoria foi concluída.",
@@ -14,10 +14,10 @@ return [
     ],
     'update_category_info' => 'Atualize a categoria.',
     'filter_by_search_msg' => 'Filtrar por pesquisa.',
-    'delete_author_error' => 'Você não pode excluir uma postagem que não pertence a você',
+    'delete_author_error' => 'Você não pode excluir uma postagem que não lhe pertence',
     'success_delete' => 'Anúncio excluído',
-    'pending_ad_status' => 'Seu anúncio está com aprovação pendente do Editor.',
-    'extended' => ': número anúncio (s) foi estendido com sucesso.',
+    'pending_ad_status' => 'O seu anúncio está com aprovação pendente do Editor.',
+    'extended' => ': número de anúncios foi estendido com sucesso.',
     'category_not_exist' => 'Esta categoria não existe mais!',
     'currency_converted' => 'Moeda convertida com sucesso.',
     'choose_user' => 'Escolha um usuário',
