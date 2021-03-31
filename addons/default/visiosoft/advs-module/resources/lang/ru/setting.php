a4799e8fe7f8f0b6932328348aadc75e88b6cfcd
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:55:36 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
@@ -112,8 +112,8 @@ return [
         'instructions' => 'Скрыть кнопку настроек на странице предварительного просмотра объявления',
     ],
     'make_all_fields_required' => [
-        'name' => 'Make All Fields Required',
-        'instructions' => 'Require the user to fill the Price and Description fields when creating an ad',
+        'name' => 'Сделать все поля обязательными',
+        'instructions' => 'Требовать от пользователя заполнения полей «Цена» и «Описание» при создании объявления.',
     ],
     'make_map_required' => [
         'name' => 'Сделать карту обязательной',
@@ -259,7 +259,7 @@ return [
         'instructions' => 'Водяной знак активен?'
     ],
     'detailed_product_options' => [
-        'name' => 'Detailed Product Options',
-        'instructions' => 'Allows you to enter stock and price information for the product'
+        'name' => 'Подробные параметры продукта',
+        'instructions' => 'Позволяет вводить информацию о наличии и цене продукта.'
     ],
 ];
