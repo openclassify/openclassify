d7f6020ff5aef888796cd8233feab25b1f742d6f
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:52:54 +0300

    New translations setting.php (Portuguese)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/pt/setting.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/pt/setting.php
@@ -2,14 +2,14 @@
 
 return [
     'sidebar_hover' => [
-        'name'         => 'Hover na barra lateral',
+        'name'         => 'Passe o cursor sobre a barra lateral',
         'instructions' => 'Expandir a barra lateral ao passar o mouse?',
     ],
     'navigation'    => [
         'name'         => 'Navegação',
-        'instructions' => 'Especifique a ordem de navegação <em>padrão</em>',
-        'warning'      => 'O primeiro item de navegação acessível é usado como <strong>área inicial</strong>',
-        'reorder'      => 'Arraste e solte os itens de navegação primários na barra lateral <strong></strong> para reordená-los.',
+        'instructions' => 'Especifique a ordem de navegação <em>padrão</em>.',
+        'warning'      => 'O primeiro item de navegação acessível é usado como a área <strong>inicial</strong>.',
+        'reorder'      => 'Arraste e solte os itens de navegação principais na barra lateral <strong></strong> para reordená-los.',
     ],
     "login_icon" => [
         'name' => 'Ícone de login',
@@ -21,13 +21,13 @@ return [
         'name' => 'Título',
     ],
     "footer_copyright_org_name" => [
-        'name' => 'Nome da organização de direitos autorais do rodapé',
+        'name' => 'Rodapé Copyright Nome da Organização',
     ],
     "sidebar_main_color" => [
         'name' => 'Cor principal da barra lateral',
     ],
     "sidebar_secondary_color" => [
-        'name' => 'Cor Secundária da Barra Lateral',
+        'name' => 'Cor secundária da barra lateral',
     ],
     "active_sidebar_border_color" => [
         'name' => 'Cor ativa da borda da barra lateral',
