fba6af907f975f0858eab4c0557a15935ad31e5f
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:00:42 +0300

    New translations setting.php (Czech)

--- addons/default/visiosoft/advs-module/resources/lang/cs/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/cs/setting.php
@@ -258,4 +258,8 @@ return [
         'name' => 'Vodoznak',
         'instructions' => 'Je vodoznak aktivní?'
     ],
+    'detailed_product_options' => [
+        'name' => 'Detailed Product Options',
+        'instructions' => 'Allows you to enter stock and price information for the product'
+    ],
 ];
