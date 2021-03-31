8239d8fa5e7c7f219fec8c40fe12966464568e5f
Muammer Top <muammer@visiosoft.com.tr>
2021-03-31 14:35:24 +0300

    #3628 index will added

---
+++ addons/default/visiosoft/advs-module/migrations/2021_03_31_100145_visiosoft.module.advs__add_index_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Anomaly\Streams\Platform\Database\Migration\Migration;
+
+class VisiosoftModuleAdvsAddIndexTable extends Migration
+{
+	public function up()
+	{
+		Schema::table('files_files', function (Blueprint $table) {
+			$table->index('deleted_at');
+			$table->index('name');
+			$table->index('folder_id');
+		});
+
+		Schema::table('advs_advs', function (Blueprint $table) {
+			$table->index('deleted_at');
+			$table->index('cat1');
+			$table->index('country_id');
+			$table->index('city');
+			$table->index('finish_at');
+			$table->index('status');
+			$table->index('count_show_ad');
+		});
+
+		Schema::table('page_link_type_pages_translations', function (Blueprint $table) {
+			$table->index('entry_id');
+			$table->index('locale');
+		});
+	}
+}
