c0e33108ba448c3477e8637c75c126fa424d3ff4
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:52:04 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/base-theme/resources/lang/pt/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/pt/field.php
@@ -10,44 +10,44 @@ return [
         'name' => 'Sair',
     ],
     'profile' => 'Perfil',
-    'email' => 'Endereço de email',
+    'email' => 'Endereço de e-mail',
     'phone' => 'Número de telefone',
     'reset_code' => 'Reiniciar código',
     'password_confirmation' => 'ConfirmaÇão Da Senha',
     'password' => 'Senha',
     'first_name' => 'Primeiro nome',
-    'last_name' => 'Sobrenome',
+    'last_name' => 'Último nome',
     'remember_me' => 'Lembre de mim',
     'not_a_member_yet' => 'Não é membro ainda',
     'not_a_member_yet_message' => 'Inscreva-se para que nossos membros se beneficiem de nosso serviço especial',
     'forgot_password' => 'Esqueci a minha senha',
-    'email_or_phone_number' => 'Endereço de e-mail ou número de telefone',
+    'email_or_phone_number' => 'Endereço de email ou número de telefone',
     'phone_number' => 'Número de telefone',
 
     // Registration instructions
-    'personal_registration_header' => 'Quais são as vantagens de associação individual?',
-    'personal_registration_body' => 'Venda e alugue sua casa, carro, venda seus itens não usados, compre novos.',
+    'personal_registration_header' => 'O que são vantagens individuais de associação?',
+    'personal_registration_body' => 'Venda e alugue sua casa, carro, venda seus itens não utilizados, obtenha novos.',
     'personal_registration_list_1' => 'Publique um anúncio grátis,',
     'personal_registration_list_2' => 'Adicione os anúncios de seu interesse, siga as alterações de preço após adicioná-los aos seus favoritos, crie pesquisas favoritas que atendam aos seus critérios,',
-    'personal_registration_list_3' => 'Envie mensagens aos proprietários de anúncios no site.',
-    'register_information_note' => 'As informações nesta página são tomadas para ' . env('APPLICATION_DOMAIN') . ' Filiação. Você pode encontrar informações detalhadas sobre a proteção de dados pessoais aqui.',
+    'personal_registration_list_3' => 'Envie mensagens para os proprietários do anúncio no site.',
+    'register_information_note' => 'As informações nesta página são usadas para ' . env('APPLICATION_DOMAIN') . ' Filiação. Você pode encontrar informações detalhadas sobre a proteção de dados pessoais aqui.',
 
     // Register page
-    'phone_validation_error' => 'Este número de telefone está sendo usado por outro membro.',
+    'phone_validation_error' => 'Este número de telefone está em uso por outro membro.',
 
     // Forgot Password
     'create_new_password' => 'Criar nova senha',
 
     // Login page
     'or' => 'ou',
-    'login_with_phone_number' => 'Login com número de telefone',
-    'login_with_email_address' => 'Login com endereço de e-mail',
+    'login_with_phone_number' => 'Entrar com número de telefone',
+    'login_with_email_address' => 'Entrar com endereço de email',
 
     // Side menu links
     'company_directory' => 'Diretório da empresa',
     'popular_ads' => 'Anúncios populares',
     'last_48_hours' => 'Últimas 48 horas',
-    'secure_e-commerce_ads' => 'Anúncios seguros de comércio eletrônico',
+    'secure_e-commerce_ads' => 'Anúncios de comércio eletrônico seguro',
     'sms' => 'Enviar mensagem de texto (SMS)',
     'mail' => 'Enviar correio',
 
