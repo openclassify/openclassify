a56ee2f648d8151d66b719effd950362cc842344
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:52:14 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/cats-module/resources/lang/pt/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/pt/field.php
@@ -3,7 +3,7 @@
 return [
     'name' => [
         'name' => 'Título',
-        'instructions' => 'É o nome da categoria principal que você definirá. Ele permite que o usuário encontre facilmente o produto que procura.',
+        'instructions' => 'É o nome da categoria principal que você definirá. Ele permite que o usuário encontre facilmente o produto que está procurando.',
     ],
     'order' => [
         'name' => 'Ordem',
@@ -11,26 +11,27 @@ return [
 
     'slug' => [
         'name' => 'Lesma',
-        'instructions' => 'Especifique quaisquer marcas organizacionais para ajudar a agrupar sua postagem com outras pessoas.',
+        'instructions' => 'Especifique as tags organizacionais para ajudar a agrupar sua postagem com outras pessoas.',
 
     ],
     'files' => [
         'name' => 'Imagem',
     ],
     'seo_keyword' => [
-        'name' => 'Palavra-chave de SEO',
-        'instructions' => 'Palavras adicionadas ao site no navegador Google para aparecerem acima dos resultados da pesquisa.',
+        'name' => 'Seo Palavra-chave',
+        'instructions' => 'Palavras adicionadas ao site no navegador do Google para aparecer acima dos resultados da pesquisa.',
     ],
     'seo_description' => [
         'name' => 'Descrição Seo',
-        'instructions' => 'Isso pode afetar a decisão dos visitantes sobre se desejam clicar no conteúdo dos resultados da pesquisa.',
+        'instructions' => 'Isso pode afetar a decisão dos visitantes sobre se eles querem clicar no conteúdo nos resultados da pesquisa.',
     ],
     'icon' => [
         'name' => 'Ícone',
-        'instructions' => 'É usado para adicionar ícones que indicam o tipo de categoria.',
+        'instructions' => 'É usado para adicionar ícones indicando o tipo de categoria.',
     ],
 
-    'please_wait' => 'Aguarde. Excluindo subcategorias',
-    'category_selection' => 'Seleção de Categoria',
+    'please_wait' => 'Por favor,
+   aguarde. Excluindo Subcategorias',
+    'category_selection' => 'Seleção de categoria',
     'go_to_parent' => 'Vá para o pai',
 ];
