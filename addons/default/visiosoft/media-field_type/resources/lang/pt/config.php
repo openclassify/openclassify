5cc0abcd9d2588ccdbccb24630a029445a8c12ef
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:52:36 +0300

    New translations config.php (Portuguese)

--- addons/default/visiosoft/media-field_type/resources/lang/pt/config.php
+++ addons/default/visiosoft/media-field_type/resources/lang/pt/config.php
@@ -7,20 +7,20 @@ return [
         'warning'      => 'As permissões de pasta existentes têm precedência sobre as pastas selecionadas.',
     ],
     'min'     => [
-        'label'        => 'Seleções mínimas',
-        'instructions' => 'Insira o número mínimo de seleções permitidas.',
+        'label'        => 'Seleções Mínimas',
+        'instructions' => 'Digite o número mínimo de seleções permitidas.',
     ],
     'max'     => [
         'label'        => 'Seleções máximas',
-        'instructions' => 'Insira o número máximo de seleções permitidas.',
+        'instructions' => 'Digite o número máximo de seleções permitidas.',
     ],
     'mode'    => [
         'name'         => 'Modo de entrada',
         'instructions' => 'Como os usuários devem fornecer entrada de arquivo?',
         'option'       => [
-            'default' => 'Faça upload e / ou selecione arquivos.',
+            'default' => 'Carregar e / ou selecionar arquivos.',
             'select'  => 'Selecione apenas arquivos.',
-            'upload'  => 'Faça upload de arquivos apenas.',
+            'upload'  => 'Carregar apenas arquivos.',
         ],
     ],
 ];
