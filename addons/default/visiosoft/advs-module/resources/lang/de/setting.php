a2556265b165ad802cdcd8935eb30f076581a797
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:00:58 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/setting.php
@@ -258,4 +258,8 @@ return [
         'name' => 'Wasserzeichen',
         'instructions' => 'Ist das Wasserzeichen aktiv?'
     ],
+    'detailed_product_options' => [
+        'name' => 'Detailed Product Options',
+        'instructions' => 'Allows you to enter stock and price information for the product'
+    ],
 ];
