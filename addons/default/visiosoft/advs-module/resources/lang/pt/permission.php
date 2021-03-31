c84d463d4dc8d872f27197315ddf0736d79d1ea1
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:51:59 +0300

    New translations permission.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/permission.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/permission.php
@@ -4,49 +4,49 @@ return [
     'advs' => [
         'name'   => 'Publicidades',
         'option' => [
-            'read'   => 'Pode ler anúncios?',
-            'write'  => 'Pode criar / editar anúncios?',
+            'read'   => 'Pode ler anúncios?        ',
+            'write'  => 'Pode criar / editar anúncios?        ',
             'delete' => 'Pode excluir anúncios?',
         ],
     ],
     'categories' => [
         'name'   => 'Categorias',
         'option' => [
-            'read'   => 'Pode ler categorias?',
-            'write'  => 'Pode criar / editar categorias?',
+            'read'   => 'Pode ler categorias?        ',
+            'write'  => 'Pode criar / editar categorias?        ',
             'delete' => 'Pode excluir categorias?',
         ],
     ],
     'village' => [
-        'name'   => 'Vila',
+        'name'   => 'Aldeia',
         'option' => [
-            'read'   => 'Pode ler a aldeia?',
-            'write'  => 'Pode criar / editar aldeia?',
-            'delete' => 'Pode excluir a aldeia?',
+            'read'   => 'Pode ler a aldeia?        ',
+            'write'  => 'Pode criar / editar aldeia?        ',
+            'delete' => 'Pode apagar a aldeia?',
         ],
     ],
     'custom_fields' => [
         'name'   => 'Os campos personalizados',
         'option' => [
-            'read'   => 'Pode ler campos personalizados?',
-            'write'  => 'Pode criar / editar campos personalizados?',
+            'read'   => 'Pode ler campos personalizados?        ',
+            'write'  => 'Pode criar / editar campos personalizados?        ',
             'delete' => 'Pode excluir campos personalizados?',
         ],
     ],
     'custom_field_advs' => [
         'name'   => 'Anúncios de campo personalizado',
         'option' => [
-            'read'   => 'Pode ler anúncios de campo personalizado?',
-            'write'  => 'Pode criar / editar anúncios de campo personalizado?',
+            'read'   => 'Pode ler anúncios de campo personalizados?        ',
+            'write'  => 'Pode criar / editar anúncios de campo personalizados?        ',
             'delete' => 'Pode excluir anúncios de campo personalizado?',
         ],
     ],
     'cf_values' => [
         'name'   => 'Valores Cf',
         'option' => [
-            'read'   => 'Pode ler valores cf?',
-            'write'  => 'Pode criar / editar valores cf?',
-            'delete' => 'Pode deletar valores cf?',
+            'read'   => 'Pode ler valores de cf?        ',
+            'write'  => 'Pode criar / editar valores cf?        ',
+            'delete' => 'Pode excluir valores de cf?',
         ],
     ],
     'options' => [
@@ -70,7 +70,7 @@ return [
         'option' => [
             'read'   => 'Pode ler o valor das opções do produto?',
             'write'  => 'Pode criar / editar o valor das opções do produto?',
-            'delete' => 'Pode excluir o valor das opções de produto?',
+            'delete' => 'Pode excluir o valor de opções de produto?',
         ],
     ],
     'options_configuration' => [
@@ -85,7 +85,7 @@ return [
         'name'   => 'Configuração de opções',
         'option' => [
             'read'   => 'Pode ler a configuração da opção?',
-            'write'  => 'Pode criar / editar a configuração da opção?',
+            'write'  => 'Pode criar / editar configuração de opção?',
             'delete' => 'Pode excluir a configuração da opção?',
         ],
     ],
@@ -93,7 +93,7 @@ return [
         'name'   => 'Configuração de opções',
         'option' => [
             'read'   => 'Pode ler a configuração da opção?',
-            'write'  => 'Pode criar / editar a configuração da opção?',
+            'write'  => 'Pode criar / editar configuração de opção?',
             'delete' => 'Pode excluir a configuração da opção?',
         ],
     ],
