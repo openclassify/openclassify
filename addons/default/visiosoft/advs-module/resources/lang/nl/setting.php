dc9b77c252cee45fcab8ed3b535b8d484aee73d6
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:59:17 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -112,8 +112,8 @@ return [
         'instructions' => 'Verberg de configuratieknop op de pagina Advertentievoorbeeld',
     ],
     'make_all_fields_required' => [
-        'name' => 'Make All Fields Required',
-        'instructions' => 'Require the user to fill the Price and Description fields when creating an ad',
+        'name' => 'Maak alle velden verplicht',
+        'instructions' => 'Vereisen dat de gebruiker de velden Prijs en Beschrijving invult bij het maken van een advertentie',
     ],
     'make_map_required' => [
         'name' => 'Maak een kaart vereist',
@@ -259,7 +259,7 @@ return [
         'instructions' => 'Is watermerk actief?'
     ],
     'detailed_product_options' => [
-        'name' => 'Detailed Product Options',
-        'instructions' => 'Allows you to enter stock and price information for the product'
+        'name' => 'Gedetailleerde productopties',
+        'instructions' => 'Hiermee kunt u voorraad- en prijsinformatie voor het product invoeren'
     ],
 ];
