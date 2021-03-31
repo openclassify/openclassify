9205cc1d15fbf3c8fe2fbe470e07f92eb2e8c6a2
Fatih Alp <mail@fatihalp.com>
2021-03-31 13:00:59 +0300

    New translations message.php (Korean)

--- addons/default/visiosoft/advs-module/resources/lang/ko/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ko/message.php
@@ -31,4 +31,5 @@ return [
     'passive_status_change' => "귀하의 광고 상태가 수동으로 설정되었습니다!",
 	'sold_status_change' => "귀하의 광고 상태가 판매로 설정되었습니다!",
 	'status_change' => "귀하의 광고 상태가 : status로 설정되었습니다!",
+	'disabled_detailed_options_for_admin_role' => "Detailed product options are disabled.",
 ];
