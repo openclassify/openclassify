31c4c08e76b39c9dc67f1b4247487382215c7c96
Fatih Alp <mail@fatihalp.com>
2021-03-31 17:39:56 +0300

    New translations message.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/message.php
@@ -31,5 +31,5 @@ return [
     'passive_status_change' => "Starea anunțului dvs. a fost setată la pasivă!",
 	'sold_status_change' => "Starea anunțului dvs. a fost setată ca vândută!",
 	'status_change' => "Starea anunțului dvs. a fost setată la: stare!",
-	'disabled_detailed_options_for_admin_role' => "Detailed product options are disabled.",
+	'disabled_detailed_options_for_admin_role' => "Opțiunile detaliate ale produsului sunt dezactivate.",
 ];
