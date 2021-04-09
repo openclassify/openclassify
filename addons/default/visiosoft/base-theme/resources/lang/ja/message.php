54706864cda30c9c53a48fd2806d86bd794cbb9d
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:47:02 +0300

    New translations message.php (Japanese)

--- addons/default/visiosoft/base-theme/resources/lang/ja/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/ja/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => '登録した電話にSMSが送信されました。チェックしてください。',
     'found_phone' => 'システムに登録されている電話番号が見つかりませんでした。',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
