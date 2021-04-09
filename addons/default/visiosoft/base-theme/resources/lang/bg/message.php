0462439725b24f04db62db781dfffbc447cde61c
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:46:46 +0300

    New translations message.php (Bulgarian)

--- addons/default/visiosoft/base-theme/resources/lang/bg/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/bg/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS, изпратен на вашия регистриран телефон. Моля те, провери.',
     'found_phone' => 'Телефонният номер, регистриран в системата, не е намерен.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
