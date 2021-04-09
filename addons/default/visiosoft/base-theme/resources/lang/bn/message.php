01572b3235cfa7979c566ec5fd02ef75e649ef09
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:46:42 +0300

    New translations message.php (Bengali)

--- addons/default/visiosoft/base-theme/resources/lang/bn/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/bn/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'আপনার নিবন্ধিত ফোনে এসএমএস পাঠানো হয়েছে। দয়া করে চেক করুন।',
     'found_phone' => 'সিস্টেমে নিবন্ধিত ফোন নম্বরটি পাওয়া যায়নি।',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
