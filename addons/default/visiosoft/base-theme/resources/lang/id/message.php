ff2f215908fc1226b86ec1af6096c54f2c945f0a
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:46:47 +0300

    New translations message.php (Indonesian)

--- addons/default/visiosoft/base-theme/resources/lang/id/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/id/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS dikirim ke ponsel Anda yang terdaftar. Silakan periksa.',
     'found_phone' => 'Nomor telepon yang terdaftar di sistem tidak ditemukan.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
