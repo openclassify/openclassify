840a18b1b6f43ed08e373c842d5de2785f4ee4d8
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:47:06 +0300

    New translations message.php (Greek)

--- addons/default/visiosoft/base-theme/resources/lang/el/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/el/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'Αποστολή SMS στο εγγεγραμμένο τηλέφωνό σας. Παρακαλώ ελέγξτε.',
     'found_phone' => 'Ο αριθμός τηλεφώνου που είναι εγγεγραμμένος στο σύστημα δεν βρέθηκε.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
