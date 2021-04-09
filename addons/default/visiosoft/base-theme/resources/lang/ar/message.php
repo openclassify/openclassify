72e4944e78e1cb61fbdafb6ff46f97eafe05985a
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:47:04 +0300

    New translations message.php (Arabic)

--- addons/default/visiosoft/base-theme/resources/lang/ar/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/ar/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'تم إرسال الرسائل القصيرة إلى هاتفك المسجل. يرجى المراجعة.',
     'found_phone' => 'لم يتم العثور على رقم الهاتف المسجل في النظام.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
