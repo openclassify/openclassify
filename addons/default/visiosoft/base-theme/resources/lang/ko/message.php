66a4a1bc38d99ccadb37ee23fa4a54760f60bdf3
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:47:10 +0300

    New translations message.php (Korean)

--- addons/default/visiosoft/base-theme/resources/lang/ko/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/ko/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => '등록 된 전화로 SMS를 보냈습니다. 확인해주십시오.',
     'found_phone' => '시스템에 등록 된 전화 번호를 찾을 수 없습니다.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
