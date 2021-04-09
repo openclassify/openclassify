0c44e5503799a4cd3dc25bb5f6c15df11f35c1c5
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:46:41 +0300

    New translations message.php (Persian)

--- addons/default/visiosoft/base-theme/resources/lang/fa/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/fa/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'پیامک ارسال شده به تلفن ثبت شده خود. لطفا بررسی کنید.',
     'found_phone' => 'شماره تلفن ثبت شده در سیستم یافت نشد.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
