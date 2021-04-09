bbba788b32f38596f972eede79bfa68c8cd98476
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:46:49 +0300

    New translations message.php (Urdu (Pakistan))

--- addons/default/visiosoft/base-theme/resources/lang/ur/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/ur/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'آپ کے رجسٹرڈ فون پر SMS بھیجا گیا۔ براہ مہربانی دیکھ لیجے.',
     'found_phone' => 'سسٹم میں درج فون نمبر نہیں ملا۔',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
