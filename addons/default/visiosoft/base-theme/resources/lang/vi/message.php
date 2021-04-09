4c10300d5afe335b2346600de578b4f0c1537498
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:46:58 +0300

    New translations message.php (Vietnamese)

--- addons/default/visiosoft/base-theme/resources/lang/vi/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/vi/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS được gửi đến điện thoại đã đăng ký của bạn. Hãy kiểm tra.',
     'found_phone' => 'Số điện thoại đã đăng ký trong hệ thống không được tìm thấy.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
