6791199bfa4890d2a8b87f91d77d0a3111ed323a
Fatih Alp <mail@fatihalp.com>
2021-04-09 10:47:14 +0300

    New translations message.php (Polish)

--- addons/default/visiosoft/base-theme/resources/lang/pl/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/pl/message.php
@@ -3,4 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS wysłany na zarejestrowany telefon. Proszę sprawdzić.',
     'found_phone' => 'Numer telefonu zarejestrowany w systemie nie został znaleziony.',
+    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
 ];
