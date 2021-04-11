d6024e42538e5435fd673239311b26309233c6b6
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:32:18 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/base-theme/resources/lang/sq/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/sq/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS dërguar në telefonin tuaj të regjistruar. Te lutem kontrollo.',
     'found_phone' => 'Numri i telefonit të regjistruar në sistem nuk u gjet.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'Transaksioni juaj u përfundua me sukses, por e-maili i njoftimit nuk mund të dërgohej.',
 ];
