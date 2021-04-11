9b6b2f39241fc2ed964571e2c7e8932ec24a4e25
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:43:17 +0300

    New translations message.php (Swedish)

--- addons/default/visiosoft/base-theme/resources/lang/sv/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/sv/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS skickas till din registrerade telefon. Vänligen kontrollera.',
     'found_phone' => 'Telefonnumret som registrerades i systemet hittades inte.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'Din transaktion slutfördes, men e-postmeddelandet kunde inte skickas.',
 ];
