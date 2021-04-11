4823e08f80193c33cd0a27abc600b07be9e78e84
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:39:07 +0300

    New translations message.php (Hindi)

--- addons/default/visiosoft/base-theme/resources/lang/hi/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/hi/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'आपके पंजीकृत फोन पर एसएमएस भेजा गया। कृपया जांचें।',
     'found_phone' => 'सिस्टम में पंजीकृत फोन नंबर नहीं मिला।',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'आपका लेनदेन सफलतापूर्वक पूरा हो गया, लेकिन अधिसूचना ई-मेल नहीं भेजी जा सकी।',
 ];
