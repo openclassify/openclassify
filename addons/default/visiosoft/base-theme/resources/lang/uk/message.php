131ef4d5fcc5f6ecbccc066991fea629a9b5e17e
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:41:40 +0300

    New translations message.php (Ukrainian)

--- addons/default/visiosoft/base-theme/resources/lang/uk/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/uk/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS, надісланий на ваш зареєстрований телефон. Будь ласка, перевірте.',
     'found_phone' => 'Номер телефону, зареєстрований у системі, не знайдено.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'Ваша транзакція була успішно виконана, але електронний лист із повідомленням не вдалося надіслати.',
 ];
