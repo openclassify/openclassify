cf42412f6f539c350f61ff2995f1ab3d4f77543c
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:39:50 +0300

    New translations message.php (Serbian (Cyrillic))

--- addons/default/visiosoft/base-theme/resources/lang/sr/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/sr/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'СМС послан на ваш регистровани телефон. Молим те провери.',
     'found_phone' => 'Телефонски број регистрован у систему није пронађен.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'Ваша трансакција је успешно завршена, али е-пошта са обавештењем није могла бити послата.',
 ];
