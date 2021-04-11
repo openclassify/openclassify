f1b3eb6448a9f564fb3d027aaca15e2b54d9e830
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:34:19 +0300

    New translations message.php (Afrikaans)

--- addons/default/visiosoft/base-theme/resources/lang/af/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/af/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS gestuur na u geregistreerde foon. Kyk asb.',
     'found_phone' => 'Die telefoonnommer wat in die stelsel geregistreer is, is nie gevind nie.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'U transaksie is suksesvol voltooi, maar die kennisgewing-e-pos kon nie gestuur word nie.',
 ];
