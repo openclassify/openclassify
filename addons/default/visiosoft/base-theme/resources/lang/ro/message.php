7ff236b7630e597e0748dbfff172d99d7f371f26
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:21:38 +0300

    New translations message.php (Romanian)

--- addons/default/visiosoft/base-theme/resources/lang/ro/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/ro/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS trimis la telefonul dvs. înregistrat. Te rog verifica.',
     'found_phone' => 'Numărul de telefon înregistrat în sistem nu a fost găsit.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'Tranzacția dvs. a fost finalizată cu succes, dar e-mailul de notificare nu a putut fi trimis.',
 ];
