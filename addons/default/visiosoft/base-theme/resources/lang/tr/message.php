0bebeed48c7a88732fc719b1b812f28ee871e6b1
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:44:19 +0300

    New translations message.php (Turkish)

--- addons/default/visiosoft/base-theme/resources/lang/tr/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/tr/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'Kayıtlı telefonunuza SMS gönderildi. Lütfen kontrol edin.',
     'found_phone' => 'Sistemde kayıtlı olan telefon numarası bulunamadı.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'İşleminiz başarıyla tamamlandı ancak bildirim e-postası gönderilemedi.',
 ];
