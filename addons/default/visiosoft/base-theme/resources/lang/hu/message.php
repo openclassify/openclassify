80eed992b9086a8d648dcba8e68b7c622d07431e
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:49:25 +0300

    New translations message.php (Hungarian)

--- addons/default/visiosoft/base-theme/resources/lang/hu/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/hu/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => 'SMS-t küldünk a regisztrált telefonjára. Kérem ellenőrizze.',
     'found_phone' => 'A rendszerben regisztrált telefonszám nem található.',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => 'A tranzakció sikeresen befejeződött, de az értesítő e-mailt nem sikerült elküldeni.',
 ];
