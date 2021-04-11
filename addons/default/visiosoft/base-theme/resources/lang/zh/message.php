655c0807e61cb1ee4e219fc5a9dcd5e86dc087bc
Fatih Alp <mail@fatihalp.com>
2021-04-11 16:51:18 +0300

    New translations message.php (Chinese Simplified)

--- addons/default/visiosoft/base-theme/resources/lang/zh/message.php
+++ addons/default/visiosoft/base-theme/resources/lang/zh/message.php
@@ -3,5 +3,5 @@
 return [
     'send_forgot_sms' => '短信已发送到您的注册电话。请检查。',
     'found_phone' => '找不到系统中注册的电话号码。',
-    'error_mail' => 'Your transaction was completed successfully, but the notification e-mail could not be sent.',
+    'error_mail' => '您的交易已成功完成，但是无法发送通知电子邮件。',
 ];
