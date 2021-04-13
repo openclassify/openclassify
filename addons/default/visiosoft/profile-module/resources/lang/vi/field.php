03317c6e06727b21fcb54051020ed36f54c2a2df
Fatih Alp <mail@fatihalp.com>
2021-04-13 10:37:51 +0300

    New translations field.php (Vietnamese)

--- addons/default/visiosoft/profile-module/resources/lang/vi/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/vi/field.php
@@ -469,6 +469,8 @@ return [
     'change_password' => 'Đổi mật khẩu',
     'choose_an_option' => 'Chọn một sự lựa chọn...',
     'change_status' => 'Thay đổi trạng thái',
+	'profile_settings' => 'Profile Settings',
+	'profile_detail' => 'Profile Detail',
 
     // Register page
     'accept_terms_label' => 'Tôi chấp nhận các điều khoản.',
