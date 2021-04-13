aa0fb69bca9e0770f85c9d53a99b9162c09337ab
Fatih Alp <mail@fatihalp.com>
2021-04-13 10:37:42 +0300

    New translations field.php (Catalan)

--- addons/default/visiosoft/profile-module/resources/lang/ca/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ca/field.php
@@ -469,6 +469,8 @@ return [
     'change_password' => 'Canvia la contrasenya',
     'choose_an_option' => 'Trieu una opció ...',
     'change_status' => 'Canvia l\'estat',
+	'profile_settings' => 'Profile Settings',
+	'profile_detail' => 'Profile Detail',
 
     // Register page
     'accept_terms_label' => 'Accepto les condicions.',
