350389ddd3778869cc8bdd18c34e28227d0e338a
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:59:25 +0300

    New translations message.php (Hungarian)

--- addons/default/visiosoft/location-module/resources/lang/hu/message.php
+++ addons/default/visiosoft/location-module/resources/lang/hu/message.php
@@ -3,5 +3,5 @@
 return [
     'placeholder' => 'Írja be ide a keresést',
     'null_msg' => 'Nem található',
-    'choose_country' => 'Choose a Country',
+    'choose_country' => 'Válassz egy országot',
 ];
