d5d7e79b612ada89bd57328b8f16ba545050d46e
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:33:46 +0300

    New translations field.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/field.php
@@ -212,7 +212,7 @@ return [
     'featured_ads' => "Anúncios em destaque",
     'trending_ads' => 'Anúncios Trendind',
     'search_ad_result_prefix' => 'em',
-    'search_ad_result_text' => '<strong>:count</strong> ads were found in your <strong>:name</strong> search',
+    'search_ad_result_text' => '<strong>: contagem de</strong> anúncios foram encontrados em sua pesquisa <strong>: nome</strong>',
     'search_ads_result_message' => [
         'name' => ':      contagem de anúncios encontrados para:pesquisa de nome',
     ],
@@ -339,11 +339,11 @@ return [
     'default_owner_instruction' => 'O proprietário assumirá como padrão o usuário atual se nenhum usuário for selecionado',
 
     // Listing page
-    'ad_title' => 'Ad title',
-    'listing_date' => 'Listing Date',
-    'view' => 'View',
-    'ads_with_map' => 'Ads With Map',
-    'advanced_sorting' => 'Advanced Sorting',
+    'ad_title' => 'Título do anúncio',
+    'listing_date' => 'Data de Listagem',
+    'view' => 'Visualizar',
+    'ads_with_map' => 'Anúncios com mapa',
+    'advanced_sorting' => 'Classificação Avançada',
 
     'free' => 'Livre',
     'ad_date' => 'Data do Anúncio',
