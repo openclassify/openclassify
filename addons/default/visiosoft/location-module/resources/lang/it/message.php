fc7beac2c5a790e50092687c00877577620ab661
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:39:02 +0300

    New translations message.php (Italian)

--- addons/default/visiosoft/location-module/resources/lang/it/message.php
+++ addons/default/visiosoft/location-module/resources/lang/it/message.php
@@ -3,5 +3,5 @@
 return [
     'placeholder' => 'Digita qui per cercare',
     'null_msg' => 'Non trovato',
-    'choose_country' => 'Choose a Country',
+    'choose_country' => 'Scegli un paese',
 ];
