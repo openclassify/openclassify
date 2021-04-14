7b3defced5149b84dadc4c7b7693eecaac5d7cab
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:54:06 +0300

    New translations message.php (Ukrainian)

--- addons/default/visiosoft/location-module/resources/lang/uk/message.php
+++ addons/default/visiosoft/location-module/resources/lang/uk/message.php
@@ -3,5 +3,5 @@
 return [
     'placeholder' => 'Введіть тут для пошуку',
     'null_msg' => 'Не знайдено',
-    'choose_country' => 'Choose a Country',
+    'choose_country' => 'Виберіть країну',
 ];
