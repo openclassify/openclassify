64f4b269a50b6e48eaac028804a6f57e860b1556
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:50:59 +0300

    New translations message.php (Urdu (Pakistan))

--- addons/default/visiosoft/location-module/resources/lang/ur/message.php
+++ addons/default/visiosoft/location-module/resources/lang/ur/message.php
@@ -3,5 +3,5 @@
 return [
     'placeholder' => 'تلاش کرنے کے لئے یہاں ٹائپ کریں',
     'null_msg' => 'نہیں ملا',
-    'choose_country' => 'Choose a Country',
+    'choose_country' => 'ایک ملک کا انتخاب کریں',
 ];
