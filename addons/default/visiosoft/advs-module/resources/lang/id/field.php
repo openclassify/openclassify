74941455f76ab99d0f69c0f1bbde17351d1b0e27
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:57:56 +0300

    New translations field.php (Indonesian)

--- addons/default/visiosoft/advs-module/resources/lang/id/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/id/field.php
@@ -211,7 +211,7 @@ return [
     'featured_ads' => "Iklan Unggulan",
     'trending_ads' => 'Iklan Tren',
     'search_ad_result_prefix' => 'di',
-    'search_ad_result_text' => '<strong>:count</strong> ads were found in your <strong>:name</strong> search',
+    'search_ad_result_text' => '<strong>:</strong> ditemukan dalam pencarian <strong>: nama</strong>',
     'search_ads_result_message' => [
         'name' => ': count iklan yang ditemukan untuk pencarian :name',
     ],
@@ -338,11 +338,11 @@ return [
     'default_owner_instruction' => 'Pemilik akan menggunakan pengguna saat ini secara bawaan/default jika tidak ada pengguna yang dipilih',
 
     // Listing page
-    'ad_title' => 'Ad title',
-    'listing_date' => 'Listing Date',
-    'view' => 'View',
-    'ads_with_map' => 'Ads With Map',
-    'advanced_sorting' => 'Advanced Sorting',
+    'ad_title' => 'Judul Iklan',
+    'listing_date' => 'Tanggal Pencatatan',
+    'view' => 'Melihat',
+    'ads_with_map' => 'Iklan Dengan Peta',
+    'advanced_sorting' => 'Penyortiran Lanjutan',
 
     'free' => 'Gratis',
     'ad_date' => 'Tanggal Iklan',
