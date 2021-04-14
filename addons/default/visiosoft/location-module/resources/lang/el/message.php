50820fc6e909b2b0037e9e03fad7caf0f1c83176
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:28:44 +0300

    New translations message.php (Greek)

--- addons/default/visiosoft/location-module/resources/lang/el/message.php
+++ addons/default/visiosoft/location-module/resources/lang/el/message.php
@@ -3,5 +3,5 @@
 return [
     'placeholder' => 'Πληκτρολογήστε εδώ για αναζήτηση',
     'null_msg' => 'Δεν βρέθηκε',
-    'choose_country' => 'Choose a Country',
+    'choose_country' => 'Διάλεξε μία χώρα',
 ];
