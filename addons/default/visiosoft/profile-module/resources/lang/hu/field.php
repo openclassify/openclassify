d029dda62e25b8a31f87bfaaf438c8b440ee3171
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:59:12 +0300

    New translations field.php (Hungarian)

--- addons/default/visiosoft/profile-module/resources/lang/hu/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/hu/field.php
@@ -469,8 +469,8 @@ return [
     'change_password' => 'Jelszó módosítása',
     'choose_an_option' => 'Válassz egy lehetőséget...',
     'change_status' => 'Állapotváltozás',
-	'profile_settings' => 'Profile Settings',
-	'profile_detail' => 'Profile Detail',
+	'profile_settings' => 'Profilbeállítások',
+	'profile_detail' => 'Profil részlete',
 
     // Register page
     'accept_terms_label' => 'Elfogadom a feltételeket.',
