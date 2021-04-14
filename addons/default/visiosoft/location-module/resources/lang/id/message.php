f7b8eb591a9b612ac1629ab40f30cf9ef549580c
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:59:08 +0300

    New translations message.php (Indonesian)

--- addons/default/visiosoft/location-module/resources/lang/id/message.php
+++ addons/default/visiosoft/location-module/resources/lang/id/message.php
@@ -3,5 +3,5 @@
 return [
     'placeholder' => 'Ketik di sini untuk mencari',
     'null_msg' => 'Tidak ditemukan',
-    'choose_country' => 'Choose a Country',
+    'choose_country' => 'Pilih satu negara',
 ];
