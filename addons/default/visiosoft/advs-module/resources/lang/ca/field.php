8401150e0239a5495aa0986d75fdb2dc373b0233
Fatih Alp <mail@fatihalp.com>
2021-04-14 11:42:37 +0300

    New translations field.php (Catalan)

--- addons/default/visiosoft/advs-module/resources/lang/ca/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ca/field.php
@@ -211,7 +211,7 @@ return [
     'featured_ads' => "Anuncis destacats",
     'trending_ads' => 'Anuncis Trendind',
     'search_ad_result_prefix' => 'encès',
-    'search_ad_result_text' => '<strong>:count</strong> ads were found in your <strong>:name</strong> search',
+    'search_ad_result_text' => '<strong>: count</strong> s\'han trobat anuncis a la vostra cerca <strong>: name</strong>',
     'search_ads_result_message' => [
         'name' => ': compta els anuncis trobats per a: cerca de noms',
     ],
@@ -338,11 +338,11 @@ return [
     'default_owner_instruction' => 'El propietari passarà per defecte a l\'usuari actual si no se selecciona cap usuari',
 
     // Listing page
-    'ad_title' => 'Ad title',
-    'listing_date' => 'Listing Date',
-    'view' => 'View',
-    'ads_with_map' => 'Ads With Map',
-    'advanced_sorting' => 'Advanced Sorting',
+    'ad_title' => 'Títol de l\'anunci',
+    'listing_date' => 'Data de publicació',
+    'view' => 'Veure',
+    'ads_with_map' => 'Anuncis amb mapa',
+    'advanced_sorting' => 'Ordenació avançada',
 
     'free' => 'Gratuït',
     'ad_date' => 'Data de l\'anunci',
