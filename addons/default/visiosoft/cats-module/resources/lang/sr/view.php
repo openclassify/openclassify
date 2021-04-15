b2d3416d3a34c499e9a6db3df472b770b51b43c2
Fatih Alp <mail@fatihalp.com>
2021-04-15 15:47:50 +0300

    New translations view.php (Serbian (Cyrillic))

--- addons/default/visiosoft/cats-module/resources/lang/sr/view.php
+++ addons/default/visiosoft/cats-module/resources/lang/sr/view.php
@@ -2,4 +2,6 @@
 
 return [
     'clean_subcategories' => 'Чисте поткатегорије',
+    'ad_count_calculate' => 'Израчунавање броја огласа',
+    'cat_level_calculate' => 'Израчунајте ниво категорије',
 ];
