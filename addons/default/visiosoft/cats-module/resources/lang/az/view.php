f63498c5ec54bd144cd1fa41f0350cb58d069555
Fatih Alp <mail@fatihalp.com>
2021-04-15 15:48:05 +0300

    New translations view.php (Azerbaijani)

--- addons/default/visiosoft/cats-module/resources/lang/az/view.php
+++ addons/default/visiosoft/cats-module/resources/lang/az/view.php
@@ -2,4 +2,6 @@
 
 return [
     'clean_subcategories' => 'Təmiz Alt kateqoriyalar',
+    'ad_count_calculate' => 'Reklam sayını hesablayın',
+    'cat_level_calculate' => 'Kateqoriya Səviyyə Hesablayın',
 ];
