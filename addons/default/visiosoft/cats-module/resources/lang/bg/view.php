fed26154d4e7840f18b728d828ed42bd238bf9de
Fatih Alp <mail@fatihalp.com>
2021-04-15 15:44:53 +0300

    New translations view.php (Bulgarian)

--- addons/default/visiosoft/cats-module/resources/lang/bg/view.php
+++ addons/default/visiosoft/cats-module/resources/lang/bg/view.php
@@ -2,4 +2,6 @@
 
 return [
     'clean_subcategories' => 'Изчистване на подкатегории',
+    'ad_count_calculate' => 'Изчислете броя на рекламите',
+    'cat_level_calculate' => 'Изчисляване на ниво категория',
 ];
