7f40ca7d0a857cf89f50bf6851e265d10dcb7f19
Fatih Alp <mail@fatihalp.com>
2021-04-15 15:28:28 +0300

    New translations view.php (Portuguese)

--- addons/default/visiosoft/cats-module/resources/lang/pt/view.php
+++ addons/default/visiosoft/cats-module/resources/lang/pt/view.php
@@ -2,4 +2,6 @@
 
 return [
     'clean_subcategories' => 'Subcategorias limpas',
+    'ad_count_calculate' => 'Calcule a contagem de anúncios',
+    'cat_level_calculate' => 'Calcular nível de categoria',
 ];
