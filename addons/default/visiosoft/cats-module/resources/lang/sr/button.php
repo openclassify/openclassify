3fadf01753f9fe24edf9095a5f07ae67a5cc64cc
Fatih Alp <mail@fatihalp.com>
2021-04-20 23:59:04 +0300

    New translations button.php (Serbian (Latin))

--- addons/default/visiosoft/cats-module/resources/lang/sr/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/sr/button.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'new_category' => 'New Category',
-    'add_sub_category' => 'Add Sub Category',
-    'sub_category' => 'Show Sub Category',
-    'new_placeholderforsearch' => 'New Placeholderforsearch',
+    'new_category' => 'Nova kategorija',
+    'add_sub_category' => 'Dodaj potkategoriju',
+    'sub_category' => 'Prikaži potkategoriju',
+    'new_placeholderforsearch' => 'Novi placeholder za pretragu',
 ];
