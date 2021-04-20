2d20eae124aa1dbeaf5f73f72d3938ff73cdb447
Fatih Alp <mail@fatihalp.com>
2021-04-21 00:39:28 +0300

    New translations message.php (Serbian (Latin))

--- addons/default/visiosoft/media-field_type/resources/lang/sr/message.php
+++ addons/default/visiosoft/media-field_type/resources/lang/sr/message.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'choose_folder' => 'Which folder would you like to upload to?',
-    'upload' => 'Add images | Upload images | Attach a photo',
-    'choose_files' => 'Which files would you like to use?',
-    'no_files_selected' => 'No files selected.',
-    'no_uploads' => 'No files uploaded.',
-    'overwrite' => 'has already been uploaded. Would you like to overwrite it?',
-    'uploading' => 'Uploading',
-    'loading' => 'Loading',
-    'error_upload' => 'There was a problem uploading the file.',
-    'error_upload_docs' => 'There is no document upload support. Please contact your system administrator!',
+    'choose_folder' => 'U koju fasciklu biste želeli da otpremite?',
+    'upload' => 'Dodaj slike | Otpremi slike | Priložite fotografiju',
+    'choose_files' => 'Koje biste datoteke želeli da koristite?',
+    'no_files_selected' => 'Nije izabrana nijedna datoteka.',
+    'no_uploads' => 'Nije otpremljena nijedna datoteka.',
+    'overwrite' => 'je već postavljeno. Da li želite da ga prepišete?',
+    'uploading' => 'Otpremanje',
+    'loading' => 'Učitavanje',
+    'error_upload' => 'Došlo je do problema pri otpremanju datoteke.',
+    'error_upload_docs' => 'Ne postoji podrška za otpremanje dokumenata. Molimo kontaktirajte svog administratora sistema!',
 ];
