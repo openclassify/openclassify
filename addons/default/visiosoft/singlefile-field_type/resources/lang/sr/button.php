60a39b60a92c6cb6069596e9e6cd65c47e5266df
Fatih Alp <mail@fatihalp.com>
2021-04-21 01:30:23 +0300

    New translations button.php (Serbian (Latin))

--- addons/default/visiosoft/singlefile-field_type/resources/lang/sr/button.php
+++ addons/default/visiosoft/singlefile-field_type/resources/lang/sr/button.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'select_file' => 'Select File',
-    'upload'      => 'Upload',
-    'remove'      => 'Remove',
-    'change'      => 'Change',
+    'select_file' => 'Izaberite datoteku',
+    'upload'      => 'Otprеmi',
+    'remove'      => 'Ukloni',
+    'change'      => 'Promeni',
 ];
