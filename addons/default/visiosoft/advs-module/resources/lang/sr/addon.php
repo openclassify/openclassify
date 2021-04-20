7c931032feb2302434f56d52c45c9476b7f8b4e2
Fatih Alp <mail@fatihalp.com>
2021-04-20 15:04:47 +0300

    New translations addon.php (Serbian (Latin))

--- addons/default/visiosoft/advs-module/resources/lang/sr/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/sr/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Огласи',
-    'name'        => 'Модул за огласе',
-    'description' => 'Модул за огласе',
+    'title'       => 'Ads',
+    'name'        => 'Ads Module',
+    'description' => 'Ads Module',
 ];
