38af2a5354840491e28bbceda2e1dfe193a7de41
Fatih Alp <mail@fatihalp.com>
2021-04-21 00:39:29 +0300

    New translations validation.php (Serbian (Latin))

--- addons/default/visiosoft/media-field_type/resources/lang/sr/validation.php
+++ addons/default/visiosoft/media-field_type/resources/lang/sr/validation.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'valid_disk' => 'The configured upload disk for :attribute does not exist.',
-	'unsupported' => 'This area is for image files only. You can add different file types in the description field.'
+    'valid_disk' => 'Konfigurisani disk za otpremanje za :atribut ne postoji.',
+	'unsupported' => 'Ovo područje je samo za datoteke slika. U polje za opis možete dodati različite tipove datoteka.'
 ];
