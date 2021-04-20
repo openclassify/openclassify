e22d9ee6d657e75d91cb7233c4d1c2612538ff9d
Fatih Alp <mail@fatihalp.com>
2021-04-20 23:50:41 +0300

    New translations errors.php (Serbian (Latin))

--- addons/default/visiosoft/base-theme/resources/lang/sr/errors.php
+++ addons/default/visiosoft/base-theme/resources/lang/sr/errors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    '404_title' => 'Not Found',
-    '403_title' => 'Forbidden',
-    '404_message' => 'The page you were looking for was not found.',
-    '403_message' => 'Access to this resources on the site is denied!',
-    'home_page' => 'Go to Home Page',
+    '404_title' => 'Nije pronađeno',
+    '403_title' => 'Zabranjeno',
+    '404_message' => 'Stranica koju ste tražili nije pronađena.',
+    '403_message' => 'Pristup ovim resursima na web lokaciji je odbijen!',
+    'home_page' => 'Idi na početnu stranicu',
 ];
