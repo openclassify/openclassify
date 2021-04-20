d954bbc030c4cfe2f330ce796c8a2a38dff3268d
Fatih Alp <mail@fatihalp.com>
2021-04-20 15:05:06 +0300

    New translations addon.php (Serbian (Latin))

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/sr/addon.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/sr/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Висиософт Админ',
-    'name'        => 'Висиософт администраторска тема',
-    'description' => 'Званична администраторска тема за ОпенЦлассифи.',
+    'title'       => 'Visiosoft Admin',
+    'name'        => 'Visiosoft Admin Theme',
+    'description' => 'The official admin theme for OpenClassify.',
 ];
