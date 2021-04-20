d1f5b177196298fc0691e0d67919a2be04f47cc0
Fatih Alp <mail@fatihalp.com>
2021-04-21 00:12:51 +0300

    New translations view.php (Serbian (Latin))

--- addons/default/visiosoft/cats-module/resources/lang/sr/view.php
+++ addons/default/visiosoft/cats-module/resources/lang/sr/view.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'clean_subcategories' => 'Clean Subcategories',
-    'ad_count_calculate' => 'Ad Count Calculate',
-    'cat_level_calculate' => 'Category Level Calculate',
+    'clean_subcategories' => 'Obriši potkategorije',
+    'ad_count_calculate' => 'Izračunaj broj Oglasa',
+    'cat_level_calculate' => 'Nivo kategorije za računanje',
 ];
