8c4406a5242a49cf38ff1b8ccd39f57ffa80d8c1
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:55:15 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'Εμφάνιση υποκατηγοριών σε προβολή για κινητά',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
