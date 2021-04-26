82921b9dd33f43a8b502aeae2e93f960a5d404c3
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:55:18 +0300

    New translations setting.php (Korean)

--- addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ko/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => '모바일보기에 하위 범주 표시',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
