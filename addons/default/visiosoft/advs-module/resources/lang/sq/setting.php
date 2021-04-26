f54ac826f46e06fc338f3235c3e4d8be0baa9bac
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:55:26 +0300

    New translations setting.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'Shfaq nënkategoritë në pamjen e celularëve',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
