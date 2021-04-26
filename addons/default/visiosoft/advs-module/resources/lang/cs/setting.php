83c2e040c9603ec93a2d2ddf3e60ca57b1cb37ef
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:54:54 +0300

    New translations setting.php (Czech)

--- addons/default/visiosoft/advs-module/resources/lang/cs/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/cs/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'Zobrazit podkategorie v mobilním zobrazení',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
