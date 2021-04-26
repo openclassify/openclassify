cb24fe36a1e3631624bd092518d0f752191e30aa
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:55:11 +0300

    New translations setting.php (Spanish)

--- addons/default/visiosoft/advs-module/resources/lang/es/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/es/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'Mostrar subcategorías en la vista móvil',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
