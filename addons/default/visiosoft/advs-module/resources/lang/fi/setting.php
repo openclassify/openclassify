4405d000df726d2e9ad7c2d1ae4f64f820c34ad3
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:54:56 +0300

    New translations setting.php (Finnish)

--- addons/default/visiosoft/advs-module/resources/lang/fi/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/fi/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'Näytä alaluokat mobiilinäkymässä',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
