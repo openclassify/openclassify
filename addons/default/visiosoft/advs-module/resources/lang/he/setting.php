af85a3c5a83834ab9ff94bae584d0083cf3f9ad8
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:55:16 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'הצג קטגוריות משנה בתצוגת הנייד',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
