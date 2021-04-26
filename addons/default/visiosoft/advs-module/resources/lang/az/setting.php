1afd407bcc57a2bfdffaf2138fc140693850c430
Fatih Alp <mail@fatihalp.com>
2021-04-26 12:54:47 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/setting.php
@@ -264,5 +264,8 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'Mobil görünüşdə alt kateqoriyalar göstərin',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
 ];
