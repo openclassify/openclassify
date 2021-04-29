5e2e10be1864898ca582378ac3f98dfd3e8c33a4
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:20:57 +0300

    New translations config.php (Danish)

--- addons/default/visiosoft/multiple-field_type/resources/lang/da/config.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/da/config.php
@@ -2,28 +2,28 @@
 
 return [
     'related'    => [
-        'label'        => 'Related Stream',
-        'instructions' => 'Specify the related stream entries to display in the dropdown.',
+        'label'        => 'Relateret strøm',
+        'instructions' => 'Angiv de relaterede strømindgange, der skal vises i rullemenuen.',
     ],
     'mode'       => [
-        'label'  => 'Input Mode',
+        'label'  => 'Input-tilstand',
         'option' => [
-            'tags'       => 'Tags',
-            'lookup'     => 'Lookup',
-            'checkboxes' => 'Checkboxes',
+            'tags'       => 'Mærker',
+            'lookup'     => 'Kig op',
+            'checkboxes' => 'Afkrydsningsfelter',
         ],
     ],
     'min'        => [
-        'label'        => 'Minimum Selections',
-        'instructions' => 'Specify the minimum number of allowed selections.',
+        'label'        => 'Minimumsvalg',
+        'instructions' => 'Angiv det mindste antal tilladte valg.',
     ],
     'max'        => [
-        'label'        => 'Maximum Selections',
-        'instructions' => 'Specify the maximum number of allowed selections.',
+        'label'        => 'Maksimale valg',
+        'instructions' => 'Angiv det maksimale antal tilladte valg.',
     ],
     'title_name' => [
-        'label'        => 'Title Field',
-        'placeholder'  => 'first_name',
-        'instructions' => 'Specify the <strong>slug</strong> of field to display for dropdown/search options.<br>You can specify parsable titles like <strong>{entry.first_name} {entry.last_name}</strong><br>The related stream\'s title column will be used by default.',
+        'label'        => 'Titelfelt',
+        'placeholder'  => 'fornavn',
+        'instructions' => 'Angiv <strong>slug</strong> i feltet, der skal vises, for rullemenu / søgemuligheder.<br>Du kan specificere parsable titler som <strong>{entry.first_name} {entry.last_name}</strong><br>Den relaterede streams titelsøjle bruges som standard.',
     ],
 ];
