df6b1c3f805ff3b37e605d537c07ff3d9d09a468
Fatih Alp <mail@fatihalp.com>
2021-04-29 11:32:58 +0300

    New translations field.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/field.php
@@ -337,6 +337,7 @@ return [
     'ad_location' => 'Advertentielocatie',
     'owner' => 'Eigenaar',
     'default_owner_instruction' => 'De eigenaar wordt standaard de huidige gebruiker als er geen gebruiker is geselecteerd',
+    'create_page_subtitle' => 'Create the ad flawlessly by entering detailed information about your ad.',
 
     // Listing page
     'ad_title' => 'Voeg titel toe',
@@ -368,4 +369,5 @@ return [
     'old_price' => [
         'name' => 'Oude prijs'
     ],
+	'save_search' => 'Save Search',
 ];
