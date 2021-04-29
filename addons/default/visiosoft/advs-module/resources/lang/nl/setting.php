57a97d139bc47a693f772dcad2ea597005e03b45
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:35:45 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/nl/setting.php
@@ -266,10 +266,10 @@ return [
         'name' => 'Toon subcategorieën op mobiele weergave',
     ],
 	'show_price_to_members_only' => [
-		'name' => 'Show Price To Members Only',
+		'name' => 'Toon prijs alleen aan leden',
 	],
 	'hide_ad_cat' => [
-		'name' => 'Hide Categories',
-		'instructions' => 'Hide categories in ads list'
+		'name' => 'Categorieën verbergen',
+		'instructions' => 'Verberg categorieën in de lijst met advertenties'
 	]
 ];
