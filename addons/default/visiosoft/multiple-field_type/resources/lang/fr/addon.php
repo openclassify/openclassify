e963bb6698055112e510447514732bcf76a00605
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:54:26 +0300

    New translations addon.php (French)

--- addons/default/visiosoft/multiple-field_type/resources/lang/fr/addon.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/fr/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Multiple',
-    'name'        => 'Multiple Field Type',
-    'description' => 'A multiple relationship field type.',
+    'title'       => 'Plusieurs',
+    'name'        => 'Type de champ multiple',
+    'description' => 'Un type de champ de relations multiples.',
 ];
