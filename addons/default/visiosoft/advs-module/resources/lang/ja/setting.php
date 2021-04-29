4653c673b5e14d4e90ef9579aa5ca3b4847e39e4
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:34:38 +0300

    New translations setting.php (Japanese)

--- addons/default/visiosoft/advs-module/resources/lang/ja/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ja/setting.php
@@ -266,10 +266,10 @@ return [
         'name' => 'モバイルビューでサブカテゴリを表示する',
     ],
 	'show_price_to_members_only' => [
-		'name' => 'Show Price To Members Only',
+		'name' => 'メンバーのみに価格を表示',
 	],
 	'hide_ad_cat' => [
-		'name' => 'Hide Categories',
-		'instructions' => 'Hide categories in ads list'
+		'name' => 'カテゴリを非表示',
+		'instructions' => '広告リストのカテゴリを非表示'
 	]
 ];
