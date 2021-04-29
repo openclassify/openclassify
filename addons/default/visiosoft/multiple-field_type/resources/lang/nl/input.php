717e1fcc1eef56c1115370556847eca28bab4504
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:56:48 +0300

    New translations input.php (Dutch)

--- addons/default/visiosoft/multiple-field_type/resources/lang/nl/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/nl/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Kies een optie...',
+    'help'        => 'Scheid waarden met een komma of door op "Enter" te drukken.',
 ];
