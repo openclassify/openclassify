5e7559dfa26521d87981e10e403b26db440b0ebf
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:20:54 +0300

    New translations input.php (Hebrew)

--- addons/default/visiosoft/multiple-field_type/resources/lang/he/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/he/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'בחר אפשרות...',
+    'help'        => 'הפרד את הערכים באמצעות פסיק או על ידי לחיצה על "Enter".',
 ];
