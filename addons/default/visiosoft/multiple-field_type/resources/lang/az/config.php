de542ec5338d40798df41a75062c83a12e59dc10
Fatih Alp <mail@fatihalp.com>
2021-04-29 16:09:22 +0300

    New translations config.php (Azerbaijani)

---
+++ addons/default/visiosoft/multiple-field_type/resources/lang/az/config.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'related'    => [
+        'label'        => 'Əlaqəli axın',
+        'instructions' => 'Açılan siyahıda göstəriləcək əlaqəli axın girişlərini göstərin.',
+    ],
+    'mode'       => [
+        'label'  => 'Giriş rejimi',
+        'option' => [
+            'tags'       => 'Teqlər',
+            'lookup'     => 'Axtarış',
+            'checkboxes' => 'Onay qutuları',
+        ],
+    ],
+    'min'        => [
+        'label'        => 'Minimum seçimlər',
+        'instructions' => 'Minimum icazə verilən seçim sayını göstərin.',
+    ],
+    'max'        => [
+        'label'        => 'Maksimum seçimlər',
+        'instructions' => 'İcazəli seçimlərin maksimum sayını göstərin.',
+    ],
+    'title_name' => [
+        'label'        => 'Başlıq sahəsi',
+        'placeholder'  => 'ilk_ad',
+        'instructions' => 'Açılan / axtarış seçimləri üçün göstəriləcək sahənin <strong>slug</strong> göstərin.<br> <strong>{entry.first_name} {entry.last_name}</strong><br>kimi ayrıştırılabilir başlıqlar təyin edə bilərsiniz. Müvafiq yayının başlıq sütunu varsayılan olaraq istifadə ediləcək.',
+    ],
+];
