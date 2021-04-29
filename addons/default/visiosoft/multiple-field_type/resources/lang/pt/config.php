9acb5887e0dc7c2ff5c1b4e85a6e1ef367386536
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:56:36 +0300

    New translations config.php (Portuguese)

--- addons/default/visiosoft/multiple-field_type/resources/lang/pt/config.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/pt/config.php
@@ -2,28 +2,28 @@
 
 return [
     'related'    => [
-        'label'        => 'Related Stream',
-        'instructions' => 'Specify the related stream entries to display in the dropdown.',
+        'label'        => 'Stream Relacionado',
+        'instructions' => 'Especifique as entradas de fluxo relacionadas a serem exibidas na lista suspensa.',
     ],
     'mode'       => [
-        'label'  => 'Input Mode',
+        'label'  => 'Modo de entrada',
         'option' => [
-            'tags'       => 'Tags',
-            'lookup'     => 'Lookup',
-            'checkboxes' => 'Checkboxes',
+            'tags'       => 'Tag',
+            'lookup'     => 'Olho para cima',
+            'checkboxes' => 'Caixas de seleção',
         ],
     ],
     'min'        => [
-        'label'        => 'Minimum Selections',
-        'instructions' => 'Specify the minimum number of allowed selections.',
+        'label'        => 'Seleções mínimas',
+        'instructions' => 'Especifique o número mínimo de seleções permitidas.',
     ],
     'max'        => [
-        'label'        => 'Maximum Selections',
-        'instructions' => 'Specify the maximum number of allowed selections.',
+        'label'        => 'Seleções máximas',
+        'instructions' => 'Especifique o número máximo de seleções permitidas.',
     ],
     'title_name' => [
-        'label'        => 'Title Field',
-        'placeholder'  => 'first_name',
-        'instructions' => 'Specify the <strong>slug</strong> of field to display for dropdown/search options.<br>You can specify parsable titles like <strong>{entry.first_name} {entry.last_name}</strong><br>The related stream\'s title column will be used by default.',
+        'label'        => 'Campo de Título',
+        'placeholder'  => 'primeiro nome',
+        'instructions' => 'Especifique <strong>slug</strong> do campo a ser exibido nas opções suspensas / de pesquisa.<br>Você pode especificar títulos analisáveis como <strong>{entry.first_name} {entry.last_name}</strong><br>A coluna de título do fluxo relacionado será usada por padrão.',
     ],
 ];
