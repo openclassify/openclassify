b4795248a0e50d6f4b625a373c0902e8b3cfaea0
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:55:55 +0300

    New translations input.php (Japanese)

--- addons/default/visiosoft/multiple-field_type/resources/lang/ja/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/ja/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'オプションを選択...',
+    'help'        => '値はコンマで区切るか、「Enter」を押してください。',
 ];
