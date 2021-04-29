39d808f6627604499be39ff039939ee6cff6fddf
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:20:59 +0300

    New translations input.php (German)

--- addons/default/visiosoft/multiple-field_type/resources/lang/de/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/de/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Wähle eine Option...',
+    'help'        => 'Trennen Sie Werte durch Komma oder durch Drücken der Eingabetaste.',
 ];
