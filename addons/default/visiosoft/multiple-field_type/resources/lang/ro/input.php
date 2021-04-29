faebd118eb791a04ffcf0f6c330695be33fd5c04
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:54:13 +0300

    New translations input.php (Romanian)

--- addons/default/visiosoft/multiple-field_type/resources/lang/ro/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/ro/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Alege o opțiune...',
+    'help'        => 'Separați valorile cu o virgulă sau apăsând pe „Enter”.',
 ];
