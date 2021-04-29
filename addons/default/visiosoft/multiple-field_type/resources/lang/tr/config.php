7b1e7982cdb9b8fd6dd0ee046a0d78ffac78dd50
Fatih Alp <mail@fatihalp.com>
2021-04-29 14:52:24 +0300

    New translations config.php (Turkish)

--- addons/default/visiosoft/multiple-field_type/resources/lang/tr/config.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/tr/config.php
@@ -1,23 +1,29 @@
 <?php
 
 return [
-    'related' => [
+    'related'    => [
         'label'        => 'İlişkili Stream',
         'instructions' => 'Açılırlistede gösterilecek ilişkili stream girişlerini belirtin.',
     ],
-    'mode'    => [
+    'mode'       => [
         'label'  => 'Giriş Modu',
         'option' => [
-            'tags'   => 'Etiketler',
-            'lookup' => 'Bakış',
+            'tags'       => 'Etiketler',
+            'lookup'     => 'Bakış',
+            'checkboxes' => 'Onay Kutuları',
         ],
     ],
-    'min'     => [
+    'min'        => [
         'label'        => 'Minimum seçim sayısı',
         'instructions' => 'İzin verilen minimum seçim sayısını belirtin.',
     ],
-    'max'     => [
+    'max'        => [
         'label'        => 'Maksimum seçim sayısı',
         'instructions' => 'İzin verilen maksimum seçim sayısını belirtin.',
+    ],
+    'title_name' => [
+        'label'        => 'Başlık Alanı',
+        'placeholder'  => 'İsim',
+        'instructions' => 'Açılır menü / arama seçeneklerini görüntülemek için alanın <strong>slug</strong> değerini belirtin.<br> <strong>{entry.first_name} {entry.last_name}</strong><br> gibi sütun isimleri belirtebilirsiniz.',
     ],
 ];
