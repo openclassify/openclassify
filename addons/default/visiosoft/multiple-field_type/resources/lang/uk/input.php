d4c64b0d69b4c7e9a9766021db6d8640277c97c2
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:56:20 +0300

    New translations input.php (Ukrainian)

--- addons/default/visiosoft/multiple-field_type/resources/lang/uk/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/uk/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Виберіть варіант ...',
+    'help'        => 'Відокремте значення комою або натиснувши "Enter".',
 ];
