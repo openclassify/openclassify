b4c6d780064ecefce5e1b4bd7076e17e76562af8
Fatih Alp <mail@fatihalp.com>
2021-04-29 15:41:07 +0300

    New translations setting.php (Serbian (Latin))

--- addons/default/visiosoft/advs-module/resources/lang/sr/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/sr/setting.php
@@ -264,5 +264,12 @@ return [
     ],
     'show_subcats_mobile' => [
         'name' => 'Prikažite potkategorije na mobilnom prikazu',
-    ]
+    ],
+	'show_price_to_members_only' => [
+		'name' => 'Show Price To Members Only',
+	],
+	'hide_ad_cat' => [
+		'name' => 'Hide Categories',
+		'instructions' => 'Hide categories in ads list'
+	]
 ];
