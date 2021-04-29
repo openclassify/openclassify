a9bdf1578bee1ee2a3b26ea61ba4c798234d6ea1
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:54:39 +0300

    New translations input.php (Afrikaans)

--- addons/default/visiosoft/multiple-field_type/resources/lang/af/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/af/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Kies \'n opsie ...',
+    'help'        => 'Skei waardes met \'n komma of deur op "Enter" te druk.',
 ];
