f1c601867e7269be126f65759a9639a2c3d097f4
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:55:47 +0300

    New translations addon.php (Korean)

--- addons/default/visiosoft/multiple-field_type/resources/lang/ko/addon.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/ko/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Multiple',
-    'name'        => 'Multiple Field Type',
-    'description' => 'A multiple relationship field type.',
+    'title'       => '배수',
+    'name'        => '다중 필드 유형',
+    'description' => '다중 관계 필드 유형입니다.',
 ];
