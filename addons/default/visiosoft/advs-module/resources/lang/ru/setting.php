89855af70d47e1eb79c7c99e21006358a09718f2
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:39:33 +0300

    New translations setting.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/setting.php
@@ -266,10 +266,10 @@ return [
         'name' => 'Показывать подкатегории в мобильном представлении',
     ],
 	'show_price_to_members_only' => [
-		'name' => 'Show Price To Members Only',
+		'name' => 'Показывать цену только участникам',
 	],
 	'hide_ad_cat' => [
-		'name' => 'Hide Categories',
-		'instructions' => 'Hide categories in ads list'
+		'name' => 'Скрыть категории',
+		'instructions' => 'Скрыть категории в списке объявлений'
 	]
 ];
