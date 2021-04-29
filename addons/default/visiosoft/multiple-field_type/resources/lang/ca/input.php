a280a9441a1cea053930abd26d6bf1080f0fd182
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:55:00 +0300

    New translations input.php (Catalan)

--- addons/default/visiosoft/multiple-field_type/resources/lang/ca/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/ca/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Trieu una opció ...',
+    'help'        => 'Separeu els valors amb una coma o prement "Retorn".',
 ];
