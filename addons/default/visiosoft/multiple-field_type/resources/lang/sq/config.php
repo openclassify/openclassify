deb05ae57a22f78dd135c7b4a121588f53661ba9
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:56:04 +0300

    New translations config.php (Albanian)

--- addons/default/visiosoft/multiple-field_type/resources/lang/sq/config.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/sq/config.php
@@ -2,28 +2,28 @@
 
 return [
     'related'    => [
-        'label'        => 'Related Stream',
-        'instructions' => 'Specify the related stream entries to display in the dropdown.',
+        'label'        => 'Rryma e ngjashme',
+        'instructions' => 'Specifikoni shënimet përkatëse të transmetimit për t\'u shfaqur në dropdown.',
     ],
     'mode'       => [
-        'label'  => 'Input Mode',
+        'label'  => 'Modaliteti i hyrjes',
         'option' => [
-            'tags'       => 'Tags',
-            'lookup'     => 'Lookup',
-            'checkboxes' => 'Checkboxes',
+            'tags'       => 'Etiketat',
+            'lookup'     => 'Kërkim',
+            'checkboxes' => 'Kutitë e zgjedhjes',
         ],
     ],
     'min'        => [
-        'label'        => 'Minimum Selections',
-        'instructions' => 'Specify the minimum number of allowed selections.',
+        'label'        => 'Përzgjedhjet minimale',
+        'instructions' => 'Specifikoni numrin minimal të zgjedhjeve të lejuara.',
     ],
     'max'        => [
-        'label'        => 'Maximum Selections',
-        'instructions' => 'Specify the maximum number of allowed selections.',
+        'label'        => 'Përzgjedhjet maksimale',
+        'instructions' => 'Specifikoni numrin maksimal të zgjedhjeve të lejuara.',
     ],
     'title_name' => [
-        'label'        => 'Title Field',
-        'placeholder'  => 'first_name',
-        'instructions' => 'Specify the <strong>slug</strong> of field to display for dropdown/search options.<br>You can specify parsable titles like <strong>{entry.first_name} {entry.last_name}</strong><br>The related stream\'s title column will be used by default.',
+        'label'        => 'Fusha e Titullit',
+        'placeholder'  => 'Emri',
+        'instructions' => 'Specifikoni <strong>goditje e fortë</strong> e fushës për të ekranit për mundësitë dropdown / e kërkimit.<br>Mund të specifikoni tituj të analizueshëm si <strong>{entry.first_name} {entry.last_name}</strong><br>Kolona e titullit të rrymës përkatëse do të përdoret si parazgjedhje.',
     ],
 ];
