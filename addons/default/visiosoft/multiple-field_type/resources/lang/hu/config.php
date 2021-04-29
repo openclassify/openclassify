03a029e9e62e774a3555267eb162aa3581736869
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:21:08 +0300

    New translations config.php (Hungarian)

--- addons/default/visiosoft/multiple-field_type/resources/lang/hu/config.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/hu/config.php
@@ -2,28 +2,28 @@
 
 return [
     'related'    => [
-        'label'        => 'Related Stream',
-        'instructions' => 'Specify the related stream entries to display in the dropdown.',
+        'label'        => 'Kapcsolódó adatfolyam',
+        'instructions' => 'Adja meg a legördülő menüben megjelenítendő kapcsolódó adatfolyamokat.',
     ],
     'mode'       => [
-        'label'  => 'Input Mode',
+        'label'  => 'Bemeneti mód',
         'option' => [
-            'tags'       => 'Tags',
-            'lookup'     => 'Lookup',
-            'checkboxes' => 'Checkboxes',
+            'tags'       => 'Címkék',
+            'lookup'     => 'Nézz fel',
+            'checkboxes' => 'Jelölőnégyzetek',
         ],
     ],
     'min'        => [
-        'label'        => 'Minimum Selections',
-        'instructions' => 'Specify the minimum number of allowed selections.',
+        'label'        => 'Minimális választások',
+        'instructions' => 'Adja meg a megengedett kiválasztások minimális számát.',
     ],
     'max'        => [
-        'label'        => 'Maximum Selections',
-        'instructions' => 'Specify the maximum number of allowed selections.',
+        'label'        => 'Maximális választási lehetőségek',
+        'instructions' => 'Adja meg a megengedett kiválasztások maximális számát.',
     ],
     'title_name' => [
-        'label'        => 'Title Field',
-        'placeholder'  => 'first_name',
-        'instructions' => 'Specify the <strong>slug</strong> of field to display for dropdown/search options.<br>You can specify parsable titles like <strong>{entry.first_name} {entry.last_name}</strong><br>The related stream\'s title column will be used by default.',
+        'label'        => 'Cím mező',
+        'placeholder'  => 'keresztnév',
+        'instructions' => 'Adja meg a <strong></strong> mezõjét a megjelenítendõ legördülõ / keresési opciókhoz.<br>Megadhat értelmezhető címeket, például <strong>{entry.first_name} {entry.last_name}</strong><br>Alapértelmezés szerint a kapcsolódó adatfolyam oszlopát fogja használni.',
     ],
 ];
