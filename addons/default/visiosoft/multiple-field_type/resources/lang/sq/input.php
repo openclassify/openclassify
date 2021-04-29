2442e1106a641b93fdfc2bf61470be5bdfa2c684
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:56:03 +0300

    New translations input.php (Albanian)

--- addons/default/visiosoft/multiple-field_type/resources/lang/sq/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/sq/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Zgjidhni një opsion ...',
+    'help'        => 'Ndani vlerat me një presje ose duke shtypur "Enter".',
 ];
