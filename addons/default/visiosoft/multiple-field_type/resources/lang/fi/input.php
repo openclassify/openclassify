1e8117c36c3cf1982b082cd81970812e2c8a3a76
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:20:50 +0300

    New translations input.php (Finnish)

--- addons/default/visiosoft/multiple-field_type/resources/lang/fi/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/fi/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Valitse vaihtoehto...',
+    'help'        => 'Erota arvot pilkulla tai painamalla "Enter".',
 ];
