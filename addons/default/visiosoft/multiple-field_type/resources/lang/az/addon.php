e620c2eea3c0518728aec0913136f2bb6ccc298d
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:59:18 +0300

    New translations addon.php (Azerbaijani)

--- addons/default/visiosoft/multiple-field_type/resources/lang/az/addon.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/az/addon.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'title'       => 'Multiple',
-    'name'        => 'Multiple Field Type',
-    'description' => 'A multiple relationship field type.',
+    'title'       => 'Çoxdur',
+    'name'        => 'Birden çox sahə növü',
+    'description' => 'Çoxsaylı münasibət sahəsi.',
 ];
