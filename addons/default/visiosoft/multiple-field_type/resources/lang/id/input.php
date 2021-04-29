8eadd1b8eb360fe82196741719d1a9a74842202a
Fatih Alp <mail@fatihalp.com>
2021-04-29 12:59:40 +0300

    New translations input.php (Indonesian)

--- addons/default/visiosoft/multiple-field_type/resources/lang/id/input.php
+++ addons/default/visiosoft/multiple-field_type/resources/lang/id/input.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'placeholder' => 'Choose an option...',
-    'help'        => 'Separate values with a comma or by pressing "Enter".',
+    'placeholder' => 'Pilih salah satu opsi ...',
+    'help'        => 'Pisahkan nilai dengan koma atau dengan menekan "Enter".',
 ];
