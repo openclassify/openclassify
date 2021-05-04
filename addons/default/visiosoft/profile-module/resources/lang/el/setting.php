f72aec443393ecdb634cfb5d56febc786dc550e0
Fatih Alp <mail@fatihalp.com>
2021-05-04 11:46:14 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/profile-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/el/setting.php
@@ -48,6 +48,11 @@ return [
         'instructions' => 'Απόκρυψη του πεδίου τύπου μητρώου στη σελίδα επεξεργασίας προφίλ.',
     ],
     'required_district' => [
-        'name' => 'Απαιτούμενη περιοχή'
+        'name' => 'Απαιτούμενη περιοχή',
+        'instructions' => 'Adding an address represents the district obligation.',
+    ],
+    'show_extends_actions' => [
+        'name' => 'Show Extends Actions',
+        'instructions' => 'Represents the Extends options available on the my profile Ads page.',
     ],
 ];
