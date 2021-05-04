dc1bf7b8fb2e6462fbd3459c1331b3ae4a48000f
Fatih Alp <mail@fatihalp.com>
2021-05-04 11:45:45 +0300

    New translations setting.php (Dutch)

--- addons/default/visiosoft/profile-module/resources/lang/nl/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/nl/setting.php
@@ -48,6 +48,11 @@ return [
         'instructions' => 'Verbergt het registertypeveld op de profielbewerkingspagina.',
     ],
     'required_district' => [
-        'name' => 'Vereist district'
+        'name' => 'Vereist district',
+        'instructions' => 'Adding an address represents the district obligation.',
+    ],
+    'show_extends_actions' => [
+        'name' => 'Show Extends Actions',
+        'instructions' => 'Represents the Extends options available on the my profile Ads page.',
     ],
 ];
