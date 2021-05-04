9a41957b4544d153ec8d501c3717985351f5af55
Fatih Alp <mail@fatihalp.com>
2021-05-04 11:46:16 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/profile-module/resources/lang/he/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/he/setting.php
@@ -48,6 +48,11 @@ return [
         'instructions' => 'מסתיר את שדה סוג הרישום בדף עריכת הפרופיל.',
     ],
     'required_district' => [
-        'name' => 'מחוז חובה'
+        'name' => 'מחוז חובה',
+        'instructions' => 'Adding an address represents the district obligation.',
+    ],
+    'show_extends_actions' => [
+        'name' => 'Show Extends Actions',
+        'instructions' => 'Represents the Extends options available on the my profile Ads page.',
     ],
 ];
