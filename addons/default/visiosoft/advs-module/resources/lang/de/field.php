d476e44aef116e936b76a7b564442cf1db59b2fd
Fatih Alp <mail@fatihalp.com>
2021-05-04 11:29:36 +0300

    New translations field.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/field.php
@@ -346,6 +346,7 @@ return [
     'ads_with_map' => 'Anzeigen mit Karte',
     'advanced_sorting' => 'Erweiterte Sortierung',
     'filter_by_word' => 'Nach Wort filtern',
+    'filter_by' => 'Filter by :name',
 
     'free' => 'Frei',
     'ad_date' => 'Anzeigedatum',
