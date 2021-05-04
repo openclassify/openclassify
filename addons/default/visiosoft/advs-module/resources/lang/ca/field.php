684f59ae7270e40f63c495e5dda4ee530428f819
Fatih Alp <mail@fatihalp.com>
2021-05-04 11:29:43 +0300

    New translations field.php (Catalan)

--- addons/default/visiosoft/advs-module/resources/lang/ca/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ca/field.php
@@ -345,6 +345,7 @@ return [
     'ads_with_map' => 'Anuncis amb mapa',
     'advanced_sorting' => 'Ordenació avançada',
     'filter_by_word' => 'Filtra per paraula',
+    'filter_by' => 'Filter by :name',
 
     'free' => 'Gratuït',
     'ad_date' => 'Data de l\'anunci',
