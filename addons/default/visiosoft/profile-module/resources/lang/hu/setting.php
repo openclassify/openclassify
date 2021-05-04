0d65dcfbe940d0c067f2a37e337235e7dbfc5343
Fatih Alp <mail@fatihalp.com>
2021-05-04 11:45:59 +0300

    New translations setting.php (Hungarian)

--- addons/default/visiosoft/profile-module/resources/lang/hu/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/hu/setting.php
@@ -48,6 +48,11 @@ return [
         'instructions' => 'Elrejti a regisztertípus mezőt a profilszerkesztő oldalon.',
     ],
     'required_district' => [
-        'name' => 'Szükséges körzet'
+        'name' => 'Szükséges körzet',
+        'instructions' => 'Adding an address represents the district obligation.',
+    ],
+    'show_extends_actions' => [
+        'name' => 'Show Extends Actions',
+        'instructions' => 'Represents the Extends options available on the my profile Ads page.',
     ],
 ];
