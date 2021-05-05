0ac0c9b28d16f9e25aa1c8351d703f9e0586961c
Fatih Alp <mail@fatihalp.com>
2021-05-05 11:56:51 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/profile-module/resources/lang/it/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/it/setting.php
@@ -49,10 +49,10 @@ return [
     ],
     'required_district' => [
         'name' => 'Distretto richiesto',
-        'instructions' => 'Adding an address represents the district obligation.',
+        'instructions' => 'L\'aggiunta di un indirizzo rappresenta l\'obbligo del distretto.',
     ],
     'show_extends_actions' => [
-        'name' => 'Show Extends Actions',
-        'instructions' => 'Represents the Extends options available on the my profile Ads page.',
+        'name' => 'Mostra azioni estensioni',
+        'instructions' => 'Rappresenta le opzioni Estende disponibili nella pagina Annunci del mio profilo.',
     ],
 ];
