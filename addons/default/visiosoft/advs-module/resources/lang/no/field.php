be9896d88f23edacbc85a48cd2ca01716596d1e7
Fatih Alp <mail@fatihalp.com>
2021-05-05 11:33:54 +0300

    New translations field.php (Norwegian)

--- addons/default/visiosoft/advs-module/resources/lang/no/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/no/field.php
@@ -345,7 +345,7 @@ return [
     'ads_with_map' => 'Annonser med kart',
     'advanced_sorting' => 'Avansert sortering',
     'filter_by_word' => 'Filtrer etter ord',
-    'filter_by' => 'Filter by :name',
+    'filter_by' => 'Filtrer etter: navn',
 
     'free' => 'Gratis',
     'ad_date' => 'Annonsedato',
