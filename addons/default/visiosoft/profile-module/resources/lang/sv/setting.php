59467e7e26fef7e5c6f7909e4aca8d03d4eca358
Fatih Alp <mail@fatihalp.com>
2021-05-05 11:59:56 +0300

    New translations setting.php (Swedish)

--- addons/default/visiosoft/profile-module/resources/lang/sv/setting.php
+++ addons/default/visiosoft/profile-module/resources/lang/sv/setting.php
@@ -49,10 +49,10 @@ return [
     ],
     'required_district' => [
         'name' => 'Obligatoriskt distrikt',
-        'instructions' => 'Adding an address represents the district obligation.',
+        'instructions' => 'Att lägga till en adress representerar distriktsförpliktelsen.',
     ],
     'show_extends_actions' => [
-        'name' => 'Show Extends Actions',
-        'instructions' => 'Represents the Extends options available on the my profile Ads page.',
+        'name' => 'Visa förlänger åtgärder',
+        'instructions' => 'Representerar de alternativ som finns tillgängliga på sidan Min profilannonser.',
     ],
 ];
