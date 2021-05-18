4ccf4057d10d1e26e70eabe40ccc589b6fce6674
Fatih Alp <fatihalp89@gmail.com>
2021-05-18 14:07:13 +0300

    include_cities_sitemap make default false

--- addons/default/visiosoft/cats-module/resources/config/settings.php
+++ addons/default/visiosoft/cats-module/resources/config/settings.php
@@ -4,14 +4,14 @@ return [
     'sitemap_dividing_number' => [
         "type"   => "anomaly.field_type.integer",
         "config" => [
-            "default_value" => 5000,
+            "default_value" => 5000
         ]
     ],
     "include_cities_sitemap" => [
         "type"   => "anomaly.field_type.boolean",
         "config" => [
-            "default_value" => true,
-            "mode"          => "checkbox",
+            "default_value" => false,
+            "mode"          => "checkbox"
         ]
     ]
 ];
