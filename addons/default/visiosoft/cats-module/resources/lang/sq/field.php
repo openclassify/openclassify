0a9c4d234f7b820ba288d42a652bbdf183581206
Fatih Alp <mail@fatihalp.com>
2021-05-20 13:30:33 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/cats-module/resources/lang/sq/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/sq/field.php
@@ -33,4 +33,5 @@ return [
     'please_wait' => 'Ju lutemi prisni. Zgjedhja e kategorive nën',
     'category_selection' => 'Zgjedhja e kategorisë',
     'go_to_parent' => 'Shkoni te Prindi',
+	'preview' => 'Preview'
 ];
