b0c6a7b56769349b3b44ff010e1f5fbc6db2c2aa
muammertop <muammertop@outlook.com>
2021-05-22 13:39:14 +0300

    #3284 Add description area in all modules on admin panel

--- addons/default/visiosoft/advs-module/resources/lang/en/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/en/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Ads',
     'name'        => 'Ads Module',
     'description' => 'Ads Module',
+    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
 ];
