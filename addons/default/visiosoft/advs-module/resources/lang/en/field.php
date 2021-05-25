aef34b291919f759212c64d5c93d997894345da7
Diatrex <diahshalabi@hotmail.com>
2021-05-25 12:02:43 +0300

    #3891 [advs-module] Add an option to show/hide phone number

--- addons/default/visiosoft/advs-module/resources/lang/en/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/en/field.php
@@ -337,6 +337,9 @@ return [
     'owner' => 'Owner',
     'default_owner_instruction' => 'The owner will default to the current user if no user is selected',
     'create_page_subtitle' => 'Create the ad flawlessly by entering detailed information about your ad.',
+    'with_my_phone_numbers' => 'With my phone numbers',
+    'not_with_my_phone_numbers' => "I don't want to be reached by phone",
+    'how_can_you_be_contacted' => "How can you be contacted?",
 
     // Listing page
     'ad_title' => 'Ad title',
