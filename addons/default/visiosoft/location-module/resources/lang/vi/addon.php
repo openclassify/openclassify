2d4900f22e6421df3d60bc05f8421bc6f60853b8
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:52 +0300

    New translations addon.php (Vietnamese)

--- addons/default/visiosoft/location-module/resources/lang/vi/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/vi/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Vị trí',
     'name'        => 'Mô-đun vị trí',
     'description' => 'Sự miêu tả',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
