e817f57860060bdc65ac07f4af07b5cfb1c13435
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:27 +0300

    New translations addon.php (Arabic)

--- addons/default/visiosoft/location-module/resources/lang/ar/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/ar/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'موقعك',
     'name'        => 'وحدة الموقع',
     'description' => 'وصف',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
