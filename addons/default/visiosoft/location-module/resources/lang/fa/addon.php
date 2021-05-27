8fd5d070be2d0b2f5eee49ca10d2950e026f0af0
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:31:11 +0300

    New translations addon.php (Persian)

--- addons/default/visiosoft/location-module/resources/lang/fa/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/fa/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'محل',
     'name'        => 'ماژول مکان',
     'description' => 'شرح',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
