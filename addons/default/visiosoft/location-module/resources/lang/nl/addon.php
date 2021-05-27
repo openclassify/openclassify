6dc152bd8c3a1b1c0d63718741af19024162bde9
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:31:49 +0300

    New translations addon.php (Dutch)

--- addons/default/visiosoft/location-module/resources/lang/nl/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/nl/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Plaats',
     'name'        => 'Locatiemodule',
     'description' => 'Omschrijving',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
