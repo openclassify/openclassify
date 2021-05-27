779b7101d68578c359187da2b79d4c607f81e43b
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:48 +0300

    New translations addon.php (Ukrainian)

--- addons/default/visiosoft/location-module/resources/lang/uk/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/uk/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Розташування',
     'name'        => 'Модуль розташування',
     'description' => 'Опис',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
