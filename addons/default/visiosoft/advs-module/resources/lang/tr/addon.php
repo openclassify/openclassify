aefcd7706d4afc07ee28de4958e3a728c9f2db1f
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:56 +0300

    New translations addon.php (Turkish)

--- addons/default/visiosoft/advs-module/resources/lang/tr/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/tr/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'İlanlar',
     'name'        => 'İlanlar Modülü',
     'description' => 'İlan Modülü',
+    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
 ];
