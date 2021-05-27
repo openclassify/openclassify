db40930048f8d3956feb08fb5e4f86a01213cc43
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:00 +0300

    New translations addon.php (Finnish)

--- addons/default/visiosoft/location-module/resources/lang/fi/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/fi/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Sijainti',
     'name'        => 'Sijainti-moduuli',
     'description' => 'Kuvaus',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
