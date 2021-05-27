24f5f1a10530c4644304944523250f7463325cc2
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:51 +0300

    New translations addon.php (Turkish)

--- addons/default/visiosoft/location-module/resources/lang/tr/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/tr/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Lokasyon',
     'name'        => 'Lokasyon Modülü',
     'description' => 'Lokasyon Modülü',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
