9a683cc6acd58967d12ab3c52832a333a24cc4a7
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:09 +0300

    New translations addon.php (Romanian)

--- addons/default/visiosoft/location-module/resources/lang/ro/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/ro/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Locație',
     'name'        => 'Modulul de locație',
     'description' => 'Descriere',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
