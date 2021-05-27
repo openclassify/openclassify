8d02efe2ba70a2d5c1bbff3e1b68585b983a325a
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:54 +0300

    New translations addon.php (Indonesian)

--- addons/default/visiosoft/advs-module/resources/lang/id/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/id/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Iklan',
     'name'        => 'Modul Iklan',
     'description' => 'Modul Iklan',
+    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
 ];
