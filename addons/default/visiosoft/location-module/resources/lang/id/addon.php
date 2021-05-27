250e3bfdae7c3ead4fdf43d2117fdc8d6af32685
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:31:15 +0300

    New translations addon.php (Indonesian)

--- addons/default/visiosoft/location-module/resources/lang/id/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/id/addon.php
@@ -4,4 +4,5 @@ return [
     'title'       => 'Lokasi',
     'name'        => 'Modul Lokasi',
     'description' => 'Deskripsi',
+    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
 ];
