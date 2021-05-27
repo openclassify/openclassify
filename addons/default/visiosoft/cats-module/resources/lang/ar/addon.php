08cf83cdcfdffe1c7e41d063f873ef776a09a09a
Fatih Alp <mail@fatihalp.com>
2021-05-27 14:30:29 +0300

    New translations addon.php (Arabic)

--- addons/default/visiosoft/cats-module/resources/lang/ar/addon.php
+++ addons/default/visiosoft/cats-module/resources/lang/ar/addon.php
@@ -3,5 +3,6 @@
 return [
     'title'       => 'الفئة',
     'name'        => 'وحدة الفئة',
-    'description' => ''
+    'description' => '',
+    'info' => 'This is the screen where you can add, delete and edit categories from the category page. You can easily manage main categories and sub-categories from this page.',
 ];
