8ca44a9ccc1d29e16248992ce963083b6261008a
Fatih Alp <mail@fatihalp.com>
2021-05-28 14:23:49 +0300

    New translations field.php (Catalan)

--- addons/default/visiosoft/advs-module/resources/lang/ca/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ca/field.php
@@ -337,6 +337,9 @@ return [
     'owner' => 'Propietari',
     'default_owner_instruction' => 'El propietari passarà per defecte a l\'usuari actual si no se selecciona cap usuari',
     'create_page_subtitle' => 'Creeu l’anunci perfectament introduint informació detallada sobre el vostre anunci.',
+    'with_my_phone_numbers' => 'With my phone numbers',
+    'not_with_my_phone_numbers' => "I don't want to be reached by phone",
+    'how_can_you_be_contacted' => "How can you be contacted?",
 
     // Listing page
     'ad_title' => 'Títol de l\'anunci',
