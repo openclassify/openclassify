4acd8d0941d682876faef735a7b9b761bedbaa30
Fatih Alp <mail@fatihalp.com>
2021-05-28 14:23:35 +0300

    New translations field.php (Vietnamese)

--- addons/default/visiosoft/advs-module/resources/lang/vi/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/vi/field.php
@@ -337,6 +337,9 @@ return [
     'owner' => 'Chủ nhân',
     'default_owner_instruction' => 'Chủ sở hữu sẽ mặc định là người dùng hiện tại nếu không có người dùng nào được chọn',
     'create_page_subtitle' => 'Tạo quảng cáo hoàn hảo bằng cách nhập thông tin chi tiết về quảng cáo của bạn.',
+    'with_my_phone_numbers' => 'With my phone numbers',
+    'not_with_my_phone_numbers' => "I don't want to be reached by phone",
+    'how_can_you_be_contacted' => "How can you be contacted?",
 
     // Listing page
     'ad_title' => 'Tiêu đề quảng cáo',
