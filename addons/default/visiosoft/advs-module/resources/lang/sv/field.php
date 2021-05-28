2d7a2899a11b9ef207ce4cb9721e0c0407ee2313
Fatih Alp <mail@fatihalp.com>
2021-05-28 14:23:28 +0300

    New translations field.php (Swedish)

--- addons/default/visiosoft/advs-module/resources/lang/sv/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/sv/field.php
@@ -337,6 +337,9 @@ return [
     'owner' => 'Ägare',
     'default_owner_instruction' => 'Ägaren kommer som standard att använda den nuvarande användaren om ingen användare har valts',
     'create_page_subtitle' => 'Skapa annonsen felfritt genom att ange detaljerad information om din annons.',
+    'with_my_phone_numbers' => 'With my phone numbers',
+    'not_with_my_phone_numbers' => "I don't want to be reached by phone",
+    'how_can_you_be_contacted' => "How can you be contacted?",
 
     // Listing page
     'ad_title' => 'Lägg till titel',
