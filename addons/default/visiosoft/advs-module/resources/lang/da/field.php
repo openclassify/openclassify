2158f4b21a8063e0e1192d3f7a5beeecd56ab67f
Fatih Alp <mail@fatihalp.com>
2021-05-28 14:23:44 +0300

    New translations field.php (Danish)

--- addons/default/visiosoft/advs-module/resources/lang/da/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/da/field.php
@@ -337,6 +337,9 @@ return [
     'owner' => 'Ejer',
     'default_owner_instruction' => 'Ejeren vil som standard være den nuværende bruger, hvis ingen bruger er valgt',
     'create_page_subtitle' => 'Opret annoncen fejlfrit ved at indtaste detaljerede oplysninger om din annonce.',
+    'with_my_phone_numbers' => 'With my phone numbers',
+    'not_with_my_phone_numbers' => "I don't want to be reached by phone",
+    'how_can_you_be_contacted' => "How can you be contacted?",
 
     // Listing page
     'ad_title' => 'Annoncetitel',
