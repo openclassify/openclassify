7440bae594fa868647ca7faf13c8204a093a9e07
Fatih Alp <mail@fatihalp.com>
2021-05-28 14:23:55 +0300

    New translations field.php (Hebrew)

--- addons/default/visiosoft/advs-module/resources/lang/he/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/he/field.php
@@ -337,6 +337,9 @@ return [
     'owner' => 'בעלים',
     'default_owner_instruction' => 'הבעלים יוגדר כברירת מחדל למשתמש הנוכחי אם לא ייבחר משתמש',
     'create_page_subtitle' => 'צור את המודעה ללא רבב על ידי הזנת מידע מפורט אודות המודעה שלך.',
+    'with_my_phone_numbers' => 'With my phone numbers',
+    'not_with_my_phone_numbers' => "I don't want to be reached by phone",
+    'how_can_you_be_contacted' => "How can you be contacted?",
 
     // Listing page
     'ad_title' => 'כותרת המודעה',
