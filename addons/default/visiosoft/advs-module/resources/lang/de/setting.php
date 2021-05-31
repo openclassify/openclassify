592bc5fffbbab9eefae6dabe139cab5286bdac42
Fatih Alp <mail@fatihalp.com>
2021-05-31 11:34:48 +0300

    New translations setting.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/setting.php
@@ -276,4 +276,8 @@ return [
         'name' => 'Enddatum anzeigen und Veröffentlichungsdatum für die Erstellung',
         'instructions' => 'Zeigt die Felder "Publish_at" und "Finish_at" auf der Seite zur Anzeigenerstellung an.'
     ],
+    'show_tax_field' => [
+        'name' => 'Show Tax Field',
+        'instructions' => 'When this option is enabled, you can set the tax rate on the ad creation page.'
+    ],
 ];
