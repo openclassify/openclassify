786c9c62f0d8b56f147e9f4bbb3d40485b97cd1b
Fatih Alp <mail@fatihalp.com>
2021-05-31 11:34:38 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ar/setting.php
@@ -276,4 +276,8 @@ return [
         'name' => 'إظهار تاريخ الانتهاء وتاريخ النشر للإنشاء',
         'instructions' => 'يعرض حقلي publish_at و finish_at في صفحة إنشاء الإعلان.'
     ],
+    'show_tax_field' => [
+        'name' => 'Show Tax Field',
+        'instructions' => 'When this option is enabled, you can set the tax rate on the ad creation page.'
+    ],
 ];
