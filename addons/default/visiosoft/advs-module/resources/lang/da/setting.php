1fb09a453fc541f9da4c727f10a134e4916e3c95
Fatih Alp <mail@fatihalp.com>
2021-05-31 11:34:47 +0300

    New translations setting.php (Danish)

--- addons/default/visiosoft/advs-module/resources/lang/da/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/da/setting.php
@@ -276,4 +276,8 @@ return [
         'name' => 'Vis slutdato og offentliggør dato for oprettelse',
         'instructions' => 'Viser felterne publish_at og finish_at på siden til annonceoprettelse.'
     ],
+    'show_tax_field' => [
+        'name' => 'Show Tax Field',
+        'instructions' => 'When this option is enabled, you can set the tax rate on the ad creation page.'
+    ],
 ];
