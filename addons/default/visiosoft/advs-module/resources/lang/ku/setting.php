b86b599f8d96290e6e44e34c073417117b229304
Fatih Alp <mail@fatihalp.com>
2021-05-31 11:34:51 +0300

    New translations setting.php (Kurdish)

--- addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/ku/setting.php
@@ -276,4 +276,8 @@ return [
         'name' => 'Ji Bo Afirandinê Dîroka Dawî And Nîşan Bikin',
         'instructions' => 'Zeviyên publ_at û finish_at li ser rûpela afirandina reklaman nîşan dide.'
     ],
+    'show_tax_field' => [
+        'name' => 'Show Tax Field',
+        'instructions' => 'When this option is enabled, you can set the tax rate on the ad creation page.'
+    ],
 ];
