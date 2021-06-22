7fe9bc41c8aca89997cf5a89196d69a9d31ac837
Diatrex <diahshalabi@hotmail.com>
2021-06-22 10:44:19 +0300

    #4145 offline design when you have no connection

--- addons/default/visiosoft/base-theme/resources/js/theme/initialize.js
+++ addons/default/visiosoft/base-theme/resources/js/theme/initialize.js
@@ -1,5 +1,20 @@
-// (function (window, document) {
-//
-//     // Go!
-//
-// })(window, document);
+(function (window, document) {
+    /* Offline */
+    window.addEventListener('offline', () => {
+        $('body > *').hide();
+        $('#offline').show();
+    });
+
+    $('#offline button').click(function () {
+        $('.spinner-border', this).css('display', 'inline-block')
+
+        setTimeout(() => {
+            if (window.navigator.onLine) {
+                window.location.reload();
+            } else {
+                $('.spinner-border', this).hide()
+            }
+        }, 250)
+    })
+    /* End Offline */
+})(window, document);
