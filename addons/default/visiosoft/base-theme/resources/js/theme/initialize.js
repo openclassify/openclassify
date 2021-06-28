3bc1744c9b77b829de9a395c3b4da417ead9b324
Diatrex <diahshalabi@hotmail.com>
2021-06-28 11:55:08 +0300

    #4258 avantaj-çelik rendering in design

--- addons/default/visiosoft/base-theme/resources/js/theme/initialize.js
+++ addons/default/visiosoft/base-theme/resources/js/theme/initialize.js
@@ -1,20 +1,18 @@
-(function (window, document) {
-    /* Offline */
-    window.addEventListener('offline', () => {
-        $('body > *').hide();
-        $('#offline').show();
-    });
+/* Offline */
+window.addEventListener('offline', () => {
+    $('body > *').hide();
+    $('#offline').show();
+});
 
-    $('#offline button').click(function () {
-        $('.spinner-border', this).css('display', 'inline-block')
+$('#offline button').click(function () {
+    $('.spinner-border', this).css('display', 'inline-block')
 
-        setTimeout(() => {
-            if (window.navigator.onLine) {
-                window.location.reload();
-            } else {
-                $('.spinner-border', this).hide()
-            }
-        }, 250)
-    })
-    /* End Offline */
-})(window, document);
+    setTimeout(() => {
+        if (window.navigator.onLine) {
+            window.location.reload();
+        } else {
+            $('.spinner-border', this).hide()
+        }
+    }, 250)
+})
+/* End Offline */
