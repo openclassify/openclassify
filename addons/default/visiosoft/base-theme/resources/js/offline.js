7b133a8459cdb25387426224ca146c2a025f2359
diashalabi <diahshalabi@hotmail.com>
2021-07-06 15:39:21 +0300

    #4306 offline error

---
+++ addons/default/visiosoft/base-theme/resources/js/offline.js
@@ -0,0 +1,18 @@
+/* Offline */
+window.addEventListener('offline', () => {
+    $('body > *').hide();
+    $('#offline').show();
+});
+
+$('#offline button').click(function () {
+    $('.spinner-border', this).css('display', 'inline-block')
+
+    setTimeout(() => {
+        if (window.navigator.onLine) {
+            window.location.reload();
+        } else {
+            $('.spinner-border', this).hide()
+        }
+    }, 250)
+})
+/* End Offline */
