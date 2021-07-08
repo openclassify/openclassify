dda2db1b10d85731a1368d7869953239b96a60dd
diashalabi <diahshalabi@hotmail.com>
2021-07-08 11:24:08 +0300

    #4231 Heavy Machinery Project

--- addons/default/visiosoft/base-theme/resources/js/visiosoft.js
+++ addons/default/visiosoft/base-theme/resources/js/visiosoft.js
@@ -1,11 +1,14 @@
-function crudAjax(params, url, type = 'GET', callback = () => {}, async = false) {
+function crudAjax(params, url, type = 'GET', callback = () => {}, async = true, options = {}) {
     return $.ajax({
-        type: type,
-        data: params,
-        async: async,
-        url: url,
-        success: function (response) {
-            callback(response);
+        ...{
+            type: type,
+            data: params,
+            async: async,
+            url: url,
+            success: function (response) {
+                callback(response);
+            },
         },
+        ...options
     });
 }
