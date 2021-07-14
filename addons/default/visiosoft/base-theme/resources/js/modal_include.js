092d5ca13c7d76792e9112ac38201073259e9307
diashalabi <diahshalabi@hotmail.com>
2021-07-14 17:12:16 +0300

    add loaded event for modal load

--- addons/default/visiosoft/base-theme/resources/js/modal_include.js
+++ addons/default/visiosoft/base-theme/resources/js/modal_include.js
@@ -1,5 +1,8 @@
 $('.remote').on('show.bs.modal', function (e) {
     if (typeof e.relatedTarget !== "undefined") {
-        $(this).find('.modal-content').load(e.relatedTarget.href);
+        $(this).find('.modal-content').load(e.relatedTarget.href, function () {
+            const event = new Event('custom.modal.loaded');
+            $(this).closest('.modal').get(0).dispatchEvent(event);
+        });
     }
 });
