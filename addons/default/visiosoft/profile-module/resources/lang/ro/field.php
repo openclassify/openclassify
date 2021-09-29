0ec06580ad5360ae88206a1a9e46f217527c6832
Fatih Alp <mail@fatihalp.com>
2021-09-29 18:08:30 +0300

    New translations field.php (Romanian)

--- addons/default/visiosoft/profile-module/resources/lang/ro/field.php
+++ addons/default/visiosoft/profile-module/resources/lang/ro/field.php
@@ -496,4 +496,12 @@ return [
     'receive_messages_email' => [
         'name' => 'Primiți mesaje prin e-mail',
     ],
+
+    // Report
+    'member' => 'Member',
+    'registered_at' => 'Registered At',
+    'login_at' => 'Login At',
+    'undefined_member' => 'Undefined Member',
+    'new_membership_report' => 'New Membership Report',
+    'member_login_reports' => 'Member Login Reports',
 ];
