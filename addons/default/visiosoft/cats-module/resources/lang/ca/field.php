47f118d584877926eb75cf9064e32fb699ba524e
Fatih Alp <mail@fatihalp.com>
2021-10-01 12:55:41 +0300

    New translations field.php (Catalan)

--- addons/default/visiosoft/cats-module/resources/lang/ca/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ca/field.php
@@ -37,5 +37,10 @@ return [
     'please_wait' => 'Si us plau, espereu. Esborra les subcategories',
     'category_selection' => 'Selecció de categories',
     'go_to_parent' => 'Ves al pare',
-	'preview' => 'Preview'
+	'preview' => 'Preview',
+
+    // Report
+    'undefined_category' => 'Undefined Category',
+    'categories_with_no_meta_report' => 'Categories with No Meta Report',
+    'category' => 'Category',
 ];
