7a1055fb0b9a8159a6c013b3375998267d9e9a75
Fatih Alp <mail@fatihalp.com>
2021-10-01 12:56:04 +0300

    New translations field.php (Indonesian)

--- addons/default/visiosoft/cats-module/resources/lang/id/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/id/field.php
@@ -37,5 +37,10 @@ return [
     'please_wait' => 'Harap tunggu. Menghapus Sub Kategori',
     'category_selection' => 'Pemilihan Kategori',
     'go_to_parent' => 'Buka Orang Tua',
-	'preview' => 'Pratinjau'
+	'preview' => 'Pratinjau',
+
+    // Report
+    'undefined_category' => 'Undefined Category',
+    'categories_with_no_meta_report' => 'Categories with No Meta Report',
+    'category' => 'Category',
 ];
