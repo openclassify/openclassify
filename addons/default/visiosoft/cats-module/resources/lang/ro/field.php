d4c2f652c8b0419fa88eecff4ebcb6fc24b242d7
Fatih Alp <mail@fatihalp.com>
2021-10-01 12:56:06 +0300

    New translations field.php (Romanian)

--- addons/default/visiosoft/cats-module/resources/lang/ro/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ro/field.php
@@ -37,5 +37,10 @@ return [
     'please_wait' => 'Vă rugăm să așteptați. Ștergeți subcategorii',
     'category_selection' => 'Selectarea categoriei',
     'go_to_parent' => 'Mergeți la părinte',
-	'preview' => 'Preview'
+	'preview' => 'Preview',
+
+    // Report
+    'undefined_category' => 'Undefined Category',
+    'categories_with_no_meta_report' => 'Categories with No Meta Report',
+    'category' => 'Category',
 ];
