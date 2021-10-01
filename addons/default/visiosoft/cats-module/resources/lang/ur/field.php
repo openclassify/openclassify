400f91550a74a05bb3d8ae328b5da7b1092eac7f
Fatih Alp <mail@fatihalp.com>
2021-10-01 12:56:02 +0300

    New translations field.php (Urdu (Pakistan))

--- addons/default/visiosoft/cats-module/resources/lang/ur/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ur/field.php
@@ -37,5 +37,10 @@ return [
     'please_wait' => 'براہ کرم انتظار کریں۔ ذیلی زمرے ختم کرنا',
     'category_selection' => 'زمرہ انتخاب',
     'go_to_parent' => 'پیرنٹ پر جائیں',
-	'preview' => 'Preview'
+	'preview' => 'Preview',
+
+    // Report
+    'undefined_category' => 'Undefined Category',
+    'categories_with_no_meta_report' => 'Categories with No Meta Report',
+    'category' => 'Category',
 ];
