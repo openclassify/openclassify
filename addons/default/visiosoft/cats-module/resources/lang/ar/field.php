853d651d580b0361063ba44a10b09d5538bda3e3
Fatih Alp <mail@fatihalp.com>
2021-10-01 12:55:38 +0300

    New translations field.php (Arabic)

--- addons/default/visiosoft/cats-module/resources/lang/ar/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ar/field.php
@@ -37,5 +37,10 @@ return [
     'please_wait' => 'يرجى الانتظار',
     'category_selection' => 'اختيار الفئة',
     'go_to_parent' => 'انتقل إلى الأصل',
-	'preview' => 'Preview'
+	'preview' => 'Preview',
+
+    // Report
+    'undefined_category' => 'Undefined Category',
+    'categories_with_no_meta_report' => 'Categories with No Meta Report',
+    'category' => 'Category',
 ];
