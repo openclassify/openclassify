6ad810f782d9aac3b0e527e214b7b35d47dfbd21
Fatih Alp <mail@fatihalp.com>
2021-10-01 12:55:47 +0300

    New translations field.php (Hungarian)

--- addons/default/visiosoft/cats-module/resources/lang/hu/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/hu/field.php
@@ -37,5 +37,10 @@ return [
     'please_wait' => 'Kérjük, várjon. Alkategóriák törlése',
     'category_selection' => 'Kategória kiválasztása',
     'go_to_parent' => 'Menjen a Szülőhöz',
-	'preview' => 'Preview'
+	'preview' => 'Preview',
+
+    // Report
+    'undefined_category' => 'Undefined Category',
+    'categories_with_no_meta_report' => 'Categories with No Meta Report',
+    'category' => 'Category',
 ];
