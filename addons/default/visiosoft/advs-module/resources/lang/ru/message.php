e1cacf11faf5078d82aa7bbd90b241a304939f0f
Fatih Alp <mail@fatihalp.com>
2021-10-02 22:59:08 +0300

    New translations message.php (Russian)

--- addons/default/visiosoft/advs-module/resources/lang/ru/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ru/message.php
@@ -32,4 +32,5 @@ return [
 	'sold_status_change' => "Статус вашего объявления изменен на \"Продано\"!",
 	'status_change' => "Статус вашего объявления был установлен на: status!",
 	'disabled_detailed_options_for_admin_role' => "Подробные параметры продукта отключены.",
+    'permission_error' => 'You do not have permission for this action',
 ];
