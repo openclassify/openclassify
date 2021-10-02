6548fddb3c4ae4cd468390c533ea5a928267148d
Fatih Alp <mail@fatihalp.com>
2021-10-02 22:59:35 +0300

    New translations message.php (Japanese)

--- addons/default/visiosoft/advs-module/resources/lang/ja/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ja/message.php
@@ -32,4 +32,5 @@ return [
 	'sold_status_change' => "広告のステータスが販売済みに設定されました。",
 	'status_change' => "広告のステータスが：statusに設定されました！",
 	'disabled_detailed_options_for_admin_role' => "詳細な製品オプションは無効になっています。",
+    'permission_error' => 'You do not have permission for this action',
 ];
