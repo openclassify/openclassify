81ce0a02671a44b406c2719a962f9ba719db0ca8
Fatih Alp <mail@fatihalp.com>
2021-10-04 12:25:44 +0300

    New translations message.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/message.php
@@ -33,4 +33,5 @@ return [
 	'status_change' => "وضعیت آگهی شما روی: status تنظیم شده است!",
 	'disabled_detailed_options_for_admin_role' => "گزینه های جزئی محصول غیرفعال هستند.",
     'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
 ];
