8a560da7c9ea1a6f8a8e9554a01ab3cf017d46be
Fatih Alp <mail@fatihalp.com>
2021-10-04 12:25:57 +0300

    New translations message.php (Bulgarian)

--- addons/default/visiosoft/advs-module/resources/lang/bg/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/bg/message.php
@@ -33,4 +33,5 @@ return [
 	'status_change' => "Състоянието на вашата реклама е зададено на: статус!",
 	'disabled_detailed_options_for_admin_role' => "Подробните опции за продукта са деактивирани.",
     'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
 ];
