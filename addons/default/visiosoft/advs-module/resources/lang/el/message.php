1c4a5729fa7a33c083af37a01bf90cc3023adde2
Fatih Alp <mail@fatihalp.com>
2021-10-04 12:26:02 +0300

    New translations message.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/message.php
@@ -33,4 +33,5 @@ return [
 	'status_change' => "Η κατάσταση της διαφήμισής σας έχει οριστεί σε: κατάσταση!",
 	'disabled_detailed_options_for_admin_role' => "Οι αναλυτικές επιλογές προϊόντος είναι απενεργοποιημένες.",
     'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
 ];
