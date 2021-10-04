498fef94b88e0a04d4eb478edcbd72ce4f16d4e9
Fatih Alp <mail@fatihalp.com>
2021-10-04 12:25:53 +0300

    New translations message.php (Danish)

--- addons/default/visiosoft/advs-module/resources/lang/da/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/da/message.php
@@ -33,4 +33,5 @@ return [
 	'status_change' => "Din annonces status er sat til: status!",
 	'disabled_detailed_options_for_admin_role' => "Detaljerede produktindstillinger er deaktiveret.",
     'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
 ];
