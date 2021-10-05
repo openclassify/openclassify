6efa927337cb7abda53de9d8d22f4e582b845986
Muammer Top <muammer@visiosoft.com.tr>
2021-10-05 18:11:52 +0300

    #2019 AD export as CSV

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/tr/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/tr/control_panel.php
@@ -10,4 +10,7 @@ return [
 
     // Users Module
     'search_by_gsm_number'     => 'GSM Numarasına Göre Ara',
+
+    // Adv Export
+    'choose_file_type' => 'Dışa Aktarmak İçin Dosya Türünü Seçin',
 ];
