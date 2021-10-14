aa6a7a98f8c7fec0dad1d4667c22ee0dea7e8d5d
Fatih Alp <mail@fatihalp.com>
2021-10-14 17:10:27 +0300

    New translations field.php (Vietnamese)

--- addons/default/visiosoft/cats-module/resources/lang/vi/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/vi/field.php
@@ -37,5 +37,11 @@ return [
     'please_wait' => 'Xin vui lòng chờ. Xóa danh mục phụ',
     'category_selection' => 'Lựa chọn danh mục',
     'go_to_parent' => 'Đi tới cha mẹ',
-	'preview' => 'Preview'
+	'preview' => 'Preview',
+
+    // Report
+    'undefined_category' => 'Undefined Category',
+    'categories_with_no_meta_report' => 'Categories with No Meta Report',
+    'category' => 'Category',
+    'id' => 'ID',
 ];
