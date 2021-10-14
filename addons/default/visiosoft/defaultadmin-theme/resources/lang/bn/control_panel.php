974496055293821c6618993a1e3d5383f393d34a
Fatih Alp <mail@fatihalp.com>
2021-10-14 17:14:16 +0300

    New translations control_panel.php (Bengali)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/bn/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/bn/control_panel.php
@@ -10,4 +10,7 @@ return [
 
     // Users Module
     'search_by_gsm_number'     => 'জিএসএম নম্বর দ্বারা অনুসন্ধান করুন',
+
+    // Adv Export
+    'choose_file_type' => 'Choose File Type For Export',
 ];
