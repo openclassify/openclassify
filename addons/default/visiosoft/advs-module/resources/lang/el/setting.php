ab1f5ca4f8ee59e8c41e87b99813de60c5b74520
Fatih Alp <mail@fatihalp.com>
2021-10-21 17:03:46 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/setting.php
@@ -128,6 +128,9 @@ return [
     'google_statistic_code' => [
         'name' => 'Στατιστικό κώδικα Google',
     ],
+    'body_custom_space' => [
+        'name' => 'Body Custom Space',
+    ],
     'register_email_field' => [
         'name' => 'Απαίτηση ηλεκτρονικού ταχυδρομείου εγγραφής μέλους',
     ],
