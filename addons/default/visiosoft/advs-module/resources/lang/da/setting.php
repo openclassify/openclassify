d8d670b949286f376ea7f93c243c538984bf2f9f
Fatih Alp <mail@fatihalp.com>
2021-10-21 17:03:36 +0300

    New translations setting.php (Danish)

--- addons/default/visiosoft/advs-module/resources/lang/da/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/da/setting.php
@@ -128,6 +128,9 @@ return [
     'google_statistic_code' => [
         'name' => 'Googles statistiske kode',
     ],
+    'body_custom_space' => [
+        'name' => 'Body Custom Space',
+    ],
     'register_email_field' => [
         'name' => 'E-mail-krav til medlemsregistrering',
     ],
