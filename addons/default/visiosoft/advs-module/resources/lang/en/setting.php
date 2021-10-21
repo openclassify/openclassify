b884939625cd1f1f69550adefe55871b89a63d57
Fatih Alp <mail@fatihalp.com>
2021-10-21 17:03:20 +0300

    New translations setting.php (English)

--- addons/default/visiosoft/advs-module/resources/lang/en/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/en/setting.php
@@ -128,6 +128,9 @@ return [
     'google_statistic_code' => [
         'name' => 'Google Statistic Code',
     ],
+    'body_custom_space' => [
+        'name' => 'Body Custom Space',
+    ],
     'register_email_field' => [
         'name' => 'Member Registration Email Requirement',
     ],
