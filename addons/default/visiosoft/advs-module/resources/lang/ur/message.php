6f33a7ea60fec286f155a74cff789c009dca2437
Fatih Alp <mail@fatihalp.com>
2021-11-15 17:19:48 +0300

    New translations message.php (Urdu (Pakistan))

--- addons/default/visiosoft/advs-module/resources/lang/ur/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/ur/message.php
@@ -32,4 +32,6 @@ return [
 	'sold_status_change' => "آپ کے اشتہار کی حیثیت بیچ دی گئی ہے!",
 	'status_change' => "آپ کے اشتہار کی حیثیت اس پر سیٹ ہوچکی ہے: درجہ!",
 	'disabled_detailed_options_for_admin_role' => "مصنوع کے تفصیلی اختیارات غیر فعال ہیں۔",
+    'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
 ];
