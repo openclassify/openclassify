c332a69fbf7282c33cc70bcaab1003d13186b645
Fatih Alp <mail@fatihalp.com>
2021-11-15 16:17:22 +0300

    New translations field.php (Russian)

--- addons/default/visiosoft/cats-module/resources/lang/ru/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/ru/field.php
@@ -30,7 +30,7 @@ return [
         'instructions' => 'Используется для добавления значков, обозначающих тип категории.',
     ],
 
-    'parent' => 'Parent',
+    'parent' => 'родитель',
     'level' => 'Level',
     'count' => 'Ad Counts',
 
@@ -43,6 +43,6 @@ return [
     // Report
     'undefined_category' => 'Undefined Category',
     'categories_with_no_meta_report' => 'Categories with No Meta Report',
-    'category' => 'Category',
+    'category' => 'категория',
     'id' => 'ID',
 ];
