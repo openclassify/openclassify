5b1eb81c66e7d3bd5765b56d947f9dacf8bfe555
Muammer Top <muammer@visiosoft.com.tr>
2021-11-16 17:43:56 +0300

    #4885 Sms verification will come at register page

--- addons/default/visiosoft/base-theme/resources/js/register.js
+++ addons/default/visiosoft/base-theme/resources/js/register.js
@@ -19,24 +19,33 @@ phoneInput.on('keydown', function () {
     clearTimeout(typingTimer);
 });
 
-function doneTyping(phoneNum) {
+function getPhoneNumber(phoneNum) {
     if (!phoneNum.includes('_') && phoneNum !== "") {
         let countryCode = $(".iti__selected-flag").attr('title').split("+");
         countryCode = '+' + countryCode[countryCode.length - 1];
+        return countryCode + phoneNum.substr(1);
+    }
+    return false;
+}
 
+function doneTyping(phoneNum) {
+    var phone = getPhoneNumber(phoneNum);
+    if (phone) {
         $.ajax({
             type: 'GET',
-            data: {'phoneNumber': countryCode + phoneNum.substr(1)},
+            data: {'phoneNumber': phone },
             url: 'ajax/phone-validation',
             success: function (response) {
                 if (response.userExists) {
                     phoneInput.addClass('rejected-phone');
                     phoneInput.removeClass('approved-phone');
                     $('.phone-validation-error').removeClass('d-none')
+                    $('.verify-button').addClass('d-none')
                 } else {
                     phoneInput.addClass('approved-phone');
                     phoneInput.removeClass('rejected-phone');
-                    $('.phone-validation-error').addClass('d-none')
+                    $('.phone-validation-error').addClass('d-none');
+                    $('.verify-button').removeClass('d-none');
                 }
             },
         });
