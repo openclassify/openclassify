bdbf7fc46c29fe982bf5c65d6e94a2f7ebb86deb
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:03 +0300

    New translations field.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'Në muajin e fundit'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Njoftime me Foto'
     ],
