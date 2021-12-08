66c3b0050f0c5be65769445140440f4fb173e344
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:30 +0300

    New translations field.php (German)

--- addons/default/visiosoft/advs-module/resources/lang/de/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/de/field.php
@@ -273,6 +273,9 @@ return [
     'in_the_last_month' => [
         'name' => 'Im letzten Monat'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Anzeigen mit Foto'
     ],
