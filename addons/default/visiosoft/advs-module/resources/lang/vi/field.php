f8c235a00ba2755425d2afbaf75885f3955c65e0
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:11 +0300

    New translations field.php (Vietnamese)

--- addons/default/visiosoft/advs-module/resources/lang/vi/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/vi/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'Trong tháng cuối cùng'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Quảng cáo với hình ảnh'
     ],
