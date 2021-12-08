b52962682ef99aa17e182f53cd16cb341e4beff3
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:40 +0300

    New translations field.php (Serbian (Latin))

--- addons/default/visiosoft/advs-module/resources/lang/sr/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/sr/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'U poslednjih mesec'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Oglasi sa slikom'
     ],
