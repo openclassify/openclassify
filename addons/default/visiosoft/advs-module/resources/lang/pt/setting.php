83e8dbee1dd4f6d990240006cf5d39ad6031dff2
Fatih Alp <mail@fatihalp.com>
2021-12-08 10:32:15 +0300

    New translations setting.php (Portuguese)

--- addons/default/visiosoft/advs-module/resources/lang/pt/setting.php
+++ addons/default/visiosoft/advs-module/resources/lang/pt/setting.php
@@ -233,6 +233,10 @@ return [
         'name' => 'Ocultar informações do vendedor',
         'instructions' => 'Escondendo as informações do vendedor na página de detalhes dos anúncios',
     ],
+    'hide_seller_info_by_category' => [
+        'name' => 'Hide Seller Information By Category',
+        'instructions' => 'Hiding to seller infos by selected main categories in ads detail page',
+    ],
     'show_post_ad_agreement' => [
         'name' => 'Mostrar Acordo de Postagem',
         'instructions' => 'Ative-o se quiser exibir um acordo de política de privacidade na página de criação de anúncios',
