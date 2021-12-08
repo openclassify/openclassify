8459a713553d0c0432abe9e8cb05a228554be3fd
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:36 +0300

    New translations field.php (Hungarian)

--- addons/default/visiosoft/advs-module/resources/lang/hu/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/hu/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'Az utolsó hónapban'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Hirdetések fényképpel'
     ],
