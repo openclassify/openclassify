d40a823ba9f89dae3cb214cc782969ea74f34a76
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:13 +0300

    New translations field.php (Persian)

--- addons/default/visiosoft/advs-module/resources/lang/fa/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/fa/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'در آخرین ماه'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'اعلانات با عکس'
     ],
