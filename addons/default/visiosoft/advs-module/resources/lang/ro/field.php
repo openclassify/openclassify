07f64de92c6bbf7130377a6cd59c0dc16fa7ab70
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:00:58 +0300

    New translations field.php (Romanian)

--- addons/default/visiosoft/advs-module/resources/lang/ro/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/ro/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'În ultima lună'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Anunțuri cu fotografie'
     ],
