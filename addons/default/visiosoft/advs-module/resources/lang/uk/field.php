f7546316086086328df4486e4ff901c446bae4d9
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:06 +0300

    New translations field.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'В останній місяць'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Оголошення з фото'
     ],
