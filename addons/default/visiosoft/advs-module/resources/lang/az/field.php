dfa52b7cb8805519122dac496940f94560c9fd15
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:15 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/field.php
@@ -273,6 +273,9 @@ return [
     'in_the_last_month' => [
         'name' => 'Son Ayda'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'Şəkilli Elanlar'
     ],
