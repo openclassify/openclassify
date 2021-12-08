9ae6101f7ff02d09060325c19532129952b5db5b
Fatih Alp <mail@fatihalp.com>
2021-12-08 17:01:16 +0300

    New translations field.php (Hindi)

--- addons/default/visiosoft/advs-module/resources/lang/hi/field.php
+++ addons/default/visiosoft/advs-module/resources/lang/hi/field.php
@@ -272,6 +272,9 @@ return [
     'in_the_last_month' => [
         'name' => 'द लास्ट मंथ में'
     ],
+    'in_the_last_two_days' => [
+        'name' => 'In The Last Two Days'
+    ],
     'ads_with_photo' => [
         'name' => 'फोटो के साथ विज्ञापन'
     ],
