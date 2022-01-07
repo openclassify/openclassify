bc44f65bbba70dd4c603150152800e38e579798d
Fatih Alp <mail@fatihalp.com>
2022-01-07 09:52:25 +0300

    New translations message.php (Hungarian)

--- addons/default/visiosoft/cats-module/resources/lang/hu/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/hu/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'A kategóriákat és a kapcsolódó alkategóriákat sikeresen törölte!',
+    'cat_slug_exists' => 'A category with a slug of :slug already exists!',
 ];
