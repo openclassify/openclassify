16f767e5e9f88ecdbe815bb779fc97f6cf604e53
Fatih Alp <mail@fatihalp.com>
2022-01-07 09:52:23 +0300

    New translations message.php (Finnish)

--- addons/default/visiosoft/cats-module/resources/lang/fi/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/fi/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Luokat ja niihin liittyvät alaluokat on poistettu onnistuneesti!',
+    'cat_slug_exists' => 'A category with a slug of :slug already exists!',
 ];
