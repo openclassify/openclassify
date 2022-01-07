83f76315087fdef4c187bd7dea11f64bc6abdef8
Fatih Alp <mail@fatihalp.com>
2022-01-07 09:52:08 +0300

    New translations message.php (Hindi)

--- addons/default/visiosoft/cats-module/resources/lang/hi/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/hi/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'श्रेणियों और संबंधित उप-श्रेणियों को सफलतापूर्वक हटा दिया गया है!',
+    'cat_slug_exists' => 'A category with a slug of :slug already exists!',
 ];
