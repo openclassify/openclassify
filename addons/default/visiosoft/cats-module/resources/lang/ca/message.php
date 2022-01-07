da8f7599c68019eb30a5eda60abb8f0a06317385
Fatih Alp <mail@fatihalp.com>
2022-01-07 09:52:18 +0300

    New translations message.php (Catalan)

--- addons/default/visiosoft/cats-module/resources/lang/ca/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/ca/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Les categories i les subcategories relacionades s\'han suprimit correctament.',
+    'cat_slug_exists' => 'A category with a slug of :slug already exists!',
 ];
