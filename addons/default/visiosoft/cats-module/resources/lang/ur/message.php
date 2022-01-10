674cdaea2e5293fac0a2976dd0cc8e2039a040f6
Fatih Alp <mail@fatihalp.com>
2022-01-10 12:03:33 +0300

    New translations message.php (Urdu (Pakistan))

--- addons/default/visiosoft/cats-module/resources/lang/ur/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/ur/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'زمرہ جات اور متعلقہ ذیلی زمرے کامیابی کے ساتھ حذف کردیئے گئے ہیں!',
+    'cat_slug_exists' => 'A category with a slug of :slug already exists!',
 ];
