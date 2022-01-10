7ded7f8c81ce2ade3f455d138c5324c5fb6a7faf
Fatih Alp <mail@fatihalp.com>
2022-01-10 11:52:49 +0300

    New translations message.php (Serbian (Latin))

--- addons/default/visiosoft/advs-module/resources/lang/sr/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/sr/message.php
@@ -32,4 +32,8 @@ return [
 	'sold_status_change' => "Status vašeg oglasa je prodato!",
 	'status_change' => "Status vašeg oglasa je :status!",
 	'disabled_detailed_options_for_admin_role' => "Detaljne opcije proizvoda su onemogućene.",
+    'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
+    'option_saved' => 'Option Saved',
+    'do_you_want_to_save_the_option' => 'Do you want to save the option?',
 ];
