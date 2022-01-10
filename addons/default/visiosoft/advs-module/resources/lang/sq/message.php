f88774b5e813cb51fb83333f1638467098c6342f
Fatih Alp <mail@fatihalp.com>
2022-01-10 11:52:37 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/advs-module/resources/lang/sq/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/sq/message.php
@@ -32,4 +32,8 @@ return [
 	'sold_status_change' => "Statusi i Reklamës suaj Setshtë vendosur të shitet!",
 	'status_change' => "Statusi i Reklamës suaj Setshtë vendosur në: status!",
 	'disabled_detailed_options_for_admin_role' => "Opsionet e hollësishme të produktit janë çaktivizuar.",
+    'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
+    'option_saved' => 'Option Saved',
+    'do_you_want_to_save_the_option' => 'Do you want to save the option?',
 ];
