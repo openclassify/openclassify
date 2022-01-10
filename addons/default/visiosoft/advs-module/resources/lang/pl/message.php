cd4395aa69f6b4cb7b272fe3d0bfca7231688085
Fatih Alp <mail@fatihalp.com>
2022-01-10 11:52:34 +0300

    New translations message.php (Polish)

--- addons/default/visiosoft/advs-module/resources/lang/pl/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/pl/message.php
@@ -32,4 +32,8 @@ return [
 	'sold_status_change' => "Stan Twojej reklamy został ustawiony na Sprzedane!",
 	'status_change' => "Status Twojej reklamy został ustawiony na: status!",
 	'disabled_detailed_options_for_admin_role' => "Szczegółowe opcje produktu są wyłączone.",
+    'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
+    'option_saved' => 'Option Saved',
+    'do_you_want_to_save_the_option' => 'Do you want to save the option?',
 ];
