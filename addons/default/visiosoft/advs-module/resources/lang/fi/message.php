2541d9ee1bcc358ed888d97fbe4c88e250f3476b
Fatih Alp <mail@fatihalp.com>
2022-01-10 11:53:15 +0300

    New translations message.php (Finnish)

--- addons/default/visiosoft/advs-module/resources/lang/fi/message.php
+++ addons/default/visiosoft/advs-module/resources/lang/fi/message.php
@@ -32,4 +32,8 @@ return [
 	'sold_status_change' => "Mainoksesi tila on myyty!",
 	'status_change' => "Mainoksesi tila on asetettu tilaan: status!",
 	'disabled_detailed_options_for_admin_role' => "Yksityiskohtaiset tuotevaihtoehdot ovat pois käytöstä.",
+    'permission_error' => 'You do not have permission for this action',
+    'currency_converter_not_available' => 'The currency converter is not available.',
+    'option_saved' => 'Option Saved',
+    'do_you_want_to_save_the_option' => 'Do you want to save the option?',
 ];
