b44436e0d1509855a00da4ed400d0f1d59e9fed1
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:51:46 +0300

    New translations message.php (Turkish)

--- addons/default/visiosoft/cats-module/resources/lang/tr/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/tr/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategoriler ve ilgili alt kategoriler başarıyla silindi!',
+    'cat_slug_exists' => ':slug sümüklü bir kategori zaten var!',
 ];
