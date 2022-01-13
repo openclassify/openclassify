720e21505afbd0fd0d35e22a3e4941efd323b59b
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:37:29 +0300

    New translations addon.php (Afrikaans)

--- addons/default/visiosoft/advs-module/resources/lang/af/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/af/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Advertensies',
     'name'        => 'Advertensie-module',
     'description' => 'Advertensie-module',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'Dit is die skerm waar jy advertensies vanaf die Advertensies-bladsy kan byvoeg, uitvee en wysig. Op hierdie bladsy kan jy eenvoudig die bewerkings doen soos posbestuur en advertensie-konfigurasie.'
 ];
