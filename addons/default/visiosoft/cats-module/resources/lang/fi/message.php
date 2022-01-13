ec8842b66cda4b009205ec9f8785e017c908c18e
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:42:38 +0300

    New translations message.php (Finnish)

--- addons/default/visiosoft/cats-module/resources/lang/fi/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/fi/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Luokat ja niihin liittyvät alaluokat on poistettu onnistuneesti!',
+    'cat_slug_exists' => 'Luokka, jonka etana on :slug, on jo olemassa!',
 ];
