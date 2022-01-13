11251ec3a1ec92460d64d208819151ac5c5f16e8
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:53:56 +0300

    New translations field.php (Urdu (Pakistan))

--- addons/default/visiosoft/base-theme/resources/lang/ur/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/ur/field.php
@@ -58,8 +58,8 @@ return [
     'instruction_list' => 'ہدایات کی فہرست',
 
     // Offline
-    'connection_problem' => 'Connection problem',
-    'your_internet_connection_is_broken' => 'Your internet connection is broken,',
-    'we_miss_you_so_much_already' => 'we miss you so much already!',
-    'try_again' => 'Try again',
+    'connection_problem' => 'کنکشن کا مسئلہ',
+    'your_internet_connection_is_broken' => 'آپ کا انٹرنیٹ کنکشن ٹوٹ گیا ہے،',
+    'we_miss_you_so_much_already' => 'ہم آپ کو پہلے ہی بہت یاد کرتے ہیں!',
+    'try_again' => 'دوبارہ کوشش کریں',
 ];
