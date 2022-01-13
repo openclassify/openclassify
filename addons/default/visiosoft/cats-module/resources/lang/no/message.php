19eb669443a7960f55e615fd652502aed18156cb
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:47:57 +0300

    New translations message.php (Norwegian)

--- addons/default/visiosoft/cats-module/resources/lang/no/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/no/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategorier og relaterte underkategorier er slettet!',
+    'cat_slug_exists' => 'En kategori med en slug :slug eksisterer allerede!',
 ];
