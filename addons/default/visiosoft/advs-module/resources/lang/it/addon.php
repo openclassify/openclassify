1110dba9aa7a5bb012a0e929731cbcc8604f2cad
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:44:06 +0300

    New translations addon.php (Italian)

--- addons/default/visiosoft/advs-module/resources/lang/it/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/it/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Annunci',
     'name'        => 'Modulo annunci',
     'description' => 'Modulo annunci',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'Questa è la schermata in cui puoi aggiungere, eliminare e modificare annunci dalla pagina Annunci. In questa pagina puoi semplicemente eseguire operazioni come la gestione dei post e la configurazione degli annunci.'
 ];
