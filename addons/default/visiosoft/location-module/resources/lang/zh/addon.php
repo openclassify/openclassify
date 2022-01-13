8a1d60d8de359a57a31795e3b37679e745a9869c
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:52:38 +0300

    New translations addon.php (Chinese Simplified)

--- addons/default/visiosoft/location-module/resources/lang/zh/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/zh/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => '位置',
     'name'        => '定位模块',
     'description' => '描述',
-    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
+    'info' => '在这个页面上，您可以管理国家、城市、地区、社区和村庄的信息。',
 ];
