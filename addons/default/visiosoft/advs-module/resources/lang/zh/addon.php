d624499a7224011c797801c878914166844cd860
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:52:32 +0300

    New translations addon.php (Chinese Simplified)

--- addons/default/visiosoft/advs-module/resources/lang/zh/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/zh/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => '广告',
     'name'        => '广告模块',
     'description' => '广告模块',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => '这是您可以从“广告”页面添加、删除和编辑广告的屏幕。在这个页面上，您可以简单地进行帖子管理和广告配置等操作。'
 ];
