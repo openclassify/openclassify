7af95e9e18b506a19a71d6d3639abe4a1d12f95b
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:50:14 +0300

    New translations addon.php (Albanian)

--- addons/default/visiosoft/location-module/resources/lang/sq/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/sq/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Vendndodhja',
     'name'        => 'Moduli i vendndodhjes',
     'description' => 'Përshkrim',
-    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
+    'info' => 'Në këtë faqe, ju mund të menaxhoni informacionin e vendit, qytetit, rrethit, lagjes dhe fshatit.',
 ];
