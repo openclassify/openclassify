c6c465d0469ccdc8fed9b60734e134b56cb230c4
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:56:48 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/base-theme/resources/lang/az/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/az/field.php
@@ -58,8 +58,8 @@ return [
     'instruction_list' => 'Təlimat siyahısı',
 
     // Offline
-    'connection_problem' => 'Connection problem',
-    'your_internet_connection_is_broken' => 'Your internet connection is broken,',
-    'we_miss_you_so_much_already' => 'we miss you so much already!',
-    'try_again' => 'Try again',
+    'connection_problem' => 'Bağlantı problemi',
+    'your_internet_connection_is_broken' => 'İnternet bağlantınız pozuldu,',
+    'we_miss_you_so_much_already' => 'artıq sənin üçün çox darıxırıq!',
+    'try_again' => 'Yenidən cəhd elə',
 ];
