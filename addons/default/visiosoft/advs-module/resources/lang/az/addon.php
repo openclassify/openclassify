e2b01f9977148f520e66222274a801e4c6df51ba
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:56:38 +0300

    New translations addon.php (Azerbaijani)

--- addons/default/visiosoft/advs-module/resources/lang/az/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/az/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Elanlar',
     'name'        => 'Elanlar Modulu',
     'description' => 'Elan Modulu',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'Bu, Reklamlar səhifəsindən reklam əlavə edə, silə və redaktə edə biləcəyiniz ekrandır. Bu səhifədə siz sadəcə olaraq postun idarə edilməsi və reklam konfiqurasiyası kimi əməliyyatları edə bilərsiniz.'
 ];
