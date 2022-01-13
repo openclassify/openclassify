766882f75072fb679e929ded40777c329d1bbee1
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:55:24 +0300

    New translations message.php (Indonesian)

--- addons/default/visiosoft/cats-module/resources/lang/id/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/id/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategori dan subkategori terkait telah berhasil dihapus!',
+    'cat_slug_exists' => 'Kategori dengan slug :slug sudah ada!',
 ];
