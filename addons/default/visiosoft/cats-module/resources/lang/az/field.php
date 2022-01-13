f42edb259aa9fd49a9c90e1b87fa2e3c96168c56
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:56:50 +0300

    New translations field.php (Azerbaijani)

--- addons/default/visiosoft/cats-module/resources/lang/az/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/az/field.php
@@ -30,12 +30,18 @@ return [
         'instructions' => 'Bu kateqoriya növünü göstərən nişanlar əlavə etmək üçün istifadə olunur.',
     ],
 
-    'parent' => 'Parent',
-    'level' => 'Level',
-    'count' => 'Ad Counts',
+    'parent' => 'Valideyn',
+    'level' => 'Səviyyə',
+    'count' => 'Reklamların sayı',
 
     'please_wait' => 'Xahiş edirəm gözləyin. Alt kateqoriyaların silinməsi',
     'category_selection' => 'Kateqoriya seçimi',
     'go_to_parent' => 'Valideynə get',
-	'preview' => 'Preview'
+	'preview' => 'Önizləmə',
+
+    // Report
+    'undefined_category' => 'Müəyyən edilməmiş Kateqoriya',
+    'categories_with_no_meta_report' => 'Meta Hesabatı olmayan kateqoriyalar',
+    'category' => 'Kateqoriya',
+    'id' => 'ID',
 ];
