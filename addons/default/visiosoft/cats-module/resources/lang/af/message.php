b0a5ee2befc442b3bd6755df31f97583778f848d
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:38:15 +0300

    New translations message.php (Afrikaans)

--- addons/default/visiosoft/cats-module/resources/lang/af/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/af/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategorieë en verwante subkategorieë is suksesvol uitgevee!',
+    'cat_slug_exists' => '\'n Kategorie met \'n slak van :slug bestaan reeds!',
 ];
