8b78d73604572b41ba94e32289fa00ff5be443d2
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:54:38 +0300

    New translations addon.php (Vietnamese)

--- addons/default/visiosoft/cats-module/resources/lang/vi/addon.php
+++ addons/default/visiosoft/cats-module/resources/lang/vi/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'thể loại',
     'name'        => 'Mô-đun danh mục',
     'description' => '',
-    'info' => 'This is the screen where you can add, delete and edit categories from the category page. You can easily manage main categories and sub-categories from this page.',
+    'info' => 'Đây là màn hình nơi bạn có thể thêm, xóa và chỉnh sửa các danh mục từ trang danh mục. Bạn có thể dễ dàng quản lý các danh mục chính và danh mục phụ từ trang này.',
 ];
