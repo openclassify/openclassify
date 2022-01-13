fb3f0cafada66b5002227e7d8ad24989b735625d
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:48:24 +0300

    New translations message.php (Polish)

--- addons/default/visiosoft/cats-module/resources/lang/pl/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/pl/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategorie i powiązane podkategorie zostały pomyślnie usunięte!',
+    'cat_slug_exists' => 'Kategoria ze ślimakiem :slug już istnieje!',
 ];
