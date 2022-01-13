b1bad54a76c99cbfc1fb18d448c80eba61a7c9dd
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:57:12 +0300

    New translations message.php (Azerbaijani)

--- addons/default/visiosoft/cats-module/resources/lang/az/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/az/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kateqoriyalar və əlaqəli alt kateqoriyalar uğurla silindi!',
+    'cat_slug_exists' => ':slug ilə kateqoriya artıq mövcuddur!',
 ];
