4a750a20bfff1a52536254cc76dd1cac1d9b7df2
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:40:22 +0300

    New translations field.php (Danish)

--- addons/default/visiosoft/base-theme/resources/lang/da/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/da/field.php
@@ -58,8 +58,8 @@ return [
     'instruction_list' => 'Instruktionsliste',
 
     // Offline
-    'connection_problem' => 'Connection problem',
-    'your_internet_connection_is_broken' => 'Your internet connection is broken,',
-    'we_miss_you_so_much_already' => 'we miss you so much already!',
-    'try_again' => 'Try again',
+    'connection_problem' => 'Forbindelsesproblem',
+    'your_internet_connection_is_broken' => 'Din internetforbindelse er afbrudt,',
+    'we_miss_you_so_much_already' => 'vi savner dig allerede så meget!',
+    'try_again' => 'Prøv igen',
 ];
