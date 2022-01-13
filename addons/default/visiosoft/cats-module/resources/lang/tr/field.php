62e145ea58e0f7e91a862e154efe09c35653ce59
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:51:18 +0300

    New translations field.php (Turkish)

--- addons/default/visiosoft/cats-module/resources/lang/tr/field.php
+++ addons/default/visiosoft/cats-module/resources/lang/tr/field.php
@@ -37,5 +37,11 @@ return [
     'please_wait' => 'Lütfen bekleyin. Alt Kategoriler Siliniyor',
     'category_selection' => 'Kategori Seçimi',
     'go_to_parent' => 'Kategoriye Dön',
-	'preview' => 'Ön İzleme'
+	'preview' => 'Ön İzleme',
+
+    // Report
+    'undefined_category' => 'Tanımsız Kategori',
+    'categories_with_no_meta_report' => 'Meta Raporu Olmayan Kategoriler',
+    'category' => 'Kategori',
+    'id' => 'İD',
 ];
