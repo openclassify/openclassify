1633b2081182d114ad063135179a0940b1cbfaa9
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:46:27 +0300

    New translations message.php (Kurdish)

--- addons/default/visiosoft/cats-module/resources/lang/ku/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/ku/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategorî û jêr-kategorîyên têkildar bi serfirazî hatin jêbirin!',
+    'cat_slug_exists' => 'Kategoriyek bi slug :slug jixwe heye!',
 ];
