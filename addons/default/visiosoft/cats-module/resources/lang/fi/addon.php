b710bb6db73bd9e3dcca26ce75f62e837a82b086
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:42:36 +0300

    New translations addon.php (Finnish)

--- addons/default/visiosoft/cats-module/resources/lang/fi/addon.php
+++ addons/default/visiosoft/cats-module/resources/lang/fi/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Kategoria',
     'name'        => 'Luokkamoduuli',
     'description' => '',
-    'info' => 'This is the screen where you can add, delete and edit categories from the category page. You can easily manage main categories and sub-categories from this page.',
+    'info' => 'Tämä on näyttö, jossa voit lisätä, poistaa ja muokata luokkia luokkasivulta. Voit helposti hallita pääluokkia ja alaluokkia tältä sivulta.',
 ];
