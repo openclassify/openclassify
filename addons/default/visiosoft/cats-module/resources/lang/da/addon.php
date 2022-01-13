51fcd4a37870b8e9d0731e96c1553eca423e2c59
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:40:29 +0300

    New translations addon.php (Danish)

--- addons/default/visiosoft/cats-module/resources/lang/da/addon.php
+++ addons/default/visiosoft/cats-module/resources/lang/da/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Kategori',
     'name'        => 'Kategorimodul',
     'description' => '',
-    'info' => 'This is the screen where you can add, delete and edit categories from the category page. You can easily manage main categories and sub-categories from this page.',
+    'info' => 'Dette er skærmen, hvor du kan tilføje, slette og redigere kategorier fra kategorisiden. Du kan nemt administrere hovedkategorier og underkategorier fra denne side.',
 ];
