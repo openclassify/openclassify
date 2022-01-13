0e1dd8793b604b635e858f8805f47fc93a2f9be0
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:50:15 +0300

    New translations message.php (Albanian)

--- addons/default/visiosoft/cats-module/resources/lang/sq/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/sq/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategoritë dhe nënkategoritë përkatëse janë fshirë me sukses!',
+    'cat_slug_exists' => 'Një kategori me një :slug ekziston tashmë!',
 ];
