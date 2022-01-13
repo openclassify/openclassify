be1ffe23a2031169fe2e89ea33256c6a2dd8f06f
Fatih Alp <mail@fatihalp.com>
2022-01-13 11:31:36 +0300

    New translations control_panel.php (Romanian)

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/ro/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ro/control_panel.php
@@ -12,5 +12,5 @@ return [
     'search_by_gsm_number'     => 'Căutare după număr GSM',
 
     // Adv Export
-    'choose_file_type' => 'Choose File Type For Export',
+    'choose_file_type' => 'Alegeți tip de fișier pentru export',
 ];
