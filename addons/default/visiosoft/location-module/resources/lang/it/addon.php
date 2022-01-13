241246d2dd11ee788347d80b2657de2577f1887a
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:44:42 +0300

    New translations addon.php (Italian)

--- addons/default/visiosoft/location-module/resources/lang/it/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/it/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Posizione',
     'name'        => 'Modulo di localizzazione',
     'description' => 'Descrizione',
-    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
+    'info' => 'In questa pagina puoi gestire le informazioni su paese, città, distretto, quartiere e villaggio.',
 ];
