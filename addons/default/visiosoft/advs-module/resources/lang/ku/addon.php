3a192e5e6dd4a0c62b918feaa10f8084d96036ee
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:46:00 +0300

    New translations addon.php (Kurdish)

--- addons/default/visiosoft/advs-module/resources/lang/ku/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/ku/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'ڕیکلامەکان',
     'name'        => 'مۆدیلی ڕیکلامەکان',
     'description' => 'مۆدیلی ڕیکلامەکان',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'Ev dîmendera ku hûn dikarin ji rûpela Ads reklaman lê zêde bikin, jêbirin û biguherînin e. Li ser vê rûpelê, hûn dikarin bi tenê karûbarên wekî rêveberiya postê û veavakirina reklamê bikin.'
 ];
