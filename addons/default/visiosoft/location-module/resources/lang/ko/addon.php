a90efa759a3ba54cffbada90aa2ca970d4945c5c
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:45:34 +0300

    New translations addon.php (Korean)

--- addons/default/visiosoft/location-module/resources/lang/ko/addon.php
+++ addons/default/visiosoft/location-module/resources/lang/ko/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => '위치',
     'name'        => '위치 모듈',
     'description' => '기술',
-    'info' => 'On this page, you can manage country, city, district, neighborhood and village information.',
+    'info' => '이 페이지에서 국가, 도시, 지역, 이웃 및 마을 정보를 관리할 수 있습니다.',
 ];
