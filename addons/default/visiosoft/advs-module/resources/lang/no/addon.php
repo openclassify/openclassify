559a0653749a0b82227b7bf17d03d5016a7d386e
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:47:26 +0300

    New translations addon.php (Norwegian)

--- addons/default/visiosoft/advs-module/resources/lang/no/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/no/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Annonser',
     'name'        => 'Annonsemodul',
     'description' => 'Annonsemodul',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'Dette er skjermen der du kan legge til, slette og redigere annonser fra Annonser-siden. På denne siden kan du enkelt utføre operasjonene som postadministrasjon og annonsekonfigurasjon.'
 ];
