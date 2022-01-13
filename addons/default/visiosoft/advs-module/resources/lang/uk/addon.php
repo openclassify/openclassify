56b515e8b063a2b43f91ddffac81985cef4a508a
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:51:41 +0300

    New translations addon.php (Ukrainian)

--- addons/default/visiosoft/advs-module/resources/lang/uk/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/uk/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Оголошення',
     'name'        => 'Модуль реклами',
     'description' => 'Модуль реклами',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'Це екран, на якому ви можете додавати, видаляти та редагувати оголошення зі сторінки «Оголошення». На цій сторінці ви можете просто виконувати такі операції, як керування публікаціями та налаштування оголошень.'
 ];
