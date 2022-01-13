d9c5681f6b288fb77355d02b54f17e8d4ef68947
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:47:56 +0300

    New translations addon.php (Polish)

--- addons/default/visiosoft/advs-module/resources/lang/pl/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/pl/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Reklamy',
     'name'        => 'Moduł reklam',
     'description' => 'Moduł reklam',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'To jest ekran, na którym możesz dodawać, usuwać i edytować reklamy ze strony Reklamy. Na tej stronie możesz po prostu wykonać operacje, takie jak zarządzanie postami i konfiguracja reklam.'
 ];
