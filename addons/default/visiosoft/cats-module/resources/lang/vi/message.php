c1e85cc0039a3013776fccbba7b7b73b8a4af734
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:54:57 +0300

    New translations message.php (Vietnamese)

--- addons/default/visiosoft/cats-module/resources/lang/vi/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/vi/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Danh mục và các tiểu mục liên quan đã được xóa thành công!',
+    'cat_slug_exists' => 'Đã tồn tại một danh mục có slug là: slug!',
 ];
