1b68c95da0553e034d0836784c8dde4502b51480
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:54:07 +0300

    New translations control_panel.php (Urdu (Pakistan))

--- addons/default/visiosoft/defaultadmin-theme/resources/lang/ur/control_panel.php
+++ addons/default/visiosoft/defaultadmin-theme/resources/lang/ur/control_panel.php
@@ -10,4 +10,7 @@ return [
 
     // Users Module
     'search_by_gsm_number'     => 'جی ایس ایم نمبر کے ذریعہ تلاش کریں',
+
+    // Adv Export
+    'choose_file_type' => 'برآمد کے لیے فائل کی قسم کا انتخاب کریں۔',
 ];
