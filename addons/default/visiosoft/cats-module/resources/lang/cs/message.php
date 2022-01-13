7f47b704fc6ef19b7938504a46af1047c9070be8
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:40:25 +0300

    New translations message.php (Czech)

--- addons/default/visiosoft/cats-module/resources/lang/cs/message.php
+++ addons/default/visiosoft/cats-module/resources/lang/cs/message.php
@@ -2,4 +2,5 @@
 
 return [
     'categories_mass_delete_success' => 'Kategorie a související podkategorie byly úspěšně smazány!',
+    'cat_slug_exists' => 'Kategorie se slugem :slug již existuje!',
 ];
