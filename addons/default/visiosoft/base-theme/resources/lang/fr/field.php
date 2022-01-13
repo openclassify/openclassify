3f053bc87a166822a46959dd20d42fb61897283f
Fatih Alp <mail@fatihalp.com>
2022-01-13 11:31:57 +0300

    New translations field.php (French)

--- addons/default/visiosoft/base-theme/resources/lang/fr/field.php
+++ addons/default/visiosoft/base-theme/resources/lang/fr/field.php
@@ -58,8 +58,8 @@ return [
     'instruction_list' => 'Liste d\'instructions',
 
     // Offline
-    'connection_problem' => 'Connection problem',
-    'your_internet_connection_is_broken' => 'Your internet connection is broken,',
-    'we_miss_you_so_much_already' => 'we miss you so much already!',
-    'try_again' => 'Try again',
+    'connection_problem' => 'Problème de connection',
+    'your_internet_connection_is_broken' => 'Votre connexion internet est interrompue,',
+    'we_miss_you_so_much_already' => 'tu nous manque déjà tellement !',
+    'try_again' => 'Essayer à nouveau',
 ];
