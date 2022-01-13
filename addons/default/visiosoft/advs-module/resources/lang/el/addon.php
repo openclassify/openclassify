c7774454f59414095664a94c952d6989ccb46d4b
Fatih Alp <mail@fatihalp.com>
2022-01-13 12:41:43 +0300

    New translations addon.php (Greek)

--- addons/default/visiosoft/advs-module/resources/lang/el/addon.php
+++ addons/default/visiosoft/advs-module/resources/lang/el/addon.php
@@ -4,5 +4,5 @@ return [
     'title'       => 'Διαφημίσεις',
     'name'        => 'Ενότητα διαφημίσεων',
     'description' => 'Ενότητα διαφημίσεων',
-    'info' => 'This is the screen where you can add, delete and edit ads from the Ads page. On this page, you can simply do the operations such as post management and ad configuration.'
+    'info' => 'Αυτή είναι η οθόνη όπου μπορείτε να προσθέσετε, να διαγράψετε και να επεξεργαστείτε διαφημίσεις από τη σελίδα Διαφημίσεις. Σε αυτήν τη σελίδα, μπορείτε απλώς να κάνετε τις λειτουργίες όπως η διαχείριση αναρτήσεων και η διαμόρφωση διαφημίσεων.'
 ];
