6b80eec36b67441db533ab3634d44412452291be
Fatih Alp <mail@fatihalp.com>
2022-01-18 14:12:47 +0300

    New translations button.php (Arabic)

--- addons/default/visiosoft/cats-module/resources/lang/ar/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/ar/button.php
@@ -5,4 +5,5 @@ return [
     'add_sub_category' => 'إضافة فئة فرعية',
     'sub_category' => 'عرض الفئة الفرعية',
     'new_placeholderforsearch' => 'Placeholderforsearch جديد',
+    'convert_main' => 'Convert Main Category',
 ];
