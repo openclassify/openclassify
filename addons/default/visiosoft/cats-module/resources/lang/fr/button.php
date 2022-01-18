1829221bc78ab9348fe2cac5e7d3ec1da0bec110
Fatih Alp <mail@fatihalp.com>
2022-01-18 14:12:43 +0300

    New translations button.php (French)

--- addons/default/visiosoft/cats-module/resources/lang/fr/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/fr/button.php
@@ -5,4 +5,5 @@ return [
     'add_sub_category' => 'Ajouter une sous catégorie',
     'sub_category' => 'Afficher la sous-catégorie',
     'new_placeholderforsearch' => 'Nouvel espace réservé pour la recherche',
+    'convert_main' => 'Convert Main Category',
 ];
