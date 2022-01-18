6f27630912d1460205db0286f74014d7a9ee6c76
Fatih Alp <mail@fatihalp.com>
2022-01-18 14:12:54 +0300

    New translations button.php (Hebrew)

--- addons/default/visiosoft/cats-module/resources/lang/he/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/he/button.php
@@ -5,4 +5,5 @@ return [
     'add_sub_category' => 'הוסף קטגוריית משנה',
     'sub_category' => 'הצג קטגוריית משנה',
     'new_placeholderforsearch' => 'מציין מקום חדש',
+    'convert_main' => 'Convert Main Category',
 ];
