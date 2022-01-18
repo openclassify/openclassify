668a3b05e07142bccd167b6a22b140ad43596316
Fatih Alp <mail@fatihalp.com>
2022-01-18 14:12:58 +0300

    New translations button.php (Korean)

--- addons/default/visiosoft/cats-module/resources/lang/ko/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/ko/button.php
@@ -5,4 +5,5 @@ return [
     'add_sub_category' => '하위 카테고리 추가',
     'sub_category' => '하위 카테고리 표시',
     'new_placeholderforsearch' => '새로운 자리 표시 자 검색',
+    'convert_main' => 'Convert Main Category',
 ];
