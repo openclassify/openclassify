c972a471938e3a43b54a9232c6e476660ed6bdfb
Fatih Alp <mail@fatihalp.com>
2022-01-18 14:12:29 +0300

    New translations button.php (Swedish)

--- addons/default/visiosoft/cats-module/resources/lang/sv/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/sv/button.php
@@ -5,4 +5,5 @@ return [
     'add_sub_category' => 'Lägg till underkategori',
     'sub_category' => 'Visa underkategori',
     'new_placeholderforsearch' => 'New Placeholderforsearch',
+    'convert_main' => 'Convert Main Category',
 ];
