e5a867282d6b716d33c30e518a4c19bcf0a2b3b8
Fatih Alp <mail@fatihalp.com>
2022-01-18 14:12:57 +0300

    New translations button.php (Japanese)

--- addons/default/visiosoft/cats-module/resources/lang/ja/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/ja/button.php
@@ -5,4 +5,5 @@ return [
     'add_sub_category' => 'サブカテゴリを追加',
     'sub_category' => 'サブカテゴリーを表示',
     'new_placeholderforsearch' => '検索用の新しいプレースホルダー',
+    'convert_main' => 'Convert Main Category',
 ];
