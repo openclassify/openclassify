999feb2133bdc9daa518543ebb3917304caf78ee
Fatih Alp <mail@fatihalp.com>
2022-01-18 14:12:40 +0300

    New translations button.php (Hindi)

--- addons/default/visiosoft/cats-module/resources/lang/hi/button.php
+++ addons/default/visiosoft/cats-module/resources/lang/hi/button.php
@@ -5,4 +5,5 @@ return [
     'add_sub_category' => 'उप श्रेणी जोड़ें',
     'sub_category' => 'उप श्रेणी दिखाएँ',
     'new_placeholderforsearch' => 'नई प्लेसहोल्डर खोज',
+    'convert_main' => 'Convert Main Category',
 ];
