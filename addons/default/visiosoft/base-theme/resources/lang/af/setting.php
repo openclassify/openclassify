971b45a224f52b90a599918312360c4a93601090
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:40 +0300

    New translations setting.php (Afrikaans)

--- addons/default/visiosoft/base-theme/resources/lang/af/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/af/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Registreer bladsy, standaardland vir telefoon',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
