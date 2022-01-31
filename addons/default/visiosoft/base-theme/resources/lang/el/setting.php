6b613cfdf5acd805ba3f1e39e39eed848523405a
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:46 +0300

    New translations setting.php (Greek)

--- addons/default/visiosoft/base-theme/resources/lang/el/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/el/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Σελίδα εγγραφής, προεπιλεγμένη χώρα για τηλέφωνο',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
