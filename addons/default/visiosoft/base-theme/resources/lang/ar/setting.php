4ccb2273a9d599c8bd448a25e0936fc072ddef57
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:41 +0300

    New translations setting.php (Arabic)

--- addons/default/visiosoft/base-theme/resources/lang/ar/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/ar/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'صفحة التسجيل ، البلد الافتراضي للهاتف',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
