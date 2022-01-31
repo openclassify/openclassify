bb28b0de42bbfca215254350224f3dd2a6f3a8c6
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:44 +0300

    New translations setting.php (Czech)

--- addons/default/visiosoft/base-theme/resources/lang/cs/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/cs/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Stránka registrace, výchozí země pro telefon',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
