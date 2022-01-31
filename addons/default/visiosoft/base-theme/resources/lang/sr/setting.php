aece5c68fc272a858628938b22b1d7f76ff82e86
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:52 +0300

    New translations setting.php (Serbian (Latin))

--- addons/default/visiosoft/base-theme/resources/lang/sr/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/sr/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Stranica za registraciju, zadata država za telefon',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
