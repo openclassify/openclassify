5fb97219590918fc7fabd67e4943b877d0e83cc8
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:48 +0300

    New translations setting.php (Hebrew)

--- addons/default/visiosoft/base-theme/resources/lang/he/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/he/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'דף הרשמה, ארץ ברירת המחדל לטלפון',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
