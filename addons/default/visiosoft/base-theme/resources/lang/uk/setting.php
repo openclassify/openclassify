8c0ddfa2cd2771ed8dcfce4463462b7bf34d8be8
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:27 +0300

    New translations setting.php (Ukrainian)

--- addons/default/visiosoft/base-theme/resources/lang/uk/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/uk/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Сторінка реєстрації, країна за замовчуванням для телефону',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
