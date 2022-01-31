9ca283337da0568eeccc3a19364d25de24afc4e6
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:34 +0300

    New translations setting.php (Azerbaijani)

--- addons/default/visiosoft/base-theme/resources/lang/az/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/az/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Qeydiyyat səhifəsi, telefon üçün standart ölkə',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
