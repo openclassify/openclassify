d7aba95ff53bb01b0046c4133be2b4c076b761ca
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:38 +0300

    New translations setting.php (Danish)

--- addons/default/visiosoft/base-theme/resources/lang/da/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/da/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Registrer side, standardland for telefon',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
