f64ab1fad9742f78f58484aef11a697369c375a4
Fatih Alp <mail@fatihalp.com>
2022-01-31 10:19:49 +0300

    New translations setting.php (Italian)

--- addons/default/visiosoft/base-theme/resources/lang/it/setting.php
+++ addons/default/visiosoft/base-theme/resources/lang/it/setting.php
@@ -42,5 +42,8 @@ return [
     ],
 	'default_country' => [
 		'name' => 'Pagina di registrazione, paese predefinito per il telefono',
-	]
+	],
+    'gallery_box_height' => [
+        'name' => 'Gallery Box Height',
+    ],
 ];
