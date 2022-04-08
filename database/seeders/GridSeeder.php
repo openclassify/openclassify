97f1221b1bb3ce536d3fef5c8e41725621e8f368
Mostafa Moradi <mostafa21tr@gmail.com>
2022-04-08 12:54:17 +0300

    database seeder was moved to demodata-extension

--- database/seeders/GridSeeder.php
+++ database/seeders/GridSeeder.php
@@ -1,18 +1,6 @@
 <?php
 
-use Anomaly\Streams\Platform\Model\Advs\AdvsCategoriesEntryModel;
-use Anomaly\Streams\Platform\Model\Options\OptionsAdvertisementEntryModel;
-use Anomaly\Streams\Platform\Model\Options\OptionsContactSettingsEntryModel;
-use Anomaly\Streams\Platform\Model\Options\OptionsGeneralSettingsEntryModel;
-use Anomaly\Streams\Platform\Model\Options\OptionsThemeSettingsEntryModel;
-use Anomaly\Streams\Platform\Model\Search\SearchCategoriesEntryModel;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Visiosoft\AdvsModule\City\CitySeeder;
-use Visiosoft\AdvsModule\Country\CountrySeeder;
-use Visiosoft\AdvsModule\District\DistrictSeeder;
-use Visiosoft\AdvsModule\Neighborhood\NeighborhoodSeeder;
-use Visiosoft\AdvsModule\Village\VillageSeeder;
 
 class GridSeeder extends Seeder
 {
