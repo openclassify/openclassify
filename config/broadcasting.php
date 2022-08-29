78a056853f857cdf92aa431a2616880d7a712bd5
spektra2147 <spektra2147@gmail.com>
2022-08-29 18:39:03 +0300

    broadcasting update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,17 +37,9 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'https',
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
+                'useTLS' => env('PUSHER_TLS', true)
             ],
         ],
-
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
